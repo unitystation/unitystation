1cd91a27c14d99187db3d5612384021a192b8336
Keir Smith <kaffse@gmail.com>
2016-10-30 16:13:39 +0000

    Added check for walking between tiles. Minor reshuffle
    + CheckPassable takes the characters current position and the direction
    they want to go, then returns if they can or not
    + Also added a debug onclick for the tiles so you can see if you can enter
    the tile above it or not.

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,51 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
-	public GameObject tile;
-	public GameObject tileGrid;
-	public int gridSizeX = 100;
-	public int gridSizeY = 100;
+namespace SS.GameLogic {
+	public class GameManager : MonoBehaviour {
+		public GameObject tile;
+		public GameObject tileGrid;
+		public int gridSizeX = 100;
+		public int gridSizeY = 100;
 
-	public GameObject playerCamera;
-	public float panSpeed = 10.0f;
+		public GameObject playerCamera;
+		public float panSpeed = 10.0f;
 
-	private Sprite[] spaceSheet;
-	private GameObject[,] grid;
+		public enum Direction {Up, Left, Down, Right};
+		public enum TileType {Space, Floor, Wall};
 
-	void InitTiles() {
-		grid = new GameObject[gridSizeX, gridSizeY];
-		//TODO find size of the sprite then use it to dynamically build the grid. Set to 32px right now
-		//int tileWidth = (int)tile.GetComponent<SpriteRenderer>().bounds.size.x;
-		//int tileHeight = (int)tile.GetComponent<SpriteRenderer>().bounds.size.y;
-		TileManager tileManager;
-		spaceSheet = Resources.LoadAll<Sprite>("turf/space"); //TODO replace this with AssetBundles for proper release
+		private Sprite[] spaceSheet;
+		private GameObject[,] grid;
 
-		for (int i = 0; i < gridSizeX; i++) {
-			for (int j = 0; j < gridSizeY; j++) {
-				grid[i, j] = Instantiate (tile);
-				grid[i, j].transform.position = new Vector3 (i * 32, j * 32);
-				grid[i, j].transform.SetParent (tileGrid.transform);
+		// Use this for initialization
+		void Start () {
+			InitTiles();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
 
-				tileManager = grid[i, j].GetComponent<TileManager>();
-				int randomSpaceTile = (int)Random.Range(0, 101);
-				tileManager.tileSprite = spaceSheet[randomSpaceTile];
+			Vector3 movement = new Vector3 (moveHorizontal, moveVertical) * panSpeed;
+
+			playerCamera.transform.position = playerCamera.transform.position + movement;
+		}
+
+		private void InitTiles() {
+			grid = new GameObject[gridSizeX, gridSizeY];
+			TileManager tileManager;
+			spaceSheet = Resources.LoadAll<Sprite>("turf/space"); //TODO replace this with AssetBundles for proper release
+
+			/*
+			TODO find size of the sprite then use it to dynamically build the grid. Set to 32px right now
+			int tileWidth = (int)spaceSheet[0].bounds.size.x;
+			int tileHeight = (int)spaceSheet[0].bounds.size.y;
+
+			Debug.Log(tileWidth + " " + tileHeight);
+			*/
+			for (int i = 0; i < gridSizeX; i++) {
+				for (int j = 0; j < gridSizeY; j++) {
+					grid[i, j] = Instantiate(tile);
+					grid[i, j].transform.position = new Vector3(i * 32, j * 32);
+					grid[i, j].transform.SetParent(tileGrid.transform);
+
+					tileManager = grid[i, j].GetComponent<TileManager>();
+					int randomSpaceTile = (int)Random.Range(0, 101);
+					tileManager.tileSprite = spaceSheet[randomSpaceTile];
+					tileManager.gridX = i;
+					tileManager.gridY = j;
+					tileManager.gameManager = gameObject.GetComponent<GameManager>();
+				}
 			}
 		}
-	}
 
-	// Use this for initialization
-	void Start () {
-		InitTiles();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		public bool CheckPassable(int gridX, int gridY, Direction direction) {
+			int newGridX = gridX;
+			int newGridY = gridY;
 
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical) * panSpeed;
+			Direction newDirection = Direction.Up;
 
-		playerCamera.transform.position = playerCamera.transform.position + movement;
+			switch (direction) {
+			case Direction.Up:
+				newGridY = newGridY + 1;
+				newDirection = Direction.Down;
+				break;
+			case Direction.Right:
+				newGridX = newGridX + 1;
+				newDirection = Direction.Left;
+				break;
+			case Direction.Down:
+				newGridY = newGridY - 1;
+				newDirection = Direction.Up;
+				break;
+			case Direction.Left:
+				newGridX = newGridX - 1;
+				newDirection = Direction.Right;
+				break;
+			}
+
+			if (newGridX > grid.GetLength(0) - 1 || newGridX < 0 || newGridY > grid.GetLength(1) - 1 || newGridY < 0) {
+				Debug.Log("Attempting to move off map");
+				return false;
+			}
+
+			return (
+				grid[gridX, gridY].GetComponent<TileManager>().passable[(int)direction] && 
+				grid[newGridX, newGridY].GetComponent<TileManager>().passable[(int)newDirection]
+			);
+		}
 	}
 }
--- Assets/scripts/TileManager.cs
+++ Assets/scripts/TileManager.cs
@@ -1,19 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SS.GameLogic;
 
 public class TileManager : MonoBehaviour {
+	public GameManager gameManager;
 	public Sprite tileSprite; 
+
+	public GameManager.TileType myTileType;
+	public bool[] passable;
+
+	public int gridX;
+	public int gridY;
 
 	private SpriteRenderer spriteRenderer;
 	private GameObject displaySprite;
 
 	// Use this for initialization
 	void Start () {
+		myTileType = GameManager.TileType.Space;
+		passable = new bool[4]{true, true, true, true};
 		displaySprite = transform.FindChild("DisplaySprite").gameObject;
 		spriteRenderer = displaySprite.GetComponent<SpriteRenderer>();
 		if (tileSprite) {
 			spriteRenderer.sprite = tileSprite; 
 		}
+	}
+
+	void OnMouseDown() {
+		Debug.Log("X: " + gridX + " Y: " + gridY);
+		Debug.Log(gameManager.CheckPassable(gridX, gridY, GameManager.Direction.Up));
 	}
 
 	//TODO long term this should change to a listener and simply changing the tileSprite will change the renderer sprite
