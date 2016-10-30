24c1c7c3a754fe39ef08323c21ba6fcb04c19b6e
Keir Smith <kaffse@gmail.com>
2016-10-30 14:48:07 +0000

    Tile tidy up and Resoruces folder
    + Space tiles now random
    + Can now set tile sprites via public method in each TileManager
    + Moved all turf to Resources folder so they can be easily loaded during
    development

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,31 +4,39 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	public GameObject tile;
 	public GameObject tileGrid;
-	public Sprite spaceSheet; //TODO find out how to get the entire sheet rather than just the first sprite
 	public int gridSizeX = 100;
 	public int gridSizeY = 100;
 
 	public GameObject playerCamera;
 	public float panSpeed = 10.0f;
 
+	private Sprite[] spaceSheet;
 	private GameObject[,] grid;
 
-	// Use this for initialization
-	void Start () {
+	void InitTiles() {
 		grid = new GameObject[gridSizeX, gridSizeY];
 		//TODO find size of the sprite then use it to dynamically build the grid. Set to 32px right now
 		//int tileWidth = (int)tile.GetComponent<SpriteRenderer>().bounds.size.x;
 		//int tileHeight = (int)tile.GetComponent<SpriteRenderer>().bounds.size.y;
 		TileManager tileManager;
+		spaceSheet = Resources.LoadAll<Sprite>("turf/space"); //TODO replace this with AssetBundles for proper release
+
 		for (int i = 0; i < gridSizeX; i++) {
 			for (int j = 0; j < gridSizeY; j++) {
 				grid[i, j] = Instantiate (tile);
 				grid[i, j].transform.position = new Vector3 (i * 32, j * 32);
 				grid[i, j].transform.SetParent (tileGrid.transform);
+
 				tileManager = grid[i, j].GetComponent<TileManager>();
-				tileManager.setSprite(spaceSheet); //TODO implement this
+				int randomSpaceTile = (int)Random.Range(0, 101);
+				tileManager.tileSprite = spaceSheet[randomSpaceTile];
 			}
 		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitTiles();
 	}
 	
 	// Update is called once per frame
--- Assets/scripts/TileManager.cs
+++ Assets/scripts/TileManager.cs
@@ -2,17 +2,27 @@
 using System.Collections;
 
 public class TileManager : MonoBehaviour {
-	public Sprite tileSprite; //TODO make private and set via method
+	public Sprite tileSprite; 
+
 	private SpriteRenderer spriteRenderer;
+	private GameObject displaySprite;
 
 	// Use this for initialization
 	void Start () {
-		GameObject displaySprite = transform.FindChild("DisplaySprite").gameObject;
+		displaySprite = transform.FindChild("DisplaySprite").gameObject;
 		spriteRenderer = displaySprite.GetComponent<SpriteRenderer>();
-		spriteRenderer.sprite = tileSprite; //TODO remove
+		if (tileSprite) {
+			spriteRenderer.sprite = tileSprite; 
+		}
 	}
 
+	//TODO long term this should change to a listener and simply changing the tileSprite will change the renderer sprite
 	public void setSprite (Sprite sprite) {
-		//TODO implement
+		tileSprite = sprite;
+		if (!gameObject.activeSelf) {
+			Debug.Log("Tile not active yet!");
+			return;
+		}
+		spriteRenderer.sprite = tileSprite; 
 	}
 }
