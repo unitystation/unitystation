4c724583140d0638c25e6be9b0a8f4c47e9a2fbe
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2016-11-04 18:08:14 +1000

    basic pete the goat
    he walks randomly
    like a stupid goat

---
+++ Assets/scripts/SS.NPC/NPC_Pete.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using MovementEffects;
+
+namespace SS.NPC{
+	
+public class NPC_Pete : MonoBehaviour {
+
+		private SpriteRenderer thisRend;
+		private bool isRight = false;
+
+
+	void Start () {
+			thisRend = GetComponent<SpriteRenderer> ();
+			//Snap to grid
+			//FIXME need to figure out the grid and how to round to it
+			Vector2 newPos = new Vector2 (Mathf.Round (transform.position.x / 100f) *100f , Mathf.Round (transform.position.y / 100f)* 100f);
+			transform.position = newPos;
+			Timing.RunCoroutine (RandMove (), "randmove");
+	}
+	
+		void OnDisable(){
+
+			Timing.KillCoroutines ("randmove");
+
+		}
+
+		IEnumerator<float>RandMove(){
+
+			float ranTime = Random.Range (5f, 15f);
+			Debug.Log (ranTime);
+			yield return Timing.WaitForSeconds (ranTime);
+
+			int ranDir = Random.Range (0, 4);
+
+			if (ranDir == 0) {
+				//Move Up
+				Vector2 movePos = new Vector2 (transform.position.x, transform.position.y + 32f);
+				transform.position = movePos;
+			
+			} else if (ranDir == 1) {
+				//Move Right
+				Vector2 movePos = new Vector2 (transform.position.x + 32f, transform.position.y);
+				transform.position = movePos;
+
+				if(!isRight){
+
+					isRight = true;
+					Flip ();
+				}
+			
+			} else if (ranDir == 2) {
+				//Move Down
+				Vector2 movePos = new Vector2 (transform.position.x, transform.position.y - 32f);
+				transform.position = movePos;
+			
+			} else if (ranDir == 3) {
+			//Move Left
+				Vector2 movePos = new Vector2 (transform.position.x - 32f, transform.position.y);
+				transform.position = movePos;
+				if(isRight){
+
+					isRight = false;
+					Flip ();
+				}
+			
+			}
+
+			Timing.RunCoroutine(RandMove (), "randmove");
+		
+		
+		}
+	
+		void Flip(){
+
+			Vector2 newScale = transform.localScale;
+			newScale.x = -newScale.x;
+			transform.localScale = newScale;
+
+
+		}
+}
+}
