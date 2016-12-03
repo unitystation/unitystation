13272b198aff1f7615856f6246c24aa34d8879e1
Keir Smith <kaffse@gmail.com>
2016-12-03 21:07:46 +0000

    First pass at atmos calc
    Very early work, currently untested
    Needs proper physics calc for rate of flow and tidy up of the coroutine

--- Assets/scripts/atmos/Atmospherics.cs
+++ Assets/scripts/atmos/Atmospherics.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace SS.Atmospherics {
-	public class Graph {
+	public class Graph : MonoBehaviour{
 		private List<Aperture> edges;
-		private List<Area> vertices;
+		private Hashtable vertices;
 
-		public Graph( List<Aperture> e, List<Area> v) {
-			e.CopyTo(edges);
-			v.CopyTo(vertices);
+		public Graph( List<Aperture> e, Hashtable v) {
+			edges = e;
+			vertices = v;
+		}
+
+		//TODO ensure there's fallback if the tick takes too long to process
+		public void PerformSimulationTick() {
+			IEnumerator coroutine = SimulationTick(edges, vertices);
+			StartCoroutine(coroutine);
+		}
+
+		private IEnumerator SimulationTick(List<Aperture> edges, Hashtable vertices) {
+			//Step 1: Iterate through all apetures, calculate rate of flow for both sides
+			Hashtable toProcess = new Hashtable();
+			//TODO can we make this parallel too?
+			foreach (Aperture edge in edges) {
+				edge.PerformFlowCalcluation(toProcess);
+			}
+
+			//Step 2: Iterate through all effected areas, apply flow from all affecting apertures
+			foreach (int id in toProcess.Keys) {
+				Area currentArea = (Area)vertices[id];
+				currentArea.pressure = currentArea.pressure + (float)toProcess[id];
+			}
+			yield return null;
 		}
 	}
 
 	public class Area {
+		public int id;
 		//Pressure measured in bar
 		public float pressure = 1.0f;
 		public float volume = 1.0f;
 		public bool breathable = true; //TODO change this for something more realistic
 
-		public Area(float p, float v, bool b) {
+		public Area(int i, float p, float v, bool b) {
+			id = i;
 			pressure = p;
 			volume = v;
 			breathable = b;
@@ -35,6 +59,27 @@ namespace SS.Atmospherics {
 			left = l;
 			right = r;
 			width = w;
+		}
+
+		public void PerformFlowCalcluation(Hashtable toProcess) {
+			//TODO replace with heat equation
+			float flow = 1.0f;
+			if (left.pressure >= right.pressure) {
+				AddToHashTable(toProcess, left, -flow);
+				AddToHashTable(toProcess, right, flow);
+			} else {
+				AddToHashTable(toProcess, left, flow);
+				AddToHashTable(toProcess, right, -flow);
+			}
+		}
+
+		private void AddToHashTable(Hashtable toProcess, Area area, float flow) {
+			if (toProcess.ContainsKey(area.id)) {
+				Area currentArea = (Area)toProcess[area.id];
+				toProcess[area.id] = currentArea.pressure + flow;
+			} else {
+				toProcess.Add(area.id, flow);
+			}
 		}
 	}		
 }
