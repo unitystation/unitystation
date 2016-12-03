4f6392d8f6bb362e14eaf54419b9bb2228d12ea0
Keir Smith <kaffse@gmail.com>
2016-12-03 17:36:06 +0000

    Added skeleton classes

---
+++ Assets/scripts/atmos/Atmospherics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS.Atmospherics {
+	public class Graph {
+		private List<Aperture> edges;
+		private List<Area> vertices;
+
+		public Graph( List<Aperture> e, List<Area> v) {
+			e.CopyTo(edges);
+			v.CopyTo(vertices);
+		}
+	}
+
+	public class Area {
+		//Pressure measured in bar
+		public float pressure = 1.0f;
+		public float volume = 1.0f;
+		public bool breathable = true; //TODO change this for something more realistic
+
+		public Area(float p, float v, bool b) {
+			pressure = p;
+			volume = v;
+			breathable = b;
+		}
+	}
+
+	public class Aperture {
+		private Area left;
+		private Area right;
+		private float width;
+
+		public Aperture(Area l, Area r, float w) {
+			left = l;
+			right = r;
+			width = w;
+		}
+	}		
+}
