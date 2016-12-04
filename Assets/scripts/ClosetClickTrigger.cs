1b1b6f6eb70eac26a3ead32c32215c2181a89657
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2016-12-04 21:03:06 +1000

    closetclicktrigger fix

--- Assets/scripts/ClosetClickTrigger.cs
+++ Assets/scripts/ClosetClickTrigger.cs
@@ -3,40 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayGroup;
 
-public class ClosetClickTrigger: MonoBehaviour {
+public class ClosetClickTrigger: MonoBehaviour
+{
 
-    public Sprite spriteClosed;
-    public Sprite spriteOpened;
+	public Sprite spriteClosed;
+	public Sprite spriteOpened;
 
-    private SpriteRenderer spriteRenderer;
-    private GameObject lockLight;
+	private SpriteRenderer spriteRenderer;
+	private GameObject lockLight;
 
-    // Use this for initialization
-    void Start() {
-        spriteRenderer = transform.FindChild("Sprite").GetComponent<SpriteRenderer>();
-        lockLight = transform.FindChild("LockLight").gameObject;
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		spriteRenderer = transform.FindChild ("Sprite").GetComponent<SpriteRenderer> ();
 
-    // Update is called once per frame
-    void Update() {
+		Transform searchLockLight = transform.FindChild ("LockLight");
+		if (searchLockLight != null) {
+			lockLight = searchLockLight.gameObject;
+		}
+	}
 
-    }
+	// Update is called once per frame
+	void Update ()
+	{
 
-    void OnMouseDown() {
-        if(PlayerManager.control.playerScript != null) {
-            var headingToPlayer = PlayerManager.control.playerScript.transform.position - transform.position;
-            var distance = headingToPlayer.magnitude;
+	}
 
-            if(distance <= 2f) {
-                SoundManager.control.sounds[3].Play(); 
-                if(spriteRenderer.sprite == spriteClosed) {
-                    spriteRenderer.sprite = spriteOpened;
-                    lockLight.SetActive(false);
-                } else {
-                    spriteRenderer.sprite = spriteClosed;
-                    lockLight.SetActive(true);
-                }
-            }
-        }
-    }
+	void OnMouseDown ()
+	{
+		if (PlayerManager.control.playerScript != null) {
+			var headingToPlayer = PlayerManager.control.playerScript.transform.position - transform.position;
+			var distance = headingToPlayer.magnitude;
+
+			if (distance <= 2f) {
+				SoundManager.control.sounds [3].Play (); 
+				if (spriteRenderer.sprite == spriteClosed) {
+					spriteRenderer.sprite = spriteOpened;
+					if (lockLight != null) {
+						lockLight.SetActive (false);
+					}
+				} else {
+					spriteRenderer.sprite = spriteClosed;
+					if (lockLight != null) {
+						lockLight.SetActive (true);
+					}
+				}
+			}
+		}
+	}
 }
