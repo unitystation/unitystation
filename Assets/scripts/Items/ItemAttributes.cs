9bc76463195105d915ff4bb351af3b0f2e4aa838
Christian Bender-Saebelkampf <Christian Bender-Saebelkampf>
2016-12-07 00:28:21 +0100

    merging

--- Assets/scripts/Items/ItemAttributes.cs
+++ Assets/scripts/Items/ItemAttributes.cs
@@ -4,12 +4,14 @@ using System.Collections;
 
 namespace UI {
 
+    public enum Size {
+        Small, Medium, Large
+    }
+
     public class ItemAttributes: MonoBehaviour {
 
-        public enum Size {
-            Small, Medium, Large
-        }
-
+        // item name. Doesn't really have be set, only useful if you want to check if what item it is.
+        public string itemName;
         //reference numbers for item on  inhands spritesheet. should be one corresponding to player facing down
         public int inHandReferenceRight; 
         public int inHandReferenceLeft;
@@ -18,12 +20,12 @@ namespace UI {
         //change values manually in the inspector -- wealth
 
         public Size size; //Small - can fit into any container Med - can fit into large containers but not pockets Large - can't fit in any container
-                            //This can easily be modified later - just adding a foundation upon which we can build
+                          //This can easily be modified later - just adding a foundation upon which we can build
 
+            
 
         // Use this for initialization
         void Start() {
-
         }
 
         // Update is called once per frame
