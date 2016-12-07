3bc224aee2019077566d18269e44b3fde128abaf
Christian Bender-Saebelkampf <Christian Bender-Saebelkampf>
2016-12-07 23:33:47 +0100

    added itemslot prefab

--- Assets/scripts/Items/ItemTriggers.cs
+++ Assets/scripts/Items/ItemTriggers.cs
@@ -15,19 +15,14 @@ namespace Items {
         }
 
         void OnMouseDown() {
-            Debug.Log("hello ");
             if(PlayerManager.control.playerScript != null) {
                 var headingToPlayer = PlayerManager.control.playerScript.transform.position - transform.position;
                 var distance = headingToPlayer.magnitude;
                 var direction = headingToPlayer / distance;
-                Debug.Log("hello !");
 
                 if(distance <= 2f && allowedPickUp) {
                     // try to add the item to hand
                     var r = ItemManager.control.TryToPickUpObject(this.gameObject);
-
-                    Debug.Log("hello " + r);
-
                 }
             }
         }
