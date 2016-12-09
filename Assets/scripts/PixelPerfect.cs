5389288c1f75e2ea9a6543771b3b817d2ddb5576
Christian Bender-Saebelkampf <Christian Bender-Saebelkampf>
2016-12-09 01:10:15 +0100

    added pixel perfect, resized items

---
+++ Assets/scripts/PixelPerfect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrthoSize: MonoBehaviour {
+
+    public int spriteSize = 32;
+    public int zoom = 2;
+
+    private int screenPixelsY = 0;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if(screenPixelsY != Screen.height) {
+            screenPixelsY = Screen.height;
+            var s_baseOrthographicSize = ((float) Screen.height) / spriteSize / (2 * zoom);
+            Camera.main.orthographicSize = s_baseOrthographicSize;
+        }
+    }
+}
