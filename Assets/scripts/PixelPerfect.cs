2199725c76e66443c3d0b8b9742aa4b5bb12911f
Christian Bender-Saebelkampf <Christian Bender-Saebelkampf>
2016-12-10 17:32:48 +0100

    changed table spritesheets to avoid bleeding edge bugs

--- Assets/scripts/PixelPerfect.cs
+++ Assets/scripts/PixelPerfect.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class PixelPerfect: MonoBehaviour {
 
     public int spriteSize = 32;
-    public int zoom = 2;
+    public float zoom = 2;
 
     private int screenPixelsY = 0;
 
-    void Start() {
+    private float currentZoom;
 
+    void Start() {
+        currentZoom = zoom;
     }
 
     void Update() {
-        if(screenPixelsY != Screen.height) {
+        if(screenPixelsY != Screen.height || currentZoom != zoom) {
             screenPixelsY = Screen.height;
+            currentZoom = zoom;
+
             var s_baseOrthographicSize = ((float) Screen.height) / spriteSize / (2 * zoom);
             Camera.main.orthographicSize = s_baseOrthographicSize;
         }
