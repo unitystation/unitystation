ef971ad2324eeeb7b91fcad6994ee458b163be7f
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2016-12-13 21:14:40 +1000

    adjusted UI_ItemSlot and UI_ClothingTrigger to search for player spawn, added RPC to ItemNetwork

--- Assets/scripts/Items/ItemNetwork.cs
+++ Assets/scripts/Items/ItemNetwork.cs
@@ -8,17 +8,68 @@ namespace Items
 {
     public class ItemNetwork : MonoBehaviour
     {
-
-        // Use this for initialization
+        
+        private Vector3 lastPos;
+        //Catch the last pos of the transform at the end of the frame
+        [HideInInspector]
+        public PhotonView photonView;
+          
         void Start()
         {
-		
+            photonView = gameObject.AddComponent<PhotonView>();
+            lastPos = transform.position;
         }
-	
-        // Update is called once per frame
-        void Update()
+
+         
+        void LateUpdate()
         {
-		
+            if (photonView != null)
+            {
+                if (transform.position != lastPos) //if the item has been moved by someone then update its transform to all other clients
+                {
+                    CallRemoteMethod(transform.position, transform.rotation);
+                }
+            }
+            lastPos = transform.position;
+
         }
+
+        public void CallRemoteMethod(Vector3 pos, Quaternion rot)
+        {
+            GetComponent<PhotonView>().RPC(
+                "UpdateItemTransform",
+                PhotonTargets.AllBufferedViaServer,
+                new object[] { pos, rot });
+
+
+        }
+
+        [PunRPC] 
+        void UpdateItemTransform(Vector3 pos, Quaternion rot) //Called on all clients for this PhotonView ID
+        {
+            transform.position = pos;
+            transform.rotation = rot;
+
+        }
+
+        // THIS IS AN EXAMPLE ON HOW TO USE SERIALIZEVIEW
+        //        void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        //        {
+        //
+        //            if (stream.isWriting)
+        //            {
+        //                stream.SendNext(transform.position);
+        //                stream.SendNext(transform.localScale);
+        //
+        //            }
+        //            else
+        //            {
+        //                transform.position = (Vector3)stream.ReceiveNext();
+        //                transform.localScale = (Vector3)stream.ReceiveNext();
+        //
+        //            }
+        //
+        //        }
+
     }
 }
