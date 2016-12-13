eae6c4bb9dcf158c9c0d4ce1240dd11cd6ecf1a4
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2016-12-13 20:26:10 +1000

    started item_network class to handle network sync

---
+++ Assets/scripts/Items/ItemNetwork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UI;
+using PlayGroup;
+
+namespace Items
+{
+    public class ItemNetwork : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+		
+        }
+	
+        // Update is called once per frame
+        void Update()
+        {
+		
+        }
+    }
+}
