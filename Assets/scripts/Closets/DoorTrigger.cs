e37e971565a33305bec9f7d4a5eca52304fec0d4
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2016-12-19 08:40:02 +1000

    Place Items in cupboard over network

--- Assets/scripts/Closets/DoorTrigger.cs
+++ Assets/scripts/Closets/DoorTrigger.cs
@@ -4,39 +4,40 @@ using System.Collections.Generic;
 using UI;
 using UnityEngine;
 
-namespace Cupboards{
-public class DoorTrigger: Photon.PunBehaviour
+namespace Cupboards
 {
-
-    public Sprite doorOpened;
-    public Vector2 offsetOpened;
-    public Vector2 sizeOpened;
-
-    private SpriteRenderer spriteRenderer;
-    private Vector2 offsetClosed;
-    private Vector2 sizeClosed;
-
-    private Sprite doorClosed;
-    private LockLightController lockLight;
-    private GameObject items;
-
-    private PhotonView photonView;
-    private bool synced = false;
-
-    private bool closed = true;
-
-    void Start()
+    public class DoorTrigger: Photon.PunBehaviour
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        doorClosed = spriteRenderer.sprite;
-        lockLight = transform.GetComponentInChildren<LockLightController>();
 
-        offsetClosed = GetComponent<BoxCollider2D>().offset;
-        sizeClosed = GetComponent<BoxCollider2D>().size;
+        public Sprite doorOpened;
+        public Vector2 offsetOpened;
+        public Vector2 sizeOpened;
 
-        items = transform.FindChild("Items").gameObject;
+        private SpriteRenderer spriteRenderer;
+        private Vector2 offsetClosed;
+        private Vector2 sizeClosed;
 
-        photonView = gameObject.GetComponent<PhotonView>();
+        private Sprite doorClosed;
+        private LockLightController lockLight;
+        private GameObject items;
+
+        private PhotonView photonView;
+        private bool synced = false;
+
+        private bool closed = true;
+
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            doorClosed = spriteRenderer.sprite;
+            lockLight = transform.GetComponentInChildren<LockLightController>();
+
+            offsetClosed = GetComponent<BoxCollider2D>().offset;
+            sizeClosed = GetComponent<BoxCollider2D>().size;
+
+            items = transform.FindChild("Items").gameObject;
+
+            photonView = gameObject.GetComponent<PhotonView>();
 
             if (PhotonNetwork.connectedAndReady)
             {
@@ -44,204 +45,220 @@ public class DoorTrigger: Photon.PunBehaviour
                 StartSync();
             }
 
-    }
+        }
 
-    void OnMouseDown()
-    {
-        if (PlayerManager.control.playerScript != null)
+        void OnMouseDown()
         {
-            var headingToPlayer = PlayerManager.control.playerScript.transform.position - transform.position;
-            var distance = headingToPlayer.magnitude;
-
-            if (distance <= 2f)
+            if (PlayerManager.control.playerScript != null)
             {
-                if (lockLight != null && lockLight.IsLocked())
+                var headingToPlayer = PlayerManager.control.playerScript.transform.position - transform.position;
+                var distance = headingToPlayer.magnitude;
+
+                if (distance <= 2f)
                 {
-                    if (PhotonNetwork.connectedAndReady)
+                    if (lockLight != null && lockLight.IsLocked())
                     {
-                        photonView.RPC("LockLight", PhotonTargets.All, null);
+                        if (PhotonNetwork.connectedAndReady)
+                        {
+                            photonView.RPC("LockLight", PhotonTargets.All, null);
+                        }
+                        else
+                        {
+                            lockLight.Unlock();
+                        }
                     }
                     else
                     {
-                        lockLight.Unlock();
-                    }
-                }
-                else
-                {
-                    SoundManager.control.Play("OpenClose");
-                    if (closed)
-                    {
-                        if (PhotonNetwork.connectedAndReady)
+                        SoundManager.control.Play("OpenClose");
+                        if (closed)
                         {
-                            photonView.RPC("Open", PhotonTargets.All, null);
+                            if (PhotonNetwork.connectedAndReady)
+                            {
+                                photonView.RPC("Open", PhotonTargets.All, null);
+                            }
+                            else
+                            {
+                                Open();
+                            }
                         }
-                        else
+                        else if (!TryDropItem())
                         {
-                            Open();
-                        }
-                    }
-                    else if (!TryDropItem())
-                    {
-                        if (PhotonNetwork.connectedAndReady)
-                        {
-                            photonView.RPC("Close", PhotonTargets.All, null);
-                        }
-                        else
-                        {
-                            Close();
+                            if (PhotonNetwork.connectedAndReady)
+                            {
+                                photonView.RPC("Close", PhotonTargets.All, null);
+                            }
+                            else
+                            {
+                                Close();
+                            }
                         }
                     }
                 }
             }
         }
-    }
 
 
-    [PunRPC]
-    void Open()
-    {
-        closed = false;
-        spriteRenderer.sprite = doorOpened;
-        if (lockLight != null)
+        [PunRPC]
+        void Open()
         {
-            lockLight.Hide();
-        }
-        GetComponent<BoxCollider2D>().offset = offsetOpened;
-        GetComponent<BoxCollider2D>().size = sizeOpened;
+            closed = false;
+            spriteRenderer.sprite = doorOpened;
+            if (lockLight != null)
+            {
+                lockLight.Hide();
+            }
+            GetComponent<BoxCollider2D>().offset = offsetOpened;
+            GetComponent<BoxCollider2D>().size = sizeOpened;
 
-        ShowItems();
-    }
-
-    [PunRPC]
-    void Close()
-    {
-        closed = true;
-        spriteRenderer.sprite = doorClosed;
-        if (lockLight != null)
-        {
-            lockLight.Show();
-        }
-        GetComponent<BoxCollider2D>().offset = offsetClosed;
-        GetComponent<BoxCollider2D>().size = sizeClosed;
-
-        HideItems();
-    }
-
-    [PunRPC]
-    void LockLight()
-    {
-        if (lockLight != null)
-        {
-            lockLight.Unlock();
-        }
-    }
-
-    private bool TryDropItem()
-    {
-        GameObject item = UIManager.control.hands.CurrentSlot.Clear();
-
-        if (item != null)
-        {
-            item.transform.parent = items.transform;
-            item.transform.localPosition = new Vector3(0, 0, -0.2f);
-
-            return true;
+            ShowItems();
         }
 
-        return false;
-    }
+        [PunRPC]
+        void Close()
+        {
+            closed = true;
+            spriteRenderer.sprite = doorClosed;
+            if (lockLight != null)
+            {
+                lockLight.Show();
+            }
+            GetComponent<BoxCollider2D>().offset = offsetClosed;
+            GetComponent<BoxCollider2D>().size = sizeClosed;
 
-    private void ShowItems()
-    {
-        items.SetActive(true);
-    }
+            HideItems();
+        }
 
-    private void HideItems()
-    {
-        items.SetActive(false);
-    }
-
-    //PUN Sync
-    [PunRPC]
-    void SendCurrentState(string playerRequesting)
-    {
-        if (PhotonNetwork.isMasterClient)
+        [PunRPC]
+        void LockLight()
         {
             if (lockLight != null)
             {
-                photonView.RPC("ReceiveCurrentState", PhotonTargets.Others, playerRequesting, lockLight.IsLocked(), closed, transform.parent.transform.position); //Gather the values and send back
-            }
-            else
-            {
-                photonView.RPC("ReceiveCurrentState", PhotonTargets.Others, playerRequesting, false, closed, transform.parent.transform.position); //Gather the values and send back 
+                lockLight.Unlock();
             }
         }
-    }
 
-    [PunRPC]
-    void ReceiveCurrentState(string playerIdent, bool isLocked, bool isClosed, Vector3 pos)
-    {
-        if (PhotonNetwork.player.NickName == playerIdent)
+        [PunRPC]
+        void DropItem(int itemViewID)
         {
-            if (closed != isClosed) //Opened or closed
-            {
-                if (isClosed)
-                {
-                    Close();
+            GameMatrix.control.items[itemViewID].transform.parent = items.transform;
+            GameMatrix.control.items[itemViewID].transform.localPosition = new Vector3(0, 0, -0.2f);
+        }
 
+        private bool TryDropItem()
+        {
+            GameObject item = UIManager.control.hands.CurrentSlot.Clear();
+
+            if (item != null)
+            {
+                //TODO add to all cupboards on all clients
+                if (PhotonNetwork.connectedAndReady)
+                {
+                    PhotonView itemView = item.GetComponent<PhotonView>();
+                    photonView.RPC("DropItem", PhotonTargets.All, itemView.viewID);
                 }
                 else
                 {
-                    Open();
+                    item.transform.parent = items.transform;
+                    item.transform.localPosition = new Vector3(0, 0, -0.2f);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ShowItems()
+        {
+            items.SetActive(true);
+        }
+
+        private void HideItems()
+        {
+            items.SetActive(false);
+        }
+
+        //PUN Sync
+        [PunRPC]
+        void SendCurrentState(string playerRequesting)
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                if (lockLight != null)
+                {
+                    photonView.RPC("ReceiveCurrentState", PhotonTargets.Others, playerRequesting, lockLight.IsLocked(), closed, transform.parent.transform.position); //Gather the values and send back
+                }
+                else
+                {
+                    photonView.RPC("ReceiveCurrentState", PhotonTargets.Others, playerRequesting, false, closed, transform.parent.transform.position); //Gather the values and send back 
                 }
             }
-            if (lockLight != null)
+        }
+
+        [PunRPC]
+        void ReceiveCurrentState(string playerIdent, bool isLocked, bool isClosed, Vector3 pos)
+        {
+            if (PhotonNetwork.player.NickName == playerIdent)
             {
-                if (isLocked != lockLight.IsLocked()) // Locked or unlocked
+                if (closed != isClosed) //Opened or closed
                 {
+                    if (isClosed)
+                    {
+                        Close();
+
+                    }
+                    else
+                    {
+                        Open();
+                    }
+                }
+                if (lockLight != null)
+                {
+                    if (isLocked != lockLight.IsLocked()) // Locked or unlocked
+                    {
             
-                    if (isLocked)
-                    {
-                        lockLight.Lock();
-                    }
-                    else
-                    {
-                        lockLight.Unlock();
+                        if (isLocked)
+                        {
+                            lockLight.Lock();
+                        }
+                        else
+                        {
+                            lockLight.Unlock();
+                        }
                     }
                 }
-            }
 
-            if (transform.parent.transform.position != pos) //Position of cupboard
-            {
-                transform.parent.transform.position = pos;
+                if (transform.parent.transform.position != pos) //Position of cupboard
+                {
+                    transform.parent.transform.position = pos;
+                }
             }
         }
-    }
 
-    //PUN Callbacks
+        //PUN Callbacks
 
-    public override void OnJoinedRoom()
-    {
-        StartSync();
-    }
-
-    void StartSync()
-    {
-        if (!synced)
+        public override void OnJoinedRoom()
         {
-            if (!PhotonNetwork.isMasterClient)
-            {
-                //If you are not the master then update the current IG state of this object from the master
-                photonView.RPC("SendCurrentState", PhotonTargets.MasterClient, PhotonNetwork.player.NickName);
-            }
-
-            GameMatrix.control.AddCupboard(photonView.viewID, this);
-            synced = true;
+            StartSync();
         }
+
+        void StartSync()
+        {
+            if (!synced)
+            {
+                if (!PhotonNetwork.isMasterClient)
+                {
+                    //If you are not the master then update the current IG state of this object from the master
+                    photonView.RPC("SendCurrentState", PhotonTargets.MasterClient, PhotonNetwork.player.NickName);
+                }
+
+                GameMatrix.control.AddCupboard(photonView.viewID, this);
+                synced = true;
+            }
+        }
+
+
     }
-
-
-}
 }
 
 //TODOS
