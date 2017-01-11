25e056bfd420ff11900ce0c4428c3d4ec777cdf9
pulsey <pulseyyyyy@gmail.com>
2017-01-12 00:12:56 +0100

    added corpse after gibbing pete

--- Assets/scripts/NPC/Kill.cs
+++ Assets/scripts/NPC/Kill.cs
@@ -6,11 +6,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Network;
 
-public class Kill : MonoBehaviour
-{
+public class Kill: Photon.MonoBehaviour {
 
     public Sprite deadSprite;
     public GameObject meatPrefab;
+    public GameObject corpsePrefab;
     public int amountSpawn = 1;
 
     private SpriteRenderer spriteRenderer;
@@ -18,57 +18,33 @@ public class Kill : MonoBehaviour
     private PhysicsMove physicsMove;
     private bool dead = false;
     private bool sliced = false;
-    private PhotonView photonView;
 
-    void Start()
-    {
+    void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
         randomMove = GetComponent<RandomMove>();
         physicsMove = GetComponent<PhysicsMove>();
-        photonView = GetComponent<PhotonView>();
     }
 
-    void OnMouseDown()
-    {
+    void OnMouseDown() {
 
-        if (!dead && UIManager.Hands.CurrentSlot.Item != null)
-        {
-            if (UIManager.Hands.CurrentSlot.Item.GetComponent<ItemAttributes>().type == ItemType.Knife)
-            {
+        if(!dead && UIManager.Hands.CurrentSlot.Item != null) {
+            if(UIManager.Hands.CurrentSlot.Item.GetComponent<ItemAttributes>().type == ItemType.Knife) {
                 Debug.Log("Knife clicked to kill");
-                if (PhotonNetwork.connectedAndReady)
-                {
-                    photonView.RPC("Die", PhotonTargets.All, null); //Send death to all clients for pete
-                }
-                else
-                {
-                    Die(); //Dev mode
-                }
+                photonView.RPC("Die", PhotonTargets.All, null); //Send death to all clients for pete
             }
-        }
-        else if (UIManager.Hands.CurrentSlot.Item != null && dead && !sliced)
-        {    
-            if (UIManager.Hands.CurrentSlot.Item.GetComponent<ItemAttributes>().type == ItemType.Knife)
-            {
-  
-                if (PhotonNetwork.connectedAndReady)
-                {   
-                    photonView.RPC("SpawnMeat", PhotonTargets.MasterClient, null); //Spawn the new meat
-                    photonView.RPC("RemoveFromNetwork", PhotonTargets.MasterClient, null); // Remove pete from the network
-                }
-                else
-                {
-                    SpawnMeat(); //For dev mode
-                    Destroy(gameObject); 
-                }
+        } else if(UIManager.Hands.CurrentSlot.Item != null && dead && !sliced) {
+            if(UIManager.Hands.CurrentSlot.Item.GetComponent<ItemAttributes>().type == ItemType.Knife) {
+
+                photonView.RPC("Gib", PhotonTargets.MasterClient, null); //Spawn the new meat
+                photonView.RPC("RemoveFromNetwork", PhotonTargets.MasterClient, null); // Remove pete from the network
+
                 sliced = true;
             }
         }
     }
 
     [PunRPC]
-    void Die()
-    {
+    void Die() {
         dead = true;
         randomMove.enabled = false;
         physicsMove.enabled = false;
@@ -83,22 +59,16 @@ public class Kill : MonoBehaviour
     }
 
     [PunRPC]
-    void SpawnMeat()
-    {
-        for (int i = 0; i < amountSpawn; i++)
-        {
-            if (PhotonNetwork.connectedAndReady)
-            {
-                if (PhotonNetwork.isMasterClient)
-                {
-                    NetworkItemDB.Instance.MasterClientCreateItem(meatPrefab.name, transform.position, Quaternion.identity, 0, null); //Create scene owned object
-                } 
+    void Gib() {
+        if(PhotonNetwork.isMasterClient) {
+            // Spawn Meat
+            for(int i = 0; i < amountSpawn; i++) {
+                NetworkItemDB.Instance.MasterClientCreateItem(meatPrefab.name, transform.position); //Create scene owned object
             }
-            else
-            { //Dev mode
-                var meat = Instantiate(meatPrefab); 
-                meat.transform.position = transform.position;
-            }
+
+            // Spawn Corpse
+            NetworkItemDB.Instance.MasterClientCreateItem(corpsePrefab.name, transform.position); //Create scene owned object
+
         }
     }
 }
