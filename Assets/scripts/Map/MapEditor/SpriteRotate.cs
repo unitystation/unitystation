afb864336de6df3813e7ba03072c5c1707f33f66
krille90 <pulseyyyyy@gmail.com>
2017-01-31 00:29:48 +0100

    map editor refactoring and improving

---
+++ Assets/scripts/Map/MapEditor/SpriteRotate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRotate: MonoBehaviour {
+    public Sprite[] sprites = new Sprite[0];
+    private SpriteRenderer spriteRenderer;
+    
+    private int spriteIndex;
+    public int SpriteIndex {
+        get { return spriteIndex; }
+        set {
+            if(spriteRenderer && sprites.Length > 1) {
+                spriteIndex = (value + sprites.Length) % sprites.Length;
+                spriteRenderer.sprite = sprites[SpriteIndex];
+            }
+        }
+    }
+
+    void Awake() {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void RotateForwards() {
+        SpriteIndex++;
+    }
+
+    public void RotateBackwards() {
+        SpriteIndex--;
+    }
+}
