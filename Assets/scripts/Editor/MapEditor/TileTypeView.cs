351c64ce059fb1259e10b1c1ca12e95e326e3f69
krille90 <pulseyyyyy@gmail.com>
2017-02-09 01:06:01 +0100

    fixed some map parts

---
+++ Assets/scripts/Editor/MapEditor/TileTypeView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TileTypeView : EditorWindow {
+
+    [MenuItem("Window/Tile Type View")]
+    public static void ShowWindow() {
+        GetWindow<TileTypeView>("Tile Type View");
+    }
+
+    public void OnEnable() {
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
+    }
+
+    public void OnDisable() {
+        SceneView.onSceneGUIDelegate -= OnSceneGUI;
+    }
+
+    void OnSceneGUI(SceneView sceneView) {
+        Gizmos.DrawCube(new Vector3(1778, 1136, 0), Vector3.one);
+        HandleUtility.Repaint();
+    }
+}
---
+++ Assets/scripts/Map/MatrixDrawer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MatrixDrawer : MonoBehaviour {
+
+    public bool allowGizmos;
+
+    void Update() {
+
+    }
+}
+
+public class MyScriptGizmoDrawer {
+
+    [DrawGizmo(GizmoType.Active | GizmoType.NonSelected)]
+    static void DrawGizmo(MatrixDrawer scr, GizmoType gizmoType) {
+        if(!scr.allowGizmos)
+            return;
+
+        var color = Color.blue;
+        color.a = 0.5f;
+
+        Gizmos.color = color;
+
+        for(int y = 1000; y < 1300; y++) {
+            for(int x = 1700; x < 2000; x++) {
+                if(!Matrix.Matrix.At(x, y).IsPassable())
+                    Gizmos.DrawCube(new Vector3(x, y, 0), Vector3.one);
+            }
+        }
+    }
+}
