0e95dbc1e9fd7cfe3d0aa6bb5dffb54d473524f7
LEEROU <claptrappp@gmail.com>
2017-03-30 19:41:02 +0400

    Blocked doors fix

--- Assets/scripts/Doors/DoorController.cs
+++ Assets/scripts/Doors/DoorController.cs
@@ -4,114 +4,137 @@ using PlayGroup;
 using Matrix;
 using Sprites;
 
-public class DoorController: Photon.PunBehaviour
+public class DoorController : Photon.PunBehaviour
 {
-	public AudioSource openSFX;
-	public AudioSource closeSFX;
-	private Animator animator;
-	private BoxCollider2D boxColl;
-	private RegisterTile registerTile;
-	public bool usingAnimator = true;
-	public bool isWindowedDoor = false;
-	[HideInInspector]
-	public bool isPerformingAction = false;
-	public DoorType doorType;
-	public float maxTimeOpen = 5;
-	private float timeOpen = 0;
-	private HorizontalDoorAnimator horizontalAnim;
+    public AudioSource openSFX;
+    public AudioSource closeSFX;
+    private Animator animator;
+    private BoxCollider2D boxColl;
+    private RegisterTile registerTile;
+    public bool usingAnimator = true;
+    public bool isWindowedDoor = false;
+    [HideInInspector]
+    public bool isPerformingAction = false;
+    public DoorType doorType;
+    public float maxTimeOpen = 5;
+    private float timeOpen = 0;
+    private HorizontalDoorAnimator horizontalAnim;
 
     public bool IsOpened { get; private set; }
 
-	void Start()
-	{
-		animator = gameObject.GetComponent<Animator>();
-		boxColl = gameObject.GetComponent<BoxCollider2D>();
-		registerTile = gameObject.GetComponent<RegisterTile>();
-		if (!usingAnimator) {
-			//TODO later change usingAnimator to horizontal checker (when vertical doors are done)
-			horizontalAnim = gameObject.AddComponent<HorizontalDoorAnimator>();
-		}
-	}
-		
-	public void BoxCollToggleOn()
-	{
-		registerTile.UpdateTileType(TileType.Door);
-		boxColl.enabled = true;
-	}
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        boxColl = gameObject.GetComponent<BoxCollider2D>();
+        registerTile = gameObject.GetComponent<RegisterTile>();
+        if (!usingAnimator)
+        {
+            //TODO later change usingAnimator to horizontal checker (when vertical doors are done)
+            horizontalAnim = gameObject.AddComponent<HorizontalDoorAnimator>();
+        }
+    }
 
-	public void BoxCollToggleOff()
-	{
-		registerTile.UpdateTileType(TileType.None);
-		boxColl.enabled = false;
-	}
+    public void BoxCollToggleOn()
+    {
+        registerTile.UpdateTileType(TileType.Door);
+        boxColl.enabled = true;
+    }
 
-	private IEnumerator _WaitUntilClose()
-	{
-		// After the door opens, wait until it's supposed to close.
-		yield return new WaitForSeconds(maxTimeOpen);
-		TryClose();
-	}
+    public void BoxCollToggleOff()
+    {
+        registerTile.UpdateTileType(TileType.None);
+        boxColl.enabled = false;
+    }
 
-	//3d sounds
-	public void PlayOpenSound()
-	{
-		if (openSFX != null)
-			openSFX.Play();
-	}
+    private bool DoorIsBlocked()
+    {
+        return Physics2D.BoxCast(gameObject.transform.position, new Vector2(0.1f, 0.1f), 0, new Vector2(1, 1), 0.1f, 1 << LayerMask.NameToLayer("Players"));
+    }
 
-	public void PlayCloseSound()
-	{
-		if (closeSFX != null)
-			closeSFX.Play();
-	}
+    private IEnumerator _WaitUntilClose()
+    {
+        // After the door opens, wait until it's supposed to close.
+        yield return new WaitForSeconds(maxTimeOpen);
+        TryClose();
+    }
 
-	public void PlayCloseSFXshort()
-	{
-		if (closeSFX != null) {
-			closeSFX.time = 0.6f;
-			closeSFX.Play();
-		}
-	}
+    //3d sounds
+    public void PlayOpenSound()
+    {
+        if (openSFX != null)
+            openSFX.Play();
+    }
 
-	public void TryOpen()
-	{
-		if (PhotonNetwork.connectedAndReady) {
-			photonView.RPC("Open", PhotonTargets.All, null);
-		} 
-	}
+    public void PlayCloseSound()
+    {
+        if (closeSFX != null)
+            closeSFX.Play();
+    }
 
-	public void TryClose()
-	{
-		if (PhotonNetwork.connectedAndReady) {
-			photonView.RPC("Close", PhotonTargets.All, null);
-		} 
-	}
+    public void PlayCloseSFXshort()
+    {
+        if (closeSFX != null)
+        {
+            closeSFX.time = 0.6f;
+            closeSFX.Play();
+        }
+    }
 
-	[PunRPC]
-	public void Open()
-	{
+    public void TryOpen()
+    {
+        if (PhotonNetwork.connectedAndReady)
+        {
+            photonView.RPC("Open", PhotonTargets.All, null);
+        }
+    }
+
+    public void TryClose()
+    {
+        if (PhotonNetwork.connectedAndReady)
+        {
+            if (DoorIsBlocked())
+            {
+                StartCoroutine(_WaitUntilClose());
+                return;
+            }
+            photonView.RPC("Close", PhotonTargets.All, null);
+        }
+    }
+
+    [PunRPC]
+    public void Open()
+    {
         IsOpened = true;
-		StartCoroutine(_WaitUntilClose());
+        StartCoroutine(_WaitUntilClose());
 
-		if (usingAnimator) {
-			animator.SetBool("open", true);
-		} else {
-			if (!isPerformingAction) {
-				horizontalAnim.OpenDoor();
-			}
-		}
-	}
+        if (usingAnimator)
+        {
+            animator.SetBool("open", true);
+        }
+        else
+        {
+            if (!isPerformingAction)
+            {
+                horizontalAnim.OpenDoor();
+            }
+        }
+    }
 
-	[PunRPC]
-	public void Close()
-	{
+    [PunRPC]
+    public void Close()
+    {
         IsOpened = false;
-		if (usingAnimator) {
-			animator.SetBool("open", false);
-		} else {
-			if (!isPerformingAction) {
-				horizontalAnim.CloseDoor();
-			}
-		}
-	}
+        if (usingAnimator)
+        {
+            animator.SetBool("open", false);
+        }
+        else
+        {
+            if (!isPerformingAction)
+            {
+                horizontalAnim.CloseDoor();
+            }
+        }
+    }
+
 }
