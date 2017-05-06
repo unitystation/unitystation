e89bf1be25e35aec79135bebb7b5d0045b909e29
krille90 <pulseyyyyy@gmail.com>
2017-05-06 15:50:05 +0200

    More strict layering, such that bullets ignore open doors, but stop at closed doors. Bullets should now stop at closed shutters

--- Assets/scripts/Doors/ShutterController.cs
+++ Assets/scripts/Doors/ShutterController.cs
@@ -9,36 +9,26 @@ public class ShutterController : MonoBehaviour {
     private RegisterTile registerTile;
 
     public bool IsClosed { get; private set; }
+
     void Start() {
         animator = gameObject.GetComponent<Animator>();
         registerTile = gameObject.GetComponent<RegisterTile>();
 
         registerTile.UpdateTileType(TileType.None);
-
+        gameObject.layer = LayerMask.NameToLayer("Door Open");
     }
 
     public void Open() {
         IsClosed = false;
         registerTile.UpdateTileType(TileType.None);
+        gameObject.layer = LayerMask.NameToLayer("Door Open");
         animator.SetBool("close", false);
     }
 
     public void Close() {
         IsClosed = true;
         registerTile.UpdateTileType(TileType.Door);
+        gameObject.layer = LayerMask.NameToLayer("Door Closed");
         animator.SetBool("close", true);
-    }
-
-    public void SyncState(bool isClosed) {
-        IsClosed = isClosed;
-
-        animator.SetBool("close", IsClosed);
-
-        if(IsClosed) {
-            animator.Play("04_shuttersClose", -1, 1);
-        } else {
-            animator.Play("04_shuttersOpen", -1, 1);
-        }
-
     }
 }
