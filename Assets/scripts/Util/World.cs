2981432cf69362991966808049180899ac0a9fd0
Necromunger <lil_tommy_tom@hotmail.com>
2017-05-12 21:59:17 +1000

    More work on item stacking and weapons
    More work on item stacking and weapons

---
+++ Assets/scripts/Util/World.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class World
+{
+	public static List<Collider2D> GetGameObjectsWithinTile(Vector2 position, LayerMask layerMask)
+	{
+		var snappedPosition = new Vector2 (Mathf.Round (position.x), Mathf.Round (position.y));
+
+		var objects = Physics2D.OverlapBoxAll(snappedPosition, new Vector2(0.9f, 0.9f), 0, 1 << layerMask).ToList();
+
+		return objects;
+	}
+
+	public static void ReorderGameobjectsOnTile(Vector2 position) {
+		//get number of objects already at this position order them and then make the new item the highest
+		List<Collider2D> colliders = World.GetGameObjectsWithinTile(position, LayerMask.NameToLayer("Items"));
+		colliders.Reverse ();
+		int orderCount = 1;
+		for (int i = 0; i < colliders.Count; i++) {
+			var collider = colliders [i];
+			var sRenderer = collider.gameObject.GetComponentInChildren<SpriteRenderer> ();
+			if (sRenderer != null) {
+				sRenderer.sortingOrder = orderCount;
+			}
+			orderCount++;
+		}
+	}
+}
+
