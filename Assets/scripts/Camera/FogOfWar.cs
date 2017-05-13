622aacc5cb3b1588e82ddeb48ae1f760d50d04a9
Necromunger <lil_tommy_tom@hotmail.com>
2017-05-13 20:29:56 +1000

    Updated shrouds + Added new tile lighting shader (#282)
    * Round restart on land man standing + Optional weapon recoil
    
    Round restart on land man standing + Optional weapon recoil
    
    * Added firing sound selectors to weapons
    
    Added firing sound selectors to weapons
    
    * Updated shrouds + Added new tile lighting shader
    
    Updated shrouds + Added new tile lighting shader

--- Assets/scripts/Camera/FogOfWar.cs
+++ Assets/scripts/Camera/FogOfWar.cs
@@ -15,27 +15,20 @@ public class FogOfWar : MonoBehaviour
     private Vector2 lastDirection;
     public int WallLayer = 9;
 
-	//FIXME make this secure, set it up for the demo
-	public void TurnOffShroud(){
-		foreach (KeyValuePair<Vector2,GameObject> s in shroudTiles) {
-			s.Value.SetActive(false);
+	GameObject ShroudContainer;
+
+	struct Line {
+		public Vector2 start;
+		public Vector2 end;
+		public Color color;
+		public Line(Vector2 start, Vector2 end, Color color) {
+			this.start = start;
+			this.end = end;
+			this.color = color;
 		}
-		this.enabled = false;
 	}
 
-    // This should return the current GameObject which is providing vision
-    // into the fog of war - such as a security camera or a player
-    public GameObject GetSightSource()
-    {
-        // TODO Support security cameras etc
-        return PlayerManager.LocalPlayer;
-    }
-
-    // TODO Support security cameras etc
-    public Vector2 GetSightSourceDirection()
-    {
-        return PlayerManager.LocalPlayer.GetComponent<PlayerSprites>().currentDirection;
-    }
+	List<Line> GizmoRays = new List<Line>();
 
     // Update is called once per frame
     public void Update()
@@ -46,6 +39,8 @@ public class FogOfWar : MonoBehaviour
 
         if (transform.hasChanged)
         {
+			GizmoRays.Clear ();
+
             transform.hasChanged = false;
 
             if (transform.position == lastPosition && GetSightSourceDirection() == lastDirection)
@@ -56,6 +51,23 @@ public class FogOfWar : MonoBehaviour
             lastDirection = GetSightSourceDirection();
         }
     }
+
+	void OnDrawGizmos() {
+		if (GizmoRays != null) {
+			foreach (Line line in GizmoRays) {
+				Gizmos.color = line.color;
+				Gizmos.DrawLine(line.start, line.end);
+			}
+		}
+	}
+
+	//FIXME make this secure, set it up for the demo
+	public void TurnOffShroud(){
+		foreach (KeyValuePair<Vector2,GameObject> s in shroudTiles) {
+			s.Value.SetActive(false);
+		}
+		this.enabled = false;
+	}
 
     // Returns all shroud nodes in field of vision
     public List<Vector2> GetInFieldOfVision(List<Vector2> inputShrouds)
@@ -72,7 +84,7 @@ public class FogOfWar : MonoBehaviour
                 continue;
             }
 
-            // In front cone
+            //In front cone
             if (Vector3.Angle(shroudTiles[inputShroud].transform.position - GetSightSource().transform.position, GetSightSourceDirection()) < FieldOfVision)
             {
                 inFieldOFVision.Add(inputShroud);
@@ -94,45 +106,72 @@ public class FogOfWar : MonoBehaviour
         // Loop through all tiles that are nearby and are in field of vision
         foreach (Vector2 inFieldOfVisionShroud in GetInFieldOfVision(nearbyShrouds))
         {
-            // There is a slight issue with linecast where objects directly diagonal to you are not hit by the cast
-            // and since we are standing next to the tile we should always be able to view it, lets always deactive the shroud
-            if (Vector2.Distance(inFieldOfVisionShroud, GetSightSource().transform.position) < 2)
-            {
-                SetShroudStatus(inFieldOfVisionShroud, false);
-                continue;
-            }
-
-            // Everything else:
-
-            // Perform a linecast to see if a wall is blocking vision of the target tile
             int WallLayerMask = 1 << WallLayer;
             int LayerMask = WallLayerMask;
-            RaycastHit2D hit = Physics2D.Linecast(GetSightSource().transform.position, inFieldOfVisionShroud, LayerMask);
 
-            // If it hits a wall we should enable the shroud
-            if (hit)
-            {
-                if (new Vector2(hit.transform.position.x, hit.transform.position.y) != inFieldOfVisionShroud)
-                {
-                    // Enable shroud, a wall was in the way
-                    SetShroudStatus(inFieldOfVisionShroud, true);
-                    continue;
-                }
-                else
-                {
-                    // Disable shroud, the wall was our target tile
-                    SetShroudStatus(inFieldOfVisionShroud, false);
-                    continue;
-                }
-            }
-            else
-            {
-                // Vision of tile not blocked by wall, disable the shroud
-                SetShroudStatus(inFieldOfVisionShroud, false);
-                continue;
-            }
+			//get the points on the face we should raytrace
+			var tileFacePoints = GetFacePointsForTile(inFieldOfVisionShroud);
+
+			//check if any points made contact
+			RaycastHit2D hit = new RaycastHit2D();
+
+			bool isClear = false;
+
+			//for each shroud tile
+			foreach (Vector2 targetTile in tileFacePoints) {
+				RaycastHit2D hitTest = Physics2D.Linecast(GetSightSource().transform.position, targetTile, LayerMask);
+
+				//hit an object that we want to keep
+				if (hitTest.transform != null && new Vector2(hitTest.transform.position.x, hitTest.transform.position.y) == inFieldOfVisionShroud)
+				{
+					isClear = true;
+					GizmoRays.Add(new Line(GetSightSource().transform.position, targetTile, new Color (1, 0, 0, 0.5F)));
+				}
+				//no obstruction for ray against testing tile
+				else if (hitTest.transform == null) {
+					isClear = true;
+					GizmoRays.Add(new Line(GetSightSource().transform.position, targetTile, new Color (0, 0, 1, 0.5F)));
+				}
+			}
+
+			if (isClear) {
+				// Vision of tile not blocked by wall, disable the shroud
+				SetShroudStatus (inFieldOfVisionShroud, false);
+				continue;
+			} else {
+				// Enable shroud, a wall was in the way
+				SetShroudStatus(inFieldOfVisionShroud, true);
+				continue;
+			}
         }
     }
+
+	public List<Vector2> GetFacePointsForTile(Vector2 referenceVector) {
+		//get the points on the face we should raytrace
+		var topFace = new Vector2(referenceVector.x, referenceVector.y + 0.50f);
+		var bottomFace = new Vector2(referenceVector.x, referenceVector.y - 0.50f);
+		var leftFace = new Vector2(referenceVector.x - 0.50f, referenceVector.y);
+		var rightFace = new Vector2(referenceVector.x + 0.50f, referenceVector.y);
+
+		var topLeftFace = new Vector2(referenceVector.x - 0.60f, referenceVector.y + 0.60f);
+		var topRightFace = new Vector2(referenceVector.x + 0.60f, referenceVector.y + 0.60f);
+		var bottomLeftFace = new Vector2(referenceVector.x - 0.60f, referenceVector.y - 0.60f);
+		var bottomRightFace = new Vector2(referenceVector.x + 0.60f, referenceVector.y - 0.60f);
+
+		//collect points
+		List<Vector2> checkPoints = new List<Vector2>();
+		checkPoints.Add (topFace);
+		checkPoints.Add (bottomFace);
+		checkPoints.Add (leftFace);
+		checkPoints.Add (rightFace);
+
+		checkPoints.Add (topLeftFace);
+		checkPoints.Add (topRightFace);
+		checkPoints.Add (bottomLeftFace);
+		checkPoints.Add (bottomRightFace);
+
+		return checkPoints;
+	}
 
     // Changes a shroud to on or off
     public void SetShroudStatus(Vector2 vector2, bool enabled)
@@ -147,7 +186,12 @@ public class FogOfWar : MonoBehaviour
     // Adds new shroud to our cache and marks it as enabled
     public GameObject RegisterNewShroud(Vector2 vector2, bool active)
     {
+		if (ShroudContainer == null) {
+			ShroudContainer = new GameObject("Shroud Container");
+		}
+
         GameObject shroudObject = Instantiate(ShroudPrefab, new Vector3(vector2.x, vector2.y, 0), Quaternion.identity);
+		shroudObject.transform.parent = ShroudContainer.transform;
         shroudTiles.Add(vector2, shroudObject);
         SetShroudStatus(vector2, active);
         return shroudObject;
@@ -180,4 +224,18 @@ public class FogOfWar : MonoBehaviour
 
         return nearbyShroudTiles;
     }
+
+	// This should return the current GameObject which is providing vision
+	// into the fog of war - such as a security camera or a player
+	public GameObject GetSightSource()
+	{
+		// TODO Support security cameras etc
+		return PlayerManager.LocalPlayer;
+	}
+
+	// TODO Support security cameras etc
+	public Vector2 GetSightSourceDirection()
+	{
+		return PlayerManager.LocalPlayer.GetComponent<PlayerSprites>().currentDirection;
+	}
 }
