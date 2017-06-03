ba9aa2a7da8f90d0734ce200f48f5bf38a6b164c
Jeremy Liberman <jliberman@strenuus.com>
2017-06-03 02:42:07 -0500

    Remove unnecessary garbage allocation in Weapon.Update()

--- Assets/scripts/Equipment/Equipment.cs
+++ Assets/scripts/Equipment/Equipment.cs
@@ -121,7 +121,7 @@ namespace Equipment
 		public void SetHandItem(string eventName, GameObject obj)
 		{
 			ItemAttributes att = obj.GetComponent<ItemAttributes>();
-			EquipmentPool.AddGameObject(gameObject.name, obj);
+			EquipmentPool.AddGameObject(gameObject, obj);
             SetHandItemSprite(eventName, att);
             RpcSendMessage(eventName, obj);
 		}
@@ -155,7 +155,7 @@ namespace Equipment
 			GameObject item = Instantiate(prefab, Vector2.zero, Quaternion.identity) as GameObject;
 			NetworkServer.Spawn(item);
 			ItemAttributes att = item.GetComponent<ItemAttributes>();
-			EquipmentPool.AddGameObject(gameObject.name, item);
+			EquipmentPool.AddGameObject(gameObject, item);
 
 			playerNetworkActions.TrySetItem(eventName, item);
 			//Sync all clothing items across network using SyncListInt syncEquipSprites
