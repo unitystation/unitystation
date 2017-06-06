c81dbb833c90c4543df34ca9f35b8497c6d72f4e
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2017-06-07 08:53:17 +1000

    Removed unityengine.editor from compilation

--- Assets/scripts/Test/TgObjectTest.cs
+++ Assets/scripts/Test/TgObjectTest.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using FullSerializer;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
-
 public class TgObjectTest {
-
+	#if UNITY_EDITOR
     [MenuItem("Tools/Test object")]
+	#endif
     public static void DeserializeJson()
     {
         var asset = Resources.Load(Path.Combine("metadata", "dm")) as TextAsset;
@@ -18,21 +20,17 @@ public class TgObjectTest {
         List<Dictionary<string, string>> diclist = null;
         var serializer = new fsSerializer();// psr
         serializer.TryDeserialize(data, ref diclist).AssertSuccessWithoutWarnings();
-        Debug.Log(diclist.ToArray()[0].Keys.Aggregate("", (current, key) => current + (key + ": ") + diclist.ToArray()[0][key] + "\n"));
-
-        
+        Debug.Log(diclist.ToArray()[0].Keys.Aggregate("", (current, key) => current + (key + ": ") + diclist.ToArray()[0][key] + "\n"));   
     }
 
     public static void DeserializeJsonMini()
     {
         var asset = Resources.Load(Path.Combine("metadata", "dm1")) as TextAsset;
-
         var data = fsJsonParser.Parse(asset.text);
         Dictionary<string, string> dic = null;
         var serializer = new fsSerializer();// psr
         serializer.TryDeserialize(data, ref dic).AssertSuccessWithoutWarnings();
-        Debug.Log(dic.Keys.Aggregate("", (current, key) => current + (key + ": ") + dic[key] + "\n"));
-        
+        Debug.Log(dic.Keys.Aggregate("", (current, key) => current + (key + ": ") + dic[key] + "\n")); 
     }
 		
     [Serializable]
