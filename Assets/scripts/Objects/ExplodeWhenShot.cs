695e8ebd798a1054bee9c8d9f3a1d6d71da1acfc
Jeremy Liberman <jliberman@strenuus.com>
2017-06-11 20:58:40 -0500

    Fuel tanks deal damage to nearby players when they explode

--- Assets/scripts/Objects/ExplodeWhenShot.cs
+++ Assets/scripts/Objects/ExplodeWhenShot.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 using UnityEngine.Networking;
 using PlayGroup;
 
-public class ExplodeWhenShot : NetworkBehaviour {
-	string[] explosions = new[] {"Explosion1", "Explosion2"};
+public class ExplodeWhenShot : NetworkBehaviour
+{
+	public int damage = 150;
+	public float radius = 3f;
 
-	void OnTriggerEnter2D(Collider2D coll){
+	const int MAX_TARGETS = 44;
+
+	readonly string[] explosions = new[] { "Explosion1", "Explosion2" };
+	readonly Collider2D[] colliders = new Collider2D[MAX_TARGETS];
+
+	int playerMask;
+	int obstacleMask;
+
+	void Start()
+	{
+		playerMask = LayerMask.GetMask("Players");
+		obstacleMask = LayerMask.GetMask("Walls", "Door Closed");
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
 		var bullet = coll.GetComponent<BulletBehaviour>();
 		if (bullet != null) {
 			if (isServer) {
@@ -23,12 +40,32 @@ public class ExplodeWhenShot : NetworkBehaviour {
 	#if !ENABLE_PLAYMODE_TESTS_RUNNER
 	[Server]
 	#endif
-	public void Explode(string bulletOwnedBy) {
-		// TODO: Damage people
+	public void Explode(string bulletOwnedBy)
+	{
+		var pos = (Vector2)transform.position;
+		var length = Physics2D.OverlapCircleNonAlloc(pos, radius, colliders, playerMask);
+
 		NetworkServer.Destroy(gameObject);
+
+		for (int i = 0; i < length; i++) {
+			var collider = colliders[i];
+			var living = collider.gameObject.GetComponent<Living>();
+			if (living != null) {
+				var livingPos = (Vector2)living.transform.position;
+				var distance = Vector3.Distance(pos, livingPos);
+
+				var hit = Physics2D.Raycast(pos, livingPos - pos, distance, obstacleMask);
+				if (hit.collider == null) {
+					var effect = 1 - ((distance * distance) / (radius * radius));
+					var actualDamage = (int)(damage * effect);
+					HurtPeople(living, bulletOwnedBy, actualDamage);
+				}
+			}
+		}
 	}
-		
-	void GoBoom() {
+
+	void GoBoom()
+	{
 		// Instantiate a clone of the source so that multiple explosions can play at the same time.
 		var name = explosions[Random.Range(0, explosions.Length)];
 		var source = SoundManager.Instance[name];
@@ -38,5 +75,10 @@ public class ExplodeWhenShot : NetworkBehaviour {
 
 		var parent = Resources.Load<GameObject>("effects/FireRing");
 		Instantiate(parent, transform.position, Quaternion.identity);
+	}
+
+	internal virtual void HurtPeople(Living living, string damagedBy, int damage)
+	{
+		living.RpcReceiveDamage(damagedBy, damage);
 	}
 }
