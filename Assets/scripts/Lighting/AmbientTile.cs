4d21636f056dd00c0d5e9eea031ae8e7d8b105b2
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2017-06-28 23:21:06 +1000

    removed temp clothfactory button. Added AmbientTile

---
+++ Assets/scripts/Lighting/AmbientTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InputControl;
+
+public class AmbientTile : ObjectTrigger {
+
+	private Color onColor = new Color32(133,133,133,255);
+	private Color offColor = new Color32(0,0,0,255);
+	private SpriteRenderer spriteRend;
+
+	void Start(){
+		spriteRend = GetComponent<SpriteRenderer>();
+	}
+
+	public override void Trigger(bool state)
+	{
+		spriteRend.color = state ? onColor : offColor;
+	}
+}
