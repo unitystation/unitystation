555d213fc55365ef6f92f7f0787c6a53c03990a6
krille90 <christian.bender.saebelkampf@gmail.com>
2017-07-28 01:50:27 +0200

    added BuildServer config and build.bat script

---
+++ Assets/scripts/Editor/BuildServer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildServer {
+    static void PerformBuild() {
+        var buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/scenes/Lobby.unity", "Assets/scenes/Deathmatch.unity" };
+        buildPlayerOptions.locationPathName = "Builds/server/server";
+        buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
+        buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
