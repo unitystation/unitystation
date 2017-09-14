a213f82e22e34bc2afd3965ce2d288fc6e0d40f7
fomaltd <fomaltd@gmail.com>
2017-09-15 00:39:47 +0300

    included some NetTransform send experiments

--- Assets/scripts/Equipment/ObjectPool.cs
+++ Assets/scripts/Equipment/ObjectPool.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,10 +12,14 @@ namespace Equipment{
 public class ObjectPool : MonoBehaviour {
 
 		public Dictionary<NetworkIdentity,ItemAttributes> currentObjects = new Dictionary<NetworkIdentity, ItemAttributes>();
+	private bool IsWaiting;
+	private bool AbortDelayedDrop;
 
-		public void AddGameObject(GameObject obj){
+	public void AddGameObject(GameObject obj){
 			obj.transform.position = transform.position;
 			obj.transform.parent = transform;
+//			lastSync = Time.time;
+	
 			NetworkIdentity id = obj.GetComponent<NetworkIdentity>();
 			ItemAttributes att = obj.GetComponent<ItemAttributes>();
 
@@ -31,6 +36,7 @@ public class ObjectPool : MonoBehaviour {
 		DropGameObject(gObj, Vector3.zero);
 	}
 
+//	private float lastSync;
 	//When dropping items etc, remove them from the player equipment pool and place in scene
 		public void DropGameObject(GameObject gObj, Vector3 dropPos)
 		{
@@ -43,13 +49,47 @@ public class ObjectPool : MonoBehaviour {
 			{
 				if ( !dropPos.Equals(Vector3.zero) )
 				{
-					currentObjects[id].gameObject.transform.parent = null;
-					currentObjects[id].gameObject.transform.position = dropPos;
-					EditModeControl e = gObj.GetComponent<EditModeControl>();
-					e.Snap();
+					var o = currentObjects[id].gameObject;
+//					var delta = Time.time - lastSync;
+//					if ( delta > 0.25f )
+//					{
+						DropNow(o, dropPos);
+//					}
+//					else
+//					{
+//						StartCoroutine(DropWait(o, dropPos, delta));
+//					}
+					
 				}
 				currentObjects.Remove(id);
 			}
 		}
+
+//	private IEnumerator DropWait(GameObject gObj, Vector3 dropPos, float waitTime)
+//	{
+//		AbortDelayedDrop = false;
+//		if (!IsWaiting)
+//		{
+//			IsWaiting = true;
+//			Debug.LogFormat("DropWait {0}", waitTime);
+//			yield return new WaitForSeconds(waitTime);
+//			IsWaiting = false;
+//			if(!AbortDelayedDrop) DropNow(gObj, dropPos);
+//		}
+//		else
+//		{
+//			AbortDelayedDrop = true;
+//			DropNow(gObj, dropPos);
+//		}
+//	}
+
+	private void DropNow(GameObject gObj, Vector3 dropPos)
+	{
+		gObj.transform.parent = null;
+		gObj.transform.position = dropPos;
+		EditModeControl e = gObj.GetComponent<EditModeControl>();
+		e.Snap();
+//		lastSync = Time.time;
+	}
 }
 }
