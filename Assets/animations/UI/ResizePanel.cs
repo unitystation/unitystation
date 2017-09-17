2e3b0a002237233299b88d3ce284ac379325b421
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2017-09-17 11:00:32 +1000

    Added resize mouse cursor
    - Also fixed a bug where the height of the panel would change sometimes
    when resizing the width

--- Assets/animations/UI/ResizePanel.cs
+++ Assets/animations/UI/ResizePanel.cs
@@ -5,7 +5,8 @@ using UnityEngine.EventSystems;
 
 namespace UI
 {
-	public class ResizePanel : MonoBehaviour, IPointerDownHandler, IDragHandler
+	public class ResizePanel : MonoBehaviour, IPointerDownHandler,
+	IPointerUpHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 	{
 		//TODO handle max size on x and hide panel when below min x (showing the transparent chatbox)
 		public Vector2 minSize = new Vector2(100, 100);
@@ -16,6 +17,8 @@ namespace UI
 		private Vector2 originalLocalPointerPosition;
 		private Vector2 originalSizeDelta;
 		private Rect originalRect;
+		private bool isDragging = false;
+		public Texture2D resizeCursor;
 
 		void Awake()
 		{
@@ -28,7 +31,12 @@ namespace UI
 			originalSizeDelta = panelRectTransform.sizeDelta;
 			originalRect = panelRectTransform.rect;
 			RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out originalLocalPointerPosition);
+			isDragging = true;
+		}
 
+		public void OnPointerUp(PointerEventData data)
+		{
+			isDragging = false;
 		}
 
 		public void OnDrag(PointerEventData data)
@@ -40,7 +48,7 @@ namespace UI
 			RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out localPointerPosition);
 			Vector3 offsetToOriginal = localPointerPosition - originalLocalPointerPosition;
 
-			Vector2 sizeDelta = originalSizeDelta + new Vector2 (-offsetToOriginal.x, originalSizeDelta.y);
+			Vector2 sizeDelta = originalSizeDelta + new Vector2 (-offsetToOriginal.x, 0f);
 			//TODO handle the min and max actions
 //			sizeDelta = new Vector2 (
 //				Mathf.Clamp (sizeDelta.x, minSize.x, maxSize.x),
@@ -48,6 +56,17 @@ namespace UI
 //			);
 
 			panelRectTransform.sizeDelta = sizeDelta;
+		}
+
+		public void OnPointerEnter(PointerEventData data)
+		{
+			Cursor.SetCursor (resizeCursor, Vector2.zero, CursorMode.Auto);
+		}
+
+		public void OnPointerExit(PointerEventData data)
+		{
+			if(!isDragging)
+			Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 		}
 	}
 }
