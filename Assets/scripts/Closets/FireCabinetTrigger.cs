c2d7a11197dc7385d6f61de1899827eaf504e1dc
dooblynoobly <dooblynoobly@smashingpixels.com.au>
2017-09-24 21:29:48 +1000

    FireCabinetTrigger now working as intended
    Fixes:
    - FireCabinet now spawns an extinguisher on start and stores it in its
    internal storage by referencing the ObjectBehaviour
    - Fixed Extinguisher inHand sprites
    - Fixed cabinet on new client sync issues
    - Fire Extinguisher can now be put back into the cabinet and the door
    closed

---
+++ Assets/scripts/Closets/FireCabinetTrigger.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using PlayGroup;
+using UI;
+using Items;
+using InputControl;
+
+public class FireCabinetTrigger : InputTrigger
+{
+	public Sprite spriteClosed;
+	public Sprite spriteOpenedOccupied;
+	public Sprite spriteOpenedEmpty;
+
+	public GameObject itemPrefab;
+
+	[SyncVar(hook = "SyncCabinet")]
+	public bool IsClosed;
+
+	[SyncVar(hook = "SyncItemSprite")]
+	public bool isFull;
+	private SpriteRenderer spriteRenderer;
+	private bool sync = false;
+	private bool hasJustPlaced = false;
+	//For storing extinguishers server side
+	[HideInInspector]
+	public ObjectBehaviour storedObject;
+
+	void Start()
+	{
+		spriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+	}
+
+	public override void OnStartServer()
+	{
+		if (spriteRenderer == null)
+			spriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+		IsClosed = true;
+		isFull = true;
+
+		GameObject item = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+		NetworkServer.Spawn(item);
+		storedObject = item.GetComponent<ObjectBehaviour>();
+		storedObject.visibleState = false;
+		base.OnStartServer();
+	}
+	public override void OnStartClient()
+	{
+		StartCoroutine(WaitForLoad());
+		base.OnStartClient();
+	}
+
+	IEnumerator WaitForLoad()
+	{
+		yield return new WaitForSeconds(3f);
+		SyncCabinet(IsClosed);
+		SyncItemSprite(isFull);
+	}
+
+	public override void Interact(GameObject originator, string hand)
+	{
+		if (IsClosed) {
+			HandleInteraction(false, hand);
+		} else {
+			if (isFull && !hasJustPlaced) {
+				if (!UIManager.Hands.CurrentSlot.IsFull) {
+					HandleInteraction(true, hand);
+				} else {
+					HandleInteraction(false, hand);
+				}
+			} else {
+				GameObject handItem = UIManager.Hands.CurrentSlot.Item;
+					if (handItem != null) {
+						if (handItem.GetComponent<ItemAttributes>().itemName == "Extinguisher") {
+							HandleInteraction(true, hand);
+							hasJustPlaced = true;
+					} else {
+						HandleInteraction(false, hand);
+						hasJustPlaced = false;	
+					}
+				} else {
+					HandleInteraction(false, hand);
+					hasJustPlaced = false;
+				}
+			} 
+		}
+	}
+
+	private void HandleInteraction(bool forItemInteract, string currentHand){
+		PlayerManager.LocalPlayerScript.playerNetworkActions.CmdToggleFireCabinet(
+			gameObject, forItemInteract, currentHand);
+	}
+
+	public void SyncItemSprite(bool _isFull)
+	{
+		isFull = _isFull;
+		if (!isFull) {
+			spriteRenderer.sprite = spriteOpenedEmpty;
+		} else {
+			if(!IsClosed)
+			spriteRenderer.sprite = spriteOpenedOccupied;
+		}
+	}
+
+	void SyncCabinet(bool _isClosed)
+	{
+		IsClosed = _isClosed;
+		if (_isClosed) {
+			Close();
+		} else {
+			Open();
+		}
+	}
+
+	void Open()
+	{
+		PlaySound();
+		if (isFull) {
+			spriteRenderer.sprite = spriteOpenedOccupied;
+		} else {
+			spriteRenderer.sprite = spriteOpenedEmpty;
+		}
+	}
+
+	void Close()
+	{
+		PlaySound();
+		spriteRenderer.sprite = spriteClosed;
+	}
+
+	void PlaySound()
+	{
+		if (!sync) {
+			sync = true;
+		} else {
+			SoundManager.PlayAtPosition("OpenClose", transform.position);
+		}
+	}
+
+	private bool IsCorrectItem(GameObject item)
+	{
+		return item.GetComponent<ItemAttributes>().itemName == itemPrefab.GetComponent<ItemAttributes>().itemName;
+	}
+}
