e73dea8e922e94a0e4b46f737575cd453592ef51
DooblyNoobly <dooblynoobly@smashingpixels.com.au>
2017-10-03 21:19:23 +1000

    StructureWire prefab adjustments
    Changes:
    - Moved the SpriteRenderer to a child GameObject
    - Moved the wiring code into the Wiring namespace
    - Created a wiring editor script to handle manually setting the wiring
    configuration in editor mode.
    
    This is WIP, I’ve only built the editor script in this commit, have not
    added the functionality to set the config yet. Also need to have
    configurations change using the rotate keys via the map editor.

---
+++ Assets/Scripts/Editor/Wires/EditorStructurePowerWire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Wiring
+{
+	/// <summary>
+	/// This StructurePowerWire editor script helps set the correct
+	/// wires when building maps
+	/// </summary>
+	[CustomEditor(typeof(StructurePowerWire))]
+	public class EditorStructurePowerWire : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+
+			StructurePowerWire sTarget = (StructurePowerWire)target;
+			serializedObject.Update();
+
+			EditorGUILayout.HelpBox("The starting dir of this wire in a turf, " +
+									"using 4 bits to indicate N S E W - 1 2 4 8\r\n" +
+									"Corners can also be used i.e.: 5 = NE (1 + 4) = 0101\r\n" +
+									"This is the edge of the location where the wire enters the turf", MessageType.Info);
+			SerializedProperty DirectionStart = serializedObject.FindProperty("DirectionStart");
+			EditorGUILayout.PropertyField((DirectionStart));
+
+			EditorGUILayout.HelpBox("The ending dir of this wire in a turf, " +
+									"using 4 bits to indicate N S E W - 1 2 4 8\r\n" +
+									"Corners can also be used i.e.: 5 = NE (1 + 4) = 0101\r\n" +
+									"This is the edge of the location where the wire exits the turf\r\n" +
+									"Can be null of knot wires", MessageType.Info);
+
+			SerializedProperty DirectionEnd = serializedObject.FindProperty("DirectionEnd");
+			EditorGUILayout.PropertyField((DirectionEnd));
+
+			SerializedProperty Color = serializedObject.FindProperty("Color");
+			EditorGUILayout.PropertyField((Color));
+
+			SerializedProperty TRay = serializedObject.FindProperty("TRay");
+			EditorGUILayout.PropertyField((TRay));
+
+			EditorGUILayout.HelpBox("TODO: Create a specific component to handle wiring changes\r\n" +
+			                        "via the map editor. Do this by inheriting the component from\r\n" +
+			                        "SpriteRotate.cs", MessageType.Warning);
+		}
+
+	}
+}
