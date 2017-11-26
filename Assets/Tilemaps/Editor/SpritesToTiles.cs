06347d9605bfacd741e4e2b75be08a1bf27c7cae
krille90 <christian.bender.saebelkampf@gmail.com>
2017-11-26 19:51:48 +0100

    added floor tiles

--- Assets/Tilemaps/Editor/SpritesToTiles.cs
+++ Assets/Tilemaps/Editor/SpritesToTiles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Tilemaps.Editor.Utils;
 using Tilemaps.Scripts.Tiles;
@@ -11,7 +12,7 @@ namespace Tilemaps.Editor
     public class SpritesToTiles : MonoBehaviour
     {
         private static string tilesPath = "Assets/Tilemaps/Tiles";
-
+    
         [MenuItem("Assets/Sprites/Generate Tiles", false, 1000)]
         public static void ImportObjects()
         {
@@ -24,31 +25,36 @@ namespace Tilemaps.Editor
         private static void LoadTiles(string path)
         {
             var assets = AssetDatabase.LoadAllAssetsAtPath(path);
-
+            
             foreach (KeyValuePair<string, DmiIcon> entry in DmiIconData.Data)
             {
-                if (entry.Key.Contains("floors.dmi"))
+                if (entry.Key.Contains("floors.dmi")) // TODO only supports floors right now
                 {
-                    var index = 0;
+                    var folderPath = Path.Combine(tilesPath, assets[0].name);
+                    
                     for(int i = 0; i < entry.Value.states.Count; i++)
                     {
                         var state = entry.Value.states[i];
-//                        Debug.Log(state.unityName + " " + state.state + " " + state.frames + " " + state.dirs);
-                
-                        var assetName = "floors_" + index;
                         
                         var dmiIndex = int.Parse(state.unityName.Replace("floors_", ""));
 
-                        if (dmiIndex != index)
-                        {
-                            Debug.Log(state.state + " " + state.unityName);
-                            index = dmiIndex;
-                        }
+                        var tileCount = state.frames * state.dirs;
                         
-                        index += Mathf.Max(state.frames, state.dirs);
+                        for (int e = 0; e < state.frames * state.dirs; e++)
+                        {
+                            var tileName = state.state + (tileCount > 1? "_" + e: "");
+                            
+                            var tile = TileBuilder.CreateTile<SimpleTile>(LayerType.Floors);
+                            tile.sprite = assets[dmiIndex + e + 1] as Sprite;
+                            tile.LayerType = LayerType.Floors;
 
-
-//                        TileBuilder.CreateTile<SimpleTile>(LayerType.Floors, state.state);
+                            if (tileName.Length == 0)
+                            {
+                                tileName = "FloorTile";
+                            }
+                            
+                            TileBuilder.CreateAsset(tile, tileName, folderPath);
+                        }
                     }
 
                     break;
