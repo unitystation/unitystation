47fb0e5416bfce9819aba36632eb5311fcab8031
krille90 <christian.bender.saebelkampf@gmail.com>
2017-12-02 22:17:41 +0100

    some performance increases regarding tile previews. Removing Connect Categories Shuttle and Station again

--- Assets/Tilemaps/Editor/Brushes/Utils/PreviewTile.cs
+++ Assets/Tilemaps/Editor/Brushes/Utils/PreviewTile.cs
@@ -6,11 +6,13 @@ namespace Tilemaps.Editor.Brushes.Utils
 {
     public class PreviewTile : LayerTile
     {
-        public LayerTile ReferenceTile;
+        public GenericTile ReferenceTile;
         
         public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
         {
             tileData.sprite = ReferenceTile.PreviewSprite;
+            tileData.colliderType = Tile.ColliderType.None;
+            tileData.flags = TileFlags.None;
         }
     }
 }
