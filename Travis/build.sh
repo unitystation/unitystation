222f6b6cc2c6aa49a8774e000b74b98d7a2bdc7f
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2017-12-09 19:30:51 +0100

    changing build settings

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,3 +1,4 @@
+
 #! /bin/sh
 
 # NOTE the command args below make the assumption that your Unity project folder is
@@ -7,7 +8,7 @@
 
 ## Run the editor unit tests
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-/Users/travis/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
@@ -18,7 +19,25 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-quit
 
 rc0=$?
+
 echo "Unit test logs"
 cat $(pwd)/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+	-quit
+
+ls -l $(pwd)/Build/osx/
+rc1=$?
+echo "Build logs (OSX)"
+cat $(pwd)/unity.log
+
+exit $(($rc1))
