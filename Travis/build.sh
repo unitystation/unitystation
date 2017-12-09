4d9bda85e8eb42f87a6c8eb66fbf811b50b44747
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2017-12-09 16:23:34 +0100

    Merging OSX related jobs into one

--- Travis/build.sh
+++ Travis/build.sh
@@ -1,3 +1,4 @@
+
 #! /bin/sh
 
 # NOTE the command args below make the assumption that your Unity project folder is
@@ -18,7 +19,24 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-quit
 
 rc0=$?
+
 echo "Unit test logs"
 cat $(pwd)/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
+/Users/travis/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+	-quit
+
+rc1=$?
+echo "Build logs (OSX)"
+cat $(pwd)/unity.log
+
+exit $(($rc1))
