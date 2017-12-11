7584b64ca98c27bfeb48b6e8b142702eb3bdc88b
krille90 <christian.bender.saebelkampf@gmail.com>
2017-12-11 22:03:31 +0100

    fixed pixel raycast

--- UnityProject/Assets/Scripts/PlayGroups/Input/InputController.cs
+++ UnityProject/Assets/Scripts/PlayGroups/Input/InputController.cs
@@ -176,7 +176,7 @@ namespace PlayGroups.Input
                     var x = hitPosition.y * Mathf.Sin(angle) - hitPosition.x * Mathf.Cos(angle);
                     var y = hitPosition.y * Mathf.Cos(angle) - hitPosition.x * Mathf.Sin(angle);
                     
-                    var texPosX = Mathf.RoundToInt(sprite.rect.x + (x / scale.x - offset.x % 1) * pixelsPerUnit + sprite.rect.width * 0.5f);
+                    var texPosX = Mathf.RoundToInt(sprite.rect.x + sprite.rect.width * 0.5f - (x / scale.x - offset.x % 1) * pixelsPerUnit);
                     var texPosY = Mathf.RoundToInt(sprite.rect.y + (y / scale.y - offset.y % 1) * pixelsPerUnit + sprite.rect.height * 0.5f);
 
                     var pixelColor = sprite.texture.GetPixel(texPosX, texPosY);
