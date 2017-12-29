edfa6b4abe124d115c2a215233ed0d9c4008b8d2
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2017-12-29 15:29:54 +0100

    Little naming fixing (+5 squashed commits) Squashed commits: [1a318bd] Change linux server build dir in accordance with the other directory structures [c006469] Server build tweaks [720f52f] more script debugging [1276f7b] OSX buildscript fixes. [482b3a1] Added preliminary OSX buildscripts and renamed windows buildscript

--- UnityProject/Assets/Scripts/Editor/BuildServer.cs
+++ UnityProject/Assets/Scripts/Editor/BuildServer.cs
@@ -6,7 +6,7 @@ public class BuildServer
 	{
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
 		buildPlayerOptions.scenes = new[] {"Assets/scenes/Lobby.unity", "Assets/scenes/OutpostDeathmatch.unity"};
-		buildPlayerOptions.locationPathName = "Builds/server/server";
+		buildPlayerOptions.locationPathName = "Builds/Server/Linux/Unitystation-Server";
 		buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
 		buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
