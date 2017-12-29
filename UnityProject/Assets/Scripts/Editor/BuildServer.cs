67f5db161c2026185bcfb3f3cdd014df5b756879
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2017-12-29 14:57:15 +0100

    Moved lobby back to scenes root (STIL NOT TO BE DEVELOPED ON) and changed dedicated server to outpostDM

--- UnityProject/Assets/Scripts/Editor/BuildServer.cs
+++ UnityProject/Assets/Scripts/Editor/BuildServer.cs
@@ -5,7 +5,7 @@ public class BuildServer
 	private static void PerformBuild()
 	{
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-		buildPlayerOptions.scenes = new[] {"Assets/scenes/Lobby.unity", "Assets/scenes/Deathmatch.unity"};
+		buildPlayerOptions.scenes = new[] {"Assets/scenes/Lobby.unity", "Assets/scenes/OutpostDeathmatch.unity"};
 		buildPlayerOptions.locationPathName = "Builds/server/server";
 		buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
 		buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
