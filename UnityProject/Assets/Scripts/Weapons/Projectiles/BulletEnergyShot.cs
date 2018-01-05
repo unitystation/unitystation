3e13d60a233e450c70f03b06850b5cf13be8fc69
luke <lquinn@vt.edu>
2018-01-05 14:54:45 -0500

    FinalUpdate
    Fixes Tazer Sound
    Has been Tested ie no related bugs.
    Sends Burn damage via BULLET (temp saved for a rewrite of some bullet behavior)

--- UnityProject/Assets/Scripts/Weapons/Projectiles/BulletEnergyShot.cs
+++ UnityProject/Assets/Scripts/Weapons/Projectiles/BulletEnergyShot.cs
@@ -2,6 +2,7 @@
 {
     public override void OnShoot()
     {
+        damageType = DamageType.BURN; // sets damage hook to burn damage these are energy weapons
         //Bullet specific stuff
     }
 }
