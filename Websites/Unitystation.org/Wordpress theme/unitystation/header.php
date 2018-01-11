11b77799a8d5ba4698b9eda6e2c4b17b3b614eff
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2018-01-11 17:47:43 +0100

    More josh work

--- Websites/Unitystation.org/Wordpress theme/unitystation/header.php
+++ Websites/Unitystation.org/Wordpress theme/unitystation/header.php
@@ -81,4 +81,45 @@
                 <!-- /headerimage -->
 
             </header>
-			<!-- /header -->
+<!-- /header -->
+<!-- loginModal -->
+						<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="loginModalLabel"><i class="fa fa-sign-in-alt"></i> Login</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										...
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+										<button type="button" class="btn btn-primary">Save changes</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+<!-- new user modal -->
+						<div class="modal fade" id="newUserModal" tabindex="-1" role="dialog" aria-labelledby="newUserModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="newUserModalLabel"><i class="fas fa-user-plus"></i> New User</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										...
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+										<button type="button" class="btn btn-primary">Save changes</button>
+									</div>
+								</div>
+							</div>
+						</div>
