2c372e5c975fae629d03cb2aac5766d7fca2a8ee
Doobly <dooblynoobly@smashingpixels.com.au>
2018-01-16 12:47:22 +1000

    ObjectBehaviour Adjustment
    - Adjusted the ObjectBehaviour editor script to expose the renderer ignore array for VisibleBehaviour

--- UnityProject/Assets/Scripts/Editor/Objects/ObjectBehaviourEditor.cs
+++ UnityProject/Assets/Scripts/Editor/Objects/ObjectBehaviourEditor.cs
@@ -8,8 +8,11 @@ public class ObjectBehaviourEditor : Editor
 		ObjectBehaviour oTarget = (ObjectBehaviour) target;
 		serializedObject.Update();
 		SerializedProperty isPushable = serializedObject.FindProperty("isPushable");
+		SerializedProperty renderersToIgnore = serializedObject.FindProperty("ignoredSpriteRenderers");
 		EditorGUI.BeginChangeCheck();
 		EditorGUILayout.PropertyField(isPushable, true);
+		EditorGUILayout.LabelField("For ignoring renderers from VisibleBehaviour updates:");
+		EditorGUILayout.PropertyField(renderersToIgnore,true);
 		if (EditorGUI.EndChangeCheck())
 		{
 			serializedObject.ApplyModifiedProperties();
