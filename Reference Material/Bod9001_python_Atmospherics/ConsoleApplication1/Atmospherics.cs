6648209e22b543e42526f76e6d626f8dc30b6f49
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2018-01-27 12:58:33 +0100

    removed typo added using

--- Reference Material/Bod9001_python_Atmospherics/ConsoleApplication1/Atmospherics.cs
+++ Reference Material/Bod9001_python_Atmospherics/ConsoleApplication1/Atmospherics.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 
 namespace Atmospherics
