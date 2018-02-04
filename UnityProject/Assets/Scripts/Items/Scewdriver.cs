bd89fef1d7263f16deeb81d610d158b7e36f8fc4
Puzzlemaker <bradfordthe3rd@gmail.com>
2018-02-03 19:45:21 -0500

    Added crowbar

--- UnityProject/Assets/Scripts/Items/Scewdriver.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine.Networking;
-
-public class Scewdriver : NetworkBehaviour
-{
-}
---
+++ UnityProject/Assets/Scripts/Items/Tools/CrobarTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Items;
+using UI;
+using UnityEngine;
+
+public class CrobarTrigger : PickUpTrigger
+{
+    public override void Interact(GameObject originator, Vector3 position, string hand)
+    {
+        //TODO:  Fill this in.
+
+        //Only peform crobar actions when holding the crobar
+        //TODO:  This should not be necessary?  
+        if (UIManager.Hands.CurrentSlot.Item != gameObject)
+        {
+            base.Interact(originator, position, hand);
+            return;
+        }
+
+        base.Interact(originator, position, hand);
+    }
+}
