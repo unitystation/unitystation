47edbd20e4fd6ab587a840b9a5eccacb6d5ffe26
foma <fomalsd@users.noreply.github.com>
2018-02-12 13:48:20 +0300

    InfoWindow bwoink* (*bwoink sound not in yet)

--- UnityProject/Assets/Scripts/UI/Info/GUI_Info.cs
+++ UnityProject/Assets/Scripts/UI/Info/GUI_Info.cs
@@ -6,8 +6,8 @@ public class GUI_Info : MonoBehaviour
 {
 	public GameObject buttonPrefab;
 
-	public static Color banColor = new Color(255f / 255f, 103f / 255f, 103f / 255f);
-	public static Color infoColor = new Color(200f / 255f, 200f / 255f, 200f / 255f);
+	private static readonly Color banColor = new Color(255f / 255f, 103f / 255f, 103f / 255f);
+	private static readonly Color infoColor = new Color(200f / 255f, 200f / 255f, 200f / 255f);
 	public Text infoText;
 	public Text title;
 
@@ -30,12 +30,13 @@ public class GUI_Info : MonoBehaviour
 //		Show(info, infoColor, titleText);
 //	}
 
-	public void Show(string info, Color color, string titleText = "")
+	public void Show(string info, bool bwoink, string titleText = "")
 	{
 		infoText.text = info;
-		infoText.color = color;
+		infoText.color = bwoink ? banColor : infoColor;
 		title.text = string.IsNullOrEmpty(titleText) ? "Info" : titleText;
 		UIManager.Display.infoWindow.SetActive(true);
+		SoundManager.Play("Bwoink", 1, 1);
 	}
 
 }
