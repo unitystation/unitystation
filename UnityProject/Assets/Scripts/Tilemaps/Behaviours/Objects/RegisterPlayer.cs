2611ef3fedd693cc5b0c7ceea991b4e2190a32cf
Doobly <dooblynoobly@smashingpixels.com.au>
2018-02-16 19:12:30 +1000

    Ability to transition between two matricies

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Objects/RegisterPlayer.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Objects/RegisterPlayer.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using PlayGroup;
 
 namespace Tilemaps.Behaviours.Objects
 {
@@ -6,6 +9,9 @@ namespace Tilemaps.Behaviours.Objects
 	public class RegisterPlayer : RegisterTile
 	{
 		public bool IsBlocking { get; set; } = true;
+		private bool isChangingMatricies = false;
+		private Transform changingCheck;
+
 
 		public override bool IsPassable()
 		{
@@ -14,9 +20,42 @@ namespace Tilemaps.Behaviours.Objects
 
 		void OnTriggerEnter2D(Collider2D coll)
 		{
-			if (coll.gameObject.layer == 24) {
+			//layer 24 is matrix layer
+			if (coll.gameObject.layer == 24 && isServer) {
 				Debug.Log("PlayerEntered Matrix: " + coll.gameObject.transform.parent.name);
+				if (!isChangingMatricies) {
+					ChangeMatricies(coll.gameObject.transform.parent);
+					isChangingMatricies = true;
+					StartCoroutine(ChangeCoolDown(coll.gameObject.transform.parent));
+				} else {
+					changingCheck = coll.gameObject.transform.parent;
+				}
 			}
+		}
+
+		[Server]
+		private void ChangeMatricies(Transform newParent){
+			NetworkIdentity netIdent = newParent.GetComponent<NetworkIdentity>();
+			if (ParentNetId != netIdent.netId) {
+				ParentNetId = netIdent.netId;
+			}
+
+			PlayerSync playerSync = GetComponent<PlayerSync>();
+			if (playerSync != null) {
+				playerSync.SetPosition(transform.localPosition);
+			}
+		}
+
+		IEnumerator ChangeCoolDown(Transform newParent){
+			//Wait 10ms and check which matrix the player did end up on
+			//Or else player will get stuck between two matricies
+			yield return new WaitForSeconds(0.1f);
+			if(changingCheck != null){
+				if(changingCheck != newParent){
+					ChangeMatricies(changingCheck);
+				}
+			}
+			isChangingMatricies = false;
 		}
 	}
 }
