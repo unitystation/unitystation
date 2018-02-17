a4b22d82528f8178e51a953ce8aa44a223898b9b
fomaltd <fomalsd@users.noreply.github.com>
2018-02-17 14:30:25 +0300

    Small ship + Enhanced experimental shuttle controller with rotation

--- UnityProject/Assets/Scripts/Shuttles/ShuttleController.cs
+++ UnityProject/Assets/Scripts/Shuttles/ShuttleController.cs
@@ -10,11 +10,29 @@ public class ShuttleController : MonoBehaviour {
 	bool doFlyingThing = false;
 	public Vector2 flyingDirection;
 	public float speed;
+	public float rotSpeed = 35;
+	public KeyCode startKey = KeyCode.G;
+	public KeyCode leftKey = KeyCode.Keypad4;
+	public KeyCode rightKey = KeyCode.Keypad6;
 
 	void Update(){
-		if(Input.GetKeyDown(KeyCode.G) && !doFlyingThing){
-			doFlyingThing = true;
+		if(Input.GetKeyDown(startKey)){
+			doFlyingThing = !doFlyingThing;
 		}
+		if(Input.GetKeyDown(KeyCode.KeypadPlus)){
+			speed++;
+		}
+		if(Input.GetKeyDown(KeyCode.KeypadMinus)){
+			speed--;
+		}
+
+		if(Input.GetKey(leftKey)){
+			transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey(rightKey)){
+			transform.Rotate(Vector3.back * rotSpeed * Time.deltaTime);
+		}
+		
 
 		if(doFlyingThing){
 			transform.Translate(flyingDirection * speed * Time.deltaTime);
