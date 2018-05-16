c4605f9ce11fd57747773ad91540da14759f138c
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-16 13:56:48 +0200

    Rewrote so CameraZoomHandler no longer uses Update. Instead SetZoomLevel() calls Refresh() which does the same thing that the body of Update() did.

--- UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
+++ UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 public class CameraZoomHandler : MonoBehaviour
 {
 	private int zoomLevel = 1;
-	private int cachedZoomLevel = 1;
 
 	void Start()
 	{
 		// Setup initial zoom level by guessing.
 		SetZoomLevel(-1);
 	}
-	
-	// Update is called once per frame
-	void Update()
+
+	// Refreshes after setting zoom level.
+	private void Refresh()
 	{
-		if(cachedZoomLevel != zoomLevel)
-		{
-			// Calculate ratio.
-			double ratio = Screen.height / (double) Screen.width;
-			// Calculate scaling factor. 409600 is a magic number.
-			double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
-			// Calculate orthographic size with full precision and then convert to float precision.
-			Camera.main.orthographicSize = Convert.ToSingle(ratio * 10 * scaleFactor / zoomLevel);
-			// Recenter camera.
-			DisplayManager.Instance.SetCameraFollowPos();
-			cachedZoomLevel = zoomLevel;
-		}
+		// Calculate ratio.
+		double ratio = Screen.height / (double) Screen.width;
+		// Calculate scaling factor. 409600 is a magic number.
+		double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
+		// Calculate orthographic size with full precision and then convert to float precision.
+		Camera.main.orthographicSize = Convert.ToSingle(ratio * 10 * scaleFactor / zoomLevel);
+		// Recenter camera.
+		DisplayManager.Instance.SetCameraFollowPos();
 	}
 
 	public void SetZoomLevel(float zoomLevel)
@@ -42,6 +37,7 @@ public class CameraZoomHandler : MonoBehaviour
 		}
 
 		this.zoomLevel = integerZoomLevel;
+		Refresh();
 	}
 
 }
