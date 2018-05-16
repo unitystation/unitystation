6978fa5d7d4ba101221f2953fe2290d4e7549e8b
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-16 14:46:30 +0200

    CameraZoomHandler now automatically updates the slider if it sets the zoomLevel automatically.

--- UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
+++ UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {	
     public class CameraZoomHandler : MonoBehaviour
     {
         private int zoomLevel = 1;
+        public Selectable selectable;
+        private Slider slider;
 
         void Start()
         {
+            slider = (Slider)selectable;
             // Setup initial zoom level by guessing.
             SetZoomLevel(-1);
         }
@@ -30,12 +34,14 @@ namespace UI
         {
             int integerZoomLevel = (int)zoomLevel;
 		
-            // Automatically try to determine zoom level if we're feeding it some dumb values.
+            // Automatically try to determine zoom level if we're feeding it some dumb values, such as when this component initializes.
             if (integerZoomLevel < 1 || integerZoomLevel > 16)
             {			
                 double ratio = Screen.height / (double) Screen.width;
                 double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
                 integerZoomLevel = Mathf.RoundToInt((float)scaleFactor);
+                //Update the slider if we select this automatically for some reason.
+                slider.value = integerZoomLevel;
             }
 
             this.zoomLevel = integerZoomLevel;
