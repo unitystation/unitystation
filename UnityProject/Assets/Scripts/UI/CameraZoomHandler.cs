2ea3aac0a828478042fbd1f368dd94ede80fd312
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-16 18:06:41 +0200

    Cleaned up duplicated code and refactored methods to look a bit nicer.

--- UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
+++ UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,47 +7,42 @@ namespace UI
 {	
     public class CameraZoomHandler : MonoBehaviour
     {
-        private int zoomLevel = 1;
+        //Reference to zoom text.
         public Text zoomText;
-
-        void Start()
-        {
-            // Setup initial zoom level by guessing.
-            SetZoomLevel(-1);
-        }
+        private int zoomLevel = 0;
+        private string zoomString = "You should not see this string!";
 
         // Refreshes after setting zoom level.
         public void Refresh()
-        {
+        {          
             // Calculate ratio.
             double ratio = Screen.height / (double) Screen.width;
+            
             // Calculate scaling factor. 409600 is a magic number.
             double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
+            
+            // Automatically set zoom level if it's less than zero.
+            if (zoomLevel < 1)
+            {
+                zoomLevel = Mathf.RoundToInt((float) scaleFactor);
+                zoomString = "auto";
+            }
+            
             // Calculate orthographic size with full precision and then convert to float precision.
             Camera.main.orthographicSize = Convert.ToSingle(ratio * 10 * scaleFactor / zoomLevel);
+            
             // Recenter camera.
             DisplayManager.Instance.SetCameraFollowPos();
+            
+            // Set zoom string.
+            zoomText.text = "Zoom: " + zoomString;
         }
 
         public void SetZoomLevel(float zoomLevel)
         {
-            int integerZoomLevel = (int)zoomLevel;
-		    
-            // Automatically try to determine zoom level if we're feeding it some dumb values, such as when this component initializes.
-            if (integerZoomLevel < 1 || integerZoomLevel > 16)
-            {
-                double ratio = Screen.height / (double) Screen.width;
-                double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
-                integerZoomLevel = Mathf.RoundToInt((float) scaleFactor);
-                //Update the slider if we select this automatically for some reason.
-                zoomText.text = "Zoom: Auto";
-            }
-            else
-            {
-                zoomText.text = "Zoom: " + zoomLevel.ToString();
-            }
-
-            this.zoomLevel = integerZoomLevel;
+            // Set the zoom string early, change if need be.
+            zoomString = zoomLevel.ToString();
+            this.zoomLevel = (int)zoomLevel;
             Refresh();
         }
 
