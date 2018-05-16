7f93382c8faaa86948fac302830a209d84cdd711
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-16 15:02:23 +0200

    Finished polish on slider and zooming. Now the ZoomText label updates as you move the slider. If the slider is set to Zero, it say's auto since it attempts to automatically find the best zoom level.

--- UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
+++ UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
@@ -7,12 +7,10 @@ namespace UI
     public class CameraZoomHandler : MonoBehaviour
     {
         private int zoomLevel = 1;
-        public Selectable selectable;
-        private Slider slider;
+        public Text zoomText;
 
         void Start()
         {
-            slider = (Slider)selectable;
             // Setup initial zoom level by guessing.
             SetZoomLevel(-1);
         }
@@ -33,15 +31,19 @@ namespace UI
         public void SetZoomLevel(float zoomLevel)
         {
             int integerZoomLevel = (int)zoomLevel;
-		
+		    
             // Automatically try to determine zoom level if we're feeding it some dumb values, such as when this component initializes.
             if (integerZoomLevel < 1 || integerZoomLevel > 16)
-            {			
+            {
                 double ratio = Screen.height / (double) Screen.width;
                 double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
-                integerZoomLevel = Mathf.RoundToInt((float)scaleFactor);
+                integerZoomLevel = Mathf.RoundToInt((float) scaleFactor);
                 //Update the slider if we select this automatically for some reason.
-                slider.value = integerZoomLevel;
+                zoomText.text = "Zoom: Auto";
+            }
+            else
+            {
+                zoomText.text = "Zoom: " + zoomLevel.ToString();
             }
 
             this.zoomLevel = integerZoomLevel;
