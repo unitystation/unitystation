1aed16bb73b81a30f5b48474dd64a95f5e61d03e
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-16 11:27:23 +0200

    Changed how zoom level works by replacing dropdown control with slider control and rewriting SetZoomLevel to take a float instead of an integer. Also added a slight optimization by only updating the camera orthographic size when needed.

--- UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
+++ UnityProject/Assets/Scripts/UI/CameraZoomHandler.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using NUnit.Framework.Constraints;
 using UnityEngine;
-using UnityEngine.SocialPlatforms;
 
 public class CameraZoomHandler : MonoBehaviour
 {
 	private int zoomLevel = 1;
+	private int cachedZoomLevel = 1;
 
 	void Start()
 	{
@@ -16,26 +15,33 @@ public class CameraZoomHandler : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// Calculate ratio.
-		double ratio = Screen.height / (double) Screen.width;
-		// Calculate scaling factor. 409600 is a magic number.
-		double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
-		// Calculate orthographic size with full precision and then convert to float precision.
-		Camera.main.orthographicSize = Convert.ToSingle(ratio * 10 * scaleFactor / zoomLevel);
+		if(cachedZoomLevel != zoomLevel)
+		{
+			// Calculate ratio.
+			double ratio = Screen.height / (double) Screen.width;
+			// Calculate scaling factor. 409600 is a magic number.
+			double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
+			// Calculate orthographic size with full precision and then convert to float precision.
+			Camera.main.orthographicSize = Convert.ToSingle(ratio * 10 * scaleFactor / zoomLevel);
+			// Recenter camera.
+			DisplayManager.Instance.SetCameraFollowPos();
+			cachedZoomLevel = zoomLevel;
+		}
 	}
 
-	public void SetZoomLevel(int zoomLevel)
+	public void SetZoomLevel(float zoomLevel)
 	{
-		zoomLevel = (int)Mathf.Pow(2f,zoomLevel);
+		int integerZoomLevel = (int)zoomLevel;
+		
 		// Automatically try to determine zoom level if we're feeding it some dumb values.
-		if (zoomLevel < 1 || zoomLevel > 16)
+		if (integerZoomLevel < 1 || integerZoomLevel > 16)
 		{			
 			double ratio = Screen.height / (double) Screen.width;
 			double scaleFactor = Math.Sqrt(Screen.height * Screen.width / (409600 * ratio));
-			zoomLevel = Mathf.RoundToInt((float)scaleFactor);
+			integerZoomLevel = Mathf.RoundToInt((float)scaleFactor);
 		}
 
-		this.zoomLevel = zoomLevel;
+		this.zoomLevel = integerZoomLevel;
 	}
 
 }
