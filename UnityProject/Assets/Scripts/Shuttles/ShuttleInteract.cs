1ded32f097b279159429a90414ceaf05119c0fa8
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2018-05-26 20:16:13 +0200

    Added shuttle console interaction and UI scripts.

---
+++ UnityProject/Assets/Scripts/Shuttles/ShuttleInteract.cs
@@ -0,0 +1,31 @@
+﻿using PlayGroups.Input;
+using UI;
+using UnityEngine;
+using Vuforia;
+
+public class ShuttleInteract : InputTrigger 
+{
+    public string interactionMessage;
+    public MatrixMove ShuttleMatrixMove;
+    private GameObject player;
+    
+    // Method that brings up shuttle controls.
+    public override void Interact(GameObject originator, Vector3 position, string hand)
+    {
+        player = originator;
+        ControlTabs.ShowShuttleTab(ShuttleMatrixMove);
+    }
+
+    /// <summary>
+    /// Checks if the player has moved away from the shuttle. If the player has moved away it hides the tab.
+    /// </summary>
+    public void Update()
+    {
+        if ( player != null && Vector3.Distance(player.transform.position, transform.position) > 1.5f )
+        {
+            Debug.Log("Player walked away from console.");
+            ControlTabs.HideShuttleTab();
+            player = null;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Shuttles/ShuttleUIControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuttleUIControl : MonoBehaviour
+{
+    public MatrixMove matrixMove;
+    private float maxSpeed = 0;
+    private bool active = false;
+
+    /// <summary>
+    /// Starts or stops the shuttle.
+    /// </summary>
+    /// <param name="off">Toggle parameter</param>
+    public void TurnOnOff(bool off)
+    {
+        if(matrixMove != null && !off)
+        {
+            maxSpeed = matrixMove.maxSpeed;
+        }
+        else
+        {
+            
+            maxSpeed = 0;
+        }
+        matrixMove.ToggleMovement();
+    }
+
+    /// <summary>
+    /// Turns the shuttle right.
+    /// </summary>
+    public void TurnRight()
+    {
+        if(matrixMove != null)
+            matrixMove.TryRotate(true);
+    }
+
+    /// <summary>
+    /// Turns the shuttle left.
+    /// </summary>
+    public void TurnLeft()
+    {
+        if(matrixMove != null)
+            matrixMove.TryRotate(false);
+    }
+
+    /// <summary>
+    /// Sets shuttle speed.
+    /// </summary>
+    /// <param name="speedMultiplier"></param>
+    public void SetSpeed(float speedMultiplier)
+    {
+        float speed = speedMultiplier*(maxSpeed-1) + 1;
+        if(matrixMove != null)
+            matrixMove.SetSpeed(speed);
+    }
+    
+}
