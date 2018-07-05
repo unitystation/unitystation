f14a85cc29562edd0036aec00df67f68b473823b
foma <fomalsd@users.noreply.github.com>
2018-07-05 18:12:58 +0300

    Finally fixed touchscreen scaling problem; Multiplayer-tested polished Autopilot

--- UnityProject/Assets/Scripts/UI/Net/NetRadiusCircle.cs
+++ UnityProject/Assets/Scripts/UI/Net/NetRadiusCircle.cs
@@ -10,25 +10,25 @@ public class NetRadiusCircle : NetUIElement
 			externalChange = true;
 			//don't update if it's the same sprite
 			if ( radius.ToString() != value ) {
-				//todo parse check
-				int parsedValue = int.Parse( value );
-				radius = parsedValue;
-				
-				//Not showing circle for invalid radii
-				if ( radius < 1 ) {
-					Color modifiedColor = Element.color;
-					modifiedColor.a = 0;
-					Element.color = modifiedColor;
-				} else {
-					float scale = Mathf.Clamp( radius/10, 1, 10 );
-					transform.localScale = new Vector3(scale, scale, 1);
-					
-					//making larger ones more transparent
-					Color modifiedColor = Element.color;
-					modifiedColor.a = 1 / scale;
-					Element.color = modifiedColor;
+				int parsedValue;
+				if ( int.TryParse( value, out parsedValue ) ) {
+					radius = parsedValue;
+
+					//Not showing circle for invalid radii
+					if ( radius < 1 ) {
+						Color modifiedColor = Element.color;
+						modifiedColor.a = 0;
+						Element.color = modifiedColor;
+					} else {
+						float scale = Mathf.Clamp( radius / 10, 1, 10 );
+						transform.localScale = new Vector3( scale, scale, 1 );
+
+						//making larger ones more transparent
+						Color modifiedColor = Element.color;
+						modifiedColor.a = 1 / scale;
+						Element.color = modifiedColor;
+					}
 				}
-				
 			}			
 			externalChange = false;
 		}
