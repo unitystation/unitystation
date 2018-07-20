803f1c4693f94d39d33d144b7a087e4962b02d7e
Doobly <rob@smashingpixels.com.au>
2018-07-20 22:06:12 +1000

    More accurate xOffset values for FollowControl

--- UnityProject/Assets/Scripts/Managers/DisplayManager.cs
+++ UnityProject/Assets/Scripts/Managers/DisplayManager.cs
@@ -9,6 +9,7 @@ public class DisplayManager : MonoBehaviour
 	public static DisplayManager Instance;
 
 	private CanvasScaler canvasScaler;
+
 	public FieldOfViewStencil fieldOfView;
 	private int height;
 	public LightingSystem lightingSystem;
@@ -66,19 +67,15 @@ public class DisplayManager : MonoBehaviour
 		}
 	}
 
-	public void SetCameraFollowPos(bool isPanelHidden = false)
+	public void SetCameraFollowPos()
 	{
 		if(Camera2DFollow.followControl == null){
 			return;
 		}
 
-		float xOffSet =
-			Mathf.Abs(Camera.main.ScreenToWorldPoint(UIManager.Hands.transform.position).x - Camera2DFollow.followControl.transform.position.x) + -0.06f;
+        float xOffSet =
+             (Camera2DFollow.followControl.transform.position.x - Camera.main.ScreenToWorldPoint(UIManager.Hands.transform.position).x) * 1.4f;
 
-		if (isPanelHidden)
-		{
-			xOffSet = -xOffSet;
-		}
 		Camera2DFollow.followControl.listenerObj.transform.localPosition = new Vector3(-xOffSet, 1f); //set listenerObj's position to player's pos
 		Camera2DFollow.followControl.SetXOffset(xOffSet);
 	}
