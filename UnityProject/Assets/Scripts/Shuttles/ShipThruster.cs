04d9007987d03a7f20a12dcb9c91ce6e92ea6f1a
Doobly <dooblynoobly@smashingpixels.com.au>
2018-08-23 10:18:35 +1000

    Apply Thrusters FX patch
    Extracted from Thrusters FX PR by Nicholas

---
+++ UnityProject/Assets/Scripts/Shuttles/ShipThruster.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipThruster : MonoBehaviour {
+
+    public MatrixMove shipMatrixMove; // ship matrix move
+    public ParticleSystem particleFX;
+
+    void Awake() {
+        //Gets ship matrix move by getting root (top parent) of current gameobject
+        shipMatrixMove = transform.root.gameObject.GetComponent<MatrixMove>();
+        particleFX = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void UpdateEngineState()
+    {
+		var emissionFX = particleFX.emission;
+        if (EngineStatus())
+        {
+            emissionFX.enabled = true;
+			//SpeedChange(0, shipMatrixMove.State.Speed, thrusterManager.particleSpeedMultiplier); //Set particle speed on engine updates, used for setting speed at beginning of flight.
+		}
+        else
+        {
+            emissionFX.enabled = false;
+        }
+    }
+
+    //Rotates FX as ship rotates
+    public void RotateFX(Orientation newOrientation)
+    {
+        var mainFX = particleFX.main;
+
+        mainFX.startRotation = newOrientation.Degree * Mathf.Deg2Rad;
+    }
+
+	public void SpeedChange(float oldSpeed, float newSpeed, float particleSpeedMultiplier)
+	{
+		var mainFX = particleFX.main;
+
+		mainFX.startSpeed = newSpeed * particleSpeedMultiplier;
+	}
+
+	bool EngineStatus() // Returns if engines are "on" (if ship is moving)
+    {
+        if (shipMatrixMove != null)
+        {
+            return shipMatrixMove.State.IsMoving;
+		}
+
+        return false;
+    }
+	
+}
