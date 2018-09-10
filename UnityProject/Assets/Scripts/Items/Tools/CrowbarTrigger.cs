74ff0da80a47a002f9a6cf9a487bbcf5dcffb6ed
Doobly <dooblynoobly@smashingpixels.com.au>
2018-09-10 15:32:47 +1000

    Crowbar progress

--- UnityProject/Assets/Scripts/Items/Tools/CrowbarTrigger.cs
+++ UnityProject/Assets/Scripts/Items/Tools/CrowbarTrigger.cs
@@ -1,17 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 public class CrowbarTrigger : PickUpTrigger
 {
-    public override void Interact(GameObject originator, Vector3 position, string hand)
+    [SyncVar]
+    private bool canBeUsed = false;
+
+    public override void Interact (GameObject originator, Vector3 position, string hand)
     {
         //TODO:  Fill this in.
 
         if (UIManager.Hands.CurrentSlot.Item != gameObject)
         {
-            base.Interact(originator, position, hand);
+            base.Interact (originator, position, hand);
             return;
         }
 
-        base.Interact(originator, position, hand);
+        if (canBeUsed)
+        {
+            //TODO: Check inreach
+        }
+
+        base.Interact (originator, position, hand);
+    }
+
+    //Broadcast from EquipmentPool.cs **ServerSide**
+    public void OnAddToPool ()
+    {
+        canBeUsed = true;
+    }
+
+    //Broadcast from EquipmentPool.cs **ServerSide**
+    public void OnRemoveFromPool ()
+    {
+        canBeUsed = false;
     }
 }
