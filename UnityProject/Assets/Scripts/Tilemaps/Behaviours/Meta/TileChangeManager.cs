54bfd6e4621d3a5425c18d56bc4fe46864dd11e8
Doobly <dooblynoobly@smashingpixels.com.au>
2018-09-11 11:32:09 +1000

    Added ability to sync tile changes with new clients

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/TileChangeManager.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/TileChangeManager.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Tilemaps;
-using System.Linq;
 
 //To track construction and damage changes on the server and update on the client
 public class TileChangeManager : NetworkBehaviour
@@ -14,17 +14,30 @@ public class TileChangeManager : NetworkBehaviour
 	private Tilemap windowTileMap;
 	private Tilemap objectTileMap;
 
-	private Dictionary<string, TileBase> FloorAssets = new Dictionary<string, TileBase>();
-	private Dictionary<string, TileBase> WallAssets = new Dictionary<string, TileBase>();
-	private Dictionary<string, TileBase> WindowAssets = new Dictionary<string, TileBase>();
-	private Dictionary<string, TileBase> TableAssets = new Dictionary<string, TileBase>();
+	private Dictionary<string, TileBase> FloorAssets = new Dictionary<string, TileBase> ();
+	private Dictionary<string, TileBase> WallAssets = new Dictionary<string, TileBase> ();
+	private Dictionary<string, TileBase> WindowAssets = new Dictionary<string, TileBase> ();
+	private Dictionary<string, TileBase> TableAssets = new Dictionary<string, TileBase> ();
 
-	private Dictionary<Tilemap, TileChangeEntries> ChangeRegister = new Dictionary<Tilemap, TileChangeEntries>();
+	private TileChangeRegister ChangeRegister = new TileChangeRegister();
 
+	bool init = false;
 	void Start ()
 	{
 		CacheTileMaps ();
-		LoadAllTileAssets();
+		LoadAllTileAssets ();
+	}
+
+	[Server]
+	public void NotifyPlayer(GameObject requestedBy){
+		Debug.Log ("Request all updates: " + requestedBy.name);
+		var jsondata = JsonUtility.ToJson(ChangeRegister);
+		TileChangesNewClientSync.Send(gameObject, requestedBy, jsondata);
+	}
+
+	public void InitServerSync(string data){
+		Debug.Log("DATA " + data);
+		var newChangeRegister = JsonUtility.FromJson<TileChangeRegister>(data);
 	}
 
 	void CacheTileMaps ()
@@ -35,67 +48,66 @@ public class TileChangeManager : NetworkBehaviour
 			if (tilemaps[i].name.Contains ("Floors"))
 			{
 				floorTileMap = tilemaps[i];
-				ChangeRegister.Add(floorTileMap, new TileChangeEntries());
 			}
 
 			if (tilemaps[i].name.Contains ("Base"))
 			{
 				baseTileMap = tilemaps[i];
-				ChangeRegister.Add(baseTileMap, new TileChangeEntries());
 			}
 
 			if (tilemaps[i].name.Contains ("Walls"))
 			{
 				wallTileMap = tilemaps[i];
-				ChangeRegister.Add(wallTileMap, new TileChangeEntries());
 			}
 
 			if (tilemaps[i].name.Contains ("Windows"))
 			{
 				windowTileMap = tilemaps[i];
-				ChangeRegister.Add(windowTileMap, new TileChangeEntries());
 			}
 
 			if (tilemaps[i].name.Contains ("Objects"))
 			{
 				objectTileMap = tilemaps[i];
-				ChangeRegister.Add(objectTileMap, new TileChangeEntries());
 			}
 		}
 	}
 
-	void LoadAllTileAssets()
+	void LoadAllTileAssets ()
 	{
-		TileBase[] floorTiles = Resources.LoadAll<TileBase>("Tiles/Floors");
-		for(int i = 0; i < floorTiles.Length; i++)
+		TileBase[] floorTiles = Resources.LoadAll<TileBase> ("Tiles/Floors");
+		for (int i = 0; i < floorTiles.Length; i++)
 		{
-			FloorAssets.Add(floorTiles[i].name, floorTiles[i]);
+			FloorAssets.Add (floorTiles[i].name, floorTiles[i]);
 		}
 
-		TileBase[] windowTiles = Resources.LoadAll<TileBase>("Tiles/Windows");
+		TileBase[] windowTiles = Resources.LoadAll<TileBase> ("Tiles/Windows");
 		for (int i = 0; i < windowTiles.Length; i++)
 		{
-			WindowAssets.Add(windowTiles[i].name, windowTiles[i]);
+			WindowAssets.Add (windowTiles[i].name, windowTiles[i]);
 		}
 
-		TileBase[] wallTiles = Resources.LoadAll<TileBase>("Tiles/Walls");
+		TileBase[] wallTiles = Resources.LoadAll<TileBase> ("Tiles/Walls");
 		for (int i = 0; i < wallTiles.Length; i++)
 		{
-			WallAssets.Add(wallTiles[i].name, wallTiles[i]);
+			WallAssets.Add (wallTiles[i].name, wallTiles[i]);
 		}
 
-		TileBase[] tableTiles = Resources.LoadAll<TileBase>("Tiles/Tables");
+		TileBase[] tableTiles = Resources.LoadAll<TileBase> ("Tiles/Tables");
 		for (int i = 0; i < tableTiles.Length; i++)
 		{
-			TableAssets.Add(tableTiles[i].name, tableTiles[i]);
+			TableAssets.Add (tableTiles[i].name, tableTiles[i]);
 		}
+		init = true;
 	}
 }
 
 [System.Serializable]
-public class TileChangeEntries
-{
-	List<TileChangeEntry> entries = new List<TileChangeEntry> ();
+public class TileChangeRegister{
+	public List<TileChangeEntry> floorEntries = new List<TileChangeEntry> ();
+	public List<TileChangeEntry> baseEntries = new List<TileChangeEntry> ();
+	public List<TileChangeEntry> wallEntries = new List<TileChangeEntry> ();
+	public List<TileChangeEntry> windowEntries = new List<TileChangeEntry> ();
+	public List<TileChangeEntry> objectEntries = new List<TileChangeEntry> ();
 }
 
 [System.Serializable]
@@ -103,7 +115,7 @@ public class TileChangeEntry
 {
 	public Vector2Int gridPosition;
 	///<Summary>
-	/// Set tile to null if this is a tile removal
+	/// Set tileKey to empty string to deconstruct
 	///</Summary>
-	public Tile tile;
+	public string tileKey;
 }
