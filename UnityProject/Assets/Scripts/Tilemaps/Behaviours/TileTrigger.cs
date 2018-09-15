cd1495cce069b57a4b65cc3d60546e0e0f8fe1d6
Doobly <dooblynoobly@smashingpixels.com.au>
2018-09-15 16:42:33 +1000

    Start of Floor Tile Placement

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/TileTrigger.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/TileTrigger.cs
@@ -5,82 +5,100 @@ public class TileTrigger : InputTrigger
 {
 	private MetaTileMap metaTileMap;
 	private ObjectLayer objectLayer;
-	
+
 	private Tilemap floorTileMap;
 	private Tilemap baseTileMap;
 	private Tilemap wallTileMap;
 	private Tilemap windowTileMap;
 	private Tilemap objectTileMap;
 
-	void Start(){
+	void Start()
+	{
 		CacheTileMaps();
 	}
 
-	void CacheTileMaps ()
+	void CacheTileMaps()
 	{
-		var tilemaps = GetComponentsInChildren<Tilemap> (true);
+		var tilemaps = GetComponentsInChildren<Tilemap>(true);
 		for (int i = 0; i < tilemaps.Length; i++)
 		{
-			if (tilemaps[i].name.Contains ("Floors"))
+			if (tilemaps[i].name.Contains("Floors"))
 			{
 				floorTileMap = tilemaps[i];
 			}
 
-			if (tilemaps[i].name.Contains ("Base"))
+			if (tilemaps[i].name.Contains("Base"))
 			{
 				baseTileMap = tilemaps[i];
 			}
 
-			if (tilemaps[i].name.Contains ("Walls"))
+			if (tilemaps[i].name.Contains("Walls"))
 			{
 				wallTileMap = tilemaps[i];
 			}
 
-			if (tilemaps[i].name.Contains ("Windows"))
+			if (tilemaps[i].name.Contains("Windows"))
 			{
 				windowTileMap = tilemaps[i];
 			}
 
-			if (tilemaps[i].name.Contains ("Objects"))
+			if (tilemaps[i].name.Contains("Objects"))
 			{
 				objectTileMap = tilemaps[i];
 			}
 		}
 	}
 
-	public override void Interact (GameObject originator, Vector3 position, string hand)
+	public override void Interact(GameObject originator, Vector3 position, string hand)
 	{
-		metaTileMap = originator.GetComponentInParent<MetaTileMap> ();
-		objectLayer = originator.GetComponentInParent<ObjectLayer> ();
+		DetermineTileAction(originator, position, hand);
+	}
 
-		Vector3Int pos = objectLayer.transform.InverseTransformPoint (position).RoundToInt ();
+	private void DetermineTileAction(GameObject originator, Vector3 position, string hand)
+	{
+		metaTileMap = originator.GetComponentInParent<MetaTileMap>();
+		objectLayer = originator.GetComponentInParent<ObjectLayer>();
+
+		Vector3Int pos = objectLayer.transform.InverseTransformPoint(position).RoundToInt();
 		pos.z = 0;
 
-		LayerTile tile = metaTileMap.GetTile (pos);
+		LayerTile tile = metaTileMap.GetTile(pos);
+
+		var handObj = UIManager.Hands.CurrentSlot.Item;
+
+		// Nothing in hand, do nothing
+		if (handObj == null)
+		{
+			return;
+		}
 
 		if (tile?.TileType == TileType.Table)
 		{
-			TableInteraction interaction = new TableInteraction (gameObject, originator, position, hand);
+			TableInteraction interaction = new TableInteraction(gameObject, originator, position, hand);
 
-			interaction.Interact (isServer);
+			interaction.Interact(isServer);
 		}
 
 		if (tile?.TileType == TileType.Floor)
 		{
-			var handObj = UIManager.Hands.CurrentSlot.Item;
-
-			if(handObj == null)
-			{
-				return;
-			}
-
 			//Crowbar
 			if (handObj.GetComponent<CrowbarTrigger>())
 			{
 				var pna = originator.GetComponent<PlayerNetworkActions>();
 				var cellPos = floorTileMap.WorldToCell(position);
 				pna.CmdCrowBarRemoveFloorTile(transform.root.gameObject, TileChangeLayer.Floor,
-				new Vector2(cellPos.x, cellPos.y), position);
+					new Vector2(cellPos.x, cellPos.y), position);
+			}
+		}
+
+		if (tile?.LayerType == LayerType.Base)
+		{
+			if (handObj.GetComponent<UniFloorTile>())
+			{
+				var pna = originator.GetComponent<PlayerNetworkActions>();
+				var cellPos = baseTileMap.WorldToCell(position);
+				pna.CmdPlaceFloorTile(transform.root.gameObject, TileChangeLayer.Floor,
+					new Vector2(cellPos.x, cellPos.y), handObj);
 			}
 		}
 	}
