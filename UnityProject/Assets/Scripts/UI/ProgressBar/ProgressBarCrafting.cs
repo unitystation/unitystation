2122a0b7842b1b29240bd0d1e52041372967e7e3
Doobly <rob@smashingpixels.com.au>
2018-09-26 14:09:57 +1000

    Update progress bar from Server

--- UnityProject/Assets/Scripts/UI/ProgressBar/ProgressBarCrafting.cs
+++ UnityProject/Assets/Scripts/UI/ProgressBar/ProgressBarCrafting.cs
@@ -9,7 +9,7 @@ using UnityEngine.Networking;
 //be capable of keeping track of more then 1 progress state at any given time
 public class ProgressBarCrafting : NetworkBehaviour
 {
-	public Sprite[] progressSprites; //21 sprites = 4.77 per unit out of 100;
+	public Sprite[] progressSprites;
 
 	private SpriteRenderer spriteRenderer;
 
@@ -32,9 +32,16 @@ public class ProgressBarCrafting : NetworkBehaviour
 	public void StartProgress(Vector3 pos, float timeForCompletion,
 		Action serverCompletedAction, GameObject _player)
 	{
-		playerProgress.Add(new PlayerProgressCrafting{
+		var _playerSprites = _player.GetComponent<PlayerSprites>();
+		playerProgress.Add(new PlayerProgressCrafting
+		{
 			player = _player,
-			completedAction = serverCompletedAction
+				timeToFinish = timeForCompletion,
+				completedAction = serverCompletedAction,
+				position = pos,
+				playerSprites = _playerSprites,
+				playerPositionCache = _player.transform.position,
+				facingDirectionCache = _playerSprites.currentDirection
 		});
 
 		//Start the progress for the player:
@@ -43,12 +50,14 @@ public class ProgressBarCrafting : NetworkBehaviour
 
 	public void ClientUpdateProgress(Vector3 pos, int spriteIndex)
 	{
-		if(!spriteRenderer.gameObject.activeInHierarchy){
+		if (!spriteRenderer.gameObject.activeInHierarchy)
+		{
 			spriteRenderer.gameObject.SetActive(true);
 		}
 
 		// -1 sent from server means the crafting is complete. dismiss the progress bar:
-		if(spriteIndex == -1){
+		if (spriteIndex == -1)
+		{
 			spriteRenderer.gameObject.SetActive(false);
 			return;
 		}
@@ -56,12 +65,77 @@ public class ProgressBarCrafting : NetworkBehaviour
 		transform.position = pos;
 		spriteRenderer.sprite = progressSprites[spriteIndex];
 	}
+
+	void Update()
+	{
+		if (playerProgress.Count > 0)
+		{
+			UpdateProgressBars();
+		}
+	}
+
+	//Server only:
+	private void UpdateProgressBars()
+	{
+		for (int i = playerProgress.Count - 1; i >= 0; i--)
+		{
+			playerProgress[i].progress += Time.deltaTime;
+			if (playerProgress[i].timeToNotifyPlayer)
+			{
+				//Update the players progress bar
+				ProgressBarMessage.Send(playerProgress[i].player,
+					playerProgress[i].spriteIndex, playerProgress[i].position);
+			}
+
+			//Cancel the progress bar if the player moves away or faces another direction:
+			if (playerProgress[i].HasMovedAway())
+			{
+				CloseProgressBar(playerProgress[i]);
+				continue;
+			}
+
+			//Finished! Invoke the action and close the progress bar for the player
+			if (playerProgress[i].progress >= playerProgress[i].timeToFinish)
+			{
+				playerProgress[i].completedAction.Invoke();
+				CloseProgressBar(playerProgress[i]);
+			}
+		}
+	}
+
+	private void CloseProgressBar(PlayerProgressCrafting playerProg)
+	{
+		//Notify player to turn off progress bar:
+		ProgressBarMessage.Send(playerProg.player, -1, playerProg.position);
+		//remove from the player progress list:
+		playerProgress.Remove(playerProg);
+	}
 }
 
 public class PlayerProgressCrafting
 {
 	public float progress = 0f;
+	public float timeToFinish;
 	public GameObject player;
+	public PlayerSprites playerSprites;
+	public Vector3 playerPositionCache;
+	public Orientation facingDirectionCache;
 	public Action completedAction;
+	public Vector3 position;
+	public float progUnit { get { return timeToFinish / 21f; } }
+	public int spriteIndex { get { return Mathf.Clamp((int) (progress / progUnit), 0, 21); } }
+	public int lastSpriteIndex = 0;
+	public bool timeToNotifyPlayer { get { return lastSpriteIndex != spriteIndex; } }
+
+	//has the player moved away while the progress bar is in progress?
+	public bool HasMovedAway()
+	{
+		if (playerSprites.currentDirection != facingDirectionCache ||
+			player.transform.position != playerPositionCache)
+		{
+			return true;
+		}
+		return false;
+	}
 
 }
