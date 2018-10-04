a5a8818a315065fea527ccc3cd642d5798cdea3e
Doobly <rob@smashingpixels.com.au>
2018-10-04 19:03:12 +1000

    Added Random name generators and Character name GUI elements

---
+++ UnityProject/Assets/Scripts/UI/Lobby/CharacterCustomization.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lobby
+{
+	public class CharacterCustomization : MonoBehaviour
+	{
+		public InputField characterNameField;
+
+		private CharacterSettings currentCharacter;
+
+		void OnEnable()
+		{
+			LoadSettings();
+		}
+		private async void LoadSettings()
+		{
+			await Task.Delay(2000);
+			currentCharacter = PlayerManager.CurrentCharacterSettings;
+			DoInitChecks();
+		}
+
+		//First time setting up this character etc?
+		private void DoInitChecks()
+		{
+			if (string.IsNullOrEmpty(currentCharacter.Name))
+			{
+				currentCharacter.Name = StringManager.GetRandomMaleName();
+			}
+
+			RefreshAll();
+		}
+
+		private void RefreshAll()
+		{
+			RefreshName();
+		}
+
+		private void RefreshName()
+		{
+			characterNameField.text = currentCharacter.Name;
+		}
+
+		public void RandomNameBtn()
+		{
+			currentCharacter.Name = StringManager.GetRandomMaleName();
+			RefreshName();
+		}
+	}
+}
