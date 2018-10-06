a2fb9930b9e4f10e4402d5bfead8ec8d2594e533
Doobly <rob@smashingpixels.com.au>
2018-10-06 16:37:34 +1000

    Character settings progress

--- UnityProject/Assets/Scripts/UI/Lobby/CharacterCustomization.cs
+++ UnityProject/Assets/Scripts/UI/Lobby/CharacterCustomization.cs
@@ -25,9 +25,14 @@ namespace Lobby
 		public CharacterSprites facialHairSpriteController;
 		public CharacterSprites underwearSpriteController;
 		public CharacterSprites socksSpriteController;
+		public CharacterSprites eyesSpriteController;
+		public CharacterSprites[] skinControllers;
 
 		[SerializeField]
 		public List<string> availableHairColors = new List<string>();
+
+		[SerializeField]
+		public List<string> availableSkinColors = new List<string>();
 		private CharacterSettings currentCharacter;
 
 		public ColorPicker colorPicker;
@@ -69,6 +74,9 @@ namespace Lobby
 			RefreshFacialHair();
 			RefreshUnderwear();
 			RefreshSocks();
+			RefreshEyes();
+			RefreshSkinColor();
+			RefreshAge();
 		}
 
 		public void RollRandomCharacter()
@@ -82,6 +90,7 @@ namespace Lobby
 				UnityEngine.Random.Range(0, SpriteManager.UnderwearCollection.Count - 1)]);
 			currentCharacter.LoadSocksSetting(SpriteManager.SocksCollection[
 				UnityEngine.Random.Range(0, SpriteManager.SocksCollection.Count - 1)]);
+			currentCharacter.skinTone = availableSkinColors[UnityEngine.Random.Range(0, availableSkinColors.Count - 1)];
 
 			RefreshAll();
 		}
@@ -130,9 +139,42 @@ namespace Lobby
 		//AGE:
 		//------------------
 
+		private void RefreshAge()
+		{
+			ageField.text = currentCharacter.Age.ToString();
+		}
+
+		public void OnAgeChange()
+		{
+			int tryInt = 22;
+			int.TryParse(ageField.text, out tryInt);
+			currentCharacter.Age = tryInt;
+		}
 		//------------------
 		//EYE COLOR:
 		//------------------
+
+		public void OpenEyesColorPicker()
+		{
+			if (!colorPicker.gameObject.activeInHierarchy)
+			{
+				OpenColorPicker(eyeColor.color, EyeColorChange, 364f);
+			}
+		}
+
+		private void EyeColorChange(Color newColor)
+		{
+			currentCharacter.eyeColor = "#" + ColorUtility.ToHtmlStringRGB(newColor);
+			RefreshEyes();
+		}
+
+		private void RefreshEyes()
+		{
+			Color setColor = Color.black;
+			ColorUtility.TryParseHtmlString(currentCharacter.eyeColor, out setColor);
+			eyesSpriteController.image.color = setColor;
+			eyeColor.color = setColor;
+		}
 
 		//------------------
 		//HAIR:
@@ -160,35 +202,37 @@ namespace Lobby
 			RefreshHair();
 		}
 
-		public void HairColorScrollRight()
-		{
-			int tryNext = availableHairColors.IndexOf(currentCharacter.hairColor);
-			tryNext++;
-			if (tryNext == availableHairColors.Count)
-			{
-				tryNext = 0;
-			}
-			currentCharacter.hairColor = availableHairColors[tryNext];
-			RefreshHair();
-		}
+		//Scrolls have been disabled for time being:
 
-		public void HairColorScrollLeft()
-		{
-			int tryNext = availableHairColors.IndexOf(currentCharacter.hairColor);
-			tryNext--;
-			if (tryNext < 0)
-			{
-				tryNext = availableHairColors.Count - 1;
-			}
-			currentCharacter.hairColor = availableHairColors[tryNext];
-			RefreshHair();
-		}
+		// public void HairColorScrollRight()
+		// {
+		// 	int tryNext = availableHairColors.IndexOf(currentCharacter.hairColor);
+		// 	tryNext++;
+		// 	if (tryNext == availableHairColors.Count)
+		// 	{
+		// 		tryNext = 0;
+		// 	}
+		// 	currentCharacter.hairColor = availableHairColors[tryNext];
+		// 	RefreshHair();
+		// }
+
+		// public void HairColorScrollLeft()
+		// {
+		// 	int tryNext = availableHairColors.IndexOf(currentCharacter.hairColor);
+		// 	tryNext--;
+		// 	if (tryNext < 0)
+		// 	{
+		// 		tryNext = availableHairColors.Count - 1;
+		// 	}
+		// 	currentCharacter.hairColor = availableHairColors[tryNext];
+		// 	RefreshHair();
+		// }
 
 		public void OpenHairColorPicker()
 		{
 			if (!colorPicker.gameObject.activeInHierarchy)
 			{
-				OpenColorPicker(hairColor.color, HairColorChange);
+				OpenColorPicker(hairColor.color, HairColorChange, 374f);
 			}
 		}
 
@@ -214,16 +258,89 @@ namespace Lobby
 		//FACIAL HAIR:
 		//------------------
 
+		public void FacialHairScrollRight()
+		{
+			int tryNext = currentCharacter.facialHairCollectionIndex + 1;
+			if (tryNext >= SpriteManager.FacialHairCollection.Count)
+			{
+				tryNext = 0;
+			}
+			currentCharacter.LoadFacialHairSetting(SpriteManager.FacialHairCollection[tryNext]);
+			RefreshFacialHair();
+		}
+
+		public void FacialHairScrollLeft()
+		{
+			int tryNext = currentCharacter.facialHairCollectionIndex - 1;
+			if (tryNext < 0)
+			{
+				tryNext = SpriteManager.FacialHairCollection.Count - 1;
+			}
+			currentCharacter.LoadFacialHairSetting(SpriteManager.FacialHairCollection[tryNext]);
+			RefreshFacialHair();
+		}
 		private void RefreshFacialHair()
 		{
 			facialHairSpriteController.reference = currentCharacter.facialHairOffset;
 			facialHairSpriteController.UpdateSprite();
 			facialHairText.text = currentCharacter.facialHairName;
+			Color setColor = Color.black;
+			ColorUtility.TryParseHtmlString(currentCharacter.facialHairColor, out setColor);
+			facialHairSpriteController.image.color = setColor;
+			facialColor.color = setColor;
+		}
+
+		public void OpenFacialHairPicker()
+		{
+			if (!colorPicker.gameObject.activeInHierarchy)
+			{
+				OpenColorPicker(facialColor.color, FacialHairColorChange, 334f);
+			}
+		}
+
+		private void FacialHairColorChange(Color newColor)
+		{
+			currentCharacter.facialHairColor = "#" + ColorUtility.ToHtmlStringRGB(newColor);
+			RefreshFacialHair();
 		}
 
 		//------------------
 		//SKIN TONE:
 		//------------------
+		private void RefreshSkinColor()
+		{
+			Color setColor = Color.black;
+			ColorUtility.TryParseHtmlString(currentCharacter.skinTone, out setColor);
+			skinColor.color = setColor;
+			for (int i = 0; i < skinControllers.Length; i++)
+			{
+				skinControllers[i].image.color = setColor;
+			}
+		}
+
+		public void SkinToneScrollRight()
+		{
+			int index = availableSkinColors.IndexOf(currentCharacter.skinTone);
+			index++;
+			if (index == availableSkinColors.Count)
+			{
+				index = 0;
+			}
+			currentCharacter.skinTone = availableSkinColors[index];
+			RefreshSkinColor();
+		}
+
+		public void SkinToneScrollLeft()
+		{
+			int index = availableSkinColors.IndexOf(currentCharacter.skinTone);
+			index--;
+			if (index < 0)
+			{
+				index = availableSkinColors.Count - 1;
+			}
+			currentCharacter.skinTone = availableSkinColors[index];
+			RefreshSkinColor();
+		}
 
 		//------------------
 		//UNDERWEAR:
@@ -234,6 +351,28 @@ namespace Lobby
 			underwearSpriteController.reference = currentCharacter.underwearOffset;
 			underwearSpriteController.UpdateSprite();
 			underwearText.text = currentCharacter.underwearName;
+		}
+
+		public void UnderwearScrollRight()
+		{
+			int index = currentCharacter.underwearCollectionIndex + 1;
+			if (index == SpriteManager.UnderwearCollection.Count)
+			{
+				index = 0;
+			}
+			currentCharacter.LoadUnderwearSetting(SpriteManager.UnderwearCollection[index]);
+			RefreshUnderwear();
+		}
+
+		public void UnderwearScrollLeft()
+		{
+			int index = currentCharacter.underwearCollectionIndex - 1;
+			if (index < 0)
+			{
+				index = SpriteManager.UnderwearCollection.Count - 1;
+			}
+			currentCharacter.LoadUnderwearSetting(SpriteManager.UnderwearCollection[index]);
+			RefreshUnderwear();
 		}
 
 		//------------------
@@ -247,12 +386,38 @@ namespace Lobby
 			socksText.text = currentCharacter.socksName;
 		}
 
+		public void SocksScrollRight()
+		{
+			int index = currentCharacter.socksCollectionIndex + 1;
+			if (index == SpriteManager.SocksCollection.Count)
+			{
+				index = 0;
+			}
+			currentCharacter.LoadSocksSetting(SpriteManager.SocksCollection[index]);
+			RefreshSocks();
+		}
+
+		public void SocksScrollLeft()
+		{
+			int index = currentCharacter.socksCollectionIndex - 1;
+			if (index < 0)
+			{
+				index = SpriteManager.SocksCollection.Count - 1;
+			}
+			currentCharacter.LoadSocksSetting(SpriteManager.SocksCollection[index]);
+			RefreshSocks();
+		}
+
 		//------------------
 		//COLOR SELECTOR:
 		//------------------
 
-		private void OpenColorPicker(Color currentColor, Action<Color> _colorChangeEvent)
+		private void OpenColorPicker(Color currentColor, Action<Color> _colorChangeEvent, float yPos)
 		{
+			var rectT = colorPicker.gameObject.GetComponent<RectTransform>();
+			var anchoredPos = rectT.anchoredPosition;
+			anchoredPos.y = yPos;
+			rectT.anchoredPosition = anchoredPos;
 			colorPicker.gameObject.SetActive(true);
 			colorChangedEvent = _colorChangeEvent;
 			beforeEditingColor = currentColor;
@@ -284,18 +449,18 @@ namespace Lobby
 public class CharacterSettings
 {
 	public string Name;
-	public Gender Gender;
+	public Gender Gender = Gender.Male;
 	public int Age = 22;
 	public int hairStyleOffset;
 	public string hairStyleName;
 	public int hairCollectionIndex;
 	public string hairColor = "black";
-	public string eyeColor;
+	public string eyeColor = "black";
 	public int facialHairOffset;
 	public string facialHairName;
 	public int facialHairCollectionIndex;
 	public string facialHairColor;
-	public string skinTone;
+	public string skinTone = "#ffe0d1";
 	public int underwearOffset;
 	public string underwearName;
 	public int underwearCollectionIndex;
