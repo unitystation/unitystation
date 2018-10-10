dbd0f9b77ce7e4efb048f212fb86ffb799136f4b
Nick M <ndplay@ymail.com>
2018-10-10 15:59:19 +0400

    Asteroid location randomizer (based on escape shuttle one)

---
+++ UnityProject/Assets/Scripts/Map/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	private MatrixMove mm;
+
+	private float asteroidDistances = 550; //How far can asteroids be spawned
+
+
+	private void Awake()
+	{
+		mm = GetComponent<MatrixMove>();
+
+		SpawnNearStation();
+	}
+
+	public void SpawnNearStation()
+	{
+		//Based on EscapeShuttle.cs
+		mm.SetPosition(Random.insideUnitCircle * 500 + new Vector2(500, -500));
+	}
+
+}
