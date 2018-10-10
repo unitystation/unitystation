474333d9db962a81425c347dd08db4d341fc3342
Nick M <ndplay@ymail.com>
2018-10-10 16:12:46 +0400

    Network code for asteroids (thanks for help foma, again)

--- UnityProject/Assets/Scripts/Map/Asteroid.cs
+++ UnityProject/Assets/Scripts/Map/Asteroid.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Asteroid : MonoBehaviour {
+public class Asteroid : NetworkBehaviour {
 
 	private MatrixMove mm;
 
 	private float asteroidDistances = 550; //How far can asteroids be spawned
 
 
-	private void Awake()
+	private void Start()
 	{
 		mm = GetComponent<MatrixMove>();
 
 		SpawnNearStation();
 	}
 
+	[Server]
 	public void SpawnNearStation()
 	{
-		//Based on EscapeShuttle.cs
-		mm.SetPosition(Random.insideUnitCircle * 500 + new Vector2(500, -500));
+		if (isServer)
+		{
+			//Based on EscapeShuttle.cs
+			mm.SetPosition(Random.insideUnitCircle * 500 + new Vector2(500, -500));
+		}
 	}
 
 }
