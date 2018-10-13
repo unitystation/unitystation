03342fdb1a3d89e81ce38f40d805673b177916e0
Doobly <rob@smashingpixels.com.au>
2018-10-13 18:15:41 +1000

    Adds the Astar algorithm for path finding

--- UnityProject/Assets/Scripts/PathFinding/PathFinder.cs
+++ UnityProject/Assets/Scripts/PathFinding/PathFinder.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace PathFinding
@@ -11,24 +13,112 @@ namespace PathFinding
 		private CustomNetTransform customNetTransform;
 		public enum Status { idle, searching, navigating, waypointreached }
 		public Status status;
-		private Node goal;
+		private Node startNode;
+		private Node goalNode;
 		private bool pathFound = false;
+		PriorityQueue<Node> frontierNodes;
+		private List<Node> exploredNodes = new List<Node>();
 
+		private Action<List<Node>> pathFoundCallBack;
+		private Action failedCallBack;
+		private bool isComplete = false;
 		private void Awake()
 		{
 			registerTile = GetComponent<RegisterTile>();
 			customNetTransform = GetComponent<CustomNetTransform>();
 		}
 
-		public void TryMoveToPosition(Vector2Int destination)
+		///<summary>
+		/// Use local positions related to the matrix the object is on
+		///</summary>
+		public void FindNewPath(Vector2Int startPos, Vector2Int targetPos,
+			Action<List<Node>> pathCallBack, Action failedPathCallBack)
 		{
-			StartCoroutine(SearchForRoute(destination));
-		}
+			pathFoundCallBack = pathCallBack;
+			failedCallBack = failedPathCallBack;
 
-		IEnumerator SearchForRoute(Vector2Int destination)
+			startNode = new Node
+			{
+				position = startPos
+			};
+
+			goalNode = new Node
+			{
+				position = targetPos
+			};
+
+			frontierNodes = new PriorityQueue<Node>();
+			frontierNodes.Enqueue(startNode);
+			exploredNodes.Clear();
+
+		}
+		IEnumerator SearchForRoute()
 		{
 			status = Status.searching;
 			yield return YieldHelper.EndOfFrame;
+
+			while (!isComplete)
+			{
+				if (frontierNodes.Count > 0)
+				{
+					Node currentNode = frontierNodes.Dequeue();
+
+					if (exploredNodes.Contains(currentNode))
+					{
+						exploredNodes.Add(currentNode);
+					}
+
+					ExpandFrontierAStar(currentNode);
+					yield return YieldHelper.EndOfFrame;
+				}
+			}
 		}
+
+		private void ExpandFrontierAStar(Node node)
+		{
+			if (node != null)
+			{
+				for (int i = 0; i < node.neighbors.Count; i++)
+				{
+					if (!exploredNodes.Contains(node.neighbors[i]))
+					{
+						float distanceToNeighbor = GetNodeDistance(node, node.neighbors[i]);
+						float newDistanceTraveled = distanceToNeighbor + node.distanceTraveled +
+							(int) node.nodeType;
+
+						if (float.IsPositiveInfinity(node.neighbors[i].distanceTraveled) ||
+							newDistanceTraveled < node.neighbors[i].distanceTraveled)
+						{
+							node.neighbors[i].previous = node;
+							node.neighbors[i].distanceTraveled = newDistanceTraveled;
+						}
+
+						if (!frontierNodes.Contains(node.neighbors[i]))
+						{
+							int distanceToGoal = (int) GetNodeDistance(node.neighbors[i], goalNode);
+							node.neighbors[i].priority = (int) node.neighbors[i].distanceTraveled +
+								distanceToGoal;
+							frontierNodes.Enqueue(node.neighbors[i]);
+						}
+					}
+				}
+			}
+		}
+
+		private float GetNodeDistance(Node source, Node target)
+		{
+			int dx = Mathf.Abs(source.position.x - target.position.x);
+			int dy = Mathf.Abs(source.position.y - target.position.y);
+
+			int min = Mathf.Min(dx, dy);
+			int max = Mathf.Max(dx, dy);
+
+			int diagonalSteps = min;
+			int straightSteps = max - min;
+
+			return (1.4f * diagonalSteps + straightSteps);
+		}
+
+		//TODO get neighbours of the node
 	}
 }
