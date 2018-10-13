557436254094b6d0602e01fa56d8e11c1fe6654d
Doobly <rob@smashingpixels.com.au>
2018-10-13 17:12:13 +1000

    Start of pathfinding system

---
+++ UnityProject/Assets/Scripts/NPC/Ian.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using PathFinding;
+
+public class Ian : MonoBehaviour
+{
+	private PathFinder pathFinder;
+	private CustomNetTransform customNetTransform;
+
+	private void Awake()
+	{
+		pathFinder = GetComponent<PathFinder>();
+		customNetTransform = GetComponent<CustomNetTransform>();
+	}
+
+}
---
+++ UnityProject/Assets/Scripts/PathFinding/PathFinder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathFinding
+{
+	public class PathFinder : MonoBehaviour
+	{
+		private RegisterTile registerTile;
+		private Matrix matrix => registerTile.Matrix;
+		private CustomNetTransform customNetTransform;
+		public enum Status { idle, searching, navigating, waypointreached }
+		public Status status;
+		private Node goal;
+		private bool pathFound = false;
+
+		private void Awake()
+		{
+			registerTile = GetComponent<RegisterTile>();
+			customNetTransform = GetComponent<CustomNetTransform>();
+		}
+
+		public void TryMoveToPosition(Vector2Int destination)
+		{
+			StartCoroutine(SearchForRoute(destination));
+		}
+
+		IEnumerator SearchForRoute(Vector2Int destination)
+		{
+			status = Status.searching;
+			yield return YieldHelper.EndOfFrame;
+		}
+	}
+}
