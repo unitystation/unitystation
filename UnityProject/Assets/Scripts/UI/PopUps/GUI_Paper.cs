950073a3ae49e78783406745ea066c9fd1aa3bdb
Doobly <rob@smashingpixels.com.au>
2018-10-15 16:00:56 +1000

    Added ability to create PopOut windows in the NetTab system

---
+++ UnityProject/Assets/Scripts/UI/PopUps/GUI_Paper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Paper : NetTab
+{
+	void Start()
+	{
+		Debug.Log("Paper GUI opened");
+	}
+
+	public void ClosePaper()
+	{
+		
+	}
+}
--- UnityProject/Assets/Scripts/UI/Tab.cs
+++ UnityProject/Assets/Scripts/UI/Tab.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class Tab : MonoBehaviour {
 	[HideInInspector]
 	public bool Hidden = false;
+	public bool isPopOut = false;
 }
