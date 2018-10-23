e479c7b960488e4219e052b2a2a9b2a4cda26cc2
Doobly <rob@smashingpixels.com.au>
2018-10-24 09:14:02 +1000

    Added subtle drag interaction ques

--- UnityProject/Assets/Scripts/UI/DragAndDrop/DragAndDrop.cs
+++ UnityProject/Assets/Scripts/UI/DragAndDrop/DragAndDrop.cs
@@ -9,10 +9,16 @@ public class DragAndDrop : MonoBehaviour
 	private bool isDragging = false;
 	public UI_ItemSlot ItemSlotCache { get; private set; }
 	public GameObject ItemCache { get; private set; }
+	public Shadow shadow;
+
+	Vector3 scaleCache;
+	Vector3 interactableScale;
 
 	public void Start()
 	{
 		dragDummy.enabled = false;
+		scaleCache = dragDummy.transform.localScale;
+		interactableScale = scaleCache * 1.1f;
 	}
 	public void UI_ItemDrag(UI_ItemSlot itemSlot)
 	{
@@ -40,6 +46,24 @@ public class DragAndDrop : MonoBehaviour
 		}
 		ItemSlotCache = null;
 		ItemCache = null;
+		ResetInteractable();
+	}
+
+	public void EnteredInteractable()
+	{
+		if(dragDummy.transform.localScale != interactableScale){
+			dragDummy.transform.localScale = interactableScale;
+		}
+		if (dragDummy.enabled)
+		{
+			shadow.enabled = true;
+		}
+	}
+
+	public void ResetInteractable()
+	{
+		dragDummy.transform.localScale = scaleCache;
+		shadow.enabled = false;
 	}
 
 	public void Update()
