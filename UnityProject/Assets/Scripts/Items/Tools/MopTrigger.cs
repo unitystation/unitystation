98aa37938a3bbc73bc44b9a7aaca2b82a90f7544
AlexTheM8 <32232543+AlexTheM8@users.noreply.github.com>
2018-12-03 22:17:49 -0500

    Added Mop Script
    Not developed

---
+++ UnityProject/Assets/Scripts/Items/Tools/MopTrigger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MopTrigger : PickUpTrigger
+{
+	[SyncVar]
+	private bool canBeUsed = false;
+
+    public override void Interact (GameObject originator, Vector3 position, string hand)
+    {
+        //TODO:  Fill this in.
+
+        if (UIManager.Hands.CurrentSlot.Item != gameObject)
+        {
+            base.Interact (originator, position, hand);
+            return;
+        }
+        var targetWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (canBeUsed && PlayerManager.PlayerScript.IsInReach(targetWorldPos))
+        {
+            //TODO INSERT CLEANING FUNCTION
+        }
+
+        base.Interact (originator, position, hand);
+    }
+
+    //Broadcast from EquipmentPool.cs **ServerSide**
+    public void OnAddToPool ()
+    {
+        canBeUsed = true;
+    }
+
+    //Broadcast from EquipmentPool.cs **ServerSide**
+    public void OnRemoveFromInventory ()
+    {
+        canBeUsed = false;
+    }
+}
