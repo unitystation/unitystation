a9f24b7a61a2351b73cba9b4f0a62b2cecd9863d
dapocalypse <dapocalypse@users.noreply.github.com>
2018-12-10 14:05:22 -0800

    Update DNABloodTypeGen.cs

--- UnityProject/Assets/Scripts/Health/Living/DNABloodTypeGen.cs
+++ UnityProject/Assets/Scripts/Health/Living/DNABloodTypeGen.cs
@@ -1,4 +1,4 @@
-using Random = UnityEngine.Random;
+using System;
 
 [System.Serializable]
 public class DNAandBloodType
@@ -7,44 +7,44 @@ public class DNAandBloodType
 	
 	public BloodTypes BloodType;
 	
-	private float BloodTypeGenerator;
+	private static Random random = new Random();
+	
+	private float BloodTypeGenerator = random.Next(0,1000);
 	
 	public DNAandBloodType()
 	{	
 		// Assigns DNA a GUID
 		DNAString = System.Guid.NewGuid().ToString();
-		// Rolls from 1 to 1000 to determine blood chance.
-		BloodTypeGenerator = Random.Range(1.0f,1000.0f);
 		// Assigns blood type by roll, note that there isnt an equal chance for each roll.
-		if (BloodTypeGenerator <= 364.0f)
+		if (BloodTypeGenerator <= 364)
 		{
 			BloodType = BloodTypes.oPos;
 		}
-		else if (BloodTypeGenerator > 364.0f && BloodTypeGenerator <= 407.0f)
+		else if (BloodTypeGenerator > 364 && BloodTypeGenerator <= 407)
 		{
 			BloodType = BloodTypes.oNeg;
 		}
-		else if (BloodTypeGenerator > 407.0f && BloodTypeGenerator <= 690.0f)
+		else if (BloodTypeGenerator > 407 && BloodTypeGenerator <= 690)
 		{
 			BloodType = BloodTypes.aPos;
 		}
-		else if (BloodTypeGenerator > 690.0f && BloodTypeGenerator <= 725.0f)
+		else if (BloodTypeGenerator > 690 && BloodTypeGenerator <= 725)
 		{
 			BloodType = BloodTypes.aNeg;
 		}
-		else if (BloodTypeGenerator > 725.0f && BloodTypeGenerator <= 931.0f)
+		else if (BloodTypeGenerator > 725 && BloodTypeGenerator <= 931)
 		{
 			BloodType = BloodTypes.bPos;
 		}
-		else if (BloodTypeGenerator > 931.0f && BloodTypeGenerator <= 945.0f)
+		else if (BloodTypeGenerator > 931 && BloodTypeGenerator <= 945)
 		{
 			BloodType = BloodTypes.bNeg;
 		}
-		else if (BloodTypeGenerator > 945.0f && BloodTypeGenerator <= 995.0f)
+		else if (BloodTypeGenerator > 945 && BloodTypeGenerator <= 995)
 		{
 			BloodType = BloodTypes.abPos;
 		}
-		else if (BloodTypeGenerator > 995.0f && BloodTypeGenerator <= 1000.0f)
+		else if (BloodTypeGenerator > 995 && BloodTypeGenerator <= 1000)
 		{
 			BloodType = BloodTypes.abNeg;
 		}
