1d143640d4c96274e868c6b05f563d1fcf232302
Jasper <jibbajabbafication@gmail.com>
2018-12-18 22:34:28 +0000

    Escape key target stack system

---
+++ UnityProject/Assets/Scripts/UI/EscapeKeyTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeKeyTarget : MonoBehaviour {
+	// This component allows the object to be disabled with the escape key automatically
+	// It pushes the object to the escape key target stack when it's enabled, and pops it when it's disabled
+
+	/// <summary>
+	/// This is the stack which keeps track of all the game objects so they can be closed later
+	/// </summary>
+	[HideInInspector]
+	public static Stack<GameObject> TargetStack = new Stack<GameObject>();
+	
+	void OnEnable()
+	{
+		// Add this game object to the top of the stack so Esc will close it next
+		TargetStack.Push(gameObject);
+		// Logger.Log("Pushing escape key target stack: " + TargetStack.Peek().name, Category.UI);
+	}
+	void OnDisable()
+	{
+		// Revert back to the previous escape key target
+		// Logger.Log("Popping escape key target stack: " + TargetStack.Peek().name, Category.UI);
+		TargetStack.Pop();	
+	}
+}
