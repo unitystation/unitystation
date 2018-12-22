e9b31c453d5ca1c578ecbba8d1d97e455a75b79f
chairbender <kwhipke1@gmail.com>
2018-12-22 11:55:56 -0800

    #389 - add basic logic for checking wallmount visibility

---
+++ UnityProject/Assets/Scripts/Objects/WallmountBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Behavior common to all wall mounts (such as visibility checking)
+/// </summary>
+public class WallmountBehavior : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		//don't run check until player is created
+		if (PlayerManager.LocalPlayer == null)
+		{
+			return;
+		}
+		//TODO: Only update on player move
+		//TODO: Only run the check if it's currently on screen, avoid wastefully running this logic every update.
+
+		//decide if we should be visible based on player position
+		var spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		//we can ignore this check if we are already visible
+		foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+		{
+			Vector3 headingToPlayer = PlayerManager.LocalPlayer.transform.position - transform.position;
+			Vector3 facing = transform.up;
+			float difference = Vector3.Angle(facing, headingToPlayer);
+				
+			spriteRenderer.enabled = difference > 90 || difference < -90;
+		}
+	}
+}
