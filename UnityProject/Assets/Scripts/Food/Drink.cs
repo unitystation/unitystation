e02cd115e387afbf499f9f1b4d4367235b885161
Hamish <blickorama@gmail.com>
2018-12-24 11:47:51 +1300

    Added food and drink leavings
    Added behavior that allows leavings to be created when food/drink is consumed.
    Added an empty plate and applied it to the meat steak.
    Added an empty glass and applied it to various drinks.

--- UnityProject/Assets/Scripts/Food/Drink.cs
+++ UnityProject/Assets/Scripts/Food/Drink.cs
@@ -7,7 +7,7 @@ public class Drink : FoodBehaviour {
 	// Use this for initialization
 	public override void TryEat()
 	{
-		PlayerManager.LocalPlayerScript.playerNetworkActions.CmdEatFood(gameObject,
-			UIManager.Hands.CurrentSlot.eventName, true);
+        isDrink = true;
+        base.TryEat();
 	}
 }
