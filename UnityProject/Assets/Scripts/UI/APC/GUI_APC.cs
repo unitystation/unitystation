5aae12b027692c6570d49e8293be46f6daf8df49
Jasper <jibbajabbafication@gmail.com>
2019-01-09 23:48:11 +0000

    Start of APC tab prefab

---
+++ UnityProject/Assets/Scripts/UI/APC/GUI_APC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_APC : NetTab
+{
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
