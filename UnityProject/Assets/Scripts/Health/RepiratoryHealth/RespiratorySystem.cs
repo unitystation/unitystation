b6a4fe6a2a726cdc2a21e1f29b69eb15bd3ff3a6
Doobly <rob@smashingpixels.com.au>
2019-01-09 14:51:28 +1000

    Respiratory system progress

--- UnityProject/Assets/Scripts/Health/RepiratoryHealth/RespiratorySystem.cs
+++ UnityProject/Assets/Scripts/Health/RepiratoryHealth/RespiratorySystem.cs
@@ -20,14 +20,18 @@ public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 	}
 	private BloodSystem bloodSystem;
 	private LivingHealthBehaviour livingHealthBehaviour;
+	private PlayerScript playerScript;
 	public bool IsBreathing { get; private set; } = true;
+	public bool IsSuffocating { get; private set; } = false;
 	private float tickRate = 1f;
 	private float tick = 0f;
+	private float suffocationTime = 0f;
 
 	void Awake()
 	{
 		bloodSystem = GetComponent<BloodSystem>();
 		livingHealthBehaviour = GetComponent<LivingHealthBehaviour>();
+		playerScript = GetComponent<PlayerScript>();
 	}
 
 	void OnEnable()
@@ -44,16 +48,123 @@ public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 	//Handle by UpdateManager
 	void UpdateMe()
 	{
-		tick += Time.deltaTime;
-		if (tick >= tickRate)
+		//Server Only:
+		if (CustomNetworkManager.Instance._isServer)
 		{
-			MonitorSystem();
+			tick += Time.deltaTime;
+			if (tick >= tickRate)
+			{
+				MonitorSystem();
+			}
 		}
 	}
 
 	void MonitorSystem()
 	{
-		
+		if (livingHealthBehaviour.IsDead)
+		{
+			return;
+		}
+
+		CheckBreathing();
+		CheckSuffocation();
 	}
 
+	/// Check breathing state
+	void CheckBreathing()
+	{
+		// Try not to make super long conditions here, break them up
+		// into each individual condition for ease of reading
+		if (IsBreathing)
+		{
+			if (livingHealthBehaviour.OverallHealth <= 0)
+			{
+				IsBreathing = false;
+				IsSuffocating = true;
+			}
+
+			if (IsInSpace() && !IsEvaCompatible())
+			{
+				IsBreathing = false;
+				IsSuffocating = true;
+			}
+			//TODO: other conditions that would prevent breathing
+		}
+
+		if (!IsBreathing)
+		{
+			if (IsInSpace() && IsEvaCompatible())
+			{
+				IsBreathing = true;
+			}
+
+			if (!IsInSpace())
+			{
+				IsBreathing = true;
+			}
+		}
+	}
+
+	/// Preform any suffocation monitoring here:
+	void CheckSuffocation()
+	{
+		if (IsSuffocating)
+		{
+			if (IsBreathing)
+			{
+				IsSuffocating = false;
+				suffocationTime = 0f;
+			}
+			else
+			{
+				suffocationTime += Time.deltaTime;
+				if (suffocationTime > 240f) //4 minutes without oxygen
+				{
+					//4 minutes of no breath = death
+					livingHealthBehaviour.Death();
+				}
+			}
+		}
+	}
+
+	/// TODO: Replace this when atmos pressure is implemented
+	/// we are just doing a space check for the time being
+	private bool IsInSpace()
+	{
+		if (MatrixManager.IsSpaceAt(Vector3Int.RoundToInt(transform.position)))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	private bool IsEvaCompatible()
+	{
+		if (playerScript == null)
+		{
+			Logger.Log("This is not a human player. Develop a way to detect EVA equipment on animals",
+				Category.Health);
+			return false;
+		}
+
+		var headItem = playerScript.playerNetworkActions.Inventory["head"].Item;
+		var suitItem = playerScript.playerNetworkActions.Inventory["suit"].Item;
+		if (headItem == null || suitItem == null)
+		{
+			return false;
+		}
+
+		var headItemAtt = headItem?.GetComponent<ItemAttributes>();
+		var suitItemAtt = suitItem?.GetComponent<ItemAttributes>();
+
+		// TODO when atmos is merged and oxy tanks are in, then check oxygen flow 
+		// through mask here
+
+		if (headItemAtt == null || suitItemAtt == null)
+		{
+			return false;
+		}
+
+		return headItemAtt.evaCapable && suitItemAtt.evaCapable;
+	}
 }
