f31a2d3d76c5a63c50703cb04b2a5dd9a8375fbe
chairbender <kwhipke1@gmail.com>
2019-01-21 15:27:09 -0800

    #1426 add server-side checks for interacting with wallmounts on the wrong side of the wall

--- UnityProject/Assets/Scripts/Objects/WallmountBehavior.cs
+++ UnityProject/Assets/Scripts/Objects/WallmountBehavior.cs
@@ -17,4 +17,18 @@ public class WallmountBehavior : MonoBehaviour {
 			renderer.gameObject.AddComponent<WallmountSpriteBehavior>();
 		}
 	}
+
+	/// <summary>
+	/// Checks if the wallmount is facing the specified position
+	/// </summary>
+	/// <param name="worldPosition">position to check</param>
+	/// <returns>true iff it is facing the position</returns>
+	public bool IsFacingPosition(Vector3 worldPosition)
+	{
+		Vector3 headingToPosition = worldPosition - transform.position;
+		Vector3 facing = transform.up;
+		float difference = Vector3.Angle(facing, headingToPosition);
+		//91 rather than 90 helps prevent flickering due to rounding
+		return difference >= 91 || difference <= -91;
+	}
 }
