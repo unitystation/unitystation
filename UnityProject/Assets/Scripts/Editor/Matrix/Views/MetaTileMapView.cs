2e275ecd83334ae188448b8a0bf0e605602a1b51
krille90 <christian.bender.saebelkampf@gmail.com>
2019-02-05 23:31:38 +0100

    * Adding breathing from internals * Renaming MetaDataNode.Atmos to MetaDataNode.GasMix * Adding Interface IGasMixContainer to identify classes that have GasMix (so far GasContainer and MetaDataNode) * Moved PlayerScript GetComponent methods to Awake instead of Start * Adding direct reference to ItemAttributes in InventorySlot

--- UnityProject/Assets/Scripts/Editor/Matrix/Views/MetaTileMapView.cs
+++ UnityProject/Assets/Scripts/Editor/Matrix/Views/MetaTileMapView.cs
@@ -105,9 +105,8 @@ public class MetaTileMapView : BasicView
 //			if (!source.IsEmptyAt(position))
 			if (Selection.activeGameObject.GetComponent<MetaTileMap>() == source)
 			{
-//				Vector3 p = source.transform.TransformPoint(position) + GizmoUtils.HalfOne;
-				Vector3 p2 = source.CellToWorld(position);
-				GizmoUtils.DrawText($"{position.x}, {position.y}", p2, false);
+				Vector3 p = source.transform.TransformPoint(position) + GizmoUtils.HalfOne;
+				GizmoUtils.DrawText($"{position.x}, {position.y}", p, false);
 			}
 		}
 	}
