07f6e72d0b0ca86dff9e930c9df112b617232478
fuele <37090676+fuele@users.noreply.github.com>
2019-02-17 16:35:01 -0600

    Fixed indentation on OxygenButton class

--- UnityProject/Assets/Scripts/UI/OxygenButton.cs
+++ UnityProject/Assets/Scripts/UI/OxygenButton.cs
@@ -2,44 +2,44 @@
 using UnityEngine.UI;
 
 
-	public class OxygenButton : MonoBehaviour
+public class OxygenButton : MonoBehaviour
+{
+	private Image image;
+	public Sprite[] stateSprites;
+
+	// Use this for initialization
+	private void Start()
 	{
-		private Image image;
-		public Sprite[] stateSprites;
-
-		// Use this for initialization
-		private void Start()
-		{
-			image = GetComponent<Image>();
-			UIManager.IsOxygen = false;
-		}
-
-		/// <summary>
-		/// toggle the button state and play any sounds
-		/// </summary>
-		public void OxygenSelect()
-		{
-			SoundManager.Play("Click01");
-			//toggle state
-			EnableOxygen(!UIManager.IsOxygen);
-		}
-
-		/// <summary>
-		/// Sets the state of the OxygenButton.
-		/// </summary>
-		/// <param name="enableOxygen"></param>
-		public void EnableOxygen(bool enableOxygen)
-		{
-			UIManager.IsOxygen = enableOxygen;
-			PlayerManager.LocalPlayerScript.playerNetworkActions.CmdSetInternalsEnabled(enableOxygen);
-			if (enableOxygen)
-			{
-				image.sprite = stateSprites[1];
-			}
-			else
-			{
-				image.sprite = stateSprites[0];
-			}
-
-		}
+		image = GetComponent<Image>();
+		UIManager.IsOxygen = false;
 	}
+
+	/// <summary>
+	/// toggle the button state and play any sounds
+	/// </summary>
+	public void OxygenSelect()
+	{
+		SoundManager.Play("Click01");
+		//toggle state
+		EnableOxygen(!UIManager.IsOxygen);
+	}
+
+	/// <summary>
+	/// Sets the state of the OxygenButton.
+	/// </summary>
+	/// <param name="enableOxygen"></param>
+	public void EnableOxygen(bool enableOxygen)
+	{
+		UIManager.IsOxygen = enableOxygen;
+		PlayerManager.LocalPlayerScript.playerNetworkActions.CmdSetInternalsEnabled(enableOxygen);
+		if (enableOxygen)
+		{
+			image.sprite = stateSprites[1];
+		}
+		else
+		{
+			image.sprite = stateSprites[0];
+		}
+
+	}
+}
