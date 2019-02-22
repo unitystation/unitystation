bc44068302eb2a62efcd9a66616cc6c0f19357c1
Leonard Kruc <kruc.leo@gmail.com>
2019-02-22 17:28:34 +0200

    Removed Redundent material form LightingSystem

--- UnityProject/Assets/Scripts/Lighting System/MaterialContainer.cs
+++ UnityProject/Assets/Scripts/Lighting System/MaterialContainer.cs
@@ -8,17 +8,17 @@ public class MaterialContainer
 	public Material blitMaterial;
 	public Material lightBlurMaterial;
 	public Material lightWallBlurMaterial;
+
 	/// <summary>
-	/// Material used for calcualting the final occlusion mask, including floor and wall occlusion
+	/// Material used for calculating the final occlusion mask, including floor and wall occlusion
 	/// </summary>
 	public Material fovMaterial;
+
 	/// <summary>
 	/// Material used for calculating floor-only occlusion
 	/// </summary>
 	public Material floorFovMaterial;
-	public Material MaskMixerMaterial;
 
 	public Material PPRTTransformMaterial;
-
 	public Shader OcclusionMaskShader;
 }
