ac8722a1a7dcd3c884b6d64584d30a130e4d5528
fuele <37090676+fuele@users.noreply.github.com>
2019-02-22 21:18:08 -0600

    Added restart-round command

--- UnityProject/Assets/IngameDebugConsole/Scripts/DebugLogUnitystationCommands.cs
+++ UnityProject/Assets/IngameDebugConsole/Scripts/DebugLogUnitystationCommands.cs
@@ -11,5 +11,31 @@ namespace IngameDebugConsole
 		{
 			SuicideMessage.Send(null);
 		}
+
+		[ConsoleMethod("damage-self", "Usage:\ndamage-self <bodyPart> <brute amount> <burn amount>\nExample: damage-self LeftArm 40 20")]
+		public static void RunDamageSelf(string bodyPartString, int burnDamage, int bruteDamage)
+		{
+			Logger.Log("passed in bodyPart[" + bodyPartString + "]");
+			bool success = BodyPartType.TryParse(bodyPartString, true, out BodyPartType bodyPart);
+
+			if (success)
+			{
+				Logger.Log("Debugger inflicting " +burnDamage+" burn damage and "+bruteDamage +" brute damage on " + bodyPart + " of " + PlayerManager.LocalPlayer.name, Category.DebugConsole);
+				HealthBodyPartMessage.Send(PlayerManager.LocalPlayer, PlayerManager.LocalPlayer, bodyPart, burnDamage, bruteDamage);
+			}
+			else
+			{
+				Logger.LogError("Usage:\ndamage-self <bodyPart> <brute amount> <burn amount>\nExample: damage-self LeftArm 40 20",Category.DebugConsole);
+			}
+		}
+
+		[ConsoleMethod("restart-round", "restarts the round")]
+		public static void RunRestartRound()
+		{
+			Logger.Log("Round restart triggered by DebugConsole.", Category.DebugConsole);
+			GameManager manager = GameObject.FindObjectOfType<GameManager>();
+			manager.RestartRound();
+		}
+
 	}
 }
