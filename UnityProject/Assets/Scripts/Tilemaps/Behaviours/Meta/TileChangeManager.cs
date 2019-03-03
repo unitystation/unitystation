efb857129ec9349fc7866ad72cf44eb0c3552e14
krille90 <christian.bender.saebelkampf@gmail.com>
2019-03-03 22:11:26 +0100

    Added Tilemap Change Syncing back in.

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/TileChangeManager.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/TileChangeManager.cs
@@ -10,9 +10,42 @@ public class TileChangeManager : NetworkBehaviour
 {
 	private MetaTileMap metaTileMap;
 
+	private TileChangeList changeList = new TileChangeList();
+
 	private void Awake()
 	{
 		metaTileMap = GetComponentInChildren<MetaTileMap>();
+	}
+
+	public void InitServerSync(string data)
+	{
+		//Unpacking the data example (and then run action change)
+		changeList = JsonUtility.FromJson<TileChangeList>(data);
+
+		foreach (TileChangeEntry entry in changeList.List)
+		{
+			// load tile & apply
+			if (entry.TileType.Equals(TileType.None))
+			{
+				RemoveTile(entry.Position, entry.LayerType, entry.RemoveAll);
+			}
+			else
+			{
+				UpdateTile(entry.Position, entry.TileType, entry.TileName);
+			}
+		}
+	}
+
+	[Server]
+	public void NotifyPlayer (GameObject requestedBy)
+	{
+		if (changeList.List.Count > 0)
+		{
+			Debug.Log ("Request all updates: " + requestedBy.name);
+			string jsondata = JsonUtility.ToJson (changeList);
+
+			TileChangesNewClientSync.Send(gameObject, requestedBy, jsondata);
+		}
 	}
 
 	[Server]
@@ -21,6 +54,8 @@ public class TileChangeManager : NetworkBehaviour
 		if (IsDifferent(position, tileType, tileName))
 		{
 			RpcUpdateTile(position, tileType, tileName);
+
+			AddToChangeList(position, tileType:tileType, tileName:tileName);
 		}
 	}
 
@@ -30,6 +65,8 @@ public class TileChangeManager : NetworkBehaviour
 		if(metaTileMap.HasTile(position, layerType))
 		{
 			RpcRemoveTile(position, layerType, false);
+
+			AddToChangeList(position, layerType);
 		}
 	}
 
@@ -41,22 +78,36 @@ public class TileChangeManager : NetworkBehaviour
 		if (metaTileMap.HasTile(position, layerType))
 		{
 			RpcRemoveTile(position, layerType, true);
+
+			AddToChangeList(position, layerType, removeAll:true);
 		}
 	}
 
 	[ClientRpc]
 	private void RpcRemoveTile(Vector3 position, LayerType layerType, bool onlyRemoveEffect)
 	{
+		RemoveTile(position, layerType, onlyRemoveEffect);
+	}
+
+	private void RemoveTile(Vector3 position, LayerType layerType, bool onlyRemoveEffect)
+	{
 		if (onlyRemoveEffect)
 		{
 			position.z = -1;
 		}
 
-		metaTileMap.RemoveTile(position.RoundToInt(), layerType, !onlyRemoveEffect);
+		Vector3Int p = position.RoundToInt();
+
+		metaTileMap.RemoveTile(p, layerType, !onlyRemoveEffect);
 	}
 
 	[ClientRpc]
 	private void RpcUpdateTile(Vector3 position, TileType tileType, string tileName)
+	{
+		UpdateTile(position, tileType, tileName);
+	}
+
+	private void UpdateTile(Vector3 position, TileType tileType, string tileName)
 	{
 		LayerTile layerTile = TileManager.GetTile(tileType, tileName);
 
@@ -65,7 +116,21 @@ public class TileChangeManager : NetworkBehaviour
 			position.z -= 1;
 		}
 
-		metaTileMap.SetTile(position.RoundToInt(), layerTile);
+		Vector3Int p = position.RoundToInt();
+
+		metaTileMap.SetTile(p, layerTile);
+	}
+
+	private void AddToChangeList(Vector3 position, LayerType layerType=LayerType.None, TileType tileType=TileType.None, string tileName=null, bool removeAll = false)
+	{
+		changeList.List.Add(new TileChangeEntry()
+		{
+			Position = position,
+			LayerType = layerType,
+			TileType = tileType,
+			TileName = tileName,
+			RemoveAll = removeAll
+		});
 	}
 
 	private bool IsDifferent(Vector3Int position, TileType tileType, string tileName)
@@ -74,4 +139,24 @@ public class TileChangeManager : NetworkBehaviour
 
 		return metaTileMap.GetTile(position, layerTile.LayerType) != layerTile;
 	}
+}
+
+[System.Serializable]
+public class TileChangeList
+{
+	public List<TileChangeEntry> List = new List<TileChangeEntry>();
+}
+
+[System.Serializable]
+public class TileChangeEntry
+{
+	public Vector3 Position;
+
+	public TileType TileType;
+
+	public LayerType LayerType;
+
+	public string TileName;
+
+	public bool RemoveAll;
 }
