85a0c578984cf58b3a75f1490b1b7c379c884b6f
James <james.blick66@gmail.com>
2019-03-07 01:02:38 +1300

    Update WindowDrag.cs
    Updates BeginDrag() to make it more efficient.
    Updates OnDrag() to make it more efficient, and to clamp the dragged window within the screen.
    Adds summaries to BeginDrag() and OnDrag().

--- UnityProject/Assets/Scripts/UI/WindowDrag.cs
+++ UnityProject/Assets/Scripts/UI/WindowDrag.cs
@@ -7,10 +7,12 @@
 		private float offsetX;
 		private float offsetY;
 		private Vector3 startPositon;
-		
+		private RectTransform rectTransform;
+
 		void Start () {
 			// Save initial window start positon
 			startPositon = gameObject.transform.position;
+			rectTransform = GetComponent<RectTransform>();
 		}
 		void OnDisable () {
 			// Reset window to start position
@@ -19,14 +21,40 @@
 				gameObject.transform.position = startPositon;
 			}
 		}
+
+		/// <summary>
+		/// Sets the windowDrag fields offsetX and offsetY from the window position and the mouse position.
+        /// The fields offsetX and offsetY are the mouse position's offset from the window's top-left corner.
+		/// In onDrag(), these offsets are used to "hook" the window to the cursor as it is dragged.
+		/// </summary>
 		public void BeginDrag()
 		{
-			offsetX = transform.position.x - Input.mousePosition.x;
-			offsetY = transform.position.y - Input.mousePosition.y;
+			var windowTransformPosition = transform.position;
+
+			offsetX = windowTransformPosition.x - Input.mousePosition.x;
+			offsetY = windowTransformPosition.y - Input.mousePosition.y;
 		}
 
+		/// <summary>
+		/// Moves the window with the cursor within the screen bounds when called.
+		/// </summary>
 		public void OnDrag()
 		{
-			transform.position = new Vector3(offsetX + Input.mousePosition.x, offsetY + Input.mousePosition.y);
+			var windowSize = rectTransform.sizeDelta;
+			var windowScale = rectTransform.lossyScale;
+
+			var windowWidth = windowSize.x;
+			var windowHeight = windowSize.y;
+
+			var widthScale = windowScale.x;
+			var heightScale = windowScale.y;
+
+			transform.position = new Vector3(
+				Mathf.Clamp(offsetX + Input.mousePosition.x,
+					windowWidth * widthScale / 2f,
+					Screen.width - windowWidth * widthScale / 2f),
+				Mathf.Clamp(offsetY + Input.mousePosition.y,
+					windowHeight * heightScale / 2f,
+					Screen.height - windowHeight * heightScale / 2f));
 		}
 	}
