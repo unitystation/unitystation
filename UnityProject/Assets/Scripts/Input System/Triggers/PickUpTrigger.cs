c04c257d279c4659b69c4057feaf780b5933cf32
foma <fomalsd@users.noreply.github.com>
2019-03-13 17:33:24 +0300

    Experimental Long Arms perk, nudge barely out-of-reach items towards you

--- UnityProject/Assets/Scripts/Input System/Triggers/PickUpTrigger.cs
+++ UnityProject/Assets/Scripts/Input System/Triggers/PickUpTrigger.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.Networking;
+using Random = UnityEngine.Random;
 
 
 public class PickUpTrigger : InputTrigger
@@ -100,7 +102,26 @@ public class PickUpTrigger : InputTrigger
 
 		if (cnt.IsFloatingServer ? !CanReachFloating(ps, state) : !ps.IsInReach(cnt.RegisterTile))
 		{
-			Logger.LogWarning($"Not in reach! server pos:{state.WorldPosition} player pos:{originator.transform.position} (floating={cnt.IsFloatingServer})", Category.Security);
+			//Long arms perk
+			if ( !cnt.IsFloatingServer && CanReachFloating(ps, state) )
+			{
+				var worldPosition = cnt.RegisterTile.WorldPosition;
+				cnt.Nudge( new NudgeInfo
+				{
+					OriginPos = worldPosition - ((Vector3)ps.WorldPos-worldPosition)/ Random.Range( 10, 31 ),
+					TargetPos = worldPosition,
+					SpinMode = SpinMode.Clockwise,
+					SpinMultiplier = 15,
+					InitialSpeed = 2
+				} );
+				Logger.LogTraceFormat( "Nudging! server pos:{0} player pos:{1}", Category.Security,
+					state.WorldPosition, originator.transform.position);
+			}
+			else
+			{
+				Logger.LogTraceFormat( "Not in reach! server pos:{0} player pos:{1} (floating={2})", Category.Security,
+					state.WorldPosition, originator.transform.position, cnt.IsFloatingServer);
+			}
 			return false;
 		}
 		Logger.LogTraceFormat("Pickup success! server pos:{0} player pos:{1} (floating={2})", Category.Security,
@@ -116,7 +137,7 @@ public class PickUpTrigger : InputTrigger
 	/// </summary>
 	private static bool CanReachFloating(PlayerScript ps, TransformState state)
 	{
-		return ps.IsInReach(state.WorldPosition) || ps.IsInReach(state.WorldPosition - (Vector3)state.Impulse, 2f);
+		return ps.IsInReach(state.WorldPosition) || ps.IsInReach(state.WorldPosition - (Vector3)state.Impulse, 1.75f);
 	}
 
 	/// <summary>
