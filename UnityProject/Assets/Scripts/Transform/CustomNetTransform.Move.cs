9309e8ce7ee3397c9840f6398533a5efd1fdebcc
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2019-03-14 22:09:40 +0000

    Revert "Revert "Better clicks""

--- UnityProject/Assets/Scripts/Transform/CustomNetTransform.Move.cs
+++ UnityProject/Assets/Scripts/Transform/CustomNetTransform.Move.cs
@@ -12,44 +12,6 @@ public enum SpinMode {
 	CounterClockwise
 }
 
-public struct ThrowInfo
-{
-	/// Null object, means that there's no throw in progress
-	public static readonly ThrowInfo NoThrow =
-		new ThrowInfo{ OriginPos = TransformState.HiddenPos, TargetPos = TransformState.HiddenPos };
-	public Vector3 OriginPos;
-	public Vector3 TargetPos;
-	public GameObject ThrownBy;
-	public BodyPartType Aim;
-	public float InitialSpeed;
-	public SpinMode SpinMode;
-	public Vector3 Trajectory => TargetPos - OriginPos;
-
-	public override string ToString() {
-		return Equals(NoThrow) ? "[No throw]" :
-			$"[{nameof( OriginPos )}: {OriginPos}, {nameof( TargetPos )}: {TargetPos}, {nameof( ThrownBy )}: {ThrownBy}, " +
-			$"{nameof( Aim )}: {Aim}, {nameof( InitialSpeed )}: {InitialSpeed}, {nameof( SpinMode )}: {SpinMode}]";
-	}
-
-	public bool Equals( ThrowInfo other ) {
-		return OriginPos.Equals( other.OriginPos ) && TargetPos.Equals( other.TargetPos );
-	}
-
-	public override bool Equals( object obj ) {
-		if ( ReferenceEquals( null, obj ) ) {
-			return false;
-		}
-
-		return obj is ThrowInfo && Equals( ( ThrowInfo ) obj );
-	}
-
-	public override int GetHashCode() {
-		unchecked {
-			return ( OriginPos.GetHashCode() * 397 ) ^ TargetPos.GetHashCode();
-		}
-	}
-}
-
 public partial class CustomNetTransform {
 	private PushPull pushPull;
 	public PushPull PushPull => pushPull ? pushPull : ( pushPull = GetComponent<PushPull>() );
@@ -294,6 +256,29 @@ public partial class CustomNetTransform {
 		}
 		serverState.ActiveThrow = correctedInfo;
 //		Logger.Log( $"Throw:{correctedInfo} {serverState}" );
+		NotifyPlayers();
+	}
+
+	/// <summary>
+	/// Experimental.
+	/// Nudge object (sliding on the ground, not in the air)
+	/// </summary>
+	[Server]
+	public void Nudge( NudgeInfo info ) {
+		Vector2 impulse = info.Trajectory.normalized;
+
+		serverState.Speed = info.InitialSpeed;
+
+		serverState.Impulse = impulse;
+		if ( info.SpinMode != SpinMode.None ) {
+			if ( info.SpinMultiplier <= 0 )
+			{
+				info.SpinMultiplier = 1;
+			}
+			serverState.SpinFactor = ( sbyte ) ( Mathf.Clamp( info.InitialSpeed * info.SpinMultiplier, sbyte.MinValue, sbyte.MaxValue )
+			                                     * ( info.SpinMode == SpinMode.Clockwise ? 1 : -1 ) );
+		}
+		Logger.LogTraceFormat( "Nudge:{0} {1}", Category.Transform, info, serverState);
 		NotifyPlayers();
 	}
 
