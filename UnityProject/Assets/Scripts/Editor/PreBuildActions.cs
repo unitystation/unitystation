75b674ff532783ff2e253a7bc251b1f78416ed0f
Doobly <rob@smashingpixels.com.au>
2019-03-14 17:42:51 +1000

    Added bash script to create json format of last 10 git commits

--- UnityProject/Assets/Scripts/Editor/PreBuildActions.cs
+++ UnityProject/Assets/Scripts/Editor/PreBuildActions.cs
@@ -17,23 +17,22 @@ class PreBuildActions : IPreprocessBuildWithReport
 
     public void OnPreprocessBuild(BuildReport report)
     {
-        SetPlayerSettingsTitle();
+        //Any actions that need to be preformed before a build starts, goes here
     }
 
-    //This sets the product name of the build to the title of the
-    //last git commit / merge
-    //TODO: Remove this after release of 0.4 beta
-    private async void SetPlayerSettingsTitle()
-    {
-        var process = new Process();
-        process.StartInfo.FileName = "git";
-        process.StartInfo.Arguments = "log -1 --pretty=%f";
-        process.StartInfo.UseShellExecute = false;
-        process.StartInfo.RedirectStandardOutput = true;
+    //Removed this feature as it causes too many problems in development.
+    //Leaving it as an example on how to run an external process via the editor:
+    // private async void SetPlayerSettingsTitle()
+    // {
+    //     var process = new Process();
+    //     process.StartInfo.FileName = "git";
+    //     process.StartInfo.Arguments = "log -1 --pretty=%f";
+    //     process.StartInfo.UseShellExecute = false;
+    //     process.StartInfo.RedirectStandardOutput = true;
 
-        process.Start();
-        string output = process.StandardOutput.ReadToEnd();
-        PlayerSettings.productName = "unitystation - Latest commit: " + output;
-        await process.WaitForExitAsync();
-    }
+    //     process.Start();
+    //     string output = process.StandardOutput.ReadToEnd();
+    //     PlayerSettings.productName = "unitystation - Latest commit: " + output;
+    //     await process.WaitForExitAsync();
+    // }
 }
