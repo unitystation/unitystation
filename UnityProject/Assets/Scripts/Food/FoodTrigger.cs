d0f5097c48d45f872808ab51eeec1f26e559251a
Vladimir Kincl <kincl.vlada@gmail.com>
2019-03-21 00:00:33 +0100

    Added FoodTrigger for eating food items via Activate keybinding for example

---
+++ UnityProject/Assets/Scripts/Food/FoodTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FoodTrigger : PickUpTrigger
+{
+
+    private FoodBehaviour food;
+    void Start()
+    {
+        food = GetComponent<FoodBehaviour>();
+    }
+
+    public override void UI_Interact(GameObject originator, string hand)
+    {
+        base.UI_Interact(originator, hand);
+
+        if (!isServer)
+        {
+            UIInteractMessage.Send(gameObject, UIManager.Hands.CurrentSlot.eventName);
+        }
+        else
+        {
+            food.TryEat();
+        }
+    }
+}
