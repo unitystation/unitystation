6508983105a948959c8f81a10a081c52e8a8580e
Vladimir Kincl <kincl.vlada@gmail.com>
2019-03-21 00:04:50 +0100

    Converted spaces to tabs in the code.

--- UnityProject/Assets/Scripts/Food/FoodTrigger.cs
+++ UnityProject/Assets/Scripts/Food/FoodTrigger.cs
@@ -3,23 +3,23 @@
 public class FoodTrigger : PickUpTrigger
 {
 
-    private FoodBehaviour food;
-    void Start()
-    {
-        food = GetComponent<FoodBehaviour>();
-    }
+	private FoodBehaviour food;
+	void Start()
+	{
+		food = GetComponent<FoodBehaviour>();
+	}
 
-    public override void UI_Interact(GameObject originator, string hand)
-    {
-        base.UI_Interact(originator, hand);
+	public override void UI_Interact(GameObject originator, string hand)
+	{
+		base.UI_Interact(originator, hand);
 
-        if (!isServer)
-        {
-            UIInteractMessage.Send(gameObject, UIManager.Hands.CurrentSlot.eventName);
-        }
-        else
-        {
-            food.TryEat();
-        }
-    }
+		if (!isServer)
+		{
+			UIInteractMessage.Send(gameObject, UIManager.Hands.CurrentSlot.eventName);
+		}
+		else
+		{
+			food.TryEat();
+		}
+	}
 }
