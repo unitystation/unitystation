36a1823c35ba79adbd014c8022d7ac1ee61b329b
Doobly <rob@smashingpixels.com.au>
2019-03-28 20:12:14 +1000

    Start of LogLevel editor window

--- UnityProject/Assets/Scripts/Debug/Logger.cs
+++ UnityProject/Assets/Scripts/Debug/Logger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -8,135 +9,154 @@ using UnityEngine;
 public static class Logger
 {
 	/// Default Log level
-	private static readonly Level LogLevel = Level.Info;
+	private static readonly LogLevel LogLevel = LogLevel.Info;
 
 	/// Log level overrides for categories. Default log level will be ignored for these:
-	private static readonly Dictionary<Category, Level> LogOverrides = new Dictionary<Category, Level>{
-		[Category.Unknown]  = Level.Info,
-//		[Category.Movement] = Level.Trace,
-		[Category.Health] = Level.Trace,
-		[Category.DmMetadata] = Level.Off,
-		[Category.Light2D] = Level.Off,
-		[Category.RightClick] = Level.Off,
-		[Category.PushPull] = Level.Info,
-		[Category.PlayerSprites] = Level.Error,
-		[Category.Lerp] = Level.Off,
-		[Category.Equipment] = Level.Trace,
-		[Category.Round] = Level.Info,
-		[Category.UI] = Level.Info,
-		[Category.Camera] = Level.Trace,
-		[Category.DebugConsole] = Level.Trace,
+	private static readonly Dictionary<Category, LogLevel> LogOverrides = new Dictionary<Category, LogLevel>
+	{
+		[Category.Unknown] = LogLevel.Info,
+		//		[Category.Movement] = Level.Trace,
+		[Category.Health] = LogLevel.Trace,
+		[Category.DmMetadata] = LogLevel.Off,
+		[Category.Light2D] = LogLevel.Off,
+		[Category.RightClick] = LogLevel.Off,
+		[Category.PushPull] = LogLevel.Info,
+		[Category.PlayerSprites] = LogLevel.Error,
+		[Category.Lerp] = LogLevel.Off,
+		[Category.Equipment] = LogLevel.Trace,
+		[Category.Round] = LogLevel.Info,
+		[Category.UI] = LogLevel.Info,
+		[Category.Camera] = LogLevel.Trace,
+		[Category.DebugConsole] = LogLevel.Trace,
 		//		[Category.NetUI] = Level.Trace,
 	};
-
-	private enum Level{
-		Off = -1,
-		Error = 0,
-		Warning = 1,
-		Info = 2,
-		Trace = 3
-	}
 
 	/// <inheritdoc cref="LogTrace"/>
 	/// <inheritdoc cref="LogFormat"/>
 	[StringFormatMethod("msg")]
-	public static void LogTraceFormat( string msg, Category category = Category.Unknown, params object[] args ) {
-		TryLog( msg, Level.Trace, category, args );
+	public static void LogTraceFormat(string msg, Category category = Category.Unknown, params object[] args)
+	{
+		TryLog(msg, LogLevel.Trace, category, args);
 	}
 
 	/// LogFormats won't format string if it's not getting printed, therefore perform better.
 	/// This is most useful for Trace level that is rarely enabled.
 	[StringFormatMethod("msg")]
-	public static void LogFormat( string msg, Category category = Category.Unknown, params object[] args ) {
-		TryLog( msg, Level.Info, category, args );
+	public static void LogFormat(string msg, Category category = Category.Unknown, params object[] args)
+	{
+		TryLog(msg, LogLevel.Info, category, args);
 	}
 
 	/// <inheritdoc cref="LogWarning"/>
 	/// <inheritdoc cref="LogFormat"/>
 	[StringFormatMethod("msg")]
-	public static void LogWarningFormat( string msg, Category category = Category.Unknown, params object[] args ) {
-		TryLog( msg, Level.Warning, category, args );
+	public static void LogWarningFormat(string msg, Category category = Category.Unknown, params object[] args)
+	{
+		TryLog(msg, LogLevel.Warning, category, args);
 	}
 
 	/// <inheritdoc cref="LogWarning"/>
 	/// <inheritdoc cref="LogFormat"/>
 	[StringFormatMethod("msg")]
-	public static void LogErrorFormat( string msg, Category category = Category.Unknown, params object[] args ) {
-		TryLog( msg, Level.Error, category, args );
+	public static void LogErrorFormat(string msg, Category category = Category.Unknown, params object[] args)
+	{
+		TryLog(msg, LogLevel.Error, category, args);
 	}
 
 	/// Try printing Trace level entry. Most verbose logs that should only be enabled when debugging something that is broken.
-	public static void LogTrace( string msg, Category category = Category.Unknown ){
-		TryLog( msg, Level.Trace, category );
+	public static void LogTrace(string msg, Category category = Category.Unknown)
+	{
+		TryLog(msg, LogLevel.Trace, category);
 	}
 
 	/// Try printing Info level entry.
-	public static void Log( string msg, Category category = Category.Unknown ){
-		TryLog( msg, Level.Info, category );
+	public static void Log(string msg, Category category = Category.Unknown)
+	{
+		TryLog(msg, LogLevel.Info, category);
 	}
 
 	/// Try printing Warning level entry.
-	public static void LogWarning( string msg, Category category = Category.Unknown ){
-		TryLog( msg, Level.Warning, category );
+	public static void LogWarning(string msg, Category category = Category.Unknown)
+	{
+		TryLog(msg, LogLevel.Warning, category);
 	}
 
 	/// Try printing Error level entry.
-	public static void LogError( string msg, Category category = Category.Unknown ){
-		TryLog( msg, Level.Error, category );
+	public static void LogError(string msg, Category category = Category.Unknown)
+	{
+		TryLog(msg, LogLevel.Error, category);
 	}
 
-	private static void TryLog( string message, Level messageLevel, Category category = Category.Unknown, params object[] args ){
-		Level referenceLevel = LogLevel;
-		if ( LogOverrides.ContainsKey( category ) ){
+	private static void TryLog(string message, LogLevel messageLevel, Category category = Category.Unknown, params object[] args)
+	{
+		LogLevel referenceLevel = LogLevel;
+		if (LogOverrides.ContainsKey(category))
+		{
 			referenceLevel = LogOverrides[category];
 		}
 
-		if ( referenceLevel < messageLevel ){
+		if (referenceLevel < messageLevel)
+		{
 			return;
 		}
 
 		string categoryPrefix = category == Category.Unknown ? "" : "[" + category + "] ";
 
 		string msg = categoryPrefix + message;
-		if ( args.Length > 0 ) {
-			switch ( messageLevel ) {
-				case Level.Off:
+		if (args.Length > 0)
+		{
+			switch (messageLevel)
+			{
+				case LogLevel.Off:
 					break;
-				case Level.Error:
-					Debug.LogErrorFormat( msg, args );
+				case LogLevel.Error:
+					Debug.LogErrorFormat(msg, args);
 					break;
-				case Level.Warning:
-					Debug.LogWarningFormat( msg, args );
+				case LogLevel.Warning:
+					Debug.LogWarningFormat(msg, args);
 					break;
-				case Level.Info:
-					Debug.LogFormat( msg, args );
+				case LogLevel.Info:
+					Debug.LogFormat(msg, args);
 					break;
-				case Level.Trace:
-					Debug.LogFormat( msg, args );
+				case LogLevel.Trace:
+					Debug.LogFormat(msg, args);
 					break;
 			}
-		} else {
-			switch ( messageLevel ) {
-				case Level.Off:
+		}
+		else
+		{
+			switch (messageLevel)
+			{
+				case LogLevel.Off:
 					break;
-				case Level.Error:
-					Debug.LogError( msg );
+				case LogLevel.Error:
+					Debug.LogError(msg);
 					break;
-				case Level.Warning:
-					Debug.LogWarning( msg );
+				case LogLevel.Warning:
+					Debug.LogWarning(msg);
 					break;
-				case Level.Info:
-					Debug.Log( msg );
+				case LogLevel.Info:
+					Debug.Log(msg);
 					break;
-				case Level.Trace:
-					Debug.Log( msg );
+				case LogLevel.Trace:
+					Debug.Log(msg);
 					break;
 			}
 		}
 	}
 }
 
-public enum Category {
+public enum LogLevel
+{
+	Off = -1,
+	Error = 0,
+	Warning = 1,
+	Info = 2,
+	Trace = 3
+}
+
+public enum Category
+{
 	Unknown,
 	Security,
 	Connections,
@@ -181,4 +201,18 @@ public enum Category {
 	Round,
 	DebugConsole,
 	Camera,
+}
+
+[Serializable]
+public class LoggerPreferences
+{
+	public List<LogOverridePref> logOverrides = new List<LogOverridePref>();
+}
+
+[Serializable]
+public class LogOverridePref
+{
+	public Category category;
+	public LogLevel logLevel = LogLevel.Info;
+
 }
