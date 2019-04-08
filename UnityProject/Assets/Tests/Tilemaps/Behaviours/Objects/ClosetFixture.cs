3d5b66fb0fb87bb619c3fd365a21101251acbeea
chairbender <kwhipke1@gmail.com>
2019-04-08 08:03:51 -0700

    #1715 lockers are still clickable when open

--- UnityProject/Assets/Tests/Tilemaps/Behaviours/Objects/ClosetFixture.cs
+++ UnityProject/Assets/Tests/Tilemaps/Behaviours/Objects/ClosetFixture.cs
@@ -14,6 +14,8 @@ namespace Tests.Tilemaps.Behaviours.Objects
 		    obj.AddComponent<BoxCollider2D>();
 		    obj.AddComponent<BoxCollider2D>();
 		    var closet = obj.AddComponent<RegisterCloset>();
+		    //always starts in machines layer
+		    obj.layer = LayerMask.NameToLayer("Machines");
 
 		    closet.closetType = type;
 		    closet.Passable = false;
@@ -24,14 +26,14 @@ namespace Tests.Tilemaps.Behaviours.Objects
 
 	    private static readonly object[] Cases =
 	    {
-		    new object[] {ClosetType.LOCKER, true, true, true, false},
-		    new object[] {ClosetType.LOCKER, true, false, false, true},
-		    new object[] {ClosetType.LOCKER, false, true, true, false},
-		    new object[] {ClosetType.LOCKER, false, false, false, true},
-		    new object[] {ClosetType.CRATE, true, false, true, false},
-		    new object[] {ClosetType.CRATE, false, true, true, false},
-		    new object[] {ClosetType.CRATE, true, true, true, false},
-		    new object[] {ClosetType.CRATE, false, false, true, false}
+		    new object[] {ClosetType.LOCKER, true, true, LayerMask.NameToLayer("Machines"), false},
+		    new object[] {ClosetType.LOCKER, true, false, LayerMask.NameToLayer("Items"), true},
+		    new object[] {ClosetType.LOCKER, false, true, LayerMask.NameToLayer("Machines"), false},
+		    new object[] {ClosetType.LOCKER, false, false, LayerMask.NameToLayer("Items"), true},
+		    new object[] {ClosetType.CRATE, true, false, LayerMask.NameToLayer("Machines"), false},
+		    new object[] {ClosetType.CRATE, false, true, LayerMask.NameToLayer("Machines"), false},
+		    new object[] {ClosetType.CRATE, true, true, LayerMask.NameToLayer("Machines"), false},
+		    new object[] {ClosetType.CRATE, false, false, LayerMask.NameToLayer("Machines"), false}
 	    };
 
 	    private static void AssertCollidersEnabled(RegisterCloset closet, bool shouldEnableColliders)
@@ -43,22 +45,25 @@ namespace Tests.Tilemaps.Behaviours.Objects
 	    }
 
         [TestCaseSource(nameof(Cases))]
-        public void GivenClosetOfTypeAndOpenCloseState_WhenOpenOrClose_ThenHasCorrectCollidersAndPassabilityStatus(ClosetType type, bool isInitiallyClosed, bool thenIsClosed, bool shouldEnableColliders, bool shouldBePassable)
+        public void GivenClosetOfTypeAndOpenCloseState_WhenOpenOrClose_ThenHasCorrectLayerAndPassabilityStatus(ClosetType type, bool isInitiallyClosed, bool thenIsClosed, int shouldBeInLayer, bool shouldBePassable)
         {
 	        var closet = GetRegisterCloset(type, isInitiallyClosed);
 	        if (isInitiallyClosed == thenIsClosed)
 	        {
 		        //check right away
-		        AssertCollidersEnabled(closet, shouldEnableColliders);
+		        Assert.AreEqual(closet.gameObject.layer, shouldBeInLayer);
 		        Assert.AreEqual(closet.Passable, shouldBePassable);
 	        }
 	        else
 	        {
 		        //change and check
 		        closet.IsClosed = thenIsClosed;
-		        AssertCollidersEnabled(closet, shouldEnableColliders);
+		        Assert.AreEqual(closet.gameObject.layer, shouldBeInLayer);
 		        Assert.AreEqual(closet.Passable, shouldBePassable);
 	        }
+
+	        //collider should always be one no matter what
+	        AssertCollidersEnabled(closet, true);
         }
     }
 }
