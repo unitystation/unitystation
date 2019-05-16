9f90e92c98e29bf61249b147c57033e1bacc8601
Bod9001 <Bod9001@gmail.com>
2019-05-16 11:59:21 +0100

    fixed a few things

--- UnityProject/Assets/Scripts/Electricity/PowerSupply/Power Supply categories/RadiationCollector.cs
+++ UnityProject/Assets/Scripts/Electricity/PowerSupply/Power Supply categories/RadiationCollector.cs
@@ -3,52 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class RadiationCollector : MonoBehaviour
+public class RadiationCollector : InputTrigger
 {
-	//public void PotentialDestroyed()
-	//{
-	//	if (SelfDestruct)
-	//	{
-	//		//Then you can destroy
-	//	}
-	//}
+	public ElectricalNodeControl ElectricalNodeControl;
+	[SyncVar(hook = "UpdateState")]
+	public bool isOn = false;
 
-	//public override void OnStartServerInitialise()
-	//{
-	//	ApplianceType = PowerTypeCategory.RadiationCollector;
-	//	CanConnectTo = new HashSet<PowerTypeCategory>
-	//	{
-	//			PowerTypeCategory.HighVoltageCable,
-	//	};
-		
-	//	powerSupply.InData.CanConnectTo = CanConnectTo;
-	//	powerSupply.InData.Categorytype = ApplianceType;
-	//	powerSupply.WireEndB = WireEndB;
-	//	powerSupply.WireEndA = WireEndA;
-	//	SupplyingVoltage = 760000;
-	//	InternalResistance = 76000;
-	//	//current = 20; 
-	//	PowerInputReactions PIRHigh = new PowerInputReactions(); //You need a resistance on the output just so supplies can communicate properly
-	//	PIRHigh.DirectionReaction = true;
-	//	PIRHigh.ConnectingDevice = PowerTypeCategory.HighVoltageCable;
-	//	PIRHigh.DirectionReactionA.AddResistanceCall.ResistanceAvailable = true;
-	//	PIRHigh.DirectionReactionA.YouShallNotPass = true;
-	//	PIRHigh.ResistanceReaction = true;
-	//	PIRHigh.ResistanceReactionA.Resistance.Ohms = MonitoringResistance;
-	//	powerSupply.InData.ConnectionReaction[PowerTypeCategory.HighVoltageCable] = PIRHigh;
-	//	isOn = true;
-	//	UpdateServerState(isOn);
-	//}
-  
-	//public override void StateChange(bool isOn)
-	//{
-	//	if (isOn)
-	//	{
-	//		Logger.Log("Not implemented: Sprite change of closing radiation collector.", Category.Power);
-	//	}
-	//	else
-	//	{
-	//		Logger.Log("Not implemented: Sprite change of opening radiation collector.", Category.Power);
-	//	}
-	//}
+	public override bool Interact(GameObject originator, Vector3 position, string hand)
+	{
+		if (!isServer)
+		{
+			InteractMessage.Send(gameObject, hand);
+		}
+		else
+		{
+			isOn = !isOn;
+			UpdateServerState(isOn);
+		}
+		//ConstructionInteraction(originator, position, hand);
+		return true;
+	}
+
+	public override void OnStartClient()
+	{
+		base.OnStartClient();
+		UpdateState(isOn);
+	}
+
+	public void UpdateServerState(bool _isOn)
+	{
+		if (isOn)
+		{
+			ElectricalNodeControl.TurnOnSupply();
+		}
+		else
+		{
+			ElectricalNodeControl.TurnOffSupply();
+		}
+	}
+
+	public void UpdateState(bool _isOn)
+	{
+		isOn = _isOn;
+		if (isOn)
+		{
+			
+		}
+		else
+		{
+
+		}
+	}
 }
