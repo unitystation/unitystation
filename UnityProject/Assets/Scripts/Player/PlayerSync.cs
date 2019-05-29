be3aa617486f99f402ba80c3b35fd28279cec378
foma <fomalsd@users.noreply.github.com>
2019-05-29 20:42:51 +0300

    Clueless attempt at fixing movement on headless

--- UnityProject/Assets/Scripts/Player/PlayerSync.cs
+++ UnityProject/Assets/Scripts/Player/PlayerSync.cs
@@ -94,8 +94,6 @@ public partial class PlayerSync : NetworkBehaviour, IPushable
 	/// For client code
 	public PlayerState ClientState => playerState;
 
-		private LivingHealthBehaviour healthBehaviorScript;
-
 	public PlayerMove playerMove;
 	private PlayerScript playerScript;
 	private UserControlledSprites playerSprites;
@@ -378,7 +376,6 @@ public partial class PlayerSync : NetworkBehaviour, IPushable
 		}
 		playerScript = GetComponent<PlayerScript>();
 		playerSprites = GetComponent<UserControlledSprites>();
-		healthBehaviorScript = GetComponent<LivingHealthBehaviour>();
 		registerTile = GetComponent<RegisterTile>();
 		pushPull = GetComponent<PushPull>();
 	}
