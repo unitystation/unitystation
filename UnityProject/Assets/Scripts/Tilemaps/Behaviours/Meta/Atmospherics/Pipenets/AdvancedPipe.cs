09bcb8c19b50d42470c26bf7bdc85cf75a52d1a0
Aranclanos <aranclanos@hotmail.com>
2019-06-01 11:31:55 -0300

    Fixes scrubbers, vents and connectors being a pixel off when connecting to normal pipes

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Pipenets/AdvancedPipe.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Pipenets/AdvancedPipe.cs
@@ -19,36 +19,4 @@ public class AdvancedPipe : Pipe
 
 	}
 
-	public override void CalculateSprite()
-	{
-		if (objectBehaviour.isNotPushable == false)
-		{
-			base.CalculateSprite();
-			return;
-		}
-		SetSprite(1);
-	}
-
-	public override void CalculateDirection()
-	{
-		direction = 0;
-		var rotation = transform.rotation.eulerAngles.z;
-		if (rotation >= 45 && rotation <= 135)
-		{
-			direction = Direction.EAST;
-		}
-		else if (rotation > 135 && rotation < 225)
-		{
-			direction = Direction.NORTH;
-		}
-		else if (rotation > 225 && rotation < 315)
-		{
-			direction = Direction.WEST;
-		}
-		else
-		{
-			direction = Direction.SOUTH;
-		}
-	}
-
 }
--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Pipenets/SimplePipe.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Pipenets/SimplePipe.cs
@@ -5,8 +5,28 @@ using Atmospherics;
 
 public class SimplePipe : Pipe
 {
-	public SpriteRenderer[] conectionRenderer;
 	public Sprite[] connectionSprite;
+	public SpriteRenderer[] conectionRenderer;
+
+	public override void DirectionEast()
+	{
+		direction = Direction.EAST | Direction.WEST;
+	}
+
+	public override void DirectionNorth()
+	{
+		direction = Direction.NORTH | Direction.SOUTH;
+	}
+
+	public override void DirectionWest()
+	{
+		direction = Direction.EAST | Direction.WEST;
+	}
+
+	public override void DirectionSouth()
+	{
+		direction = Direction.NORTH | Direction.SOUTH;
+	}
 
 	public override void CalculateSprite()
 	{
@@ -14,79 +34,30 @@ public class SimplePipe : Pipe
 		{
 			conectionRenderer[i].sprite = null;
 		}
-		if(objectBehaviour.isNotPushable == false)
+		base.CalculateSprite();
+		if(anchored)
 		{
-			base.CalculateSprite();
-			return;
-		}
-		for (int i = 0; i < nodes.Count; i++)
-		{
-			var pipe = nodes[i];
-			if (pipe.transform.position.y < transform.position.y)
+			for (int i = 0; i < nodes.Count; i++)
 			{
-				conectionRenderer[0].sprite = connectionSprite[0];
+				var pipe = nodes[i];
+				if (pipe.transform.position.y < transform.position.y)
+				{
+					conectionRenderer[0].sprite = connectionSprite[0];
+				}
+				else if (pipe.transform.position.y > transform.position.y)
+				{
+					conectionRenderer[1].sprite = connectionSprite[1];
+				}
+				else if (pipe.transform.position.x > transform.position.x)
+				{
+					conectionRenderer[2].sprite = connectionSprite[2];
+				}
+				else if (pipe.transform.position.x < transform.position.x)
+				{
+					conectionRenderer[3].sprite = connectionSprite[3];
+				}
 			}
-			else if (pipe.transform.position.y > transform.position.y)
-			{
-				conectionRenderer[1].sprite = connectionSprite[1];
-			}
-			else if (pipe.transform.position.x > transform.position.x)
-			{
-				conectionRenderer[2].sprite = connectionSprite[2];
-			}
-			else if (pipe.transform.position.x < transform.position.x)
-			{
-				conectionRenderer[3].sprite = connectionSprite[3];
-			}
 		}
 	}
-
-	public override void CalculateDirection()
-	{
-		direction = 0;
-		var rotation = transform.rotation.eulerAngles.z;
-		transform.rotation = Quaternion.identity;
-		if ((rotation >= 45 && rotation < 135))
-		{
-			SetSprite(3);
-			DirectionEast();
-		}
-		else if (rotation >= 135 && rotation < 225)
-		{
-			SetSprite(2);
-			DirectionNorth();
-		}
-		else if (rotation >= 225 && rotation < 315)
-		{
-			SetSprite(4);
-			DirectionWest();
-		}
-		else
-		{
-			SetSprite(1);
-			DirectionSouth();
-		}
-	}
-
-	public virtual void DirectionEast()
-	{
-		direction = Direction.EAST | Direction.WEST;
-	}
-
-	public virtual void DirectionNorth()
-	{
-		direction = Direction.NORTH | Direction.SOUTH;
-	}
-
-	public virtual void DirectionWest()
-	{
-		direction = Direction.EAST | Direction.WEST;
-	}
-
-	public virtual void DirectionSouth()
-	{
-		direction = Direction.NORTH | Direction.SOUTH;
-	}
-
 
 }
