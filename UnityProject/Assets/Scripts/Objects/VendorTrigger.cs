47648d733ce547513e4fd6e9f600e916d11b2f3a
vlandemart <vlandemart@gmail.com>
2019-06-11 12:46:40 +0300

    Vendor script and prefab
    Moved logic from VendorTrigger to GUI_Vendor. VendorTrigget is now NetworkTabTrigger.

--- UnityProject/Assets/Scripts/Objects/VendorTrigger.cs
+++ UnityProject/Assets/Scripts/Objects/VendorTrigger.cs
@@ -1,103 +1,28 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Random = UnityEngine.Random;
 
-public class VendorTrigger : InputTrigger
+public class VendorTrigger : NetworkTabTrigger
 {
-	public GameObject[] vendorcontent;
-
-	public bool allowSell = true;
-	public float cooldownTimer = 2f;
-	public int stock = 5;
-	public string interactionMessage;
-	public string deniedMessage;
-	public bool EjectObjects = false;
-	public EjectDirection EjectDirection = EjectDirection.None;
+	public List<VendorItem> VendorContent = new List<VendorItem>();
+	public GameObject Originator;
 
 	public override bool Interact(GameObject originator, Vector3 position, string hand)
 	{
-		if (!CanUse(originator, hand, position, false))
-		{
-			return false;
-		}
-		if (!isServer)
-		{
-			//ask server to perform the interaction
-			InteractMessage.Send(gameObject, position, hand);
-			return true;
-		}
-
-		if (!allowSell && deniedMessage != null && !GameData.Instance.testServer && !GameData.IsHeadlessServer)
-		{
-			UpdateChatMessage.Send(originator, ChatChannel.Examine, deniedMessage);
-		}
-		else if (allowSell)
-		{
-			allowSell = false;
-			if (!GameData.Instance.testServer && !GameData.IsHeadlessServer)
-			{
-				UpdateChatMessage.Send(originator, ChatChannel.Examine, interactionMessage);
-			}
-			ServerVendorInteraction(position);
-			StartCoroutine(VendorInputCoolDown());
-		}
-
-		return true;
+		if (CustomNetworkManager.Instance._isServer)
+			Originator = originator;
+		return base.Interact(originator, position, hand);
 	}
-
-	[Server]
-	private bool ServerVendorInteraction(Vector3 position)
-	{
-		//		Debug.Log("status" + allowSell);
-		if (vendorcontent.Length == 0)
-		{
-			return false;
-		}
-
-		int randIndex = Random.Range(0, vendorcontent.Length);
-
-		var spawnedItem = PoolManager.PoolNetworkInstantiate(vendorcontent[randIndex], transform.position, transform.parent);
-
-		//Ejecting in direction
-		if (EjectObjects && EjectDirection != EjectDirection.None)
-		{
-			Vector3 offset = Vector3.zero;
-			switch (EjectDirection)
-			{
-				case EjectDirection.Up:
-					offset = transform.rotation * Vector3.up / Random.Range(4, 12);
-					break;
-				case EjectDirection.Down:
-					offset = transform.rotation * Vector3.down / Random.Range(4, 12);
-					break;
-				case EjectDirection.Random:
-					offset = new Vector3(Random.Range(-0.15f, 0.15f), Random.Range(-0.15f, 0.15f), 0);
-					break;
-			}
-			spawnedItem.GetComponent<CustomNetTransform>()?.Throw(new ThrowInfo
-			{
-				ThrownBy = gameObject,
-				Aim = BodyPartType.Chest,
-				OriginPos = transform.position,
-				TargetPos = transform.position + offset,
-				SpinMode = EjectDirection == EjectDirection.Random ? SpinMode.Clockwise : SpinMode.None
-			});
-		}
-		stock--;
-
-		return true;
-	}
-
-	private IEnumerator VendorInputCoolDown()
-	{
-		yield return WaitFor.Seconds(cooldownTimer);
-		if (stock > 0)
-		{
-			allowSell = true;
-		}
-	}
-
 }
 
 public enum EjectDirection { None, Up, Down, Random }
+
+//Adding this as a separate class so we can easily extend it in future -
+//add price or required access, stock amount and etc.
+[System.Serializable]
+public class VendorItem
+{
+	public GameObject item;
+}
---
+++ UnityProject/Assets/Scripts/UI/GUI_Vendor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Vendor : NetTab
+{
+	[SerializeField]
+	private bool allowSell = true;
+	[SerializeField]
+	private float cooldownTimer = 2f;
+	[SerializeField]
+	private string interactionMessage;
+	[SerializeField]
+	private string deniedMessage;
+	public bool EjectObjects = false;
+	[SerializeField]
+	private EjectDirection ejectDirection = EjectDirection.None;
+
+	private VendorTrigger vendor;
+	private List<VendorItem> vendorContent = new List<VendorItem>();
+	
+
+	protected override void InitServer()
+	{
+		vendor = Provider.GetComponent<VendorTrigger>();
+		vendorContent = vendor.VendorContent;
+	}
+
+	private void GenerateList()
+	{
+		
+	}
+
+	private void SpawnItem(VendorItem item)
+	{
+		if (CanSell() == false)
+			return;
+
+		var spawnedItem = PoolManager.PoolNetworkInstantiate(item.item, transform.position, transform.parent);
+
+		//Ejecting in direction
+		if (EjectObjects && ejectDirection != EjectDirection.None)
+		{
+			Vector3 offset = Vector3.zero;
+			switch (ejectDirection)
+			{
+				case EjectDirection.Up:
+					offset = transform.rotation * Vector3.up / Random.Range(4, 12);
+					break;
+				case EjectDirection.Down:
+					offset = transform.rotation * Vector3.down / Random.Range(4, 12);
+					break;
+				case EjectDirection.Random:
+					offset = new Vector3(Random.Range(-0.15f, 0.15f), Random.Range(-0.15f, 0.15f), 0);
+					break;
+			}
+			spawnedItem.GetComponent<CustomNetTransform>()?.Throw(new ThrowInfo
+			{
+				ThrownBy = gameObject,
+				Aim = BodyPartType.Chest,
+				OriginPos = transform.position,
+				TargetPos = transform.position + offset,
+				SpinMode = ejectDirection == EjectDirection.Random ? SpinMode.Clockwise : SpinMode.None
+			});
+		}
+
+		allowSell = false;
+		StartCoroutine(VendorInputCoolDown());
+	}
+
+	public bool CanSell()
+	{
+		if (!allowSell && deniedMessage != null && !GameData.Instance.testServer && !GameData.IsHeadlessServer)
+		{
+			UpdateChatMessage.Send(vendor.Originator, ChatChannel.Examine, deniedMessage);
+		}
+		else if (allowSell && !GameData.Instance.testServer && !GameData.IsHeadlessServer)
+		{
+			UpdateChatMessage.Send(vendor.Originator, ChatChannel.Examine, interactionMessage);
+			return true;
+		}
+		return false;
+	}
+
+	private IEnumerator VendorInputCoolDown()
+	{
+		yield return WaitFor.Seconds(cooldownTimer);
+		allowSell = true;
+	}
+}
