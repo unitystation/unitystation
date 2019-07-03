66a1ab0875e08fed32c22f5a6030992882f67d21
Jibby <jibbajabbafication@gmail.com>
2019-07-03 19:42:03 +0100

    feat(travis): added build deployment

--- ci/build.sh
+++ ci/build.sh
@@ -33,4 +33,11 @@ else
 fi
 
 ls -la $BUILD_PATH
-[ -n "$(ls -A $BUILD_PATH)" ] # fail job if build folder is empty
+
+# If success, zip the build, otherwise fail the job
+if [ -n "$(ls -A $BUILD_PATH)" ]; then
+  cd ./Builds/
+  zip -r "${BUILD_TARGET}.zip" "./${BUILD_TARGET}/"
+else
+  return 1
+fi
