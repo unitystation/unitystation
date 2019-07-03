6c2ca3882e8317e203bfbd4913d6c2c2ec51a84d
Jibby <jibbajabbafication@gmail.com>
2019-07-03 23:02:00 +0100

    refactor(travis): moved new scripts to Travis dir

--- Travis/docker_build.sh
+++ Travis/docker_build.sh
@@ -3,11 +3,12 @@
 set -e
 
 docker run \
+  -e BUILD_NAME \
   -e UNITY_LICENSE_CONTENT \
-  -e TEST_PLATFORM \
+  -e BUILD_TARGET \
   -e UNITY_USERNAME \
   -e UNITY_PASSWORD \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/test.sh"
+  /bin/bash -c "/project/Travis/before_script.sh && /project/Travis/build.sh"
--- Travis/docker_test.sh
+++ Travis/docker_test.sh
@@ -3,12 +3,11 @@
 set -e
 
 docker run \
-  -e BUILD_NAME \
   -e UNITY_LICENSE_CONTENT \
-  -e BUILD_TARGET \
+  -e TEST_PLATFORM \
   -e UNITY_USERNAME \
   -e UNITY_PASSWORD \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/build.sh"
+  /bin/bash -c "/project/Travis/before_script.sh && /project/Travis/test.sh"
