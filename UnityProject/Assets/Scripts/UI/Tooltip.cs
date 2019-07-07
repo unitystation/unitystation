2da9023ed6848d08bbcd700b7310184e0d699959
Ofek <ofeklavie@gmail.com>
2019-07-07 23:32:05 +0300

    Fix CR

--- UnityProject/Assets/Scripts/UI/Tooltip.cs
+++ UnityProject/Assets/Scripts/UI/Tooltip.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private static GameObject tooltipResource = null;
+    [SerializeField]
+    private GameObject tooltipTemplate = null;
     const float TOOLTIP_INTERVAL = 1.0f;
     private float enterTime = 0;
     private GameObject tooltipObject = null;
@@ -15,31 +16,29 @@ public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     void Start()
     {
-        if (tooltipResource == null) {
-            tooltipResource = Resources.Load<GameObject>("UI/GUI/Tooltip");
-        }
+        tooltipObject = Instantiate(tooltipTemplate, Vector3.zero, Quaternion.identity);
+        tooltipObject.GetComponentInChildren<Text>().text = tooltipText;
+        // While the tooltip exists, we place it under the canvas so it'll be in the top layer
+        tooltipObject.transform.SetParent(this.GetComponentInParent<Canvas>().transform);
+        tooltipObject.SetActive(false);
     }
 
     void Update()
     {
-        if (tooltipObject) {
+        if (tooltipObject.activeSelf) {
             // Move tooltip to mouse
             tooltipObject.transform.position = Input.mousePosition - new Vector3(0, 20, 0);
         } else if (enterTime != 0 && Time.realtimeSinceStartup - enterTime > TOOLTIP_INTERVAL) { 
-            // Create tooltip if needed
-            tooltipObject = Instantiate(tooltipResource, Vector3.zero, Quaternion.identity);
-            // While the tooltip exists, we place it under the canvas so it'll be in the top layer
-            tooltipObject.transform.SetParent(this.GetComponentInParent<Canvas>().transform);
+            // Move tooltip above all other layers. We do it now so new objects wont hide it.
             tooltipObject.transform.SetAsLastSibling();
-            tooltipObject.GetComponentInChildren<Text>().text = tooltipText;
+            tooltipObject.SetActive(true);
+            tooltipObject.transform.position = Input.mousePosition - new Vector3(0, 20, 0);
         }
     }
 
     public void SetText(string newText) {
         tooltipText = newText;
-        if (tooltipObject) {
-            tooltipObject.GetComponentInChildren<Text>().text = tooltipText;
-        }
+        tooltipObject.GetComponentsInChildren<Text>(false)[0].text = tooltipText;
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
@@ -49,8 +48,6 @@ public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public void OnPointerExit(PointerEventData eventData) {
         enterTime = 0;
         
-        if (tooltipObject) {
-            Destroy(tooltipObject);
-        }
+        tooltipObject.SetActive(false);
     }
 }
