9eec1d7f30f08091eaf391e38e42f23e689f0161
Doobly <rob@smashingpixels.com.au>
2019-07-18 11:42:26 +1000

    Firestore basic rules and example

--- UnityProject/Assets/Scripts/Database/ServerData.UpdateChar.cs
+++ UnityProject/Assets/Scripts/Database/ServerData.UpdateChar.cs
@@ -11,14 +11,15 @@ namespace DatabaseAPI
 		public static void UpdateCharacterProfile(CharacterSettings updateSettings, Action<string> callBack, Action<string> errorCallBack)
 		{
 			var json = JsonUtility.ToJson(updateSettings);
-			var url = URL_TryCreateChar + Instance.user.UserId + "/users";
+			var url = FirebaseRoot + "/users/doobly";
 			Instance.StartCoroutine(Instance.TryUpdateChar(url, callBack, errorCallBack));
 		}
 
 		IEnumerator TryUpdateChar(string request, Action<string> callBack, Action<string> errorCallBack)
 		{
 			UnityWebRequest r = UnityWebRequest.Get(request);
-			r.SetRequestHeader("Authorization", $"{Instance.token}");
+			r.SetRequestHeader("Authorization", $"Bearer {Instance.token}");
+	
 
 			yield return r.SendWebRequest();
 			if (r.error != null)
@@ -29,6 +30,7 @@ namespace DatabaseAPI
 			}
 			else
 			{
+				Debug.Log(r.downloadHandler.text);
 				callBack.Invoke(r.downloadHandler.text);
 
 			}
