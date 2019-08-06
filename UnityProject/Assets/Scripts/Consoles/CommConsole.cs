bb79b0764c569efc90c45d3c8f14a64f6e7af4c3
BuildTools <unconfigured@null.spigotmc.org>
2019-08-05 22:39:24 -0700

    Login Screen Pt1

--- UnityProject/Assets/Scripts/Consoles/CommConsole.cs
+++ UnityProject/Assets/Scripts/Consoles/CommConsole.cs
@@ -1,8 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CommConsole : MonoBehaviour {
+public class CommConsole : NBHandApplyInteractable
+{
+    public IDCard IdCard;
+    public CommunicationsUpdateEvent OnConsoleUpdate = new CommunicationsUpdateEvent();
 
-	//TODO CommConsole stuff (TabCommsConsole, GUI_CommsConsole, Interact handling, ID validation)
+	private void UpdateGUI()
+    {
+        OnConsoleUpdate?.Invoke();
+    }
+
+	protected override bool WillInteract(HandApply interaction, NetworkSide side)
+    {
+        if (!base.WillInteract(interaction, side))
+            return false;
+
+        //interaction only works if using an ID card on console
+        if (!Validations.HasComponent<IDCard>(interaction.HandObject))
+            return false;
+
+        return true;
+    }
+
+	protected override void ServerPerformInteraction(HandApply interaction)
+    {
+        //Put ID card inside
+        var handIDCard = interaction.HandObject.GetComponent<IDCard>();
+		if(handIDCard)
+        {
+            InsertID(handIDCard);
+        }
+		var slot = InventoryManager.GetSlotFromOriginatorHand(interaction.Performer, interaction.HandSlot.SlotName);
+        InventoryManager.UpdateInvSlot(true, "", interaction.HandObject, slot.UUID);
+        UpdateGUI();
+    }
+
+    /// <summary>
+    /// Insert some ID into console and update login details.
+    /// Will spit out currently inserted ID card.
+    /// </summary>
+    ///<param name="cardToInsert">Card you want to insert</param>
+    private void InsertID(IDCard cardToInsert)
+    {
+        if (IdCard)
+        {
+            RemoveID();
+        }
+        IdCard = cardToInsert;
+    }
+
+    /// <summary>
+    /// Spits out ID card from console and updates login details.
+    /// </summary>
+    public void RemoveID()
+    {
+        ObjectBehaviour objBeh = IdCard.GetComponentInChildren<ObjectBehaviour>();
+        Vector3Int pos = gameObject.RegisterTile().WorldPosition;
+        CustomNetTransform netTransform = objBeh.GetComponent<CustomNetTransform>();
+        netTransform.AppearAtPosition(pos);
+        netTransform.AppearAtPositionServer(pos);
+        IdCard = null;
+    }
 }
+
+public class CommunicationsUpdateEvent : UnityEvent { }
---
+++ UnityProject/Assets/Scripts/UI/Communications/GUICommunications.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GUICommunications : NetTab
+{
+	[SerializeField]
+	private NetPageSwitcher nestedSwitcher;
+	[SerializeField]
+	private NetLabel idText;
+	private CommConsole console;
+
+	public override void OnEnable()
+	{
+		base.OnEnable();
+		if (CustomNetworkManager.Instance._isServer) 
+		{
+			StartCoroutine(WaitForProvider());
+		}
+	}
+
+	IEnumerator WaitForProvider() 
+	{
+		while (Provider == null) 
+		{
+			yield return WaitFor.EndOfFrame;
+		}
+
+		console = Provider.GetComponentInChildren<CommConsole>();
+		console.OnConsoleUpdate.AddListener (UpdateScreen);
+		UpdateScreen();
+	}
+
+	public void UpdateScreen()
+	{
+		UpdateIdText(idText);
+	}
+
+	public void RemoveId()
+	{
+		if (console.IdCard)
+		{
+			console.RemoveID();
+			UpdateScreen();
+		}
+	}
+
+	public void UpdateIdText(NetLabel labelToSet)
+	{
+		var IdCard = console.IdCard;
+		if (IdCard)
+		{
+			labelToSet.SetValue = $"{IdCard.RegisteredName}, {IdCard.GetJobType.ToString()}";
+		}
+		else
+		{
+			labelToSet.SetValue = "********";
+		}
+	}
+
+	public void LogIn()
+	{
+		if (console.IdCard == null || !console.IdCard.accessSyncList.Contains((int) Access.security)) 
+		{
+			return;
+		}
+	}
+
+	public void LogOut()
+	{
+		nestedSwitcher.SetActivePage(nestedSwitcher.DefaultPage);
+		UpdateIdText(idText);
+	}
+
+	public void CloseTab()
+	{
+		ControlTabs.CloseTab(Type, Provider);
+	}
+}
