84411ef02efb5adcb0b4452806e7af3e401e8b7f
Doobly <rob@smashingpixels.com.au>
2019-08-10 14:25:03 +1000

    Added PlayerChatBubble component

---
+++ UnityProject/Assets/Scripts/UI/PlayerChatBubble/PlayerChatBubble.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles the ChatIcon and PlayerChatBubble. 
+/// Automatically checks PlayerPrefs to determine 
+/// the use of each one.
+/// </summary>
+public class PlayerChatBubble : MonoBehaviour
+{
+    /// <summary>
+    /// The const string of the PlayerPref key for ChatBubble preference.
+    /// Use PlayerPrefs.GetInt(chatBubblePref) to determine the players
+    /// preference for showing the chat bubble or not.
+    /// 0 = false
+    /// 1 = true
+    /// </summary>
+    [HideInInspector]
+    public const string chatBubblePref = "ChatBubble";
+
+    [SerializeField]
+    private ChatIcon chatIcon;
+
+    //TODO
+    //Save pref to playerprefs
+    //Determine localPlayer for each event
+    //Showchat icon when chat bubble is disabled
+
+    public void ShowLocalMessage(string message)
+    {
+
+    }
+
+    /// <summary>
+    /// Show the ChatBubble or the ChatIcon
+    /// </summary>
+    private bool ShowChatBubble()
+    {
+        if (!PlayerPrefs.HasKey(chatBubblePref))
+        {
+            PlayerPrefs.SetInt(chatBubblePref, 0);
+            PlayerPrefs.Save();
+        }
+
+        return PlayerPrefs.GetInt(chatBubblePref) == 1;
+    }
+}
