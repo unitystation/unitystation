fd29edff2ec45caff8a639518787aa96452c1fea
Doobly <rob@smashingpixels.com.au>
2019-08-11 12:26:05 +1000

    Added ChatBubble msg queue

--- UnityProject/Assets/Scripts/UI/PlayerChatBubble/PlayerChatBubble.cs
+++ UnityProject/Assets/Scripts/UI/PlayerChatBubble/PlayerChatBubble.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Handles the ChatIcon and PlayerChatBubble. 
@@ -19,6 +22,19 @@ public class PlayerChatBubble : MonoBehaviour
 
     [SerializeField]
     private ChatIcon chatIcon;
+    [SerializeField]
+    private GameObject chatBubble;
+    [SerializeField]
+    private Text bubbleText;
+    class BubbleMsg { public float maxTime; public string msg; public float elapsedTime = 0f; }
+    private Queue<BubbleMsg> msgQueue = new Queue<BubbleMsg>();
+    private bool showingDialogue = false;
+
+    void Start()
+    {
+        chatBubble.SetActive(false);
+        bubbleText.text = "";
+    }
 
     public void DetermineChatVisual(bool toggle, string message, ChatChannel chatChannel)
     {
@@ -28,8 +44,87 @@ public class PlayerChatBubble : MonoBehaviour
         }
         else
         {
-            
+            AddChatBubbleMsg(message, chatChannel);
         }
+    }
+
+    private void AddChatBubbleMsg(string msg, ChatChannel channel)
+    {
+        if (msg.Length > 82)
+        {
+            while (msg.Length > 82)
+            {
+                int ws = -1;
+                //Searching for the nearest whitespace
+                for (int i = 82; i >= 0; i--)
+                {
+                    if (char.IsWhiteSpace(msg[i]))
+                    {
+                        ws = i;
+                        break;
+                    }
+                }
+                //Player is spamming with no whitespace. Cut it up at index 82
+                if (ws == -1 || ws == 0) ws = 82;
+
+                var split = msg.Substring(0, ws - 1);
+                msgQueue.Enqueue(new BubbleMsg { maxTime = TimeToShow(split.Length), msg = split });
+
+                msg = msg.Substring(ws + 1);
+                if (msg.Length <= 82)
+                {
+                    msgQueue.Enqueue(new BubbleMsg { maxTime = TimeToShow(msg.Length), msg = msg });
+                }
+            }
+        }
+        else
+        {
+            msgQueue.Enqueue(new BubbleMsg { maxTime = TimeToShow(msg.Length), msg = msg });
+        }
+
+        if (!showingDialogue) StartCoroutine(ShowDialogue());
+    }
+
+    IEnumerator ShowDialogue()
+    {
+        showingDialogue = true;
+        if (msgQueue.Count == 0)
+        {
+            yield return WaitFor.EndOfFrame;
+            yield break;
+        }
+        var b = msgQueue.Dequeue();
+        chatBubble.SetActive(true);
+        bubbleText.text = b.msg;
+        while (showingDialogue)
+        {
+            yield return WaitFor.EndOfFrame;
+            b.elapsedTime += Time.deltaTime;
+            if (b.elapsedTime >= b.maxTime)
+            {
+                if (msgQueue.Count == 0)
+                {
+                    bubbleText.text = "";
+                    chatBubble.SetActive(false);
+                    showingDialogue = false;
+                }
+                else
+                {
+                    b = msgQueue.Dequeue();
+                    bubbleText.text = b.msg;
+                }
+            }
+        }
+
+        yield return WaitFor.EndOfFrame;
+    }
+
+    /// <summary>
+    /// Used to calculate showing length time
+    /// </summary>
+    private float TimeToShow(int charCount)
+    {
+        return Mathf.Clamp((float) charCount / 20f, 1f, 10f);
     }
 
     /// <summary>
