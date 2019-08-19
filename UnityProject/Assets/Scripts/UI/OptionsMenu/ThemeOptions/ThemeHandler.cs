ffe55cdda5c5928ed9cc29378e203673394840dd
Doobly <rob@smashingpixels.com.au>
2019-08-19 13:11:00 +1000

    Added YamlDotNet and finished the majority of the Theme system

---
+++ UnityProject/Assets/Scripts/UI/OptionsMenu/ThemeOptions/ThemeHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unitystation.Options
+{
+    /// <summary>
+    /// The ThemeHandler allows UI elements
+    /// to be customized by a user defined theme 
+    /// configuration. Add your own types to the ThemeType 
+    /// list if they are not available
+    /// </summary>
+    public class ThemeHandler : MonoBehaviour
+    {
+        [Tooltip("Add your own type if needed")]
+        public ThemeType themeType;
+        [Tooltip("Is this a Text, Image or something else?" 
+        + " Add a ThemeHandler for each UIElement you want to"
+        + " customize")]
+        public UIElement targetElement;
+        [Tooltip("Indentifies the element via the key"
+        + " found in the yaml theme config file")]
+        public string configKey;
+        public Image image;
+        public Text text;
+
+        void OnEnable()
+        {
+            Register();
+        }
+
+        void OnDisable()
+        {
+            if (ThemeManager.Instance != null)
+            {
+                ThemeManager.UnregisterHandler(this);
+            }
+        }
+
+        IEnumerator Register()
+        {
+            while (ThemeManager.Instance == null)
+            {
+                yield return WaitFor.EndOfFrame;
+            }
+            yield return WaitFor.EndOfFrame;
+            ThemeManager.RegisterHandler(this);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/UI/OptionsMenu/ThemeOptions/ThemeManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unitystation.Options
+{
+    /// <summary>
+    /// Handles everything to do with Themes
+    /// Add a ThemeHandler to your customizable UI elements
+    /// </summary>
+    public class ThemeManager : MonoBehaviour
+    {
+        private static ThemeManager themeManager;
+        public static ThemeManager Instance
+        {
+            get
+            {
+                if (themeManager == null)
+                {
+                    themeManager = FindObjectOfType<ThemeManager>();
+                }
+                return themeManager;
+            }
+        }
+
+        void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+        }
+
+        public static void RegisterHandler(ThemeHandler handler)
+        {
+
+        }
+
+        public static void UnregisterHandler(ThemeHandler handler)
+        {
+
+        }
+    }
+
+    public enum UIElement
+    {
+        Text,
+        Image 
+        //Add new elements that can be customized here
+        //Then you need to open ThemeHandlerEditor.cs
+        //and add the inspector gui for it (follow examples already there)
+    }
+}
