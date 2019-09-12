eefdfa4ae2023f62781b087a411fe5479c8e57db
Bod9001 <Bod9001@gmail.com>
2019-09-12 16:24:03 +0100

    Fixes and changes

--- UnityProject/Assets/Scripts/Construction/Glass/glasssheet.cs
+++ UnityProject/Assets/Scripts/Construction/Glass/glasssheet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlassSheet : MonoBehaviour
+public class glassSheet : MonoBehaviour
 {
 
 }
--- UnityProject/Assets/Scripts/Editor/Objects/ObjectBehaviourEditor.cs
+++ UnityProject/Assets/Scripts/Editor/Objects/ObjectBehaviourEditor.cs
@@ -8,18 +8,19 @@ public class ObjectBehaviourEditor : Editor
 		ObjectBehaviour oTarget = (ObjectBehaviour) target;
 		serializedObject.Update();
 		SerializedProperty isNotPushable = serializedObject.FindProperty("isNotPushable");
-		SerializedProperty renderersToIgnore = serializedObject.FindProperty("ignoredSpriteRenderers");
+		//SerializedProperty renderersToIgnore = serializedObject.FindProperty("ignoredSpriteRenderers"); //#####
 		SerializedProperty registerTile = serializedObject.FindProperty("registerTile");
-		SerializedProperty visibleState = serializedObject.FindProperty("visibleState");
+		//SerializedProperty visibleState = serializedObject.FindProperty("visibleState"); //####
 		SerializedProperty networkChannel = serializedObject.FindProperty("networkChannel");
 		SerializedProperty networkSendInterval = serializedObject.FindProperty("networkSendInterval");
+		
 		EditorGUI.BeginChangeCheck();
 
 		EditorGUILayout.LabelField("For ignoring renderers from VisibleBehaviour updates:");
-		EditorGUILayout.PropertyField(renderersToIgnore,true);
-		EditorGUILayout.PropertyField(registerTile, false);
+		//EditorGUILayout.PropertyField(renderersToIgnore,true); //#####
+		EditorGUILayout.PropertyField(registerTile, false); 
 		EditorGUILayout.LabelField("PushPull properties:");
-		EditorGUILayout.PropertyField(visibleState, false);
+		//EditorGUILayout.PropertyField(visibleState, false); //#####
 		EditorGUILayout.PropertyField(isNotPushable, false);
 
 		if (EditorGUI.EndChangeCheck())
