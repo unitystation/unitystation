97a03125f5bb0f359c10bb622ae66c7bb39d4a12
Doobly <rob@smashingpixels.com.au>
2019-09-17 13:40:04 +1000

    Attempt02

---
+++ UnityProject/Assets/Mirror/Migration/Editor/EditorConfig/IndentStyle.cs
@@ -0,0 +1,11 @@
+namespace EditorConfig.Core
+{
+	/// <summary>
+	/// set to tab or space to use hard tabs or soft tabs respectively.
+	/// </summary>
+	public enum IndentStyle
+	{
+		Tab,
+		Space
+	}
+}
