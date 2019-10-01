88fffda8fa224ee2f475937a3e457cf032893007
Doobly <rob@smashingpixels.com.au>
2019-10-01 10:33:16 +1000

    Mapping initial observations and actions

--- UnityProject/Assets/Scripts/NPC/AI/NpcMoveAgent.cs
+++ UnityProject/Assets/Scripts/NPC/AI/NpcMoveAgent.cs
@@ -13,6 +13,7 @@ public class NpcMoveAgent : Agent, IOnStageServer
 {
 	public Transform testTarget;
 	public CustomNetTransform cnt;
+	public RegisterObject registerObj;
 	private bool performingDecision = false;
 	private bool alive = false;
 
@@ -33,13 +34,63 @@ public class NpcMoveAgent : Agent, IOnStageServer
 
 	public override void CollectObservations()
 	{
+		//Observe the direction to target
+		AddVectorObs(((Vector2) (testTarget.transform.position - transform.position)).normalized);
+
+		var curPos = registerObj.LocalPositionServer;
+
+		//Observe adjacent tiles
+		for (int y = 1; y > -2; y--)
+		{
+			for (int x = -1; x < 2; x++)
+			{
+				if (x == 0 && x == 0) continue;
+				var checkPos = curPos;
+				checkPos.x += x;
+				checkPos.y += y;
+				var passable = registerObj.Matrix.IsPassableAt(checkPos, true);
+
+				//Record the passable observation:
+				AddVectorObs(passable);
+
+				if (!passable)
+				{
+					//Is the path blocked because of a door?
+					//Feed observations to AI
+					DoorController tryGetDoor;
+					if (registerObj.Matrix.TryGetComponent(out tryGetDoor))
+					{
+						AddVectorObs(true);
+					}
+					else
+					{
+						AddVectorObs(false);
+					}
+				}
+			}
+		}
+
 		base.CollectObservations();
 	}
 
 	public override void AgentAction(float[] vectorAction, string textAction)
 	{
 		base.AgentAction(vectorAction, textAction);
-		performingDecision = false;
+
+		PerformMoveAction(Mathf.FloorToInt(vectorAction[0]));
+	}
+
+	void PerformMoveAction(int act)
+	{
+		//0 = no move (1 - 8 are directions to move in reading from left to right)
+		if (act == 0)
+		{
+			performingDecision = false;
+		}
+		else
+		{
+			
+		}
 	}
 
 	void UpdateMe()
