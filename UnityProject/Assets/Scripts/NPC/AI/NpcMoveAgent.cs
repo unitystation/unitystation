ab83a61a39565acad0ee893c0afa3cb22b3fb067
Doobly <rob@smashingpixels.com.au>
2019-10-01 12:37:23 +1000

    First training session and environment test

--- UnityProject/Assets/Scripts/NPC/AI/NpcMoveAgent.cs
+++ UnityProject/Assets/Scripts/NPC/AI/NpcMoveAgent.cs
@@ -9,31 +9,42 @@ using MLAgents;
 /// but keep the observations the same or you will need
 /// to train a separate brain
 /// </summary>
-public class NpcMoveAgent : Agent, IOnStageServer
+public class NpcMoveAgent : Agent
 {
 	public Transform testTarget;
 	public CustomNetTransform cnt;
 	public RegisterObject registerObj;
-	private bool performingDecision = false;
-	private bool alive = false;
+	public bool performingDecision = false;
+	public bool move = false;
+	private float rewardCache;
+	public int rewardDay = 7;
+	private int decisionCount = 0;
+	private float distanceCache = 0;
 
-	public void GoingOnStageServer(OnStageInfo info)
+	void Awake()
 	{
-		alive = true;
+		agentParameters.onDemandDecision = true;
 	}
 
-	void OnEnable()
+	public override void OnEnable()
 	{
+		base.OnEnable();
+		cnt.OnTileReached().AddListener(OnTileReached);
 		UpdateManager.Instance.Add(UpdateMe);
+		move = true;
 	}
 
-	void OnDisable()
+	public override void OnDisable()
 	{
+		base.OnDisable();
+		cnt.OnTileReached().RemoveListener(OnTileReached);
 		UpdateManager.Instance.Remove(UpdateMe);
 	}
 
 	public override void CollectObservations()
 	{
+		distanceCache = Vector2.Distance(testTarget.transform.position, transform.position);
+
 		//Observe the direction to target
 		AddVectorObs(((Vector2) (testTarget.transform.position - transform.position)).normalized);
 
@@ -44,7 +55,8 @@ public class NpcMoveAgent : Agent, IOnStageServer
 		{
 			for (int x = -1; x < 2; x++)
 			{
-				if (x == 0 && x == 0) continue;
+				if (x == 0 && y == 0) continue;
+
 				var checkPos = curPos;
 				checkPos.x += x;
 				checkPos.y += y;
@@ -60,23 +72,19 @@ public class NpcMoveAgent : Agent, IOnStageServer
 					DoorController tryGetDoor;
 					if (registerObj.Matrix.TryGetComponent(out tryGetDoor))
 					{
-						AddVectorObs(true);
+						//	AddVectorObs(true);
 					}
 					else
 					{
-						AddVectorObs(false);
+						//	AddVectorObs(false);
 					}
 				}
 			}
 		}
-
-		base.CollectObservations();
 	}
 
 	public override void AgentAction(float[] vectorAction, string textAction)
 	{
-		base.AgentAction(vectorAction, textAction);
-
 		PerformMoveAction(Mathf.FloorToInt(vectorAction[0]));
 	}
 
@@ -89,7 +97,62 @@ public class NpcMoveAgent : Agent, IOnStageServer
 		}
 		else
 		{
-			
+			Vector2Int dirToMove = Vector2Int.zero;
+			int count = 1;
+			for (int y = 1; y > -2; y--)
+			{
+				for (int x = -1; x < 2; x++)
+				{
+					if (count == act)
+					{
+						dirToMove.x += x;
+						dirToMove.y += y;
+						y = -100;
+						break;
+					}
+					else
+					{
+						count++;
+					}
+				}
+			}
+
+			if (!cnt.Push(dirToMove))
+			{
+				//Path is blocked try again
+				performingDecision = false;
+			}
+		}
+
+		decisionCount++;
+		MonitorRewards();
+	}
+
+	void OnTileReached(Vector3Int tilePos)
+	{
+		var compareDist = Vector2.Distance(testTarget.transform.position, transform.position);
+
+		if (compareDist < distanceCache)
+		{
+			rewardCache += 0.1f;
+		}
+
+		if (compareDist < 0.5f)
+		{
+			Done();
+			move = false;
+		}
+
+		if (performingDecision) performingDecision = false;
+	}
+
+	void MonitorRewards()
+	{
+		if (decisionCount >= rewardDay)
+		{
+			decisionCount = 0;
+			SetReward(rewardCache);
+			rewardCache = 0;
 		}
 	}
 
@@ -100,8 +163,7 @@ public class NpcMoveAgent : Agent, IOnStageServer
 
 	void MonitorDecisionMaking()
 	{
-		if (!alive || performingDecision) return;
-
+		if (!move || performingDecision) return;
 		performingDecision = true;
 		RequestDecision();
 	}
