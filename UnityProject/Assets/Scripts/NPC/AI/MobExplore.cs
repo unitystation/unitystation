1c1c9ee8dbe4f417d01f5fbb668b5c92c9b2bc3a
Doobly <rob@smashingpixels.com.au>
2019-10-03 16:16:11 +1000

    Setting up MobExplore

--- UnityProject/Assets/Scripts/NPC/AI/MobExplore.cs
+++ UnityProject/Assets/Scripts/NPC/AI/MobExplore.cs
@@ -1,13 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using MLAgents;
+﻿using UnityEngine;
 
 /// <summary>
 /// AI brain specifically trained to explore
 /// the surrounding area for specific objects
 /// </summary>
-public class MobExplore : Agent
+public class MobExplore : MobAgent
 {
-	public Component componentToFind;
+	//Add your targets as needed
+	public enum Target{ food }
+	public Target target;
+
+	public bool performingDecision;
+	public bool exploring;
+
+	protected override void AgentServerStart()
+	{
+		exploring = true;
+	}
+
+	public override void CollectObservations()
+	{
+
+	}
+
+	public override void AgentAction(float[] vectorAction, string textAction)
+	{
+
+	}
+
+	protected override void OnTileReached(Vector3Int tilePos)
+	{
+		if (performingDecision) performingDecision = false;
+	}
+
+	protected override void UpdateMe()
+	{
+		MonitorDecisionMaking();
+	}
+
+	void MonitorDecisionMaking()
+	{
+		if (!exploring || performingDecision) return;
+		performingDecision = true;
+		RequestDecision();
+	}
 }
