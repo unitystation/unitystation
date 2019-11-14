ce0b442c89029fb25b8ee3a410234cf020cb4549
Doobly <rob@smashingpixels.com.au>
2019-11-14 18:16:52 +1000

    progress IV

--- UnityProject/Assets/Scripts/UI/Lobby/AccountLogin.cs
+++ UnityProject/Assets/Scripts/UI/Lobby/AccountLogin.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using DatabaseAPI;
 using Firebase;
 using Firebase.Auth;
+using Firebase.Extensions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -31,82 +32,90 @@ namespace Lobby
 		public async void TryLogin(Action<string> successAction, Action<string> errorAction)
 		{
 			FirebaseUser user;
-			try
+			await FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(userNameInput.text,
+				passwordInput.text).ContinueWithOnMainThread(async task =>
 			{
-				user = await FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(userNameInput.text,
-						passwordInput.text);
-			}
-			catch (FirebaseException e)
-			{
-				Logger.LogError($"Sign in error: {e.Message}", Category.DatabaseAPI);
-				errorAction?.Invoke(e.Message);
-				passwordInput.text = "";
-				return;
-			}
-
-			await user.ReloadAsync();
-
-			if (!user.IsEmailVerified)
-			{
-				errorAction?.Invoke(" Email Not Verified");
-				passwordInput.text = "";
-				return;
-			}
-
-			HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, ServerData.UserFirestoreURL);
-			req.Headers.Add("Authorization", $"Bearer {ServerData.IdToken}");
-
-			CancellationToken cancellationToken = new CancellationTokenSource(120000).Token;
-
-			HttpResponseMessage response;
-			try
-			{
-				response = await ServerData.HttpClient.SendAsync(req, cancellationToken);
-			}
-			catch (Exception e)
-			{
-				Logger.LogError($"Error Accessing Firestore: {e.Message}", Category.DatabaseAPI);
-				return;
-			}
-
-			string content = await response.Content.ReadAsStringAsync();
-			Debug.Log(content);
-			FireStoreResponse fr = JsonUtility.FromJson<FireStoreResponse>(content);
-
-			var newChar = "";
-			if (fr.fields.character == null)
-			{
-				var newCharacter = new CharacterSettings();
-				newCharacter.Name = StringManager.GetRandomMaleName();
-				newCharacter.username = user.DisplayName;
-				newChar = JsonUtility.ToJson(newCharacter);
-				var updateSuccess = await ServerData.UpdateCharacterProfile(newChar);
-
-				if (!updateSuccess)
+				if (task.IsCanceled)
 				{
-					Logger.LogError($"Error when updating character", Category.DatabaseAPI);
-					errorAction?.Invoke("Error when updating character");
+					Logger.LogError($"Sign in error: {task.Exception.Message}", Category.DatabaseAPI);
+					errorAction?.Invoke(task.Exception.Message);
+					passwordInput.text = "";
 					return;
 				}
-			}
 
-			if (string.IsNullOrEmpty(newChar))
-			{
-				var characterSettings =
-					JsonUtility.FromJson<CharacterSettings>(Regex.Unescape(fr.fields.character.stringValue));
-				PlayerPrefs.SetString("currentcharacter", fr.fields.character.stringValue);
-				PlayerManager.CurrentCharacterSettings = characterSettings;
-			}
-			else
-			{
-				Debug.Log("NEW CHAR: " + newChar);
-				PlayerManager.CurrentCharacterSettings = JsonUtility.FromJson<CharacterSettings>(newChar);
-			}
+				if (task.IsFaulted)
+				{
+					Logger.LogError($"Sign in error: {task.Exception.Message}", Category.DatabaseAPI);
+					errorAction?.Invoke(task.Exception.Message);
+					passwordInput.text = "";
+					return;
+				}
 
-			successAction.Invoke("Login success");
-			PlayerPrefs.SetString("lastLogin", userNameInput.text);
-			PlayerPrefs.Save();
-			passwordInput.text = "";
+				user = task.Result;
+
+				await user.ReloadAsync();
+
+				if (!user.IsEmailVerified)
+				{
+					errorAction?.Invoke(" Email Not Verified");
+					passwordInput.text = "";
+					return;
+				}
+
+				HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, ServerData.UserFirestoreURL);
+				req.Headers.Add("Authorization", $"Bearer {ServerData.IdToken}");
+
+				CancellationToken cancellationToken = new CancellationTokenSource(120000).Token;
+
+				HttpResponseMessage response;
+				try
+				{
+					response = await ServerData.HttpClient.SendAsync(req, cancellationToken);
+				}
+				catch (Exception e)
+				{
+					Logger.LogError($"Error Accessing Firestore: {e.Message}", Category.DatabaseAPI);
+					return;
+				}
+
+				string content = await response.Content.ReadAsStringAsync();
+				Debug.Log(content);
+				FireStoreResponse fr = JsonUtility.FromJson<FireStoreResponse>(content);
+
+				var newChar = "";
+				if (fr.fields.character == null)
+				{
+					var newCharacter = new CharacterSettings();
+					newCharacter.Name = StringManager.GetRandomMaleName();
+					newCharacter.username = user.DisplayName;
+					newChar = JsonUtility.ToJson(newCharacter);
+					var updateSuccess = await ServerData.UpdateCharacterProfile(newChar);
+
+					if (!updateSuccess)
+					{
+						Logger.LogError($"Error when updating character", Category.DatabaseAPI);
+						errorAction?.Invoke("Error when updating character");
+						return;
+					}
+				}
+
+				if (string.IsNullOrEmpty(newChar))
+				{
+					var characterSettings =
+						JsonUtility.FromJson<CharacterSettings>(Regex.Unescape(fr.fields.character.stringValue));
+					PlayerPrefs.SetString("currentcharacter", fr.fields.character.stringValue);
+					PlayerManager.CurrentCharacterSettings = characterSettings;
+				}
+				else
+				{
+					PlayerManager.CurrentCharacterSettings = JsonUtility.FromJson<CharacterSettings>(newChar);
+				}
+
+				successAction.Invoke("Login success");
+				PlayerPrefs.SetString("lastLogin", userNameInput.text);
+				PlayerPrefs.Save();
+				passwordInput.text = "";
+			});
 		}
 
 		public bool ValidLogin()
