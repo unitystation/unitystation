16544b7d98303674b652517cdca5ff72e64f7b6c
Alex Evgrashin <aevgrashin@yandex.ru>
2019-11-14 23:09:32 +0300

    Added basice effect shape

---
+++ UnityProject/Assets/Scripts/EffectShape/EffectShape.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectShapeType
+{
+	Square, // radius is equal in all directions from center []
+	Diamond, // classic SS13 diagonals are reduced and angled <>
+	Bomberman, // plus +
+	Circle // Diamond without tip
+}
+
+public abstract class EffectShape : IEnumerable<Vector3Int>
+{
+	public abstract IEnumerator<Vector3Int> GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+
+	public static EffectShape CreateEffectShape(EffectShapeType type, Vector3Int center, int radius)
+	{
+		switch (type)
+		{
+			case EffectShapeType.Square:
+				return new SquareEffectShape(center, radius);
+		}
+
+		return null;
+	}
+}
