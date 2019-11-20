b81de2fcc478f3fc85f4aa1f6bdbaccf995513ae
foma <fomalsd@users.noreply.github.com>
2019-11-20 20:45:30 +0300

    Mop work #1

--- UnityProject/Assets/Scripts/Tool Construction Deconstruction/Mop.cs
+++ UnityProject/Assets/Scripts/Tool Construction Deconstruction/Mop.cs
@@ -1,24 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Main component for Mop. Allows mopping to be done on tiles.
 /// </summary>
 [RequireComponent(typeof(Pickupable))]
+[RequireComponent(typeof(ReagentContainer))]
 public class Mop : MonoBehaviour, ICheckedInteractable<PositionalHandApply>
 {
+	private ReagentContainer reagentContainer;
+
+	private void Awake()
+	{
+		if (!reagentContainer)
+		{
+			reagentContainer = GetComponent<ReagentContainer>();
+		}
+	}
+
+
 	public bool WillInteract(PositionalHandApply interaction, NetworkSide side)
 	{
 		if (!DefaultWillInteract.Default(interaction, side)) return false;
 		//can only mop tiles
 		if (!Validations.HasComponent<InteractableTiles>(interaction.TargetObject)) return false;
+
+		//don't attempt to mop walls
+		if (MatrixManager.IsWallAt(interaction.WorldPositionTarget.RoundToInt(), isServer: side == NetworkSide.Server))
+		{
+			return false;
+		}
+
 		return true;
 	}
 
 	public void ServerPerformInteraction(PositionalHandApply interaction)
 	{
+		if (reagentContainer.IsEmpty) //regs < 1
+		{	//warning
+			Chat.AddExamineMsg(interaction.Performer, "Your mop is dry!");
+			return;
+		}
 		//server is performing server-side logic for the interaction
 		//do the mopping
-		var progressFinishAction = new ProgressCompleteAction(() => CleanTile(interaction.WorldPositionTarget));
+		var progressFinishAction = new ProgressCompleteAction(() =>
+		{
+			CleanTile(interaction.WorldPositionTarget);
+			Chat.AddExamineMsg(interaction.Performer, "You finish mopping.");
+		});
+
+		//todo clean what exactly
+		Chat.AddActionMsgToChat(interaction.Performer, $"You begin to clean the floor with {gameObject.ExpensiveName()}...",
+			$"{interaction.Performer.ExpensiveName()} begins to clean the floor with {gameObject.ExpensiveName()}.");
 		//Start the progress bar:
 		UIManager.ServerStartProgress(ProgressAction.Mop, interaction.WorldPositionTarget.RoundToInt(),
 			5f, progressFinishAction, interaction.Performer);
