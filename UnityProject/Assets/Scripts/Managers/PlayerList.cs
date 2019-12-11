5422f278adcb1f52e1ebc218deae6b307a444899
chairbender <kwhipke1@gmail.com>
2019-12-11 09:11:29 -0800

    fix issue where client ID was not synced between client / server, causing client to not reenter their body. Also forces client to reenter their body if there is one.

--- UnityProject/Assets/Scripts/Managers/PlayerList.cs
+++ UnityProject/Assets/Scripts/Managers/PlayerList.cs
@@ -151,32 +151,40 @@ public class PlayerList : NetworkBehaviour
 			return list;
 		});
 
+	/// <summary>
+	/// Adds this connected player to the list, or updates an existing entry if there's already one for
+	/// this player's networkconnection. Returns the ConnectedPlayer that was added or updated.
+	/// </summary>
+	/// <param name="player"></param>
 	[Server]
-	public void Add(ConnectedPlayer player)
+	public ConnectedPlayer AddOrUpdate(ConnectedPlayer player)
 	{
 		if ( player.Equals(ConnectedPlayer.Invalid) )
 		{
 			Logger.Log("Refused to add invalid connected player to this server's player list", Category.Connections);
-			return;
+			return player;
 		}
 		if ( ContainsConnection(player.Connection) )
 		{
 //			Logger.Log($"Updating {Get(player.Connection)} with {player}");
 
 			ConnectedPlayer existingPlayer = Get(player.Connection);
-			Logger.LogFormat("ConnectedPlayer {0} already exists in this server's PlayerList as {1}. Will update GameObject," +
-				" Name, and Job for the existing player instead of adding this new connected player.", Category.Connections, player, existingPlayer);
+			Logger.LogFormat("ConnectedPlayer {0} already exists in this server's PlayerList as {1}. Will update existing player instead of adding this new connected player.", Category.Connections, player, existingPlayer);
 			//TODO: Are we sure these are the only things that need to be updated?
 			existingPlayer.GameObject = player.GameObject;
 			existingPlayer.Name = player.Name; //Note that name won't be changed to empties/nulls
 			existingPlayer.Job = player.Job;
+			existingPlayer.ClientId = player.ClientId;
+			CheckRcon();
+			return existingPlayer;
 		}
 		else
 		{
 			values.Add(player);
 			Logger.LogFormat("Added to this server's PlayerList {0}. Total:{1}; {2}", Category.Connections, player, values.Count, string.Join(";", values));
+			CheckRcon();
+			return player;
 		}
-		CheckRcon();
 	}
 
 	[Server]
@@ -274,8 +282,10 @@ public class PlayerList : NetworkBehaviour
 	[Server]
 	public GameObject TakeLoggedOffPlayer(string clientId)
 	{
+		Logger.LogTraceFormat("Searching for logged off players with id {0}", Category.Connections, clientId);
 		foreach (var player in loggedOff)
 		{
+			Logger.LogTraceFormat("Found logged off player with id {0}", Category.Connections, player.ClientId);
 			if (player.ClientId == clientId)
 			{
 				loggedOff.Remove(player);
