80bb5a67e62c044f64b1e46663db1d97dadaf0e1
chairbender <kwhipke1@gmail.com>
2019-12-12 07:57:02 -0800

    lag script improvements

--- UnityProject/lag.sh
+++ UnityProject/lag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PING=${1:300}
+PING=${1:-300}
 PORT=7777
 
 sudo pfctl -E
@@ -12,9 +12,9 @@ echo "Setting ping to ${PING} on port ${PORT} for tcp and udp traffic..."
 
 # Define `customRule` to pipe traffic to `pipe 1`.
 # Note this is the actual port definition, not a textual comment
-echo "dummynet in quick proto { tcp udp } from any to any port 7777 pipe 1" | sudo pfctl -a customRule -f -
+echo "dummynet in quick proto { tcp udp } from any to any port ${PORT} pipe 1" | sudo pfctl -a customRule -f -
 
 # Define what `pipe 1` should do to traffic
-sudo dnctl pipe 1 config delay 300
+sudo dnctl pipe 1 config delay ${PING}
 
 echo "Done. Call stoplag.sh to undo these changes."
