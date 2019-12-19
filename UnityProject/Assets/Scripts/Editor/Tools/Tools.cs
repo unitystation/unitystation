5d39e6383aa19802e475a37a30b1d67ec1e914f0
chairbender <kwhipke1@gmail.com>
2019-12-18 20:19:28 -0800

    Add wallprotrusions, add it to appropriate prefabs, add directional components to the wall protrusion prefabs, add scripts for performing migrations

--- UnityProject/Assets/Scripts/Editor/Tools/Tools.cs
+++ UnityProject/Assets/Scripts/Editor/Tools/Tools.cs
@@ -85,6 +85,48 @@ public class Tools : Editor
 			}
 		}
 
+		//this is just for migrating from old way of setting wall protrusion directions to the new way
+		[MenuItem("Tools/Set WallProtrusion Directionals from Transforms")]
+		private static void FixWallProtrusionDirectionals()
+		{
+			foreach (GameObject gameObject in SceneManager.GetActiveScene().GetRootGameObjects())
+			{
+				foreach (var wallProtrusion in gameObject.GetComponentsInChildren<WallProtrusion>())
+				{
+					var directional = wallProtrusion.GetComponent<Directional>();
+					var directionalSO = new SerializedObject(directional);
+					var initialD = directionalSO.FindProperty("InitialDirection");
+
+					Vector3 facing = -wallProtrusion.transform.up;
+					var initialOrientation = Orientation.From(facing);
+					initialD.enumValueIndex = (int) initialOrientation.AsEnum();
+					directionalSO.ApplyModifiedPropertiesWithoutUndo();
+				}
+			}
+		}
+
+		/// <summary>
+		/// With the new way mapping works, now you should never have a situation where you've
+		/// mapped something with a transform rotation, they should always have a local rotation
+		/// of 0,0,0, and directional / rotation logic must be set using components.
+		///
+		/// This is a script for making sure that's the case
+		/// </summary>
+		[MenuItem("Tools/Set All Object Local Rotations to Upright")]
+		private static void SetAllObjectLocalRotationsUpright()
+		{
+			foreach (GameObject gameObject in SceneManager.GetActiveScene().GetRootGameObjects())
+			{
+				foreach (var registerTile in gameObject.GetComponentsInChildren<RegisterTile>())
+				{
+					var transform = new SerializedObject(registerTile.transform);
+					var localRotation = transform.FindProperty("m_LocalRotation");
+					localRotation.quaternionValue = Quaternion.identity;
+					transform.ApplyModifiedPropertiesWithoutUndo();
+				}
+			}
+		}
+
 		//this is for fixing some prefabs that have duplicate Meleeable components
 		[MenuItem("Prefabs/Remove Duplicate Meleeable")]
 		private static void RemoveDuplicateMeleeable()
