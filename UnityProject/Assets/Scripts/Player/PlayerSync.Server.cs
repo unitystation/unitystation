bdbc50517f92ee4373f33529f875f7008911d01b
Bod9001 <Bod9001@gmail.com>
2019-12-29 23:55:23 +0000

    Fixes Web edit formatting

--- UnityProject/Assets/Scripts/Player/PlayerSync.Server.cs
+++ UnityProject/Assets/Scripts/Player/PlayerSync.Server.cs
@@ -522,11 +522,11 @@ public partial class PlayerSync
 		PlayerState nextState = NextState(state, action, true);
 
 		nextState.Speed = SpeedServer;
-    if (!playerScript.IsGhost)
+		if (!playerScript.IsGhost)
 		{
-      playerScript.OnTileReached().Invoke(nextState.WorldPosition);
-      SoundManager.FootstepAtPosition(nextState.WorldPosition);
-    }
+			playerScript.OnTileReached().Invoke(nextState.WorldPosition);
+			SoundManager.FootstepAtPosition(nextState.WorldPosition);
+		}
 
 		return nextState;
 	}
