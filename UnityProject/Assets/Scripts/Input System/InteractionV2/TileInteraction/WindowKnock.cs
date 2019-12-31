e84dc64ffcf446277496f5fb89413276b1e9382c
Bod9001 <Bod9001@gmail.com>
2019-12-31 00:01:14 +0000

    window knocking done the right way

---
+++ UnityProject/Assets/Scripts/Input System/InteractionV2/TileInteraction/WindowKnock.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// Interaction logic for table tiles. Simply places the item on the table.
+/// </summary>
+[CreateAssetMenu(fileName = "InteractionWindowKnock", menuName = "Interaction/TileInteraction/InteractionWindowKnock")]
+public class InteractionWindowKnock : TileInteraction
+{
+	public override bool WillInteract(TileApply interaction, NetworkSide side)
+	{
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+		if (interaction.Intent == Intent.Harm) return false;
+		return interaction.HandObject == null;
+	}
+
+	public override void ServerPerformInteraction(TileApply interaction)
+	{
+		//place item
+		SoundManager.GlassknockAtPosition(interaction.WorldPositionTarget);
+	}
+}
