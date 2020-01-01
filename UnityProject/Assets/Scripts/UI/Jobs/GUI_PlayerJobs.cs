efc5e4b34b560acd02d7a46cead1ef486ec9ab38
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2020-01-01 21:00:01 +0000

    fix(joblist): job selection counters work now
    Refactors job list to only update when a ConnectedPlayer changes.
    Removes unused ForceJobListUpdateMessage.
    Removes some unneeded GetComponents from the UI code too.

--- UnityProject/Assets/Scripts/UI/Jobs/GUI_PlayerJobs.cs
+++ UnityProject/Assets/Scripts/UI/Jobs/GUI_PlayerJobs.cs
@@ -6,33 +6,19 @@ using UnityEngine.UI;
 public class GUI_PlayerJobs : MonoBehaviour
 {
 	public GameObject buttonPrefab;
-	public bool hasPickedAJob;
-
-	public bool isUpToDate;
 	private CustomNetworkManager networkManager;
 	public GameObject screen_Jobs;
-	public Text title;
-	
-	private void Update()
-	{
-		//We only want the job selection screen to show up once
-		//And only when we've received the connectedPlayers list from the server
-		if (CanBeUpdated())
-		{
-			UpdateJobsList();
-		}
-	}
 
 	private void BtnOk(JobType preference)
 	{
 		SoundManager.Play("Click01");
 		PlayerManager.LocalViewerScript.CmdRequestJob(preference, PlayerManager.CurrentCharacterSettings);
-		UIManager.Instance.GetComponent<ControlDisplays>().jobSelectWindow.SetActive(false);
-		hasPickedAJob = true;
 		SoundManager.SongTracker.Stop();
+		// Close this window
+		gameObject.SetActive(false);
 	}
 
-	private void UpdateJobsList()
+	public void UpdateJobsList()
 	{
 		screen_Jobs.SetActive(false);
 
@@ -41,9 +27,7 @@ public class GUI_PlayerJobs : MonoBehaviour
 			Destroy(child.gameObject);
 		}
 
-		var occupations = OccupationList.Instance.Occupations.ToList();
-
-		foreach (Occupation occupation in occupations)
+		foreach (Occupation occupation in OccupationList.Instance.Occupations)
 		{
 			JobType jobType = occupation.JobType;
 
@@ -81,23 +65,5 @@ public class GUI_PlayerJobs : MonoBehaviour
 			occupationGO.SetActive(true);
 		}
 		screen_Jobs.SetActive(true);
-		isUpToDate = true;
-	}
-
-	private bool CanBeUpdated()
-	{
-		if (isUpToDate || hasPickedAJob)
-		{
-			return false;
-		}
-
-//		//nameList is a syncvar with instant sync on join, while connectedPlayers only happens after player has been spawned
-//		//We should not show job selection if we haven't received all the connectedPlayers GOs
-//		if (PlayerList.Instance.nameList.Count != PlayerList.Instance.PlayerCount)
-//		{
-//			return false;
-//		}
-
-		return true;
 	}
 }
