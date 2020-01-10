aa639f080c7483230a9f79871f18892bf2c8fa07
Doobly <rob@smashingpixels.com.au>
2020-01-10 13:41:11 +1000

    Adds handcuff overlays to player prefab

---
+++ UnityProject/Assets/Scripts/Clothing/RestraintOverlay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the overlays for the handcuff sprites
+/// </summary>
+public class RestraintOverlay : ClothingItem
+{
+	//TODO Different colored overlays for different restraints
+	[SerializeField]
+	private List<Sprite> handCuffOverlays = new List<Sprite>();
+
+	[SerializeField] private SpriteRenderer spriteRend;
+
+	public override void SetReference(GameObject Item)
+	{
+		GameObjectReference = Item;
+		if (Item == null)
+		{
+			spriteRend.sprite = null;
+		}
+		else
+		{
+			spriteRend.sprite = handCuffOverlays[referenceOffset];
+		}
+	}
+
+	public override void UpdateSprite()
+	{
+		if (GameObjectReference != null)
+		{
+			spriteRend.sprite = handCuffOverlays[referenceOffset];
+		}
+	}
+}
