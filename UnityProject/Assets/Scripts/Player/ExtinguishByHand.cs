777f87171a11b69b5caab0036fe9acb8180358e1
Daniel Tauritis <tauritis@uni-bremen.de>
2020-02-02 15:16:10 +0100

    Autoformatted ExtinguishByHand.

--- UnityProject/Assets/Scripts/Player/ExtinguishByHand.cs
+++ UnityProject/Assets/Scripts/Player/ExtinguishByHand.cs
@@ -14,10 +14,10 @@ public class ExtinguishByHand : MonoBehaviour, IClientInteractable<PositionalHan
 		// Is the target in range for a pat? Is the performer's intent set to help?
 		// Is the performer's hand empty? Is the performer not stunned/downed? Is the performer conscious to perform the interaction?
 		if (!PlayerManager.LocalPlayerScript.IsInReach(interaction.WorldPositionTarget, false) ||
-		    UIManager.CurrentIntent != Intent.Help ||
-		    interaction.HandObject != null ||
-		    performerPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS ||
-		    performerRegisterPlayer.IsLayingDown)
+			UIManager.CurrentIntent != Intent.Help ||
+			interaction.HandObject != null ||
+			performerPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS ||
+			performerRegisterPlayer.IsLayingDown)
 		{
 			return false;
 		}
