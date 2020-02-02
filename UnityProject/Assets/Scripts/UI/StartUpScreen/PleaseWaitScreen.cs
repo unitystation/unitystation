7a8937315aa972cc20a43848270b030917ddd5c1
Doobly <rob@smashingpixels.com.au>
2020-02-02 13:55:42 +1000

    Adds Start up screen with basic Please Wait message

---
+++ UnityProject/Assets/Scripts/UI/StartUpScreen/PleaseWaitScreen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class PleaseWaitScreen : MonoBehaviour
+{
+	[SerializeField] private Image spinningImage;
+	[SerializeField] private List<Sprite> possibleSprites;
+	[SerializeField] private bool isStartScreen;
+	[SerializeField] private Vector3 rotationSpeed;
+
+	private void OnEnable()
+	{
+		if (isStartScreen)
+		{
+			StartCoroutine(WaitForLoad());
+		}
+		SetRandomSpinImage();
+	}
+
+	IEnumerator WaitForLoad()
+	{
+		yield return WaitFor.EndOfFrame;
+
+		AsyncOperation AO = SceneManager.LoadSceneAsync(1);
+		AO.allowSceneActivation = false;
+		while(AO.progress < 0.9f)
+		{
+			yield return WaitFor.EndOfFrame;
+		}
+		yield return WaitFor.EndOfFrame;
+		AO.allowSceneActivation = true;
+	}
+
+	void SetRandomSpinImage()
+	{
+		var randomIndex = Random.Range(0, possibleSprites.Count);
+		spinningImage.sprite = possibleSprites[randomIndex];
+	}
+
+	void Update()
+	{
+		spinningImage.transform.Rotate(rotationSpeed * Time.deltaTime);
+	}
+}
