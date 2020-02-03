5e092a6b2a2de9c87171950708fc50820acb0f94
Bod9001 <Bod9001@gmail.com>
2020-02-03 21:06:11 +0000

    Requested changes have been done

--- UnityProject/Assets/Scripts/Tool/Multitool.cs
+++ UnityProject/Assets/Scripts/Tool/Multitool.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Multitool : MonoBehaviour, ICheckedInteractable<PositionalHandApply>
 {
 
@@ -10,6 +12,7 @@ public class Multitool : MonoBehaviour, ICheckedInteractable<PositionalHandApply
 	public bool WillInteract(PositionalHandApply interaction, NetworkSide side)
 	{
 		// Use default interaction checks
+		if (!Validations.HasTarget(interaction)) return false;
 		if (!DefaultWillInteract.Default(interaction, side)) return false;
 		if (!Validations.IsTarget(gameObject, interaction))
 		{
@@ -34,12 +37,17 @@ public class Multitool : MonoBehaviour, ICheckedInteractable<PositionalHandApply
 			if (PoweredDevice != null)
 			{
 				if (APCBuffer != null)
-				{					PoweredDevice.SetAPC(APCBuffer);
+				{
+					Chat.AddExamineMsgToClient("You set the power device to use the APC in the buffer");					PoweredDevice.SetAPC(APCBuffer);
+				}
+				else { 
+					Chat.AddExamineMsgToClient("Your buffer is empty fill it with something");
 				}
 			}
 			APC _APC = interaction.TargetObject.GetComponent<APC>();
 			if (_APC != null)
 			{
+				Chat.AddExamineMsgToClient("You set the internal buffer of the multitool to the APC");
 				APCBuffer = _APC;
 			}
 		}
