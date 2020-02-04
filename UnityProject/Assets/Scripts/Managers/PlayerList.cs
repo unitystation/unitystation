b4c692e26ad2fe2fd25562b2f8366fcdbde01716
Doobly <rob@smashingpixels.com.au>
2020-02-04 14:00:36 +1000

    prevent nuke ops names from appearing in playerlist display

--- UnityProject/Assets/Scripts/Managers/PlayerList.cs
+++ UnityProject/Assets/Scripts/Managers/PlayerList.cs
@@ -119,7 +119,7 @@ public partial class PlayerList : NetworkBehaviour
 		foreach (var player in ClientConnectedPlayers)
 		{
 			if (player.PendingSpawn) continue;
-
+			if (player.Job == JobType.SYNDICATE) continue;
 			UIManager.Instance.playerListUIControl.nameList.text +=
 				$"{player.Name} ({player.Job.JobString()})\r\n";
 		}
