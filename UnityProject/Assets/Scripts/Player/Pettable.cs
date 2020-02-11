61b6d4a8a038da2be44e9d3e46cd183b678753c0
h3killa <36843760+h3killa@users.noreply.github.com>
2020-02-10 23:40:48 -0500

    Added Petting to NPCs
    Basically a carbon copy of Huggable

---
+++ UnityProject/Assets/Scripts/Player/Pettable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Allows an object to be hugged by a player.
+/// </summary>
+public class Pettable : MonoBehaviour, IClientInteractable<PositionalHandApply>
+{
+	public bool Interact(PositionalHandApply interaction)
+	{
+		var targetNPCHealth = interaction.TargetObject.GetComponent<LivingHealthBehaviour>();
+		var performerPlayerHealth = interaction.Performer.GetComponent<PlayerHealth>();
+		var performerRegisterPlayer = interaction.Performer.GetComponent<RegisterPlayer>();
+
+		// Is the target in range for a hug? Is the target conscious for the hug? Is the performer's intent set to help?
+		// Is the performer's hand empty? Is the performer not stunned/downed? Is the performer conscious to perform the interaction?
+		// Is the performer interacting with itself?
+		if (!PlayerManager.LocalPlayerScript.IsInReach(interaction.WorldPositionTarget, false) ||
+			targetNPCHealth.ConsciousState != ConsciousState.CONSCIOUS ||
+			UIManager.CurrentIntent != Intent.Help ||
+			interaction.HandObject != null ||
+			performerPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS ||
+			performerRegisterPlayer.IsLayingDown ||
+			interaction.Performer == interaction.TargetObject)
+		{
+			return false;
+		}
+
+		Chat.AddActionMsgToChat(interaction.Performer,
+	$"You pet {interaction.TargetObject.name}.", $"{interaction.Performer.ExpensiveName()} pets {interaction.TargetObject.name}.");
+		return true;
+	}
+}
