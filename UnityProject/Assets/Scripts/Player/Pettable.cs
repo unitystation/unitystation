cdde8defc8210b78672d1648fd8a5e09f4b91f2e
h3killa <36843760+h3killa@users.noreply.github.com>
2020-02-11 04:25:03 -0500

    Update Pettable.cs

--- UnityProject/Assets/Scripts/Player/Pettable.cs
+++ UnityProject/Assets/Scripts/Player/Pettable.cs
@@ -14,13 +14,7 @@ public class Pettable : MonoBehaviour, IClientInteractable<PositionalHandApply>
 		// Is the target in range for a pet? Is the target conscious for the pet? Is the performer's intent set to help?
 		// Is the performer's hand empty? Is the performer not stunned/downed? Is the performer conscious to perform the interaction?
 		// Is the performer interacting with itself?
-		if (!PlayerManager.LocalPlayerScript.IsInReach(interaction.WorldPositionTarget, false) ||
-			targetNPCHealth.ConsciousState != ConsciousState.CONSCIOUS ||
-			UIManager.CurrentIntent != Intent.Help ||
-			interaction.HandObject != null ||
-			performerPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS ||
-			performerRegisterPlayer.IsLayingDown ||
-			interaction.Performer == interaction.TargetObject)
+		if (!Validations.CanApply(interaction.Performer, interaction.TargetObject, NetworkSide.Server, true, ReachRange.Standard, interaction.TargetVector))
 		{
 			return false;
 		}
