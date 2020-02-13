016a1b2c4c456a3dc0618b5dd152dfc8c012fbcb
Doobly <rob@smashingpixels.com.au>
2020-02-13 16:59:38 +1000

    Adds allocation protection

--- UnityProject/Assets/Scripts/AutoMod/AutoMod.cs
+++ UnityProject/Assets/Scripts/AutoMod/AutoMod.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using Telepathy;
 
 namespace AdminTools
 {
@@ -29,6 +31,65 @@ namespace AdminTools
 		private static string AutoModConfigPath =>
 			Path.Combine(Application.streamingAssetsPath, "admin", "automodconfig.json");
 
+		private void Start()
+		{
+			LoadConfig();
+		}
+
+		private void SaveConfig()
+		{
+			if (loadedConfig == null) return;
+
+			File.WriteAllText(AutoModConfigPath, JsonUtility.ToJson(loadedConfig));
+		}
+
+		private void LoadConfig()
+		{
+			if (File.Exists(AutoModConfigPath))
+			{
+				var config = File.ReadAllText(AutoModConfigPath);
+				loadedConfig = JsonUtility.FromJson<AutoModConfig>(config);
+				Logger.Log("Successfully loaded Auto Mod config");
+			}
+		}
+
+		void Update()
+		{
+			if (!IsEnabled()) return;
+			MonitorEnvironment();
+		}
+
+		void MonitorEnvironment()
+		{
+			if (Common.allocationAttackQueue.Count > 0)
+			{
+				ProcessAllocationAttack(Common.allocationAttackQueue.Dequeue());
+			}
+		}
+
+		public static void ProcessAllocationAttack(string ipAddress)
+		{
+			if (!Instance.loadedConfig.enableAllocationProtection) return;
+			if (Application.platform == RuntimePlatform.LinuxPlayer)
+			{
+				Logger.Log($"Auto mod has taken steps to protect against an allocation attack from {ipAddress}");
+				ProcessStartInfo processInfo = new ProcessStartInfo();
+				processInfo.FileName = "/bin/bash";
+				string argument = "-c \" ufw insert 1 deny from \"{0}\" to any;";
+				processInfo.Arguments = string.Format(argument, ipAddress);
+				processInfo.WindowStyle = ProcessWindowStyle.Hidden;
+				processInfo.CreateNoWindow = true;
+				processInfo.UseShellExecute = false;
+				Process.Start(processInfo);
+			}
+		}
+
+		private static bool IsEnabled()
+		{
+			if (Instance != null || !GameData.IsHeadlessServer || Instance.loadedConfig == null) return false;
+			if (!Instance.loadedConfig.enableAutoMod) return false;
+			return true;
+		}
 	}
 
 	[Serializable]
