534f7da43e0137992c7fb0e10165db0cdcd0e90b
Doobly <rob@smashingpixels.com.au>
2020-02-13 15:03:01 +1000

    start of automod

---
+++ UnityProject/Assets/Scripts/AutoMod/AutoMod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace AdminTools
+{
+	//A serverside optional auto moderator to help make
+	//server admin work easier. Only works in headless mode
+	public class AutoMod : MonoBehaviour
+	{
+		private static AutoMod autoMod;
+
+		public static AutoMod Instance
+		{
+			get
+			{
+				if (autoMod == null)
+				{
+					autoMod = FindObjectOfType<AutoMod>();
+				}
+				return autoMod;
+			}
+		}
+
+		private AutoModConfig loadedConfig;
+
+		private static string AutoModConfigPath =>
+			Path.Combine(Application.streamingAssetsPath, "admin", "automodconfig.json");
+
+	}
+
+	[Serializable]
+	public class AutoModConfig
+	{
+		public bool enableAutoMod;
+		public bool enableAllocationProtection;
+		public bool enableSpamAutoBan;
+		public bool enableBadWordBan;
+		public bool enableRdmNotification;
+		public bool enablePlasmaReleaseNotification;
+	}
+}
