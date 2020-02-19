b3106e454ad6b8c073061ef6ae8726fcac9bb949
Redline <39059512+RedlineTriad@users.noreply.github.com>
2020-02-19 10:59:22 +0100

    Add build number.

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -3,8 +3,4 @@ if test $RCON_PASSWORD; then jq --arg v "$RCON_PASSWORD" '.RconPass = $v' config
 if test $HUB_USER     ; then jq --arg v "$HUB_USER"      '.HubUser = $v'  config.json | sponge config.json; fi
 if test $HUB_PASSWORD ; then jq --arg v "$HUB_PASSWORD"  '.HubPass = $v'  config.json | sponge config.json; fi
 
-cd /server/UnityStation_Data/StreamingAssets
-if test $BUILD_NUMBER; then jq --arg v "$BUILD_NUMBER" '.BuildNumber = $v' buildinfo.json | sponge buildinfo.json; fi
-if test $BUILD_FORK  ; then jq --arg v "$BUILD_FORK"   '.ForkName = $v'    buildinfo.json | sponge buildinfo.json; fi
-
 /server/UnityStation -batchmode -nographics -logfile /dev/stdout
