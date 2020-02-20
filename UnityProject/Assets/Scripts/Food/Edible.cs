a4f1616e41becafd6713434ba370bff0a875f016
Raptorizer <44679539+Raptorizer@users.noreply.github.com>
2020-02-20 04:17:43 -0600

    Revert "Edible.CS change"
    This reverts commit 28f4a6b8f7767cae535597d980b77c99ce156251.

--- UnityProject/Assets/Scripts/Food/Edible.cs
+++ UnityProject/Assets/Scripts/Food/Edible.cs
@@ -1,4 +1,4 @@
-﻿ quiusing System;
+﻿using System;
 using System.Collections;
 using UnityEngine;
 using Mirror;
@@ -31,10 +31,11 @@ public class Edible : NetworkBehaviour, IClientInteractable<HandActivate>, IClie
 	public void NPCTryEat()
 	{
 		SoundManager.PlayNetworkedAtPos("EatFood", transform.position);
-		if (leavings != null)
+		//Keeping this out allows food to be eaten and disappeared, change to despawn at some point.
+		/*if (leavings != null)
 		{
 			Spawn.ServerPrefab(leavings, transform.position, transform.parent);
-		}
+		}*/
 
 		GetComponent<CustomNetTransform>().DisappearFromWorldServer();
 	}
