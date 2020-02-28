fdf697fa52d187bf2ee4746204582c7e3f34ff62
Robustin <superspamspot@gmail.com>
2020-02-28 10:06:06 -0500

    Foma Suggest #1
    Co-Authored-By: Foma <fomalsd@users.noreply.github.com>

--- UnityProject/Assets/Scripts/Objects/Construction/Girder.cs
+++ UnityProject/Assets/Scripts/Objects/Construction/Girder.cs
@@ -211,7 +211,7 @@ public class Girder : NetworkBehaviour, ICheckedInteractable<HandApply>, IServer
 	{
 		GameObject theWall = Spawn.ServerPrefab(FalseWall, SpawnDestination.At(gameObject)).GameObject;
 		DoorController doorController = theWall.GetComponent<DoorController>();
-		tileChangeManager.UpdateTile(Vector3Int.RoundToInt(transform.localPosition), falseTile);
+		tileChangeManager.UpdateTile(registerObject.LocalPositionServer, falseTile);
 		interaction.HandObject.GetComponent<Stackable>().ServerConsume(2);
 		Despawn.ServerSingle(gameObject);
 		doorController.ServerTryClose();
