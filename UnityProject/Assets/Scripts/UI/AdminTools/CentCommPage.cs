fe80b36fcc9851957c0e73b8e287b2c7fec82847
Andrés Riquelme <andresfranco.rs@gmail.com>
2020-03-04 17:34:04 -0300

    add centcomm page to admin panel

---
+++ UnityProject/Assets/Scripts/UI/AdminTools/CentCommPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AdminTools
+{
+    public class CentCommPage : AdminPage
+    {
+        [SerializeField] InputFieldFocus AnnouncementText;
+
+        public void SendCentCommAnnouncement()
+        {
+            string text = AnnouncementText.text;
+            Logger.Log( nameof(SendCentCommAnnouncement), Category.NetUI );
+            CentComm.MakeAnnouncement(CentComm.CentCommAnnounceTemplate, text);
+            adminTools.ShowMainPage();
+        }
+    }
+}
+
