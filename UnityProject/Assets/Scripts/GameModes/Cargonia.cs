8664e28fb596fa685ccd75075534affb02a10406
Doobly <rob@smashingpixels.com.au>
2020-03-05 16:00:17 +1000

    Start of cargonia game mode

---
+++ UnityProject/Assets/Scripts/GameModes/Cargonia.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Antagonists;
+
+[CreateAssetMenu(menuName="ScriptableObjects/GameModes/Cargonia")]
+public class Cargonia : GameMode
+{
+	/// <summary>
+	/// Set up the station for the game mode
+	/// </summary>
+	public override void SetupRound()
+	{
+		Logger.Log("Setting up traitor round!", Category.GameMode);
+	}
+	/// <summary>
+	/// Begin the round
+	/// </summary>
+	public override void StartRound()
+	{
+		Logger.Log("Starting traitor round!", Category.GameMode);
+		base.StartRound();
+	}
+	// /// <summary>
+	// /// Check if the round should end yet
+	// /// </summary>
+	// public override void CheckEndCondition()
+	// {
+	// 	Logger.Log("Check end round conditions!", Category.GameMode);
+	// }
+
+	// /// <summary>
+	// /// End the round and display any relevant reports
+	// /// </summary>
+	// public override void EndRound()
+	// {
+
+	// }
+
+	protected override bool ShouldSpawnAntag(PlayerSpawnRequest spawnRequest)
+	{
+		return AntagManager.Instance.AntagCount == 0 && PlayerList.Instance.InGamePlayers.Count > 0;
+	}
+}
