94d55e2c729ff27c77a8b04b21811297cb02c632
ReiderJack <asetonlim@gmail.com>
2020-03-08 17:07:31 +0300

    Master Volume slider

---
+++ UnityProject/Assets/Scripts/UI/IngameMenu/SetVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SetVolume : MonoBehaviour
+{
+		public AudioMixer mixer;
+
+		public void SetLevel(float sliderValue)
+		{
+			mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+		}
+}
