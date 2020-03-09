02bcfc4bf9656d5a66ca4417b53f1e211315e964
ReiderJack <asetonlim@gmail.com>
2020-03-09 08:15:25 +0300

    AudioListener.volume

--- UnityProject/Assets/Scripts/UI/IngameMenu/SetVolume.cs
+++ UnityProject/Assets/Scripts/UI/IngameMenu/SetVolume.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using UnityEngine.Audio;
 public class SetVolume : MonoBehaviour
 {
-		public AudioMixer mixer;
-
+		
 		public void SetLevel(float sliderValue)
 		{
-			mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+			AudioListener.volume = sliderValue;
 		}
 }
