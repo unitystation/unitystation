e851a17fc18ac1f8c7de78e727e94c46f87c4ab6
ReiderJack <asetonlim@gmail.com>
2020-03-10 20:52:09 +0300

    BulletKinetic script

---
+++ UnityProject/Assets/BulletKinetic.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKinetic : BulletBehaviour
+{
+	public float maxBulletDistance;
+
+	public override void Shoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone = BodyPartType.Chest)
+	{
+		StartShoot(dir, controlledByPlayer, fromWeapon, targetZone);
+		StartCoroutine(countTiles());
+	}
+
+	private IEnumerator countTiles()
+	{
+		Vector2 startPos = gameObject.AssumedWorldPosServer();
+		//List<Vector3Int> positionList = MatrixManager.GetTiles(startPos, dir, 3);
+		float time = maxBulletDistance / weapon.ProjectileVelocity;
+		yield return WaitFor.Seconds(time);
+		ReturnToPool();
+	}
+
+	public override void HandleCollisionEnter2D(Collision2D coll)
+	{
+		GetComponent<BulletMineOnHit>()?.bulletEnter2D(coll);
+		ReturnToPool();
+	}
+
+	protected override void ReturnToPool()
+	{
+		if (trailRenderer != null)
+		{
+			trailRenderer.ShotDone();
+		}
+
+		rigidBody.velocity = Vector2.zero;
+		StartCoroutine(KineticAnim());
+
+	}
+
+	public IEnumerator KineticAnim()
+	{
+		/*Vector3 posBullet = rigidBody.gameObject.AssumedWorldPosServer();
+		Vector3 vec = posBullet;
+		vec.z = 0;
+		posBullet +=(vec * 0.2f);
+		*/
+		Transform cellTransform = rigidBody.gameObject.transform;
+		MetaTileMap layerMetaTile = cellTransform.GetComponentInParent<MetaTileMap>();
+		var position = layerMetaTile.WorldToCell(Vector3Int.RoundToInt(rigidBody.gameObject.AssumedWorldPosServer()));
+		if (position == null) Chat.AddGameWideSystemMsgToChat("position is null");
+		TileChangeManager tileChangeManager = transform.GetComponentInParent<TileChangeManager>();
+		if (tileChangeManager == null) Chat.AddGameWideSystemMsgToChat("position is null");
+		// Store the old effect for restoring after fire is gone
+		LayerTile oldEffectLayerTile = tileChangeManager.GetLayerTile(position, LayerType.Effects);
+
+		tileChangeManager.UpdateTile(position, TileType.Effects, "Kinetic");
+
+		yield return WaitFor.Seconds(.4f);
+
+		tileChangeManager.RemoveTile(position, LayerType.Effects);
+		//tileChangeManager.RemoveEffect(position, LayerType.Effects);
+		Chat.AddGameWideSystemMsgToChat("Remove was called");
+		// Restore the old effect if any (ex: cracked glass)
+		if (oldEffectLayerTile)
+			tileChangeManager.UpdateTile(position, oldEffectLayerTile);
+		Despawn.ClientSingle(gameObject);
+	}
+
+}
--- UnityProject/Assets/Scripts/Weapons/Projectiles/BulletBehaviour.cs
+++ UnityProject/Assets/Scripts/Weapons/Projectiles/BulletBehaviour.cs
@@ -16,19 +16,19 @@ public class BulletBehaviour : MonoBehaviour
 	[Range(0, 100)]
 	public float damage = 25;
 	private GameObject shooter;
-	private Gun weapon;
+	protected Gun weapon;
 	public DamageType damageType;
 	public AttackType attackType = AttackType.Bullet;
 	private bool isSuicide = false;
 	/// <summary>
 	/// Cached trailRenderer. Note that not all bullets have a trail, thus this can be null.
 	/// </summary>
-	private LocalTrailRenderer trailRenderer;
+	protected LocalTrailRenderer trailRenderer;
 
 	/// <summary>
 	/// Rigidbody on the child transform (the one that actually moves when a shot happens)
 	/// </summary>
-	private Rigidbody2D rigidBody;
+	protected Rigidbody2D rigidBody;
 	//	public BodyPartType BodyPartAim { get; private set; };
 
 	private void Awake()
@@ -65,13 +65,13 @@ public class BulletBehaviour : MonoBehaviour
 	/// <param name="controlledByPlayer"></param>
 	/// <param name="targetZone"></param>
 	/// <param name="fromWeapon">Weapon the shot is being fired from</param>
-	public void Shoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone = BodyPartType.Chest)
+	public virtual void Shoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone = BodyPartType.Chest)
 	{
 		isSuicide = false;
 		StartShoot(dir, controlledByPlayer, fromWeapon, targetZone);
 	}
 
-	private void StartShoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone)
+	protected void StartShoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone)
 	{
 		weapon = fromWeapon;
 		Direction = dir;
@@ -103,7 +103,7 @@ public class BulletBehaviour : MonoBehaviour
 	/// <summary>
 	/// Invoked when BulletColliderBehavior passes the event up to us.
 	/// </summary>
-	public void HandleCollisionEnter2D(Collision2D coll)
+	public virtual void HandleCollisionEnter2D(Collision2D coll)
 	{
 		if (coll.gameObject == shooter && !isSuicide)
 		{
@@ -159,7 +159,7 @@ public class BulletBehaviour : MonoBehaviour
 		ReturnToPool();
 	}
 
-	private void ReturnToPool()
+	protected virtual void ReturnToPool()
 	{
 		if (trailRenderer != null)
 		{
