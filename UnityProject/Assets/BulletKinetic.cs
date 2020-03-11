b83744ce38d6b97282c27104e64819e7f14dd915
ReiderJack <asetonlim@gmail.com>
2020-03-11 19:21:32 +0300

    sound and not broken animation

--- UnityProject/Assets/BulletKinetic.cs
+++ UnityProject/Assets/BulletKinetic.cs
@@ -5,6 +5,7 @@ public class BulletKinetic : BulletBehaviour
 {
 	public float maxBulletDistance;
 
+	private bool isOnDespawn = false;
 	public override void Shoot(Vector2 dir, GameObject controlledByPlayer, Gun fromWeapon, BodyPartType targetZone = BodyPartType.Chest)
 	{
 		StartShoot(dir, controlledByPlayer, fromWeapon, targetZone);
@@ -26,28 +27,80 @@ public class BulletKinetic : BulletBehaviour
 		ReturnToPool(coll);
 	}
 
-	protected override void ReturnToPool()
+	public override void HandleTriggerEnter2D(Collider2D coll)
 	{
-		if (trailRenderer != null)
+		//only harm others if it's not a suicide
+		if (coll.gameObject == shooter && !isSuicide)
 		{
-			trailRenderer.ShotDone();
+			return;
 		}
 
-		rigidBody.velocity = Vector2.zero;
-		StartCoroutine(KineticAnim());
+		//only harm the shooter if it's a suicide
+		if (coll.gameObject != shooter && isSuicide)
+		{
+			return;
+		}
+
+		//body or object?
+		var livingHealth = coll.GetComponent<LivingHealthBehaviour>();
+		var integrity = coll.GetComponent<Integrity>();
+		if (integrity != null)
+		{
+			//damage object
+			integrity.ApplyDamage(damage, attackType, damageType);
+			Chat.AddAttackMsgToChat(shooter, coll.gameObject, BodyPartType.None, weapon.gameObject);
+			Logger.LogTraceFormat("Hit {0} for {1} with HealthBehaviour! bullet absorbed", Category.Firearms, integrity.gameObject.name, damage);
+		}
+		else
+		{
+			//damage human if there is one
+			if (livingHealth == null || livingHealth.IsDead)
+			{
+				return;
+			}
+
+			// Trigger for things like stuns
+			GetComponent<BulletHitTrigger>()?.BulletHitInteract(coll.gameObject);
+
+			var aim = isSuicide ? bodyAim : bodyAim.Randomize();
+			livingHealth.ApplyDamageToBodypart(shooter, damage, attackType, damageType, aim);
+			Chat.AddAttackMsgToChat(shooter, coll.gameObject, aim, weapon.gameObject);
+			Logger.LogTraceFormat("Hit {0} for {1} with HealthBehaviour! bullet absorbed", Category.Firearms, livingHealth.gameObject.name, damage);
+		}
+
+		//ReturnToPool();
+	}
+
+	protected override void ReturnToPool()
+	{
+		if (!isOnDespawn)
+		{
+
+			isOnDespawn = true;
+			if (trailRenderer != null)
+			{
+				trailRenderer.ShotDone();
+			}
+
+			rigidBody.velocity = Vector2.zero;
+			StartCoroutine(KineticAnim());
+		}
 
 	}
 
 	protected void ReturnToPool(Collision2D coll)
 	{
-		if (trailRenderer != null)
+		if (!isOnDespawn)
 		{
-			trailRenderer.ShotDone();
+			isOnDespawn = true;
+			if (trailRenderer != null)
+			{
+				trailRenderer.ShotDone();
+			}
+
+			rigidBody.velocity = Vector2.zero;
+			StartCoroutine(KineticAnim(coll));
 		}
-
-		rigidBody.velocity = Vector2.zero;
-		StartCoroutine(KineticAnim(coll));
-
 	}
 
 	public IEnumerator KineticAnim()
@@ -63,14 +116,18 @@ public class BulletKinetic : BulletBehaviour
 		LayerTile oldEffectLayerTile = tileChangeManager.GetLayerTile(position, LayerType.Effects);
 
 		tileChangeManager.UpdateTile(position, TileType.Effects, "KineticAnimation");
+		Chat.AddGameWideSystemMsgToChat("--------------------------------");
+		Chat.AddGameWideSystemMsgToChat("UpdateTile is called. No CALL.");
 
 		yield return WaitFor.Seconds(.4f);
 
 		tileChangeManager.RemoveTile(position, LayerType.Effects);
-
+		Chat.AddGameWideSystemMsgToChat("RemoveTile is called. No COLL.");
+		Chat.AddGameWideSystemMsgToChat("--------------------------------");
 		// Restore the old effect if any (ex: cracked glass)
 		if (oldEffectLayerTile)
 			tileChangeManager.UpdateTile(position, oldEffectLayerTile);
+		isOnDespawn = false;
 		Despawn.ClientSingle(gameObject);
 	}
 
@@ -93,15 +150,20 @@ public class BulletKinetic : BulletBehaviour
 		LayerTile oldEffectLayerTile = tileChangeManager.GetLayerTile(cellPos, LayerType.Effects);
 
 		tileChangeManager.UpdateTile(cellPos, TileType.Effects, "KineticAnimation");
-
+		Chat.AddGameWideSystemMsgToChat("--------------------------------");
+		Chat.AddGameWideSystemMsgToChat("UpdateTile is called. With COLL.");
 		yield return WaitFor.Seconds(.4f);
 
 		tileChangeManager.RemoveTile(cellPos, LayerType.Effects);
 		//tileChangeManager.RemoveEffect(position, LayerType.Effects);
-		Chat.AddGameWideSystemMsgToChat("Remove was called");
+		Chat.AddGameWideSystemMsgToChat("RemoveTile is called. With COLL.");
+		Chat.AddGameWideSystemMsgToChat("--------------------------------");
 		// Restore the old effect if any (ex: cracked glass)
 		if (oldEffectLayerTile)
+		{
 			tileChangeManager.UpdateTile(cellPos, oldEffectLayerTile);
+		}
+		isOnDespawn = false;
 		Despawn.ClientSingle(gameObject);
 	}
 
