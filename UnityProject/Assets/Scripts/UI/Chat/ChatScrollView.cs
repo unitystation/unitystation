87e7b3340858c5ab3ee7949ee908417538fa530b
Doobly <rob@smashingpixels.com.au>
2020-03-12 14:25:05 +1000

    Beginning of ChatScrollRect

---
+++ UnityProject/Assets/Scripts/UI/Chat/ChatScrollView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A specific ScrollRect that also handles the
+/// Data binding and display of a collection of
+/// chat messages. Loosely based off MVVM where
+/// this would be the ViewModel
+/// </summary>
+public class ChatScrollRect : ScrollRect
+{
+
+}
+
+public class ChatEntryData
+{
+
+}
+
+public class ChatEntryView : MonoBehaviour
+{
+	[SerializeField] private Text visibleText = null;
+	private Transform tMarkerBottom;
+	private Transform tMarkerTop;
+
+	/// <summary>
+	/// The current message of the ChatEntry
+	/// </summary>
+	public string Message => visibleText.text;
+
+	public void SetChatEntryView(string message)
+	{
+		visibleText.text = message;
+		gameObject.SetActive(true);
+	}
+
+	private void OnEnable()
+	{
+		UpdateManager.Add(CallbackType.LATE_UPDATE, LateUpdateMe);
+	}
+
+	private void OnDisable()
+	{
+		UpdateManager.Remove(CallbackType.LATE_UPDATE, LateUpdateMe);
+	}
+
+	void LateUpdateMe()
+	{
+		CheckPosition();
+	}
+
+	void CheckPosition()
+	{
+
+	}
+
+	public void ReturnToPool()
+	{
+
+	}
+}
