39d617af73c9e113e4bf31eb548830a2e8f6f7ee
Doobly <rob@smashingpixels.com.au>
2020-03-12 12:55:44 +1000

    Ahelp progress

--- UnityProject/Assets/Scripts/UI/AdminTools/AdminChatButtons.cs
+++ UnityProject/Assets/Scripts/UI/AdminTools/AdminChatButtons.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AdminTools
 {
@@ -10,7 +11,13 @@ namespace AdminTools
 		[SerializeField] private GUI_Notification adminNotification = null;
 		[SerializeField] private GUI_Notification playerNotification = null;
 		[SerializeField] private GUI_Notification prayerNotification = null;
-
+		[SerializeField] private AdminChatWindows adminChatWindows = null;
+		[SerializeField] private Button adminChatButton = null;
+		[SerializeField] private Button playerChatButton = null;
+		[SerializeField] private Button prayerWindowButton = null;
+		[SerializeField] private Color selectedColor;
+		[SerializeField] private Color unSelectedColor;
+		
 		void ResetNotifications()
 		{
 			adminNotification.gameObject.SetActive(false);
@@ -21,6 +28,39 @@ namespace AdminTools
 		private void OnEnable()
 		{
 			ResetNotifications();
+			adminChatWindows.WindowChangeEvent += OnAdminChatWindowChange;
+			ToggleButtons(AdminChatWindow.None);
 		}
+
+		private void OnDisable()
+		{
+			adminChatWindows.WindowChangeEvent -= OnAdminChatWindowChange;
+		}
+
+		void OnAdminChatWindowChange(AdminChatWindow selectedWindow)
+		{
+			ToggleButtons(selectedWindow);
+		}
+
+		void ToggleButtons(AdminChatWindow selectedWindow)
+		{
+			adminChatButton.image.color = unSelectedColor;
+			playerChatButton.image.color = unSelectedColor;
+			prayerWindowButton.image.color = unSelectedColor;
+
+			switch (selectedWindow)
+			{
+				case AdminChatWindow.AdminPlayerChat:
+					playerChatButton.image.color = selectedColor;
+					break;
+				case AdminChatWindow.AdminToAdminChat:
+					adminChatButton.image.color = selectedColor;
+					break;
+				case AdminChatWindow.PrayerWindow:
+					prayerWindowButton.image.color = selectedColor;
+					break;
+			}
+		}
+
 	}
 }
