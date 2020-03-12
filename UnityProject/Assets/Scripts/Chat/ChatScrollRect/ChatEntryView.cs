7f36ecef85902afc6dc1defda28c7160cdc556fa
Doobly <rob@smashingpixels.com.au>
2020-03-12 16:02:56 +1000

    ChatScrollRect progress

---
+++ UnityProject/Assets/Scripts/Chat/ChatScrollRect/ChatEntryView.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatEntryView : MonoBehaviour
+{
+	[SerializeField] private Text visibleText = null;
+	private Transform tMarkerBottom;
+	private Transform tMarkerTop;
+	private ChatEntryData entryData;
+	private ChatScrollRect chatScrollRect;
+	private bool isResetting = false;
+
+	/// <summary>
+	/// The current message of the ChatEntry
+	/// </summary>
+	public string Message => visibleText.text;
+
+	/// <summary>
+	/// Get the currently loaded entry data
+	/// </summary>
+	public ChatEntryData EntryData => entryData;
+
+	public void SetChatEntryView(ChatEntryData data, ChatScrollRect chatScroll, Transform markerBottom,
+		Transform markerTop)
+	{
+		chatScrollRect = chatScroll;
+		tMarkerBottom = markerBottom;
+		tMarkerTop = markerTop;
+		isResetting = false;
+		visibleText.text = data.Message;
+		entryData = data;
+		gameObject.SetActive(true);
+	}
+
+	private void OnEnable()
+	{
+		UpdateManager.Add(CallbackType.LATE_UPDATE, LateUpdateMe);
+	}
+
+	private void OnDisable()
+	{
+		UpdateManager.Remove(CallbackType.LATE_UPDATE, LateUpdateMe);
+	}
+
+	void LateUpdateMe()
+	{
+		if (isResetting) return;
+
+		CheckPosition();
+	}
+
+	void CheckPosition()
+	{
+		var bottomTest = transform.position - tMarkerBottom.position;
+		if (bottomTest.y < -20f)
+		{
+			ReturnToPool(true);
+			return;
+		}
+
+		var topTest = transform.position - tMarkerTop.position;
+		if (topTest.y > 20f)
+		{
+			ReturnToPool();
+			return;
+		}
+	}
+
+	public void ReturnToPool(bool isExitBottom = false)
+	{
+		isResetting = true;
+		chatScrollRect.ReturnViewToPool(this, isExitBottom);
+	}
+}
---
+++ UnityProject/Assets/Scripts/Chat/ChatScrollRect/ChatScrollRect.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A specific ScrollRect that also handles the
+/// Data binding and display of a collection of
+/// chat messages. Loosely based off MVVM where
+/// this would be the ViewModel
+/// </summary>
+public class ChatScrollRect : ScrollRect
+{
+	[SerializeField] private Transform chatContentParent = null;
+	[SerializeField] private InputField inputField = null;
+	[SerializeField] private GameObject defaultChatEntryPrefab;
+
+	private List<ChatEntryData> chatLog = new List<ChatEntryData>();
+	private List<ChatEntryView> chatViewPool = new List<ChatEntryView>();
+	private int bottomIndex;
+	private int topIndex;
+
+	void Awake()
+	{
+		InitPool();
+	}
+
+	void InitPool()
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			chatViewPool.Add(InstantiateChatView());
+		}
+	}
+
+	ChatEntryView InstantiateChatView()
+	{
+		var obj = Instantiate(defaultChatEntryPrefab, chatContentParent);
+		obj.transform.localScale = Vector3.one;
+		obj.SetActive(false);
+		return obj.GetComponent<ChatEntryView>();
+	}
+
+	public void ReturnViewToPool(ChatEntryView view, bool isExitBottom)
+	{
+		var data = view.EntryData;
+		view.gameObject.SetActive(false);
+		var dataIndex = chatLog.IndexOf(data);
+
+		if (isExitBottom)
+		{
+			if (dataIndex == 0)
+			{
+				bottomIndex = 0;
+				return;
+			}
+		}
+		else
+		{
+			if (dataIndex == chatLog.Count - 1)
+			{
+				topIndex = chatLog.Count - 1;
+				return;
+			}
+		}
+	}
+}
