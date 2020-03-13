32757ffcffcab2afba64ff7887d69f7f643f077c
ReiderJack <asetonlim@gmail.com>
2020-03-13 19:15:15 +0300

    some progress

--- UnityProject/Assets/WallMountItemContainer.cs
+++ UnityProject/Assets/WallMountItemContainer.cs
@@ -9,6 +9,15 @@ public class WallMountItemContainer : NetworkBehaviour, ICheckedInteractable<Han
 	public ItemTrait traitRequired;
 	public GameObject appliableItem;
 
+	public Sprite[] spriteListBroken;
+	public Sprite[] spriteListEmpty;
+	public Sprite[] spriteListFull;
+	public Sprite[] spriteListLightOn;
+
+	public SpriteRenderer spriteRenderer;
+	public SpriteRenderer spriteRendererLightOn;
+
+	private Orientation orientation;
 	bool hasItem = true;
 
 	//private ItemStorage itemStorage;
@@ -16,6 +25,7 @@ public class WallMountItemContainer : NetworkBehaviour, ICheckedInteractable<Han
 
 	private void Awake()
 	{
+		orientation = GetComponent<Directional>().CurrentDirection;
 		//itemStorage = GetComponent<ItemStorage>();
 		//itemSlot = itemStorage.GetIndexedItemSlot(0);
 	}
@@ -30,8 +40,12 @@ public class WallMountItemContainer : NetworkBehaviour, ICheckedInteractable<Han
 	{
 		if (interaction.HandObject == null && hasItem)
 		{
+
 			//take out mountedItem
 			//Inventory.ServerAdd(appliableItem, interaction.HandSlot);
+			Chat.AddGameWideSystemMsgToChat(orientation.Degrees.ToString());
+			spriteRenderer.sprite = GetSprite(spriteListEmpty);
+			spriteRendererLightOn.sprite = null;
 			Spawn.ServerPrefab(appliableItem, interaction.Performer.WorldPosServer());
 			//Inventory.ServerTransfer(itemSlot, interaction.HandSlot);
 			Chat.AddExamineMsg(interaction.Performer, "You took the light tube out!");
@@ -39,10 +53,28 @@ public class WallMountItemContainer : NetworkBehaviour, ICheckedInteractable<Han
 		}
 		else if (Validations.HasItemTrait(interaction.HandObject, traitRequired) && !hasItem)
 		{
-			hasItem = true;
+			spriteRenderer.sprite = GetSprite(spriteListFull);
+			spriteRendererLightOn.sprite = GetSprite(spriteListLightOn);
 			//Inventory.ServerTransfer(interaction.HandSlot, itemSlot);
 			Despawn.ServerSingle(interaction.HandObject);
 			Chat.AddExamineMsg(interaction.Performer, "You put light tube in!");
+			hasItem = true;
+		}
+	}
+
+	private Sprite GetSprite(Sprite[] spriteList)
+	{
+		int angle = orientation.Degrees;
+		switch(angle)
+		{
+			case 0:
+				return spriteList[1];
+			case 90:
+				return spriteList[0];
+			case 180:
+				return spriteList[3];
+			default:
+				return spriteList[2];
 		}
 	}
 
