5eb1da1bf2b58a002a83b175f8c7dd1845af9008
Alex Evgrashin <aevgrashin@yandex.ru>
2020-03-14 15:48:03 +0300

    Moved electricity fx to lazy initialisations

--- UnityProject/Assets/Scripts/Electricity/Inheritance/CableInheritance.cs
+++ UnityProject/Assets/Scripts/Electricity/Inheritance/CableInheritance.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using Mirror;
+using UnityEngine.Serialization;
 
 public class CableInheritance : NetworkBehaviour, ICheckedInteractable<PositionalHandApply>, IDeviceControl
 {
@@ -15,8 +16,13 @@ public class CableInheritance : NetworkBehaviour, ICheckedInteractable<Positiona
 	public PowerTypeCategory ApplianceType;
 	public HashSet<PowerTypeCategory> CanConnectTo;
 
-	public ParticleSystem Sparks;
-	public ParticleSystem Smoke;
+	[SerializeField]
+	[FormerlySerializedAs("Sparks")]
+	private ParticleSystem sparksPrefab;
+
+	[SerializeField]
+	[FormerlySerializedAs("Sparks")]
+	private ParticleSystem smokePrefab;
 
 	public float MaximumInstantBreakCurrent;
 	public float MaximumBreakdownCurrent;
@@ -321,6 +327,28 @@ public class CableInheritance : NetworkBehaviour, ICheckedInteractable<Positiona
 		if (SR.sprite == null)
 		{
 			Logger.LogError("SetSprite: Couldn't find wire sprite, sprite value didn't return anything!", Category.Electrical);
+		}
+	}
+
+	private ParticleSystem sparks;
+	public ParticleSystem Sparks
+	{
+		get
+		{
+			if (!sparks)
+				sparks = Instantiate(sparksPrefab, transform);
+			return sparks;
+		}
+	}
+
+	private ParticleSystem smoke;
+	public ParticleSystem Smoke
+	{
+		get
+		{
+			if (!smoke)
+				smoke = Instantiate(smokePrefab, transform);
+			return smoke;
 		}
 	}
 }
