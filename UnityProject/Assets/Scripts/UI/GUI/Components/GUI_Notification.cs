f6a67ca8f6969f5fba41e685a95ed1968b38afb5
Doobly <rob@smashingpixels.com.au>
2020-03-15 18:19:21 +1000

    Notification basics

--- UnityProject/Assets/Scripts/UI/GUI/Components/GUI_Notification.cs
+++ UnityProject/Assets/Scripts/UI/GUI/Components/GUI_Notification.cs
@@ -8,5 +8,31 @@ using UnityEngine.UI;
 /// </summary>
 public class GUI_Notification : MonoBehaviour
 {
+	private Dictionary<string, int> notification = new Dictionary<string, int>();
 	[SerializeField] private Text label;
+
+	/// <summary>
+	/// Adds the amount to the count of the given key
+	/// </summary>
+	public void AddNotification(string key, int amountToAdd)
+	{
+
+	}
+
+	/// <summary>
+	/// Removes a key entry and its notification count
+	/// </summary>
+	/// <param name="key"></param>
+	public void RemoveNotification(string key)
+	{
+
+	}
+
+	/// <summary>
+	/// Removes all notification entries and resets the count to 0
+	/// </summary>
+	public void ClearAll()
+	{
+
+	}
 }
