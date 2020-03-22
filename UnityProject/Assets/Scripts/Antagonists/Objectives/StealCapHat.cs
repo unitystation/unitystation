3d00d692050362cdfad02232691ed992710d2cb1
Andrés Riquelme <andresfranco.rs@gmail.com>
2020-03-22 14:02:53 -0300

    change rebels second objective

---
+++ UnityProject/Assets/Scripts/Antagonists/Objectives/StealCapHat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Antagonists
+{
+    [CreateAssetMenu(menuName="ScriptableObjects/Objectives/StealCapHat")]
+    public class SpaceCapsHat : Objective
+    {
+        [SerializeField]
+        private List<GameObject> allowedHats;
+
+        protected override void Setup()
+        {
+
+        }
+
+        protected override bool CheckCompletion()
+        {
+            foreach (Transform t in GameManager.Instance.PrimaryEscapeShuttle.MatrixInfo.Objects.transform)
+            {
+                var player = t.GetComponent<PlayerScript>();
+                if (player != null)
+                {
+                    var inventory = player.GetComponent<ItemStorage>();
+                    var headSlot = inventory.GetNamedItemSlot(NamedSlot.head).Item;
+                    var playerDetails = PlayerList.Instance.Get(player.gameObject);
+                    if (playerDetails.Job == JobType.CAPTAIN &&
+                        (headSlot == null || !allowedHats.Contains(headSlot.gameObject)))
+                    {
+                      return true;
+                    } 
+                }
+            }
+
+            return false;
+        }
+    }
+}
