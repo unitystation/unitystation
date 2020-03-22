76772310a2f12c50ee9a19b743f2bc4bacb74ca0
ReiderJack <asetonlim@gmail.com>
2020-03-22 18:21:36 +0300

    disk/pinpointer

---
+++ UnityProject/Assets/ItemPinpointer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Atmospherics;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+
+public class ItemPinpointer : NetworkBehaviour, IInteractable<HandActivate>
+{
+
+	private GameObject objectToTrack;
+	void Awake()
+	{
+		EnsureInit();
+	}
+
+	private void EnsureInit()
+	{
+		objectToTrack = FindObjectOfType<NukeDiskScript>().gameObject;	
+	}
+
+	public void ServerPerformInteraction(HandActivate interaction)
+	{
+		Chat.AddExamineMsgFromServer(interaction.Performer,"Nuke disk:" + objectToTrack.AssumedWorldPosServer().ToString());
+		Chat.AddExamineMsgFromServer(interaction.Performer,"You:" + gameObject.AssumedWorldPosServer().ToString());
+	}
+
+
+
+
+}
---
+++ UnityProject/Assets/NukeDiskScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Atmospherics;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+public class NukeDiskScript : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
