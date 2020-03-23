12f4fde534cd5a5bacab25b8de422c0210c2aaee
Doobly <rob@smashingpixels.com.au>
2020-03-24 09:29:46 +1000

    Start of Admin Overlay

---
+++ UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AdminTools
+{
+	public class AdminOverlay : MonoBehaviour
+	{
+
+	}
+}
---
+++ UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlayPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AdminTools
+{
+	/// <summary>
+	/// Displays customized data for viewing by admins via
+	/// the Admin Overlay canvas
+	/// </summary>
+	public class AdminOverlayPanel : MonoBehaviour
+	{
+
+	}
+}
