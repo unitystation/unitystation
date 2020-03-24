5a44b2d608948d4ddb3d76da59764b955f78aa94
Doobly <rob@smashingpixels.com.au>
2020-03-24 10:47:27 +1000

    Admin overlay progress

--- UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlay.cs
+++ UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlay.cs
@@ -1,11 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace AdminTools
 {
+	/// <summary>
+	/// Controls the updating and display of the info panels
+	/// of in game items for admin use
+	/// </summary>
 	public class AdminOverlay : MonoBehaviour
 	{
+		[SerializeField] private GameObject infoPanelPrefab;
+		private List<AdminOverlayPanel> infoPanelPool = new List<AdminOverlayPanel>();
+		private List<AdminOverlayPanel> panelsInUse = new List<AdminOverlayPanel>();
 
+		private void OnEnable()
+		{
+			SceneManager.activeSceneChanged += OnSceneChange;
+		}
+
+		private void OnDisable()
+		{
+			SceneManager.activeSceneChanged -= OnSceneChange;
+		}
+
+		void OnSceneChange(Scene oldScene, Scene newScene)
+		{
+			Init();
+		}
+
+		void Init()
+		{
+			for (int i = infoPanelPool.Count - 1; i > 0; i--)
+			{
+				if (infoPanelPool[i] != null)
+				{
+					Destroy(infoPanelPool[i].gameObject);
+				}
+			}
+
+			for (int i = panelsInUse.Count - 1; i > 0; i--)
+			{
+				if (panelsInUse[i] != null)
+				{
+					Destroy(panelsInUse[i].gameObject);
+				}
+			}
+
+			infoPanelPool.Clear();
+			panelsInUse.Clear();
+
+			for (int i = 0; i < 20; i++)
+			{
+				var obj = Instantiate(infoPanelPrefab, transform);
+				infoPanelPool.Add(obj.GetComponent<AdminOverlayPanel>());
+			}
+		}
+
+		public void ReturnToPool(AdminOverlayPanel panelToReturn)
+		{
+			if (panelToReturn != null)
+			{
+				if (panelsInUse.Contains(panelToReturn))
+				{
+					panelsInUse.Remove(panelToReturn);
+				}
+
+				if (!infoPanelPool.Contains(panelToReturn))
+				{
+					infoPanelPool.Add(panelToReturn);
+				}
+
+				panelToReturn.gameObject.SetActive(false);
+			}
+		}
 	}
 }
--- UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlayPanel.cs
+++ UnityProject/Assets/Scripts/UI/AdminTools/AdminOverlayPanel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AdminTools
 {
@@ -10,6 +12,80 @@ namespace AdminTools
 	/// </summary>
 	public class AdminOverlayPanel : MonoBehaviour
 	{
+		[SerializeField] private Text displayText;
+		private ObjectBehaviour objectToFollow;
+		private AdminOverlay adminOverlay;
+		private Transform target;
+		private Vector3 followOffset;
+		private Camera cam;
 
+		/// <summary>
+		/// Set the Panel up so it can display the text and follow the target
+		/// </summary>
+		/// <param name="text"> Text to display in the info box.
+		/// Character limit for each line is 20. Anything past
+		/// this point is cut off</param>
+		/// <param name="objectToFollow">The object behaviour of the object being followed.
+		/// The object behaviour allows the checking of parent containers.</param>
+		/// <param name="followOffset">The offset in world meters from the centre of the
+		/// following target</param>
+		public void SetAdminOverlayPanel(string text, AdminOverlay adminOverlay,
+			ObjectBehaviour objectToFollow, Vector2 followOffset)
+		{
+			if (objectToFollow == null) return;
+
+			target = objectToFollow.transform;
+			cam = Camera.main;
+			displayText.text = text;
+			this.adminOverlay = adminOverlay;
+			this.objectToFollow = objectToFollow;
+			this.followOffset = followOffset;
+			gameObject.SetActive(true);
+		}
+
+		private void OnEnable()
+		{
+			UpdateManager.Add(CallbackType.LATE_UPDATE, FixedUpdateMe);
+		}
+
+		private void OnDisable()
+		{
+			UpdateManager.Remove(CallbackType.LATE_UPDATE, FixedUpdateMe);
+		}
+
+		void FixedUpdateMe()
+		{
+			if (target != null && objectToFollow != null)
+			{
+				FollowTarget();
+			}
+		}
+
+		void FollowTarget()
+		{
+			// check container:
+			if (objectToFollow.parentContainer != null)
+			{
+				if (objectToFollow.parentContainer.transform != target)
+				{
+					target = objectToFollow.parentContainer.transform;
+				}
+			}
+			else
+			{
+				if (target != objectToFollow.transform)
+				{
+					target = objectToFollow.transform;
+				}
+			}
+
+			Vector3 viewPos = cam.WorldToScreenPoint(target.position + followOffset);
+			transform.position = viewPos;
+		}
+
+		public void ReturnToPool()
+		{
+			adminOverlay.ReturnToPool();
+		}
 	}
 }
