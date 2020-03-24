9ecc8902d58dec2b730a5c86a3d2c818ee3fcd95
ReiderJack <asetonlim@gmail.com>
2020-03-25 01:33:36 +0300

    progress

--- UnityProject/Assets/ItemPinpointer.cs
+++ UnityProject/Assets/ItemPinpointer.cs
@@ -8,16 +8,25 @@ using UnityEngine.Events;
 
 public class ItemPinpointer : NetworkBehaviour, IInteractable<HandActivate>
 {
-
+	public GameObject rendererSprite;
 	private GameObject objectToTrack;
 	void Awake()
 	{
 		EnsureInit();
 	}
-
+	private void Update()
+	{
+		Vector3 moveDirection = objectToTrack.AssumedWorldPosServer() - gameObject.AssumedWorldPosServer();
+		if (moveDirection != Vector3.zero)
+		{
+			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			rendererSprite.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+	}
 	private void EnsureInit()
 	{
-		objectToTrack = FindObjectOfType<NukeDiskScript>().gameObject;	
+		objectToTrack = FindObjectOfType<NukeDiskScript>().gameObject;
+		StartCoroutine(Animation());
 	}
 
 	public void ServerPerformInteraction(HandActivate interaction)
@@ -26,7 +35,20 @@ public class ItemPinpointer : NetworkBehaviour, IInteractable<HandActivate>
 		Chat.AddExamineMsgFromServer(interaction.Performer,"You:" + gameObject.AssumedWorldPosServer().ToString());
 	}
 
+	private IEnumerator Animation()
+	{
 
+		while (true)
+		{
+			Vector3 moveDirection = gameObject.AssumedWorldPosServer() - objectToTrack.AssumedWorldPosServer();
+			if (moveDirection != Vector3.zero)
+			{
+				float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+				rendererSprite.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			}
+			yield return WaitFor.Seconds(1.0f);
+		}
+	}
 
 
 }
--- UnityProject/Assets/NukeDiskScript.cs
+++ UnityProject/Assets/NukeDiskScript.cs
@@ -5,17 +5,31 @@ using Atmospherics;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 public class NukeDiskScript : NetworkBehaviour
 {
+	public Sprite[] spriteList;
+	public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+		StartCoroutine(Animation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		
     }
+	private IEnumerator Animation()
+	{
+		int i = 0;
+		while (true)
+		{
+			if(i >= spriteList.Length) { i = 0; }
+			spriteRenderer.sprite = spriteList[i];
+			yield return WaitFor.Seconds(0.2f);
+			i++;
+		}
+	}
 }
