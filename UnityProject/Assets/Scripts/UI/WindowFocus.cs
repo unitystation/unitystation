510a28d5d45dc8254dfdb030b128f4548a707540
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2020-03-26 16:20:52 +0000

    Notes Tab

---
+++ UnityProject/Assets/Scripts/UI/WindowFocus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// use to focus InputBoxes
+/// </summary>
+public class WindowFocus : MonoBehaviour
+{
+	private InputField Searchtext;
+	private void Start()
+	{
+		Searchtext = GetComponent<InputField>();
+	}
+	void Update()
+	{
+		if (Searchtext.isFocused)
+		{
+			InputFocus();
+		}
+		else if (!Searchtext.isFocused)
+		{
+			InputUnfocus();
+		}
+	}
+
+	private void InputFocus()
+	{
+		//disable keyboard commands while input is focused
+		UIManager.IsInputFocus = true;
+	}
+	private void InputUnfocus()
+	{
+		//disable keyboard commands while input is focused
+		UIManager.IsInputFocus = false;
+	}
+}
