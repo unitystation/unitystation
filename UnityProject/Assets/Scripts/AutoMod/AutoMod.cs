d832fd364f52d438746fbfc13b2547611dd93ca3
Doobly <rob@smashingpixels.com.au>
2020-03-27 15:35:08 +1000

    Fixed spam protection bugs

--- UnityProject/Assets/Scripts/AutoMod/AutoMod.cs
+++ UnityProject/Assets/Scripts/AutoMod/AutoMod.cs
@@ -37,104 +37,6 @@ namespace AdminTools
 
 		private static float maxScore = 0.7f; //0 - 1f;
 
-		class MuteRecord
-		{
-			public DateTime timeOfMute;
-			public int lengthOfMute;
-
-			public int RemainingSeconds()
-			{
-				var remainingSeconds = lengthOfMute - ((int) (DateTime.Now - timeOfMute).TotalSeconds);
-				Debug.Log("Remaining seconds: " + remainingSeconds);
-				return remainingSeconds;
-			}
-		}
-
-		class MessageRecord
-		{
-			private Dictionary<DateTime, string> messageLog = new Dictionary<DateTime, string>();
-			private List<MuteRecord> muteRecords = new List<MuteRecord>();
-			public ConnectedPlayer player;
-
-			public bool IsSpamming(string message)
-			{
-				if (muteRecords.Count != 0)
-				{
-					var remainingSeconds = muteRecords[muteRecords.Count - 1].RemainingSeconds();
-					if (remainingSeconds > 0)
-					{
-						SendMuteMessageToPlayer(remainingSeconds);
-						return true;
-					}
-				}
-
-				messageLog.Add(DateTime.Now, message);
-				if (CalculateSpamScore() > maxScore)
-				{
-					AddMuteRecord();
-					return true;
-				}
-
-				return false;
-			}
-
-			private void AddMuteRecord()
-			{
-				var record = new MuteRecord
-				{
-					timeOfMute = DateTime.Now,
-					lengthOfMute = 10 * (muteRecords.Count + 1)
-				};
-				muteRecords.Add(record);
-
-				SendMuteMessageToPlayer(record.lengthOfMute);
-			}
-
-			private void SendMuteMessageToPlayer(int remainingSeconds)
-			{
-				if (player.GameObject != null)
-				{
-					Chat.AddExamineMsgFromServer(player.GameObject,
-						$"You are doing that too often. Please wait {remainingSeconds} seconds");
-				}
-			}
-
-			private float CalculateSpamScore()
-			{
-				float currentScore = 0f;
-				int repeatMessages = 0;
-				for (int i = messageLog.Count - 1;
-					i >= 0 && i >= messageLog.Count - 6;
-					i--)
-				{
-					int prevIndex = i - 1;
-					if (prevIndex < 0) break;
-
-					var thisKvp = messageLog.ElementAt(i);
-					var prevKvp = messageLog.ElementAt(prevIndex);
-					var totalSeconds = (thisKvp.Key
-					                    - prevKvp.Key).TotalSeconds;
-
-					if (totalSeconds < 1f)
-					{
-						currentScore += 0.2f;
-						if (thisKvp.Value == prevKvp.Value)
-						{
-							currentScore += 0.2f;
-							repeatMessages++;
-						}
-					}
-
-					if (repeatMessages >= 4)
-					{
-						currentScore += 0.8f;
-					}
-				}
-
-				return Mathf.Clamp(currentScore, 0f, 1f);
-			}
-		}
-
 		private AutoModConfig loadedConfig;
 
 		private static string AutoModConfigPath =>
@@ -193,7 +95,8 @@ namespace AdminTools
 
 		public static string ProcessChatServer(ConnectedPlayer player, string message)
 		{
-			if (player == null || !IsEnabled()) return message;
+			if (player == null || Instance.loadedConfig == null
+			                   || !Instance.loadedConfig.enableSpamProtection) return message;
 
 			if (!chatLogs.ContainsKey(player))
 			{
@@ -217,16 +120,122 @@ namespace AdminTools
 			if (!Instance.loadedConfig.enableAutoMod) return false;
 			return true;
 		}
+
+		[ContextMenu("Create default config file")]
+		void CreateDefaultConfig()
+		{
+			File.WriteAllText(AutoModConfigPath, JsonUtility.ToJson(new AutoModConfig()));
+		}
+
+		class MuteRecord
+		{
+			public DateTime timeOfMute;
+			public int lengthOfMute;
+
+			public int RemainingSeconds()
+			{
+				var remainingSeconds = lengthOfMute - ((int) (DateTime.Now - timeOfMute).TotalSeconds);
+				return remainingSeconds;
+			}
+		}
+
+		class MessageRecord
+		{
+			private Dictionary<DateTime, string> messageLog = new Dictionary<DateTime, string>();
+			private List<MuteRecord> muteRecords = new List<MuteRecord>();
+			public ConnectedPlayer player;
+
+			public bool IsSpamming(string message)
+			{
+				if (muteRecords.Count != 0)
+				{
+					var remainingSeconds = muteRecords[muteRecords.Count - 1].RemainingSeconds();
+					if (remainingSeconds > 0)
+					{
+						SendMuteMessageToPlayer(remainingSeconds);
+						return true;
+					}
+				}
+
+				messageLog.Add(DateTime.Now, message);
+				if (CalculateSpamScore() > maxScore)
+				{
+					AddMuteRecord();
+					return true;
+				}
+
+				return false;
+			}
+
+			private void AddMuteRecord()
+			{
+				var record = new MuteRecord
+				{
+					timeOfMute = DateTime.Now,
+					lengthOfMute = 5 * (muteRecords.Count + 1)
+				};
+				muteRecords.Add(record);
+				//clear them so we can start the spam
+				//checks on a clean slate when they are unmuted
+				messageLog.Clear();
+
+				SendMuteMessageToPlayer(record.lengthOfMute);
+			}
+
+			private void SendMuteMessageToPlayer(int remainingSeconds)
+			{
+				if (player.GameObject != null)
+				{
+					Chat.AddExamineMsgFromServer(player.GameObject,
+						$"You are doing that too often. Please wait {remainingSeconds} seconds");
+				}
+			}
+
+			private float CalculateSpamScore()
+			{
+				float currentScore = 0f;
+				int repeatMessages = 0;
+				for (int i = messageLog.Count - 1;
+					i >= 0 && i >= messageLog.Count - 6;
+					i--)
+				{
+					int prevIndex = i - 1;
+					if (prevIndex < 0) break;
+
+					var thisKvp = messageLog.ElementAt(i);
+					var prevKvp = messageLog.ElementAt(prevIndex);
+					var totalSeconds = (thisKvp.Key
+					                    - prevKvp.Key).TotalSeconds;
+
+					if (totalSeconds < 1f)
+					{
+						currentScore += 0.2f;
+						if (thisKvp.Value == prevKvp.Value)
+						{
+							currentScore += 0.2f;
+							repeatMessages++;
+						}
+					}
+
+					if (repeatMessages >= 4)
+					{
+						currentScore += 0.8f;
+					}
+				}
+
+				return Mathf.Clamp(currentScore, 0f, 1f);
+			}
+		}
 	}
 
 	[Serializable]
 	public class AutoModConfig
 	{
-		public bool enableAutoMod;
-		public bool enableAllocationProtection;
-		public bool enableSpamProtection;
-		public bool enableBadWordFilter;
-		public bool enableRdmNotification;
-		public bool enablePlasmaReleaseNotification;
+		public bool enableAutoMod = true;
+		public bool enableAllocationProtection = true;
+		public bool enableSpamProtection = true;
+		public bool enableBadWordFilter = true;
+		public bool enableRdmNotification = true;
+		public bool enablePlasmaReleaseNotification = true;
 	}
 }
