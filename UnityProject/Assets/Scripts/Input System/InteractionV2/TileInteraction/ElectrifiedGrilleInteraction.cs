2073c5045cea336a4e4efa84f53f853b88f26f68
NoooneyDude <noooneydude@gmail.com>
2020-03-29 19:43:57 +1300

    Fix connector finding for client, start debug voltage for client

--- UnityProject/Assets/Scripts/Input System/InteractionV2/TileInteraction/ElectrifiedGrilleInteraction.cs
+++ UnityProject/Assets/Scripts/Input System/InteractionV2/TileInteraction/ElectrifiedGrilleInteraction.cs
@@ -1,25 +1,37 @@
 using System;
 using UnityEngine;
-using System.Diagnostics;
+
+using System.Linq;
+using Mirror;
+//using System.Collections.Generic;
 
 /// <summary>
-///  Extended interaction logic for grilles. Checks if the performer should be electrocuted. Needs to come before other interactions.
+///  Extended interaction logic for grilles.
+///  Checks if the performer should be electrocuted. Needs to come before other interactions.
 /// </summary>
-[CreateAssetMenu(fileName = "ElectrifiedGrilleInteraction", menuName = "Interaction/TileInteraction/ElectrifiedGrilleInteraction")]
-public class ElectrifiedGrilleInteraction : TileInteraction
+[CreateAssetMenu(fileName = "ElectrifiedGrilleInteraction",
+	menuName = "Interaction/TileInteraction/ElectrifiedGrilleInteraction")]
+public class ElectrifiedGrilleInteraction : TileInteraction//, INodeControl
 {
-	private float voltage;
+	[SyncVar(hook = nameof(SyncVoltage))]
+	public float voltageSync;
+	public ElectricalNodeControl ElectricalNodeControl;
+
+	private float voltage = 0;
 
 	public override bool WillInteract(TileApply interaction, NetworkSide side)
 	{
+		Debug.LogError("Running " + side.ToString() + "-side!");
+
 		// Make sure performer is near the grille.
 		if (!DefaultWillInteract.Default(interaction, side)) return false;
-		if (!ElectrocutionCriteriaMet(interaction)) return false;
+		if (!ElectrocutionCriteriaMet(interaction, side)) return false;
+
+		Debug.LogError("Electrocution criteria met for " + side.ToString() + ", check voltage: " + voltage.ToString());
 
 		var severity = (new Electrocution()).GetPlayerSeverity(interaction.Performer, voltage);
 
-		// If the potential electrocution is painful, return true to stop
-		// other interactions.
+		// If the potential electrocution is painful, return true to stop other interactions.
 		if (severity > Electrocution.Severity.Mild) return true;
 
 		return false;
@@ -38,8 +50,10 @@ public class ElectrifiedGrilleInteraction : TileInteraction
     /// </summary>
     /// <param name="interaction">TileApply interaction</param>
     /// <returns>Boolean</returns>
-	private bool ElectrocutionCriteriaMet(TileApply interaction)
+	private bool ElectrocutionCriteriaMet(TileApply interaction, NetworkSide side)
 	{
+		Debug.LogError("Checking electrocution criteria...");
+
 		Vector3Int targetCellPos = interaction.TargetCellPos;
 		Matrix matrix = interaction.Performer.GetComponentInParent<Matrix>();
 		MetaTileMap metaDataLayer = matrix.GetComponentInParent<MetaTileMap>();
@@ -49,26 +63,61 @@ public class ElectrifiedGrilleInteraction : TileInteraction
 
 		// Check for cables underneath the grille.
 		var eConns = matrix.GetElectricalConnections(targetCellPos);
+		Debug.LogError("eConns count: " + eConns.Count());
 		if (eConns == null) return false;
 
 		// Get the highest voltage and whether there is a machine connector.
-		// Unfortunately, the current powernet implementation means the cable will only report
-		// a voltage if both ends of the cable are connected to a consumer/producer, it seems.
+		// The current powernet implementation means the cable
+		// will only report a voltage if there is current flow, it seems.
 		// That's why we cannot simply read the connector voltage, and both ends of the cable
 		// need to be connected to the powernet.
 		bool connectorExists = false;
-		this.voltage = 0.0f;
-		foreach (var conn in eConns)
-		{
-			if (conn.InData.Categorytype == PowerTypeCategory.LowMachineConnector
-				|| conn.InData.Categorytype == PowerTypeCategory.MediumMachineConnector
-				|| conn.InData.Categorytype == PowerTypeCategory.HighMachineConnector)
-			{
-				connectorExists = true;
-				continue; // Connector won't report a voltage
+		// Unfortunately client won't report conn.InData.Categorytype and conn.Data.ActualVoltage correctly.
+		if (side == NetworkSide.Client)
+        {
+			foreach (var conn in eConns)
+            {
+				Debug.LogError(conn);
+				if (conn.ToString().Contains("MachineConnector"))
+                {
+					connectorExists = true;
+					continue; // Connector won't report a voltage.
+                }
+				float resistance = ElectricityFunctions.WorkOutActualNumbers(conn).Item1;
+				float actualVoltage = ElectricityFunctions.WorkOutActualNumbers(conn).Item2;
+				float current = ElectricityFunctions.WorkOutActualNumbers(conn).Item3;
+				float myVoltage = MyOwnVoltage(conn);
+				var getENCComponent = conn.GetComponent<ElectricalNodeControl>();
+				var getENCComponentInParent = conn.GetComponentInParent<ElectricalNodeControl>();
+				Debug.LogError(" > Actual voltage found: " + actualVoltage.ToString());
+				Debug.LogError(" > Resistance: " + resistance.ToString());
+				Debug.LogError(" > Current: " + current.ToString());
+				Debug.LogError(" > My voltage found: " + myVoltage.ToString());
+				Debug.LogError(" > Voltage update: " + conn.Data.ActualVoltage.ToString());
+				Debug.LogError(" > Alt. current: " + conn.Data.CurrentInWire.ToString());
+				Debug.LogError(" > Alt. resistance: " + conn.Data.EstimatedResistance.ToString());
+				Debug.LogError(" > GetENCComponent: " + ((getENCComponent) ? getENCComponent.ToString() : "null"));
+				Debug.LogError(" > GetENCComponentInParent: " + ((getENCComponentInParent) ? getENCComponentInParent.ToString() : "null"));
+				if (actualVoltage > voltage) voltage = actualVoltage;
+            }
+        }
+		else
+        {
+			foreach (var conn in eConns)
+            {
+				Debug.LogError(conn);
+				if (conn.InData.Categorytype == PowerTypeCategory.LowMachineConnector
+					|| conn.InData.Categorytype == PowerTypeCategory.MediumMachineConnector
+					|| conn.InData.Categorytype == PowerTypeCategory.HighMachineConnector)
+                {
+					connectorExists = true;
+					continue; // Connector won't report a voltage.
+                }
+
+				Debug.LogError(" > New voltage found: " + conn.Data.ActualVoltage.ToString());
+				if (conn.Data.ActualVoltage > voltage) voltage = conn.Data.ActualVoltage;
 			}
-			if (conn.Data.ActualVoltage > this.voltage) this.voltage = conn.Data.ActualVoltage;
-		}
+        }
 
 		// Check that there is a machine connector.
 		if (!connectorExists) return false;
@@ -76,4 +125,32 @@ public class ElectrifiedGrilleInteraction : TileInteraction
 		// All checks passed, electrocute the performer!
 		return true;
 	}
+
+	//public void PowerNetworkUpdate()
+	//{
+	//	SyncVoltage(voltageSync, ElectricalNodeControl.Node.Data.ActualVoltage);
+	//}
+
+	private void SyncVoltage(float oldVoltage, float newVoltage)
+	{
+		voltageSync = newVoltage;
+	}
+
+
+	// Copy of WorkOutVoltage();
+	private float MyOwnVoltage(ElectricalOIinheritance electricItem)
+    {
+		Debug.LogError("Running MyOwnVoltage...");
+		float voltage = 0;
+
+		foreach (var supply in electricItem.Data.SupplyDependent)
+        {
+			Debug.LogError(" > > " + supply.ToString());
+			voltage += supply.Value.SourceVoltages;
+        }
+
+		electricItem.Data.ActualVoltage = voltage;
+
+		return voltage;
+    }
 }
