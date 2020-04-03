aa676ca4f23f150ba6854b9e29678935d3273a84
Andrés Riquelme <andresfranco.rs@gmail.com>
2020-04-03 18:57:35 -0300

    Finished SpeechModManager

--- UnityProject/Assets/Scripts/ScriptableObject/SpeechModManager.cs
+++ UnityProject/Assets/Scripts/ScriptableObject/SpeechModManager.cs
@@ -4,66 +4,21 @@ using System;
 [CreateAssetMenu(fileName = "SpeechModManager", menuName = "Singleton/SpeechModManager")]
 public class SpeechModManager: SingletonScriptableObject<SpeechModManager>
 {
-    // Add your new speech mutations here
-    public SpeechModifierSO Canadian;
-    public SpeechModifierSO Chav;
-    public SpeechModifierSO Elvis;
-    public SpeechModifierSO French;
-    public SpeechModifierSO Italian;
-    public SpeechModifierSO Spurdo;
-    public SpeechModifierSO Swedish;
-    public SpeechModifierSO UwU;
-    public SpeechModifierSO Stutter;
-
+    public SpeechModDict speechModifier;
 
     public string ApplyMod(ChatModifier modifiers, string message)
     {
-		if ((modifiers & ChatModifier.Canadian) == ChatModifier.Canadian)
+        foreach (var kvp in speechModifier)
         {
-            message = Canadian.ProcessMessage(message);
-        }
-
-		if ((modifiers & ChatModifier.Chav) == ChatModifier.Chav)
-        {
-            message = Chav.ProcessMessage(message);
-        }
-
-		if ((modifiers & ChatModifier.Elvis) == ChatModifier.Elvis)
-        {
-            message = Elvis.ProcessMessage(message);
-        }
-
-		if ((modifiers & ChatModifier.French) == ChatModifier.French)
-        {
-            message = French.ProcessMessage(message);
-        }
-
-		if ((modifiers & ChatModifier.Italian) == ChatModifier.Italian)
-        {
-            message = Italian.ProcessMessage(message);
-        }
-
-		if ((modifiers & ChatModifier.Swedish) == ChatModifier.Swedish)
-        {
-            message = Swedish.ProcessMessage(message);
-        }
-
-        if ((modifiers & ChatModifier.Spurdo) == ChatModifier.Spurdo)
-        {
-            message = Spurdo.ProcessMessage(message);
-        }
-
-        if ((modifiers & ChatModifier.UwU) == ChatModifier.UwU)
-        {
-            message = UwU.ProcessMessage(message);
-        }
-
-        if ((modifiers & ChatModifier.Stutter) == ChatModifier.Stutter)
-        {
-            message = Stutter.ProcessMessage(message);
+            if (modifiers.HasFlag(kvp.Key))
+            {
+                message = kvp.Value.ProcessMessage(message);
+            }
         }
 
         return message;
     }
 }
 
+[Serializable]
+public class SpeechModDict : SerializableDictionary<ChatModifier, SpeechModifierSO>{}
