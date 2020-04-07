1d4862eca887a4dda2f16c9e95ad3ed00896c668
LuxMiz <Cardioxo@hotmail.com>
2020-04-07 20:30:12 +0200

    added ScriptableObject for machine products

---
+++ UnityProject/Assets/Scripts/Machines/MachineProducts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MachineProducts", menuName = "ScriptableObjects/Machines/MachineProducts")]
+public class MachineProducts : ScriptableObject
+{
+	public MachineProductCategory[] productCategoryList;
+}
+
+[System.Serializable]
+public class MachineProductCategory
+{
+	public string categoryName;
+
+	public MachineProduct[] products;
+}
+
+[System.Serializable]
+public class MachineProduct
+{
+	public string name;
+	public GameObject product;
+}
