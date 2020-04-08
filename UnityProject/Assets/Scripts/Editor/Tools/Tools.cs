95ad71b42a527fbed3f062c034f09ad870aa8bbf
Doobly <rob@smashingpixels.com.au>
2020-04-08 13:56:56 +1000

    Remove SceneID tool and reinstate PogStation

--- UnityProject/Assets/Scripts/Editor/Tools/Tools.cs
+++ UnityProject/Assets/Scripts/Editor/Tools/Tools.cs
@@ -15,18 +15,6 @@ public class Tools : Editor
 
 	}
 
-	[MenuItem("Tools/Reset NetworkObj Scene IDs")]
-	private static void ResetNetIDs()
-	{
-		var identities = FindObjectsOfType<NetworkIdentity>();
-		foreach (var i in identities)
-		{
-			i.AssignSceneID();
-			EditorUtility.SetDirty(i);
-		}
-		Logger.Log($"{identities.Length} network identities were reassigned scene ids");
-	}
-
 	[MenuItem("Tools/Clean Up Wire Dupes")]
 	private static void RemoveWireDupes()
 	{
