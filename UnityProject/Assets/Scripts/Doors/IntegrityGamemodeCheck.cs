8de3167a50f16c1bde43c79b0c21bab5095d1839
NuclearWinter <nukeuler123@gmail.com>
2020-04-26 19:18:53 -0600

    Re added the checker
    So uhh, some weird file stuff happened so i hardreset this branch and re added the component for mappers to use on their syndicate outposts and shuttles

---
+++ UnityProject/Assets/Scripts/Doors/IntegrityGamemodeCheck.cs
@@ -0,0 +1,34 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class IntegrityGamemodeCheck : NetworkBehaviour
+{
+	[SerializeField]
+	[Tooltip("Check this if you want this object on this matrix to not be able to take damage unless on certain round types")]
+	private bool indestructableNotOnRound;
+
+	[SerializeField]
+	[Tooltip("Any round in this list will make the matrix destructable")]
+	private GameMode roundToBeDestructable;
+
+	[Server]
+	// This simply checks if the bool is true, if not the door will not be affected by rounds
+	private void Start()
+	{
+		if (indestructableNotOnRound)
+		{
+			var component = gameObject.GetComponent<Integrity>();
+			DestructableRoundCheck(component);
+		}
+	}
+	[Server]
+	//Will check the gameManger's current gamemode against its list, if it returns true then it will make the door indestructable
+	//Or anything with integrity for that matter
+	private void DestructableRoundCheck(Integrity currentObject)
+	{
+		if (GameManager.Instance.GetGameModeName(true) != roundToBeDestructable.Name)
+		{
+			currentObject.Resistances.Indestructable = true;
+		}
+	}
+}
