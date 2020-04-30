8cc7698f09e97175c99242422a3c8681f89e5325
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2020-04-30 16:35:48 +0100

    Scripts, states, storage v1

---
+++ UnityProject/Assets/Scripts/Construction/Machine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Machines
+{
+	/// <summary>
+	/// Main Component for Machine deconstruction
+	/// </summary>
+	public class Machine : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
