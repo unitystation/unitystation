67c9198f7ce96fdeef84549519f2d608803c77bf
ReiderJack <asetonlim@gmail.com>
2020-05-01 20:04:23 +0300

    SO LightMountState, SpritesDirectional, filled them all

---
+++ UnityProject/Assets/Scripts/ScriptableObject/SOLightMountState.cs
@@ -0,0 +1,34 @@
+﻿using Items;
+using UnityEngine;
+public enum LightMountState
+{
+	On,
+	Off,
+	MissingBulb,
+	Broken,
+	Emergency,
+	BurnedOut
+}
+[CreateAssetMenu(fileName = "SOLightMountState", menuName = "ScriptableObjects/States/SOLightMountState", order = 0)]
+public class SOLightMountState : UnityEngine.ScriptableObject
+{
+	public SpritesDirectional spritesDirectional;
+
+	public LightMountState state;
+
+	[Tooltip("Will drop this item.")]
+	public GameObject Tube;
+
+	[Tooltip("Item with this trait will be put in.")]
+	public ItemTrait traitRequired;
+
+	[Tooltip("On what % of integrity mount changes state.")]
+	[Range(0.1f, 0.90f)]
+	public float multiplierBroken;
+
+	[Tooltip("Drops this on destroy.")]
+	public RandomItemPool lootDrop;
+
+	[Tooltip("Light color.")]
+	public Color lightState;
+}
