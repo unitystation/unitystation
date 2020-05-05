2a211045a2e5c93b2ad89f970e0421b798bc9293
Doobly <rob@smashingpixels.com.au>
2020-05-05 13:04:33 +1000

    Start of SubScene manager and SubScene SO's

---
+++ UnityProject/Assets/Scripts/Managers/SubSceneManager/SubSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubSceneManager : MonoBehaviour
+{
+	private static SubSceneManager subSceneManager;
+
+	public static SubSceneManager Instance
+	{
+		get
+		{
+			if (subSceneManager == null)
+			{
+				subSceneManager = FindObjectOfType<SubSceneManager>();
+			}
+
+			return subSceneManager;
+		}
+	}
+
+	[SerializeField] private AwayWorldListSO awayWorldList;
+}
