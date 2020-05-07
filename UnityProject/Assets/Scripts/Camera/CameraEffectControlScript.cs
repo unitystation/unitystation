9698ec6629bf81ba7a694f4d652042408780cbae
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2020-05-07 22:54:11 +0100

    Drunk and Glitch camera effect

---
+++ UnityProject/Assets/Scripts/Camera/CameraEffectControlScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraEffects
+{
+	public class CameraEffectControlScript : MonoBehaviour
+	{
+
+		public DrunkCamera drunkCamera;
+
+		public GlitchEffect glitchEffect;
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Backspace))
+			{
+				ChangeDrunkState(drunkCamera);
+			}
+
+			if (Input.GetKeyDown(KeyCode.P))
+			{
+				ChangeGlitchState(glitchEffect);
+			}
+		}
+
+		public void ChangeDrunkState(DrunkCamera drunkCamera)
+		{
+			drunkCamera.enabled = !drunkCamera.enabled;
+		}
+
+		public void ChangeGlitchState(GlitchEffect glitchEffect)
+		{
+			glitchEffect.enabled = !glitchEffect.enabled;
+		}
+	}
+}
