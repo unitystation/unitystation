e3138357b6afaa8249af3def60a6f766bb352a99
MajorMoonjump <brandibrooks4@gmail.com>
2020-05-15 20:22:38 -1000

    xeno food and crafting setup
    mmmmm tasty aliens

--- UnityProject/Assets/Scripts/Items/Others/Knife.cs
+++ UnityProject/Assets/Scripts/Items/Others/Knife.cs
@@ -11,7 +11,7 @@ public class Knife : MonoBehaviour, ICheckedInteractable<InventoryApply>
 	//check if item is being applied to offhand with cuttable object on it.
 	public bool WillInteract(InventoryApply interaction, NetworkSide side)
 	{
-	
+
 		//can the player act at all?
 		if (!DefaultWillInteract.Default(interaction, side)) return false;
 
@@ -20,8 +20,8 @@ public class Knife : MonoBehaviour, ICheckedInteractable<InventoryApply>
 
 		//if the item isn't a butcher knife, no go.
 		if (!Validations.HasUsedItemTrait(interaction, CommonTraits.Instance.Knife)) return false;
-		
-		
+
+
 		//TargetSlot must not be empty.
 		if (interaction.TargetSlot.Item == null) return false;
 
@@ -29,34 +29,38 @@ public class Knife : MonoBehaviour, ICheckedInteractable<InventoryApply>
 	}
 	public void ServerPerformInteraction(InventoryApply interaction)
 	{
-
 		//is the target item cuttable?
 		ItemAttributesV2 attr = interaction.TargetObject.GetComponent<ItemAttributesV2>();
 		Ingredient ingredient = new Ingredient(attr.ArticleName);
 		GameObject cut = CraftingManager.Cuts.FindRecipe(new List<Ingredient> { ingredient });
-		if (cut)
+		if (cut && interaction.TargetObject.GetComponent<Stackable>() != null && ingredient.requiredAmount
+			== interaction.TargetObject.GetComponent<Stackable>().Amount)
 		{
 			Inventory.ServerDespawn(interaction.TargetSlot);
 
-			SpawnResult spwn = Spawn.ServerPrefab(CraftingManager.Cuts.FindOutputMeal(cut.name), 
+			SpawnResult spwn = Spawn.ServerPrefab(CraftingManager.Cuts.FindOutputMeal(cut.name),
 			SpawnDestination.At(), 1);
 
 			if (spwn.Successful)
 			{
-				
-				//foreach (GameObject obj in spwn.GameObjects)
-				//{
-				//	Inventory.ServerAdd(obj,interaction.TargetSlot);
-				//}
-
-				Inventory.ServerAdd(spwn.GameObject ,interaction.TargetSlot);
-
+				Inventory.ServerAdd(spwn.GameObject, interaction.TargetSlot);
 			}
+		}
+		else if (cut)
+		{
+			Inventory.ServerDespawn(interaction.TargetSlot);
 
-		} else {
+			SpawnResult spwn = Spawn.ServerPrefab(CraftingManager.Cuts.FindOutputMeal(cut.name),
+			SpawnDestination.At(), 1);
 
+			if (spwn.Successful)
+			{
+				Inventory.ServerAdd(spwn.GameObject, interaction.TargetSlot);
+			}
+		}
+		else
+		{
 			Chat.AddExamineMsgFromServer(interaction.Performer, "You can't cut this.");
 		}
-
 	}
 }
