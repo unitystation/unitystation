dad03747833e0c63b2affd4ce8379a7b9115f1b5
Rember <timmermanrembrandt@gmail.com>
2020-07-01 19:49:15 +0200

    Added a Door Wedger component, allowing items to wedge open closed doors

---
+++ UnityProject/Assets/Scripts/Tool/DoorWedger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorWedger : MonoBehaviour, IInteractable<HandApply>, ICheckedInteractable<HandApply>
+{
+    [SerializeField] [Tooltip("The time in seconds it takes to wedge open a door with this tool.")]
+    private float openingDelay = 10f;
+
+    public bool WillInteract(HandApply interaction, NetworkSide side)
+    {
+        if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+        if (Validations.IsTarget(gameObject, interaction)) return false;
+        return true;
+    }
+    
+    public void ServerPerformInteraction(HandApply interaction)
+    {
+        var targetObject = interaction.TargetObject.GetComponent<DoorController>();
+
+        if (targetObject == null)
+        {
+            Chat.AddExamineMsgFromServer(interaction.Performer, "DEBUG: You hit NULL.");
+            return;
+        }
+
+        if (targetObject.IsWelded)
+        {
+            Chat.AddExamineMsgFromServer(interaction.Performer, $"You can't force this door open with your {gameObject.name}, it's welded shut!");
+        }
+
+        if (targetObject.IsClosed)
+        {
+            ToolUtils.ServerUseToolWithActionMessages(interaction, openingDelay,
+            "You start wedging open the door...",
+            $"{interaction.Performer.ExpensiveName()} starts wedging open the door...",
+            $"You force the door open with your {gameObject.name}!",
+            $"{interaction.Performer.ExpensiveName()} forces the door open!",
+            () =>
+            {
+                targetObject.ServerOpen();
+            });
+        }
+    }
+}
