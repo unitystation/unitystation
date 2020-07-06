90d6dd8d6c513d8cf55fe8198f38f3975beda702
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2020-07-06 20:48:51 +0100

    Tiles, added random brush tile support and start of map

---
+++ UnityProject/Assets/Scripts/Shuttles/ShuttleTeleport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuttleTeleport : MonoBehaviour
+{
+	public Vector2 TeleportCoordinate;
+
+	private MatrixMove matrixMove;
+
+	private void Awake()
+	{
+		matrixMove = GetComponent<MatrixMove>();
+	}
+
+	public void TeleportShuttle()
+	{
+		matrixMove.SetPosition(TeleportCoordinate);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			TeleportShuttle();
+		}
+	}
+}
