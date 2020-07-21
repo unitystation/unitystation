d1e8e58a2cf3a70b89f065873052aebea3a4dcbf
yoysam <54789844+yoysam@users.noreply.github.com>
2020-07-21 16:50:19 -0400

    Update RegisterPlayer.cs

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Objects/RegisterPlayer.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Objects/RegisterPlayer.cs
@@ -160,7 +160,7 @@ public class RegisterPlayer : RegisterTile, IServerSpawn
 		}
 		// Don't slip while walking unless its enabled with "slipWhileWalking".
 		// Don't slip while player's consious state is crit, soft crit, or dead.
-		// Don't slip while the players hunger state is Malnourished, or Straving
+		// Don't slip while the players hunger state is Malnourished, or Strarving
 		if (IsSlippingServer
 			|| !slipWhileWalking && playerScript.PlayerSync.SpeedServer <= playerScript.playerMove.WalkSpeed
 			|| playerScript.playerHealth.IsCrit
