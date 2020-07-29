800c671ff0093ba15c033a1c7b4f70f53b92c23c
Bod9001 <27101670+Bod9001@users.noreply.github.com>
2020-07-29 14:04:22 +0100

    humm lets try this

--- UnityProject/SetEditorSettingsForBuild.py
+++ UnityProject/SetEditorSettingsForBuild.py
@@ -2,9 +2,9 @@ import os
 #python 3
 isto = "3"
 setto = "4"
-with open('ProjectSettings/EditorSettings.asset', "r+") as RmetaFile:
+with open('./UnityProject/ProjectSettings/EditorSettings.asset', "r+") as RmetaFile:
     Data = RmetaFile.read()
     RmetaFile.close()
-    metaFile = open('ProjectSettings/EditorSettings.asset',"w+")
+    metaFile = open('./UnityProject/ProjectSettings/EditorSettings.asset',"w+")
     metaFile.write(Data.replace('m_SpritePackerMode: '+ isto,"m_SpritePackerMode: "+ setto))
     metaFile.close()
