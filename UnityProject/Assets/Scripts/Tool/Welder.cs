076f12200f1c17a2203c6a92a76cdf60d68f8993
Pet Mudstone <pinalesjrmudstone@gmail.com>
2020-08-14 22:04:17 -0700

    Adjust welder.cs comments.

--- UnityProject/Assets/Scripts/Tool/Welder.cs
+++ UnityProject/Assets/Scripts/Tool/Welder.cs
@@ -216,8 +216,8 @@ public class Welder : NetworkBehaviour, IInteractable<HandActivate>, IServerSpaw
 			//Server fuel burning:
 			if (isServer)
 			{
-				//With the variable below, it takes about 4:48 minutes (or 288 seconds) for a emergency welding tool (starts with 10 fuel) to run dry. In /tg/ it would have taken about 4:30 minutes (or 270 seconds). - PM
-				// I would try to cut the time down further if I wasn't quite annoyed and uncertain of how to do further calculations. Original variable below was 0.041f (emergency welder ran out after about 25 seconds with it). - PM
+				//With the variable below, it takes about 3:40 minutes (or 220 seconds) for a emergency welding tool (starts with 10 fuel) to run dry. In /tg/ it would have taken about 4:30 minutes (or 270 seconds). - PM
+				//Original variable below was 0.041f (emergency welder ran out after about 25 seconds with it). - PM
 				reagentContainer.TakeReagents(0.005f);
 
 				//Ran out of fuel
