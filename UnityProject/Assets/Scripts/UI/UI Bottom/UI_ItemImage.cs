345b38cf4093fbad72ab7616462f3538ee8df0fe
Alex Evgrashin <aevgrashin@yandex.ru>
2020-08-19 22:24:15 +0300

    Fixed UI icons scale for some resolutions

--- UnityProject/Assets/Scripts/UI/UI Bottom/UI_ItemImage.cs
+++ UnityProject/Assets/Scripts/UI/UI Bottom/UI_ItemImage.cs
@@ -196,6 +196,7 @@ public class UI_ItemImage
 		rt.anchorMax = Vector2.one;
 		rt.sizeDelta = Vector2.zero;
 		rt.anchoredPosition = Vector2.zero;
+		rt.localScale = Vector3.one;
 
 		var img = go.AddComponent<Image>();
 		var imgMat = Resources.Load<Material>("Materials/Palettable UI");
