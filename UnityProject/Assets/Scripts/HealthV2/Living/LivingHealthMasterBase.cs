43d731d8e2c104a689ce4805a07a4dd064314304
Fortune <fortune11709@gmail.com>
2020-09-04 05:53:11 +1000

    Basics of Respiratory System
    Needs some changes, since the lungs organ should be doing a significant amount of work.
    
    The respiratory system moslty just needs to be there to tell the lungs organ when to breath.

--- UnityProject/Assets/Scripts/HealthV2/Living/LivingHealthMasterBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/LivingHealthMasterBase.cs
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Health.Sickness;
 using HealthV2;
 using JetBrains.Annotations;
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 using Mirror;
 using UnityEngine;
 
 public abstract class LivingHealthMasterBase : NetworkBehaviour
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 	/// <summary>
 	/// Server side, each mob has a different one and never it never changes
 	/// </summary>
@@ -33,7 +23,6 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 	private float tick = 0;
 
 	private RegisterTile registerTile;
-<<<<<<< HEAD
 
 	[NonSerialized] public ConsciousStateEvent OnConsciousStateChangeServer = new ConsciousStateEvent();
 	public ConsciousState ConsciousState
@@ -54,30 +43,17 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 	}
 	private ConsciousState consciousState;
 
-	//I don't know what this is. It was in the first Health system.
-	public float RTT;
-
 	[SerializeField]
 	private float maxHealth = 100;
 
 	private float overallHealth = 100;
 	public float OverallHealth => overallHealth;
 
-	[SerializeField]
-	[Tooltip("These are the things that will hold all our organs and implants.")]
-	private List<BodyPartContainerBase> bodyPartContainers;
-
 	[CanBeNull]
 	private CirculatorySystemBase circulatorySystem;
-	public CirculatorySystemBase CirculatorySystem => circulatorySystem;
 
 	[CanBeNull]
 	private RespiratorySystemBase respiratorySystem;
-	public RespiratorySystemBase RespiratorySystem => respiratorySystem;
-
-	[CanBeNull]
-	private MetabolismSystemV2 metabolism;
-	public MetabolismSystemV2 Metabolism => metabolism;
 
 	private bool isDead = false;
 	public bool IsDead => isDead;
@@ -87,40 +63,7 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 
 	public bool InCardiacArrest => circulatorySystem != null && circulatorySystem.HeartIsStopped;
 
-	private List<ImplantBase> implantList = new List<ImplantBase>();
 
-	public List<ImplantBase> ImplantList => implantList;
-
-	public event Action<GameObject> applyDamageEvent;
-
-	public event Action OnDeathNotifyEvent;
-
-	//how on fire we are, sames as tg fire_stacks. 0 = not on fire.
-	//It's called "stacks" but it's really just a floating point value that
-	//can go up or down based on possible sources of being on fire. Max seems to be 20 in tg.
-	[SyncVar(hook = nameof(SyncFireStacks))]
-	private float fireStacks;
-	private float maxFireStacks = 5f;
-	private bool maxFireStacksReached = false;
-
-	/// <summary>
-	/// How on fire we are. Exists client side - synced with server.
-	/// </summary>
-	public float FireStacks => fireStacks;
-
-	/// <summary>
-	/// Client side event which fires when this object's fire status changes
-	/// (becoming on fire, extinguishing, etc...). Use this to update
-	/// burning sprites.
-	/// </summary>
-	[NonSerialized]
-	public FireStackEvent OnClientFireStacksChange = new FireStackEvent();
-
-
-=======
-	private ConsciousState consciousState;
-
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 	public virtual void Awake()
 	{
 		EnsureInit();
@@ -140,15 +83,8 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 
 	private void EnsureInit()
 	{
-<<<<<<< HEAD
-		if (registerTile) return;
-		registerTile = GetComponent<RegisterTile>();
-		respiratorySystem = GetComponent<RespiratorySystemBase>();
-		circulatorySystem = GetComponent<CirculatorySystemBase>();
-=======
 		if (registerTile != null) return;
 		registerTile = GetComponent<RegisterTile>();
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 		//Always include blood for living entities:
 	}
 
@@ -161,25 +97,6 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 		DNABloodType = new DNAandBloodType();
 	}
 
-<<<<<<< HEAD
-	/// <summary>
-	/// Adds a new implant to the health master.
-	/// This is NOT how implants should be added, it is called automatically by the body part container system!
-	/// </summary>
-	/// <param name="implant"></param>
-	public void AddNewImplant(ImplantBase implant)
-	{
-		implantList.Add(implant);
-	}
-
-	public void RemoveImplant(ImplantBase implantBase)
-	{
-		implantList.Remove(implantBase);
-	}
-
-
-=======
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 	public override void OnStartClient()
 	{
 		base.OnStartClient();
@@ -187,21 +104,14 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 		StartCoroutine(WaitForClientLoad());
 	}
 
-<<<<<<< HEAD
 	private void UpdateMe()
 	{
-		foreach (ImplantBase implant in implantList)
-		{
-			implant.ImplantUpdate(this);
-		}
 	}
 
 	private void PeriodicUpdate()
 	{
 	}
 
-=======
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 	IEnumerator WaitForClientLoad()
 	{
 		//wait for DNA:
@@ -221,168 +131,4 @@ public abstract class LivingHealthMasterBase : NetworkBehaviour
 		DNABloodTypeJSON = updatedDNA;
 		DNABloodType = JsonUtility.FromJson<DNAandBloodType>(updatedDNA);
 	}
-<<<<<<< HEAD
-
-		/// <summary>
-	///  Apply Damage to the whole body of this Living thing. Server only
-	/// </summary>
-	/// <param name="damagedBy">The player or object that caused the damage. Null if there is none</param>
-	/// <param name="damage">Damage Amount. will be distributed evenly across all bodyparts</param>
-	/// <param name="attackType">type of attack that is causing the damage</param>
-	/// <param name="damageType">The Type of Damage</param>
-	[Server]
-	public void ApplyDamage(GameObject damagedBy, float damage,
-		AttackType attackType, DamageType damageType)
-	{
-		//TODO: Reimplement
-	}
-
-	/// <summary>
-	///  Apply Damage to random bodypart of the Living thing. Server only
-	/// </summary>
-	/// <param name="damagedBy">The player or object that caused the damage. Null if there is none</param>
-	/// <param name="damage">Damage Amount</param>
-	/// <param name="attackType">type of attack that is causing the damage</param>
-	/// <param name="damageType">The Type of Damage</param>
-	[Server]
-	public void ApplyDamageToBodypart(GameObject damagedBy, float damage,
-		AttackType attackType, DamageType damageType)
-	{
-		ApplyDamageToBodypart(damagedBy, damage, attackType, damageType, BodyPartType.Chest.Randomize(0));
-	}
-
-	/// <summary>
-	///  Apply Damage to the Living thing. Server only
-	/// </summary>
-	/// <param name="damagedBy">The player or object that caused the damage. Null if there is none</param>
-	/// <param name="damage">Damage Amount</param>
-	/// <param name="attackType">type of attack that is causing the damage</param>
-	/// <param name="damageType">The Type of Damage</param>
-	/// <param name="bodyPartAim">Body Part that is affected</param>
-	[Server]
-	public virtual void ApplyDamageToBodypart(GameObject damagedBy, float damage,
-		AttackType attackType, DamageType damageType, BodyPartType bodyPartAim)
-	{
-		//TODO: Reimplement
-	}
-
-	private void TryGibbing(float damage)
-	{
-		//TODO: Reimplement
-	}
-
-	/// <summary>
-	///  Apply healing to a living thing. Server Only
-	/// </summary>
-	/// <param name="healingItem">the item used for healing (bruise pack etc). Null if there is none</param>
-	/// <param name="healAmt">Amount of healing to add</param>
-	/// <param name="damageType">The Type of Damage To Heal</param>
-	/// <param name="bodyPartAim">Body Part to heal</param>
-	[Server]
-	public virtual void HealDamage(GameObject healingItem, int healAmt,
-		DamageType damageTypeToHeal, BodyPartType bodyPartAim)
-	{
-		//TODO: Reimplement
-	}
-
-	[Server]
-	public void Harvest()
-	{
-
-		//Reimplement
-
-		Gib();
-	}
-
-	[Server]
-	protected virtual void Gib()
-	{
-
-		//TODO: Reimplement
-
-		//never destroy players!
-		Despawn.ServerSingle(gameObject);
-	}
-
-	/// ---------------------------
-	/// CRIT + DEATH METHODS
-	/// ---------------------------
-	///Death from other causes
-	public virtual void Death()
-	{
-		//TODO: Reimplemenmt
-	}
-
-	public virtual void AddSickness(Sickness sickness)
-	{
-		//TODO: Reimplement
-	}
-
-	public void UpdateClientHealthStats(float overallHealth)
-	{
-		//TODO: Reimplement
-	}
-
-	/// <summary>
-	/// Updates the conscious state from the server via NetMsg
-	/// </summary>
-	public void UpdateClientConsciousState(ConsciousState proposedState)
-	{
-		ConsciousState = proposedState;
-	}
-
-	/// <summary>
-	/// Updates the respiratory health stats from the server via NetMsg
-	/// </summary>
-	public void UpdateClientRespiratoryStats(bool value)
-	{
-		respiratorySystem.IsSuffocating = value;
-	}
-
-	public void UpdateClientTemperatureStats(float value)
-	{
-		respiratorySystem.temperature = value;
-	}
-
-	public void UpdateClientPressureStats(float value)
-	{
-		respiratorySystem.pressure = value;
-	}
-
-	/// <summary>
-	/// Updates the blood health stats from the server via NetMsg
-	/// </summary>
-	public void UpdateClientBloodStats(int heartRate, float bloodVolume, float oxygenDamage, float toxinLevel)
-	{
-		//TODO: Reimplement bloodSystem.UpdateClientBloodStats(heartRate, bloodVolume, oxygenDamage, toxinLevel);
-	}
-
-	/// <summary>
-	/// Updates the brain health stats from the server via NetMsg
-	/// </summary>
-	public void UpdateClientBrainStats(bool isHusk, int brainDamage)
-	{
-		//TODO: Reimplement
-	}
-
-	public void ChangeFireStacks(float deltaValue)
-	{
-		SyncFireStacks(fireStacks, fireStacks + deltaValue);
-	}
-
-	private void SyncFireStacks(float oldValue, float newValue)
-	{
-		EnsureInit();
-		this.fireStacks = Math.Max(0, newValue);
-		OnClientFireStacksChange.Invoke(this.fireStacks);
-	}
-
-	public void Extinguish()
-	{
-		SyncFireStacks(fireStacks, 0);
-	}
-
-
-=======
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 }
