a16d9e31a7b87f2c4777c231808df77ec82ea55f
Fortune <fortune11709@gmail.com>
2020-09-04 05:53:11 +1000

    Basics of Respiratory System
    Needs some changes, since the lungs organ should be doing a significant amount of work.
    
    The respiratory system moslty just needs to be there to tell the lungs organ when to breath.

--- UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespitorySystem : MonoBehaviour
+public class BodyPartBase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
