4f8fffa2b706304e5aa0419532f04c894f09a20f
Fortune <fortune11709@gmail.com>
2020-09-04 04:26:15 +1000

    Initial Commit to Save Progress

--- UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/BloodTypes/BloodType.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/BloodTypes/BloodType.cs
@@ -1,14 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Chemistry;
 
-[CreateAssetMenu(fileName = "BloodType", menuName = "ScriptableObjects/Health/BloodType", order = 0)]
 public class BloodType : ScriptableObject
 {
 	[Tooltip("This is the reagent actually metabolised and circulated through this circulatory system.")]
-	public Chemistry.Reagent CirculatedReagent;
+	public Reagent CirculatedReagent;
 
 	[Tooltip("This is the reagent mix that the blood is composed of.")]
-	public Chemistry.ReagentMix CompleteBloodMix;
+	public ReagentMix CompleteBloodMix;
 
 	[Tooltip("The color of this bloodtype.")]
 	public Color Color;
