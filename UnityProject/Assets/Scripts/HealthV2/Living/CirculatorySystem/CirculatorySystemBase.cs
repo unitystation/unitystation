9a5e51049f9366f816bb4152561b1527781be649
Fortune <fortune11709@gmail.com>
2020-09-04 04:26:15 +1000

    Initial Commit to Save Progress

--- UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/CirculatorySystemBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/CirculatorySystemBase.cs
@@ -15,20 +15,13 @@ namespace HealthV2
 		private BloodType bloodType = null;
 		public BloodType BloodType => bloodType;
 
-		public ReagentMix UseBloodPool;
-		public ReagentMix ReadyBloodPool;
-
-
 		//How much of our blood reagent we have.
 		//In a human, this would be oxygen.
-		//private float bloodReagentAmount;
-		// public float BloodReagentAmount => bloodReagentAmount;
-
-		// private float bloodAmount = 0;
-		// public float BloodAmount => bloodAmount;
+		private float bloodReagentAmount;
+		public float BloodReagentAmount => bloodReagentAmount;
 
 		//The actual reagent our blood uses.
-		public Chemistry.Reagent BloodReagent => bloodType.CirculatedReagent;
+		public Reagent BloodReagent => bloodType.CirculatedReagent;
 
 		//This is the list of the toxins in the circulatory system.
 		//Different toxins can have different effects on different parts of the body.
@@ -42,26 +35,11 @@ namespace HealthV2
 		private CirculatoryInfo bloodInfo = null;
 		public CirculatoryInfo BloodInfo => bloodInfo;
 
-		//For every point of strength that the heart has, this is how much blood flow the body will receive.
-		//In the human body, it takes about 45 seconds for blood to fully circulate through your body.
-		//This means that at 0.95f blood flow, the entirety of a humans blood (about 5L) would be cycled in  ~50 heart beats.
-		private float heartStrengthRatio = 0.95f;
-
 		//Now, this is useful, but it isn't actually set here. This will need to be modified by an organ.
 		//So I need to remember that! It may be zero the frame the player is spawned in.
 		//Probably update this each tick. Set to zero if there is no update from organs.
 		private float heartRate = 0;
-
-		//This bool is to let the heart organs know that they need to increase their heartrate.
-		private bool heartRateNeedsIncreasing = false;
-
-		public bool HeartRateNeedsIncreasing => heartRateNeedsIncreasing;
-
-		public float HeartRate
-		{
-			get => heartRate;
-			set => heartRate = value;
-		}
+		public float HeartRate => heartRate;
 
 		public bool HeartIsStopped => heartRate <= 0;
 
@@ -70,61 +48,14 @@ namespace HealthV2
 		private void Awake()
 		{
 			healthMaster = GetComponent<LivingHealthMasterBase>();
-			ReadyBloodPool.Add(BloodReagent,5);
-			// bloodReagentAmount = BloodInfo.BLOOD_REAGENT_DEFAULT;
-			// bloodAmount = BloodInfo.BLOOD_DEFAULT;
-		}
-
-		public virtual void AddUsefulBloodReagent(ReagentMix amount) //Return excess
-		{
-			ReadyBloodPool.Add(amount);
-			//Add mechanism for recovering blood naturally
-			//ReadyBloodPool = Mathf.Min(bloodInfo.BLOOD_REAGENT_MAX, bloodReagentAmount);
-			//return 0;
-		}
-
-		public void ConvertUsedBlood(float Toadd)
-		{
-			//TODOH technically violates the laws of thermodynamics but meh
-			var addAmount = healthMaster.CirculatorySystem.UseBloodPool.Take(Toadd);
-			AddUsefulBloodReagent(addAmount);
+			bloodReagentAmount = BloodInfo.BLOOD_REAGENT_NORMAL;
 		}
 
 		//This circulates blood around the body.
 		//This isn't actually called by the circulatory system, it is going to be activated by an organ pumping blood.
-		public virtual void HeartBeat(float strength)
+		public void PumpBlood()
 		{
-			//TODOH Balance all this stuff, Currently takes an eternity to suffocate
-			float initialPumpAmount = (heartStrengthRatio * strength);
-
-			// Logger.Log("heart Available  " + ReadyBloodPool);
-			float pumpedReagent = Math.Min(initialPumpAmount, ReadyBloodPool.Total);
-
-			// Logger.Log("heart pumpedReagent " + pumpedReagent);
-
-			float WantedBlood = 0;
-			foreach (BodyPart implant in healthMaster.ImplantList)
-			{
-				if (implant.IsBloodReagentConsumed == false) continue;
-				WantedBlood += implant.BloodReagentStoreAmount;
-			}
-
-			// Logger.Log("heart Wanted blood " + WantedBlood);
-
-			ReagentMix SpareBlood = new ReagentMix();
-
-			foreach (BodyPart implant in healthMaster.ImplantList)
-			{
-				if (implant.IsBloodReagentConsumed == false) continue;
-				var BloodToGive = ReadyBloodPool.Take((implant.BloodReagentStoreAmount / WantedBlood) * pumpedReagent);
-				BloodToGive.Add(SpareBlood);
-				SpareBlood.Clear();
-				SpareBlood.Add(implant.BloodPumpedEvent(BloodToGive));
-			}
-
-			// Logger.Log("heart SpareBlood " + SpareBlood);
-			heartRateNeedsIncreasing = (SpareBlood.Total == 0);
-			ReadyBloodPool = SpareBlood;
+			//TODO: The whole thing. Need to start working on organs now.
 		}
 
 	}
--- UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/Toxins/ToxinBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/Toxins/ToxinBase.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public abstract class ToxinBase
 {
 
