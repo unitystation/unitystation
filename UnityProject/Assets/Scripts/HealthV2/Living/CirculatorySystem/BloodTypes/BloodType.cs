a6ac6faf7f30ed707b482bcd67c7de4a0578f0cf
Fortune <fortune11709@gmail.com>
2020-09-04 04:26:15 +1000

    Initial Commit to Save Progress

---
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/BloodTypes/BloodType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chemistry;
+
+public class BloodType : ScriptableObject
+{
+	[Tooltip("This is the reagent actually metabolised and circulated through this circulatory system.")]
+	public Reagent CirculatedReagent;
+
+	[Tooltip("This is the reagent mix that the blood is composed of.")]
+	public ReagentMix CompleteBloodMix;
+
+	[Tooltip("The color of this bloodtype.")]
+	public Color Color;
+}
