9c3366b9d59779f0d47c0302f4e6575ec8a76ae8
Fortune <fortune11709@gmail.com>
2020-09-04 10:04:55 +1000

    Restructured body part containers to represent the layer system
    I've mostly done this commit just to save progress because ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartMaster : MonoBehaviour
+{
+	private BodyPartContainerExternal externalLayer;
+	private BodyPartContainerStructure structuerLayer;
+	private BodyPartContainerInternal internalLayer;
+
+	private void Awake()
+	{
+		externalLayer = GetComponent<BodyPartContainerExternal>();
+		structuerLayer = GetComponent<BodyPartContainerStructure>();
+		internalLayer = GetComponent<BodyPartContainerInternal>();
+	}
+}
