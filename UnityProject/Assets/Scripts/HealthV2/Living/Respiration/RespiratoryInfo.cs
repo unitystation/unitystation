4e715d0377d5c971e280eb6aac9bd5fd846447ed
Fortune <fortune11709@gmail.com>
2020-09-06 06:54:08 +1000

    I dont remember, lots of changes

--- UnityProject/Assets/Scripts/HealthV2/Living/Respiration/RespiratoryInfo.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/Respiration/RespiratoryInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Systems.Atmospherics;
+using Atmospherics;
 using NaughtyAttributes;
 using UnityEngine;
 
--- UnityProject/Assets/Scripts/ScriptableObjects/Implants/ImplantProperty.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/Implants/ImplantProperty.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace HealthV2
+public abstract class ImplantProperty : ScriptableObject
 {
-	public abstract class ImplantProperty : ScriptableObject
-	{
-		public abstract void ImplantUpdate(BodyPart implant, LivingHealthMasterBase healthMaster);
-	}
-
+	public abstract void ImplantUpdate(ImplantBase implant, LivingHealthMasterBase healthMaster);
 }
