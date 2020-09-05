bfa3bfbf547e2ec52c516535b869bb376b141226
Fortune <fortune11709@gmail.com>
2020-09-06 06:54:08 +1000

    I dont remember, lots of changes

--- UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartContainerBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartContainerBase.cs
@@ -1,8 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using Objects;
 
-public class BodyPartContainerBase : ItemStorage
+public class BodyPartContainerBase : MonoBehaviour, IServerInventoryMove
 {
+	[SerializeField]
+	[Required("Need a health master to send updates too.")]
+	private LivingHealthMasterBase healthMaster = null;
+
+	private ItemStorage storage;
+	private void Awake()
+	{
+		storage = GetComponent<ItemStorage>();
+	}
+
+	public void OnInventoryMoveServer(InventoryMove info)
+	{
+		//If the implant is being added, or transfered to the storage.
+		if (info.InventoryMoveType == InventoryMoveType.Add || (info.InventoryMoveType == InventoryMoveType.Transfer && storage.HasSlot(info.ToSlot.SlotIdentifier)))
+		{
+			//Note: This assumes the moved item will have the implant base component.
+			//I would not normally make this kind of assumption, especially one without any checks.
+			//However, if this happens and returns a null component, that really is an error. In which case
+			//we want to be logging it as such anyway.
+			healthMaster.AddNewImplant(info.MovedObject.GetComponent<ImplantBase>());
+		}
+		else //I'm pretty sure the only other option is removal?
+		{
+			healthMaster.RemoveImplant(info.MovedObject.GetComponent<ImplantBase>());
+		}
+	}
 }
---
+++ UnityProject/Assets/Scripts/HealthV2/Living/PlayerHealthV2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthV2 : LivingHealthMasterBase
+{
+	public PlayerMove PlayerMove;
+	private PlayerSprites playerSprites;
+
+	private PlayerNetworkActions playerNetworkActions;
+	/// <summary>
+	/// Cached register player
+	/// </summary>
+	private RegisterPlayer registerPlayer;
+
+	private ItemStorage itemStorage;
+
+	private bool init = false;
+
+	public override void Awake()
+	{
+		base.Awake();
+		EnsureInit();
+	}
+
+
+	void EnsureInit()
+	{
+		if (init) return;
+
+		init = true;
+		playerNetworkActions = GetComponent<PlayerNetworkActions>();
+		PlayerMove = GetComponent<PlayerMove>();
+		playerSprites = GetComponent<PlayerSprites>();
+		registerPlayer = GetComponent<RegisterPlayer>();
+		itemStorage = GetComponent<ItemStorage>();
+
+		OnConsciousStateChangeServer.AddListener(OnPlayerConsciousStateChangeServer);
+	}
+
+	public override void OnStartClient()
+	{
+		EnsureInit();
+		base.OnStartClient();
+	}
+
+	public override void OnStartServer()
+	{
+		EnsureInit();
+		base.OnStartServer();
+	}
+
+	private void OnPlayerConsciousStateChangeServer(ConsciousState oldState, ConsciousState newState)
+	{
+		if (playerNetworkActions == null || registerPlayer == null) EnsureInit();
+
+		if (isServer)
+		{
+			playerNetworkActions.OnConsciousStateChanged(oldState, newState);
+		}
+
+		//we stay upright if buckled or conscious
+		registerPlayer.ServerSetIsStanding(newState == ConsciousState.CONSCIOUS || PlayerMove.IsBuckled);
+	}
+
+}
