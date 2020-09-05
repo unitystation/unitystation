fda8c36f5325744cf37b4946178ca8d01e712998
Fortune <fortune11709@gmail.com>
2020-09-06 06:54:08 +1000

    I dont remember, lots of changes

--- UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/BloodTypes/BloodType.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/BloodTypes/BloodType.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Chemistry;
 
-<<<<<<< HEAD
 [CreateAssetMenu(fileName = "BloodType", menuName = "ScriptableObjects/Health/BloodType", order = 0)]
 public class BloodType : ScriptableObject
 {
@@ -12,15 +11,6 @@ public class BloodType : ScriptableObject
 
 	[Tooltip("This is the reagent mix that the blood is composed of.")]
 	public Chemistry.ReagentMix CompleteBloodMix;
-=======
-public class BloodType : ScriptableObject
-{
-	[Tooltip("This is the reagent actually metabolised and circulated through this circulatory system.")]
-	public Reagent CirculatedReagent;
-
-	[Tooltip("This is the reagent mix that the blood is composed of.")]
-	public ReagentMix CompleteBloodMix;
->>>>>>> f6fdd9fe97... Initial Commit to Save Progress
 
 	[Tooltip("The color of this bloodtype.")]
 	public Color Color;
--- UnityProject/Assets/Scripts/Items/Implants/ImplantOrgan.cs
+++ UnityProject/Assets/Scripts/Items/Implants/ImplantOrgan.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< HEAD
 namespace HealthV2
 {
 	public class ImplantOrgan : ImplantBase
@@ -19,4 +20,19 @@ namespace HealthV2
 		}
 	}
 
+=======
+public class ImplantOrgan : ImplantBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+>>>>>>> bea84a13e1... I dont remember, lots of changes
 }
