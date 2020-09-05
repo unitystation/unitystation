b44a91f8da98e1efb811137627122087e97537bb
Fortune <fortune11709@gmail.com>
2020-09-06 06:54:08 +1000

    I dont remember, lots of changes

--- UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/CirculatorySystemBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/CirculatorySystem/CirculatorySystemBase.cs
@@ -20,8 +20,11 @@ namespace HealthV2
 		private float bloodReagentAmount;
 		public float BloodReagentAmount => bloodReagentAmount;
 
+		private float bloodAmount = 0;
+		public float BloodAmount => bloodAmount;
+
 		//The actual reagent our blood uses.
-		public Reagent BloodReagent => bloodType.CirculatedReagent;
+		public Chemistry.Reagent BloodReagent => bloodType.CirculatedReagent;
 
 		//This is the list of the toxins in the circulatory system.
 		//Different toxins can have different effects on different parts of the body.
@@ -35,11 +38,26 @@ namespace HealthV2
 		private CirculatoryInfo bloodInfo = null;
 		public CirculatoryInfo BloodInfo => bloodInfo;
 
+		//For every point of strength that the heart has, this is how much blood flow the body will receive.
+		//In the human body, it takes about 45 seconds for blood to fully circulate through your body.
+		//This means that at 0.95f blood flow, the entirety of a humans blood (about 5L) would be cycled in  ~50 heart beats.
+		private float heartStrengthRatio = 0.95f;
+
 		//Now, this is useful, but it isn't actually set here. This will need to be modified by an organ.
 		//So I need to remember that! It may be zero the frame the player is spawned in.
 		//Probably update this each tick. Set to zero if there is no update from organs.
 		private float heartRate = 0;
-		public float HeartRate => heartRate;
+
+		//This bool is to let the heart organs know that they need to increase their heartrate.
+		private bool heartRateNeedsIncreasing = false;
+
+		public bool HeartRateNeedsIncreasing => heartRateNeedsIncreasing;
+
+		public float HeartRate
+		{
+			get => heartRate;
+			set => heartRate = value;
+		}
 
 		public bool HeartIsStopped => heartRate <= 0;
 
@@ -48,14 +66,30 @@ namespace HealthV2
 		private void Awake()
 		{
 			healthMaster = GetComponent<LivingHealthMasterBase>();
-			bloodReagentAmount = BloodInfo.BLOOD_REAGENT_NORMAL;
+			bloodReagentAmount = BloodInfo.BLOOD_REAGENT_DEFAULT;
+			bloodAmount = BloodInfo.BLOOD_DEFAULT;
 		}
 
 		//This circulates blood around the body.
 		//This isn't actually called by the circulatory system, it is going to be activated by an organ pumping blood.
-		public void PumpBlood()
+		public virtual void HeartBeat(float strength)
 		{
-			//TODO: The whole thing. Need to start working on organs now.
+			float initialPumpAmount = (bloodReagentAmount / bloodAmount) * (heartStrengthRatio * strength);
+			float pumpedReagent = initialPumpAmount;
+			foreach (ImplantBase implant in healthMaster.ImplantList)
+			{
+				pumpedReagent = Math.Max(implant.BloodPumpedEvent(bloodType.CirculatedReagent, pumpedReagent), 0);
+			}
+
+			if (pumpedReagent <= 0)
+			{
+				heartRateNeedsIncreasing = true;
+			}
+
+			float usedReagent = initialPumpAmount - pumpedReagent;
+			bloodReagentAmount -= usedReagent + bloodInfo.BLOOD_REAGENT_CONSUME_PER_BEAT;
+			bloodReagentAmount = Mathf.Max(bloodReagentAmount, 0);
+
 		}
 
 	}
