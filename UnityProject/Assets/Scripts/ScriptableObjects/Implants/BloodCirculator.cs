d1ff9b57306239ed43dd1055501c1e7fc8827d7e
Fortune <fortune11709@gmail.com>
2020-09-06 06:54:08 +1000

    I dont remember, lots of changes

--- UnityProject/Assets/Scripts/ScriptableObjects/Implants/BloodCirculator.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/Implants/BloodCirculator.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
 using HealthV2;
+=======
+>>>>>>> bea84a13e1... I dont remember, lots of changes
 
 public class BloodCirculator : ImplantProperty
 {
