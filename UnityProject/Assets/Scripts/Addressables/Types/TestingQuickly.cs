33d570e1c5e21b1dbc637231cfb2be78e8291ad7
Bod9001 <27101670+Bod9001@users.noreply.github.com>
2020-09-12 20:15:47 +0100

    added settings and added Generic AddressableReference

---
+++ UnityProject/Assets/Scripts/Addressables/Types/TestingQuickly.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingQuickly : MonoBehaviour
+{
+	public AddressableReferences.AddressableSprite Reference;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
