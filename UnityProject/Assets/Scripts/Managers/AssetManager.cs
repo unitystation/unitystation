19296834962022761fb4422a77acfa3a9a1abf85
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2020-09-13 14:37:26 +0200

    Readded asset manager.

---
+++ UnityProject/Assets/Scripts/Managers/AssetManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AssetManager : MonoBehaviour
+{
+	private static AssetManager _assetManager;
+
+	public static AssetManager Instance
+	{
+		get
+		{
+			if (_assetManager == null)
+			{
+				_assetManager = FindObjectOfType<AssetManager>();
+			}
+
+			return _assetManager;
+		}
+	}
+
+	public void LoadAssetFromReference<T>(AssetReference reference, System.Action<AsyncOperationHandle<T>> callback)
+	{
+		Addressables.LoadAssetAsync<T>(reference).Completed += callback;
+	}
+
+	public void LoadAssetFromPath<T>(string path, System.Action<AsyncOperationHandle<T>> callback)
+	{
+		Addressables.LoadAssetAsync<T>(path).Completed += callback;
+	}
+
+	//public ConnectToRemoteHostingService(string URL)
+	//{
+
+	//}
+}
