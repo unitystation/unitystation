f5f883e644cb7964b7c053cd6367c68dbb0252ca
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2020-09-13 18:16:55 +0200

    Added a new method to AddressableReference to pass handle to assetmanager. AssetManager will now count amount of references being loaded and activate/deactivate loading screen. AssetLoadingPopup will report that it's done loading to the AssetManager.

--- UnityProject/Assets/Scripts/Managers/AssetLoadingPopupManager.cs
+++ UnityProject/Assets/Scripts/Managers/AssetLoadingPopupManager.cs
@@ -7,6 +7,7 @@ public class AssetLoadingPopupManager : MonoBehaviour
 {
 	[SerializeField] private GameObject popupPrefab;
 	[SerializeField] private RectTransform popupHolder;
+
 	public void AddAssetLoadingPopup(AsyncOperationHandle handle, string path)
 	{
 		var popup = Instantiate(popupPrefab, popupHolder);
