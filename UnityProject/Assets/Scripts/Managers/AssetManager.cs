8e056e415977a6843f4b37032ba05b0a29092b58
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2020-09-13 15:42:57 +0200

    Refactored assetmanager and addressablereference to pass along asyncoperationhandle.

--- UnityProject/Assets/Scripts/Managers/AssetManager.cs
+++ UnityProject/Assets/Scripts/Managers/AssetManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using AddressableReferences;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -21,15 +23,25 @@ public class AssetManager : MonoBehaviour
 		}
 	}
 
-	public void LoadAssetFromReference<T>(AssetReference reference, System.Action<AsyncOperationHandle<T>> callback)
+	//public async Task LoadAssetFromReference<T>(AddressableReference<T> reference) where T : UnityEngine.Object
+	//{
+	//	var loadTask = reference.Load();
+	//}
+
+	public void AddLoadingAssetHandle(AsyncOperationHandle handle)
 	{
-		Addressables.LoadAssetAsync<T>(reference).Completed += callback;
+		// pass to asset loading in order to show the player what is currently downloading.
 	}
 
-	public void LoadAssetFromPath<T>(string path, System.Action<AsyncOperationHandle<T>> callback)
-	{
-		Addressables.LoadAssetAsync<T>(path).Completed += callback;
-	}
+	//public void LoadAssetFromReference<T>(AssetReference reference, System.Action<AsyncOperationHandle<T>> callback)
+	//{
+	//	Addressables.LoadAssetAsync<T>(reference).Completed += callback;
+	//}
+
+	//public void LoadAssetFromPath<T>(string path, System.Action<AsyncOperationHandle<T>> callback)
+	//{
+	//	Addressables.LoadAssetAsync<T>(path).Completed += callback;
+	//}
 
 	//public ConnectToRemoteHostingService(string URL)
 	//{
