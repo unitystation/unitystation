5327af27288b251726e9c1ca8acdea1115619b12
Fortune <fortune11709@gmail.com>
2020-09-21 05:04:59 +1000

    Limbs can now be swapped out and replaced

--- UnityProject/Assets/Scripts/Items/ClothingItem.cs
+++ UnityProject/Assets/Scripts/Items/ClothingItem.cs
@@ -142,7 +142,8 @@ public class ClothingItem : MonoBehaviour
 				return;
 			}
 
-			equippedClothing.LinkClothingItem(this);
+				// Set the slots defined in hidesSlots as hidden.
+				//thisPlayerScript.Equipment.obscuredSlots |= equippedClothing.HiddenSlots;
 
 			// Set the slots defined in hidesSlots as hidden.
 			thisPlayerScript.Equipment.obscuredSlots |= equippedClothing.HiddenSlots;
