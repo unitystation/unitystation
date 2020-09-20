5279ebc13d45bac79f41f1840d5b7d64a2531a3a
Fortune <fortune11709@gmail.com>
2020-09-20 14:42:04 +1000

    Implants Working, Starting Limb Rendering

--- UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartSprites.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPartSprites.cs
@@ -1,92 +1,40 @@
-﻿using Systems.Clothing;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using HealthV2;
-using Mirror;
-using Newtonsoft.Json;
 using UnityEngine;
 
 /// <summary>
 /// This is used to contain data about the basic rendering of a body part.
 /// It will have info about what to render when there's no limb, the position of the rendering, etc.
 /// </summary>
-public class BodyPartSprites : NetworkBehaviour
+public class BodyPartSprites : MonoBehaviour
 {
-	[SerializeField] public SpriteHandler baseSpriteHandler;
+	[SerializeField]
+	private SpriteHandler baseSpriteHandler;
 
-	[SerializeField] public BodyPartType bodyPartType;
+	[SerializeField]
+	private SpriteHandler baseOverlaySpriteHandler;
 
-	public SpriteRenderer spriteRenderer;
+	[SerializeField]
+	private SpriteHandler damageOverlaySpriteHandler;
 
-	public CharacterSettings ThisCharacter;
-
-	public SpriteOrder SpriteOrder;
-
-	public ClothingHideFlags ClothingHide;
-
-	[SyncVar(hook = nameof(UpdateData))]
-	private string Data;
-
-	public void UpdateData(string InOld, string InNew)
+	public void UpdateSpritesForImplant(ImplantBase implant)
 	{
-		Data = InNew;
-		if (CustomNetworkManager.Instance._isServer) return;
-		SpriteOrder = JsonConvert.DeserializeObject<SpriteOrder>(Data);
-		SpriteOrder.Orders.RemoveRange(0, 4);
+		if (implant.LimbSpriteData)
+		{
+			baseSpriteHandler.SetSpriteSO(implant.LimbSpriteData);
+		}
+
+		if (implant.LimbOverlaySpriteData)
+		{
+			baseOverlaySpriteHandler.SetSpriteSO(implant.LimbOverlaySpriteData);
+		}
+
 	}
 
-	public virtual void UpdateSpritesForImplant(BodyPart implant,ClothingHideFlags INClothingHide, SpriteDataSO Sprite, RootBodyPartContainer rootBodyPartContainer, SpriteOrder _SpriteOrder = null)
-	{
-		if (baseSpriteHandler == null) return;
-		ClothingHide = INClothingHide;
-		UpdateData("", JsonConvert.SerializeObject(_SpriteOrder));
-		//baseSpriteHandler.name = baseSpriteHandler.name + implant.name;
-		baseSpriteHandler.SetSpriteSO(Sprite, Color.white);
-		SpriteOrder = _SpriteOrder;
-		if (SpriteOrder != null)
-		{
-			if (SpriteOrder.Orders.Count > 0)
-			{
-				spriteRenderer.sortingOrder = SpriteOrder.Orders[0];
-			}
-		}
-	}
-
-	public virtual void OnDirectionChange(Orientation direction)
+	public void UpdateSpritesOnImplantRemoved(ImplantBase implant)
 	{
 
-		int referenceOffset = 0;
-
-		if (direction == Orientation.Down)
-		{
-			referenceOffset = 0;
-		}
-
-		if (direction == Orientation.Up)
-		{
-			referenceOffset = 1;
-		}
-
-		if (direction == Orientation.Right)
-		{
-			referenceOffset = 2;
-		}
-
-		if (direction == Orientation.Left)
-		{
-			referenceOffset = 3;
-		}
-
-		if (SpriteOrder != null)
-		{
-			if (SpriteOrder.Orders.Count > referenceOffset)
-			{
-				spriteRenderer.sortingOrder = SpriteOrder.Orders[referenceOffset];
-			}
-		}
-
-		baseSpriteHandler.ChangeSpriteVariant(referenceOffset);
 	}
-
-
-
-
 }
