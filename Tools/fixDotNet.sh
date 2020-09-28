44549e6cc1e68de8e56055dbf15efc06364b22c2
Bod9001 <27101670+Bod9001@users.noreply.github.com>
2020-09-28 18:40:27 +0100

    This work?

--- Tools/fixDotNet.sh
+++ Tools/fixDotNet.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 FROM node
 RUN curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin \
 && /root/.dotnet/dotnet tool install --global PowerShell
@@ -6,4 +7,4 @@ ENV PATH="/root/.dotnet:/root/.dotnet/tools:${PATH}"
 #export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
 ENV DOTNET_ROOT="/root/.dotnet"
 SHELL ["pwsh"]
-ENTRYPOINT ["pwsh"]
+ENTRYPOINT ["pwsh"] 
