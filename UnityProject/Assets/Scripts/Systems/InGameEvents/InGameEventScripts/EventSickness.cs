4c72de0e53301ca8275532a4e92115387a7b88fb
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2020-10-12 16:57:04 +0100

    TODO

--- UnityProject/Assets/Scripts/Systems/InGameEvents/InGameEventScripts/EventSickness.cs
+++ UnityProject/Assets/Scripts/Systems/InGameEvents/InGameEventScripts/EventSickness.cs
@@ -35,8 +35,8 @@ namespace InGameEvents
 			SicknessEventParameters sicknessEventParameters;
 
 			//If null means its a server random event so has no default parameters
-			//Todo: when infection system is more deadly change random effects based on player numbers
-			//Todo: eg more players == more chance of deadlier disease?
+			//TODO: when infection system is more deadly change random effects based on player numbers
+			//TODO: eg more players == more chance of deadlier disease?
 			if (serializedEventParameters == null)
 			{
 				sicknessEventParameters = new SicknessEventParameters()
