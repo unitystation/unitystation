4acd5082905060b40e490a71882a18d6f1e7f7c8
NoooneyDude <noooneydude@gmail.com>
2020-10-27 22:05:36 +1300

    Add spell books (fireball, forcewall)

---
+++ UnityProject/Assets/Scripts/Items/Others/Magical/SpellBooks/FireballPunishment.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Items.Others.Magical
+{
+	/// <summary>
+	/// Creates an explosion centered on the player.
+	/// </summary>
+	public class FireballPunishment : SpellBookPunishment
+	{
+		[SerializeField]
+		private GameObject explosionPrefab = default;
+
+		public override void Punish(ConnectedPlayer player)
+		{
+			Spawn.ServerPrefab(explosionPrefab, player.Script.WorldPos);
+		}
+	}
+}
