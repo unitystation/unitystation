b5643fb81a2535c5c525675602c59ed5eec3e345
the-other-autumn <55225562+the-other-autumn@users.noreply.github.com>
2020-11-07 18:22:02 +1100

    guntrigger additions

--- UnityProject/Assets/Scripts/Items/Weapons/GunTrigger.cs
+++ UnityProject/Assets/Scripts/Items/Weapons/GunTrigger.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-
+using Mirror;
 
 
 namespace Weapons
@@ -12,6 +12,8 @@ namespace Weapons
         private Gun gun;
         [SerializeField]
         private bool allowClumsy;
+        [SerializeField]
+        private bool allowNonClumsy;
         private System.Random rnd = new System.Random();
 
         public void Awake()
@@ -19,12 +21,13 @@ namespace Weapons
             gun = GetComponent<Gun>();
         }
 
+        [Server]
         public void TriggerPull(GameObject shotBy, Vector2 target,
 			BodyPartType damageZone, bool isSuicideShot)
         {
             JobType job = PlayerList.Instance.Get(shotBy).Job;
             if (PlayerList.Instance.Get(shotBy).Job == setRestriction || (setRestriction == JobType.NULL && 
-            (job != JobType.CLOWN && !allowClumsy || job == JobType.CLOWN && allowClumsy)))
+            (job != JobType.CLOWN && allowNonClumsy || job == JobType.CLOWN && allowClumsy)))
             {
     			gun.ServerShoot(shotBy , target, damageZone, isSuicideShot);
             }
@@ -33,24 +36,55 @@ namespace Weapons
                 int chance = rnd.Next(0 ,2);
                 if (chance == 0)
                 {
-    		   	gun.ServerShoot(shotBy , target, damageZone, true);
-                Chat.AddActionMsgToChat(
-	            shotBy,
-			    "You fumble up and shoot yourself!",
-			    $"{shotBy.ExpensiveName()} fumbles up and shoots themself!");
+    		   	    gun.ServerShoot(shotBy , target, damageZone, true);
+                    Chat.AddActionMsgToChat(
+	                shotBy,
+			        "You fumble up and shoot yourself!",
+			        $"{shotBy.ExpensiveName()} fumbles up and shoots themself!");
                 }
                 else
                 {
             	    gun.ServerShoot(shotBy , target, damageZone, isSuicideShot);
                 }
             }
-            else if (setRestriction == JobType.NULL && (job != JobType.CLOWN && allowClumsy))
+            else if (setRestriction == JobType.NULL && (job != JobType.CLOWN && !allowNonClumsy))
             {
     		   	gun.ServerShoot(shotBy , target, damageZone, true);
                 Chat.AddActionMsgToChat(
 				shotBy,
 				"You somehow shoot yourself in the face! How the hell?!",
 				$"{shotBy.ExpensiveName()} somehow manages to shoot themself in the face!");
+            }
+            else
+            {
+                Chat.AddExamineMsgToClient($"The {gameObject.ExpensiveName()} displays \'User authentication failed\'");
+            }
+       }
+
+        public void TriggerPullClient(GameObject shotBy, Vector2 target,
+			BodyPartType damageZone, bool isSuicideShot, string proj, int quant)
+        {
+            JobType job = PlayerList.Instance.Get(shotBy).Job;
+            if (PlayerList.Instance.Get(shotBy).Job == setRestriction || (setRestriction == JobType.NULL && 
+            (job != JobType.CLOWN && allowNonClumsy || job == JobType.CLOWN && allowClumsy)))
+            {
+                gun.DisplayShot(shotBy, target, damageZone, isSuicideShot, proj, quant);
+            }
+            else if (setRestriction == JobType.NULL && (job == JobType.CLOWN && !allowClumsy))
+            {
+                int chance = rnd.Next(0 ,2);
+                if (chance == 0)
+                {
+                    gun.DisplayShot(shotBy, target, damageZone, true, proj, quant);
+                }
+                else
+                {
+                    gun.DisplayShot(shotBy, target, damageZone, isSuicideShot, proj, quant);        	    
+                }
+            }
+            else if (setRestriction == JobType.NULL && (job != JobType.CLOWN && !allowNonClumsy))
+            {
+                gun.DisplayShot(shotBy, target, damageZone, true, proj, quant);
             }
             else
             {
