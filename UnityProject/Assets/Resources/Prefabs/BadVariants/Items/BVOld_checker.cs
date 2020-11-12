620c0edc2db76a9ad4aff9cd3c4267375c80a2f1
Aranclanos <Aranclanos@gmail.com>
2020-11-12 16:55:21 -0300

    Removes the "BVOld_" name spam from map objects.

--- UnityProject/Assets/Resources/Prefabs/BadVariants/Items/BVOld_checker.cs
+++ UnityProject/Assets/Resources/Prefabs/BadVariants/Items/BVOld_checker.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 public class BVOld_checker : MonoBehaviour
 {
 	void Start()
-    {
-	    gameObject.name = "BVOld_" + gameObject.name;
-    }
+	{
+		var name = gameObject.name;
+		while (name.Contains("BVOld_"))
+	    {
+		    name = name.Replace("BVOld_", "");
+	    }
+
+	    gameObject.name = "BVOld_" + name;
+	}
 
 }
