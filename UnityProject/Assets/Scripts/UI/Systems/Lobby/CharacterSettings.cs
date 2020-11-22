3b71c408baebce1c551ae44ff50d89c84045de2b
James <jamessimo@outlook.com>
2020-11-22 15:54:25 +0800

    Player Prefab V4 rework
    Limbs are now 100% coontroled by Bodypart implants (removed old cloth system)
    Bodypart prefabs control gender
    Added first pass of multi races (still WIP)
    
    Lots of issues, player is SLOW because for some reason player speed is not updating when implants are added after the playerprefab. needs investigation

--- UnityProject/Assets/Scripts/UI/Systems/Lobby/CharacterSettings.cs
+++ UnityProject/Assets/Scripts/UI/Systems/Lobby/CharacterSettings.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Lobby;
-using Newtonsoft.Json;
 using UI.CharacterCreator;
 
 /// <summary>
@@ -20,40 +17,40 @@ public class CharacterSettings
 	public const int MAX_NAME_LENGTH = 26; //Arbitrary limit, but 26 is the max the current UI can fit
 	public string Username;
 	public string Name = "Cuban Pete";
-	public BodyType BodyType = BodyType.Male;
+	public Gender Gender = Gender.Male;
 	public ClothingStyle ClothingStyle = ClothingStyle.JumpSuit;
 	public BagStyle BagStyle = BagStyle.Backpack;
 	public int Age = 22;
 	public Speech Speech = Speech.None;
+	public string HairStyleName = "None";
+	public string HairColor = "black";
+	public string EyeColor = "black";
+	public string FacialHairName = "None";
+	public string FacialHairColor = "black";
 	public string SkinTone = "#ffe0d1";
-	public List<CustomisationStorage> SerialisedBodyPartCustom;
-	public List<ExternalCustomisation> SerialisedExternalCustom;
-
-
-	public string Species = "Human";
+	public string UnderwearName = "Mankini";
+	public string SocksName = "Knee-High (Freedom)";
+	public Race Race = Race.Human;
 	public JobPrefsDict JobPreferences = new JobPrefsDict();
 	public AntagPrefsDict AntagPreferences = new AntagPrefsDict();
-
-
-	[System.Serializable]
-	public class CustomisationClass
-	{
-		public string SelectedName = "None";
-		public string Colour = "#ffffff";
-	}
-
-
-
 
 	public override string ToString()
 	{
 		var sb = new StringBuilder($"{Username}'s character settings:\n", 300);
 		sb.AppendLine($"Name: {Name}");
+		sb.AppendLine($"Gender: {Gender}");
 		sb.AppendLine($"ClothingStyle: {ClothingStyle}");
 		sb.AppendLine($"BagStyle: {BagStyle}");
 		sb.AppendLine($"Age: {Age}");
 		sb.AppendLine($"Speech: {Speech}");
+		sb.AppendLine($"HairStyleName: {HairStyleName}");
+		sb.AppendLine($"HairColor: {HairColor}");
+		sb.AppendLine($"EyeColor: {EyeColor}");
+		sb.AppendLine($"FacialHairName: {FacialHairName}");
+		sb.AppendLine($"FacialHairColor: {FacialHairColor}");
 		sb.AppendLine($"SkinTone: {SkinTone}");
+		sb.AppendLine($"UnderwearName: {UnderwearName}");
+		sb.AppendLine($"SocksName: {SocksName}");
 		sb.AppendLine($"JobPreferences: \n\t{string.Join("\n\t", JobPreferences)}");
 		sb.AppendLine($"AntagPreferences: \n\t{string.Join("\n\t", AntagPreferences)}");
 		return sb.ToString();
@@ -103,11 +100,11 @@ public class CharacterSettings
 	/// </summary>
 	public string TheirPronoun()
 	{
-		switch (BodyType)
+		switch (Gender)
 		{
-			case BodyType.Male:
+			case Gender.Male:
 				return "his";
-			case BodyType.Female:
+			case Gender.Female:
 				return "her";
 			default:
 				return "their";
@@ -119,11 +116,11 @@ public class CharacterSettings
 	/// </summary>
 	public string TheyPronoun()
 	{
-		switch (BodyType)
+		switch (Gender)
 		{
-			case BodyType.Male:
+			case Gender.Male:
 				return "he";
-			case BodyType.Female:
+			case Gender.Female:
 				return "she";
 			default:
 				return "they";
@@ -134,11 +131,11 @@ public class CharacterSettings
 	/// </summary>
 	public string ThemPronoun()
 	{
-		switch (BodyType)
+		switch (Gender)
 		{
-			case BodyType.Male:
+			case Gender.Male:
 				return "him";
-			case BodyType.Female:
+			case Gender.Female:
 				return "her";
 			default:
 				return "them";
@@ -150,11 +147,11 @@ public class CharacterSettings
 	/// </summary>
 	public string TheyrePronoun()
 	{
-		switch (BodyType)
+		switch (Gender)
 		{
-			case BodyType.Male:
+			case Gender.Male:
 				return "he's";
-			case BodyType.Female:
+			case Gender.Female:
 				return "she's";
 			default:
 				return "they're";
