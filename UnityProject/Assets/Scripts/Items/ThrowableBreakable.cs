f5274669db8bdda9d24b441dadfeae9b46b3235f
Emma3DSS <ruben9676@outlook.com>
2020-12-17 21:06:16 +1000

    Throwable breakables

---
+++ UnityProject/Assets/Scripts/Items/ThrowableBreakable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class ThrowableBreakable : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject BrokenItem = default;
+
+	private CustomNetTransform customNetTransform;
+
+	private void Start()
+	{
+		customNetTransform = GetComponent<CustomNetTransform>();
+		customNetTransform.OnThrowEnd.AddListener(MyListener);
+	}
+
+	private void OnDisable()
+	{
+		customNetTransform.OnThrowEnd.RemoveListener(MyListener);
+	}
+
+	private void MyListener(ThrowInfo info)
+	{
+		Spawn.ServerPrefab(BrokenItem, gameObject.AssumedWorldPosServer());
+		Despawn.ServerSingle(gameObject);
+		SoundManager.PlayNetworkedAtPos(SingletonSOSounds.Instance.GlassBreak01, gameObject.AssumedWorldPosServer());
+	}
+
+}
