e1d41e95d2fa12ac0fbb06337d41c6c4e395dbef
NoooneyDude <noooneydude@gmail.com>
2020-12-20 18:33:15 +1300

    Fix method name

--- UnityProject/Assets/Scripts/Items/ThrowableBreakable.cs
+++ UnityProject/Assets/Scripts/Items/ThrowableBreakable.cs
@@ -22,12 +22,12 @@ namespace Items
 		private void Start()
 		{
 			customNetTransform = GetComponent<CustomNetTransform>();
-			customNetTransform.OnThrowEnd.AddListener(MyListener);
+			customNetTransform.OnThrowEnd.AddListener(OnThrown);
 		}
 
 		private void OnDisable()
 		{
-			customNetTransform.OnThrowEnd.RemoveListener(MyListener);
+			customNetTransform.OnThrowEnd.RemoveListener(OnThrown);
 		}
 
 		private void OnThrown(ThrowInfo info)
