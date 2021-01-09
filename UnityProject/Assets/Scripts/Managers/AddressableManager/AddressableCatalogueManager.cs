e5a2c261aa0f9bde376b9c3e932ad415486d1931
Bod9001 <Bod9001@gmail.com>
2021-01-09 16:35:53 +0000

    Addressable should version properly now (#5845)
    * Addressable should version properly now
    
    * Package content update
    
    Co-authored-by: Bod9001 <27101670+Bod9001@users.noreply.github.com>

--- UnityProject/Assets/Scripts/Managers/AddressableManager/AddressableCatalogueManager.cs
+++ UnityProject/Assets/Scripts/Managers/AddressableManager/AddressableCatalogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using DatabaseAPI;
 using Initialisation;
 using Mirror;
@@ -85,7 +86,10 @@ public class AddressableCatalogueManager : NetworkBehaviour, IInitialise
 		Instance.ToloadeCount = LoadCatalogues.Count;
 		foreach (var Catalogue in LoadCatalogues)
 		{
-			var Task = Addressables.LoadContentCatalogAsync(Catalogue);
+
+			HttpClient client = new HttpClient();
+			string result = await client.GetStringAsync(Catalogue);
+			var Task = Addressables.LoadContentCatalogAsync(result);
 			await Task.Task;
 			Instance.AssetBundleDownloadDependencies(Task, RegisterComplete);
 		}
