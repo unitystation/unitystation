a9df2a30f073046b9fece91eb42eda648a0f4795
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-01-26 01:26:22 -0300

    improve plasma intoxication symptoms

--- UnityProject/Assets/Scripts/Health/RespiratoryHealth/RespiratorySystem.cs
+++ UnityProject/Assets/Scripts/Health/RespiratoryHealth/RespiratorySystem.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using Systems.Atmospherics;
 using Items;
 using Objects.Atmospherics;
+using Random = UnityEngine.Random;
 
 /// <inheritdoc />
 /// <summary>
@@ -12,7 +13,7 @@ using Objects.Atmospherics;
 public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 {
 	private const float OXYGEN_SAFE_MIN = 16;
-	private const float PLASMA_SAFE_MAX = 1;
+	private const float PLASMA_SAFE_MAX = 0.5F;//Minimum amount of plasma moles to be visible
 	public bool IsSuffocating;
 	public float temperature = 293.15f;
 	public float pressure = 101.325f;
@@ -35,6 +36,32 @@ public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 	private float breatheCooldown = 0;
 	public bool CanBreatheAnywhere { get; set; }
 
+	[SerializeField] private string[] plasmaLowYouMessages =
+	{
+		"Your nose is tingling!",
+		"You feel an urge to cough!",
+		"Your throat itches!",
+		"You sneeze."
+	};
+
+	[SerializeField] private string[] plasmaLowOthersMessages =
+	{
+		"{0} scratches {1} nose.",
+		"{0} clears {1} throat.",
+		"{0} sneezes."
+	};
+
+	[SerializeField] private string[] plasmaHighYouMessages =
+	{
+		"Your throat stings as you draw a breath!",
+		"Your throat burns as you draw a breath!"
+	};
+
+	[SerializeField] private string[] plasmaHighOthersMessages =
+	{
+		"{0} coughs.",
+		"{0} coughs frantically."
+	};
 
 	void Awake()
 	{
@@ -154,27 +181,13 @@ public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 	private float HandleBreathing(GasMix gasMix)
 	{
 		float oxygenPressure = gasMix.GetPressure(Gas.Oxygen);
-		float plasmaPressure = gasMix.GetPressure(Gas.Plasma);
+		float plasmaAmount = gasMix.GetMoles(Gas.Plasma);
 
 		float oxygenUsed = 0;
-		float plasmaDamage = 0;
 
-		if (plasmaPressure > PLASMA_SAFE_MAX)
+		if (plasmaAmount > 0)
 		{
-			plasmaDamage = (plasmaPressure - PLASMA_SAFE_MAX) / 5;
-			bloodSystem.ToxinLevel = Mathf.Clamp(bloodSystem.ToxinLevel + plasmaDamage, 0, 100);
-		
-			if (Random.value < 0.1)
-			{
-				if (plasmaPressure < 5)
-				{
-					Chat.AddActionMsgToChat(gameObject, "Your throat stings as you draw a breath", $"{gameObject.ExpensiveName()} coughs");
-				}
-				else
-				{
-					Chat.AddActionMsgToChat(gameObject, "Your throat burns as you draw a breath", $"{gameObject.ExpensiveName()} coughs frantically");
-				}
-			}
+			HandleBreathingPlasma(plasmaAmount);
 		}
 
 		if (oxygenPressure < OXYGEN_SAFE_MIN)
@@ -204,6 +217,57 @@ public class RespiratorySystem : MonoBehaviour //Do not turn into NetBehaviour
 			breatheCooldown = 4;
 		}
 		return oxygenUsed;
+	}
+
+	/// <summary>
+	/// Placeholder method to add some effects for breathing plasma. Eventually this behavior should be
+	/// handled with interfaces we can implement so different species react differently.
+	/// </summary>
+	/// <param name="plasmaAmount"></param>
+	private void HandleBreathingPlasma(float plasmaAmount)
+	{
+		// there is some plasma in the ambient but it is still safe
+		if (plasmaAmount <= PLASMA_SAFE_MAX)
+		{
+			if (DMMath.Prob(90))
+			{
+				return;
+			}
+
+			// 10% chances of message
+			var theirPronoun = gameObject.Player() != null
+				? gameObject.Player().Script.characterSettings.TheirPronoun()
+				: "its";
+			Chat.AddActionMsgToChat(
+				gameObject,
+				plasmaLowYouMessages.PickRandom(),
+				string.Format(
+					plasmaLowOthersMessages.PickRandom(),
+					gameObject.ExpensiveName(),
+					string.Format(plasmaLowOthersMessages.PickRandom(), gameObject.ExpensiveName(), theirPronoun))
+			);
+		}
+		// enough plasma to be visible and damage us!
+		else
+		{
+			var plasmaDamage = (plasmaAmount - 0.5f) * 5;
+			bloodSystem.ToxinLevel = Mathf.Clamp(bloodSystem.ToxinLevel + plasmaDamage, 0, 200);
+
+			if (DMMath.Prob(90))
+			{
+				return;
+			}
+
+			// 10% chances of message
+			var theirPronoun = gameObject.Player() != null
+				? gameObject.Player().Script.characterSettings.TheirPronoun()
+				: "its";
+			Chat.AddActionMsgToChat(
+				gameObject,
+				plasmaHighYouMessages.PickRandom(),
+				string.Format(plasmaHighOthersMessages.PickRandom(), gameObject.ExpensiveName(), theirPronoun)
+			);
+		}
 	}
 
 	private void CheckPressureDamage()
