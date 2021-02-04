bdc0677c4f5ce0d898bc973410a451d0858ec82e
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-02-04 20:50:26 -0300

    finish ancient engineer first implementation

--- UnityProject/Assets/Scripts/Managers/SubSceneManager/AdditionalSceneListSO.cs
+++ UnityProject/Assets/Scripts/Managers/SubSceneManager/AdditionalSceneListSO.cs
@@ -11,34 +11,39 @@ public class AdditionalSceneListSO : ScriptableObject
 	[InfoBox("Add your additional scenes to this list for it to be " +
 	         "spawned at runtime. Remember to also add your scene to " +
 	         "the build settings list",EInfoBoxType.Normal)]
+
+	[Scene]
 	public List<string> AdditionalScenes = new List<string>();
 
+	[Scene]
 	[Tooltip("Default Central Command scene used if no specific map is set")]
 	public List<string> defaultCentComScenes = new List<string>();
 
 	[Tooltip("List of CentCom scenes that will be picked randomly at round load unless specific map is set")]
 	public List<CentComData> CentComScenes = new List<CentComData>();
 
+	[Scene]
 	[Tooltip("List of Syndie bases that will be picked randomly at round load unless specific map is set")]
 	public List<string> defaultSyndicateScenes = new List<string>();
 
 	[Tooltip("Used to set a specific scene to load for a map")]
 	public List<SyndicateData> SyndicateScenes = new List<SyndicateData>();
 
+	[Scene]
 	[Tooltip("List of wizard scenes that will be picked randomly at round load, if wizard gamemode.")]
 	public List<string> WizardScenes = new List<string>();
 
 	[Serializable]
 	public class CentComData
 	{
-		public string CentComSceneName;
-		public string DependentScene = null;
+		[Scene] public string CentComSceneName;
+		[Scene] public string DependentScene = null;
 	}
 
 	[Serializable]
 	public class SyndicateData
 	{
-		public string SyndicateSceneName;
-		public string DependentScene = null;
+		[Scene] public string SyndicateSceneName;
+		[Scene] public string DependentScene = null;
 	}
 }
