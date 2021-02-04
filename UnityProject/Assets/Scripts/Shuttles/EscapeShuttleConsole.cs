f9a09e05203c8b5192b7d00da3f88c58817683c8
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-02-04 13:29:01 +0000

    New escape objectives

---
+++ UnityProject/Assets/Scripts/Shuttles/EscapeShuttleConsole.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Strings;
+using UnityEngine;
+
+/// <summary>
+/// Escape shuttle logic
+/// </summary>
+public class EscapeShuttleConsole : MonoBehaviour, ICheckedInteractable<HandApply>
+{
+	private bool beenEmagged;
+
+	public bool WillInteract(HandApply interaction, NetworkSide side)
+	{
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+		return Validations.HasItemTrait(interaction.HandObject, CommonTraits.Instance.Emag);
+	}
+
+	public void ServerPerformInteraction(HandApply interaction)
+	{
+		TryEmagConsole(interaction);
+	}
+
+	private void TryEmagConsole(HandApply interaction)
+	{
+		if (beenEmagged)
+		{
+			Chat.AddExamineMsgFromServer(interaction.Performer, "The shuttle has already been emagged!");
+			return;
+		}
+
+		beenEmagged = true;
+
+		Chat.AddActionMsgToChat(interaction.Performer, "You emag the shuttle console", $"{interaction.Performer.ExpensiveName()} emags the shuttle console");
+
+		if (GameManager.Instance.ShuttleSent) return;
+
+		Chat.AddSystemMsgToChat("\n\n<color=#FF151F><size=60><b>Escape Shuttle Emergency Launch Triggered!</b></size></color>\n\n",
+			MatrixManager.MainStationMatrix);
+
+		Chat.AddSystemMsgToChat("\n\n<color=#FF151F><size=60><b>Escape Shuttle Emergency Launch Triggered!</b></size></color>\n\n",
+			GameManager.Instance.PrimaryEscapeShuttle.MatrixInfo);
+
+		SoundManager.PlayNetworked(SingletonSOSounds.Instance.Notice1);
+
+		GameManager.Instance.ForceSendEscapeShuttleFromStation(10);
+	}
+}
