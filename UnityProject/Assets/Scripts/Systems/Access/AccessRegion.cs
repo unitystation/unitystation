b18a2bb4ed253bd0facbc2d7dbd79028c84eedea
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-02-08 20:43:44 -0300

    moar access progress

---
+++ UnityProject/Assets/Scripts/Systems/Access/AccessRegion.cs
@@ -0,0 +1,14 @@
+﻿namespace Systems.Access
+{
+	public enum AccessRegion
+	{
+		All = 0,
+		General = 1,
+		Security = 2,
+		Medbay = 3,
+		Research = 4,
+		Engineering = 5,
+		Supply = 6,
+		Command = 7
+	}
+}
