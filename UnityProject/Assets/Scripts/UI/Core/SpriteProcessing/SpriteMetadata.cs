77ca03cfe4ca8ec4f9f8bb31f5782776d76c131b
Copern <copern@gmail.com>
2021-02-07 21:25:49 -0800

    Adds UI sprite scaling, switch to leantween for radial menu highlighting
    Many sprites can be hard to see/read in the UI and radial. This adds in processing for sprites to gather scaling and offset data without the need to modify all textures and sprites. UI images can apply sprite scaling with an extension method by passing it the sprite. Sprite/texture metadata is created lazily and cached.
    
    Adds icon scaling for the radial menu.
    
    Switches from using CrossFadeAlpha for highlighting in the radial menu and uses leantween instead.

---
+++ UnityProject/Assets/Scripts/UI/Core/SpriteProcessing/SpriteMetadata.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI.Core.SpriteProcessing
+{
+	public readonly struct SpriteMetadata
+	{
+		public static readonly SpriteMetadata Default = new SpriteMetadata(Vector2.zero, 1f);
+
+		public Vector2 Offset { get; }
+
+		public float Scale { get; }
+
+		private SpriteMetadata(Vector2 offset, float scale)
+		{
+			Offset = offset;
+			Scale = scale;
+		}
+
+		public static SpriteMetadata Create(float texWidth, float texHeight, in Rect spriteRect, Color32[] pixels)
+		{
+			var spriteX = spriteRect.x;
+			var spriteY = spriteRect.y;
+			var xMax = spriteRect.xMax;
+			var yMax = spriteRect.yMax;
+			var left = xMax;
+			var top = spriteY;
+
+			// Start in the top left. Check left to right, top to bottom (note: texture pixel data is stored bottom to top),
+			// to find the top most and left most pixels. Loop through the x dimension using the left most non-transparent
+			// pixel as the bound.
+			for (var y = yMax; y > spriteY && left > spriteX; --y)
+			{
+				int pixelIndex = (int)((y - 1) * texWidth + spriteX);
+
+				for (var x = spriteX; x < left; ++x, ++pixelIndex)
+				{
+					if (pixels[pixelIndex].a <= 0) continue;
+
+					left = x;
+					if (y > top)
+					{
+						top = y;
+					}
+				}
+			}
+
+			var right = spriteX;
+			var bottom = yMax;
+
+			// Now start in the bottom right to find the furthest bottom and right pixels. Loop through the x dimension
+			// using the right most non-transparent pixel as the bound.
+			for (var y = spriteY; y < top && right < xMax; ++y)
+			{
+				int pixelIndex = (int)((texHeight - (yMax - y)) * texWidth + xMax - 1);
+				for (var x = xMax; x > right; --x, --pixelIndex)
+				{
+					if (pixels[pixelIndex].a <= 0) continue;
+
+					right = x;
+					if (y < bottom)
+					{
+						bottom = y;
+					}
+				}
+			}
+
+			int spriteWidth = (int)spriteRect.width;
+			int spriteHeight = (int)spriteRect.height;
+			var center = new Vector2(spriteX + spriteWidth / 2f, spriteY + spriteHeight / 2f);
+			var offsetX = (left - center.x + right - center.x) / 2;
+			var offsetY = (top - center.y + bottom - center.y) / 2;
+			var realSize = new Vector2(right - left, top - bottom);
+			var offset = new Vector2(-offsetX, offsetY);
+			var scale = realSize.x > realSize.y ? spriteWidth / realSize.x : spriteHeight / realSize.y;
+			return new SpriteMetadata(offset, Math.Max(1, scale));
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/UI/Core/SpriteProcessing/TextureMetadata.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+namespace UI.Core.SpriteProcessing
+{
+	/// <summary>
+	/// Stores sprite scaling information for a texture. Most sprites don't fill up the area of a texture and are
+	/// surrounded by transparent pixels. This class keeps data required to scale and center a sprite on a UI image
+	/// so that it gives the appearance of having no extraneous transparent pixels around it.
+	/// </summary>
+	public class TextureMetadata
+	{
+		// Largest texture I've seen is WinDoor which is 768x768. This should cover most cases. Default sprite metadata
+		// is used if the texture is larger.
+		private const int PixelCacheSize = 768;
+
+		public static readonly Color32[] PixelCache = new Color32[PixelCacheSize * PixelCacheSize];
+
+		private Dictionary<Vector2Int, SpriteMetadata> SpritesMetadata { get; } =
+			new Dictionary<Vector2Int, SpriteMetadata>();
+
+		public SpriteMetadata GetSpriteData(Sprite sprite)
+		{
+			var rect = sprite.textureRect;
+			var key = new Vector2Int((int)rect.x, (int)rect.y);
+
+			SpritesMetadata.TryGetValue(key, out var spriteData);
+			if (spriteData.Scale <= 0)
+			{
+				spriteData = CreateSpriteData(sprite.texture, rect);
+				SpritesMetadata.Add(key, spriteData);
+			}
+
+			return spriteData;
+		}
+
+		private SpriteMetadata CreateSpriteData(Texture2D texture, in Rect spriteRect)
+		{
+			if (texture.isReadable == false)
+			{
+				Logger.LogWarning(
+					$"Texture \"{texture.name}\" is not read enabled. Using default sprite metadata",
+					Category.UI);
+				return SpriteMetadata.Default;
+			}
+			var texWidth = texture.width;
+			var texHeight = texture.height;
+			var pixelCache = PixelCache;
+			if (texWidth * texHeight > pixelCache.Length)
+			{
+				return SpriteMetadata.Default;
+			}
+
+			// Using GetPixelData and native arrays to avoid garbage created from GetPixels32. Indexing a native array
+			// is slow though, so do a relatively quick copy to the pixel cache. Could avoid copying and the need for
+			// a cache with raw pointers (requires unsafe).
+			var pixels = texture.GetPixelData<Color32>(0);
+			NativeArray<Color32>.Copy(pixels, PixelCache, pixels.Length);
+			return SpriteMetadata.Create(texWidth, texHeight, spriteRect, pixelCache);
+		}
+	}
+}
