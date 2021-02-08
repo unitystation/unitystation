71d4d43c6c577b2189ceb58a73fb2f859227f378
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-02-08 20:49:07 -0300

    default thing for unity warnings

---
+++ UnityProject/Assets/Scripts/Systems/Access/AccessHolder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Items.PDA;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Systems.Access
+{
+	public class AccessHolder: MonoBehaviour
+	{
+		[SerializeField]
+		[ReorderableList]
+		[Tooltip("Assign all acceses this object should store to be checked against the relevant objects that require access.")]
+		private List<AccessRestrictions> restrictions = default;
+
+		private PDALogic pdaLogic;
+
+		// If PDA, let's get the restrictions from the ID inside PDA.
+		public List<AccessRestrictions> Restrictions => pdaLogic != null ? pdaLogic.IDCard.Restrictions : restrictions;
+
+		private void Awake()
+		{
+			pdaLogic = GetComponent<PDALogic>();
+		}
+
+		/// <summary>
+		/// Interface to update access restrictions on this access holder.
+		/// </summary>
+		/// <param name="newRestrictions">Complete list of wanted restrictions</param>
+		public void SetRestrictions(List<AccessRestrictions> newRestrictions)
+		{
+			if (pdaLogic)
+			{
+				pdaLogic.IDCard.gameObject.GetComponent<AccessHolder>().SetRestrictions(newRestrictions);
+				return;
+			}
+
+			restrictions = newRestrictions;
+		}
+	}
+}
