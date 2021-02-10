b189757bfc375d995d9cfe7d53f771377c6152eb
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-02-10 19:53:18 -0300

    attempt on networking accesss shit

--- UnityProject/Assets/Scripts/Systems/Access/AccessHolder.cs
+++ UnityProject/Assets/Scripts/Systems/Access/AccessHolder.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Items.PDA;
+using Mirror;
 using NaughtyAttributes;
 using UnityEngine;
 
 namespace Systems.Access
 {
-	public class AccessHolder: MonoBehaviour
+	public class AccessHolder: NetworkBehaviour
 	{
 		[SerializeField]
 		[ReorderableList]
@@ -18,6 +21,9 @@ namespace Systems.Access
 		private List<AccessDefinitions> minimalAccess = default;
 
 		private PDALogic pdaLogic;
+
+		private readonly SyncList<AccessDefinitions> syncedAccess = new SyncListAccess();
+		private readonly SyncList<AccessDefinitions> syncedMinimalAccess = new SyncListAccess();
 
 		public List<AccessDefinitions> Access
 		{
@@ -40,21 +46,131 @@ namespace Systems.Access
 			pdaLogic = GetComponent<PDALogic>();
 		}
 
+		private void Start()
+		{
+			syncedAccess.Callback += OnAccessListUpdated;
+			syncedMinimalAccess.Callback += OnMinimalAccessListUpdated;
+		}
+
+		public override void OnStartServer()
+		{
+			base.OnStartServer();
+			if (access.Any())
+			{
+				ServerSetAccess(access);
+			}
+
+			if (minimalAccess.Any())
+			{
+				ServerSetMinimalAccess(minimalAccess);
+			}
+		}
+
+		// ReSharper disable Unity.PerformanceAnalysis
+		private void OnAccessListUpdated(SyncList<AccessDefinitions>.Operation op, int index, AccessDefinitions oldAccess,
+			AccessDefinitions newAccess )
+		{
+			switch (op)
+			{
+				case SyncList<AccessDefinitions>.Operation.OP_ADD:
+					access.Add(newAccess);
+					minimalAccess.Add(newAccess);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_CLEAR:
+					access.Clear();
+					minimalAccess.Clear();
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_INSERT:
+					access.Insert(index, newAccess);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_REMOVEAT:
+					access.RemoveAt(index);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_SET:
+					break;
+				default:
+					Logger.LogError($"Tried to update access sync list with unexpected operation: {op}");
+					break;
+			}
+		}
+
+		private void OnMinimalAccessListUpdated(SyncList<AccessDefinitions>.Operation op, int index, AccessDefinitions oldAccess,
+			AccessDefinitions newAccess )
+		{
+			switch (op)
+			{
+				case SyncList<AccessDefinitions>.Operation.OP_ADD:
+					minimalAccess.Add(newAccess);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_CLEAR:
+					minimalAccess.Clear();
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_INSERT:
+					minimalAccess.Insert(index, newAccess);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_REMOVEAT:
+					minimalAccess.RemoveAt(index);
+					break;
+				case SyncList<AccessDefinitions>.Operation.OP_SET:
+					break;
+				default:
+					Logger.LogError($"Tried to update access sync list with unexpected operation: {op}");
+					break;
+			}
+		}
+
 		/// <summary>
-		/// Interface to update access restrictions on this access holder.
+		/// Interface to update access definitions on this access holder.
 		/// </summary>
 		/// <param name="newAccess">Complete list of wanted access</param>
-		public void SetAccess(List<AccessDefinitions> newAccess)
+		[Server]
+		public void ServerSetAccess(List<AccessDefinitions> newAccess)
 		{
 			if (pdaLogic)
 			{
-				pdaLogic.IDCard.gameObject.GetComponent<AccessHolder>().SetAccess(newAccess);
+				pdaLogic.IDCard.gameObject.GetComponent<AccessHolder>().ServerSetAccess(newAccess);
 				return;
 			}
+			syncedAccess.Clear();
+			//FIXME this looks shitty, every element will trigger the syncing call back. Is there no way to set
+			//values directly?
+			foreach (var na in newAccess)
+			{
+				syncedAccess.Add(na);
+			}
+		}
 
-			access = newAccess;
-			//TODO handle this better maybe. This means any custom access would override the lowpop list.
-			minimalAccess = newAccess;
+		/// <summary>
+		/// Interface to update minimal access for this access holder
+		/// </summary>
+		/// <param name="newAccess"></param>
+		[Server]
+		public void ServerSetMinimalAccess(List<AccessDefinitions> newAccess)
+		{
+			if (pdaLogic)
+			{
+				pdaLogic.IDCard.gameObject.GetComponent<AccessHolder>().ServerSetMinimalAccess(newAccess);
+			}
+
+			syncedMinimalAccess.Clear();
+			//FIXME this looks shitty, every element will trigger the syncing call back. Is there no way to set
+			//values directly?
+			foreach (var a in newAccess)
+			{
+				syncedMinimalAccess.Add(a);
+			}
+		}
+
+		[Server]
+		public void ServerClearAccess()
+		{
+			syncedAccess.Clear();
+		}
+
+		[Server]
+		public void ServerClearMinimalAccess()
+		{
+			syncedMinimalAccess.Clear();
 		}
 	}
 }
---
+++ UnityProject/Assets/Scripts/Systems/Access/SyncListAccess.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+
+namespace Systems.Access
+{
+	public class SyncListAccess: SyncList<AccessDefinitions>
+	{
+
+	}
+}
