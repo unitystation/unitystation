be76356be030fabf58ce9f745982488c50e6c317
dedhiadev5@gmail.com <dedhiadev5@gmail.com>
2021-02-10 11:28:50 -0800

    Adds an advice thingy.

---
+++ UnityProject/Assets/Scripts/UI/Systems/Advice/AdviceController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Advice
+{
+
+	private int experienceLevel;
+	private TriggerCondition tCondition;
+	private string Text;
+
+	public Advice(int xpLevel, TriggerCondition trigger, string t)
+	{
+		experienceLevel = xpLevel;
+		tCondition = trigger;
+		Text = t;
+	}
+}
+
+public class TriggerCondition
+{
+
+	public TriggerCondition()
+	{
+
+	}
+}
