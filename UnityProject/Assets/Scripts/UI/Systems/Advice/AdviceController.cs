0503667f5b520f6ae7f712dcd0778a579148248c
dedhiadev5@gmail.com <dedhiadev5@gmail.com>
2021-02-10 12:00:04 -0800

    Finishes advice object.

--- UnityProject/Assets/Scripts/UI/Systems/Advice/AdviceController.cs
+++ UnityProject/Assets/Scripts/UI/Systems/Advice/AdviceController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,22 +23,35 @@ public class Advice
 {
 
 	private int experienceLevel;
-	private TriggerCondition tCondition;
-	private string Text;
 
-	public Advice(int xpLevel, TriggerCondition trigger, string t)
+	/*
+	 * 1. The integer 0 on the experience level shall represent a total newbie to SS13.
+	 * 2. The integer 1 on the experience level shall represent a newbie to UnityStation.
+	 * 3. the integer 2 on the experience level shall represent someone who is fairly experienced.
+	 */
+
+	private string Text;
+	public GameObject gObject = null;
+	int type;
+
+	/*
+	 * 1. The integer 0 shall represent a held object.
+	 * 2. The integer 1 shall represent a held class of objects.
+	 * 3. The integer 2 shall represent beign near a world object.
+	 * 4. The integer 3 shall represent opening a GUI.
+	 * 5. The integer 4 shall represent hazardous areas.
+	 * 6. The integer 5 shall represent time from joining the server.
+	 * 7. The integer 6 shall represent player actions.
+	 * 8. The integer 7 shall represent nearby player health states.
+	 * 9. The integer 8 shall represent changing intents.
+	 * 10. The integer 9 shall represent switching states on an object.
+	 */
+
+	public Advice(int xpLevel, int typet, string t, GameObject gobj = null)
 	{
 		experienceLevel = xpLevel;
-		tCondition = trigger;
+		type = typet;
 		Text = t;
-	}
-}
-
-public class TriggerCondition
-{
-
-	public TriggerCondition()
-	{
-
+		gObject = gobj;
 	}
 }
