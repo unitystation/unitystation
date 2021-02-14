f8eb29369231f65b0c208e78b490d03c8b450c34
Bod9001 <Bod9001@gmail.com>
2021-02-14 19:35:13 +0000

    Hopefully fixes tiles trying to be removed before the scene is loaded  (#6026)
    * Adds limit check on sound playing
    
    * more readable distance
    
    * Hopefully fixes tiles trying to be removed before the scene is loaded
    
    * Cleaned
    
    Co-authored-by: Bod9001 <27101670+Bod9001@users.noreply.github.com>

--- UnityProject/Assets/Scripts/Messages/Server/RemoveTileMessage.cs
+++ UnityProject/Assets/Scripts/Messages/Server/RemoveTileMessage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Mirror;
 
 public class RemoveTileMessage : ServerMessage
@@ -8,11 +9,51 @@ public class RemoveTileMessage : ServerMessage
 	public bool RemoveAll;
 	public uint TileChangeManager;
 
+	public static List<delayedData> DelayedStuff = new List<delayedData>();
+
+	public struct delayedData
+	{
+		public Vector3 Position;
+		public LayerType LayerType;
+		public bool RemoveAll;
+		public uint TileChangeManager;
+		public delayedData(Vector3 inPosition, LayerType inLayerType, bool inRemoveAll, uint inTileChangeManager)
+		{
+			Position = inPosition;
+			LayerType = inLayerType;
+			RemoveAll = inRemoveAll;
+			TileChangeManager = inTileChangeManager;
+		}
+	}
 	public override void Process()
 	{
 		LoadNetworkObject(TileChangeManager);
-		var tileChangerManager = NetworkObject.GetComponent<TileChangeManager>();
-		tileChangerManager.InternalRemoveTile(Position, LayerType, RemoveAll);
+		if (NetworkObject == null)
+		{
+			DelayedStuff.Add(new delayedData(Position, LayerType, RemoveAll, TileChangeManager));
+		}
+		else
+		{
+			var tileChangerManager = NetworkObject.GetComponent<TileChangeManager>();
+			tileChangerManager.InternalRemoveTile(Position, LayerType, RemoveAll);
+			TryDoNotDoneTiles();
+		}
+	}
+
+	public void TryDoNotDoneTiles()
+	{
+		for (int i = 0; i < DelayedStuff.Count; i++)
+		{
+			NetworkObject = null;
+			LoadNetworkObject(DelayedStuff[i].TileChangeManager);
+			if (NetworkObject != null)
+			{
+				var tileChangerManager = NetworkObject.GetComponent<TileChangeManager>();
+				tileChangerManager.InternalRemoveTile(DelayedStuff[i].Position, DelayedStuff[i].LayerType, DelayedStuff[i].RemoveAll);
+				DelayedStuff.RemoveAt(i);
+				i--;
+			}
+		}
 	}
 
 	public static RemoveTileMessage Send(uint tileChangeManagerNetID, Vector3 position, LayerType layerType, bool removeAll)
