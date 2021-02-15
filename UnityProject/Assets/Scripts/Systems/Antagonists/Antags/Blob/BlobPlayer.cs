e97f4fd3b0a0d479050a3d8fa77f02c8bebe3951
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-02-15 21:56:13 +0000

    remove debug stuff

--- UnityProject/Assets/Scripts/Systems/Antagonists/Antags/Blob/BlobPlayer.cs
+++ UnityProject/Assets/Scripts/Systems/Antagonists/Antags/Blob/BlobPlayer.cs
@@ -731,7 +731,6 @@ namespace Blob
 
 			var pos = worldPos.RoundToInt();
 
-			DebugDrawBox(pos.To2Int(), new Vector2(0.8f, 0.8f), 0, Color.blue, 5);
 			var objects = Physics2D.OverlapBoxAll(pos.To2Int(), new Vector2(0.8f, 0.8f), 0, layerMask);
 
 			foreach (var hitObject in objects)
@@ -780,27 +779,6 @@ namespace Blob
 			}
 
 			return false;
-		}
-
-		void DebugDrawBox( Vector2 point, Vector2 size, float angle, Color color, float duration) {
-
-			var orientation = Quaternion.Euler(0, 0, angle);
-
-			// Basis vectors, half the size in each direction from the center.
-			Vector2 right = orientation * Vector2.right * size.x/2f;
-			Vector2 up = orientation * Vector2.up * size.y/2f;
-
-			// Four box corners.
-			var topLeft = point + up - right;
-			var topRight = point + up + right;
-			var bottomRight = point - up + right;
-			var bottomLeft = point - up - right;
-
-			// Now we've reduced the problem to drawing lines.
-			Debug.DrawLine(topLeft, topRight, color, duration);
-			Debug.DrawLine(topRight, bottomRight, color, duration);
-			Debug.DrawLine(bottomRight, bottomLeft, color, duration);
-			Debug.DrawLine(bottomLeft, topLeft, color, duration);
 		}
 
 		#endregion
