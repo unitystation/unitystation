2e1aea26b7947fa40ff40ba85fa1b257986e833f
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-02-22 19:39:18 +0200

    Cleaned up the donut script to meet code standards.

--- UnityProject/Assets/Scripts/Items/Food/Donut.cs
+++ UnityProject/Assets/Scripts/Items/Food/Donut.cs
@@ -4,62 +4,58 @@ using UnityEngine;
 
 public class Donut : Edible
 {
-    [Tooltip("The amount of brute damage healed for a player who has a secuirty role.")]
-    public int SecurityHealBruteDamage = 5;
-    private int securityHealBruteDamage => SecurityHealBruteDamage;
+	[Tooltip("The amount of brute damage healed for a player who has a secuirty role.")]
+	[SerialiezedField]
+	private int securityHealBruteDamage = 5;
 
 
-    /// <summary>
+	/// <summary>
 	/// Checks if the player eating the donut has a secuirty job and heals them if they are part of security.
-    /// If the player is not part of secuirty then nothing special will be triggered.
+	/// If the player is not part of secuirty then nothing special will be triggered.
 	/// </summary>
 	public override void Eat(PlayerScript eater, PlayerScript feeder)
 	{
-		if (CheckForJob(eater))
+		if (CheckForJob(eater)) 
 		{
-            Heal(eater);
-		    Eat(eater, feeder, NutritionLevel);
+			Heal(eater);
 		}
-		else
-		{
-		    Eat(eater, feeder, NutritionLevel);
-		}
+		Eat(eater, feeder, NutritionLevel);
 	}
 
-    /// <summary>
+	/// <summary>
 	/// Heals all body parts that have brute damage.
 	/// </summary>
     private void Heal(PlayerScript player)
     {
-        var livingHealth = player.GetComponent<LivingHealthBehaviour>();
-        foreach (BodyPartBehaviour BodyPart in livingHealth.BodyParts)
+		var livingHealth = player.GetComponent<LivingHealthBehaviour>();
+		foreach (BodyPartBehaviour BodyPart in livingHealth.BodyParts)
 		{
-            if(BodyPart.BruteDamage > 0)
-            {
-                BodyPart.HealDamage(securityHealBruteDamage, DamageType.Brute);
-            }
+			if(BodyPart.BruteDamage > 0)
+			{
+				BodyPart.HealDamage(securityHealBruteDamage, DamageType.Brute);
+			}
 		}
-    }
+	}
 
-    /// <summary>
+	/// <summary>
 	/// Checks if the player has a secuirty job.
 	/// </summary>
-    private bool CheckForJob(PlayerScript JobHolder)
-    {
-        switch (JobHolder.mind.occupation.JobType)
-        {
-            case JobType.SECURITY_OFFICER:
-                return true;
-            case JobType.HOS:
-                return true;
-            case JobType.DETECTIVE:
-                return true;
-            case JobType.WARDEN:
-                return true;
-            default:
-                return false;
-        }
-    }
+	private bool CheckForJob(PlayerScript JobHolder)
+	{
+		switch (JobHolder.mind.occupation.JobType)
+		{
+			case JobType.SECURITY_OFFICER:
+				return true;
+			case JobType.HOS:
+				return true;
+			case JobType.DETECTIVE:
+				return true;
+			case JobType.WARDEN:
+				return true;
+			default:
+				return false;
+		}
+	}
 	private void Eat(PlayerScript eater, PlayerScript feeder, int nutrition)
 	{
 		SoundManager.PlayNetworkedAtPos(eatSound, eater.WorldPos, sourceObj: eater.gameObject);
