edd17d018031ed2c839f4fa4b8c37c18a9288a82
Bod9001 <27101670+Bod9001@users.noreply.github.com>
2021-02-24 23:08:28 +0000

    Hopefully fixed dev addressable builds

--- UnityProject/Assets/Scripts/Managers/AddressableManager/AddressableCatalogueManager.cs
+++ UnityProject/Assets/Scripts/Managers/AddressableManager/AddressableCatalogueManager.cs
@@ -7,6 +7,8 @@ using System.Net.Http;
 using DatabaseAPI;
 using Initialisation;
 using Mirror;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
@@ -47,7 +49,6 @@ public class AddressableCatalogueManager : NetworkBehaviour, IInitialise
 			LoadCatalogue(cool, false);
 			return;
 		}
-
 		LoadCatalogue(cool);
 	}
 
@@ -197,7 +198,7 @@ public class AddressableCatalogueManager : NetworkBehaviour, IInitialise
 		var path = Application.dataPath.Remove(Application.dataPath.IndexOf("/Assets"));
 		//path = path + "/AddressablePackingProjects/SoundAndMusic/ServerData"; //Make OS agnostic
 		path = path + "/AddressablePackingProjects";
-		Logger.Log(path);
+		//Logger.Log(path);
 		var Directories = System.IO.Directory.GetDirectories(path);
 		var FoundFiles = new List<string>();
 		foreach (var Directori in Directories)
@@ -242,15 +243,18 @@ public class AddressableCatalogueManager : NetworkBehaviour, IInitialise
 		var pathss = Application.streamingAssetsPath + "/AddressableCatalogues";
 		var Directories = System.IO.Directory.GetDirectories(pathss);
 		var Catalogues = new List<string>();
-
 		foreach (var Directorie in Directories)
 		{
 			var newpaths = Directorie.Replace(@"\", "/");
-			var directory_info = new DirectoryInfo(newpaths);
-			var cataloguePath = newpaths + "/" + directory_info.Name + ".json";
-			if (File.Exists(cataloguePath))
+			var newDirectories = System.IO.Directory.GetFiles(newpaths);
+
+			foreach (var pathST in newDirectories)
 			{
-				Catalogues.Add(cataloguePath);
+				if (pathST.Contains(".json"))
+				{
+					Catalogues.Add(pathST);
+				}
+
 			}
 		}
 
