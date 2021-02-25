cd559634d7378b1101bf7e9c7d4cd79c9ae30569
Bod9001 <27101670+Bod9001@users.noreply.github.com>
2021-02-25 14:11:24 +0000

    Radiation penetration is now affected by tile stats and bundle is given a unique name each build to prevent collisions

--- UnityProject/Assets/Scripts/Tilemaps/Tiles/BasicTile.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Tiles/BasicTile.cs
@@ -34,6 +34,10 @@ public abstract class BasicTile : LayerTile
 	[Tooltip("Can this tile be mined?")] [FormerlySerializedAs("Mineable")] [SerializeField]
 	private bool mineable = false;
 
+
+	[Tooltip("RadiationPassability 0 = 100% Resistant")] [SerializeField]
+	public float RadiationPassability = 1;
+
 	/// <summary>
 	/// Can this tile be mined?
 	/// </summary>
