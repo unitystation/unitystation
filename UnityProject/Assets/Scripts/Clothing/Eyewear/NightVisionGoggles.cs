3a90a0beece4cda749086c70f5ee626bb2caa8a2
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-02-26 22:41:25 +0200

    Adds the ability to turn on/off goggles

--- UnityProject/Assets/Scripts/Clothing/Eyewear/NightVisionGoggles.cs
+++ UnityProject/Assets/Scripts/Clothing/Eyewear/NightVisionGoggles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Mirror;
 using CameraEffects;
 
-public class NightVisionGoggles : NetworkBehaviour, IServerInventoryMove
+public class NightVisionGoggles : NetworkBehaviour, IServerInventoryMove, ICheckedInteractable<InventoryApply>
 {
 	[SerializeField, Tooltip("How far the player will be able to see in the dark while he has the goggles on.")]
 	private Vector3 nightVisionVisibility;
@@ -12,16 +12,26 @@ public class NightVisionGoggles : NetworkBehaviour, IServerInventoryMove
 	[SerializeField, Tooltip("The default minimal visibility size.")]
 	private Vector3 defaultVisionVisibility;
 
+	[SerializeField, Tooltip("How fast will the player gain visibility?")]
+	private float visibilityAnimationSpeed = 1.50f;
+
+	private bool isOn = true;
+
+	private Pickupable pickupable;
 	
 	private void OnStartClient()
 	{
 		//(ThatDan123) : this wont work as equipment wont be sync'd in time for the check,
 		if(PlayerManager.LocalPlayerScript != null)
 		{
-			var item = PlayerManager.LocalPlayerScript.Equipment.GetClothingItem(NamedSlot.eyes).GameObjectReference;
+			var item = getWhatsOnThePlayerEyes();
 			if(item == gameObject) {enableEffect(true);}
 		}
 	}
+	private void Awake()
+		{
+			pickupable = GetComponent<Pickupable>();
+		}
 
 	//IServerInventoryMove should be replaced with IClienInventoryMove but that needs more functionality first
 	public void OnInventoryMoveServer(InventoryMove info)
@@ -46,16 +56,46 @@ public class NightVisionGoggles : NetworkBehaviour, IServerInventoryMove
 		}
 	}
 	
-	[TargetRpc]
-	private void TargetOnTakingOff(NetworkConnection target)
+	public bool WillInteract(InventoryApply interaction, NetworkSide side)
 	{
-		enableEffect(false);
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+		return true;
 	}
 
-	[TargetRpc]
-	private void TargetOnWearing(NetworkConnection target)
+	//Note : Do not merge before fixing the inventory issue
+	//Players can't take off goggles by clicking anymore, they need to remove the goggles
+	//by dragging it to their hand.
+	//someone else smarter than me should fix this.
+	public void ServerPerformInteraction(InventoryApply interaction)
 	{
-		enableEffect(true);
+		if(interaction.IsAltClick)
+		{
+			turnOnGoggles();
+		}
+
+	}
+
+	private void turnOnGoggles()
+	{
+		if(isOn == true)
+		{
+			isOn = false;
+			var item = getWhatsOnThePlayerEyes();
+			if(item == gameObject) {enableEffect(false);}
+			Chat.AddExamineMsgToClient($"You turned off the {gameObject.ExpensiveName()}.");
+		}
+		else
+		{
+			isOn = true;
+			var item = getWhatsOnThePlayerEyes();
+			if(item == gameObject) {enableEffect(true);}
+			Chat.AddExamineMsgToClient($"You turned on the {gameObject.ExpensiveName()}.");
+		}
+	}
+
+	private GameObject getWhatsOnThePlayerEyes()
+	{
+		return PlayerManager.LocalPlayerScript.Equipment.GetClothingItem(NamedSlot.eyes).GameObjectReference;
 	}
 
 	private void enableEffect(bool check)
@@ -64,13 +104,25 @@ public class NightVisionGoggles : NetworkBehaviour, IServerInventoryMove
 		if(check == true)
 		{
 			camera.GetComponent<CameraEffectControlScript>().ToggleNightVisionEffectState();
-			camera.GetComponent<CameraEffectControlScript>().AdjustPlayerVisibility(nightVisionVisibility);
+			camera.GetComponent<CameraEffectControlScript>().AdjustPlayerVisibility(nightVisionVisibility, visibilityAnimationSpeed);
 		}
 		else
 		{
 			camera.GetComponent<CameraEffectControlScript>().ToggleNightVisionEffectState();
-			camera.GetComponent<CameraEffectControlScript>().AdjustPlayerVisibility(defaultVisionVisibility);
+			camera.GetComponent<CameraEffectControlScript>().AdjustPlayerVisibility(defaultVisionVisibility, 0.1f);
 		}
+	}
+
+	[TargetRpc]
+	private void TargetOnTakingOff(NetworkConnection target)
+	{
+		if(isOn == true){enableEffect(false);}
+	}
+
+	[TargetRpc]
+	private void TargetOnWearing(NetworkConnection target)
+	{
+		if(isOn == true){enableEffect(true);}
 	}
 }
 
