df57fa93db687981241a0e64a62f79eea3e8e3fb
the-other-autumn <55225562+the-other-autumn@users.noreply.github.com>
2021-03-05 13:37:32 +1100

    this commit was brought to you by everything fucking breaking

--- UnityProject/Assets/Scripts/Systems/Antagonists/Antags/NuclearOperativeReinforcement.cs
+++ UnityProject/Assets/Scripts/Systems/Antagonists/Antags/NuclearOperativeReinforcement.cs
@@ -6,12 +6,5 @@ namespace Antagonists
 	[CreateAssetMenu(menuName="ScriptableObjects/Antagonist/NuclearOperativeReinforcement")]
 	public class NuclearOperativeReinforcement : NuclearOperative
 	{
-		public override void AfterSpawn(ConnectedPlayer player)
-		{
-			UpdateChatMessage.Send(player.GameObject, ChatChannel.Syndicate, ChatModifier.None,
-				$"We have intercepted the code for the nuclear weapon: <b>{AntagManager.SyndiNukeCode}</b>.");
-
-			AntagManager.TryInstallPDAUplink(player, 0, true);
-		}
 	}
 }
