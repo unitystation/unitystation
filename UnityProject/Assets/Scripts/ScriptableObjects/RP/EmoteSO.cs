0342623dd01f65ebbbda072ffd0c864e8bb40146
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-06 03:29:14 +0200

    maybe i'll try working on making this work with SOs later idk

---
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects")]
+public class EmoteSO : ScriptableObject
+{
+	public string emote;
+	public string viewText;
+	public string youText = "";
+
+	public Emote emoteScript;
+}
