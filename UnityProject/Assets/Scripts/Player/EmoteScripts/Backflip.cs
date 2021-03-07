3804749f54ed2de0a96ba0055db542fca1e35be7
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-07 06:08:27 +0200

    Added rotation randomness

--- UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
@@ -7,8 +7,7 @@ public class Backflip : EmoteSO
 	public override void Do(GameObject player)
     {
         RotateEffect backflipEffect = PlayerManager.LocalPlayerScript.PlayerEffectsManager.GetComponent<RotateEffect>();
-		Debug.Log(backflipEffect);
-        backflipEffect.setupEffectvars(1, 0.2f, 180);
+        backflipEffect.setupEffectvars(1, 0.2f, 180, true);
         backflipEffect.CmdStartAnimation();
         base.Do(player);
     }
