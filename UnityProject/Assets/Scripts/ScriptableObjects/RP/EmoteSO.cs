d424a1b7433e3f88b412bf4526cc6032621d3002
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-07 19:28:19 +0200

    emotes 95% done #6157
    woohoo!

--- UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
@@ -9,35 +9,47 @@ using SoundMessages;
 [CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects/RP/Emotes/BasicEmote")]
 public class EmoteSO : ScriptableObject
 {
+	//All these are public for a reason!
+	//They cannot be accssed from classes that inheirt them if they're private!
+
 	[Tooltip("Never leave this blank!")]
 	public string emoteName = "";
 
-	[SerializeField, Tooltip("The emote text viewed by others around you.")]
-	private string viewText = "did something!";
+	[Tooltip("The emote text viewed by others around you.")]
+	public string viewText = "did something!";
 
-	[SerializeField, Tooltip("The emote text viewed by you only. Leave blank if you don't want text to appear.")]
-	private string youText = "";
+	[Tooltip("The emote text viewed by you only. Leave blank if you don't want text to appear.")]
+	public string youText = "";
 
-	[SerializeField, Tooltip("A list of sounds that can be played when this emote happens.")]
-	private List<AddressableAudioSource> emoteDefaultSounds = new List<AddressableAudioSource>();
+	[Tooltip("A list of sounds that can be played when this emote happens.")]
+	public List<AddressableAudioSource> defaultSounds = new List<AddressableAudioSource>();
+
+	[Tooltip("A list of sounds for male characters.")]
+	public List<AddressableAudioSource> maleSounds = new List<AddressableAudioSource>();
+
+	[Tooltip("A list of sounds for female characters.")]
+	public List<AddressableAudioSource> femaleSounds = new List<AddressableAudioSource>();
 
 	[SerializeField, Range(0.1f, 2)]
-	private RangeAttribute pitchRange = new RangeAttribute(0.7f, 1f);
+	private RangeAttribute pitchRange = new RangeAttribute(0.7f, 1f); //This doesn't want to appear in the inspector for some reason.
+
+	[HideInInspector]
+	public List<AddressableAudioSource> audioToUse;
 
 	public virtual void Do(GameObject player)
 	{
 		Chat.AddActionMsgToChat(player, $"{youText}", $"{player.ExpensiveName()} {viewText}.");
-		playAudio(emoteDefaultSounds, player);
+		playAudio(defaultSounds, player);
 	}
 
-	private void playAudio(List<AddressableAudioSource> audio, GameObject player)
+	public void playAudio(List<AddressableAudioSource> audio, GameObject player)
 	{
 		List<AddressableAudioSource> audioList = audio;
 
 		//If there is no audio in the audio list, exit out of this function.
 		if (audioList.Count == 0)
 		{
-			Debug.LogWarning("[EmoteSO/" + $"{this.name}]" + "No audio files detected! No sounds will be played.");
+			Debug.LogWarning("[EmoteSO/" + $"{this.name}] - " + "No audio files detected!.");
 			return;
 		}
 
@@ -45,8 +57,33 @@ public class EmoteSO : ScriptableObject
 		SoundManager.PlayNetworkedAtPos(audioList.PickRandom(), player.transform.position, audioSourceParameters, polyphonic: true);
 	}
 
-	private Gender checkPlayerGender()
+	public void genderCheck(Gender gender)
+	{
+		//Add race checks later when lizard men, slime people and lusty xeno-maids get added after the new health system gets merged.
+		switch (gender)
+		{
+			case (Gender.Male):
+				audioToUse = maleSounds;
+				break;
+			case (Gender.Female):
+				audioToUse = femaleSounds;
+				break;
+			case (Gender.Neuter):
+				audioToUse = defaultSounds;
+				break;
+			default:
+				audioToUse = defaultSounds;
+				break;
+		}
+	}
+
+	public Gender checkPlayerGender()
 	{
 		return PlayerManager.LocalPlayerScript.characterSettings.Gender;
+	}
+
+	public PlayerHealth getPlayerHealth()
+	{
+		return PlayerManager.LocalPlayerScript.playerHealth;
 	}
 }
---
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/GenderedEmote.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using AddressableReferences;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using SoundMessages;
+
+[CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects/RP/Emotes/GenderedEmote")]
+public class GenderedEmote : EmoteSO
+{
+	[SerializeField]
+	private string critViewText = "screams in pain!";
+
+	private string viewText_Final;
+
+	
+	public override void Do(GameObject player)
+	{
+		Gender playerGender = checkPlayerGender();
+		PlayerHealth playerHealth = getPlayerHealth();
+		checkPlayerState(playerHealth, playerGender);
+		Chat.AddActionMsgToChat(player, $"{youText}", $"{player.ExpensiveName()} {viewText_Final}.");
+		playAudio(audioToUse, player);
+	}
+
+	private void checkPlayerState(PlayerHealth health, Gender gender)
+	{
+		HealthCheck(health);
+		genderCheck(gender);
+	}
+
+	private void HealthCheck(PlayerHealth health)
+	{
+		if (health.IsDead)
+		{
+			return;
+		}
+		if (health.IsCrit)
+		{
+			viewText_Final = critViewText;
+		}
+		else
+		{
+			viewText_Final = viewText;
+		}
+	}
+}
