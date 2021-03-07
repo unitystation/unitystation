5989beaca2c1ed3322b0403cfee98d7a66133c54
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-07 05:03:17 +0200

    ExpensiveName() is now used.

--- UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AddressableReferences;
 
 public class EmoteSO : ScriptableObject
 {
+	[Tooltip("The name of the emote that triggers it's behavior when called under /me or *, never leave it blank!")]
 	public string emoteName = "";
-	public string viewText = "did something!";
-	public string youText = "";
+	[SerializeField, Tooltip("The emote text viewed by others around you.")]
+	private string viewText = "did something!";
+	[SerializeField, Tooltip("The emote text viewed by you only. Leave blank if you don't want text to appear.")]
+	private string youText = "";
 
-	public List<AudioClip> clips;
+	[SerializeField, Tooltip("A list of sounds that can be played when this emote happens, " +
+		"Mainly used for simple emotes that don't require gender/Job checks.")]
+	private List<AddressableAudioSource> emoteDefaultSounds = new List<AddressableAudioSource>();
 
 	public virtual void Do(GameObject player)
 	{
-		Chat.AddActionMsgToChat(player, "", $"{player.name} {viewText}.");
+		Chat.AddActionMsgToChat(player, "", $"{player.ExpensiveName()} {viewText}.");
 	}
 }
