7759e4474ad5231c357b7a5265896a0a355688a5
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-09 23:49:27 +0200

    Fixed an issue that caused the rotate effect to only work on one client

--- UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
@@ -6,7 +6,12 @@ public class Backflip : EmoteSO
 {
 	public override void Do(GameObject player)
     {
-        RotateEffect backflipEffect = PlayerManager.LocalPlayerScript.PlayerEffectsManager.GetComponent<RotateEffect>();
+        RotateEffect backflipEffect = player.transform.GetComponent<PlayerScript>().PlayerEffectsManager.GetComponent<RotateEffect>();
+		if(backflipEffect == null)
+		{
+			Logger.LogError("[EmoteSO/Backflip] - Could not find a rotate effect on the player!");
+			return;
+		}
         backflipEffect.setupEffectvars(1, 0.2f, 180, true);
         backflipEffect.CmdStartAnimation();
         base.Do(player);
