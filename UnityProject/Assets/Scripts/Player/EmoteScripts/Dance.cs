a53e36273a01c6aa1fdf1875169b74080bad9f70
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-03-12 03:51:23 +0200

    Added a dance emote

---
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Dance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects/RP/Emotes/Dance")]
+public class Dance : EmoteSO
+{
+	public override void Do(GameObject player)
+	{
+		//I can't use StartCoroutine in a scriptableobject so someone else find a solution to this.
+		dance(player);
+	}
+
+	private void dance(GameObject player)
+	{
+		Directional directional = player.transform.GetComponent<Directional>();
+		PlayerMove move = player.transform.GetComponent<PlayerMove>();
+		if (move.allowInput && !move.IsBuckled)
+		{
+			//Yes I know this is stupid but Unity is stupid as well.
+			//No, I'm not making a seperate script for this because that's stupid as well.
+			directional.FaceDirection(Orientation.Up);
+			directional.FaceDirection(Orientation.Left);
+			directional.FaceDirection(Orientation.Right);
+			directional.FaceDirection(Orientation.Down);
+			directional.FaceDirection(Orientation.Up);
+			directional.FaceDirection(Orientation.Left);
+			directional.FaceDirection(Orientation.Right);
+		}
+	}
+}
--- UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/EmoteSO.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using AddressableReferences;
 using UnityEngine;
 using Random = UnityEngine.Random;
