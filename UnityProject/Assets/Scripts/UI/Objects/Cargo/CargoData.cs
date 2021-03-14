76f3c49cb6fda3e69ad8e843f4ed3e230c3a8757
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-03-13 23:08:55 -0300

    final cleanup and removal of auto price button for cargo data.

--- UnityProject/Assets/Scripts/UI/Objects/Cargo/CargoData.cs
+++ UnityProject/Assets/Scripts/UI/Objects/Cargo/CargoData.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
-using Objects;
 
 namespace Systems.Cargo
 {
@@ -10,44 +9,5 @@ namespace Systems.Cargo
 	{
 		//Stores all possible supplies broken into categories
 		public List<CargoOrderCategory> Supplies = new List<CargoOrderCategory>();
-
-		//TO-DO - comeup with clever idea for bounties
-		public int GetBounty(ObjectBehaviour item)
-		{
-			return 50;
-		}
-
-
-
-		[Button("Auto Calculate Order Price")]
-		public void AutoCalculateOrderPrice()
-		{
-			foreach (var category in Supplies)
-			{
-				foreach (var order in category.Supplies)
-				{
-					if (order.CreditsCost != 0) continue;
-
-					int newValue = 0;
-					foreach (var item in order.Items)
-					{
-						if (item.TryGetComponent<Attributes>(out var attributes) == false)
-						{
-							continue;
-						}
-
-						newValue += attributes.ExportCost;
-					}
-
-					if (order.Crate.TryGetComponent<Attributes>(out var crateValue))
-					{
-						newValue += crateValue.ExportCost;
-					}
-
-					order.CreditsCost = (int) (newValue * 1.8f);
-				}
-			}
-		}
-
 	}
 }
--- UnityProject/Assets/Tests/Balance/CargoTest.cs
+++ UnityProject/Assets/Tests/Balance/CargoTest.cs
@@ -2,6 +2,7 @@
 using Systems.Cargo;
 using NUnit.Framework;
 using UnityEditor;
+using UnityEngine;
 
 namespace Tests.Balance
 {
@@ -24,27 +25,10 @@ namespace Tests.Balance
 			{
 				foreach (var items in cargoOrderCategory.Supplies)
 				{
-					int value = 0;
-					foreach (var item in items.Items)
-					{
-						if (item == null) continue;
-
-						var attributes = item.GetComponent<Attributes>();
-
-						if (attributes != null)
-						{
-							value += attributes.ExportCost;
-						}
-					}
-
-					var crate = items.Crate.GetComponent<Attributes>();
-
-					if (crate != null)
-					{
-						value += crate.ExportCost;
-					}
+					var value = CalculateOrderValue(items);
 
 					if (value <= items.CreditsCost) continue;
+
 					report.AppendLine("Found possible cargo order exploit in: ");
 					report.AppendFormat("{0}/{1}.", cargoOrderCategory.CategoryName, items.OrderName);
 					report.AppendFormat("\nBuy price: {0} Sell price: {1}\n\n", items.CreditsCost, value);
@@ -54,5 +38,46 @@ namespace Tests.Balance
 			Logger.Log(report.ToString(), Category.Tests);
 			Assert.IsEmpty(report.ToString());
 		}
+
+		private static int CalculateOrderValue(CargoOrder order)
+		{
+			int value = 0;
+			foreach (var item in order.Items)
+			{
+				if (item == null) continue;
+				value += CalculateItemValue(item);
+			}
+
+			value += CalculateCrateValue(order);
+
+			return value;
+		}
+
+		private static int CalculateItemValue(GameObject item)
+		{
+			int value = 0;
+			var attributes = item.GetComponent<Attributes>();
+
+			if (attributes != null)
+			{
+				value += attributes.ExportCost;
+			}
+
+			return value;
+		}
+
+		private static int CalculateCrateValue(CargoOrder order)
+		{
+			int value = 0;
+			var crate = order.Crate.GetComponent<Attributes>();
+
+			if (crate != null)
+			{
+				value += crate.ExportCost;
+			}
+
+			return value;
+		}
+
 	}
 }
