fb6e74ae3bbb28efe10aa2cdc3e5d35883a7b399
Lizurt <lizard.x@mail.ru>
2021-03-29 23:05:43 +0300

    ttt

--- UnityProject/Assets/Scripts/Clothing/WearableArmor.cs
+++ UnityProject/Assets/Scripts/Clothing/WearableArmor.cs
@@ -23,16 +23,15 @@ public class WearableArmor : MonoBehaviour, IServerInventoryMove
 	public class ProtectedBodyPart
 	{
 		[SerializeField]
-		public GameObject bodyPartPrefab;
+		private BodyPartTypeBitFlag bodyPartType;
 		[SerializeField]
-		public Armor armor;
+		private Armor armor;
 
-		internal BodyPart BodyPart = null;
+		private List<BodyPart> bodyPartsCovered = new List<BodyPart>();
 	}
 
 	public void OnInventoryMoveServer(InventoryMove info)
 	{
-
 		//Wearing
 		if (info.ToSlot != null & info.ToSlot?.NamedSlot != null)
 		{
@@ -64,60 +63,63 @@ public class WearableArmor : MonoBehaviour, IServerInventoryMove
 	{
 		foreach (ProtectedBodyPart protectedBodyPart in armoredBodyParts)
 		{
+			if (protectedBodyPart.bodyPartType == BodyPartType.None)
+			{
+				continue;
+			}
+
 			foreach (RootBodyPartContainer rootBodyPartContainer in playerHealthV2.RootBodyPartContainers)
 			{
-				foreach (BodyPart bodyPart in rootBodyPartContainer.ContainsLimbs)
+				foreach (BodyPart bodyPart in rootBodyPartContainer.ContainsLimbs.Where(
+						bp => bp.BodyPartType == bodyPartsDictionary[protectedBodyPart.bodyPartType]
+					)
+				)
 				{
-					DeepUpdateBodyPartArmor(bodyPart, protectedBodyPart, currentlyRemovingArmor);
+					if (bodyPart.BodyPartType == protectedBodyPart.bodyPartType)
+					{
+					}
 				}
 			}
 		}
 	}
 
-	/// <summary>
-	/// Adds or removes armor per body part depending on the characteristics of this armor.
-	/// Checks not only the bodyPart, but also all other body parts nested in bodyPart.
-	/// </summary>
-	/// <param name="bodyPart">body part to update</param>
-	/// <param name="protectedBodyPart">a tuple of the body part associated with the body part</param>
-	/// <param name="currentlyRemovingArmor">Are we taking off our armor or putting it on?</param>
-	/// <returns>true if the bodyPart was updated, false otherwise</returns>
-	private static bool DeepUpdateBodyPartArmor(
-		BodyPart bodyPart,
-		ProtectedBodyPart protectedBodyPart,
-		bool currentlyRemovingArmor
-	)
+	[Flags]
+	private enum BodyPartTypeBitFlag
 	{
-		if (bodyPart.gameObject == protectedBodyPart.bodyPartPrefab.gameObject)
-		{
-			if (currentlyRemovingArmor)
-			{
-				bodyPart.ClothingArmor.Remove(protectedBodyPart.armor);
-				protectedBodyPart.BodyPart = null;
-			}
-			else
-			{
-				bodyPart.ClothingArmor.AddFirst(protectedBodyPart.armor);
-				protectedBodyPart.BodyPart = bodyPart;
-			}
-			return true;
-		}
-
-		if (protectedBodyPart.BodyPart.ContainBodyParts.Count == 0)
-		{
-			return false;
-		}
-
-		foreach (BodyPart innerBodyPart in protectedBodyPart.BodyPart.ContainBodyParts)
-		{
-			if (DeepUpdateBodyPartArmor(bodyPart, protectedBodyPart, currentlyRemovingArmor))
-			{
-				return true;
-			}
-		}
-
-		return false;
+		None = 0,
+		Head = 1 << 1,
+		Eyes = 1 << 2,
+		Mouth = 1 << 3,
+		Chest = 1 << 4,
+		LeftArm = 1 << 5,
+		RightArm = 1 << 6,
+		Groin = 1 << 7,
+		LeftLeg = 1 << 8,
+		RightLeg = 1 << 9,
+		RightHand = 1 << 10,
+		LeftHand = 1 << 11,
+		LeftFoot = 1 << 12,
+		RightFoot = 1 << 13
 	}
+
+	private readonly Dictionary<BodyPartTypeBitFlag, BodyPartType> bodyPartsDictionary =
+		new Dictionary<BodyPartTypeBitFlag, BodyPartType>()
+		{
+			{BodyPartTypeBitFlag.None, BodyPartType.None},
+			{BodyPartTypeBitFlag.Head, BodyPartType.Head},
+			{BodyPartTypeBitFlag.Eyes, BodyPartType.Eyes},
+			{BodyPartTypeBitFlag.Mouth, BodyPartType.Mouth},
+			{BodyPartTypeBitFlag.Chest, BodyPartType.Chest},
+			{BodyPartTypeBitFlag.LeftArm, BodyPartType.LeftArm},
+			{BodyPartTypeBitFlag.RightArm, BodyPartType.RightArm},
+			{BodyPartTypeBitFlag.Groin, BodyPartType.Groin},
+			{BodyPartTypeBitFlag.LeftLeg, BodyPartType.LeftLeg},
+			{BodyPartTypeBitFlag.RightLeg, BodyPartType.RightLeg},
+			{BodyPartTypeBitFlag.LeftHand, BodyPartType.LeftHand},
+			{BodyPartTypeBitFlag.RightHand, BodyPartType.RightHand},
+			{BodyPartTypeBitFlag.LeftFoot, BodyPartType.LeftFoot},
+			{BodyPartTypeBitFlag.RightFoot, BodyPartType.RightFoot},
+		};
 
 	/*
 	 * using System;
@@ -199,42 +201,7 @@ public class WearableArmor : MonoBehaviour, IServerInventoryMove
 		}
 	}
 
-[Flags]
-	private enum BodyPartsCovered
-	{
-		None = 0,
-		Head = 1 << 1,
-		Eyes = 1 << 2,
-		Mouth = 1 << 3,
-		Chest = 1 << 4,
-		LeftArm = 1 << 5,
-		RightArm = 1 << 6,
-		Groin = 1 << 7,
-		LeftLeg = 1 << 8,
-		RightLeg = 1 << 9,
-		RightHand = 1 << 10,
-		LeftHand = 1 << 11,
-		LeftFoot = 1 << 12,
-		RightFoot = 1 << 13
-	}
 
-	private readonly Dictionary<BodyPartsCovered, BodyPartType> bodyParts = new Dictionary<BodyPartsCovered, BodyPartType> ()
-	{
-		{BodyPartsCovered.None, BodyPartType.None},
-		{BodyPartsCovered.Head, BodyPartType.Head},
-		{BodyPartsCovered.Eyes, BodyPartType.Eyes},
-		{BodyPartsCovered.Mouth, BodyPartType.Mouth},
-		{BodyPartsCovered.Chest, BodyPartType.Chest},
-		{BodyPartsCovered.LeftArm, BodyPartType.LeftArm},
-		{BodyPartsCovered.RightArm, BodyPartType.RightArm},
-		{BodyPartsCovered.Groin, BodyPartType.Groin},
-		{BodyPartsCovered.LeftLeg, BodyPartType.LeftLeg},
-		{BodyPartsCovered.RightLeg, BodyPartType.RightLeg},
-		{BodyPartsCovered.LeftHand, BodyPartType.LeftHand},
-		{BodyPartsCovered.RightHand, BodyPartType.RightHand},
-		{BodyPartsCovered.LeftFoot, BodyPartType.LeftFoot},
-		{BodyPartsCovered.RightFoot, BodyPartType.RightFoot},
-	};
 
 }
 	 */
