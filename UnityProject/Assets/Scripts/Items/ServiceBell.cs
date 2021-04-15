0d2384a9e30b7f0f78de993832e6b3b48302f06e
Ewy_ <ewy0@protonmail.com>
2021-04-15 13:32:18 +0200

    Add logic for the elusive big bell.

--- UnityProject/Assets/Scripts/Items/ServiceBell.cs
+++ UnityProject/Assets/Scripts/Items/ServiceBell.cs
@@ -5,11 +5,18 @@ using UnityEngine.Serialization;
 
 namespace Objects
 {
-	public class ServiceBell : Pickupable
+	public class ServiceBell : Pickupable, IServerSpawn
 	{
 
 		[Tooltip("The sound the bell makes when it rings.")]
 		[SerializeField] private AddressableAudioSource RingSound = null;
+
+		[Tooltip("The additional sound for when the bell spawns as a large bell.")]
+		[SerializeField]
+		private AddressableAudioSource BigBellRingSound = null;
+
+		[SerializeField] private SpriteHandler BellSpriteRenderer;
+
 
 		public override void ServerPerformInteraction(HandApply interaction)
 		{
@@ -20,6 +27,16 @@ namespace Objects
 				return;
 			}
 			SoundManager.PlayNetworkedAtPos(RingSound, interaction.TargetObject.WorldPosServer());
+		}
+
+		public void OnSpawnServer(SpawnInfo info)
+		{
+			// Roll for the big bell
+			if (Random.value <= 0.005)
+			{
+				RingSound = BigBellRingSound;
+				BellSpriteRenderer.ChangeSpriteVariant(1);
+			}
 		}
 	}
 }
