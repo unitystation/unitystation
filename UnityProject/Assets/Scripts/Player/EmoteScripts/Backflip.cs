473a26d5290a654f20c420423052d69d4e568a64
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-18 00:38:55 +0200

    This was removed from git desktop for some reason

--- UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Backflip.cs
@@ -8,7 +8,7 @@ namespace Player.EmoteScripts
 	{
 		public override void Do(GameObject player)
 		{
-			if (CheckPlayerCritState(player) == false)
+			if (CheckPlayerCritState(player) == false && CheckIfPlayerIsCrawling(player) == true)
 			{
 				var manager = player.GetComponent<PlayerEffectsManager>();
 				manager.RotatePlayer(1, 0.2f, 180, false);
