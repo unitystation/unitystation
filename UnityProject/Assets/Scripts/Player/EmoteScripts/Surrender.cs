1d9cb581b250be0f3976820be6331ec15102ce02
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-19 05:25:52 +0200

    Fixes surrendering completely and re-enables them

--- UnityProject/Assets/Scripts/Player/EmoteScripts/Surrender.cs
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Surrender.cs
@@ -6,15 +6,40 @@ namespace Player.EmoteScripts
 	[CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects/RP/Emotes/Surrender")]
 	public class Surrender : EmoteSO
 	{
+		private RegisterPlayer registerPlayer;
+
 		public override void Do(GameObject player)
 		{
-			if(player.GetComponent<RegisterPlayer>() == null)
+			registerPlayer = player.GetComponent<RegisterPlayer>();
+
+			if (registerPlayer == null)
 			{
 				Logger.LogError("RegisterPlayer could not be found!");
 				Chat.AddActionMsgToChat(player, $"{failText}", "");
 				return;
 			}
-			if(CheckHandState(player) == false)
+			SurrenderLogic(player);
+		}
+
+		private void SurrenderLogic(GameObject player)
+		{
+			bool isCrawling = CheckIfPlayerIsCrawling(player);
+			if (CheckPlayerCritState(player) == false && isCrawling == false)
+			{
+				LayDown(player);
+			}
+			if(CheckPlayerCritState(player) == true)
+			{
+				FailText(player, FailType.Critical);
+			}
+			if(isCrawling == true)
+			{
+				base.Do(player);
+			}
+		}
+		private void LayDown(GameObject player)
+		{
+			if (CheckHandState(player) == false)
 			{
 				Chat.AddActionMsgToChat(player, "You surrender in defeat then lay faced down on the ground.", $"{player.ExpensiveName()} surrenders in defeat then lay faced down on the ground.");
 			}
@@ -22,7 +47,8 @@ namespace Player.EmoteScripts
 			{
 				base.Do(player);
 			}
-			player.GetComponent<RegisterPlayer>().ServerSetIsStanding(false);
+
+			registerPlayer.ServerSetIsStanding(false);
 		}
 	}
 }
