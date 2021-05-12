bdcb5827ba243275a0b9f1b9c64936fb43a9244d
Korywon <22848944+Korywon@users.noreply.github.com>
2021-05-11 21:54:49 -0500

    Add more documentation and typed variables

--- UnityProject/Assets/Scripts/Items/Engineering/CableCoil.cs
+++ UnityProject/Assets/Scripts/Items/Engineering/CableCoil.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
@@ -141,23 +141,38 @@ namespace Objects.Electrical
 
 					econs.Clear();
 					ElectricalPool.PooledFPCList.Add(econs);
+					// Builds the cable using the position and connections.
 					BuildCable(localPosInt, interaction.Performer.transform.parent, WireEndA, WireEndB, interaction);
+					// Consume a cable from the hand.
+					// TODO: We sometimes need to consume more than one cable coil.
+					//       Either we cut one joint at a time, or retrieve the number of
+					//       cables.
 					Inventory.ServerConsume(interaction.HandSlot, 1);
 				}
 			}
 		}
 
-		private void BuildCable(Vector3 position, Transform parent, Connection WireEndA, Connection WireEndB, ConnectionApply interaction)
+		private void BuildCable(Vector3 position, Transform parent, Connection WireEndA, Connection WireEndB,
+			ConnectionApply interaction)
 		{
 			ElectricalManager.Instance.electricalSync.StructureChange = true;
 			FindOverlapsAndCombine(position, WireEndA, WireEndB, interaction);
 		}
 
+		/// <summary>
+		/// TODO: Document this.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="WireEndA"></param>
+		/// <param name="WireEndB"></param>
+		/// <param name="interaction"></param>
 		public void FindOverlapsAndCombine(Vector3 position, Connection WireEndA, Connection WireEndB,
 			ConnectionApply interaction)
 		{
+			// If the wire ends are overlapping...
 			if (WireEndA == Connection.Overlap | WireEndB == Connection.Overlap)
 			{
+				// TODO: What is this...? Needs more documentation.
 				bool isA;
 				if (WireEndA == Connection.Overlap)
 				{
@@ -172,16 +187,23 @@ namespace Objects.Electrical
 					interaction.Performer.GetComponentInParent<Matrix>().GetElectricalConnections(position.RoundToInt());
 				List<IntrinsicElectronicData> Econns = new List<IntrinsicElectronicData>(IEnumerableEconns);
 
+				// TODO: What does this do? By this, we're removing all elements from
+				//       the list, but then adding this empty, but with-capacity list
+				//       to this.
 				IEnumerableEconns.Clear();
 				ElectricalPool.PooledFPCList.Add(IEnumerableEconns);
 
+				// First, make sure we actually found electrical connections.
 				if (Econns != null)
 				{
 					// Now, we loop through each electrical connection.
 					for (int i = 0; i < Econns.Count; i++)
 					{
+						// If the power type of the current cable matches the the
+						// connection's category...
 						if (powerTypeCategory == Econns[i].Categorytype)
 						{
+							// If the end of the wire is overlapping...
 							if (Econns[i].WireEndA == Connection.Overlap)
 							{
 								if (isA)
@@ -192,10 +214,15 @@ namespace Objects.Electrical
 								{
 									WireEndB = Econns[i].WireEndB;
 								}
+
+								// Destroy the current electrical connection.
 								Econns[i].DestroyThisPlease();
-								var tile = ElectricityFunctions.RetrieveElectricalTile(WireEndA, WireEndB,
+								// Get the electrical cable tile with the wire connection direction.
+								ElectricalCableTile tile = ElectricityFunctions.RetrieveElectricalTile(WireEndA, WireEndB,
 									powerTypeCategory);
-								interaction.Performer.GetComponentInParent<Matrix>().AddElectricalNode(position.RoundToInt(), tile, true);
+								// Then, add an electrical node at the tile.
+								interaction.Performer.GetComponentInParent<Matrix>()
+									.AddElectricalNode(position.RoundToInt(), tile, true);
 
 								return;
 							}
@@ -209,10 +236,12 @@ namespace Objects.Electrical
 								{
 									WireEndB = Econns[i].WireEndA;
 								}
+
 								Econns[i].DestroyThisPlease();
-								var tile = ElectricityFunctions.RetrieveElectricalTile(WireEndA, WireEndB,
+								ElectricalCableTile tile = ElectricityFunctions.RetrieveElectricalTile(WireEndA, WireEndB,
 									powerTypeCategory);
-								interaction.Performer.GetComponentInParent<Matrix>().AddElectricalNode(position.RoundToInt(), tile, true);
+								interaction.Performer.GetComponentInParent<Matrix>()
+									.AddElectricalNode(position.RoundToInt(), tile, true);
 
 								return;
 							}
