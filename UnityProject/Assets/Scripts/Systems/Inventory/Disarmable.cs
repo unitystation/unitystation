fb0c3736e665ea4dcdc8f73ab8efe421dc41d020
Bod9001 <Bod9001@gmail.com>
2021-05-21 16:31:54 +0100

    Uses cooldown system now

--- UnityProject/Assets/Scripts/Systems/Inventory/Disarmable.cs
+++ UnityProject/Assets/Scripts/Systems/Inventory/Disarmable.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 /// <summary>
 /// Allows an object to be disarmed by a player.
 /// </summary>
-public class Disarmable : MonoBehaviour, ICheckedInteractable<PositionalHandApply>
+public class Disarmable : MonoBehaviour, ICheckedInteractable<PositionalHandApply>, ICooldown
 {
 	// This is based off the alien/humanoid/attack_hand disarm code of TGStation's codebase.
 	// Disarms have 5% chance to knock down, then it has a 50% chance to disarm.
 
 	public float TimeBetweenDisarms = 0.5f;
+
+	public float DefaultTime => TimeBetweenDisarms;
 
 	const float KNOCKDOWN_CHANCE = 5; // Percent
 	const float DISARM_CHANCE = 50; // Percent
@@ -45,21 +47,7 @@ public class Disarmable : MonoBehaviour, ICheckedInteractable<PositionalHandAppl
 		targetName = interaction.TargetObject.ExpensiveName();
 		interactionWorldPosition = interaction.WorldPositionTarget;
 
-		foreach (var Interaction in InteractionTime)
-		{
-			if (DateTime.Now.Subtract(Interaction.Value).Seconds > TimeBetweenDisarms)
-			{
-				ToRemove.Add(Interaction.Key);
-			}
-		}
-
-		foreach (var gamobjt in ToRemove)
-		{
-			InteractionTime.Remove(gamobjt);
-		}
-		ToRemove.Clear();
-
-		if (InteractionTime.ContainsKey(interaction.Performer)) return;
+		if (Cooldowns.TryStart(interaction, this, side: NetworkSide.Server) == false) return;
 
 		var rng = new System.Random();
 		if (rng.Next(1, 100) <= KNOCKDOWN_CHANCE)
@@ -81,7 +69,6 @@ public class Disarmable : MonoBehaviour, ICheckedInteractable<PositionalHandAppl
 					$"{performerName} attempts to disarm {targetName}!");
 			Chat.AddExamineMsgFromServer(target, $"{performerName} attempts to disarm you!");
 		}
-		InteractionTime[interaction.Performer] = DateTime.Now;
 	}
 
 	private void KnockDown()
