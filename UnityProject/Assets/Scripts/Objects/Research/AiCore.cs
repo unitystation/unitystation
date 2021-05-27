1bf947b2337f6aa2946dd575fc9316c6d067f904
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-05-27 22:42:57 +0100

    comment out future code for AI core

--- UnityProject/Assets/Scripts/Objects/Research/AiCore.cs
+++ UnityProject/Assets/Scripts/Objects/Research/AiCore.cs
@@ -7,7 +7,7 @@ namespace Objects.Research
 	/// <summary>
 	/// This script controls the AI core object, for core AI job logic see AiPlayer.cs
 	/// </summary>
-	public class AiCore : MonoBehaviour, ICheckedInteractable<HandApply>
+	public class AiCore : MonoBehaviour//, ICheckedInteractable<HandApply>
 	{
 		private AiPlayer linkedPlayer;
 		public AiPlayer LinkedPlayer => linkedPlayer;
@@ -18,18 +18,19 @@ namespace Objects.Research
 			linkedPlayer = aiPlayer;
 		}
 
-		public bool WillInteract(HandApply interaction, NetworkSide side)
-		{
-			if (DefaultWillInteract.HandApply(interaction, side) == false) return false;
-
-			//if (Validations.HasItemTrait(interaction, CommonTraits.Instance)) return false;
-
-			return false;
-		}
-
-		public void ServerPerformInteraction(HandApply interaction)
-		{
-			throw new System.NotImplementedException();
-		}
+		//TODO intelicard interaction
+		// public bool WillInteract(HandApply interaction, NetworkSide side)
+		// {
+		// 	if (DefaultWillInteract.HandApply(interaction, side) == false) return false;
+		//
+		// 	//if (Validations.HasItemTrait(interaction, CommonTraits.Instance)) return false;
+		//
+		// 	return false;
+		// }
+		//
+		// public void ServerPerformInteraction(HandApply interaction)
+		// {
+		// 	throw new System.NotImplementedException();
+		// }
 	}
 }
