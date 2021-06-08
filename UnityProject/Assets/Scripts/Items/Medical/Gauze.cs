7a7f7e91e5eb74bb1b001858fca1cea5223367ae
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-06-08 18:01:03 +0200

    Adds Gauze in a namespace

--- UnityProject/Assets/Scripts/Items/Medical/Gauze.cs
+++ UnityProject/Assets/Scripts/Items/Medical/Gauze.cs
@@ -6,68 +6,71 @@ using UnityEngine;
 using Items;
 using Messages.Server.HealthMessages;
 
-public class Gauze : HealsTheLiving
+namespace Items.Medical
 {
-
-    public override void ServerPerformInteraction(HandApply interaction)
-    {
-        var LHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
-        if(CheckForBleedingLimbs(LHB, interaction))
-        {
-            RemoveLimbExternalBleeding(LHB, interaction);
-            stackable.ServerConsume(1);
-        }
-		else
-		{
-			Chat.AddExamineMsgFromServer(interaction.Performer, 
-			$"{LHB.PlayerScriptOwner.visibleName}'s {interaction.TargetBodyPart} doesn't seem to be bleeding.");
-		}
-    }
-
-    private void RemoveLimbExternalBleeding(LivingHealthMasterBase targetBodyPart, HandApply interaction)
+	public class Gauze : HealsTheLiving
 	{
-		foreach(var container in targetBodyPart.RootBodyPartContainers)
+
+		public override void ServerPerformInteraction(HandApply interaction)
 		{
-			if(container.BodyPartType == interaction.TargetBodyPart)
+			var LHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
+			if(CheckForBleedingLimbs(LHB, interaction))
 			{
-				foreach(BodyPart limb in container.ContainsLimbs)
-				{
-                    if(limb.IsBleedingExternally)
-                    {
-                        limb.StopExternalBleeding();
-                        if(interaction.Performer.Player().GameObject == interaction.TargetObject.Player().GameObject)
-                        {
-                            Chat.AddActionMsgToChat(interaction.Performer.gameObject, 
-					        $"You stopped your {interaction.TargetObject.Player().Script.visibleName}'s bleeding.",
-					        $"{interaction.PerformerPlayerScript.visibleName} stopped their own bleeding from their {interaction.TargetObject.ExpensiveName()}.");
-                        }
-                        else
-                        {
-                            Chat.AddActionMsgToChat(interaction.Performer.gameObject, 
-					        $"You stopped {interaction.TargetObject.Player().Script.visibleName}'s bleeding.",
-				            $"{interaction.PerformerPlayerScript.visibleName} stopped {interaction.TargetObject.Player().Script.visibleName}'s bleeding.");
-                        }
-                    }
-				}
+				RemoveLimbExternalBleeding(LHB, interaction);
+				stackable.ServerConsume(1);
+			}
+			else
+			{
+				Chat.AddExamineMsgFromServer(interaction.Performer, 
+				$"{LHB.PlayerScriptOwner.visibleName}'s {interaction.TargetBodyPart} doesn't seem to be bleeding.");
 			}
 		}
-	}
 
-    private bool CheckForBleedingLimbs(LivingHealthMasterBase targetBodyPart, HandApply interaction)
-	{
-		foreach(var container in targetBodyPart.RootBodyPartContainers)
+		private void RemoveLimbExternalBleeding(LivingHealthMasterBase targetBodyPart, HandApply interaction)
 		{
-			if(container.BodyPartType == interaction.TargetBodyPart)
+			foreach(var container in targetBodyPart.RootBodyPartContainers)
 			{
-				foreach(BodyPart limb in container.ContainsLimbs)
+				if(container.BodyPartType == interaction.TargetBodyPart)
 				{
-					if(limb.IsBleedingExternally)
+					foreach(BodyPart limb in container.ContainsLimbs)
 					{
-						return true;
+						if(limb.IsBleedingExternally)
+						{
+							limb.StopExternalBleeding();
+							if(interaction.Performer.Player().GameObject == interaction.TargetObject.Player().GameObject)
+							{
+								Chat.AddActionMsgToChat(interaction.Performer.gameObject, 
+								$"You stopped your {interaction.TargetObject.Player().Script.visibleName}'s bleeding.",
+								$"{interaction.PerformerPlayerScript.visibleName} stopped their own bleeding from their {interaction.TargetObject.ExpensiveName()}.");
+							}
+							else
+							{
+								Chat.AddActionMsgToChat(interaction.Performer.gameObject, 
+								$"You stopped {interaction.TargetObject.Player().Script.visibleName}'s bleeding.",
+								$"{interaction.PerformerPlayerScript.visibleName} stopped {interaction.TargetObject.Player().Script.visibleName}'s bleeding.");
+							}
+						}
 					}
 				}
 			}
 		}
-		return false;
+
+		private bool CheckForBleedingLimbs(LivingHealthMasterBase targetBodyPart, HandApply interaction)
+		{
+			foreach(var container in targetBodyPart.RootBodyPartContainers)
+			{
+				if(container.BodyPartType == interaction.TargetBodyPart)
+				{
+					foreach(BodyPart limb in container.ContainsLimbs)
+					{
+						if(limb.IsBleedingExternally)
+						{
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
 	}
 }
