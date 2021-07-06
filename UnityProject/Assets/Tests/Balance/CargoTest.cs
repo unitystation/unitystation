d301375f1d7cc2654390b7044d0ec68fb106459f
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-07-06 15:28:57 +0100

    Update CargoTest.cs

--- UnityProject/Assets/Tests/Balance/CargoTest.cs
+++ UnityProject/Assets/Tests/Balance/CargoTest.cs
@@ -29,7 +29,7 @@ namespace Tests.Balance
 					if (value < order.CreditCost)
 					{
 						//Check for within 10 percent
-						if (value > order.CreditCost * 0.9f)
+						if (value >= order.CreditCost * 0.9f)
 						{
 							report.AppendLine("Found possible cargo order exploit in: ");
 							report.AppendFormat("{0}/{1}.", category.CategoryName, order.OrderName);
