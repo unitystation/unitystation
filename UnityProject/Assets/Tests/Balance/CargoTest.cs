9e7e2db171aa645a9c494f0995fa6b93c05fc122
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-07-07 12:27:50 +0100

    Fix Some Cargo Costs (#6955)
    * Cost check for crate and within 10%
    
    * Fix cargo prices
    
    * 10%
    
    * Update CargoTest.cs
    
    * Update CargoTest.cs

--- UnityProject/Assets/Tests/Balance/CargoTest.cs
+++ UnityProject/Assets/Tests/Balance/CargoTest.cs
@@ -26,7 +26,19 @@ namespace Tests.Balance
 				foreach (var order in category.Orders)
 				{
 					var value = order.ContentSellPrice;
-					if (value <= order.CreditCost) continue;
+					if (value < order.CreditCost)
+					{
+						//Check for within 10 percent
+						if (value >= order.CreditCost * 0.9f)
+						{
+							report.AppendLine("Found possible cargo order exploit in: ");
+							report.AppendFormat("{0}/{1}.", category.CategoryName, order.OrderName);
+							report.AppendLine("The export cost is within 10% of the sell price, the items might be too cheap!");
+							report.AppendFormat("\nBuy price: {0} Sell price: {1}\n\n", order.CreditCost, value);
+						}
+
+						continue;
+					}
 
 					report.AppendLine("Found possible cargo order exploit in: ");
 					report.AppendFormat("{0}/{1}.", category.CategoryName, order.OrderName);
