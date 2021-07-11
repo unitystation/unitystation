2d3253a36b173ef8ede866e415fad61f3aec6069
Lizurt <lizard.x@mail.ru>
2021-07-11 12:42:10 +0300

    recipe.cs & ingredient.cs

---
+++ UnityProject/Assets/Scripts/Systems/CraftingV2/Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Items;
+using UnityEngine;
+
+namespace Systems.CraftingV2
+{
+	[Serializable]
+	public class Ingredient
+	{
+		[SerializeField]
+		[Min(1)]
+		private int requiredAmount = 1;
+
+		public int RequiredAmount => requiredAmount;
+
+		[SerializeField]
+		private ItemAttributesV2 requiredItem;
+
+		public ItemAttributesV2 RequiredItem => requiredItem;
+	}
+}
