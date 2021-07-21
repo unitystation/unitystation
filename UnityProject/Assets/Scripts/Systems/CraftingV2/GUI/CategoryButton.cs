9edcbe60b31313be70c2ea12716e23888a95d3af
Lizurt <lizard.x@mail.ru>
2021-07-21 14:53:38 +0300

    crafting ui template

---
+++ UnityProject/Assets/Scripts/Systems/CraftingV2/GUI/CategoryButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Systems.CraftingV2.GUI
+{
+	public class CategoryButton : MonoBehaviour, IPointerDownHandler
+	{
+		[SerializeField]
+		private GameObject selectedLine = null;
+		[SerializeField]
+		[Tooltip("This is the actual window that contains all of the" +
+		         " GUI buttons/inputs for this particular settings option")]
+		private GameObject contentWindow = null;
+		public bool IsActive => selectedLine.activeSelf;
+
+		public void OnPointerDown(PointerEventData data)
+		{
+			_ = SoundManager.Play(SingletonSOSounds.Instance.Click01);
+			CraftingMenu.Instance.ChooseCategory(this);
+		}
+
+		public void Toggle(bool activeState)
+		{
+			selectedLine.SetActive(activeState);
+			if (contentWindow) contentWindow.SetActive(activeState);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Systems/CraftingV2/GUI/CraftingMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using HealthV2;
+using Initialisation;
+using Player;
+using UnityEngine;
+
+namespace Systems.CraftingV2.GUI
+{
+	public class CraftingMenu : MonoBehaviour, IInitialise
+	{
+		public static CraftingMenu Instance;
+		//All the nav buttons in the left column
+		private CategoryButton[] categoryButtons;
+
+		void Awake()
+		{
+			if (Instance == null)
+			{
+				Instance = this;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		public InitialisationSystems Subsystem { get; }
+
+		void IInitialise.Initialise()
+		{
+			Init();
+		}
+
+
+		void Init()
+		{
+			categoryButtons = GetComponentsInChildren<CategoryButton>(true);
+			this.SetActive(false);
+		}
+
+		public void ChooseCategory(CategoryButton button)
+		{
+			foreach (CategoryButton categoryButton in categoryButtons)
+			{
+				if (categoryButton == button)
+				{
+					categoryButton.Toggle(true);
+				}
+				else
+				{
+					categoryButton.Toggle(false);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Open the Crafting Menu
+		/// </summary>
+		public void Open(PlayerCrafting playerCrafting)
+		{
+//			ChooseCategory(categoryButtons[0]);
+			this.SetActive(true);
+		}
+
+		/// <summary>
+		/// Close the Crafting Menu
+		/// </summary>
+		public void Close()
+		{
+			this.SetActive(false);
+		}
+	}
+}
