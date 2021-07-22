5248d19be8d7632c196b1a7e3e66dfe46902a8e3
Aranclanos <Aranclanos@gmail.com>
2021-07-22 15:59:46 -0300

    Changes the temp duck sprite from the syndie tc withdraw console to its proper one. Fixes TC's not being transferred to the the player's PDA. The withdraw console will now transfer 5 TC's per use. Simplifies the syndicate war console code.

--- UnityProject/Assets/Scripts/Objects/Consoles/SyndicateTelecrystalStation.cs
+++ UnityProject/Assets/Scripts/Objects/Consoles/SyndicateTelecrystalStation.cs
@@ -6,6 +6,7 @@ namespace SyndicateOps
 {
 	class SyndicateTelecrystalStation : MonoBehaviour,  ICheckedInteractable<HandApply>, IExaminable
 	{
+		private static int TransferAmount = 5;
 		public bool WillInteract(HandApply interaction, NetworkSide side)
 		{
 			if (DefaultWillInteract.Default(interaction, side)) return true;
@@ -19,9 +20,14 @@ namespace SyndicateOps
 
 		public void WithdrawTeleCrystals(HandApply interaction)
 		{
+			if (SyndicateOpConsole.Instance.TcReserve == 0)
+			{
+				Chat.AddExamineMsgFromServer(interaction.Performer, $"There are no telecrystals in reserve");
+				return;
+			}
 			if (interaction.UsedObject == null)
 			{
-				Chat.AddExamineMsgFromServer(interaction.Performer, $"It seems to have {SyndicateOpConsole.Instance.TcReserve} telecrystals in reserve");
+				Chat.AddExamineMsgFromServer(interaction.Performer, $"There are {SyndicateOpConsole.Instance.TcReserve} telecrystals in reserve");
 				return;
 			}
 			PDALogic pdaComp = interaction.UsedObject.GetComponent<PDALogic>();
@@ -29,12 +35,10 @@ namespace SyndicateOps
 			{
 				if (pdaComp.IsUplinkLocked == false)
 				{
-
-					int tc = Mathf.FloorToInt(SyndicateOpConsole.Instance.Operatives.Count / SyndicateOpConsole.Instance.TcIncrement);
-					//this is to prevent tc being unobtainable when the value above is bigger then the amount of tc left within the reserves
-					tc = Math.Min(tc, SyndicateOpConsole.Instance.TcReserve);
-					pdaComp.UplinkTC += tc;
-					SyndicateOpConsole.Instance.TcReserve -= tc;
+					var amount = Math.Min(TransferAmount, SyndicateOpConsole.Instance.TcReserve);
+					pdaComp.UplinkTC += amount;
+					SyndicateOpConsole.Instance.TcReserve -= amount;
+					Chat.AddExamineMsgFromServer(interaction.Performer, $"You successfully transfer {amount} telecrystals into the {interaction.TargetObject.ExpensiveName()}");
 				}
 				else
 				{
@@ -42,7 +46,7 @@ namespace SyndicateOps
 				}
 			}
 		}
-	
+
 		public string Examine(Vector3 vector)
 		{
 			return $"It seems to have {SyndicateOpConsole.Instance.TcReserve} telecrystals in reserve";
