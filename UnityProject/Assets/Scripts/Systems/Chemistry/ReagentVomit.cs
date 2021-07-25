a959529794b1ad22893dca78a3bec018ffd7ee02
dedhiadev5@gmail.com <dedhiadev5@gmail.com>
2021-07-24 21:42:54 -0700

    adds the Vomit component

---
+++ UnityProject/Assets/Scripts/Systems/Chemistry/ReagentVomit.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReagentVomit : MonoBehaviour
+{
+	public int vomitchance;
+	public bool vomitblood;
+}
