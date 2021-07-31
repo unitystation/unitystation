08bfc713f24c4ed8d5e9d0cf3f7f9c36cdf73a8b
Atner <48405920+AtnerNT@users.noreply.github.com>
2021-07-31 22:39:06 +0800

    Gets spriteHandler on awake

--- UnityProject/Assets/Scripts/Clothing/FacehuggerImpregnation.cs
+++ UnityProject/Assets/Scripts/Clothing/FacehuggerImpregnation.cs
@@ -27,17 +27,16 @@ namespace Clothing
 		[Tooltip("Reference to larvae gameObject so we can spawn it")] [SerializeField]
 		private GameObject larvae = null;
 
-		[Tooltip("The SpriteHandler the facehugger should use once it successfully impregnates a host")] [SerializeField]
-		private SpriteHandler spriteHandler = default;
-
 		private bool isAlive = true;
 		private ClothingV2 clothingV2;
 		private ItemAttributesV2 itemAttributesV2;
+		private SpriteHandler spriteHandler;
 
 		private void Awake()
 		{
 			clothingV2 = GetComponent<ClothingV2>();
 			itemAttributesV2 = GetComponent<ItemAttributesV2>();
+			spriteHandler = GetComponentInChildren<SpriteHandler>();
 		}
 
 		public override void OnStartServer()
