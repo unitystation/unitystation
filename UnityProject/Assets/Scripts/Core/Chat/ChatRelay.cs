e91635c9f770dd3a5bb1a9d08d7f249fbcccd376
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-08-05 18:11:59 +0100

    Carded check

--- UnityProject/Assets/Scripts/Core/Chat/ChatRelay.cs
+++ UnityProject/Assets/Scripts/Core/Chat/ChatRelay.cs
@@ -99,7 +99,9 @@ public class ChatRelay : NetworkBehaviour
 					//Distance check failed so if we are Ai, then try send action and combat messages to their camera location
 					//as well as if possible
 					if (chatEvent.channels.HasFlag(ChatChannel.Local) == false &&
-					    players[i].Script.PlayerState == PlayerScript.PlayerStates.Ai)
+					    players[i].Script.PlayerState == PlayerScript.PlayerStates.Ai &&
+					    players[i].Script.TryGetComponent<AiPlayer>(out var aiPlayer) &&
+					    aiPlayer.IsCarded == false)
 					{
 						playerPosition = players[i].Script.gameObject.AssumedWorldPosServer();
 						
