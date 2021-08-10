b457345cd47ea2a0c217c261d89651ba44739023
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-08-10 06:38:23 +0200

    Examine text will now show how many items there is now in a stack (#7235)
    * Examine text will now show how many items there is now in a stack
    
    * There we go
    
    * better text
    
    * Update UnityProject/Assets/Scripts/Items/Stackable.cs
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>
    
    * fixed
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>

--- UnityProject/Assets/Scripts/Items/Stackable.cs
+++ UnityProject/Assets/Scripts/Items/Stackable.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 /// <summary>
 /// Allows an item to be stacked, occupying a single inventory slot.
 /// </summary>
-public class Stackable : NetworkBehaviour, IServerLifecycle, ICheckedInteractable<InventoryApply>, ICheckedInteractable<HandApply>
+public class Stackable : NetworkBehaviour, IServerLifecycle, ICheckedInteractable<InventoryApply>, ICheckedInteractable<HandApply>, IExaminable
 {
 	[Tooltip("Amount initially in the stack when this is spawned.")]
 	[SerializeField]
@@ -340,5 +340,10 @@ public class Stackable : NetworkBehaviour, IServerLifecycle, ICheckedInteractabl
 	public void ServerPerformInteraction(HandApply interaction)
 	{
 		ServerCombine(interaction.TargetObject.GetComponent<Stackable>());
+	}
+
+	public string Examine(Vector3 worldPos)
+	{
+		return $"This {gameObject.ExpensiveName()} contains {Amount} stacks.";
 	}
 }
