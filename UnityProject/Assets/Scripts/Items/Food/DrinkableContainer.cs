f6d6efdf99ae8f15caeddba92d4e297b2069064c
PetMudstone <38266309+PetMudstone@users.noreply.github.com>
2021-08-16 15:31:45 -0700

    Random pitches for food and drink sounds. (#7283)
    * Random pitches for food and drink.
    
    * Pascalcase compliance.

--- UnityProject/Assets/Scripts/Items/Food/DrinkableContainer.cs
+++ UnityProject/Assets/Scripts/Items/Food/DrinkableContainer.cs
@@ -7,6 +7,7 @@ using ScriptableObjects;
 using UnityEngine;
 using AddressableReferences;
 using Messages.Server.SoundMessages;
+using Random = UnityEngine.Random;
 using WebSocketSharp;
 
 [RequireComponent(typeof(ItemAttributesV2))]
@@ -19,11 +20,11 @@ public class DrinkableContainer : Consumable
 	[Tooltip("The name of the sound the player makes when drinking (must be in soundmanager")]
 	[SerializeField] private AddressableAudioSource drinkSound = null;
 
+	private float RandomPitch => Random.Range( 0.7f, 1.3f );
+
 	private ReagentContainer container;
 	private ItemAttributesV2 itemAttributes;
 	private RegisterItem item;
-
-	private AudioSourceParameters audioSourceParameters = new AudioSourceParameters(spatialBlend: 1f);
 
 	private static readonly StandardProgressActionConfig ProgressConfig
 		= new StandardProgressActionConfig(StandardProgressActionType.Restrain);
@@ -87,6 +88,7 @@ public class DrinkableContainer : Consumable
 		// Play sound
 		if (item && drinkSound != null)
 		{
+			AudioSourceParameters audioSourceParameters = new AudioSourceParameters(RandomPitch, spatialBlend: 1f);
 			SoundManager.PlayNetworkedAtPos(drinkSound, eater.WorldPos, audioSourceParameters, sourceObj: eater.gameObject);
 		}
 	}
