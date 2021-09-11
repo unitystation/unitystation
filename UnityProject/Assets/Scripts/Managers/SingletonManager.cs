2668864ca714a8a1e4b6660baea3f11b71224af5
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-09-11 21:13:11 +0100

    network singleton manager

--- UnityProject/Assets/Scripts/Managers/SingletonManager.cs
+++ UnityProject/Assets/Scripts/Managers/SingletonManager.cs
@@ -1,12 +1,35 @@
+using Mirror;
 using UnityEngine;
 
 namespace Managers
 {
 	/// <summary>
-	/// Singleton manager using static instances without use of FindObject
+	/// Singleton Manager using static instances without use of FindObject
 	/// If you are using Awake() override remember to call base.Awake()!
 	/// </summary>
 	public class SingletonManager<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static T instance;
+		public static T Instance => instance;
+
+		public virtual void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this as T;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Networked Singleton Manager using static instances without use of FindObject
+	/// If you are using Awake() override remember to call base.Awake()!
+	/// </summary>
+	public class NetworkedSingletonManager<T> : NetworkBehaviour where T : NetworkBehaviour
 	{
 		private static T instance;
 		public static T Instance => instance;
