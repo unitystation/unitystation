abfe9937eb06ba34103ba33816ff5b97da9cf152
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-09-26 17:02:07 +0100

    still needs some work

--- UnityProject/Assets/Tests/Asset/NullTests.cs
+++ UnityProject/Assets/Tests/Asset/NullTests.cs
@@ -1,17 +1,27 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using Core.Editor.Attributes;
+using Items;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using Object = System.Object;
 
 namespace Tests.Asset
 {
 	public class NullTests
 	{
+		private static string sceneName = "";
+
+		private static State state;
+
+		#region GameObject Checking
+
 		/// <summary>
 		/// Checks to make sure all objects in the prefabs with fields with CannotBeNull are not null
 		/// </summary>
@@ -22,16 +32,18 @@ namespace Tests.Asset
 			var prefabGUIDs = AssetDatabase.FindAssets("t:prefab", new string[] {"Assets/Prefabs"});
 			var prefabPaths = prefabGUIDs.Select(AssetDatabase.GUIDToAssetPath);
 
+			state = State.Prefab;
+
 			foreach (var prefab in prefabPaths)
 			{
 				var gameObject = AssetDatabase.LoadAssetAtPath<GameObject>(prefab);
 
 				if(gameObject == null) continue;
 
-				CheckCannotBeNull(gameObject, report, "", true);
+				CheckCannotBeNullGameObject(gameObject, report);
 
 				RandomUtils.IterateChildren(gameObject,
-					delegate(GameObject go) { CheckCannotBeNull(go, report, "", true); },
+					delegate(GameObject go) { CheckCannotBeNullGameObject(go, report); },
 					true);
 			}
 
@@ -47,17 +59,23 @@ namespace Tests.Asset
 			var report = new StringBuilder();
 			var scenesGUIDs = AssetDatabase.FindAssets("t:Scene", new string[] {"Assets/Scenes"});
 			var scenesPaths = scenesGUIDs.Select(AssetDatabase.GUIDToAssetPath);
+
+			state = State.Scene;
+
 			foreach (var scene in scenesPaths)
 			{
 				if (scene.Contains("DevScenes") || scene.StartsWith("Packages")) continue;
 
 				var openScene = EditorSceneManager.OpenScene(scene);
+
+				sceneName = openScene.name;
+
 				var gameObjects = openScene.GetRootGameObjects();
 
 				foreach (var gameObject in gameObjects)
 				{
 					RandomUtils.IterateChildren(gameObject,
-						delegate(GameObject go) { CheckCannotBeNull(go, report, openScene.name); },
+						delegate(GameObject go) { CheckCannotBeNullGameObject(go, report); },
 						true);
 				}
 			}
@@ -65,24 +83,119 @@ namespace Tests.Asset
 			Assert.IsEmpty(report.ToString());
 		}
 
-		private void CheckCannotBeNull(GameObject toCheck, StringBuilder report, string scene, bool isPrefab = false)
+		private void CheckCannotBeNullGameObject(GameObject toCheck, StringBuilder report)
 		{
 			var components = toCheck.GetComponents<MonoBehaviour>();
 
 			foreach (var component in components)
 			{
-				var fields = component.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-				foreach (var field in fields)
-				{
-					if(Attribute.IsDefined(field, typeof(CannotBeNullAttribute)) == false) continue;
-
-					if(field.GetValue(component) != null) continue;
-
-					report.AppendLine(
-						$"{toCheck.ExpensiveName()} {(isPrefab ? "prefab" : $"scene: {scene}")} has a null value on component: {component.GetType().Name} field: {field.Name}");
-				}
+				CheckTypes(component, report, component.GetType());
 			}
+		}
+
+		#endregion
+
+		#region Scriptable Object Checking
+
+		/// <summary>
+		/// Checks to make sure all ScriptableObjects with fields with CannotBeNull are not null
+		/// </summary>
+		[Test]
+		public void CheckCannotBeNullScriptableObject()
+		{
+			var report = new StringBuilder();
+			var prefabGUIDs = AssetDatabase.FindAssets("t:ScriptableObject", new string[] {"Assets"});
+			var prefabPaths = prefabGUIDs.Select(AssetDatabase.GUIDToAssetPath);
+
+			state = State.ScriptableObject;
+
+			foreach (var prefab in prefabPaths)
+			{
+				var scriptableObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(prefab);
+
+				if(scriptableObject == null) continue;
+
+				CheckTypes(scriptableObject, report, scriptableObject.GetType());
+			}
+
+			Assert.IsEmpty(report.ToString());
+		}
+
+		#endregion
+
+		private void CheckTypes<T>(T toCheck, StringBuilder report, Type type, int count = 0) where T : UnityEngine.Object
+		{
+			var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (var field in fields)
+			{
+				CheckField(toCheck, report, field);
+
+				CheckList(toCheck, report, field);
+			}
+
+			count++;
+
+			if(count > 3) return;
+
+			foreach (var field in fields)
+			{
+				CheckTypes(toCheck, report, field.GetType(), count);
+			}
+		}
+
+		private void CheckField<T>(T toCheck, StringBuilder report, FieldInfo checkField) where T : UnityEngine.Object
+		{
+			if (Attribute.IsDefined(checkField, typeof(CannotBeNullAttribute)) == false) return;
+
+			if(checkField.GetValue(toCheck) != null) return;
+
+			if (state == State.Prefab)
+			{
+				report.AppendLine($"{toCheck.name} prefab has a null value on {checkField.GetType().Name}");
+			}
+			else if (state == State.Scene)
+			{
+				report.AppendLine(
+					$"{toCheck.name} scene object has a null value on {checkField.GetType().Name} in scene: {sceneName}");
+			}
+			else
+			{
+				report.AppendLine($"{toCheck.name} scriptable object has a null value on field: {checkField.Name}");
+			}
+		}
+
+		private void CheckList<T>(T toCheck, StringBuilder report, FieldInfo checkField) where T : UnityEngine.Object
+		{
+			Type type = checkField.GetType();
+
+			if(type.IsGenericType)
+			{
+				//TODO this isnt detecting lists???
+
+				Debug.LogError($"{type.Name} is generic");
+				if (type.GetGenericTypeDefinition()
+				    != typeof(List<>))
+				{
+					Debug.LogError($"{type.Name} is not list");
+					return;
+				}
+
+				Type itemType = type.GetGenericArguments()[0];
+
+				Debug.LogError($"{type.Name} {itemType.Name}");
+
+				//TODO need to check instance first?
+
+				CheckTypes(toCheck, report, itemType);
+			}
+		}
+
+		private enum State
+		{
+			Prefab,
+			Scene,
+			ScriptableObject
 		}
 	}
 }
