96bbcf9c2c9ee5965c7d8c13be14e12b3c09977b
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-09-28 17:02:24 +0200

    Adds the ability to climb crates

---
+++ UnityProject/Assets/Scripts/Objects/Climbable.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbable : MonoBehaviour, ICheckedInteractable<MouseDrop>
+{
+
+	public float ClimbTime = 1.0f;
+
+
+	public bool WillInteract(MouseDrop interaction, NetworkSide side)
+	{
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+		PlayerSync playerSync;
+		CustomNetTransform netTransform;
+		if(interaction.UsedObject.TryGetComponent(out playerSync))
+		{
+			if (playerSync.IsMoving || playerSync.playerMove.IsBuckled)
+			{
+				return false;
+			}
+
+			// Do a sanity check to make sure someone isn't dropping the shadow from like 9000 tiles away.
+			var mag = (interaction.TargetObject.transform.position - playerSync.ServerPosition).magnitude;
+			if (mag > PlayerScript.interactionDistance)
+			{
+				return false;
+			}
+		}
+		else if(interaction.UsedObject.TryGetComponent(out netTransform)) // Do the same check but for mouse draggable objects this time.
+		{
+			var mag = (interaction.TargetObject.transform.position - playerSync.ServerPosition).magnitude;
+			if (mag > PlayerScript.interactionDistance)
+			{
+				return false;
+			}
+		}
+		else // Not sure what this object is so assume that we can't interact with it at all.
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	public void ServerPerformInteraction(MouseDrop interaction)
+	{
+		StandardProgressActionConfig cfg =
+			new StandardProgressActionConfig(StandardProgressActionType.Construction, false, false, false);
+		var x = StandardProgressAction.Create(cfg, () =>
+		{
+			PlayerScript playerScript;
+			if (interaction.UsedObject.TryGetComponent(out playerScript))
+			{
+				playerScript.PlayerSync.SetPosition(this.gameObject.WorldPosServer());
+			}
+			else
+			{
+				var transformComp = interaction.UsedObject.GetComponent<CustomNetTransform>();
+				if (transformComp != null)
+				{
+					transformComp.AppearAtPositionServer(this.gameObject.WorldPosServer());
+				}
+			}
+		}).ServerStartProgress(interaction.UsedObject.RegisterTile(), ClimbTime, interaction.Performer);
+
+		Chat.AddActionMsgToChat(interaction.Performer,
+			$"You begin climbing onto the {interaction.TargetObject.gameObject.ExpensiveName()}...",
+			$"{interaction.Performer.ExpensiveName()} begins climbing onto the {interaction.TargetObject.gameObject.ExpensiveName()}...");
+	}
+}
