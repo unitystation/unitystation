4d2f801aebd9e1baf2c2b2e94217b2c8040ed73f
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-10-02 08:08:40 +0200

    functions are cool

--- UnityProject/Assets/Scripts/Objects/Doors/Modules/EmagInteractionModule.cs
+++ UnityProject/Assets/Scripts/Objects/Doors/Modules/EmagInteractionModule.cs
@@ -18,89 +18,69 @@ namespace Doors.Modules
 
 		public override ModuleSignal ClosedInteraction(HandApply interaction, HashSet<DoorProcessingStates> States)
 		{
-			if (interaction != null)
+			var ItemStorage = interaction.Performer.GetComponent<DynamicItemStorage>();
+			return EmagChecks(ItemStorage, interaction, States);
+		}
+
+		public override ModuleSignal BumpingInteraction(GameObject byPlayer, HashSet<DoorProcessingStates> States)
+		{
+			var ItemStorage = byPlayer.GetComponent<DynamicItemStorage>();
+			return EmagChecks(ItemStorage, null, States);
+		}
+
+		/// <summary>
+		/// Checks to see if a door can be emagged, does checks for BumpInteraction and Hand Interactions.
+		/// </summary>
+		/// <param name="itemStorage">The player's inventory that may contain the emag</param>
+		/// <param name="interaction">If we're calling this from ClosedInteraction() to provide a HandApply</param>
+		/// <param name="States">Door process states</param>
+		/// <returns>Either hacked or ModuleSignal.Continue</returns>
+		private ModuleSignal EmagChecks(DynamicItemStorage itemStorage, HandApply interaction, HashSet<DoorProcessingStates> States)
+		{
+			if (itemStorage != null)
 			{
 				try
 				{
-					var ItemStorage = interaction.Performer.GetComponent<DynamicItemStorage>();
-					if (ItemStorage != null)
+					Emag emagInHand = itemStorage.GetActiveHandSlot().Item?.OrNull().gameObject.GetComponent<Emag>()?.OrNull();
+					if (emagInHand != null)
 					{
-						Emag emagInHand = ItemStorage.GetActiveHandSlot().Item?.OrNull().gameObject.GetComponent<Emag>()?.OrNull();
-						if (emagInHand != null)
+						if (interaction != null)
 						{
-							if (emagInHand.UseCharge(interaction))
-							{
-								States.Add(DoorProcessingStates.SoftwareHacked);
-								StartCoroutine(ToggleBolts());
-								return ModuleSignal.Continue;
-							}
+							if (emagInHand.UseCharge(interaction)) return EmagSuccessLogic(States);
 						}
+						if (emagInHand.UseCharge(gameObject, itemStorage.registerPlayer.PlayerScript.gameObject)) return EmagSuccessLogic(States);
+					}
 
-						foreach (var item in ItemStorage.GetNamedItemSlots(NamedSlot.id))
+					foreach (var item in itemStorage.GetNamedItemSlots(NamedSlot.id))
+					{
+						Emag emagInIdSlot = item.Item?.OrNull().gameObject.GetComponent<Emag>()?.OrNull();
+						if (emagInIdSlot == null) continue;
+						if (interaction != null)
 						{
-							Emag emagInIdSlot = item.Item?.OrNull().gameObject.GetComponent<Emag>()?.OrNull();
-
-							if (emagInIdSlot == null) continue;
-							if (emagInIdSlot.UseCharge(interaction))
-							{
-								States.Add(DoorProcessingStates.SoftwareHacked);
-								StartCoroutine(ToggleBolts());
-								return ModuleSignal.Continue;
-							}
+							if (emagInIdSlot.UseCharge(interaction)) return EmagSuccessLogic(States);
 						}
+						if (emagInIdSlot.UseCharge(gameObject, itemStorage.registerPlayer.PlayerScript.gameObject)) return EmagSuccessLogic(States);
 					}
 				}
 				catch (NullReferenceException exception)
 				{
-					Logger.LogError("A NRE was caught in EmagInteractionModule.ClosedInteraction() " + exception.Message, Category.Interaction);
+					Logger.LogError(
+						"A NRE was caught in EmagInteractionModule.ClosedInteraction() " + exception.Message,
+						Category.Interaction);
 				}
 			}
 
 			return ModuleSignal.Continue;
 		}
 
-		public override ModuleSignal BumpingInteraction(GameObject byPlayer, HashSet<DoorProcessingStates> States)
+		/// <summary>
+		/// What happens after a door gets emagged.
+		/// </summary>
+		/// <returns>ModuleSignal.Continue</returns>
+		private ModuleSignal EmagSuccessLogic(HashSet<DoorProcessingStates> States)
 		{
-			var ItemStorage = byPlayer.GetComponent<DynamicItemStorage>();
-			if (ItemStorage != null)
-			{
-				try
-				{
-					var Hand = ItemStorage.GetActiveHandSlot().ItemAttributes;
-					if (Hand != null)
-					{
-						if (Hand.HasTrait(CommonTraits.Instance.Emag))
-						{
-							States.Add(DoorProcessingStates.SoftwareHacked);
-							StartCoroutine(ToggleBolts());
-							return ModuleSignal.Continue;
-						}
-					}
-				}
-				catch (NullReferenceException exception)
-				{
-					Logger.LogError(
-						"A NRE was caught in EmagInteractionModule.BumpingInteraction(): " + exception.Message,
-						Category.Interaction);
-					return ModuleSignal.ContinueWithoutDoorStateChange;
-				}
-
-				foreach (var item in ItemStorage.GetNamedItemSlots(NamedSlot.id))
-				{
-					var ID = item.ItemAttributes;
-
-					if (ID != null)
-					{
-						if (ID.HasTrait(CommonTraits.Instance.Emag))
-						{
-							States.Add(DoorProcessingStates.SoftwareHacked);
-							StartCoroutine(ToggleBolts());
-							return ModuleSignal.Continue;
-						}
-					}
-				}
-			}
-
+			States.Add(DoorProcessingStates.SoftwareHacked);
+			StartCoroutine(ToggleBolts());
 			return ModuleSignal.Continue;
 		}
 
