dcc036707fdba0565cb1a152e551b71aa5edb5d4
Aranclanos <Aranclanos@gmail.com>
2021-10-08 23:02:07 -0300

    Changes FPSMonitor to be a SingletonManager. Puts FPSMonitor under the Manager namespace.

--- UnityProject/Assets/Scripts/RCON/FPSMonitor.cs
+++ UnityProject/Assets/Scripts/RCON/FPSMonitor.cs
@@ -3,88 +3,78 @@ using System.Collections.Generic;
 using Messages.Server;
 using Mirror;
 
-
-public class FPSMonitor : MonoBehaviour
+namespace Managers
 {
-	public static FPSMonitor Instance { get; private set; }
+	public class FPSMonitor : SingletonManager<FPSMonitor>
+	{
+		private List<float> avgSamples = new List<float>();
 
-	private List<float> avgSamples = new List<float>();
+	    public float Current { get; private set; }
+	    public float Average { get; private set; }
+	    public float Min { get; private set; }
+	    public float Max { get; private set; }
 
-    public float Current { get; private set; }
-    public float Average { get; private set; }
-    public float Min { get; private set; }
-    public float Max { get; private set; }
+	    float timeInMax = 0f;
+	    float timeInMin = 0f;
 
-    float timeInMax = 0f;
-    float timeInMin = 0f;
+	    private float updateClientsTimer;
 
-    private float updateClientsTimer;
-
-    private void Awake()
-    {
-	    if (Instance == null)
+	    void Update()
 	    {
-		    Instance = this;
-	    }
-    }
+	        timeInMax += Time.unscaledDeltaTime;
+	        timeInMin += Time.unscaledDeltaTime;
 
-    void Update()
-    {
-        timeInMax += Time.unscaledDeltaTime;
-        timeInMin += Time.unscaledDeltaTime;
+	        Current = 1 / Time.unscaledDeltaTime;
+	        Average = 0;
 
-        Current = 1 / Time.unscaledDeltaTime;
-        Average = 0;
-
-        if (avgSamples.Count >= 200)
-        {
-            avgSamples.Add(Current);
-            avgSamples.RemoveAt(0);
-        }
-        else
-        {
-            avgSamples.Add(Current);
-        }
-
-        for (int i = 0; i < avgSamples.Count; i++)
-        {
-            Average += avgSamples[i];
-        }
-
-        Average /= 200;
-
-        if (timeInMin > 10f)
-        {
-            Min = -1;
-            timeInMin = 0f;
-        }
-
-        if (timeInMax > 10f)
-        {
-            Max = -1;
-            timeInMax = 0f;
-        }
-
-        if (Current < Min || Min < 0)
-        {
-            Min = Current;
-        }
-
-        if (Current > Max || Max < 0)
-        {
-            Max = Current;
-        }
-
-        updateClientsTimer += Time.unscaledDeltaTime;
-        if (updateClientsTimer > 0.5f)
-        {
-	        if (NetworkServer.active)
+	        if (avgSamples.Count >= 200)
 	        {
-		        UpdateServerFPS.Send(Current, Average);
+	            avgSamples.Add(Current);
+	            avgSamples.RemoveAt(0);
 	        }
-	        updateClientsTimer = 0;
-        }
-    }
+	        else
+	        {
+	            avgSamples.Add(Current);
+	        }
+
+	        for (int i = 0; i < avgSamples.Count; i++)
+	        {
+	            Average += avgSamples[i];
+	        }
+
+	        Average /= 200;
+
+	        if (timeInMin > 10f)
+	        {
+	            Min = -1;
+	            timeInMin = 0f;
+	        }
+
+	        if (timeInMax > 10f)
+	        {
+	            Max = -1;
+	            timeInMax = 0f;
+	        }
+
+	        if (Current < Min || Min < 0)
+	        {
+	            Min = Current;
+	        }
+
+	        if (Current > Max || Max < 0)
+	        {
+	            Max = Current;
+	        }
+
+	        updateClientsTimer += Time.unscaledDeltaTime;
+	        if (updateClientsTimer > 0.5f)
+	        {
+		        if (NetworkServer.active)
+		        {
+			        UpdateServerFPS.Send(Current, Average);
+		        }
+		        updateClientsTimer = 0;
+	        }
+	    }
+	}
 }
-
-
