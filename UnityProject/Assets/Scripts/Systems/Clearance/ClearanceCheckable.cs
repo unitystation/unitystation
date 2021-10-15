3f14d2e47796b3100e6af7913e2b0ba1eabcfea9
Moon <brandibrooks4@gmail.com>
2021-10-15 12:29:13 -1000

    Update ClearanceCheckable.cs

--- UnityProject/Assets/Scripts/Systems/Clearance/ClearanceCheckable.cs
+++ UnityProject/Assets/Scripts/Systems/Clearance/ClearanceCheckable.cs
@@ -30,9 +30,8 @@ namespace Systems.Clearance
 			{
 				return true;
 			}
-			// If the player has null or no defined access, access is denied
-			if (requesterClearance == null ||
-			    requesterClearance.Any() == false)
+			// If the player has null access, access is denied
+			if (requesterClearance == null)
 			{
 				return false;
 			}
