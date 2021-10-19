3785488c03a3b4bef22f4a29f10671e857bf0cc8
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-10-19 02:19:38 +0200

    Update BoneSetter.cs

--- UnityProject/Assets/Scripts/Items/Medical/BoneSetter.cs
+++ UnityProject/Assets/Scripts/Items/Medical/BoneSetter.cs
@@ -3,43 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using HealthV2;
 
-public class BoneSetter : HealsTheLiving
+namespace Items.Medical
 {
-	public override void ServerPerformInteraction(HandApply interaction)
+	public class BoneSetter : HealsTheLiving
 	{
-		LivingHealthMasterBase LHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
-		PushPull pull = interaction.Performer.gameObject.GetComponent<PushPull>();
-		if (!pull.IsPullingSomething) return;
-
-		BodyPart partToHeal = null;
-
-		foreach (BodyPart bodyPart in LHB.BodyPartList)
+		public override void ServerPerformInteraction(HandApply interaction)
 		{
-			if (bodyPart.BodyPartType == interaction.TargetBodyPart)
+			LivingHealthMasterBase LHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
+			PushPull pull = interaction.Performer.gameObject.GetComponent<PushPull>();
+			if (!pull.IsPullingSomething) return;
+
+			BodyPart partToHeal = null;
+
+			foreach (BodyPart bodyPart in LHB.BodyPartList)
 			{
-				partToHeal = bodyPart;
-				break;
+				if (bodyPart.BodyPartType == interaction.TargetBodyPart)
+				{
+					partToHeal = bodyPart;
+					break;
+				}
+			}
+
+			if (partToHeal.CurrentBluntDamageLevel <= TraumaDamageLevel.NONE)
+			{
+				Chat.AddExamineMsg(interaction.Performer, "There's nothing broken to fix!");
+			}
+			else if (partToHeal.CurrentBluntDamageLevel == TraumaDamageLevel.SMALL)
+			{
+				void ProgressComplete()
+				{
+					SetBone(partToHeal);
+				}
+
+				StandardProgressAction.Create(ProgressConfig, ProgressComplete)
+					.ServerStartProgress(interaction.Performer.RegisterTile(), timeTakenToHeal, interaction.Performer);
 			}
 		}
 
-		if (partToHeal.CurrentBluntDamageLevel <= TraumaDamageLevel.NONE)
+		public void SetBone(BodyPart bodyPart, TraumaDamageLevel level = TraumaDamageLevel.NONE)
 		{
-			Chat.AddExamineMsg(interaction.Performer, "There's nothing broken to fix!");
+			bodyPart.HealTraumaticDamage(TraumaticDamageTypes.BLUNT, level);
+			if (level == TraumaDamageLevel.NONE) bodyPart.AnnounceJointHealEvent();
 		}
-		else if(partToHeal.CurrentBluntDamageLevel == TraumaDamageLevel.SMALL)
-		{
-			void ProgressComplete()
-			{
-				SetBone(partToHeal);
-			}
-			StandardProgressAction.Create(ProgressConfig, ProgressComplete)
-				.ServerStartProgress(interaction.Performer.RegisterTile(), timeTakenToHeal, interaction.Performer);
-		}
-	}
-
-	public void SetBone(BodyPart bodyPart, TraumaDamageLevel level = TraumaDamageLevel.NONE)
-	{
-		bodyPart.HealTraumaticDamage(TraumaticDamageTypes.BLUNT, level);
-		if(level == TraumaDamageLevel.NONE) bodyPart.AnnounceJointHealEvent();
 	}
 }
