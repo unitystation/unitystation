2aa8b4381c84e6695e34523f8fea70fa5fc3186a
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-11-03 22:57:02 +0200

    Update ExplosionUtils.cs

--- UnityProject/Assets/Scripts/Items/Weapons/ExplosionUtils.cs
+++ UnityProject/Assets/Scripts/Items/Weapons/ExplosionUtils.cs
@@ -15,9 +15,6 @@ namespace Systems.Explosions
 		private static readonly AddressableAudioSource[] DISTANT_EXPLOSION_SOUNDS = { CommonSounds.Instance.ExplosionDistant1, CommonSounds.Instance.ExplosionDistant2 };
 		private static readonly AddressableAudioSource[] STATION_GROAN_SOUNDS = { CommonSounds.Instance.ExplosionCreak1, CommonSounds.Instance.ExplosionCreak2, CommonSounds.Instance.ExplosionCreak3 };
 
-		private static readonly int DISTANT_THRESHOLD = 63;
-		private static readonly int GROAN_THRESHOLD = 29;
-
 		/// <summary>
 		/// Play explosion sound and shake ground
 		/// </summary>
@@ -26,20 +23,22 @@ namespace Systems.Explosions
 		/// <param name="shakeDistance">how far away the shaking can be felt</param>
 		public static void PlaySoundAndShake(Vector3Int worldPosition, byte shakeIntensity, int shakeDistance)
 		{
-			AddressableAudioSource sndName = EXPLOSION_SOUNDS[Random.Range(0, EXPLOSION_SOUNDS.Length)];
+
+			AddressableAudioSource explosionSound = EXPLOSION_SOUNDS[Random.Range(0, EXPLOSION_SOUNDS.Length)];
+			AddressableAudioSource groanSound     = STATION_GROAN_SOUNDS[Random.Range(0, STATION_GROAN_SOUNDS.Length)];
+			AddressableAudioSource distantSound   = DISTANT_EXPLOSION_SOUNDS[Random.Range(0, DISTANT_EXPLOSION_SOUNDS.Length)];
 			AudioSourceParameters audioSourceParameters = new AudioSourceParameters(0f, 100f);
 			ShakeParameters shakeParameters = new ShakeParameters(true, shakeIntensity, shakeDistance);
-			_ = SoundManager.PlayNetworkedAtPosAsync(sndName, worldPosition, audioSourceParameters, true, false, shakeParameters: shakeParameters);
 
-			if (shakeDistance > DISTANT_THRESHOLD)
+			_ = SoundManager.PlayNetworkedAtPosAsync(explosionSound, worldPosition, audioSourceParameters, true, false, shakeParameters);
+
+			if (shakeDistance > distantSound.AudioSource.maxDistance)
 			{
-				AddressableAudioSource distantName = DISTANT_EXPLOSION_SOUNDS[Random.Range(0, DISTANT_EXPLOSION_SOUNDS.Length)];
-				_ = SoundManager.PlayNetworkedAtPosAsync(distantName, worldPosition, global: false);
+				_ = SoundManager.PlayNetworkedAtPosAsync(distantSound, worldPosition, global: true);
 			}
-			if (shakeIntensity > GROAN_THRESHOLD)
+			if (shakeIntensity > groanSound.AudioSource.maxDistance)
 			{
-				AddressableAudioSource groanName = STATION_GROAN_SOUNDS[Random.Range(0, STATION_GROAN_SOUNDS.Length)];
-				_ = SoundManager.PlayNetworkedAtPosAsync(groanName, worldPosition, global: false);
+				_ = SoundManager.PlayNetworkedAtPosAsync(groanSound, worldPosition, global: true);
 			}
 		}
 	}
