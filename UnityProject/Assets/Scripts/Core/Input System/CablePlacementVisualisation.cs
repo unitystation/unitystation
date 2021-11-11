0c7a8615e3a06dff77635e80087e39e2f8f2911f
Bod9001 <Bod9001@gmail.com>
2021-11-11 22:53:54 +0000

    target position

--- UnityProject/Assets/Scripts/Core/Input System/CablePlacementVisualisation.cs
+++ UnityProject/Assets/Scripts/Core/Input System/CablePlacementVisualisation.cs
@@ -151,8 +151,9 @@ public class CablePlacementVisualisation : MonoBehaviour
 	private void Build()
 	{
 		if (startPoint == endPoint || target == null) return;
+		var Position = cablePlacementVisualisation.transform.position + new Vector3(0.5f, 0.5f, 0);
 
-		Vector2 targetVector = (connectionPointRenderers[endPoint].transform.position - transform.position);
+		Vector2 targetVector = Position.ToLocal(PlayerManager.LocalPlayer.RegisterTile().Matrix);; // transform.position ( - transform.position); //TODO? what? is this
 		ConnectionApply cableApply = ConnectionApply.ByLocalPlayer(target, startPoint, endPoint, targetVector);
 
 		//if HandObject is null, then its an empty hand apply so we only need to check the receiving object
@@ -297,7 +298,7 @@ public class CablePlacementVisualisation : MonoBehaviour
 				if (topTile && (topTile.LayerType == LayerType.Base || topTile.LayerType == LayerType.Underfloor))
 				{
 					// move cable placement visualisation to rounded mouse position and enable it
-					cablePlacementVisualisation.transform.position = mousePosition - new Vector3(0.5f, 0.5f, 0); ;
+					cablePlacementVisualisation.transform.position = mousePosition - new Vector3(0.5f, 0.5f, 0);
 					cablePlacementVisualisation.SetActive(true);
 				}
 				// disable visualisation if active
