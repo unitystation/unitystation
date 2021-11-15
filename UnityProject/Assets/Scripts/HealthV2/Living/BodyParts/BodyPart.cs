42b8c89d2370f9b29ad3e9af8b47819759fd6403
Moon <brandibrooks4@gmail.com>
2021-11-15 13:21:17 -1000

    fix bleeding

--- UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPart.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/BodyParts/BodyPart.cs
@@ -182,7 +182,7 @@ namespace HealthV2
 			if(IsBleeding)
 			{
 				InternalBleedingLogic();
-				HealthMaster.CirculatorySystem.Bleed(limbLossBleedingValue);
+				//HealthMaster.CirculatorySystem.Bleed(limbLossBleedingValue); causes you to continue bleeding after hitting 0 bleedstacks
 			}
 
 		}
@@ -333,12 +333,14 @@ namespace HealthV2
 		/// </summary>
 		public void TryRemoveFromBody(bool beingGibbed = false)
 		{
+			bool alreadyBleeding = false;
 			SetRemovedColor();
 			foreach (var bodyPart in HealthMaster.BodyPartList)
 			{
-				if (bodyPart.BodyPartType == BodyPartType.Chest)
+				if (bodyPart.BodyPartType == BodyPartType.Chest && alreadyBleeding == false)
 				{
 					bodyPart.IsBleeding = true;
+					alreadyBleeding = true;
 					HealthMaster.ChangeBleedStacks(limbLossBleedingValue);
 				}
 			}
@@ -371,7 +373,6 @@ namespace HealthV2
 			{
 				HealthMaster.OrNull()?.BodyPartStorage.OrNull()?.ServerTryRemove(gameObject);
 			}
-
 		}
 
 		/// <summary>
