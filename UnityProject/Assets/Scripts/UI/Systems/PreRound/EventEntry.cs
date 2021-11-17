5c138c17d51f278443e04b5b401b3c611834370c
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-11-17 09:04:09 +0200

    event ui stuff

---
+++ UnityProject/Assets/Scripts/UI/Systems/PreRound/EventEntry.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Systems.PreRound
+{
+	public class EventEntry : MonoBehaviour
+	{
+		public Image EventImage;
+		public TMP_Text EventName;
+		public TMP_Text EventDesc;
+	}
+}
