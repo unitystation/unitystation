bfa1f69506d44714ca65d53ddc62c25919865027
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-11-17 06:11:09 +0200

    interactions

--- UnityProject/Assets/Scripts/Objects/Other/PineTreeXmas.cs
+++ UnityProject/Assets/Scripts/Objects/Other/PineTreeXmas.cs
@@ -2,29 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Managers;
+using Mirror;
 using ScriptableObjects.TimedGameEvents;
 using UnityEngine;
 
 namespace Objects.Other
 {
-	public class PineTreeXmas : MonoBehaviour
+	public class PineTreeXmas : NetworkBehaviour, ICheckedInteractable<HandApply>
 	{
 		[SerializeField] private SpriteDataSO xmasSpriteSO;
 		[SerializeField] private TimedGameEventSO eventData;
 
+		[SyncVar] private List<string> giftedPlayers;
 		private bool canPickUpGifts;
 
 		private SpriteHandler spriteHandler;
 
-		private IEnumerator Start()
+		private void Awake()
 		{
-			yield return WaitFor.Seconds(12);
-			spriteHandler = GetComponent<SpriteHandler>();
-			if (SingletonManager<TimedEventsManager>.Instance.ActiveEvents.Contains(eventData))
+			spriteHandler = GetComponentInChildren<SpriteHandler>();
+			if (TimedEventsManager.Instance.ActiveEvents.Contains(eventData))
 			{
 				canPickUpGifts = true;
 				spriteHandler.SetSpriteSO(xmasSpriteSO);
 			}
+		}
+
+		public bool WillInteract(HandApply interaction, NetworkSide side)
+		{
+			if (canPickUpGifts == false) return false;
+			if (DefaultWillInteract.Default(interaction, side) == false) return false;
+			if (giftedPlayers.Contains(interaction.PerformerPlayerScript.connectedPlayer.UserId))
+			{
+				Chat.AddExamineMsg(interaction.Performer, "You already received a gift!");
+				return false;
+			}
+			return true;
+		}
+
+		public void ServerPerformInteraction(HandApply interaction)
+		{
+			Chat.AddActionMsgToChat(interaction.Performer,
+				$"You pick up a gift with your name on it.",
+				$"{interaction.PerformerPlayerScript.visibleName} picks up a gift with {interaction.PerformerPlayerScript.characterSettings.TheirPronoun(interaction.PerformerPlayerScript)} name on it.");
+			giftedPlayers.Add(interaction.PerformerPlayerScript.connectedPlayer.UserId);
 		}
 	}
 }
