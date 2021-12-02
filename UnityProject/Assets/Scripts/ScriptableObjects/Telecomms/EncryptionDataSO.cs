faf997777cc0c7827c9a753203b0b576821adbb4
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-12-02 20:58:48 +0200

    Update EncryptionDataSO.cs

--- UnityProject/Assets/Scripts/ScriptableObjects/Telecomms/EncryptionDataSO.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/Telecomms/EncryptionDataSO.cs
@@ -18,18 +18,10 @@ namespace ScriptableObjects.Communications
 		/// </summary>
 		public string EncryptionFlaw;
 
-		[SerializeField] private TextAsset randomWords;
-
 		private void Awake()
 		{
 			//TODO (Max): This will be expanded upon in the future to allow for shared random keys When we start working on the telecomms update
 			//Encryption flaws are meant for the telecomms updates so this right now doesn't really have much use at the moment
-			if (randomWords != null)
-			{
-				string[] lines = file.ReadAllLines(randomWords.text);
-				EncryptionFlaw = lines[new Random().Next(lines.Length)];
-				return;
-			}
 			EncryptionFlaw = new Random().Next(10, 99).ToString();
 		}
 	}
