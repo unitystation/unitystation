d20f28415eac0f895ec13801b56867eb19d494e0
Bod9001 <Bod9001@gmail.com>
2021-12-09 20:05:15 +0000

    c;end

--- UnityProject/Assets/Scripts/ScriptableObjects/ChemistryReagentsSO.cs
+++ UnityProject/Assets/Scripts/ScriptableObjects/ChemistryReagentsSO.cs
@@ -38,25 +38,6 @@ namespace ScriptableObjects
 	[CustomEditor(typeof(ChemistryReagentsSO))]
 	public class ChemistryReagentsSOEditor : Editor
 	{
-		public static List<T> FindAssetsByType<T>() where T : UnityEngine.Object
-		{
-			List<T> assets = new List<T>();
-			string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
-			for (int i = 0; i < guids.Length; i++)
-			{
-				string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
-				T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
-				if (asset != null)
-				{
-					assets.Add(asset);
-				}
-			}
-
-			return assets;
-		}
-
-
-
 		public override void OnInspectorGUI()
 		{
 			base.OnInspectorGUI();
