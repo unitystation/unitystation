02bf029264c59b1d038cc1a0371f7f89182ea4f7
Bod9001 <Bod9001@gmail.com>
2021-12-16 14:40:28 +0000

    Better interaction target (#7965)
    * target position
    
    * fix
    
    * Update UnityProject/Assets/Scripts/Core/Input System/CablePlacementVisualisation.cs
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/AimApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/AimApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/ConnectionApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/AimApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/ConnectionApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/PositionalHandApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/PositionalHandApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileMouseDrop.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileMouseDrop.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>
    
    * More readaable
    
    * Update UnityProject/Assets/Scripts/Core/Input System/InteractionV2/Interactions/ConnectionApply.cs
    
    Co-authored-by: NoooneyDude <noooneydude@gmail.com>

--- UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileMouseDrop.cs
+++ UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileMouseDrop.cs
@@ -24,24 +24,20 @@ public class TileMouseDrop : Interaction
 	/// </summary>
 	public BasicTile BasicTile => basicTile;
 
-	private readonly Vector2 targetVector;
+	private readonly Vector2 targetPosition;
 
-	/// <summary>
-	/// Targeted world position deduced from target vector and performer position.
-	/// </summary>
-	public Vector2 WorldPositionTarget => (Vector2)Performer.transform.position + targetVector;
+	/// <summary>Target world position calculated from matrix local position.</summary>
+	public Vector2 WorldPositionTarget => (Vector2)targetPosition.To3().ToWorld(Performer.RegisterTile().Matrix);
 
-	/// <summary>
-	/// Vector pointing from the performer to the targeted position. Set to Vector2.zero if aiming at self.
-	/// </summary>
-	public Vector2 TargetVector => targetVector;
+	/// <summary>Requested local position target.</summary>
+	public Vector2 TargetPosition => targetPosition;
 
 	public TileMouseDrop(GameObject performer, GameObject usedObject, Intent intent, Vector2Int targetCellPos,
-		InteractableTiles targetInteractableTiles, BasicTile basicTile, Vector2 targetVector) : base(performer, usedObject, intent)
+		InteractableTiles targetInteractableTiles, BasicTile basicTile, Vector2 targetPosition) : base(performer, usedObject, intent)
 	{
 		this.targetCellPos = targetCellPos;
 		this.targetInteractableTiles = targetInteractableTiles;
 		this.basicTile = basicTile;
-		this.targetVector = targetVector;
+		this.targetPosition = targetPosition;
 	}
 }
