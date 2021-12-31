b7d8513c8bf6e3bc56d078db3f7ed243417718fe
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2021-12-31 19:50:35 +0000

    lava fire tick system

--- UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/LavaStepInteraction.cs
+++ UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/LavaStepInteraction.cs
@@ -1,19 +1,116 @@
-﻿using Objects;
+﻿using System.Collections.Generic;
+using HealthV2;
+using Objects;
 using UnityEngine;
 
 /// <summary>
 /// Tiles are not prefabs, but we still want to be able to associate interaction logic with them.
 /// This abstract base scriptable object allows tiles to define their interaction logic by referencing
 /// subclasses of this class.
+///
+/// LavaStepInteraction, contains dictionary of all objects which should be lit on fire every tick
 /// </summary>
 [CreateAssetMenu(fileName = "LavaStepInteraction", menuName = "Interaction/TileInteraction/LavaStepInteraction")]
 public class LavaStepInteraction : TileStepInteraction
 {
 	[SerializeField]
-	private float fireStacks = 10;
+	private float playerMobFireStacks = 3;
 
 	[SerializeField]
 	private float objectFireDamage = 5;
+
+	[SerializeField]
+	private float fireTimer = 1f;
+
+	//All lava tiles will use this same dictionary as this is a Scriptable object
+	private Dictionary<GameObject, BasicTile> stuffToLightOnFire = new Dictionary<GameObject, BasicTile>();
+	private bool active;
+
+	private void TryLightOnFire()
+	{
+		var keep = new Dictionary<GameObject, BasicTile>();
+
+		foreach (var objectToTest in stuffToLightOnFire)
+		{
+			if(objectToTest.Key == null) continue;
+			if(objectToTest.Key.TryGetComponent<RegisterTile>(out var registerTile) == false) continue;
+			LayerTile tile = registerTile.Matrix.MetaTileMap.GetTile(registerTile.LocalPositionServer, true);
+
+			if (tile is BasicTile basicTile)
+			{
+				//Work out if the object is still on the same tile as before
+				if(basicTile != objectToTest.Value) continue;
+
+				keep.Add(objectToTest.Key, objectToTest.Value);
+			}
+		}
+
+		stuffToLightOnFire = keep;
+
+		if (stuffToLightOnFire.Count == 0)
+		{
+			active = false;
+			UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, TryLightOnFire);
+			return;
+		}
+
+		foreach (var objectToLight in stuffToLightOnFire.Keys)
+		{
+			if (objectToLight.TryGetComponent<PlayerHealthV2>(out var playerHealth))
+			{
+				playerHealth.ChangeFireStacks(playerMobFireStacks);
+				continue;
+			}
+
+			if (objectToLight.TryGetComponent<LivingHealthBehaviour>(out var livingHealthBehaviour))
+			{
+				livingHealthBehaviour.ChangeFireStacks(playerMobFireStacks);
+				continue;
+			}
+
+			if (objectToLight.TryGetComponent<Integrity>(out var integrity))
+			{
+				integrity.ApplyDamage(objectFireDamage, AttackType.Fire, DamageType.Burn);
+			}
+		}
+	}
+
+	private void AddToFireDict(GameObject objectToAdd)
+	{
+		if (objectToAdd.TryGetComponent<RegisterTile>(out var registerTile) == false) return;
+
+		LayerTile tile = registerTile.Matrix.MetaTileMap.GetTile(registerTile.LocalPositionServer, true);
+
+		if (tile is BasicTile basicTile)
+		{
+			if (stuffToLightOnFire.TryGetValue(objectToAdd, out var oldBasicTile))
+			{
+				//If the object has moved to another lava tile, replace the dictionary value
+				if (basicTile != oldBasicTile)
+				{
+					stuffToLightOnFire[objectToAdd] = basicTile;
+				}
+
+				ActivateFireUpdate();
+				return;
+			}
+
+			stuffToLightOnFire.Add(objectToAdd, basicTile);
+		}
+
+		ActivateFireUpdate();
+	}
+
+	private void ActivateFireUpdate()
+	{
+		if(active) return;
+
+		//Trigger one fire tick and then every fireTimer
+		TryLightOnFire();
+		UpdateManager.Add(TryLightOnFire, fireTimer);
+
+		active = true;
+	}
 
 	//Player enter tile interaction//
 	public override bool CanPlayerStep(PlayerScript playerScript)
@@ -23,7 +120,7 @@ public class LavaStepInteraction : TileStepInteraction
 
 	public override void OnPlayerStep(PlayerScript playerScript)
 	{
-		playerScript.playerHealth.ChangeFireStacks(fireStacks);
+		AddToFireDict(playerScript.gameObject);
 	}
 
 	//Object, mob, item enter tile interaction//
@@ -34,15 +131,6 @@ public class LavaStepInteraction : TileStepInteraction
 
 	public override void OnObjectEnter(GameObject eventData)
 	{
-		if (eventData.TryGetComponent<LivingHealthBehaviour>(out var mobHealth))
-		{
-			mobHealth.ChangeFireStacks(fireStacks);
-			return;
-		}
-
-		if (eventData.TryGetComponent<Integrity>(out var integrity))
-		{
-			integrity.ApplyDamage(objectFireDamage, AttackType.Fire, DamageType.Burn);
-		}
+		AddToFireDict(eventData);
 	}
 }
