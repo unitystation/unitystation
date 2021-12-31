72952a3f75028d5771aa857eeaa3242d7f78e307
Atner <48405920+AtnerNT@users.noreply.github.com>
2021-12-31 13:45:12 +0800

    Adds Stun Prod and Teleprod

---
+++ UnityProject/Assets/Scripts/Items/Weapons/Melee/MeleeTeleport.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using Systems.Teleport;
+using UnityEngine;
+using AddressableReferences;
+
+/// <summary>
+/// Adding this to a weapon randomly teleports the target on hit. Note: This script works almost identically to MeleeStun.cs with just a seperate OnHit effect.
+/// </summary>
+public class MeleeTeleport : MonoBehaviour, ICheckedInteractable<HandApply>
+{
+	public bool avoidSpace;
+	public bool avoidImpassable = true;
+
+	[Tooltip("Min distance players could be teleported")]
+	public int minTeleportDistance = 1;
+	[Tooltip("Max distance players could be teleported")]
+	public int maxTeleportDistance = 5;
+
+	[Tooltip("How long you have to wait before teleporting another target. i.e Teleport Cooldown")]
+	[SerializeField]
+	private int delay = 5;
+	/// <summary>
+	/// if you can teleport a target
+	/// </summary>
+	private bool canTeleport = true;
+
+	/// <summary>
+	/// Sounds to play when teleporting someone
+	/// </summary>
+	[SerializeField] private AddressableAudioSource teleportSound = null;
+
+	private int timer = 0;
+
+	//Send only one message per second.
+	private bool coolDownMessage;
+
+	public bool WillInteract(HandApply interaction, NetworkSide side)
+	{
+		if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+		return interaction.UsedObject == gameObject
+			&& interaction.TargetObject.GetComponent<RegisterPlayer>();
+	}
+
+	public void ServerPerformInteraction(HandApply interaction)
+	{
+		GameObject target = interaction.TargetObject;
+		GameObject performer = interaction.Performer;
+
+		// Direction for lerp
+		Vector2 dir = (target.transform.position - performer.transform.position).normalized;
+
+		WeaponNetworkActions wna = performer.GetComponent<WeaponNetworkActions>();
+
+		// If we're on harm intent we deal damage!
+		// Note: this has to be done before the teleport, otherwise the target may be moved out of range.
+		if (interaction.Intent == Intent.Harm)
+		{
+			// Direction of attack towards the attack target.
+			wna.ServerPerformMeleeAttack(target, dir, interaction.TargetBodyPart, LayerType.None);
+		}
+
+		RegisterPlayer registerPlayerVictim = target.GetComponent<RegisterPlayer>();
+
+		// Teleport the victim. We check if there is a cooldown preventing the attacker from teleporting the victim.
+		if (registerPlayerVictim && canTeleport)
+		{
+			// deactivates the teleport and makes you wait.
+			if (delay != 0)
+			{
+				canTeleport = false;
+				timer = delay;
+			}
+
+			TeleportUtils.ServerTeleportRandom(target, minTeleportDistance, maxTeleportDistance, avoidSpace, avoidImpassable);
+
+			SoundManager.PlayNetworkedAtPos(teleportSound, target.transform.position, sourceObj: target.gameObject);
+
+			// Special case: If we're off harm intent (only teleporting), we should still show the lerp (unless we're hitting ourselves).
+			if (interaction.Intent != Intent.Harm && performer != target)
+			{
+				wna.RpcMeleeAttackLerp(dir, gameObject);
+			}
+		}
+		else if (!canTeleport)
+		{
+			if (coolDownMessage) return;
+			coolDownMessage = true;
+			Chat.AddExamineMsg(performer, $"{gameObject.ExpensiveName()} is on cooldown.");
+		}
+	}
+
+	private void OnEnable()
+	{
+		UpdateManager.Add(Timer, 1f);
+	}
+
+	private void OnDisable()
+	{
+		UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, Timer);
+	}
+
+	private void Timer()
+	{
+		if (timer == 0) return;
+
+		timer--;
+
+		coolDownMessage = false;
+
+		if (timer == 0)
+		{
+			canTeleport = true;
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Items/Weapons/Melee/Teleprod.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AddressableReferences;
+using Mirror;
+using UnityEngine;
+
+/// <summary>
+/// Logic for toggling a teleprod on and off, same as the logic for StunBatonV2 but uses MeleeTeleport instead.
+/// Both of these could probably be compressed into one class, but they are seperate for now so its less finicky.
+/// </summary>
+[RequireComponent(typeof(Pickupable))]
+[RequireComponent(typeof(MeleeTeleport))]
+public class Teleprod : NetworkBehaviour, ICheckedInteractable<HandActivate>, IServerSpawn
+{
+	private SpriteHandler spriteHandler;
+
+	private MeleeTeleport meleeTeleport;
+	
+	// Sound played when turning this prod on/off.
+	public AddressableAudioSource ToggleSound;
+
+	///Both used as states for the teleprod and for the sub-catalogue in the sprite handler.
+	private enum ProdState
+	{
+		Off,
+		On,
+		NoCell
+	}
+
+	private ProdState prodState;
+
+	private void Awake()
+	{
+		spriteHandler = GetComponentInChildren<SpriteHandler>();
+		meleeTeleport = GetComponent<MeleeTeleport>();
+	}
+
+	// Calls TurnOff() when teleprod is spawned, see below.
+	public void OnSpawnServer(SpawnInfo info)
+	{
+		TurnOff();
+	}
+
+	private void TurnOn()
+	{
+		meleeTeleport.enabled = true;
+		prodState = ProdState.On;
+		spriteHandler.ChangeSprite((int) ProdState.On);
+	}
+
+	private void TurnOff()
+	{
+		//logic to turn the teleprod off.
+		meleeTeleport.enabled = false;
+		prodState = ProdState.Off;
+		spriteHandler.ChangeSprite((int) ProdState.Off);
+	}
+
+	//For making sure the user is actually conscious.
+	public bool WillInteract(HandActivate interaction, NetworkSide side)
+	{
+		return DefaultWillInteract.Default(interaction, side);
+	}
+
+	//Activating the teleprod in-hand turns it off or off depending on its state.
+	public void ServerPerformInteraction(HandActivate interaction)
+	{
+		SoundManager.PlayNetworkedAtPos(ToggleSound, interaction.Performer.AssumedWorldPosServer(), sourceObj: interaction.Performer);
+		if (prodState == ProdState.Off)
+		{
+			TurnOn();
+		}
+		else
+		{
+			TurnOff();
+		}
+	}
+}
