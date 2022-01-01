0db2c23e51d426aeda9a9da346bb4341cd3a5dfc
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-01-02 00:32:03 +0200

    Update Huggable.cs

--- UnityProject/Assets/Scripts/Player/Huggable.cs
+++ UnityProject/Assets/Scripts/Player/Huggable.cs
@@ -1,142 +1,144 @@
-using System.Threading.Tasks;
 using HealthV2;
 using Messages.Server.SoundMessages;
 using UnityEngine;
 using System.Collections.Generic;
 
 
-/// <summary>
-/// Allows an object to be hugged by a player.
-/// </summary>
-public class Huggable : MonoBehaviour, ICheckedInteractable<HandApply>, ICooldown
+namespace Player
 {
-	private HandApply interaction;
-	private string performerName;
-	private string targetName;
-
-	public float DefaultTime { get; } = 15f;
-
 	/// <summary>
-	/// The chance of being gibbed after pulling a tail multiple times.
-	/// </summary>
-	private readonly float tailPullJudgementChance = 0.4f;
-	/// <summary>
-	/// Who was the last one to pull a tail?
-	/// </summary>
-	private LivingHealthMasterBase lastPuller;
+    /// Allows an object to be hugged by a player.
+    /// </summary>
+    public class Huggable : MonoBehaviour, ICheckedInteractable<HandApply>, ICooldown
+    {
+    	private HandApply interaction;
+    	private string performerName;
+    	private string targetName;
 
-	[SerializeField] private ItemTrait tailTrait;
+    	public float DefaultTime { get; } = 15f;
 
-	public bool WillInteract(HandApply interaction, NetworkSide side)
-	{
-		if (!DefaultWillInteract.Default(interaction, side)) return false;
-		if (interaction.Intent != Intent.Help) return false;
-		if (interaction.HandObject != null) return false;
-		if (interaction.TargetObject == interaction.Performer) return false;
+    	/// <summary>
+    	/// The chance of being gibbed after pulling a tail multiple times.
+    	/// </summary>
+    	private readonly float tailPullJudgementChance = 0.4f;
+    	/// <summary>
+    	/// Who was the last one to pull a tail?
+    	/// </summary>
+    	private LivingHealthMasterBase lastPuller;
 
-		if (interaction.TargetObject.TryGetComponent(out PlayerHealthV2 targetPlayerHealth))
-		{
-			if (targetPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS) return false;
-		}
+    	[SerializeField] private ItemTrait tailTrait;
 
-		var performerRegisterPlayer = interaction.Performer.GetComponent<RegisterPlayer>();
-		if (performerRegisterPlayer.IsLayingDown) return false;
+    	public bool WillInteract(HandApply interaction, NetworkSide side)
+    	{
+    		if (!DefaultWillInteract.Default(interaction, side)) return false;
+    		if (interaction.Intent != Intent.Help) return false;
+    		if (interaction.HandObject != null) return false;
+    		if (interaction.TargetObject == interaction.Performer) return false;
 
-		return true;
-	}
+    		if (interaction.TargetObject.TryGetComponent(out PlayerHealthV2 targetPlayerHealth))
+    		{
+    			if (targetPlayerHealth.ConsciousState != ConsciousState.CONSCIOUS) return false;
+    		}
 
-	public void ServerPerformInteraction(HandApply interaction)
-	{
-		this.interaction = interaction;
-		performerName = interaction.Performer.ExpensiveName();
-		targetName = interaction.TargetObject.ExpensiveName();
+    		var performerRegisterPlayer = interaction.Performer.GetComponent<RegisterPlayer>();
+    		if (performerRegisterPlayer.IsLayingDown) return false;
 
-		if (interaction.TargetObject.TryGetComponent(out RegisterPlayer targetRegisterPlayer) && targetRegisterPlayer.IsLayingDown)
-		{
-			HelpUp();
-		}
-		else if (!TryFieryHug())
-		{
-			Hug();
-		}
+    		return true;
+    	}
 
-		AudioSourceParameters audioSourceParameters = new AudioSourceParameters(pitch: Random.Range(0.8f, 1.2f));
-		SoundManager.PlayNetworkedAtPos(
-				CommonSounds.Instance.ThudSwoosh, interaction.TargetObject.WorldPosServer(), audioSourceParameters, sourceObj: interaction.TargetObject);
-	}
+    	public void ServerPerformInteraction(HandApply interaction)
+    	{
+    		this.interaction = interaction;
+    		performerName = interaction.Performer.ExpensiveName();
+    		targetName = interaction.TargetObject.ExpensiveName();
 
-	// TODO Consider moving this into its own component, or merging Huggable, this and CPRable into
-	// one "Helpable" component.
-	private void HelpUp()
-	{
-		var targetRegisterPlayer = interaction.TargetObject.GetComponent<RegisterPlayer>();
-		targetRegisterPlayer.ServerHelpUp();
+    		if (interaction.TargetObject.TryGetComponent(out RegisterPlayer targetRegisterPlayer) && targetRegisterPlayer.IsLayingDown)
+    		{
+    			HelpUp();
+    		}
+    		else if (!TryFieryHug())
+    		{
+    			Hug();
+    		}
 
-		Chat.AddActionMsgToChat(interaction.Performer,
-				$"You try to help {targetName} up.", $"{performerName} tries to help {targetName} up.");
-		Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} tries help you up!");
-	}
+    		AudioSourceParameters audioSourceParameters = new AudioSourceParameters(pitch: Random.Range(0.8f, 1.2f));
+    		SoundManager.PlayNetworkedAtPos(
+    				CommonSounds.Instance.ThudSwoosh, interaction.TargetObject.WorldPosServer(), audioSourceParameters, sourceObj: interaction.TargetObject);
+    	}
 
-	private bool TryFieryHug()
-	{
-		var performerLHB = interaction.Performer.GetComponent<LivingHealthMasterBase>();
-		var targetLHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
+    	// TODO Consider moving this into its own component, or merging Huggable, this and CPRable into
+    	// one "Helpable" component.
+    	private void HelpUp()
+    	{
+    		var targetRegisterPlayer = interaction.TargetObject.GetComponent<RegisterPlayer>();
+    		targetRegisterPlayer.ServerHelpUp();
 
-		if (performerLHB != null && targetLHB != null && (performerLHB.FireStacks > 0 || targetLHB.FireStacks > 0))
-		{
-			performerLHB.ApplyDamageAll(interaction.TargetObject, 1, AttackType.Fire, DamageType.Burn);
-			targetLHB.ApplyDamageAll(interaction.Performer, 1, AttackType.Fire, DamageType.Burn);
+    		Chat.AddActionMsgToChat(interaction.Performer,
+    				$"You try to help {targetName} up.", $"{performerName} tries to help {targetName} up.");
+    		Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} tries help you up!");
+    	}
 
-			Chat.AddCombatMsgToChat(
-					interaction.Performer, $"You hug {targetName} with fire!", $"{performerName} hugs {targetName} with fire!");
-			Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} hugs you with fire!");
+    	private bool TryFieryHug()
+    	{
+    		var performerLHB = interaction.Performer.GetComponent<LivingHealthMasterBase>();
+    		var targetLHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
 
-			return true;
-		}
+    		if (performerLHB != null && targetLHB != null && (performerLHB.FireStacks > 0 || targetLHB.FireStacks > 0))
+    		{
+    			performerLHB.ApplyDamageAll(interaction.TargetObject, 1, AttackType.Fire, DamageType.Burn);
+    			targetLHB.ApplyDamageAll(interaction.Performer, 1, AttackType.Fire, DamageType.Burn);
 
-		return false;
-	}
+    			Chat.AddCombatMsgToChat(
+    					interaction.Performer, $"You hug {targetName} with fire!", $"{performerName} hugs {targetName} with fire!");
+    			Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} hugs you with fire!");
 
-	private void Judgement(LivingHealthMasterBase puller)
-	{
-		if (Cooldowns.TryStart(
-			    interaction.PerformerPlayerScript, this, NetworkSide.Server) == false)
-		{
-			lastPuller = null;
-		}
-		if (lastPuller == null || lastPuller != puller)
-		{
-			lastPuller = puller;
-			return;
-		}
-		if (DMMath.Prob(tailPullJudgementChance))
-		{
-			Chat.AddExamineMsg(puller.gameObject, $"<color=red><size=+24>You have been judged for your lust..</size></color>");
-			puller.Gib();
-		}
-	}
+    			return true;
+    		}
 
-	private bool PullTail()
-	{
-		var performerLHB = interaction.Performer.GetComponent<LivingHealthMasterBase>();
-		var targetLHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
-		if (targetLHB == null) return false;
-		foreach (var possibleTail in targetLHB.BodyPartList)
-		{
-			if (possibleTail.gameObject.Item().HasTrait(tailTrait) == false) continue;
-			Chat.AddActionMsgToChat(interaction.Performer, $"<color=#be2596>You pull on {targetName}'s tail.</color>",
-				$"<color=#be2596>{performerName} pulls on {targetName}'s tail!</color>");
-			Chat.AddExamineMsgFromServer(interaction.TargetObject, $"<color=#be2596>{performerName} hugs you.</color>");
-			Judgement(performerLHB);
-			return true;
-		}
-		return false;
-	}
+    		return false;
+    	}
 
-	private void Hug()
-	{
-		if(interaction.TargetBodyPart == BodyPartType.Groin && PullTail()) return;
-		Chat.AddActionMsgToChat(interaction.Performer, $"You hug {targetName}.", $"{performerName} hugs {targetName}.");
-		Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} hugs you.");
-	}
+    	private void Judgement(LivingHealthMasterBase puller)
+    	{
+    		if (Cooldowns.TryStart(
+    			    interaction.PerformerPlayerScript, this, NetworkSide.Server) == false)
+    		{
+    			lastPuller = null;
+    		}
+    		if (lastPuller == null || lastPuller != puller)
+    		{
+    			lastPuller = puller;
+    			return;
+    		}
+    		if (DMMath.Prob(tailPullJudgementChance))
+    		{
+    			Chat.AddExamineMsg(puller.gameObject, $"<color=red><size=+24>You have been judged for your lust..</size></color>");
+    			puller.Gib();
+    		}
+    	}
+
+    	private bool PullTail()
+    	{
+    		var performerLHB = interaction.Performer.GetComponent<LivingHealthMasterBase>();
+    		var targetLHB = interaction.TargetObject.GetComponent<LivingHealthMasterBase>();
+    		if (targetLHB == null) return false;
+    		foreach (var possibleTail in targetLHB.BodyPartList)
+    		{
+    			if (possibleTail.gameObject.Item().HasTrait(tailTrait) == false) continue;
+    			Chat.AddActionMsgToChat(interaction.Performer, $"<color=#be2596>You pull on {targetName}'s tail.</color>",
+    				$"<color=#be2596>{performerName} pulls on {targetName}'s tail!</color>");
+    			Chat.AddExamineMsgFromServer(interaction.TargetObject, $"<color=#be2596>{performerName} hugs you.</color>");
+    			Judgement(performerLHB);
+    			return true;
+    		}
+    		return false;
+    	}
+
+    	private void Hug()
+    	{
+    		if(interaction.TargetBodyPart == BodyPartType.Groin && PullTail()) return;
+    		Chat.AddActionMsgToChat(interaction.Performer, $"You hug {targetName}.", $"{performerName} hugs {targetName}.");
+    		Chat.AddExamineMsgFromServer(interaction.TargetObject, $"{performerName} hugs you.");
+    	}
+    }
 }
