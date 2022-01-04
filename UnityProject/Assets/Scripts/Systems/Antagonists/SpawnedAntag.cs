02e700ff0f5a4d686f8525b0b173a6ad63287aae
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-01-04 20:14:40 +0200

    Update SpawnedAntag.cs

--- UnityProject/Assets/Scripts/Systems/Antagonists/SpawnedAntag.cs
+++ UnityProject/Assets/Scripts/Systems/Antagonists/SpawnedAntag.cs
@@ -76,6 +76,17 @@ namespace Antagonists
 			}
 			// Adding back italic tag so rich text doesn't break
 			objSB.AppendLine("<i>");
+			if (Antagonist.AntagJobType == JobType.TRAITOR || Antagonist.AntagJobType == JobType.SYNDICATE)
+			{
+				var playerInventory = Owner.body.DynamicItemStorage.GetItemSlots();
+				foreach (var item in playerInventory)
+				{
+					if(item.IsEmpty) continue;
+					if (item.ItemObject.TryGetComponent<PDALogic>(out var PDA) == false) continue;
+					if(PDA.IsUplinkCapable == false) continue;
+					objSB.AppendLine($"Your uplink code is {PDA.UplinkUnlockCode}");
+				}
+			}
 			return objSB.ToString();
 		}
 
