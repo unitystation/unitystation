a642af83abd1f826438591b576ab96ddbdbde150
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-01-09 20:36:52 +0000

    Stuff

--- UnityProject/Assets/Scripts/Systems/CraftingV2/ClientServerLogic/RequestInitRecipes.cs
+++ UnityProject/Assets/Scripts/Systems/CraftingV2/ClientServerLogic/RequestInitRecipes.cs
@@ -12,6 +12,14 @@ namespace Systems.CraftingV2.ClientServerLogic
 
 		public override void Process(NetMessage msg)
 		{
+			if (SentByPlayer == null) return;
+
+			if (SentByPlayer.Script == null)
+			{
+				Logger.LogError($"{SentByPlayer.Username} has null script and asked for recipes");
+				return;
+			}
+
 			SendInitRecipesOrder.SendTo(
 				SentByPlayer,
 				SentByPlayer.Script.PlayerCrafting.KnownRecipesByCategory
