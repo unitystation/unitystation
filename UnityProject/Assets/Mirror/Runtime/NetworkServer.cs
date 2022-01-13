0c113127fc1caec79769045c184446bc496e1288
Bod9001 <Bod9001@gmail.com>
2022-01-13 11:44:31 +0000

    Update UnityProject/Assets/Mirror/Runtime/NetworkServer.cs
    Co-authored-by: ThatDan123 <56727168+ThatDan123@users.noreply.github.com>

--- UnityProject/Assets/Mirror/Runtime/NetworkServer.cs
+++ UnityProject/Assets/Mirror/Runtime/NetworkServer.cs
@@ -937,7 +937,7 @@ namespace Mirror
 
                 ///CUSTOM UNITYSTATION CODE///
                 //Mirror specifically says that they dont support NetworkIdentities nested in normal gameobjects.
-                //But, we do it, so we have to override this and make sure we read and send world pos
+                //But, we do it, so we have to override this and make sure we read and send local pos
                 message.position = identity.transform.localPosition;
                 ///CUSTOM UNITYSTATION CODE///
 
