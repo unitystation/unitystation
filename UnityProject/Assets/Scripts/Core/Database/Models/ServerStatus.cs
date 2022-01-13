a29ec96c2c8c45d080dceda26408699d8c19fbc3
corp-0 <andresfranco.rs@gmail.com>
2022-01-13 06:32:14 -0300

    refactor: move some models outside of ServerData file

---
+++ UnityProject/Assets/Scripts/Core/Database/Models/ServerStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.Database.Models
+{
+	[Serializable]
+	public class ServerStatus
+	{
+		public string ServerName;
+		public string ForkName;
+		public int BuildVersion;
+		public string CurrentMap;
+		public string GameMode;
+		public string IngameTime;
+		public int PlayerCount;
+		public string ServerIP;
+		public int ServerPort;
+		public string WinDownload;
+		public string OSXDownload;
+		public string LinuxDownload;
+		public int fps;
+	}
+}
--- UnityProject/Assets/Scripts/Core/Database/ServerData.cs
+++ UnityProject/Assets/Scripts/Core/Database/ServerData.cs
@@ -163,32 +163,4 @@ namespace DatabaseAPI
 		public string refreshToken;
 		public string userID;
 	}
-
-	[Serializable]
-	public class FireStoreResponse
-	{
-		public FireStoreError error;
-		public string name;
-		public FireStoreFields fields;
-	}
-
-	[Serializable]
-	public class FireStoreError
-	{
-		public ushort code;
-		public string message;
-		public string status;
-	}
-
-	[Serializable]
-	public class FireStoreFields
-	{
-		public FireStoreCharacter character;
-	}
-
-	[Serializable]
-	public class FireStoreCharacter
-	{
-		public string stringValue;
-	}
 }
