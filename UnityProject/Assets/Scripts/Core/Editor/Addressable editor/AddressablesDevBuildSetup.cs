3eb1c4497d76149e0bb6118e4728a2cf20968410
Bod9001 <Bod9001@gmail.com>
2022-01-25 15:55:31 +0000

    Ready players fix (#8201)
    * Fixes addressable bundles building and Notifies players of Not enough players
    
    * Better implementation
    
    Co-authored-by: ThatDan123 <56727168+ThatDan123@users.noreply.github.com>

--- UnityProject/Assets/Scripts/Core/Editor/Addressable editor/AddressablesDevBuildSetup.cs
+++ UnityProject/Assets/Scripts/Core/Editor/Addressable editor/AddressablesDevBuildSetup.cs
@@ -92,7 +92,7 @@ public class AddressablesDevBuildSetup : IPreprocessBuild
 			{
 				var newID = IDs[i].ToString();
 				newID = newID.Replace("AddressablePackingProjects/" + flip.Directory.Parent.Name + "/ServerData",
-					"unitystation_Data/StreamingAssets/AddressableCatalogues/"+  flip.Directory.Parent.Name + "");
+					"unitystation_Data/StreamingAssets/AddressableCatalogues/"+  flip.Directory.Parent.Name);
 				//Assets < Editor, build > unitystation_Data
 				//Check cache in app data if changes aren't applying
 				IDs[i] = newID;
