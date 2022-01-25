5d0a1878fbc03ffaf56d76fe8887ca1653f94219
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-01-25 15:33:52 +0000

    Update MapList.cs

--- UnityProject/Assets/Scripts/Map/MapList.cs
+++ UnityProject/Assets/Scripts/Map/MapList.cs
@@ -35,25 +35,27 @@ public class MapList
 			mapsToChooseFrom = highPopMaps;
 		}
 		
+		// Check that we can actually load the scene.
+		mapsToChooseFrom = mapsToChooseFrom.Where(map => SceneUtility.GetBuildIndexByScenePath(map) > -1).ToList();
+
 		if (mapsToChooseFrom.Count == 0)
 		{
 			Logger.LogError($"No maps with playerCount: {playerCount} were found, trying to pick any map now.");
+			
 			var allMaps = new List<string>();
 			allMaps.AddRange(lowPopMaps);
 			allMaps.AddRange(medPopMaps);
 			allMaps.AddRange(highPopMaps);
 
-			mapsToChooseFrom = allMaps;
+			// Check that we can actually load the scene.
+			mapsToChooseFrom = allMaps.Where(map => SceneUtility.GetBuildIndexByScenePath(map) > -1).ToList();
 		}
 
-		// Check that we can actually load the scene.
-		var validMaps = mapsToChooseFrom.Where(map => SceneUtility.GetBuildIndexByScenePath(map) > -1).ToList();
-
-		if (validMaps.Count == 0)
+		if (mapsToChooseFrom.Count == 0)
 		{
 			Logger.LogError("No valid maps found! Make sure theres a map inside the Maps.json that is also in the build settings");
 		}
-		
-		return validMaps.PickRandom();
+
+		return mapsToChooseFrom.PickRandom();
 	}
 }
