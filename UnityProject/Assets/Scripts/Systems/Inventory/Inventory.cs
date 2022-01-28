2026295c9be5ae29a263344fd020dc979a02056a
Pyrosshade <willofshadow@gmail.com>
2022-01-27 19:44:07 -0500

    wire and construction dupe fixes

--- UnityProject/Assets/Scripts/Systems/Inventory/Inventory.cs
+++ UnityProject/Assets/Scripts/Systems/Inventory/Inventory.cs
@@ -82,8 +82,7 @@ public static class Inventory
 		var stackable = fromSlot.ItemObject.GetComponent<Stackable>();
 		if (stackable != null)
 		{
-			stackable.ServerConsume(amountToConsume);
-			return true;
+			return stackable.ServerConsume(amountToConsume);
 		}
 		else
 		{
@@ -431,6 +430,12 @@ public static class Inventory
 		foreach (var onMove in pickupable.GetComponents<IServerInventoryMove>())
 		{
 			onMove.OnInventoryMoveServer(toPerform);
+		}
+		
+		if (pickupable.gameObject.TryGetComponent<Stackable>(out var stack))
+		{
+			var cnt = pickupable.GetComponent<CustomNetTransform>();
+			stack.ServerStackOnGround(cnt.ServerLocalPosition);
 		}
 
 		if (pickupable.gameObject.TryGetComponent<Stackable>(out var stack))
