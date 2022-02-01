ef7d275039e5520bbf77f9428a4d309a781c51f7
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-02-01 04:06:31 +0200

    Update ChatRelay.cs

--- UnityProject/Assets/Scripts/Core/Chat/ChatRelay.cs
+++ UnityProject/Assets/Scripts/Core/Chat/ChatRelay.cs
@@ -263,6 +263,7 @@ public class ChatRelay : NetworkBehaviour
 						    && listener != chatEvent.originator)
 						{
 							listener.SendData(chatEvent);
+							SBRSpamCheck = true;
 						}
 					}
 				}
