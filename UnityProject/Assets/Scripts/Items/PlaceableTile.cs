847b4d418529a952127ba313c0d53472e6c6716d
Pyrosshade <willofshadow@gmail.com>
2022-02-09 12:50:22 -0500

    Dechonk: the privating

--- UnityProject/Assets/Scripts/Items/PlaceableTile.cs
+++ UnityProject/Assets/Scripts/Items/PlaceableTile.cs
@@ -65,7 +65,6 @@ public class PlaceableTile : MonoBehaviour, ICheckedInteractable<PositionalHandA
 	{
 		//which matrix are we clicking on
 
-
 		var matrix = InteractableTiles.TryGetNonSpaceMatrix(interaction.WorldPositionTarget.RoundToInt(), true);
 
 		if (matrix.IsSpaceMatrix)
@@ -87,6 +86,80 @@ public class PlaceableTile : MonoBehaviour, ICheckedInteractable<PositionalHandA
 		{
 			itemAmount = stackable.Amount;
 		}
+
+		placeableTileEntry = FindValidTile(itemAmount, tileAtPosition);
+
+		if (placeableTileEntry != null)
+		{
+			GameObject performer = interaction.Performer;
+			Vector2 targetPosition = interaction.WorldPositionTarget;
+
+
+			void ProgressFinishAction()
+			{
+				ProgressFinishActionPriv(interaction, placeableTileEntry, interactableTiles, cellPos, targetPosition);
+			}
+
+			void ProgressInterruptedAction(ActionInterruptionType reason)
+			{
+				ProgressInterruptedActionPriv(interaction, reason, placeableTileEntry);
+			}
+
+			var bar = StandardProgressAction.Create(ProgressConfig, ProgressFinishAction,ProgressInterruptedAction)
+				.ServerStartProgress(targetPosition, placeableTileEntry.timeToPlace, performer);
+		}
+	}
+
+	private static void ProgressInterruptedActionPriv(PositionalHandApply interaction, ActionInterruptionType reason,
+		PlaceableTileEntry placeableTileEntry)
+	{
+		switch (reason)
+		{
+			case ActionInterruptionType.ChangeToPerformerActiveSlot:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"You lack the materials to finish placing {placeableTileEntry.layerTile.DisplayName}");
+				break;
+			case ActionInterruptionType.PerformerUnconscious:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"You dream of placing {placeableTileEntry.layerTile.DisplayName}");
+				break;
+			case ActionInterruptionType.WelderOff:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"You need the welder to be on to finish this");
+				break;
+			case ActionInterruptionType.PerformerOrTargetMoved:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"Cannot place {placeableTileEntry.layerTile.DisplayName}, you or your target moved too much");
+				break;
+			case ActionInterruptionType.TargetDespawn:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"Placing {placeableTileEntry.layerTile.DisplayName} has become a tall order, seeing as the conceptual physical placement space has ceased to exist");
+				break;
+			case ActionInterruptionType.PerformerDirection:
+				Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+					$"You have looked away from placing {placeableTileEntry.layerTile.DisplayName}");
+				break;
+		}
+	}
+
+	private void ProgressFinishActionPriv(PositionalHandApply interaction, PlaceableTileEntry placeableTileEntry,
+		InteractableTiles interactableTiles, Vector3Int cellPos, Vector2 targetPosition)
+	{
+		if (Inventory.ServerConsume(interaction.HandSlot, placeableTileEntry.itemCost))
+		{
+			interactableTiles.TileChangeManager.MetaTileMap.SetTile(cellPos, placeableTileEntry.layerTile);
+			interactableTiles.TileChangeManager.SubsystemManager.UpdateAt(cellPos);
+			SoundManager.PlayNetworkedAtPos(placeSound, targetPosition);
+		}
+		else
+		{
+			Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
+				$"You lack the materials to finish placing {placeableTileEntry.layerTile.DisplayName}");
+		}
+	}
+
+	private PlaceableTileEntry FindValidTile(int itemAmount, LayerTile tileAtPosition)
+	{
 		// find the first valid way possible to place a tile
 		foreach (var entry in waysToPlace)
 		{
@@ -97,75 +170,17 @@ public class PlaceableTile : MonoBehaviour, ICheckedInteractable<PositionalHandA
 			//open space
 			if (tileAtPosition == null && entry.placeableOn == LayerType.None && entry.placeableOnlyOnTile == null)
 			{
-				placeableTileEntry = entry;
-				break;
+				return entry;
 			}
 
 			// placing on an existing tile
-			else if (tileAtPosition.LayerType == entry.placeableOn && (entry.placeableOnlyOnTile == null || entry.placeableOnlyOnTile == tileAtPosition))
+			else if (tileAtPosition.LayerType == entry.placeableOn &&
+			         (entry.placeableOnlyOnTile == null || entry.placeableOnlyOnTile == tileAtPosition))
 			{
-				placeableTileEntry = entry;
-				break;
+				return entry;
 			}
 		}
-
-		if (placeableTileEntry != null)
-		{
-			GameObject performer = interaction.Performer;
-			Vector2 targetPosition = interaction.WorldPositionTarget;
-
-
-			void ProgressFinishAction()
-			{
-				if (Inventory.ServerConsume(interaction.HandSlot, placeableTileEntry.itemCost))
-				{
-				interactableTiles.TileChangeManager.MetaTileMap.SetTile(cellPos, placeableTileEntry.layerTile);
-				interactableTiles.TileChangeManager.SubsystemManager.UpdateAt(cellPos);
-				SoundManager.PlayNetworkedAtPos(placeSound, targetPosition);
-				}
-				else
-				{
-					Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-						$"You lack the materials to finish placing {placeableTileEntry.layerTile.DisplayName}");
-				}
-			}
-
-			void ProgressInterruptedAction(ActionInterruptionType reason)
-			{
-				switch (reason)
-				{
-					case ActionInterruptionType.ChangeToPerformerActiveSlot:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"You lack the materials to finish placing {placeableTileEntry.layerTile.DisplayName}");
-						break;
-					case ActionInterruptionType.PerformerUnconscious:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"You dream of placing {placeableTileEntry.layerTile.DisplayName}");
-						break;
-					case ActionInterruptionType.WelderOff:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"You need the welder to be on to finish this");
-						break;
-					case ActionInterruptionType.PerformerOrTargetMoved:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"Cannot place {placeableTileEntry.layerTile.DisplayName}, you or your target moved too much");
-						break;
-					case ActionInterruptionType.TargetDespawn:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"Placing {placeableTileEntry.layerTile.DisplayName} has become a tall order, seeing as the conceptual physical placement space has ceased to exist");
-						break;
-					case ActionInterruptionType.PerformerDirection:
-						Chat.AddExamineMsgFromServer(interaction.PerformerPlayerScript.connectedPlayer,
-							$"You have looked away from placing {placeableTileEntry.layerTile.DisplayName}");
-						break;
-
-				}
-
-			}
-
-			var bar = StandardProgressAction.Create(ProgressConfig, ProgressFinishAction,ProgressInterruptedAction)
-				.ServerStartProgress(targetPosition, placeableTileEntry.timeToPlace, performer);
-		}
+		return null;
 	}
 
 	/// <summary>
