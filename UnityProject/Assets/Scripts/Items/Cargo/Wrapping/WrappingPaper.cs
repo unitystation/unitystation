5cc6b033e0a3a3e7294b017999935dcc7786d987
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-02-15 22:14:16 +0200

    Update WrappingPaper.cs

--- UnityProject/Assets/Scripts/Items/Cargo/Wrapping/WrappingPaper.cs
+++ UnityProject/Assets/Scripts/Items/Cargo/Wrapping/WrappingPaper.cs
@@ -30,8 +30,8 @@ namespace Items.Cargo.Wrapping
 
 		public bool WillInteract(HandApply interaction, NetworkSide side)
 		{
-			if (interaction.IsAltClick == false) return false;
-			return DefaultWillInteract.Default(interaction, side) &&
+
+			return return interaction.IsAltClick == true && DefaultWillInteract.Default(interaction, side) &&
 			       CommonWillInteract(interaction, side);
 		}
 
@@ -46,8 +46,7 @@ namespace Items.Cargo.Wrapping
 
 		public bool WillInteract(InventoryApply interaction, NetworkSide side)
 		{
-			if (interaction.IsAltClick == false) return false;
-			return interaction.IsToHandSlot &&
+			return interaction.IsAltClick == true && interaction.IsToHandSlot &&
 				   DefaultWillInteract.Default(interaction, side) &&
 			       CommonWillInteract(interaction, side);
 		}
