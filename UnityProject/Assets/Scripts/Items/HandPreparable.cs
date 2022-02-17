ca42acd65f7f915eff186f7426cc3e29a96d7a38
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-02-17 20:37:37 +0200

    e

---
+++ UnityProject/Assets/Scripts/Items/HandPreparable.cs
@@ -0,0 +1,47 @@
+﻿using AddressableReferences;
+using UnityEngine;
+
+namespace Items
+{
+	public class HandPreparable : MonoBehaviour, IInteractable<HandActivate>, IRightClickable
+	{
+		private bool isPrepared = false;
+		public bool IsPrepared => isPrepared;
+
+
+		[SerializeField] private SpriteHandler spriteHandler;
+		[Header("Feedback")]
+		[SerializeField] private AddressableAudioSource openingNoise;
+		[SerializeField] private SpriteDataSO openedSprite;
+		[Header("Chat Stuff")]
+		[SerializeField] private string openingVerb = "open";
+		public string openingRequirementText = "You need to open this first before using it!";
+
+		public void ServerPerformInteraction(HandActivate interaction)
+		{
+			TryOpen();
+			Chat.AddExamineMsg(interaction.Performer, $"You {openingVerb} the {gameObject.ExpensiveName()}");
+		}
+
+		public void TryOpen()
+		{
+			if(isPrepared) return;
+			isPrepared = true;
+			if (openingNoise != null) SoundManager.PlayNetworkedAtPos(openingNoise, gameObject.AssumedWorldPosServer());
+			if (openedSprite != null) spriteHandler.SetSpriteSO(openedSprite);
+			Destroy(this);
+		}
+
+		/// <summary>
+		/// Generates a right click button for items like Space Cola where another script gets in the way of HandActivate
+		/// </summary>
+		/// <returns></returns>
+		public RightClickableResult GenerateRightClickOptions()
+		{
+			var rightClickResult = new RightClickableResult();
+			if (isPrepared) return rightClickResult;
+			rightClickResult.AddElement("Open This", TryOpen);
+			return rightClickResult;
+		}
+	}
+}
