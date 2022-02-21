6deafb0be0c7c7bca8e55cd1aca034444f29929d
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-02-21 07:19:34 +0200

    Flares and Glowsticks need to be sparked to light up now (#8405)
    * Flares can now be togglable
    
    * glowsticks
    
    * Update LightPreparable.cs
    
    * Update Flare.prefab
    
    * Update LightPreparable.cs

--- UnityProject/Assets/Scripts/Items/HandPreparable.cs
+++ UnityProject/Assets/Scripts/Items/HandPreparable.cs
@@ -10,11 +10,11 @@ namespace Items
 		public bool IsPrepared => isPrepared;
 
 
-		[SerializeField] private SpriteHandler spriteHandler;
-		[SerializeField] private bool destroyThisComponentOnOpen = true;
+		[SerializeField] protected SpriteHandler spriteHandler;
+		[SerializeField] protected bool destroyThisComponentOnOpen = true;
 		[Header("Feedback")]
 		[SerializeField] private AddressableAudioSource openingNoise;
-		[SerializeField] private SpriteDataSO openedSprite;
+		[SerializeField] protected SpriteDataSO openedSprite;
 		[Header("Chat Stuff")]
 		[SerializeField] private string openingVerb = "open";
 		public string openingRequirementText = "You need to open this first before using it!";
@@ -25,7 +25,7 @@ namespace Items
 			Chat.AddExamineMsg(interaction.Performer, $"You {openingVerb} the {gameObject.ExpensiveName()}");
 		}
 
-		public void Open()
+		public virtual void Open()
 		{
 			isPrepared = true;
 			if (openingNoise != null) SoundManager.PlayNetworkedAtPos(openingNoise, gameObject.AssumedWorldPosServer());
@@ -37,7 +37,7 @@ namespace Items
 		{
 			var rightClickResult = new RightClickableResult();
 			if (isPrepared) return rightClickResult;
-			rightClickResult.AddElement("Open This", Open);
+			rightClickResult.AddElement($"{openingVerb}", Open);
 			return rightClickResult;
 		}
 
