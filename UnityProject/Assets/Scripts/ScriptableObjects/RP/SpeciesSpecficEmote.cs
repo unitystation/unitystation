b8ce504c4f2e6f4b53f58aebbfe7918d2b84519b
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-03-10 14:35:36 +0200

    New emotes pog

---
+++ UnityProject/Assets/Scripts/ScriptableObjects/RP/SpeciesSpecficEmote.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjects.RP
+{
+	[CreateAssetMenu(fileName = "Emote", menuName = "ScriptableObjects/RP/Emotes/SpeciesSpecificEmote")]
+	public class SpeciesSpecificEmote : GenderedEmote
+	{
+
+		[SerializeField] private List<PlayerHealthData> allowedSpecies = new List<PlayerHealthData>();
+		[SerializeField] private string wrongSpeciesText = "Your species can't do that!";
+
+		public override void Do(GameObject player)
+		{
+			if (player.TryGetComponent<PlayerScript>(out var playerScript) == false)
+			{
+				FailText(player, FailType.Normal);
+				return;
+			}
+			var race = CharacterSettings.GetRaceData(playerScript.characterSettings);
+			if (race == null || allowedSpecies.Contains(race) == false)
+			{
+				Chat.AddExamineMsg(player, wrongSpeciesText);
+				return;
+			}
+			base.Do(player);
+		}
+	}
+}
