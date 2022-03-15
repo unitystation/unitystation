22fdebd31385590ae5e046ab16dfa30ecbb7765b
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-03-15 17:16:51 +0200

    Techweb json data (#8339)
    * Techweb stuff part 1
    
    * bazinga
    
    * ayyyyymacarna
    
    * ref
    
    * beep
    
    * oinsaonasfd
    
    * Update Techweb.cs
    
    * Update Techweb.cs
    
    * Update Techweb.cs
    
    * Update Techweb.cs
    
    * sdfgsdfgsdfgsdfgsdfg
    
    * Update TechWebNodeItem.cs
    
    * Update TabTechWeb.prefab
    
    * jghkghjkbnm,bnm,
    
    * Update TabTechWeb.prefab
    
    * jbnjb
    
    * mmm, timer
    
    * Update NetworkManager.prefab
    
    * fasdfasdf
    
    * fasdfasdfascv
    
    * importers exporters
    
    * Update TechwebJSONImporter.cs
    
    * Update Techweb.cs
    
    * should be done?
    
    * Update GameManager.cs
    
    * asdfgcvxzcvaswdfg
    
    * Update ResearchServer.cs
    
    * Update ResearchServer.prefab
    
    * Update Designs.cs
    
    * Update ResearchServer.prefab
    
    * Update TabTechWeb.prefab
    
    * Update Techweb.cs
    
    * Update CraftingManager.prefab
    
    * Update Techweb.cs

---
+++ UnityProject/Assets/Scripts/Systems/Research/Objects/ResearchServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using Mirror;
+using ScriptableObjects.Research;
+using UnityEngine;
+
+namespace Systems.Research.Objects
+{
+	public class ResearchServer : NetworkBehaviour
+	{
+		//TODO: PLACE HOLDER UNTIL WE GET A TECHWEB EDITOR OF SOME SORT
+		[SerializeField] private DefaultTechwebData defaultTechwebData;
+		//TODO: PLACEHOLDER, TECHWEBS SHOULD BE STORED LOCALLY ON IN-GAME DISKS/CIRCUITS TO BE STOLEN AND MERGED
+		[SyncVar] private Techweb techweb = new Techweb();
+		//TODO : PLACEHOLDER, THIS PATH MUST BE ASSIGNED ON THE CIRCUIT/DISK INSTEAD OF ON THE SERVER PREFAB
+		[SerializeField] private string techWebPath = "/GameData/Research/";
+		[SerializeField] private string techWebFileName = "TechwebData.json";
+		[SerializeField] private int researchPointsTrickl = 25;
+		[SerializeField] private int TrickleTime = 60; //seconds
+
+		private void Awake()
+		{
+			if (File.Exists($"{techWebPath}{techWebFileName}") == false) defaultTechwebData.GenerateDefaultData();
+			techweb.LoadTechweb($"{techWebPath}{techWebFileName}");
+			StartCoroutine(TrickleResources());
+		}
+
+		private void OnDisable()
+		{
+			StopCoroutine(TrickleResources());
+		}
+
+		private IEnumerator TrickleResources()
+		{
+			while (this != null || techweb != null)
+			{
+				yield return WaitFor.Seconds(TrickleTime);
+				techweb.AddResearchPoints(researchPointsTrickl);
+			}
+		}
+	}
+}
