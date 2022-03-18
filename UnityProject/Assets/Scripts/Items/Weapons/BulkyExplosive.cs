1a990495a4a3ada80702c2109fc9aacee9f89477
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-03-18 20:38:19 +0200

    Adds in Syndicate Bombs (#8466)
    * Adds in Syndicate Bombs
    
    * brhuh moment
    
    * Fixes ui not working
    
    * Update Explosive.cs
    
    * Separating logic for SB and attachable explosives
    
    * fasdfasd
    
    * doc
    
    * sfdgsdfg
    
    * AAAAAAAAAAAAAAAAAAAAAAAAAAAA
    
    * Update BulkyExplosive.cs
    
    * Update BulkyExplosive.cs
    
    * Fixes display not showing up initially
    
    * becon
    
    * Update NetworkManager.prefab
    
    * beep sounds
    
    * Update BulkyExplosive.cs
    
    * Update AttachableExplosive.cs
    
    * Fixes merge conflicts
    
    * Update packages-lock.json

---
+++ UnityProject/Assets/Scripts/Items/Weapons/BulkyExplosive.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using AddressableReferences;
+using Communications;
+using Mirror;
+using UnityEngine;
+
+namespace Items.Weapons
+{
+	/// <summary>
+	/// Interaction script for Bulky explosives that cannot be picked up and instead pulled.
+	/// </summary>
+	public class BulkyExplosive : ExplosiveBase, ICheckedInteractable<HandApply>
+	{
+		[SerializeField] private ItemTrait wrenchTrait;
+		[SerializeField] private AddressableAudioSource beepSound;
+
+		[Server]
+		public override IEnumerator Countdown()
+		{
+			countDownActive = true;
+			spriteHandler.SetSpriteSO(activeSpriteSO);
+			if (GUI != null) GUI.StartCoroutine(GUI.UpdateTimer());
+			StartCoroutine(BeepBeep());
+			yield return WaitFor.Seconds(timeToDetonate); //Delay is in milliseconds
+			countDownActive = false;
+			Detonate();
+		}
+
+		private IEnumerator BeepBeep()
+		{
+			while (countDownActive && gameObject != null)
+			{
+				SoundManager.PlayNetworkedAtPos(beepSound, gameObject.AssumedWorldPosServer());
+				yield return WaitFor.Seconds(2f);
+			}
+		}
+
+		public bool WillInteract(HandApply interaction, NetworkSide side)
+		{
+			if (explosiveType != ExplosiveType.SyndicateBomb ||
+			    DefaultWillInteract.Default(interaction, side) == false) return false;
+			return true;
+		}
+
+		public void ServerPerformInteraction(HandApply interaction)
+		{
+			if (interaction.HandObject != null && interaction.HandObject.Item().HasTrait(wrenchTrait))
+			{
+				objectBehaviour.ServerSetPushable(!objectBehaviour.IsPushable);
+				SoundManager.PlayNetworkedAtPos(CommonSounds.Instance.Wrench, gameObject.AssumedWorldPosServer());
+				var wrenchText = objectBehaviour.IsPushable ? "wrench down" : "unwrench";
+				Chat.AddExamineMsg(interaction.Performer, $"You {wrenchText} the {gameObject.ExpensiveName()}");
+				return;
+			}
+			if (interaction.HandObject != null && interaction.HandObject.TryGetComponent<SignalEmitter>(out var emitter))
+			{
+				Emitter = emitter;
+				Frequency = emitter.Frequency;
+				Chat.AddExamineMsg(interaction.Performer, "You successfully pair the remote signal to the device.");
+				return;
+			}
+			explosiveGUI.ServerPerformInteraction(interaction);
+		}
+	}
+}
--- UnityProject/Assets/Scripts/UI/Items/GUI_Explosive.cs
+++ UnityProject/Assets/Scripts/UI/Items/GUI_Explosive.cs
@@ -13,12 +13,18 @@ namespace UI.Items
 	/// </summary>
 	public class GUI_Explosive : NetTab
 	{
-		private Explosive explosiveDevice;
+		private ExplosiveBase explosiveDevice;
 
 		[SerializeField] private NetLabel status;
 		[SerializeField] private NetLabel timer;
 		[SerializeField] private NetToggle modeToggleButton;
+		[SerializeField] private NetToggle sbModeToggleButton;
 		[SerializeField] private NetToggle armToggleButton;
+		[SerializeField] private NetToggle sbArmToggleButton;
+		[SerializeField] private GameObject sbButtons;
+		[SerializeField] private GameObject sharedButtons;
+		[SerializeField] private GameObject sbDispalyLoc;
+		[SerializeField] private GameObject sbTimerLoc;
 		[SerializeField] private Image background;
 
 		[SerializeField] private Color safeColor = Color.green;
@@ -26,6 +32,7 @@ namespace UI.Items
 
 		[SerializeField] private Sprite C4Graphic;
 		[SerializeField] private Sprite X4Graphic;
+		[SerializeField] private Sprite SBGraphic;
 
 		private float timerCount;
 
@@ -34,13 +41,35 @@ namespace UI.Items
 			StartCoroutine(WaitForProvider());
 		}
 
+		public void EnsureVisualsAreCorrect()
+		{
+			if(explosiveDevice == null) return; //properly start() has not finished yet
+			switch (explosiveDevice.ExplosiveType)
+			{
+				case ExplosiveType.C4:
+					background.sprite = C4Graphic;
+					break;
+				case ExplosiveType.X4:
+					background.sprite = X4Graphic;
+					break;
+				case ExplosiveType.SyndicateBomb:
+					sbButtons.SetActive(true);
+					sharedButtons.SetActive(false);
+					status.transform.position = sbDispalyLoc.transform.position;
+					timer.transform.position = sbTimerLoc.transform.position;
+					background.sprite = SBGraphic;
+					break;
+			}
+		}
+
 		private IEnumerator WaitForProvider()
 		{
 			while (Provider == null)
 			{
 				yield return WaitFor.EndOfFrame;
 			}
-			explosiveDevice = Provider.GetComponent<Explosive>();
+			sbButtons.SetActive(false);
+			explosiveDevice = Provider.GetComponent<ExplosiveBase>();
 			if (explosiveDevice.DetonateImmediatelyOnSignal == false)
 			{
 				timerCount = explosiveDevice.TimeToDetonate;
@@ -52,28 +81,31 @@ namespace UI.Items
 				status.ElementTmp.color = dangerColor;
 			}
 
-			switch (explosiveDevice.ExplosiveType)
-			{
-				case ExplosiveType.C4:
-					background.sprite = C4Graphic;
-					break;
-				case ExplosiveType.X4:
-					background.sprite = X4Graphic;
-					break;
-			}
+			EnsureVisualsAreCorrect();
 
 			modeToggleButton.Value = explosiveDevice.DetonateImmediatelyOnSignal ? "1" : "0";
 			timerCount = explosiveDevice.TimeToDetonate;
+			timer.Value = DisplayTime();
 			explosiveDevice.GUI = this;
 		}
 
 		public void ArmDevice()
 		{
-			explosiveDevice.IsArmed = armToggleButton.Value == "1";
-			if (modeToggleButton.Value == "0" && armToggleButton.Value == "1")
+			if (explosiveDevice.ExplosiveType == ExplosiveType.SyndicateBomb)
+			{
+				explosiveDevice.IsArmed = sbArmToggleButton.Value == "1";
+			}
+			else
+			{
+				explosiveDevice.IsArmed = armToggleButton.Value == "1";
+			}
+			if ((modeToggleButton.Value == "0" || sbModeToggleButton.Value == "0")
+			    && (armToggleButton.Value == "1" || sbArmToggleButton.Value == "1"))
 			{
 				modeToggleButton.enabled = false;
-				armToggleButton.enabled = true;
+				sbModeToggleButton.enabled = false;
+				armToggleButton.enabled = false;
+				sbArmToggleButton.enabled = false;
 				StartCoroutine(explosiveDevice.Countdown());
 				UpdateStatusText();
 				return;
@@ -83,7 +115,7 @@ namespace UI.Items
 
 		public void ToggleMode()
 		{
-			if(modeToggleButton.Value == "1")
+			if(modeToggleButton.Value == "1" || sbModeToggleButton.Value == "1")
 			{
 				explosiveDevice.ToggleMode(true);
 			}
@@ -96,25 +128,27 @@ namespace UI.Items
 
 		public void IncreaseTimeByOne()
 		{
-			if(armToggleButton.Value == "1") return;
+			if(armToggleButton.Value == "1" || sbArmToggleButton.Value == "1") return;
 			explosiveDevice.TimeToDetonate += 1;
 			StartCoroutine(UpdateTimer());
 		}
 		public void IncreaseTimeByTen()
 		{
-			if (armToggleButton.Value == "1") return;
+			if(armToggleButton.Value == "1" || sbArmToggleButton.Value == "1") return;
 			explosiveDevice.TimeToDetonate += 10;
 			StartCoroutine(UpdateTimer());
 		}
 		public void DecreaseTimeByOne()
 		{
-			if (explosiveDevice.TimeToDetonate  - 1  < explosiveDevice.MinimumTimeToDetonate || armToggleButton.Value == "1") return;
+			if (explosiveDevice.TimeToDetonate  - 1  < explosiveDevice.MinimumTimeToDetonate
+			    || armToggleButton.Value == "1" || sbArmToggleButton.Value == "1") return;
 			explosiveDevice.TimeToDetonate -= 1;
 			StartCoroutine(UpdateTimer());
 		}
 		public void DecreaseTimeByTen()
 		{
-			if (explosiveDevice.TimeToDetonate  - 10 < explosiveDevice.MinimumTimeToDetonate|| armToggleButton.Value == "1") return;
+			if (explosiveDevice.TimeToDetonate  - 10 < explosiveDevice.MinimumTimeToDetonate
+			    || armToggleButton.Value == "1" || sbArmToggleButton.Value == "1") return;
 			explosiveDevice.TimeToDetonate -= 10;
 			StartCoroutine(UpdateTimer());
 		}
