0abaab47c8f5b84b1cb523ee90b090601875b962
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-03-30 01:04:03 +0200

    Adds in Lean (#8525)
    * Adds in Lean
    
    * leantracker
    
    * Fixes tests

---
+++ UnityProject/Assets/Scripts/Items/Food/LeanDrink.cs
@@ -0,0 +1,26 @@
+﻿using HealthV2;
+using UnityEngine;
+
+namespace Items.Food
+{
+	public class LeanDrink : DrinkableContainer
+	{
+		[SerializeField] private float lungDamage = 15f;
+
+		public override void Drink(PlayerScript eater, PlayerScript feeder)
+		{
+			base.Drink(eater, feeder);
+			eater.playerHealth.IndicatePain(lungDamage);
+			foreach (var part in eater.playerHealth.BodyPartList)
+			{
+				foreach (var organ in part.OrganList)
+				{
+					if(organ is Lungs == false) continue;
+					organ.RelatedPart.TakeDamage(null, lungDamage, AttackType.Internal, DamageType.Tox);
+				}
+			}
+			
+			Chat.AddExamineMsg(eater.gameObject, "You feel like your lungs are going to fail at some point because of this..");
+		}
+	}
+}
