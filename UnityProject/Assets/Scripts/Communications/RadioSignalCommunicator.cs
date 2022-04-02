66892138c070ed6da47ba17e6bc1a661bbce789e
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-04-02 23:48:02 +0200

    eeeee

--- UnityProject/Assets/Scripts/Communications/RadioSignalCommunicator.cs
+++ UnityProject/Assets/Scripts/Communications/RadioSignalCommunicator.cs
@@ -1,13 +1,24 @@
-﻿using Managers;
+﻿using System;
+using Managers;
 
 namespace Communications
 {
 	public class RadioSignalCommunicator : SignalEmitter
 	{
 
+		protected RadioSignalProcessor processor;
+
+		private void Awake()
+		{
+			processor = GetComponent<RadioSignalProcessor>();
+		}
+
 		protected override bool SendSignalLogic()
 		{
-			throw new System.NotImplementedException();
+			//the most basic thing the base can check for now
+			//other servers should override this if they want more fine control over the requirements of a successful TrySendSignal()
+			if (processor.requiresPower && processor.isPowered == false) return false;
+			return true;
 		}
 
 		public override void SignalFailed()
