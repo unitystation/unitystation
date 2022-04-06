d16cc4c3acc9a3a8511f3cce5e0955ac4035c37c
Bod9001 <Bod9001@gmail.com>
2022-04-06 19:55:57 +0100

    fixes builds , stop using Castle.Core.Internal;!

--- UnityProject/Assets/Scripts/Systems/Voting/VotingManager.cs
+++ UnityProject/Assets/Scripts/Systems/Voting/VotingManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Castle.Core.Internal;
 using Messages.Client.Admin;
 using Messages.Server;
 using Mirror;
