d8c8ee6ae830b927118c501ec323ba8f3665a72e
Mitchell Kuppersmith <mkuppers@umich.edu>
2022-04-14 21:57:45 -0400

    Added SecBarrier Script
    SecBarrier script checks if player has wrench and if they pass access restriction. Has message denying access and plays access denial sound if they cannot wrench down barrier

---
+++ UnityProject/Assets/Scripts/Objects/Other/SecBarrier.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Objects.Security
+{
+	[RequireComponent(typeof(AccessRestrictions))]
+	[RequireComponent(typeof(Construction.WrenchSecurable))]
+	public class SecBarrier : MonoBehaviour, IPredictedCheckedInteractable<HandApply>
+	{
+		private AccessRestrictions accessRestrictions;
+		public AccessRestrictions AccessRestrictions
+		{
+			get
+			{
+				if (accessRestrictions == false)
+				{
+					accessRestrictions = GetComponent<AccessRestrictions>();
+				}
+				return accessRestrictions;
+			}
+		}
+
+		public bool WillInteract(HandApply interaction, NetworkSide side)
+		{
+			//start with the default HandApply WillInteract logic.
+			if (!DefaultWillInteract.Default(interaction, side)) return false;
+
+			//Only go through with interaction if correct access restriction
+			if (AccessRestrictions.CheckAccess(interaction.Performer))
+			{
+				//Player needs wrench to interact
+				if (Validations.HasItemTrait(interaction.HandObject, CommonTraits.Instance.Wrench))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public void ClientPredictInteraction(HandApply interaction)
+		{
+
+		}
+
+
+		public void ServerRollbackClient(HandApply interaction)
+		{
+
+		}
+
+		//invoked when the server recieves the interaction request and WIllinteract returns true
+		public async void ServerPerformInteraction(HandApply interaction)
+		{
+			if (Validations.HasItemTrait(interaction.HandObject, CommonTraits.Instance.Wrench))
+			{
+				//Notify player that they are unable to wrench down barrier
+				Chat.AddActionMsgToChat(interaction, "You try to wrench down the barrier...",
+					"The barrier denies you access.");
+			}
+			else {
+				Chat.AddActionMsgToChat(interaction, "ACCESS DENIED","");
+			}
+			SoundManager.PlayAtPosition(CommonSounds.Instance.AccessDenied, transform.position);
+		}
+	}
+}
