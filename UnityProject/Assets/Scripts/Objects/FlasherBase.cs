f8c2055cdac6574d4bcb6e2161d400d6188a64cb
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-04-19 11:02:47 +0200

    Update FlasherBase.cs

--- UnityProject/Assets/Scripts/Objects/FlasherBase.cs
+++ UnityProject/Assets/Scripts/Objects/FlasherBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using AddressableReferences;
 using Mirror;
 using NaughtyAttributes;
 using Player;
@@ -10,35 +12,44 @@ namespace Objects
 	{
 		[SerializeField] protected float flashRadius = 12f;
 		[SerializeField, MinMaxSlider(3,20)] protected float flashStrength = 12f;
+		[SerializeField, MinMaxSlider(3,6), ShowIf(nameof(stunsPlayers))] protected float stunExtraTime = 3f;
 		[SerializeField] protected float flashCooldown = 24f;
 		[SerializeField] protected ItemTrait sunglassesTrait;
+		[SerializeField] protected AddressableAudioSource flashSound;
 		[SerializeField] protected bool stunsPlayers = true;
 		[SyncVar] private bool onCooldown = false;
 		public bool OnCooldown => onCooldown;
 		private LayerMask pLayerMask;
 
-		[Server]
-		public void Flash()
+		private void Start()
 		{
-			if(onCooldown) return;
+			//(Max) : How would this work when we get to the mind rework? Will we force all objects that inherit from player to change their mask to "Players"? Or will we make a new one?
+			pLayerMask = LayerMask.GetMask("Players");
+		}
+
+		[Server]
+		public void FlashInRadius()
+		{
+			if (onCooldown) return;
+			if (flashSound != null) _ = SoundManager.PlayNetworkedAtPosAsync(flashSound, gameObject.AssumedWorldPosServer());
 			var possibleTargets = Physics2D.OverlapCircleAll(gameObject.AssumedWorldPosServer(), flashRadius, pLayerMask);
 			foreach (var target in possibleTargets)
 			{
 				if(gameObject == target.gameObject) continue;
-				if(target.gameObject.TryGetComponent<RegisterPlayer>(out var player) == false) continue;
-				if(target.gameObject.TryGetComponent<PlayerFlashEffects>(out var flashEffector) == false) continue;
-				if(target.gameObject.TryGetComponent<DynamicItemStorage>(out var playerStorage) == false) continue;
+				if(target.gameObject.TryGetComponent<RegisterPlayer>(out var player) == false) continue; //If it's not a player, check next
+				if(target.gameObject.TryGetComponent<PlayerFlashEffects>(out var flashEffector) == false) continue; //If the player doesn't have the ability to be flashed for whatever reason, check next
+				if(target.gameObject.TryGetComponent<DynamicItemStorage>(out var playerStorage) == false) continue; //If the player has no storage for whatever reason... would this be a bug?
 				foreach (var slots in playerStorage.ServerContents)
 				{
 					if(slots.Key != NamedSlot.eyes) continue;
 					foreach (var onSlots in slots.Value)
 					{
-						if (onSlots.IsEmpty)
+						if (onSlots.IsEmpty) //Nothing protecting the eye, flash immediately
 						{
 							TellClientThatTheyHaveBeenFlashed(flashEffector, player);
 							continue;
 						}
-						if(onSlots.ItemAttributes.HasTrait(sunglassesTrait)) continue;
+						if(onSlots.ItemAttributes.HasTrait(sunglassesTrait)) continue; //If the player is wearing protective glasses, check next.
 						TellClientThatTheyHaveBeenFlashed(flashEffector, player);
 						break;
 					}
@@ -47,10 +58,37 @@ namespace Objects
 			StartCoroutine(Cooldown());
 		}
 
+		[Server]
+		public void FlashTarget(GameObject target)
+		{
+			if (onCooldown) return;
+			if (flashSound != null) _ = SoundManager.PlayNetworkedAtPosAsync(flashSound, gameObject.AssumedWorldPosServer());
+			StartCoroutine(Cooldown());
+			if (target.TryGetComponent<RegisterPlayer>(out var player) == false) return;
+			if (target.gameObject.TryGetComponent<PlayerFlashEffects>(out var flashEffector) == false) return;
+			if (target.gameObject.TryGetComponent<DynamicItemStorage>(out var playerStorage) == false) return;
+			foreach (var slots in playerStorage.ServerContents)
+			{
+				if(slots.Key != NamedSlot.eyes) continue;
+				foreach (var onSlots in slots.Value)
+				{
+					if (onSlots.IsEmpty) //Nothing protecting the eye, flash immediately
+					{
+						TellClientThatTheyHaveBeenFlashed(flashEffector, player);
+						continue;
+					}
+					if(onSlots.ItemAttributes.HasTrait(sunglassesTrait)) continue; //If the player is wearing protective glasses, check next.
+					TellClientThatTheyHaveBeenFlashed(flashEffector, player);
+					break;
+				}
+			}
+		}
+
+		[Server]
 		private void TellClientThatTheyHaveBeenFlashed(PlayerFlashEffects effects, RegisterPlayer player)
 		{
 			effects.ServerSendMessageToClient(player.gameObject, flashStrength);
-			if(stunsPlayers) player.ServerStun(flashStrength + 3);
+			if(stunsPlayers) player.ServerStun(flashStrength + stunExtraTime);
 		}
 
 		[Server]
