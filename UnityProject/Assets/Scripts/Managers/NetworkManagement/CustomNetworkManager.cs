5a6ffab45cb86055934e014ccd08c8866e8e2b16
NoooneyDude <noooneydude@gmail.com>
2022-05-01 10:45:35 +1200

    Fix starting the game from the lobby in the editor (#8626)
    * Fix starting the game from the lobby in the editor
    
    * Fix it for real

--- UnityProject/Assets/Scripts/Managers/NetworkManagement/CustomNetworkManager.cs
+++ UnityProject/Assets/Scripts/Managers/NetworkManagement/CustomNetworkManager.cs
@@ -114,8 +114,9 @@ public class CustomNetworkManager : NetworkManager, IInitialise
 	{
 		CheckTransport();
 		ApplyConfig();
-		//Automatically host if starting up game *not* from lobby
-		if (SceneManager.GetActiveScene().name != "Lobby")
+
+		var prevEditorScene = SubSceneManager.GetEditorPrevScene();
+		if (prevEditorScene != string.Empty && prevEditorScene != "StartUp" && prevEditorScene != "Lobby")
 		{
 			StartHost();
 		}
