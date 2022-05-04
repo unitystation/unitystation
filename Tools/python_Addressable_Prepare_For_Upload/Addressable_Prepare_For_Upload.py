e62733e3defc46045b62b6658876fc0d25db9f32
Bod9001 <Bod9001@gmail.com>
2022-05-04 18:05:26 +0100

    Update tool to include Addressables inside  a project and set it

--- Tools/python_Addressable_Prepare_For_Upload/Addressable_Prepare_For_Upload.py
+++ Tools/python_Addressable_Prepare_For_Upload/Addressable_Prepare_For_Upload.py
@@ -1,10 +1,12 @@
 import sys 
 import os
 import json
+import shutil
+
 
 
 print("Have you done an addressable bundle build in the editor to ensure its up-to-date? and deleted the old bundle build?")
-In = input("y or n \n")
+In = "y" # input("y or n \n")
 
 if In != "y":
     print("either you entered something other than 'y' or you haven't done it yet so do it!")
@@ -26,6 +28,23 @@ print(paths)
 
 print(os.listdir(paths))
 
+Addressable_path = os.getcwd()
+
+Addressable_path = Addressable_path.replace("\\", "/")
+
+Addressable_path = Addressable_path.replace("Tools/python_Addressable_Prepare_For_Upload", "AddressableContent/")
+
+
+if os.path.isdir(Addressable_path):
+    shutil.rmtree(Addressable_path)
+    os.mkdir(Addressable_path)
+else:
+    os.mkdir(Addressable_path)
+
+
+
+
+
 newPthas = []
 
 Folders = os.listdir(paths)
@@ -33,14 +52,34 @@ Folders = os.listdir(paths)
 for Folder in Folders:
     Folder_Name = Folder
     Folder = paths + Folder  + "/ServerData/"
+    Local_Addressable_path = Addressable_path  + "/" + Folder_Name + "/"
+    
+    if os.path.isdir(Local_Addressable_path):
+        shutil.rmtree(Local_Addressable_path)
+        os.mkdir(Local_Addressable_path)
+    else:
+        os.mkdir(Local_Addressable_path)
+    
+    Files = os.listdir(Folder)
+    for File in Files:
+        FilePath =  Folder + File
+        shutil.copy(FilePath, Local_Addressable_path)
 
+print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+print(Addressable_path)
+Folders = os.listdir(Addressable_path)
+print(Folders)
+for Folder in Folders:
+    Folder_Name = Folder
+    Folder = Addressable_path + Folder+ "/"
 
     Files = os.listdir(Folder)
+    
     print(Folder)
     
     for File in Files:
         if ".json" in File:
-            FilePath =  Folder + File
+            FilePath = Folder + File
             print(FilePath)
             with open(FilePath, "r+") as f:
                 d = json.load(f)
@@ -48,14 +87,14 @@ for Folder in Folders:
 
                 for InternalIdsin in d["m_InternalIds"]:
                     if "AddressablePackingProjects" in InternalIdsin:
-                        InternalIdsin = InternalIdsin.replace("AddressablePackingProjects", "https://unitystationfile.b-cdn.net/Addressables")
+                        InternalIdsin = InternalIdsin.replace("AddressablePackingProjects", "https://raw.githubusercontent.com/unitystation/unitystation/AddressableContent")
                         InternalIdsin = InternalIdsin.replace("/ServerData", "")
                         print(InternalIdsin)
                         d["m_ProviderIds"][i] = InternalIdsin
                     i = i + 1
                 f.close()
-                
-            open(FilePath, "w").close()
+
+ 
             with open(FilePath, "w") as f:
                 json.dump(d, f)
                 f.close()
@@ -64,36 +103,25 @@ for Folder in Folders:
             txtptahs = Folder + Folder_Name + ".txt"
             print(txtptahs)
             with open(txtptahs, "w+") as f:
-                f.write('https://unitystationfile.b-cdn.net/Addressables/' + Folder_Name + "/" + File )
-                newPthas.append("https://unitystationfile.b-cdn.net/Addressables/" + Folder_Name + "/" + Folder_Name + ".txt" )
+                f.write('https://raw.githubusercontent.com/unitystation/unitystation/AddressableContent/' + Folder_Name + "/" + File )
+                newPthas.append("https://raw.githubusercontent.com/unitystation/unitystation/AddressableContent/" + Folder_Name + "/" + Folder_Name + ".txt" )
                 
-                
+print(str(newPthas))
             
     
 #Tools\python_Addressable_Prepare_For_Upload
 input("""
 ========================================================
 
-now go to the respective folders and upload them to the respective folders on the CDN E.G
-   unitystation\\UnityProject\\AddressablePackingProjects\\SoundAndMusic\\ServerData\\ to Addressables/SoundAndMusic
+now Commit your changes and make a PR
 
   are you done? press enter if you are""")
 
-input("""
-========================================================
-
-now go to
-https://panel.bunny.net/purge
-
-And in the Purge URL List enter individually, without the quotes! and press the button 
-
-Press enter when done
-""" +   str(newPthas))
 
 
 input("""
 ========================================================
 
-Good job everything is done press enter to exit bye
+Good job just have to wait for the PR to be merged, press enter to exit bye
 
 """)
