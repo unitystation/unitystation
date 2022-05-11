b776ae32437d0a4831421c23f84b9139a76de652
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-05-11 07:41:15 +0200

    Update GUI_CargoOfflinePage.cs

--- UnityProject/Assets/Scripts/UI/Objects/Cargo/GUI_CargoOfflinePage.cs
+++ UnityProject/Assets/Scripts/UI/Objects/Cargo/GUI_CargoOfflinePage.cs
@@ -1,10 +1,3 @@
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
-using UI.Core.NetUI;
-using UI.Objects.Cargo;
-
-
 namespace UI.Objects.Cargo
 {
     public class GUI_CargoOfflinePage : GUI_CargoPage { }
