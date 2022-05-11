ab63359b630c736c9ff4757f278734d90bd5ef48
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-05-11 07:25:52 +0200

    Doesn't even want to work so im just gonna leave it as is for now

--- UnityProject/Assets/Scripts/UI/Objects/Cargo/GUI_CargoOfflinePage.cs
+++ UnityProject/Assets/Scripts/UI/Objects/Cargo/GUI_CargoOfflinePage.cs
@@ -7,26 +7,5 @@ using UI.Objects.Cargo;
 
 namespace UI.Objects.Cargo
 {
-    public class GUI_CargoOfflinePage : GUI_CargoPage
-    {
-        public Text errorText;
-        public float blinkTime = 0.3f;
-
-        public override void OpenTab()
-        {
-            base.OpenTab();
-            StartCoroutine(AnimateText());
-        }
-
-        private IEnumerator AnimateText()
-        {
-            while(gameObject.activeSelf)
-            {
-	            errorText.SetActive(false);
-	            yield return WaitFor.Seconds(blinkTime);
-	            errorText.SetActive(true);
-            }
-            errorText.SetActive(true);
-        }
-    }
+    public class GUI_CargoOfflinePage : GUI_CargoPage { }
 }
