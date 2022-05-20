30527ce989d82fceffa73cfe450a46ecc1e95968
PerfectTangent <68730437+PerfectTangent@users.noreply.github.com>
2022-05-19 21:03:59 -0400

    added projectile fail feedback.

--- UnityProject/Assets/Scripts/Items/Weapons/Projectiles/Behaviours/ProjectileMine.cs
+++ UnityProject/Assets/Scripts/Items/Weapons/Projectiles/Behaviours/ProjectileMine.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Tiles;
+using AddressableReferences;
 
 namespace Weapons.Projectiles.Behaviours
 {
@@ -14,13 +15,21 @@ namespace Weapons.Projectiles.Behaviours
 		[Tooltip("what degree of hardness this projectile can overcome. Higher means the projectile can mine more types of things.")]
 		[SerializeField] private int projectileHardness = 5;
 
+		[Tooltip("The sound made when this projectile fails to break a tile due to insufficient hardness.")]
+		public AddressableAudioSource projectileMineFail;
+
 		public virtual bool Interact(MatrixManager.CustomPhysicsHit hit, InteractableTiles interactableTiles, Vector3 worldPosition)
 		{
 
 			var tile = interactableTiles.InteractableLayerTileAt(worldPosition, true);
 			if (tile is BasicTile basicTile)
 			{
-				if (projectileHardness < basicTile.MiningHardness) return false;
+
+				if (projectileHardness < basicTile.MiningHardness)
+				SoundManager.PlayNetworkedAtPos(projectileMineFail, gameObject.AssumedWorldPosServer());
+				Chat.AddLocalMsgToChat($"The projectile pings off the surface, leaving hardly a scratch.", gameObject);
+				return false;
+				
 			}
 			return interactableTiles.TryMine(worldPosition);
 		}
