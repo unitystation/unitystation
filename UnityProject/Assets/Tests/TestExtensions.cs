1cdbb502060bccbf1d0d6fcd6fd78f3f263d0ef6
Copern <copern@gmail.com>
2022-05-19 18:12:31 -0700

    Tests refactor (#8700)
    * Testing Refactors
    
    Refactors and reorganizes some Scene and Prefab/ScriptableObject related tests.
    
    This drastically reduces the time it takes to run the entire edit mode test suite.
    
    * Remove some unnecessary imports.
    
    * Give enumerable extensions statement bodies.
    
    Add some more comments
    
    * Auto prefix "Assets" for FindGUIDsOfType
    
    Change "CheckMissingComponentOnPrefabs". Missing components are true nulls so we can't get the type off them.
    
    * Some fixes
    
    Actually add the object's name in HierarchyName.
    
    Just return if the object is destroyed in ValidateLate since the call is removed afterward anyway.
    
    Accidently iterated the found layers instead of their children in PrefabsAreNotAt00.
    
    * Couple more fixes
    
    Remove some redundant component searching.
    
    Fix index out of bounds for recipes test that have recipes with index below 0.
    
    Re-add using list for cables testing (oops, now I understand what it's for...).
    
    * Remove TODO. It's not test related but affects the whole editor.
    
    * Check singleton SOs for missing refs too
    
    * Codacy
    
    * Remove comments that don't apply to those methods

---
+++ UnityProject/Assets/Tests/TestExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Tests
+{
+	/// <summary>
+	/// Extensions designed for testing purposes only.
+	/// </summary>
+	public static class TestExtensions
+	{
+		/// <summary>
+		/// Get the name of the unity object including the name of all its parents.
+		/// </summary>
+		public static string HierarchyName(this Transform transform, char separator = '/')
+		{
+			if (transform == null) return string.Empty;
+
+			using var pool = ListPool<string>.Get(out var names);
+			var current = transform;
+
+			while (current != null)
+			{
+				names.Add(current.name);
+				current = current.transform.parent;
+			}
+
+			names.Reverse();
+			return string.Join(separator, names);
+		}
+	}
+}
