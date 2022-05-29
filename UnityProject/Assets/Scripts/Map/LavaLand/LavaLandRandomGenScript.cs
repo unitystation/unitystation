bda9786f5c2684364ce8ea2574c9d9f7deecb264
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-05-29 11:32:01 +0200

    Update LavaLandRandomGenScript.cs

--- UnityProject/Assets/Scripts/Map/LavaLand/LavaLandRandomGenScript.cs
+++ UnityProject/Assets/Scripts/Map/LavaLand/LavaLandRandomGenScript.cs
@@ -38,7 +38,7 @@ namespace Systems.Scenes
 		[SerializeField]
 		private RandomItemSpot mobPools = null;
 
-		private void Awake()
+		private void Start()
 		{
 			LavaLandManager.Instance.randomGenScripts.Add(this);
 
