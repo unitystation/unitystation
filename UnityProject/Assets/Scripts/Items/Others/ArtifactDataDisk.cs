7d4dfd1e8f59f64f0193e700660567cece5c6315
Atner <48405920+AtnerNT@users.noreply.github.com>
2022-06-09 20:00:40 +0800

    Console starts

---
+++ UnityProject/Assets/Scripts/Items/Others/ArtifactDataDisk.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items.Science
+{
+	public class ArtifactDataDisk : MonoBehaviour
+	{
+		public int radrecord;
+	}
+}
