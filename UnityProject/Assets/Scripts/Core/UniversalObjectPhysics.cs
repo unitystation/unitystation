6470b6823d4c163bcfac6e07f995b955930e29d7
Bod9001 <Bod9001@gmail.com>
2022-07-04 14:45:45 +0100

    Better pulling and slipping synchroonissed

--- UnityProject/Assets/Scripts/Core/UniversalObjectPhysics.cs
+++ UnityProject/Assets/Scripts/Core/UniversalObjectPhysics.cs
@@ -1277,7 +1277,7 @@ public class UniversalObjectPhysics : NetworkBehaviour, IRightClickable
 	{
 		get
 		{
-			return isFlyingSliding;
+			return isFlyingSliding ;
 			//Is animating with space flying
 		}
 		set
