8180115730f73844e1d6dc50a3d4b5a075f2abd4
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-07-18 15:42:28 +0100

    stuff

---
+++ UnityProject/Assets/Scripts/NPC/AI/Hostile/BlobAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Systems.MobAIs
+{
+	public class BlobAI : GenericHostileAI
+	{
+		//Null if no rally point set
+		private Vector3? localTargetPosition;
+		public Vector3? LocalTargetPosition => localTargetPosition;
+
+		private BlobRally blobRally;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			blobRally = GetComponent<BlobRally>();
+		}
+
+		public void PathToRally(Vector3 worldPos)
+		{
+			localTargetPosition = worldPos;
+			if(blobRally.activated || localTargetPosition == null) return;
+
+			blobRally.Activate();
+			blobRally.PathToRally(localTargetPosition.Value);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/NPC/AI/Hostile/BlobRally.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Systems.MobAIs
+{
+	public class BlobRally : MobPathFinder
+	{
+		//Null if no rally point set
+		private Vector3? localTargetPosition;
+		public Vector3? LocalTargetPosition => localTargetPosition;
+
+		private int attempts = 0;
+
+		private MobPathFinder mobPathFinder;
+
+		private void Awake()
+		{
+			mobPathFinder = GetComponent<MobPathFinder>();
+		}
+
+		public void PathToRally(Vector3 worldPos)
+		{
+			localTargetPosition =
+				MatrixManager.WorldToLocal(worldPos, MatrixManager.AtPoint(worldPos, true, registerTile.Matrix.MatrixInfo));
+
+			var path =
+				mobPathFinder.FindNewPath(
+					MatrixManager.WorldToLocal(uop.OfficialPosition, registerTile.Matrix).To2Int(),
+					localTargetPosition.Value.To2Int());
+
+			if (path != null)
+			{
+				if (path.Count != 0)
+				{
+					FollowPath(path);
+				}
+			}
+		}
+
+		protected override void FollowCompleted()
+		{
+			base.FollowCompleted();
+
+			Deactivate();
+		}
+	}
+}
