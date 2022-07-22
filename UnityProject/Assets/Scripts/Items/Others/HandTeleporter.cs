8642fd5280ed69e83237a704cee6a0468377da16
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-07-22 13:07:54 +0100

    Update HandTeleporter.cs

--- UnityProject/Assets/Scripts/Items/Others/HandTeleporter.cs
+++ UnityProject/Assets/Scripts/Items/Others/HandTeleporter.cs
@@ -7,7 +7,7 @@ using Random = UnityEngine.Random;
 
 namespace Items.Others
 {
-	public class HandTeleporter : MonoBehaviour, ICheckedInteractable<HandActivate>, ICheckedInteractable<HandApply>
+	public class HandTeleporter : MonoBehaviour, ICheckedInteractable<HandApply>, ICheckedInteractable<HandActivate>
 	{
 		[SerializeField]
 		private TrackingBeacon.TrackingBeaconTypes trackingBeaconType = TrackingBeacon.TrackingBeaconTypes.Station;
@@ -23,6 +23,10 @@ namespace Items.Others
 
 		private UniversalObjectPhysics objectPhysics;
 
+		private Portal entrancePortal;
+
+		private Portal exitPortal;
+
 		private void Awake()
 		{
 			objectPhysics = GetComponent<UniversalObjectPhysics>();
@@ -33,7 +37,7 @@ namespace Items.Others
 			if (DefaultWillInteract.Default(interaction, side) == false) return false;
 
 			//Don't allow alt click so that the nettab can be opened
-			if((side == NetworkSide.Client || CustomNetworkManager.IsServer) && KeyboardInputManager.IsAltActionKeyPressed()) return false;
+			if(interaction.IsAltClick) return false;
 
 			return true;
 		}
@@ -41,7 +45,7 @@ namespace Items.Others
 		public void ServerPerformInteraction(HandActivate interaction)
 		{
 			//If theres more than 6 portals (3 pairs) don't allow more
-			if (Portal.PortalPairs.Count > maxPortalPairs * 2)
+			if (Portal.PortalPairs.Count >= maxPortalPairs * 2)
 			{
 				Chat.AddExamineMsgFromServer(interaction.Performer, $"{gameObject.ExpensiveName()} is recharging!");
 				return;
@@ -52,10 +56,21 @@ namespace Items.Others
 
 			if (emergency)
 			{
+				//Close old portals!
+				if (entrancePortal != null) entrancePortal.PortalDeath();
+				if (exitPortal != null) exitPortal.PortalDeath();
+
+
 				Chat.AddExamineMsg(interaction.Performer, $"The {gameObject.ExpensiveName()} flashes briefly. No target is locked in, opening unstable portal!");
 			}
 			else
 			{
+				if (entrancePortal != null || exitPortal != null)
+				{
+					Chat.AddExamineMsg(interaction.Performer, $"There is already a portal open, close it first!");
+					return;
+				}
+
 				Chat.AddExamineMsg(interaction.Performer, $"The {gameObject.ExpensiveName()} flashes briefly. Opening portal to {linkedBeacon.gameObject.ExpensiveName()}!");
 			}
 
@@ -88,8 +103,8 @@ namespace Items.Others
 			var exit = Spawn.ServerPrefab(portalPrefab, worldPosExit);
 			if(exit.Successful == false) return;
 
-			var entrancePortal = entrance.GameObject.GetComponent<Portal>();
-			var exitPortal = exit.GameObject.GetComponent<Portal>();
+			entrancePortal = entrance.GameObject.GetComponent<Portal>();
+			exitPortal = exit.GameObject.GetComponent<Portal>();
 
 			entrancePortal.SetNewPortal(exitPortal, emergency ? 30 : 300);
 			exitPortal.SetNewPortal(entrancePortal, emergency ? 30 : 300);
