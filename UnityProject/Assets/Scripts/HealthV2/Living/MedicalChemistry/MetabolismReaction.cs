2fab2641529488a6dbeaefcc53c6572a4cf9523d
Silver-Ace <64869528+Silver-8ce@users.noreply.github.com>
2022-08-01 13:50:04 -0400

    Update MetabolismReaction.cs

--- UnityProject/Assets/Scripts/HealthV2/Living/MedicalChemistry/MetabolismReaction.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/MedicalChemistry/MetabolismReaction.cs
@@ -63,15 +63,15 @@ public class MetabolismReaction : Reaction
 		{
 			reactionMultiple *= (ReactionAmount / AmountProcessing);
 		}
-		//out must be asigned to something, Overdose is never used here
-		bool Overdose;
-		PossibleReaction(sender, reagentMix, reactionMultiple, ReactionAmount, AmountProcessing, out Overdose);
+		//out must be asigned to something, overdose is never used here
+		bool overdose;
+		PossibleReaction(sender, reagentMix, reactionMultiple, ReactionAmount, AmountProcessing, out overdose);
 	}
 
-	public virtual void PossibleReaction(List<BodyPart> senders, ReagentMix reagentMix, float reactionMultiple, float BodyReactionAmount, float TotalChemicalsProcessed, out bool Overdose)
+	public virtual void PossibleReaction(List<BodyPart> senders, ReagentMix reagentMix, float reactionMultiple, float BodyReactionAmount, float TotalChemicalsProcessed, out bool overdose)
 	{
-		//out must be asigned to something, Overdose is never used here.
-		Overdose = false;
+		//out must be asigned to something, overdose is never used here.
+		overdose = false;
 		foreach (var ingredient in ingredients.m_dict)
 		{
 			reagentMix.Subtract(ingredient.Key, reactionMultiple * ingredient.Value);
