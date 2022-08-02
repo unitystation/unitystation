2517687b2ca99184ce5b446d7e38e418915625ed
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-08-02 15:18:22 +0100

    fixes

--- UnityProject/Assets/Scripts/Items/Others/HolosignProjector.cs
+++ UnityProject/Assets/Scripts/Items/Others/HolosignProjector.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Objects.Other;
 using UnityEngine;
+using Util;
 
 namespace Items.Others
 {
-	public class HolosignProjector : MonoBehaviour, IServerDespawn, ICheckedInteractable<TileApply>,
-		ICheckedInteractable<HandActivate>, ICheckedInteractable<HandApply>, IExaminable
+	public class HolosignProjector : MonoBehaviour, IServerDespawn,
+		ICheckedInteractable<HandActivate>, ICheckedInteractable<PositionalHandApply>, IExaminable
 	{
 		[SerializeField]
 		private List<GameObject> holosignPrefabs = new List<GameObject>();
@@ -29,67 +31,6 @@ namespace Items.Others
 		public void OnDespawnServer(DespawnInfo info)
 		{
 			RemoveAll();
-		}
-
-		public bool WillInteract(TileApply interaction, NetworkSide side)
-		{
-			if(DefaultWillInteract.Default(interaction, side) == false) return false;
-
-			if(interaction.HandObject != gameObject) return false;
-
-			if (side == NetworkSide.Server && holosigns.Count >= maxHolosigns)
-			{
-				Chat.AddExamineMsgFromServer(interaction.Performer, "Projector at max holosign capacity!");
-				return false;
-			}
-
-			return true;
-		}
-
-		public void ServerPerformInteraction(TileApply interaction)
-		{
-			if(ValidatePosition(interaction) == false) return;
-
-			ToolUtils.ServerUseToolWithActionMessages(
-				interaction, timeToPlace,
-				"You start projecting the holosign...",
-				$"{interaction.Performer.ExpensiveName()} starts projecting the holosign...",
-				"You project the holosign.",
-				$"{interaction.Performer.ExpensiveName()} projects the holosign.", () =>
-				{
-					SpawnHolosign(interaction);
-				}
-			);
-		}
-
-		private bool ValidatePosition(TileApply interaction)
-		{
-			var matrixAt = MatrixManager.AtPoint(interaction.WorldPositionTarget, true, interaction.PerformerPlayerScript.registerTile.Matrix.MatrixInfo);
-			var holosignsAtPos = matrixAt.Matrix.GetFirst<Holosign>(interaction.TargetCellPos, true);
-
-			if (holosignsAtPos != null)
-			{
-				Chat.AddExamineMsgFromServer(interaction.Performer, "There is already a holosign there!");
-				return false;
-			}
-
-			return true;
-		}
-
-		private void SpawnHolosign(TileApply interaction)
-		{
-			if(ValidatePosition(interaction) == false) return;
-
-			var newHolosign = Spawn.ServerPrefab(holosignPrefabs[index], interaction.WorldPositionTarget);
-			if (newHolosign.Successful == false)
-			{
-				Logger.LogError("Failed to spawn holosign!");
-				return;
-			}
-
-			var holosignScript = newHolosign.GameObject.GetComponent<Holosign>();
-			holosigns.Add(holosignScript);
-			holosignScript.SetUp(this);
 		}
 
 		public bool WillInteract(HandActivate interaction, NetworkSide side)
@@ -131,23 +72,85 @@ namespace Items.Others
 			}
 		}
 
-		public bool WillInteract(HandApply interaction, NetworkSide side)
+		public bool WillInteract(PositionalHandApply interaction, NetworkSide side)
 		{
 			if(DefaultWillInteract.Default(interaction, side) == false) return false;
 
 			if (interaction.HandObject != gameObject) return false;
 
-			if (interaction.TargetObject.OrNull()?.GetComponent<Holosign>() == null) return false;
+			if (interaction.TargetObject.OrNull()?.GetComponent<Holosign>() != null) return true;
+
+			if (side == NetworkSide.Server && holosigns.Count >= maxHolosigns)
+			{
+				Chat.AddExamineMsgFromServer(interaction.Performer, "Projector at max holosign capacity!");
+				return false;
+			}
 
 			return true;
 		}
 
-		public void ServerPerformInteraction(HandApply interaction)
+		public void ServerPerformInteraction(PositionalHandApply interaction)
 		{
-			Chat.AddActionMsgToChat(interaction.Performer, $"You clear the {interaction.TargetObject.ExpensiveName()}",
-				$"{interaction.Performer.ExpensiveName()} clears the {interaction.TargetObject.ExpensiveName()}");
+			if (interaction.TargetObject != null && interaction.TargetObject.TryGetComponent<Holosign>(out var holosign))
+			{
+				//Only allow deletion of holo where this projector contains the prefab
+				if (holosignPrefabs.Any(x =>
+					    x.gameObject.GetComponent<PrefabTracker>().ForeverID ==
+					    holosign.GetComponent<PrefabTracker>().ForeverID) == false)
+				{
+					Chat.AddExamineMsgFromServer(interaction.Performer, $"You cannot use this projector to clear the {interaction.TargetObject.ExpensiveName()}");
+					return;
+				}
 
-			interaction.TargetObject.GetComponent<Holosign>().DestroyHolosign();
+				Chat.AddActionMsgToChat(interaction.Performer, $"You clear the {interaction.TargetObject.ExpensiveName()}",
+					$"{interaction.Performer.ExpensiveName()} clears the {interaction.TargetObject.ExpensiveName()}");
+
+				_ = Despawn.ServerSingle(holosign.gameObject);
+				return;
+			}
+
+			if(ValidatePosition(interaction) == false) return;
+
+			ToolUtils.ServerUseToolWithActionMessages(
+				interaction, timeToPlace,
+				"You start projecting the holosign...",
+				$"{interaction.Performer.ExpensiveName()} starts projecting the holosign...",
+				"You project the holosign.",
+				$"{interaction.Performer.ExpensiveName()} projects the holosign.", () =>
+				{
+					SpawnHolosign(interaction);
+				}
+			);
+		}
+
+		private bool ValidatePosition(PositionalHandApply interaction)
+		{
+			var matrixAt = MatrixManager.AtPoint(interaction.WorldPositionTarget, true, interaction.PerformerPlayerScript.registerTile.Matrix.MatrixInfo);
+			var holosignsAtPos = matrixAt.Matrix.GetFirst<Holosign>(interaction.TargetPosition.To3Int(), true);
+
+			if (holosignsAtPos != null)
+			{
+				Chat.AddExamineMsgFromServer(interaction.Performer, "There is already a holosign there!");
+				return false;
+			}
+
+			return true;
+		}
+
+		private void SpawnHolosign(PositionalHandApply interaction)
+		{
+			if(ValidatePosition(interaction) == false) return;
+
+			var newHolosign = Spawn.ServerPrefab(holosignPrefabs[index], interaction.WorldPositionTarget);
+			if (newHolosign.Successful == false)
+			{
+				Logger.LogError("Failed to spawn holosign!");
+				return;
+			}
+
+			var holosignScript = newHolosign.GameObject.GetComponent<Holosign>();
+			holosigns.Add(holosignScript);
+			holosignScript.SetUp(this);
 		}
 
 		public string Examine(Vector3 worldPos = default(Vector3))
