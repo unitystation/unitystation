c5e219912a49d40b8cef2cdf9ab532e4c8942ff1
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-08-02 17:52:04 +0100

    initial scripts

---
+++ UnityProject/Assets/Scripts/Player/Language/LanguageSO.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Language
+{
+	[CreateAssetMenu(fileName = "LanguageSO", menuName = "ScriptableObjects/Player/LanguageSO")]
+	public class LanguageSO : ScriptableObject
+	{
+		[SerializeField]
+		private string languageName = "";
+		public string LanguageName => languageName;
+
+		[SerializeField]
+		[TextArea(10, 10)]
+		private string desc = "";
+		public string Desc => desc;
+
+		[SerializeField]
+		[Tooltip("The keycode for language use in the chat system")]
+		private string key = "";
+		public string Key => key;
+
+		[SerializeField]
+		private List<string> syllables = new List<string>();
+		public List<string> Syllables => syllables;
+
+		[SerializeField]
+		[Tooltip("Chance of making a new sentence after each syllable")]
+		private float sentenceChance = 5;
+		public float SentenceChance => sentenceChance;
+
+		[SerializeField]
+		[Tooltip("Chance of getting a space in the random scramble string")]
+		private float spaceChance = 55;
+		public float SpaceChance => spaceChance;
+
+		[SerializeField]
+		[Tooltip("Priority that this language is the default, higher is preferred")]
+		private int priority = 0;
+		public int Priority => priority;
+
+		[SerializeField]
+		[Tooltip("Sprite icon for this language")]
+		private Sprite sprite = null;
+		public Sprite Sprite => sprite;
+	}
+}
---
+++ UnityProject/Assets/Scripts/Player/Language/MobLanguages.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Player.Language
+{
+	public class MobLanguages : MonoBehaviour
+	{
+		[SerializeField]
+		private DefaultLanguageHolderSO defaultLanguages = null;
+
+		[SerializeField]
+		private bool omniTongue = false;
+
+		private List<LanguageSO> understoodLanguages = new List<LanguageSO>();
+		public List<LanguageSO> UnderstoodLanguages => understoodLanguages;
+
+		private List<LanguageSO> spokenLanguages = new List<LanguageSO>();
+		public List<LanguageSO> SpokenLanguages => spokenLanguages;
+
+		private List<LanguageSO> blockedLanguages  = new List<LanguageSO>();
+		public List<LanguageSO> BlockedLanguages => blockedLanguages;
+
+		private void Start()
+		{
+			if(defaultLanguages == null) return;
+
+			//Copy the default lists to this script lists so we can add to it during runtime without adding to the SO
+			understoodLanguages = defaultLanguages.UnderstoodLanguages.ToList();
+			spokenLanguages = defaultLanguages.SpokenLanguages.ToList();
+			blockedLanguages = defaultLanguages.BlockedLanguages.ToList();
+		}
+
+		public bool CanUnderstandLanguage(LanguageSO languageToTest)
+		{
+			return understoodLanguages.Contains(languageToTest);
+		}
+
+		public bool CanSpeakLanguage(LanguageSO languageToTest)
+		{
+			return spokenLanguages.Contains(languageToTest);
+		}
+
+		public bool IsBlockedLanguage(LanguageSO languageToTest)
+		{
+			return blockedLanguages.Contains(languageToTest);
+		}
+
+		public void LearnLanguage(LanguageSO languageToLearn, bool canSpeak = false, bool overrideBlocked = false)
+		{
+			if (overrideBlocked == false && IsBlockedLanguage(languageToLearn))
+			{
+				Chat.AddExamineMsgFromServer(gameObject, $"You cannot learn to understand {languageToLearn.LanguageName} it is too complex!");
+				return;
+			}
+
+			if (CanUnderstandLanguage(languageToLearn) == false)
+			{
+				understoodLanguages.Add(languageToLearn);
+				Chat.AddExamineMsgFromServer(gameObject, $"You learn to understand {languageToLearn.LanguageName}");
+			}
+
+			if (canSpeak && CanSpeakLanguage(languageToLearn) == false)
+			{
+				spokenLanguages.Add(languageToLearn);
+				Chat.AddExamineMsgFromServer(gameObject, $"You learn to speak {languageToLearn.LanguageName}");
+			}
+		}
+	}
+}
