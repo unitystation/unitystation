0fd13af6436f7f2d4a92170035e9547f37514249
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-08-04 17:35:31 +0100

    electrified sprite

--- UnityProject/Assets/Scripts/Objects/Doors/Modules/ElectrifiedDoorModule.cs
+++ UnityProject/Assets/Scripts/Objects/Doors/Modules/ElectrifiedDoorModule.cs
@@ -1,24 +1,55 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using HealthV2;
+using Messages.Server;
+using Mirror;
+using UnityEngine.Serialization;
 
 namespace Doors.Modules
 {
-	public class ElectrifiedDoorModule : DoorModuleBase, IServerSpawn
+	public class ElectrifiedDoorModule : DoorModuleBase, IServerLifecycle
 	{
-		[SerializeField] private int voltageDamage = 9080;
-		[SerializeField] private bool isElectrecuted = false;
+		[SerializeField]
+		private int voltageDamage = 9080;
 
-		public bool IsElectrecuted
+		[SerializeField]
+		private bool isElectrified = false;
+
+		[SerializeField]
+		private SpriteHandler spriteHandler = null;
+
+		public static HashSet<ElectrifiedDoorModule> ElectrifiedDoors { get; private set; } = new HashSet<ElectrifiedDoorModule>();
+
+		public bool IsElectrified
 		{
-			get => isElectrecuted;
-			set => isElectrecuted = value;
+			get => isElectrified;
+			set
+			{
+				isElectrified = value;
+
+				if (isElectrified)
+				{
+					AddToElectrified(this);
+				}
+				else
+				{
+					RemoveFromElectrified(this);
+				}
+			}
 		}
 
 		public void OnSpawnServer(SpawnInfo info)
 		{
 			master.HackingProcessBase.RegisterPort(ToggleElectrocution, master.GetType());
 			master.HackingProcessBase.RegisterPort(PreventElectrocution, master.GetType());
+
+			if(isElectrified == false) return;
+			AddToElectrified(this);
+		}
+
+		public void OnDespawnServer(DespawnInfo info)
+		{
+			RemoveFromElectrified(this);
 		}
 
 		public void ToggleElectrocutionInput()
@@ -30,7 +61,7 @@ namespace Doors.Modules
 		{
 			if (master.HasPower == false)
 				return;
-			IsElectrecuted = !IsElectrecuted;
+			IsElectrified = !IsElectrified;
 		}
 
 		public override ModuleSignal OpenInteraction(HandApply interaction, HashSet<DoorProcessingStates> States)
@@ -67,7 +98,7 @@ namespace Doors.Modules
 		{
 			if (master.HasPower)
 			{
-				if (IsElectrecuted == false)
+				if (IsElectrified == false)
 				{
 					if (PulsePreventElectrocution())
 					{
@@ -124,12 +155,57 @@ namespace Doors.Modules
 
 		public override bool CanDoorStateChange()
 		{
-			if (master.HasPower && IsElectrecuted)
+			if (master.HasPower && IsElectrified)
 			{
 				return false;
 			}
 
 			return true;
 		}
+
+		#region Synthetic sprite
+
+		private static void AddToElectrified(ElectrifiedDoorModule electrifiedDoor)
+		{
+			ElectrifiedDoors.Add(electrifiedDoor);
+
+			ElectrifiedDoorMessage.Send(electrifiedDoor.master, true);
+		}
+
+		private static void RemoveFromElectrified(ElectrifiedDoorModule electrifiedDoor)
+		{
+			ElectrifiedDoors.Remove(electrifiedDoor);
+
+			ElectrifiedDoorMessage.Send(electrifiedDoor.master, false);
+		}
+
+		public void NewSpriteState(bool state)
+		{
+			spriteHandler.ChangeSprite(state ? 1 : 0);
+		}
+
+		public static void Rejoined(NetworkConnectionToClient conn)
+		{
+			foreach (var electrifiedDoor in ElectrifiedDoors)
+			{
+				ElectrifiedDoorMessage.SendTo(conn, electrifiedDoor.master, electrifiedDoor.isElectrified);
+			}
+		}
+
+		public static void LeftBody(NetworkConnectionToClient conn)
+		{
+			foreach (var electrifiedDoor in ElectrifiedDoors)
+			{
+				ElectrifiedDoorMessage.SendTo(conn, electrifiedDoor.master, false);
+			}
+		}
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+		private static void ClearStatics()
+		{
+			ElectrifiedDoors = new HashSet<ElectrifiedDoorModule>();
+		}
+
+		#endregion
 	}
 }
