388e1247d0a2d370992acc4562462fd6bffc1419
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-08-05 01:59:33 +0200

    Update EmergencyOxygenTank.cs

--- UnityProject/Assets/Scripts/Items/Others/EmergencyOxygenTank.cs
+++ UnityProject/Assets/Scripts/Items/Others/EmergencyOxygenTank.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 namespace Items.Others
 {
-	public class EmergencyOxygenTank : MonoBehaviour, IInteractable<HandActivate>
+	public class EmergencyOxygenTank : MonoBehaviour, IPredictedInteractable<HandActivate>
 	{
 		private GasContainer container;
 		[SerializeField] private float gasReleaseOnPush = 10f;
+
+		private Vector3 rollbackMemory = Vector3.zero;
 
 		private void Awake()
 		{
@@ -16,9 +18,20 @@ namespace Items.Others
 
 		public void ServerPerformInteraction(HandActivate interaction)
 		{
+			return;
+		}
+
+		public void ClientPredictInteraction(HandActivate interaction)
+		{
 			if(container.FullPercentage <= 1f) return;
 			Jetpack.PushPlayerInFacedDirection(interaction.PerformerPlayerScript, container, gasReleaseOnPush);
 			Chat.AddExamineMsg(interaction.Performer, "You release some of the gas in this tank.");
+			rollbackMemory = interaction.Performer.RegisterTile().LocalPosition;
+		}
+
+		public void ServerRollbackClient(HandActivate interaction)
+		{
+			interaction.Performer.RegisterTile().ServerSetLocalPosition(rollbackMemory.CutToInt());
 		}
 	}
 }
