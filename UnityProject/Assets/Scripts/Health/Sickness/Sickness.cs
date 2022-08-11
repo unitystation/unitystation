847552ef169370c88e957ac54e65bd5ce1e302a9
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-08-11 22:42:28 +0200

    fixes #9109

--- UnityProject/Assets/Scripts/Health/Sickness/Sickness.cs
+++ UnityProject/Assets/Scripts/Health/Sickness/Sickness.cs
@@ -74,7 +74,7 @@ namespace Health.Sickness
 		{
 			if (PossibleCures.Count == 0) return;
 			cureIndex = Random.Range(0, PossibleCures.Count - 1);
-			CureForSickness = PossibleCures[cureIndex].ingredients.PickRandom().Key;
+			CureForSickness = PossibleCures[cureIndex].results.PickRandom().Key;
 			FillCureHints();
 		}
 
