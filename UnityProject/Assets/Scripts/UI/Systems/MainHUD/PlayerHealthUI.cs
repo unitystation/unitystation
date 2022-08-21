0ce148682d12365f1519f199108055e473d3ae51
ThatDan123 <56727168+ThatDan123@users.noreply.github.com>
2022-08-21 13:41:34 +0100

    ui alert sprite stuff

--- UnityProject/Assets/Scripts/UI/Systems/MainHUD/PlayerHealthUI.cs
+++ UnityProject/Assets/Scripts/UI/Systems/MainHUD/PlayerHealthUI.cs
@@ -113,7 +113,7 @@ public class PlayerHealthUI : MonoBehaviour
 			SetSpecificVisibility(false, coldAlert);
 		}
 
-		if (temperature > 510)
+		if (PlayerManager.LocalPlayerScript.playerHealth.FireStacks > 0 || temperature > 510)
 		{
 			SetSpecificVisibility(true, heatAlert);
 		}
