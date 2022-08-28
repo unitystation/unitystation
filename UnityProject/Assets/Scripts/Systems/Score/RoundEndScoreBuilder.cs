9c6ca30af7cb0e6fadd2f26c58f14f3813a48209
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-08-28 06:27:59 +0200

    Update RoundEndScoreBuilder.cs

--- UnityProject/Assets/Scripts/Systems/Score/RoundEndScoreBuilder.cs
+++ UnityProject/Assets/Scripts/Systems/Score/RoundEndScoreBuilder.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections.Generic;
+using NSubstitute.ReceivedExtensions;
 using Shared.Managers;
+using UnityEngine;
 
 namespace Systems.Score
 {
 	public class RoundEndScoreBuilder : SingletonManager<RoundEndScoreBuilder>
 	{
+		/// <summary>
+		/// How much does score entry that returns true or false score?
+		/// </summary>
+		[SerializeField] private int boolScore = 10;
+
 		public override void Awake()
 		{
 			base.Awake();
@@ -21,10 +28,24 @@ namespace Systems.Score
 			List<ScoreEntry> stationScoreEntries = new List<ScoreEntry>();
 			List<ScoreEntry> antagScoreEntries = new List<ScoreEntry>();
 
+			var finalStationScore = 0;
+			var finalAntagScore = 0;
+
 			foreach (var entry in ScoreMachine.Instance.Scores)
 			{
-				if(entry.Value.Category == ScoreCategory.StationScore) stationScoreEntries.Add(entry.Value);
-				if(entry.Value.Category == ScoreCategory.AntagScore) antagScoreEntries.Add(entry.Value);
+				if (entry.Value.Category == ScoreCategory.StationScore)
+				{
+					stationScoreEntries.Add(entry.Value);
+					if (entry.Value is ScoreEntryInt a) finalStationScore += a.Score;
+					if (entry.Value is ScoreEntryBool m) finalStationScore += m.Score ? boolScore : -boolScore;
+				}
+
+				if (entry.Value.Category == ScoreCategory.AntagScore)
+				{
+					antagScoreEntries.Add(entry.Value);
+					if (entry.Value is ScoreEntryInt o) finalAntagScore += o.Score;
+					if (entry.Value is ScoreEntryBool g) finalAntagScore += g.Score ? boolScore : -boolScore;
+				}
 			}
 
 			stationScores = ScoresInString(stationScoreEntries);
