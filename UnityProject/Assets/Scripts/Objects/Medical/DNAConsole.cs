24f5c2bb4f5554210252c77dadf323e224eaa0c5
Bod9001 <Bod9001@gmail.com>
2022-09-10 12:54:26 +0100

    First ssteps

--- UnityProject/Assets/Scripts/Objects/Medical/DNAConsole.cs
+++ UnityProject/Assets/Scripts/Objects/Medical/DNAConsole.cs
@@ -7,6 +7,43 @@ public class DNAConsole : MonoBehaviour
 {
 	public DNAScanner DNAScanner;
 
+	public char SelectionChar;
+	public BodyPartSelectionType SelectionType;
 
+	public enum BodyPartSelectionType
+	{
+		MustInclude,
+		MustNotContain
+	}
+
+
+	[NaughtyAttributes.Button()]
+	public void Inject()
+	{
+		foreach (var BP in DNAScanner.occupant.BodyPartList)
+		{
+			switch (SelectionType)
+			{
+				case BodyPartSelectionType.MustNotContain:
+					if (BP.name.Contains(SelectionChar) == false)
+					{
+						Logger.LogError($"Found {BP.name}");
+					}
+					break;
+				case BodyPartSelectionType.MustInclude:
+					if (BP.name.Contains(SelectionChar))
+					{
+						Logger.LogError($"Found {BP.name}");
+					}
+					break;
+
+			}
+
+		}
+
+
+
+
+	}
 
 }
