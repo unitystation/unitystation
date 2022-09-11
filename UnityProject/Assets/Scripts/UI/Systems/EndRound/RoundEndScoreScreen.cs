f0f9bee4a5790735cde593fbcea31a8b0ce45a6e
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-09-11 13:34:17 +0200

    Update RoundEndScoreScreen.cs

--- UnityProject/Assets/Scripts/UI/Systems/EndRound/RoundEndScoreScreen.cs
+++ UnityProject/Assets/Scripts/UI/Systems/EndRound/RoundEndScoreScreen.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Messages.Server;
 using Mirror;
 using Systems.Score;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 
 namespace UI.Systems.EndRound
