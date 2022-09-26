17e5b9ee9f171392bd389644ba5a446269acfb4d
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-09-26 19:24:02 +0200

    Entriesin the weird category with a score of 0 wont be tracked

--- UnityProject/Assets/Scripts/Systems/Score/RoundEndScoreBuilder.cs
+++ UnityProject/Assets/Scripts/Systems/Score/RoundEndScoreBuilder.cs
@@ -5,6 +5,9 @@ using Shared.Managers;
 
 namespace Systems.Score
 {
+	/// <summary>
+	/// Here is where all common entries are initialised once and where we check for extra stuff when the round ends.
+	/// </summary>
 	public partial class RoundEndScoreBuilder : SingletonManager<RoundEndScoreBuilder>
 	{
 		public override void Start()
@@ -21,7 +24,9 @@ namespace Systems.Score
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_FOODMADE, "Meals Prepared", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Good);
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_HOSTILENPCDEAD, "Hostile NPCs dead", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Good);
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_HEALING, "Healing Done", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Good);
+			ScoreMachine.AddNewScoreEntry(COMMON_HUG_SCORE_ENTRY, "Hugs Given", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Good);
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_FOODEATEN, "Food Eaten", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Weird);
+			ScoreMachine.AddNewScoreEntry(COMMON_TAIL_SCORE_ENTRY, "Tails Pulled", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Weird);
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_EXPLOSION, "Number of explosions this round", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Weird);
 			ScoreMachine.AddNewScoreEntry(COMMON_SCORE_CLOWNABUSE, "Clown Abused", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Bad);
 		}
@@ -45,6 +50,7 @@ namespace Systems.Score
 			if (PlayerList.Instance.AllPlayers.Count > 2)
 			{
 				ScoreMachine.AddNewScoreEntry("deadCrew", "Dead Crew", ScoreMachine.ScoreType.Int, ScoreCategory.StationScore, ScoreAlignment.Bad);
+				//We can get away with using expensive LINQ methods here at round end.
 				foreach (var player in PlayerList.Instance.AllPlayers.Where(player => player.Mind != null && player.Script != null))
 				{
 					ScoreMachine.AddToScoreInt(DEAD_CREW_SCORE * negativeModifer, "deadCrew");
@@ -79,6 +85,11 @@ namespace Systems.Score
 
 			List<ScoreEntry> stationScoreEntries = new List<ScoreEntry>();
 			List<ScoreEntry> antagScoreEntries = new List<ScoreEntry>();
+
+			//Removes all int scores from the weird category with a score of 0
+			stationScoreEntries.RemoveAll(x => x.Category == ScoreCategory.StationScore
+			                                   && x.Alignment == ScoreAlignment.Weird
+			                                   && x is ScoreEntryInt { Score: 0 });
 
 			var finalStationScore = 0;
 			var finalAntagScore = 0;
