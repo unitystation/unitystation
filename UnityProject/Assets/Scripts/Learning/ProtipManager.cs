ec431a018c998af34385408cb8fb65a1dc6724af
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-10-04 05:53:28 +0200

    unused usings

--- UnityProject/Assets/Scripts/Learning/ProtipManager.cs
+++ UnityProject/Assets/Scripts/Learning/ProtipManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Newtonsoft.Json;
 using Shared.Managers;
 using UnityEngine;
--- UnityProject/Assets/Scripts/Learning/UI/ProtipListUI.cs
+++ UnityProject/Assets/Scripts/Learning/UI/ProtipListUI.cs
@@ -1,7 +1,5 @@
-using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Learning
 {
