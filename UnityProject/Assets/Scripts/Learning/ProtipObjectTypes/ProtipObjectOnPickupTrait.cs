77824b8f03819eb39f8c86690188b8d133246eca
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-10-16 11:35:17 +0200

    Fixes and stuff

--- UnityProject/Assets/Scripts/Learning/ProtipObjectTypes/ProtipObjectOnPickupTrait.cs
+++ UnityProject/Assets/Scripts/Learning/ProtipObjectTypes/ProtipObjectOnPickupTrait.cs
@@ -1,45 +1,72 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using System.Collections;
 
 namespace Learning.ProtipObjectTypes
 {
 	public class ProtipObjectOnPickupTrait : ProtipObject
 	{
-		public List<TraitProtips> protipsForTraits;
-
-		/// <summary>
-		/// Dictionaries are not supported in the unity editor for some reason.
-		/// </summary>
-		[Serializable]
-		public class TraitProtips
-		{
-			public ItemTrait Trait;
-			public ProtipSO Tip;
-		}
+		public SerializableDictionary<ItemTrait, ProtipSO> protipsForTraits = new SerializableDictionary<ItemTrait, ProtipSO>();
 
 		private void OnEnable()
 		{
-			PlayerManager.LocalPlayerScript.DynamicItemStorage.OnContentsChangeClient.AddListener(InventoryChange);
+			StartCoroutine(SetupEvents());
 		}
 
 		private void OnDisable()
 		{
-			PlayerManager.LocalPlayerScript.DynamicItemStorage.OnContentsChangeClient.RemoveListener(InventoryChange);
+			StartCoroutine(UnsubscribeFromEvents());
 		}
 
 
-		private void InventoryChange()
+		private void OnInventoryChange()
 		{
+			StartCoroutine(CheckHand());
+		}
+
+		private IEnumerator CheckHand()
+		{
+			yield return WaitFor.EndOfFrame;
 			var handslot = PlayerManager.LocalPlayerScript.DynamicItemStorage.GetActiveHandSlot();
-			if(handslot == null || handslot.IsEmpty || handslot.ItemAttributes.GetTraits().Count() == 0) return;
-			foreach (var trait in protipsForTraits)
+			if(handslot == null || handslot.IsEmpty || handslot.ItemAttributes.GetTraits().Count() == 0) yield break;
+			foreach (var trait in handslot.ItemAttributes.GetTraits())
 			{
-				if(handslot.ItemAttributes.GetTraits().Any(x => x == trait.Trait) == false) continue;
-				TriggerTip(trait.Tip);
+				if(protipsForTraits.ContainsKey(trait) == false) continue;
+				TriggerTip(protipsForTraits[trait]);
 			}
+		}
+
+		private IEnumerator SetupEvents()
+		{
+			// Await a frame for everything to be initialised properly
+			yield return WaitFor.EndOfFrame;
+			if (PlayerManager.LocalPlayerScript == null)
+			{
+				Logger.LogError("[Protips] - Something went wrong accessing the player's local player script.. Are you sure everything is setup correctly?");
+				yield break;
+			}
+			PlayerManager.LocalPlayerScript.DynamicItemStorage.OnContentsChangeClient.AddListener(OnInventoryChange);
+			// For the hosts and editor use, check if we're a headless server or not.
+			if(CustomNetworkManager.IsHeadless == false) PlayerManager.LocalPlayerScript.
+				DynamicItemStorage.OnContentsChangeServer.AddListener(OnInventoryChange);
+		}
+
+		private IEnumerator UnsubscribeFromEvents()
+		{
+			yield return WaitFor.EndOfFrame;
+			// It's fine to do try catches on the client as it doesn't affect headless server performance.
+			try
+			{
+				PlayerManager.LocalPlayerScript.DynamicItemStorage.OnContentsChangeClient.RemoveListener(OnInventoryChange);
+				if(CustomNetworkManager.IsHeadless == false) PlayerManager.LocalPlayerScript.
+					DynamicItemStorage.OnContentsChangeServer.RemoveListener(OnInventoryChange);
+			}
+			catch (Exception e)
+			{
+				Logger.LogWarning("[Protips] - Attempted to unsubscribe an event while the player script is null, player might have become a ghost or deleted.");
+			}
+
 		}
 	}
 }
