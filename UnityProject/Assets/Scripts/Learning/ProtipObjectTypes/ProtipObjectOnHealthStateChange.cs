7e44a4729fabf4cb8f3f67b83fab0b633c9d4ada
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-10-24 14:55:10 +0200

    Protip fixes and improvements for health state (#9277)
    * adds missing cooldowns to health protips
    
    * Button bigger, cooldown changes and fix animation not reseting

--- UnityProject/Assets/Scripts/Learning/ProtipObjectTypes/ProtipObjectOnHealthStateChange.cs
+++ UnityProject/Assets/Scripts/Learning/ProtipObjectTypes/ProtipObjectOnHealthStateChange.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using HealthV2;
 using Systems.Atmospherics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Learning.ProtipObjectTypes
 {
@@ -10,31 +11,55 @@ namespace Learning.ProtipObjectTypes
 	{
 		private HealthStateController healthState;
 
-		[SerializeField] private ProtipSO BleedingVeryLowEvent;
-		[SerializeField] private ProtipSO BleedingLowEvent;
-		[SerializeField] private ProtipSO BleedingHighEvent;
-		[SerializeField] private ProtipSO BleedingDanagerEvent;
-		[SerializeField] private ProtipSO HungerEvent;
-		[SerializeField] private ProtipSO StarvingEvent;
-		[SerializeField] private ProtipSO FatsoEvent;
-		[SerializeField] private ProtipSO CritHealthEvent;
-		[SerializeField] private ProtipSO SoftHealthEvent;
-		[SerializeField] private ProtipSO DeathlHealthEvent;
-		[SerializeField] private ProtipSO FireStacksEvent;
-		[SerializeField] private ProtipSO SuffuicationEvent;
-		[SerializeField] private ProtipSO ToxinEvent;
-		[SerializeField] private ProtipSO TemperatureColdEvent;
-		[SerializeField] private ProtipSO TemperatureHotEvent;
-		[SerializeField] private ProtipSO UnconsciousEvent;
-		[SerializeField] private ProtipSO BarelyConsciousEvent;
-		[SerializeField] private ProtipSO PressureHighEvent;
-		[SerializeField] private ProtipSO PressureLowEvent;
+		[FormerlySerializedAs("BleedingVeryLowEvent")] [SerializeField]
+		private ProtipSO bleedingVeryLowEvent;
+		[FormerlySerializedAs("BleedingLowEvent")] [SerializeField]
+		private ProtipSO bleedingLowEvent;
+		[FormerlySerializedAs("BleedingHighEvent")] [SerializeField]
+		private ProtipSO bleedingHighEvent;
+		[FormerlySerializedAs("BleedingDanagerEvent")] [SerializeField]
+		private ProtipSO bleedingDanagerEvent;
+		[FormerlySerializedAs("HungerEvent")] [SerializeField]
+		private ProtipSO hungerEvent;
+		[FormerlySerializedAs("StarvingEvent")] [SerializeField]
+		private ProtipSO starvingEvent;
+		[FormerlySerializedAs("FatsoEvent")] [SerializeField]
+		private ProtipSO fatsoEvent;
+		[FormerlySerializedAs("CritHealthEvent")] [SerializeField]
+		private ProtipSO critHealthEvent;
+		[FormerlySerializedAs("SoftHealthEvent")] [SerializeField]
+		private ProtipSO softHealthEvent;
+		[FormerlySerializedAs("DeathlHealthEvent")] [SerializeField]
+		private ProtipSO deathlHealthEvent;
+		[FormerlySerializedAs("FireStacksEvent")] [SerializeField]
+		private ProtipSO fireStacksEvent;
+		[FormerlySerializedAs("SuffuicationEvent")] [SerializeField]
+		private ProtipSO suffuicationEvent;
+		[FormerlySerializedAs("ToxinEvent")] [SerializeField]
+		private ProtipSO toxinEvent;
+		[FormerlySerializedAs("TemperatureColdEvent")] [SerializeField]
+		private ProtipSO temperatureColdEvent;
+		[FormerlySerializedAs("TemperatureHotEvent")] [SerializeField]
+		private ProtipSO temperatureHotEvent;
+		[FormerlySerializedAs("UnconsciousEvent")] [SerializeField]
+		private ProtipSO unconsciousEvent;
+		[FormerlySerializedAs("BarelyConsciousEvent")] [SerializeField]
+		private ProtipSO barelyConsciousEvent;
+		[FormerlySerializedAs("PressureHighEvent")] [SerializeField]
+		private ProtipSO pressureHighEvent;
+		[FormerlySerializedAs("PressureLowEvent")] [SerializeField]
+		private ProtipSO pressureLowEvent;
 
-		private bool PressureTipOnCooldown = false;
-		private bool TempTipOnCooldown = false;
-		private bool SufficationTipCooldown = false;
-		private bool ToxinTipCooldown = false;
-		private HungerState LastHungerState = HungerState.Normal;
+		private bool pressureTipOnCooldown = false;
+		private bool tempTipOnCooldown = false;
+		private bool sufficationTipCooldown = false;
+		private bool toxinTipCooldown = false;
+		private bool hungerTipCooldown = false;
+		private bool deathTipCooldown = false;
+		private bool unconciousTipCooldown = false;
+		private bool fireStackTipCooldown = false;
+		private bool bleedingTipCooldown = false;
+		private HungerState lastHungerState = HungerState.Normal;
 
 		private const float PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM = 35f;
 
@@ -74,18 +99,20 @@ namespace Learning.ProtipObjectTypes
 
 		private void TriggerConsciousEvent(ConsciousState state)
 		{
+			if(unconciousTipCooldown || deathTipCooldown || IsPlayerGhostInBody()) return;
 			switch (state)
 			{
-				case ConsciousState.CONSCIOUS:
-					break;
 				case ConsciousState.BARELY_CONSCIOUS:
-					TriggerTip(BarelyConsciousEvent);
+					TriggerTip(barelyConsciousEvent);
+					StartCoroutine(AsleepTipCooldown());
 					break;
 				case ConsciousState.UNCONSCIOUS:
-					TriggerTip(UnconsciousEvent);
+					TriggerTip(unconsciousEvent);
+					StartCoroutine(AsleepTipCooldown());
 					break;
 				case ConsciousState.DEAD:
-					TriggerTip(DeathlHealthEvent);
+					TriggerTip(deathlHealthEvent);
+					StartCoroutine(DeathTipCooldown());
 					break;
 				default:
 					break;
@@ -94,15 +121,15 @@ namespace Learning.ProtipObjectTypes
 
 		private void TriggerPressureEvent(float state)
 		{
-			if(PressureTipOnCooldown) return;
+			if(pressureTipOnCooldown || IsPlayerGhostInBody()) return;
 			switch (state)
 			{
 				case <= AtmosConstants.HAZARD_LOW_PRESSURE:
-					TriggerTip(PressureLowEvent);
+					TriggerTip(pressureLowEvent);
 					StartCoroutine(PressureCooldown());
 					break;
 				case >= AtmosConstants.WARNING_HIGH_PRESSURE:
-					TriggerTip(PressureHighEvent);
+					TriggerTip(pressureHighEvent);
 					StartCoroutine(PressureCooldown());
 					break;
 			}
@@ -110,88 +137,90 @@ namespace Learning.ProtipObjectTypes
 
 		private void TriggerHungerTip(HungerState state)
 		{
-			if(LastHungerState == state) return;
-			LastHungerState = state;
+			if(lastHungerState == state || IsPlayerGhostInBody()) return;
+			lastHungerState = state;
 			switch (state)
 			{
 				case HungerState.Full:
-					TriggerTip(FatsoEvent);
-					break;
-				case HungerState.Normal:
+					TriggerTip(fatsoEvent);
 					break;
 				case HungerState.Hungry:
-					TriggerTip(HungerEvent);
+					TriggerTip(hungerEvent);
 					break;
 				case HungerState.Malnourished:
-					TriggerTip(HungerEvent);
+					TriggerTip(hungerEvent);
 					break;
 				case HungerState.Starving:
-					TriggerTip(StarvingEvent);
+					TriggerTip(starvingEvent);
 					break;
 				default:
 					break;
 			}
+
+			if(lastHungerState != HungerState.Normal) StartCoroutine(HungerCooldown());
 		}
 
 		private void TriggerBleedingTip(BleedingState state)
 		{
+			if(bleedingTipCooldown || IsPlayerGhostInBody()) return;
 			switch (state)
 			{
-				case BleedingState.None:
-					break;
 				case BleedingState.VeryLow:
-					TriggerTip(BleedingVeryLowEvent);
+					TriggerTip(bleedingVeryLowEvent);
 					break;
 				case BleedingState.Low:
-					TriggerTip(BleedingLowEvent);
+					TriggerTip(bleedingLowEvent);
 					break;
 				case BleedingState.Medium:
-					TriggerTip(BleedingHighEvent);
+					TriggerTip(bleedingHighEvent);
 					break;
 				case BleedingState.High:
-					TriggerTip(BleedingHighEvent);
+					TriggerTip(bleedingHighEvent);
 					break;
 				case BleedingState.UhOh:
-					TriggerTip(BleedingDanagerEvent);
+					TriggerTip(bleedingDanagerEvent);
 					break;
 				default:
 					break;
 			}
+			StartCoroutine(BleedingCooldown());
 		}
 
 		private void TriggerFireStacksTip(float state)
 		{
+			if(fireStackTipCooldown || IsPlayerGhostInBody()) return;
 			if (state > 1f)
 			{
-				TriggerTip(FireStacksEvent);
+				TriggerTip(fireStacksEvent);
+				StartCoroutine(FireStackTipCooldown());
 			}
 		}
 
 		private void TriggerSuffocatingTip(bool state)
 		{
-			if (state == false || SufficationTipCooldown) return;
-			TriggerTip(SuffuicationEvent);
+			if (state == false || sufficationTipCooldown || IsPlayerGhostInBody()) return;
+			TriggerTip(suffuicationEvent);
 			StartCoroutine(SufficationCooldown());
 		}
 
 		private void TriggerToxinsTip(bool state)
 		{
-			if(state == false || ToxinTipCooldown) return;
-			TriggerTip(ToxinEvent);
+			if(state == false || toxinTipCooldown || IsPlayerGhostInBody()) return;
+			TriggerTip(toxinEvent);
 			StartCoroutine(ToxinCooldown());
 		}
 
 		private void TriggerTemperatureTip(float state)
 		{
-			if(TempTipOnCooldown) return;
+			if(tempTipOnCooldown || IsPlayerGhostInBody()) return;
 			switch (state)
 			{
 				case <= AtmosConstants.BARELY_COLD_HEAT:
-					TriggerTip(TemperatureColdEvent);
+					TriggerTip(temperatureColdEvent);
 					StartCoroutine(TempCooldown());
 					break;
 				case >= AtmosConstants.HOT_HEAT:
-					TriggerTip(TemperatureHotEvent);
+					TriggerTip(temperatureHotEvent);
 					StartCoroutine(TempCooldown());
 					break;
 				default:
@@ -199,32 +228,72 @@ namespace Learning.ProtipObjectTypes
 			}
 		}
 
+		private bool IsPlayerGhostInBody()
+		{
+			return healthState.livingHealthMasterBase.playerScript.IsGhost;
+		}
+
 		private IEnumerator TempCooldown()
 		{
-			TempTipOnCooldown = true;
+			tempTipOnCooldown = true;
 			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
-			TempTipOnCooldown = false;
+			tempTipOnCooldown = false;
 		}
 
 		private IEnumerator PressureCooldown()
 		{
-			PressureTipOnCooldown = true;
+			pressureTipOnCooldown = true;
 			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
-			PressureTipOnCooldown = false;
+			pressureTipOnCooldown = false;
 		}
 
 		private IEnumerator SufficationCooldown()
 		{
-			SufficationTipCooldown = true;
+			sufficationTipCooldown = true;
 			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
-			SufficationTipCooldown = false;
+			sufficationTipCooldown = false;
 		}
 
 		private IEnumerator ToxinCooldown()
 		{
-			ToxinTipCooldown = true;
+			toxinTipCooldown = true;
 			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
-			ToxinTipCooldown = false;
+			toxinTipCooldown = false;
+		}
+
+		private IEnumerator HungerCooldown()
+		{
+			hungerTipCooldown = true;
+			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
+			hungerTipCooldown = false;
+		}
+
+		private IEnumerator DeathTipCooldown()
+		{
+			deathTipCooldown = true;
+			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
+			deathTipCooldown = false;
+		}
+
+		private IEnumerator AsleepTipCooldown()
+		{
+			unconciousTipCooldown = true;
+			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
+			unconciousTipCooldown = false;
+		}
+
+		private IEnumerator FireStackTipCooldown()
+		{
+			fireStackTipCooldown = true;
+			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
+			fireStackTipCooldown = false;
+		}
+
+		private IEnumerator BleedingCooldown()
+		{
+			bleedingTipCooldown = true;
+			yield return WaitFor.Seconds(PROTIP_COOLDOWN_TO_AVOID_QUEUE_SPAM);
+			bleedingTipCooldown = false;
 		}
 	}
 }
