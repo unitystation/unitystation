192513b39e889da49b4ecf119906c80493c05820
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-11-03 01:00:32 +0200

    Update CharacterAttributeBehavior.cs

--- UnityProject/Assets/Scripts/Core/Characters/CharacterAttributeBehavior.cs
+++ UnityProject/Assets/Scripts/Core/Characters/CharacterAttributeBehavior.cs
@@ -8,7 +8,7 @@ namespace Core.Characters
 		private bool spawn = false;
 
 		public bool Spawn => spawn;
-		private bool destroyWhenDone = false;
+		[SerializeField] private bool destroyWhenDone = false;
 
 		/// <summary>
 		/// What does this attribute do as soon as it's added to the player?
