e913d0206d8f4ef860b26811601d9e578d4242fc
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-11-18 09:10:53 +0200

    ducky debuggy

--- UnityProject/Assets/Scripts/UI/Systems/Tooltips/HoverTooltips/HoverTooltipUI.cs
+++ UnityProject/Assets/Scripts/UI/Systems/Tooltips/HoverTooltips/HoverTooltipUI.cs
@@ -48,17 +48,27 @@ namespace UI.Systems.Tooltips.HoverTooltips
 		public void SetupTooltip(GameObject hoverObject)
 		{
 			targetObject = hoverObject;
+			// Clean up everything for the upcoming data.
 			ResetTool();
+			// Don't do anything if there's no object to start with.
 			if (hoverObject == null) return;
+
+			//Setup the title and description.
 			UpdateMainInfo(hoverObject);
 			CaptureIconFromSpriteHandler(hoverObject);
 			if(detailsModeEnabled) UpdateDetailedView(hoverObject);
-			// Don't show if the description/name is empty.
-			if (String.IsNullOrEmpty(nameText.text) || String.IsNullOrEmpty(descText.text)) return;
 
+			// Don't show if the description/name is empty.
+			// (Max): It looks better and more intentional when there's no empty fields.
+			// Also reduces hovertip presence on the screen when its not needed.
+			if (IsDescOrTitleEmpty()) return;
 			content.LeanAlpha(1f, ANIM_TIME);
 		}
 
+		/// <summary>
+		/// As the name implies, grabs the icon for the hovertip from the gameObject's sprite handler.
+		/// Will not do anything if the gameObject does not have one.
+		/// </summary>
 		private void CaptureIconFromSpriteHandler(GameObject target)
 		{
 			var imageObj = targetObject.GetComponentInChildren<SpriteHandler>();
@@ -68,12 +78,18 @@ namespace UI.Systems.Tooltips.HoverTooltips
 			}
 		}
 
+		/// <summary>
+		/// Updates the icon to another sprite that an IHoverTooltip returns.
+		/// </summary>
 		private void UpdateIconSprite(IHoverTooltip target)
 		{
 			if (target.CustomIcon() == null) return;
 			iconTarget.sprite = target.CustomIcon();
 		}
 
+		/// <summary>
+		/// Grabs the item name and description from the Attributes base class which ItemAttributes and ObjectAttributes inherit from.
+		/// </summary>
 		private void UpdateMainInfo(GameObject target)
 		{
 			if (target.TryGetComponent<Attributes>(out var attribute) == false) return;
@@ -81,26 +97,39 @@ namespace UI.Systems.Tooltips.HoverTooltips
 			descText.text = attribute.ArticleDescription;
 		}
 
+		/// <summary>
+		/// The extra data to show when the player presses shift.
+		/// </summary>
 		private void UpdateDetailedView(GameObject target)
 		{
 			var tips = target.GetComponents<IHoverTooltip>();
 			foreach (var data in tips)
 			{
 				if (String.IsNullOrEmpty(data.CustomTitle()) == false) nameText.text = data.CustomTitle();
-				if (String.IsNullOrEmpty(data.HoverTip())) continue;
-				descText.text += $"\n \n{data.HoverTip()}";
+				if (String.IsNullOrEmpty(data.HoverTip()) == false)
+				{
+					descText.text += $"\n \n{data.HoverTip()}";
+				}
 				UpdateIconSprite(data);
-				UpdateInteractionsView(data.InteractionsStrings());
+				// Only show interactions if there is a description or title in the tooltip.
+				if (IsDescOrTitleEmpty() == false) UpdateInteractionsView(data.InteractionsStrings());
 			}
+		}
+
+		private bool IsDescOrTitleEmpty()
+		{
+			return string.IsNullOrEmpty(descText.text) || string.IsNullOrEmpty(nameText.text);
 		}
 
 		private void UpdateInteractionsView(List<TextColor> newInteractions)
 		{
 			if (newInteractions == null) return;
+			Debug.Log(newInteractions.Count);
 			foreach (var interaction in newInteractions)
 			{
-				var textObj = Instantiate(interactionPrefab, interactionList);
+				var textObj = Instantiate(interactionPrefab, interactionList, false);
 				textObj.text = $"<color={interaction.Color.ToString()}>{interaction.Text}</color>";
+				textObj.SetActive(true);
 			}
 		}
 
