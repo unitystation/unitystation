c66817de3fa490902c881148939d3d64f735bd8d
Bod9001 <Bod9001@gmail.com>
2022-12-10 18:31:06 +0000

    ads mutation and fixes MachineBoardAllInOneGrinder

---
+++ UnityProject/Assets/Scripts/HealthV2/Living/Mutations/Ears/Deafness.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using HealthV2;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Deafness", menuName = "ScriptableObjects/Mutations/Deafness")]
+public class Deafness  : MutationSO
+{
+	public override Mutation GetMutation(BodyPart BodyPart,MutationSO _RelatedMutationSO)
+	{
+		return new InDeafnessMutation(BodyPart,_RelatedMutationSO);
+	}
+
+	private class InDeafnessMutation: Mutation
+	{
+
+		public Ears RelatedEars;
+
+		public InDeafnessMutation(BodyPart BodyPart,MutationSO _RelatedMutationSO) : base(BodyPart,_RelatedMutationSO)
+		{
+
+		}
+
+		public override void SetUp()
+		{
+			RelatedEars = BodyPart.GetComponent<Ears>();
+			RelatedEars.MutationMultiplier = 0;
+		}
+
+		public override void Remove()
+		{
+			RelatedEars.MutationMultiplier = 1;
+		}
+
+	}
+}
