23c942df3711b945bc6e43b0a01411a73ebfc730
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-12-22 03:56:50 +0200

    correct asset references

--- UnityProject/Assets/Scripts/Managers/SubSceneManager/SubSceneManager.cs
+++ UnityProject/Assets/Scripts/Managers/SubSceneManager/SubSceneManager.cs
@@ -100,16 +100,14 @@ public partial class SubSceneManager : MonoBehaviour
 		if (CustomNetworkManager.IsServer)
 		{
 			NetworkServer.SpawnObjects();
-			RequestObserverRefresh.Send(sceneName.ToString());
+			RequestObserverRefresh.Send(AO.Result.Scene.ToString());
 		}
 		else
 		{
-			if (HandlSynchronising)
-			{
-				NetworkClient.PrepareToSpawnSceneObjects();
-				yield return WaitFor.Seconds(0.2f);
-				RequestObserverRefresh.Send(sceneName.ToString());
-			}
+			if (HandlSynchronising == false) yield break;
+			NetworkClient.PrepareToSpawnSceneObjects();
+			yield return WaitFor.Seconds(0.2f);
+			RequestObserverRefresh.Send(AO.Result.Scene.ToString());
 		}
 	}
 
