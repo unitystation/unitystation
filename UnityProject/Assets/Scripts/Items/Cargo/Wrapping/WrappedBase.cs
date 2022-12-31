bccad9f3bdec9654b0d09cd62a1ee2606c8e261e
Bod9001 <Bod9001@gmail.com>
2022-12-31 17:37:59 +0000

    Cooler Christmas present from the tree

--- UnityProject/Assets/Scripts/Items/Cargo/Wrapping/WrappedBase.cs
+++ UnityProject/Assets/Scripts/Items/Cargo/Wrapping/WrappedBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using AddressableReferences;
+using NaughtyAttributes;
 using Objects;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -11,6 +12,19 @@ namespace Items.Cargo.Wrapping
 	{
 		[SerializeField][Tooltip("When unwrapped, if no content was defined, we will spawn one of these")]
 		private List<GameObject> randomContentList;
+
+		[SerializeField]
+		private bool RandomPrefab = false;
+
+		[SerializeField] [ShowIf(nameof(RandomPrefab))] private SpawnType OnlySpawn = SpawnType.ItemAndObject;
+
+		private enum SpawnType
+		{
+			Item,
+			Object,
+			ItemAndObject,
+		}
+
 
 		[FormerlySerializedAs("unwrapSound")] [SerializeField] [Tooltip("Alternative sound to use when unwrapped")]
 		private AddressableAudioSource alternativeUnwrapSound = null;
@@ -28,6 +42,8 @@ namespace Items.Cargo.Wrapping
 
 		private Attributes attributes;
 		protected SpriteHandler spriteHandler;
+
+		private bool ContentGenerated = false;
 
 		protected virtual void OnEnable()
 		{
@@ -76,11 +92,67 @@ namespace Items.Cargo.Wrapping
 		public GameObject GetOrGenerateContent()
 		{
 			GameObject content = null;
-			if (randomContentList.Count > 0)
+
+			if (ContentGenerated == false)
 			{
-				content  = Spawn.ServerPrefab(randomContentList.PickRandom(), gameObject.AssumedWorldPosServer()).GameObject;
-				return content;
+				ContentGenerated = true;
+				if (RandomPrefab)
+				{
+					int Runs = 0;
+
+					bool runLoop = true;
+
+					while (runLoop)
+					{
+
+						Runs++;
+						if (Runs > 100)
+						{
+							Logger.LogError("Unable to find prefab matching parameters from allSpawnablePrefabs Breaking loop");
+							break;
+						}
+
+						var Prefab = CustomNetworkManager.Instance.allSpawnablePrefabs.PickRandom();
+
+						if (Prefab.GetComponent<UniversalObjectPhysics>() == null)
+						{
+							continue;
+						}
+
+						if (OnlySpawn == SpawnType.ItemAndObject)
+						{
+							content  = Spawn.ServerPrefab(Prefab, gameObject.AssumedWorldPosServer()).GameObject;
+							return content;
+						}
+						else if (OnlySpawn == SpawnType.Item)
+						{
+							if (Prefab.GetComponent<ItemAttributesV2>() == null)
+							{
+								continue;
+							}
+
+							content  = Spawn.ServerPrefab(Prefab, gameObject.AssumedWorldPosServer()).GameObject;
+							return content;
+						}
+						else if (OnlySpawn == SpawnType.Object)
+						{
+							if (Prefab.GetComponent<ObjectAttributes>() == null)
+							{
+								continue;
+							}
+							content  = Spawn.ServerPrefab(Prefab, gameObject.AssumedWorldPosServer()).GameObject;
+							return content;
+						}
+					}
+				}
+
+				if (randomContentList.Count > 0)
+				{
+					content  = Spawn.ServerPrefab(randomContentList.PickRandom(), gameObject.AssumedWorldPosServer()).GameObject;
+					return content;
+				}
 			}
+
 			if (GetStoredObjects() != null)
 			{
 				content = GetStoredObjects().FirstOrDefault();
@@ -88,9 +160,13 @@ namespace Items.Cargo.Wrapping
 			return content;
 		}
 
-		protected void MakeContentVisible()
+		protected void MakeContentVisible(GameObject Content = null)
 		{
-			var netTransform = GetOrGenerateContent().gameObject.GetComponent<UniversalObjectPhysics>();
+			if (Content == null)
+			{
+				Content = GetOrGenerateContent();
+			}
+			var netTransform = Content.gameObject.GetComponent<UniversalObjectPhysics>();
 			var pos = gameObject.RegisterTile().WorldPositionServer;
 			netTransform.AppearAtWorldPositionServer(pos);
 		}
