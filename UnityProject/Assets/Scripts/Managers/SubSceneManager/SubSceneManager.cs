6c62da887d671b7cc9c522b97984fadc8b6192d6
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-01-14 18:18:18 +0200

    Update SubSceneManager.cs

--- UnityProject/Assets/Scripts/Managers/SubSceneManager/SubSceneManager.cs
+++ UnityProject/Assets/Scripts/Managers/SubSceneManager/SubSceneManager.cs
@@ -192,7 +192,7 @@ public partial class SubSceneManager : MonoBehaviour
 		}
 		try
 		{
-			AO = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive, false);
+			AO = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
 		}
 		catch (Exception e)
 		{
@@ -205,8 +205,6 @@ public partial class SubSceneManager : MonoBehaviour
 			loadTimer?.IncrementLoadBar();
 			yield return WaitFor.EndOfFrame;
 		}
-
-		yield return AO.Result.ActivateAsync();
 
 		loadTimer?.IncrementLoadBar();
 		yield return StartCoroutine(HandleObjectsSync(HandlSynchronising, AO));
