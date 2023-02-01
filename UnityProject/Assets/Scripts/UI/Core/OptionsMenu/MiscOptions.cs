4d5b6838ab53507ae7e0a3d5070bcc391c70da53
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-02-01 19:13:45 -0300

    feat: introduce new streamer mode option in settings menu

---
+++ UnityProject/Assets/Scripts/UI/Core/OptionsMenu/MiscOptions.cs
@@ -0,0 +1,22 @@
+﻿using Managers.SettingsManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unitystation.Options
+{
+	public class MiscOptions: MonoBehaviour
+	{
+		[SerializeField] private Toggle streamerMode = null;
+
+		private void OnEnable()
+		{
+			streamerMode.isOn = MiscSettings.Instance.StreamerModeEnabled;
+		}
+
+		public void SetStreamerMode()
+		{
+			MiscSettings.Instance.StreamerModeEnabled = streamerMode.isOn;
+		}
+
+	}
+}
