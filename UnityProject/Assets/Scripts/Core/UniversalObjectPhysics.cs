a1122519676494df47ca057a7ee824200a19867c
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-02-10 06:03:23 +0200

    Update UniversalObjectPhysics.cs

--- UnityProject/Assets/Scripts/Core/UniversalObjectPhysics.cs
+++ UnityProject/Assets/Scripts/Core/UniversalObjectPhysics.cs
@@ -562,7 +562,7 @@ public class UniversalObjectPhysics : NetworkBehaviour, IRightClickable, IRegist
 				sprite.enabled = true;
 			}
 
-			if (isServer == false || this is not MovementSynchronisation c) return;
+			if (this is not MovementSynchronisation c) return;
 			transform.localRotation = c.playerScript.RegisterPlayer.IsLayingDown ? Quaternion.Euler(0, 0, -90) : Quaternion.Euler(0,0,0);
 		}
 		else
