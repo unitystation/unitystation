3f877ce2f7ee6fdabb96748d54cd08bbeaf71b23
Gilles <43683714+corp-0@users.noreply.github.com>
2023-02-18 15:11:52 -0300

    permissions iteration 1 (#9618)
    * chore: add two new dependencies, nuget compatibility and toml parser
    
    * feat: add new Permissions system
    This is merely a first iteration and isn't yet integrated into the game.
    
    * chore: add nuget restore argument for tests
    
    * chore: add nuget restore step for builds
    
    * chore: remove unused namespace import
    
    * chore: improved example file with some permissions and ranks that better represent current reality.
    
    * feat: add "*" wildcard to grant all permissions
    
    * refactor: separate test config from example config given to servers.

--- UnityProject/Assets/Scripts/Core/Editor/BuildServer.cs
+++ UnityProject/Assets/Scripts/Core/Editor/BuildServer.cs
@@ -116,6 +116,9 @@ static class BuildScript
 
 		ReportOptions(buildOptions);
 
+		// Restore NuGet packages
+		RestoreNugetPackages();
+
 		// Perform build
 		BuildReport buildReport = BuildPipeline.BuildPlayer(buildOptions);
 
@@ -126,6 +129,11 @@ static class BuildScript
 		// Result
 		BuildResult result = summary.result;
 		ExitWithResult(result);
+	}
+
+	private static void RestoreNugetPackages()
+	{
+		NugetForUnity.NugetHelper.Restore();
 	}
 
 	private static void ReportOptions(BuildPlayerOptions options)
---
+++ UnityProject/Assets/Tests/Permissions/PermissionsManagerTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using Systems.Permissions;
+using UnityEngine;
+
+namespace Tests.Permissions
+{
+	public class PermissionsManagerTests
+	{
+		private GameObject managerObject;
+		private PermissionsManager manager;
+
+		private const string CONFIG_CONTENT = @"
+		[ranks.god]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""*""]
+
+		[ranks.abc]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""perm_a"", ""perm_b"", ""perm_c""]
+
+		[ranks.bcd]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""perm_b"", ""perm_c"", ""perm_d""]
+
+		[ranks.no_perms]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = []
+
+		[[players]]
+		identifier = ""player_god""
+		rank = ""god""
+
+		[[players]]
+		identifier = ""player_abc""
+		rank = ""abc""
+
+		[[players]]
+		identifier = ""player_bcd""
+		rank = ""bcd""
+
+		[[players]]
+		identifier = ""player_no_perms""
+		rank = ""no_perms""
+		";
+
+		[SetUp]
+		public void Setup()
+		{
+			managerObject = new GameObject();
+			managerObject.AddComponent<PermissionsManager>();
+			manager = managerObject.GetComponent<PermissionsManager>();
+			manager.LoadPermissionsConfig(CONFIG_CONTENT);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			Object.DestroyImmediate(managerObject);
+		}
+
+		[Test]
+		public void GivenValidConfig_WhenLoadingConfig_ThenConfigIsLoaded()
+		{
+			Assert.NotNull(manager.Config);
+		}
+
+		[Test]
+		[TestCase("player_abc", "perm_a")]
+		[TestCase("player_abc", "perm_b")]
+		[TestCase("player_abc", "perm_c")]
+		[TestCase("player_bcd", "perm_b")]
+		[TestCase("player_bcd", "perm_c")]
+		[TestCase("player_bcd", "perm_d")]
+		public void GivenAUserWhoHasPermission_WhenCheckingPermission_ThenPermissionIsGranted(string identifier, string permission)
+		{
+			Assert.True(manager.HasPermission(identifier, permission));
+		}
+
+		[Test]
+		[TestCase("player_abc", "perm_d")]
+		[TestCase("player_bcd", "perm_a")]
+		[TestCase("player_no_perms", "perm_a")]
+		[TestCase("player_no_perms", "perm_a")]
+		[TestCase("player_no_perms", "perm_b")]
+		[TestCase("player_no_perms", "perm_c")]
+		[TestCase("player_no_perms", "perm_d")]
+		public void GivenAUserWhoDoesNotHavePermission_WhenCheckingPermission_ThenPermissionIsNotGranted(string identifier, string permission)
+		{
+			Assert.False(manager.HasPermission(identifier, permission));
+		}
+
+		[Test]
+		[TestCase("player_god", "perm_a")]
+		[TestCase("player_god", "perm_b")]
+		[TestCase("player_god", "perm_c")]
+		[TestCase("player_god", "perm_d")]
+		[TestCase("player_god", "perm_not_listed")]
+		public void GivenAUserWithWildcardPermission_WhenCheckingPermission_ThenPermissionIsGranted(string identifier, string permission)
+		{
+			Assert.True(manager.HasPermission(identifier, permission));
+		}
+
+		[Test]
+		public void GivenAUserThatDoesNotExist_WhenCheckingPermission_ThenPermissionIsNotGranted()
+		{
+			Assert.False(manager.HasPermission("nonexistentPlayer", "perm_a"));
+		}
+	}
+}
