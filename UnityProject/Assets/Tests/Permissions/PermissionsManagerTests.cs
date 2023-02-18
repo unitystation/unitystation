6038d1506158cb21d53ae7b6f96f049bcdf4407c
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-02-18 14:32:24 -0300

    refactor: separate test config from example config given to servers.

--- UnityProject/Assets/Tests/Permissions/PermissionsManagerTests.cs
+++ UnityProject/Assets/Tests/Permissions/PermissionsManagerTests.cs
@@ -9,13 +9,55 @@ namespace Tests.Permissions
 		private GameObject managerObject;
 		private PermissionsManager manager;
 
+		private const string CONFIG_CONTENT = @"
+		[ranks.god]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""*""]
+
+		[ranks.abc]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""perm_a"", ""perm_b"", ""perm_c""]
+
+		[ranks.bcd]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = [""perm_b"", ""perm_c"", ""perm_d""]
+
+		[ranks.no_perms]
+		show_in_chat = false
+		abbreviation = """"
+		color = """"
+		permissions = []
+
+		[[players]]
+		identifier = ""player_god""
+		rank = ""god""
+
+		[[players]]
+		identifier = ""player_abc""
+		rank = ""abc""
+
+		[[players]]
+		identifier = ""player_bcd""
+		rank = ""bcd""
+
+		[[players]]
+		identifier = ""player_no_perms""
+		rank = ""no_perms""
+		";
+
 		[SetUp]
 		public void Setup()
 		{
 			managerObject = new GameObject();
 			managerObject.AddComponent<PermissionsManager>();
 			manager = managerObject.GetComponent<PermissionsManager>();
-			manager.LoadPermissionsConfig();
+			manager.LoadPermissionsConfig(CONFIG_CONTENT);
 		}
 
 		[TearDown]
@@ -31,26 +73,45 @@ namespace Tests.Permissions
 		}
 
 		[Test]
-		[TestCase("hostPlayer", "fun")]
-		[TestCase("hostPlayer", "perma_promote")]
-		[TestCase("adminPlayer", "fun")]
+		[TestCase("player_abc", "perm_a")]
+		[TestCase("player_abc", "perm_b")]
+		[TestCase("player_abc", "perm_c")]
+		[TestCase("player_bcd", "perm_b")]
+		[TestCase("player_bcd", "perm_c")]
+		[TestCase("player_bcd", "perm_d")]
 		public void GivenAUserWhoHasPermission_WhenCheckingPermission_ThenPermissionIsGranted(string identifier, string permission)
 		{
 			Assert.True(manager.HasPermission(identifier, permission));
 		}
 
 		[Test]
-		[TestCase("normalPlayer", "perma_promote")]
-		[TestCase("normalPlayer", "fun")]
+		[TestCase("player_abc", "perm_d")]
+		[TestCase("player_bcd", "perm_a")]
+		[TestCase("player_no_perms", "perm_a")]
+		[TestCase("player_no_perms", "perm_a")]
+		[TestCase("player_no_perms", "perm_b")]
+		[TestCase("player_no_perms", "perm_c")]
+		[TestCase("player_no_perms", "perm_d")]
 		public void GivenAUserWhoDoesNotHavePermission_WhenCheckingPermission_ThenPermissionIsNotGranted(string identifier, string permission)
 		{
 			Assert.False(manager.HasPermission(identifier, permission));
 		}
 
 		[Test]
+		[TestCase("player_god", "perm_a")]
+		[TestCase("player_god", "perm_b")]
+		[TestCase("player_god", "perm_c")]
+		[TestCase("player_god", "perm_d")]
+		[TestCase("player_god", "perm_not_listed")]
+		public void GivenAUserWithWildcardPermission_WhenCheckingPermission_ThenPermissionIsGranted(string identifier, string permission)
+		{
+			Assert.True(manager.HasPermission(identifier, permission));
+		}
+
+		[Test]
 		public void GivenAUserThatDoesNotExist_WhenCheckingPermission_ThenPermissionIsNotGranted()
 		{
-			Assert.False(manager.HasPermission("nonexistentPlayer", "fun"));
+			Assert.False(manager.HasPermission("nonexistentPlayer", "perm_a"));
 		}
 	}
 }
