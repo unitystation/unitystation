d638cb58f470959c66c657ba2e5494872bcc4589
rygorborodoolin <borodilo2@gmail.com>
2023-02-26 13:28:06 +0200

    Added docstrings

--- UnityProject/Assets/Scripts/Core/Lifecycle/CleanupUtil.cs
+++ UnityProject/Assets/Scripts/Core/Lifecycle/CleanupUtil.cs
@@ -8,7 +8,12 @@ using Audio.Containers;
 
 public static class CleanupUtil
 {
-
+	/// <summary>
+	/// this methods scans the provided list and removes from it all elements, that are not marked as DontDestroyOnLoad. Will keep elements that are equal to null
+	/// <paramref name="targetExtractor"/> is just used to get the MonoBehaviour that is then being used to figure out whether or not element should be removed
+	/// </summary>
+	/// <param name="listInQuestion"> is a list that should be altered according to the rules provided by the method</param>
+	/// <param name="targetExtractor"> is a delegate that is transforming <typeparamref name="T"/> into a <c>UnityEngine.MonoBehaviour</c> that is later then removed from the <paramref name="listInQuestion"> if it's not marked as DontDestroyOnLoad </param>
 	public static int RidListOfSoonToBeDeadElements<T>(IList<T> listInQuestion, Func<T, UnityEngine.MonoBehaviour> targetExtractor)
 	{
 		if (listInQuestion == null)
@@ -39,6 +44,13 @@ public static class CleanupUtil
 		return res;
 	}
 
+	/// <summary>
+	/// this methods scans the provided list and removes from it all elements that are equal to null
+	/// <paramref name="targetExtractor"/> is just used to get the MonoBehaviour that is then being used to figure out whether or not element should be removed
+	/// </summary>
+	/// <param name="listInQuestion"> is a  list that should be altered according to the rules provided by the method</param>
+	/// <param name="targetExtractor"> is a delegate that is transforming <typeparamref name="T"/> into a <c>UnityEngine.MonoBehaviour</c> that is later then removed from the <paramref name="listInQuestion"> if it's equal to null </param>
+	/// <param name="verbose"> set it to True if you want to allow it to attempt to log typenames or even object names of objects, that would be removed from the list </param>
 	public static int RidListOfDeadElements<T>(IList<T> listInQuestion, Func<T, UnityEngine.MonoBehaviour> targetExtractor, bool verbose = false)
 	{
 		if (listInQuestion == null)
@@ -91,6 +103,11 @@ public static class CleanupUtil
 
 		return res;
 	}
+	/// <summary>
+	/// this methods scans the provided list and removes from it all instances of <c>System.Action</c> that have targets equal to null
+	/// </summary>
+	/// <param name="listInQuestion"> is a list that should be altered according to the rules provided by the method</param>
+	/// <param name="verbose"> set it to True if you want to allow it to attempt to log typenames or even object names of objects, that would be removed from the list </param>
 
 	public static int RidListOfDeadElements(IList<Action> listInQuestion, bool verbose = false)
 	{
@@ -143,6 +160,11 @@ public static class CleanupUtil
 		return res;
 	}
 
+	/// <summary>
+	/// this methods scans the provided list and removes from it all elements that are equal to null
+	/// </summary>
+	/// <param name="listInQuestion"> is a  list that should be altered according to the rules provided by the method</param>
+	/// <param name="verbose"> set it to True if you want to allow it to attempt to log typenames or even object names of objects, that would be removed from the list </param>
 	public static int RidListOfDeadElements<T>(IList<T> listInQuestion, bool verbose = false) where T: MonoBehaviour
 	{
 		if (listInQuestion == null)
@@ -193,6 +215,13 @@ public static class CleanupUtil
 		return res;
 	}
 
+	/// <summary>
+	/// this methods scans the provided dictionary and removes from it all elements that didn't pass <paramref name="survivalCondition"> curvival condition
+	/// <paramref name="survivalCondition"/> should return true of provided pair of key and value is supposed to stay in the dictionary
+	/// </summary>
+	/// <param name="dictInQuestion"> is a dictionary that should be altered according to the rules provided by the method</param>
+	/// <param name="survivalCondition"> is a delegate that is deciding whether or not pair of key and value of the dictionary should stay in the dictionary</param>
+	/// <param name="verbose"> set it to True if you want to allow it to attempt to log typenames or even object names of objects, that would be removed from the list </param>
 	public static int RidDictionaryOfDeadElements<TKey, TValue>(IDictionary<TKey, TValue> dictInQuestion, Func<TKey, TValue, bool> survivalCondition, bool verbose = false)
 	{
 		if (dictInQuestion == null)
@@ -243,7 +272,11 @@ public static class CleanupUtil
 
 		return res;
 	}
-
+	/// <summary>
+	/// this methods scans the provided dictionary and removes from it all keyvaluepairs in which either key or value are equal to null
+	/// </summary>
+	/// <param name="dictInQuestion"> is a dictionary that should be altered according to the rules provided by the method</param>
+	/// <param name="verbose"> set it to True if you want to allow it to attempt to log typenames or even object names of objects, that would be removed from the list </param>
 	public static int RidDictionaryOfDeadElements<TKey, TValue>(IDictionary<TKey, TValue> dictInQuestion, bool verbose = false)
 	{
 		if (dictInQuestion == null)
@@ -295,14 +328,23 @@ public static class CleanupUtil
 		return res;
 	}
 
+	/// <summary>
+	/// Should be called right when round ended, but before any scene unloaded
+	/// </summary>
 	public static void EndRoundCleanup()
 	{
 	}
 
+	/// <summary>
+	/// Should be called right after previous round ended, and some scenes might already be unloaded
+	/// </summary>
 	public static void CleanupInbetweenScenes()
 	{
 	}
 
+	/// <summary>
+	/// Should be called sometimes after round started and all necessary scenes are loaded
+	/// </summary>
 	public static void RoundStartCleanup()
 	{
 	}
