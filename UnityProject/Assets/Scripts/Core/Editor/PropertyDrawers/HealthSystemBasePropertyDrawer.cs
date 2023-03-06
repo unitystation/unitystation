5979322d72493b183224644f78d2f55efca9763f
Bod9001 <Bod9001@gmail.com>
2023-03-06 17:24:57 +0000

    Readable!

--- UnityProject/Assets/Scripts/Core/Editor/PropertyDrawers/HealthSystemBasePropertyDrawer.cs
+++ UnityProject/Assets/Scripts/Core/Editor/PropertyDrawers/HealthSystemBasePropertyDrawer.cs
@@ -13,22 +13,31 @@ public class HealthSystemBasePropertyDrawer : PropertyDrawer
     private Type[] _implementations;
     private int _implementationTypeIndex;
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+	    // Begin a new property group with EditorGUI
 	    EditorGUI.BeginProperty(position, label, property);
 
+	    // Set the height of the current position to be the height of a single line of text
 	    position.height = EditorGUIUtility.singleLineHeight;
 
+	    // Set the width of the dropdown menu to be the same as the position width
 	    var dropdownWidth = position.width;
+	    // Create a new rectangle at the same position as the original rectangle, but with the width of the dropdown menu
 	    var dropdownPosition = new Rect(position.x, position.y, dropdownWidth, position.height);
 
+	    // If the list of implementation types has not been created yet, create it now
 	    if (_implementations == null)
-		    _implementations = GetImplementations((attribute as SelectImplementationAttribute).FieldType).Where(impl => !impl.IsSubclassOf(typeof(UnityEngine.Object))).ToArray();
+	        // Get all classes that implement the specified attribute field type, but exclude any subclasses of UnityEngine.Object
+	        _implementations = GetImplementations((attribute as SelectImplementationAttribute).FieldType)
+	            .Where(impl => !impl.IsSubclassOf(typeof(UnityEngine.Object)))
+	            .ToArray();
 
 	    // Find the index of the current implementation type in the _implementations array
 	    var currentValue = property.managedReferenceValue;
 	    var currentIndex = currentValue == null ? -1 : Array.IndexOf(_implementations, currentValue.GetType());
 
+	    // Set forceUpdate to true if the current value is null, otherwise set it to false
 	    var forceUpdate = currentValue == null;
 
 	    // Set the initial value of _implementationTypeIndex to the index of the current implementation type, or 0 if currentValue is null
@@ -36,20 +45,25 @@ public class HealthSystemBasePropertyDrawer : PropertyDrawer
 
 	    // Show the dropdown and create an instance of the selected implementation type
 	    EditorGUI.BeginChangeCheck();
+	    // Display a dropdown menu of all available implementation types, using their full names as the display text
 	    _implementationTypeIndex = EditorGUI.Popup(dropdownPosition, "", _implementationTypeIndex, _implementations.Select(impl => impl.FullName).ToArray());
+	    // If the dropdown selection has changed or the value was null, create a new instance of the selected implementation type
 	    if (EditorGUI.EndChangeCheck() || forceUpdate)
 	    {
-		    var newImplementation = Activator.CreateInstance(_implementations[_implementationTypeIndex]);
-		    property.managedReferenceValue = newImplementation;
+	        var newImplementation = Activator.CreateInstance(_implementations[_implementationTypeIndex]);
+	        property.managedReferenceValue = newImplementation;
 	    }
 
+	    // Move the position down by one line and set the height to be the height of the property field
 	    position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
 	    position.height = EditorGUI.GetPropertyHeight(property);
 
+	    // Display the property field for the current implementation
 	    EditorGUI.PropertyField(position, property, true);
 
+	    // End the current property group with EditorGUI
 	    EditorGUI.EndProperty();
-    }
+	}
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         var height = EditorGUIUtility.singleLineHeight * 1; // Add one line for the dropdown and four for the labels
