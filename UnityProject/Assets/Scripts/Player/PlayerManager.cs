512185a2b5cb92ae1fc109871c751e2e0756a2c7
rygorborodoolin <borodilo2@gmail.com>
2023-03-06 19:59:01 +0200

    Adding back stuff that been accidentally yeeted during cherrypicking

--- UnityProject/Assets/Scripts/Player/PlayerManager.cs
+++ UnityProject/Assets/Scripts/Player/PlayerManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Player;
 using Shared.Util;
+using Systems.Character;
 using Util;
 
 public class PlayerManager : MonoBehaviour
@@ -43,6 +44,7 @@ public class PlayerManager : MonoBehaviour
 
 	/// <summary>The player script for the player while in the lobby.</summary>
 	public static JoinedViewer LocalViewerScript { get; private set; }
+	public static CharacterManager CharacterManager { get; } = new CharacterManager();
 
 	public static bool HasSpawned { get; private set; }
 
@@ -65,6 +67,10 @@ public class PlayerManager : MonoBehaviour
 		CurrentCharacterSheet = deserialized ?? new CharacterSheet();
 	}
 #endif
+	private void Start()
+	{
+		CharacterManager.Init();
+	}
 
 	private void OnEnable()
 	{
