a743f0ac26d739d57df0a0987d701bfcf922da07
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-04-13 13:27:26 +0200

    Balance changes for gibber

--- UnityProject/Assets/Scripts/Objects/Machines/Gibber.cs
+++ UnityProject/Assets/Scripts/Objects/Machines/Gibber.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using HealthV2;
 using Mirror;
 using NaughtyAttributes;
 using Systems.Electricity;
+using UI.Systems.Tooltips.HoverTooltips;
 using UnityEngine;
 
 namespace Objects.Machines
 {
-	public class Gibber : MonoBehaviour, ICheckedInteractable<HandApply>, ICheckedInteractable<MouseDrop>, IExaminable, IRightClickable
+	public class Gibber : MonoBehaviour, ICheckedInteractable<HandApply>, ICheckedInteractable<MouseDrop>, IExaminable, IRightClickable, IHoverTooltip
 	{
 		[SerializeField] private ObjectContainer storage;
 		[SerializeField] private UniversalObjectPhysics physics;
 		[SerializeField] private Integrity machineIntegrity;
 		[SerializeField] private int numberOfTimesToDamage = 8;
-		[SerializeField] private int produceMultiplier = 4;
+		[SerializeField] private int produceMultiplier = 3;
 		[SerializeField] private int damagePerFrame = 20;
 		[SerializeField] private GameObject defaultProduce;
 		[SerializeField] private SpriteHandler lights;
@@ -95,7 +97,7 @@ namespace Objects.Machines
 
 			foreach (var products in gibbed)
 			{
-				_ = Spawn.ServerPrefab(products.Key, gameObject.TileWorldPosition().To3(), gameObject.RegisterTile().Matrix.transform,
+				_ = Spawn.ServerPrefab(products.Key, gameObject.transform.position, gameObject.RegisterTile().Matrix.transform,
 					null, Mathf.Max(1, products.Value));
 			}
 			gibbed.Clear();
@@ -113,7 +115,8 @@ namespace Objects.Machines
 				{
 					gib.ApplyDamageAll(gameObject, gib.PainScreamDamage + damagePerFrame,
 						AttackType.Melee, DamageType.Brute, true);
-					if (gib.OverallHealth > LOW_HEALTH) continue;
+					if (numberOfTimesToDamage / 2 > damageNumber) gib.Death();
+					if (gib.IsDead == false) continue;
 					var meatToProduce = gib.MeatProduce.OrNull() ?? defaultProduce;
 					var skinToProduce = gib.SkinProduce.OrNull() ?? defaultProduce;
 					AddItemsThatWillBeSpawned(meatToProduce, skinToProduce);
@@ -125,7 +128,6 @@ namespace Objects.Machines
 				{
 					oldMob.Death();
 					AddItemsThatWillBeSpawned(defaultProduce);
-
 					continue;
 				}
 				if (slot.TryGetComponent<Integrity>(out var integrity) == false) continue;
@@ -159,11 +161,17 @@ namespace Objects.Machines
 			}
 		}
 
-		public string Examine(Vector3 worldPos = default(Vector3))
+		private string ExamineText()
 		{
+			if (physics.isNotPushable == false) return "It is not anchored. It will not function until it is.";
 			if (isRunning == false) return "The display reads 'Waiting..'";
 			var totalMeat = gibbed.Values.Sum();
 			return $"The display reads 'Current Output: {totalMeat}'";
+		}
+
+		public string Examine(Vector3 worldPos = default(Vector3))
+		{
+			return ExamineText();
 		}
 
 		public RightClickableResult GenerateRightClickOptions()
@@ -172,6 +180,48 @@ namespace Objects.Machines
 			if (isRunning) return rightClickResult;
 			rightClickResult.AddElement("Eject Content", () => storage.RetrieveObjects());
 			return rightClickResult;
+		}
+
+		public string HoverTip()
+		{
+			var SB = new StringBuilder();
+			SB.AppendLine("CAUTION: VERY DEADLY");
+			SB.AppendLine(ExamineText());
+			return SB.ToString();
+		}
+
+		public string CustomTitle()
+		{
+			var active = isRunning ? "[Active]" : "[Inactive]";
+			return gameObject.Object().ArticleName + $" {active}";
+		}
+
+		public Sprite CustomIcon()
+		{
+			return null;
+		}
+
+		public List<Sprite> IconIndicators()
+		{
+			return null;
+		}
+
+		public List<TextColor> InteractionsStrings()
+		{
+			List<TextColor> interactions = new List<TextColor>()
+			{
+				new TextColor()
+				{
+					Color = Color.red,
+					Text = "Drag and drop object, item or mob to this object to put it inside."
+				},
+				new TextColor()
+				{
+					Color = Color.red,
+					Text = "Click on this with an empty hand to activate it."
+				}
+			};
+			return interactions;
 		}
 	}
 }
