ff3e23284b278bc53a85493a3b3ba64f67c93198
Bod9001 <Bod9001@gmail.com>
2023-04-16 00:44:11 +0100

    Bleeding state fixed (#9802)

--- UnityProject/Assets/Scripts/HealthV2/Living/LivingHealthMasterBase.cs
+++ UnityProject/Assets/Scripts/HealthV2/Living/LivingHealthMasterBase.cs
@@ -982,7 +982,7 @@ namespace HealthV2
 			//TODO HungerState should properly have a cash optimisation here!!
 			if (BleedingState != CashedBleedingState)
 			{
-				var old = GetAlertSOFromBleedingState(BleedingState);
+				var old = GetAlertSOFromBleedingState(CashedBleedingState);
 				if (old != null)
 				{
 					BodyAlertManager.UnRegisterAlert(old);
