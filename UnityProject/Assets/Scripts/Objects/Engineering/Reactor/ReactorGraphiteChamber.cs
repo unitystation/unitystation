1392a74efd609dbe81e0002059357b262e9aafcc
Bod9001 <Bod9001@gmail.com>
2023-05-14 18:12:09 +0100

    Requires reactor core to be hooked up to a console before allowing insertion of starter rod

--- UnityProject/Assets/Scripts/Objects/Engineering/Reactor/ReactorGraphiteChamber.cs
+++ UnityProject/Assets/Scripts/Objects/Engineering/Reactor/ReactorGraphiteChamber.cs
@@ -15,11 +15,11 @@ namespace Objects.Engineering
 	{
 		public float EditorPresentNeutrons;
 		public float EditorEnergyReleased;
+
 		public GameObject UraniumOre;
 		public GameObject MetalOre;
 
-		public GameObject
-			ConstructMaterial; //Was set to PlasSteel. Changed to generic material in anticipation of changing to graphite in future.
+		public GameObject ConstructMaterial; //Was set to PlasSteel. Changed to generic material in anticipation of changing to graphite in future.
 
 		[SerializeField] private int droppedMaterialAmount = 40;
 
@@ -345,6 +345,8 @@ namespace Objects.Engineering
 
 		public bool TryInsertRod(HandApply interaction)
 		{
+
+
 			if (Validations.HasItemTrait(interaction.UsedObject, CommonTraits.Instance.ReactorRod))
 			{
 				var Rod = interaction.UsedObject.gameObject.GetComponent<ReactorChamberRod>();
@@ -360,9 +362,16 @@ namespace Objects.Engineering
 						ReactorFuelRods.Add(fuelRod);
 					}
 
+
+
 					var engineStarter = Rod as EngineStarter;
 					if (engineStarter != null)
 					{
+						if (ConnectedConsoles.Count == 0)
+						{
+							Chat.AddExamineMsgFromServer(interaction.Performer, " The hole for the starter rod seems to be closed, Seems like you need to hook it up to a console for it to open " );
+							return true;
+						}
 						ReactorEngineStarters.Add(engineStarter);
 					}
 				}
@@ -487,9 +496,11 @@ namespace Objects.Engineering
 
 		#region Multitool Interaction
 
+		public List<ReactorControlConsole> ConnectedConsoles = new List<ReactorControlConsole>();
+
 		public MultitoolConnectionType ConType => MultitoolConnectionType.ReactorChamber;
 		public bool MultiMaster => false;
-		int IMultitoolMasterable.MaxDistance => int.MaxValue;
+		int IMultitoolMasterable.MaxDistance => 30;
 
 		#endregion
 	}
