998aeca97d0af7c77d2186b36e9395e2bb9a8be1
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-06-30 17:11:44 +0300

    Vomit Bounty (#9931)
    * Vomit start
    
    * yuppie
    
    * fix text
    
    * moldy bread
    
    * Update StunOnVomit.cs
    
    * Update DefaultReactions.asset
    
    * e
    
    * RGGHHHAAAAAAAAAAAAAA
    
    * Update Human Stomach.prefab
    
    * working finally
    
    * eeeeeeeeee
    
    * example of vomiting blood
    
    * forgot this
    
    * rename to please codacy
    
    * Update StomachExpulsion.cs

---
+++ UnityProject/Assets/Scripts/Items/Implants/Organs/Vomit/LogicExtensions/VomitBloodOnTraumaLevel.cs
@@ -0,0 +1,27 @@
+﻿using Core.Factories;
+using HealthV2;
+using UnityEngine;
+
+namespace Items.Implants.Organs.Vomit.LogicExtensions
+{
+	public class VomitBloodOnTraumaLevel : MonoBehaviour, IVomitExtension
+	{
+
+		[SerializeField] private TraumaticDamageTypes traumaTypes = TraumaticDamageTypes.PIERCE;
+		[SerializeField] private Vector2 minMaxToTakeFromBlood = new Vector2(2, 12);
+		[SerializeField] private int traumaLevelToVomitBloodOn = 2;
+
+		public void OnVomit(float amount, LivingHealthMasterBase health, Stomach stomach)
+		{
+			if (stomach.RelatedPart.TryGetComponent<BodyPartTrauma>(out var trauma) == false) return;
+			foreach (var traumaLogic in trauma.TraumaTypesOnBodyPart)
+			{
+				if (traumaLogic.traumaTypes.HasFlag(traumaTypes) == false) continue;
+				if (traumaLogic.CurrentStage < traumaLevelToVomitBloodOn) continue;
+				var bloodReagent = health.reagentPoolSystem.BloodPool.Take(Random.Range(minMaxToTakeFromBlood.x, minMaxToTakeFromBlood.y));
+				EffectsFactory.BloodSplat(health.gameObject.AssumedWorldPosServer(),
+					bloodReagent, bloodReagent.Total > 6 ? BloodSplatSize.medium : BloodSplatSize.small);
+			}
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Player/EmoteScripts/Vomit.cs
@@ -0,0 +1,44 @@
+﻿using HealthV2;
+using Items.Implants.Organs.Vomit;
+using ScriptableObjects.RP;
+using UnityEngine;
+
+namespace Player.EmoteScripts
+{
+	[CreateAssetMenu(fileName = "Vomit", menuName = "ScriptableObjects/RP/Emotes/Vomit")]
+	public class Vomit : GenderedEmote
+	{
+
+		[SerializeField] private bool instant = false;
+
+		public override void Do(GameObject player)
+		{
+			var health = player.GetComponent<LivingHealthMasterBase>();
+			if (health.IsDead) return;
+
+			if (instant == false)
+			{
+				StandardProgressAction action = StandardProgressAction.Create(
+					new StandardProgressActionConfig(StandardProgressActionType.SelfHeal),
+					() => CheckAndDo(player, health));
+				Chat.AddActionMsgToChat(player, $"<color=red>{health.playerScript.visibleName} attempts to make themselves vomit.</color>");
+				action.ServerStartProgress(player.RegisterTile(), 6f, player);
+				return;
+			}
+			CheckAndDo(player, health);
+		}
+
+		private void CheckAndDo(GameObject player, LivingHealthMasterBase health)
+		{
+			var bodyParts = health.BodyPartList;
+			foreach (var part in bodyParts)
+			{
+				if (part.TryGetComponent<StomachExpulsion>(out var stomach) == false) continue;
+				stomach.Vomit();
+				base.Do(player);
+				return;
+			}
+			if(instant == false) Chat.AddExamineMsg(player, "You do not have a stomach to do this...");
+		}
+	}
+}
