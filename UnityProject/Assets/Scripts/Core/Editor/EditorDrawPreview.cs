1bbeeb93a5011f4fd11a1e906dec877d83738153
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-07-05 22:09:59 +0300

    Update EditorDrawPreview.cs

--- UnityProject/Assets/Scripts/Core/Editor/EditorDrawPreview.cs
+++ UnityProject/Assets/Scripts/Core/Editor/EditorDrawPreview.cs
@@ -102,6 +102,7 @@ public class EditorDrawPreview
 
 	private static void DrawProjectItem(string guid, Rect selectionRect)
 	{
+		if (EditorPrefs.GetBool("editorPreviewsDisable")) return;
 		if (TryGetSpriteData(guid, out var spriteData) == false) return;
 
 		var sprite = spriteData.CurrentSprite;
@@ -195,6 +196,12 @@ public class EditorDrawPreview
 		}
 
 		return new Rect(x, y, width, height);
+	}
+
+	[MenuItem("Tools/DisableEnableEditorSpritePreviews #&]")]
+	public static void CompiledDammit()
+	{
+		EditorPrefs.SetBool("editorPreviewsDisable", !EditorPrefs.GetBool("editorPreviewsDisable", false));
 	}
 }
 #endif
