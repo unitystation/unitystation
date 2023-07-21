950433b0b6ab57e78715637c822360f2a685cb69
Gilles <43683714+corp-0@users.noreply.github.com>
2023-07-21 15:17:39 -0400

    chore: tools to aid in the migration process

---
+++ UnityProject/Assets/Scripts/Core/Editor/Tools/MigrateToIdentity.cs
@@ -0,0 +1,68 @@
+﻿using Core.Identity;
+using UnityEditor;
+using UnityEngine;
+
+namespace Core.Editor.Tools
+{
+	/// <summary>
+	/// This tool is a single use tool that will migrate names and descriptions from the Attributes component to the new Identity component.
+	/// </summary>
+	public class MigrateToIdentity: ScriptableWizard
+	{
+
+		[MenuItem("Tools/Migrations/Migrate to Identity")]
+		private static void CreateWizard()
+		{
+			DisplayWizard<MigrateToIdentity>("Migrate to Identity", "Migrate");
+		}
+
+		/// <summary>
+		/// Reads all game objects in the Assets folder and returns them as an array.
+		/// </summary>
+		/// <returns></returns>
+		private GameObject[] AllGameObjects()
+		{
+			string[] guids = AssetDatabase.FindAssets("t:GameObject");
+			GameObject[] gameObjects = new GameObject[guids.Length];
+			for (int i = 0; i < guids.Length; i++)
+			{
+				string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+				gameObjects[i] = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+			}
+
+			return gameObjects;
+		}
+
+		private void MigrateNameAndDescription(GameObject[] gameObjects)
+		{
+			foreach (var go in gameObjects)
+			{
+				var entityIdentity = go.GetComponent<EntityIdentity>();
+				var attributes = go.GetComponent<global::Attributes>();
+
+				if (entityIdentity != null && attributes != null)
+				{
+					entityIdentity.SetDisplayName(entityIdentity.InitialName, attributes.ArticleName);
+					entityIdentity.SetDescription(string.Empty ,BuildDescription(attributes.InitialDescription));
+				}
+			}
+
+			AssetDatabase.SaveAssets();
+		}
+
+		private string BuildDescription(string description)
+		{
+			if (string.IsNullOrEmpty(description))
+			{
+				return "This is a {0}.";
+			}
+
+			return description;
+		}
+
+		private void OnWizardCreate()
+		{
+			MigrateNameAndDescription(AllGameObjects());
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Core/Editor/Tools/PrefabsWithoutIdentity.cs
@@ -0,0 +1,101 @@
+﻿using Core.Identity;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+using System.Collections.Generic;
+using UnityEditorInternal;
+
+namespace Core.Editor.Tools
+{
+	public class PrefabsWithoutIdentity: ScriptableWizard
+	{
+		[FormerlySerializedAs("path")] [Tooltip("Folder inside Assets where the prefabs we want to look for are located.")]
+		public Object folderObject;
+
+		private Vector2 logScrollPos;
+		private Vector2 noIdentityScrollPos;
+		private List<GameObject> noIdentityObjects = new();
+		private List<string> log = new();
+
+		[MenuItem("Tools/Migrations/Find Prefabs without Identity")]
+		private static void CreateWizard()
+		{
+			DisplayWizard<PrefabsWithoutIdentity>("Find Prefabs without Identity", "Find");
+		}
+
+		public void OnGUI()
+		{
+			folderObject = EditorGUILayout.ObjectField("Folder", folderObject, typeof(Object), false);
+			logScrollPos = EditorGUILayout.BeginScrollView(logScrollPos, GUILayout.Height(200));
+			foreach (var text in log)
+			{
+				EditorGUILayout.LabelField(text);
+			}
+			EditorGUILayout.EndScrollView();
+
+			EditorGUILayout.LabelField("Prefabs without Identity", EditorStyles.boldLabel);
+			noIdentityScrollPos = EditorGUILayout.BeginScrollView(noIdentityScrollPos, GUILayout.Height(200));
+			EditorGUILayout.BeginVertical();
+			for (int i = 0; i < noIdentityObjects.Count; i++)
+			{
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.ObjectField(noIdentityObjects[i], typeof(GameObject), false);
+				if (GUILayout.Button("Add EntityIdentity", GUILayout.Width(120)))
+				{
+					AddEntityIdentity(noIdentityObjects[i]);
+					noIdentityObjects.RemoveAt(i);
+					--i;
+				}
+				EditorGUILayout.EndHorizontal();
+			}
+			EditorGUILayout.EndVertical();
+			EditorGUILayout.EndScrollView();
+
+			EditorGUILayout.LabelField("Found " + noIdentityObjects.Count + " prefabs without identity inside " + folderObject.OrNull()?.name, EditorStyles.boldLabel);
+
+			if (GUILayout.Button("Find"))
+			{
+				OnWizardCreate();
+			}
+		}
+
+		private void OnWizardCreate()
+		{
+			log.Clear();
+			noIdentityObjects.Clear();
+			string folder = AssetDatabase.GetAssetPath(folderObject);
+			log.Add("Running!");
+			var guids = AssetDatabase.FindAssets("t:GameObject", new[] {folder});
+			var gameObjects = new GameObject[guids.Length];
+			for (int i = 0; i < guids.Length; i++)
+			{
+				string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+				gameObjects[i] = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+			}
+
+			foreach (var go in gameObjects)
+			{
+				log.Add($"Testing {go.name}...");
+				if (go.GetComponent<EntityIdentity>() == null)
+				{
+					noIdentityObjects.Add(go);
+				}
+			}
+		}
+
+		private void AddEntityIdentity(GameObject go)
+		{
+			var entityIdentity = go.AddComponent<EntityIdentity>();
+			int componentsCount = go.GetComponents<Component>().Length;
+			for (int i = 1; i < componentsCount - 2; i++)
+			{
+				ComponentUtility.MoveComponentUp(entityIdentity);
+			}
+
+			EditorUtility.SetDirty(go);
+			AssetDatabase.SaveAssets();
+			log.Add("EntityIdentity added to " + go.name);
+			Selection.activeObject = go;
+		}
+	}
+}
