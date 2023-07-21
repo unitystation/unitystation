04cb33a4bd2f5b8ca50b4ccb8a41694b646593f3
Gilles <43683714+corp-0@users.noreply.github.com>
2023-07-21 15:17:23 -0400

    feat: new unified way to get entity identity

---
+++ UnityProject/Assets/Scripts/Core/Identity/EntityIdentity.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Core.Identity
+{
+	[DisallowMultipleComponent]
+	public class EntityIdentity: NetworkBehaviour, IIdentifiable
+	{
+		[SerializeField] private string initialName = "Unknown";
+		[SyncVar(hook = nameof(SetDisplayName))]
+		private string displayName;
+
+		[SerializeField]
+		[TextArea(3, 5)]
+		[Tooltip("Most basic description of this entity. Will be the first thing that shows when examining it." +
+		         " {0} will be replaced with the current name of the entity.")]
+		private string initialDescription = "This is a {0}.";
+
+		[SerializeField]
+		[Tooltip("If true, this entity can be labelled with a Hand Labeler. This can change its name for everyone who sees it.")]
+		private bool canBeLabelled = true;
+
+		public string DisplayName => displayName;
+		public string InitialName => initialName;
+
+		public override void OnStartServer()
+		{
+			base.OnStartServer();
+			SetDisplayName(displayName, initialName);
+		}
+
+		// [Server]
+		public void SetDisplayName(string oldName, string newName)
+		{
+			displayName = newName;
+		}
+
+		public void SetDescription(string oldDescription, string newDescription)
+		{
+			initialDescription = newDescription;
+		}
+
+		public string Examine(Vector3 worldPos = default(Vector3))
+		{
+			return initialDescription.Replace("{0}", displayName);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Core/Identity/IIdentifiable.cs
@@ -0,0 +1,27 @@
+﻿namespace Core.Identity
+{
+	/// <summary>
+	/// Provides a mechanism to identify game entities in a flexible manner.
+	/// Entities implementing this interface provide their current display name,
+	/// which could be influenced by conditions such as disguise or visibility.
+	/// </summary>
+	public interface IIdentifiable: IExaminable
+	{
+		/// <summary>
+		/// Current name of this entity. This might change over time due to disguises, visibility, labelling, etc.
+		/// </summary>
+		public string DisplayName { get; }
+
+		/// <summary>
+		/// The absolute initial name of this entity. This will never change. Useful to try to identify an object by its initial name.
+		/// </summary>
+		public string InitialName { get; }
+
+		/// <summary>
+		/// Server only. Sets the display name of this entity. Make sure to add the [Server] attribute to your implementation.
+		/// </summary>
+		/// <param name="oldName"></param>
+		/// <param name="newName"></param>
+		public void SetDisplayName(string oldName, string newName);
+	}
+}
