c1c4a4c7858841bd7e6c98bb7fc7184f448207e9
Gilles <43683714+corp-0@users.noreply.github.com>
2023-07-22 20:15:35 -0400

    tools

--- UnityProject/Assets/Scripts/Core/Editor/Tools/MigrateToIdentity.cs
+++ UnityProject/Assets/Scripts/Core/Editor/Tools/MigrateToIdentity.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
-using Core.Identity;
 using UnityEditor;
 using UnityEngine;
+using Core.Identity;
 
 namespace Core.Editor.Tools
 {
-	/// <summary>
-	/// This tool is a single use tool that will migrate names and descriptions from the Attributes component to the new Identity component.
-	/// </summary>
-	public class MigrateToIdentity: ScriptableWizard
+	public class MigrateToIdentity : ScriptableWizard
 	{
 		[Tooltip("Folder inside Assets where the prefabs we want to look for are located.")]
 		public Object folderObject;
+
 		private Vector2 logScrollPos;
 		private readonly List<string> log = new();
-		private int processedPrefabs = new();
-		private int prefabsWithoutAttributes = new();
+		private int processedPrefabs = 0;
+		private int prefabsWithoutAttributes = 0;
+		private int noComponents = 0;
+
+		private Queue<GameObject> gameObjectsToProcess = new();
+		private bool isProcessing = false;
 
 		[MenuItem("Tools/Migrations/Migrate to Identity")]
 		private static void CreateWizard()
@@ -26,30 +28,45 @@ namespace Core.Editor.Tools
 
 		public void OnGUI()
 		{
+			if (isProcessing && gameObjectsToProcess.Count > 0)
+			{
+				var go = gameObjectsToProcess.Dequeue();
+				MigrateNameAndDescription(go);
+
+				if (gameObjectsToProcess.Count == 0)
+				{
+					isProcessing = false;
+					log.Add("Migration finished!");
+				}
+			}
+
 			folderObject = EditorGUILayout.ObjectField("Folder", folderObject, typeof(Object), false);
 			logScrollPos = EditorGUILayout.BeginScrollView(logScrollPos, GUILayout.Height(200));
 			foreach (var text in log)
 			{
 				EditorGUILayout.LabelField(text);
 			}
+
 			EditorGUILayout.EndScrollView();
 			EditorGUILayout.LabelField("Report", EditorStyles.boldLabel);
-			EditorGUILayout.LabelField("Processed " + processedPrefabs + " prefabs from " + folderObject.OrNull()?.name, EditorStyles.boldLabel);
-			EditorGUILayout.LabelField(prefabsWithoutAttributes + " prefabs had identity but no Attributes, so they are named Unknown ", EditorStyles.boldLabel);
+			EditorGUILayout.LabelField("Processed " + processedPrefabs + " prefabs from " + folderObject.OrNull()?.name,
+				EditorStyles.boldLabel);
+			EditorGUILayout.LabelField(
+				prefabsWithoutAttributes + " prefabs had identity but no Attributes, so they are named Unknown ",
+				EditorStyles.boldLabel);
+			EditorGUILayout.LabelField(
+				noComponents + " prefabs had neither component",
+				EditorStyles.boldLabel);
 			if (GUILayout.Button("Migrate"))
 			{
 				OnWizardCreate();
 			}
 		}
 
-		/// <summary>
-		/// Reads all game objects in the Assets folder and returns them as an array.
-		/// </summary>
-		/// <returns></returns>
-		private GameObject[] AllGameObjects()
+		private GameObject[] GetAllGameObjects()
 		{
 			string folder = AssetDatabase.GetAssetPath(folderObject);
-			var guids = AssetDatabase.FindAssets("t:GameObject", new[] {folder});
+			var guids = AssetDatabase.FindAssets("t:GameObject", new[] { folder });
 			GameObject[] gameObjects = new GameObject[guids.Length];
 			for (int i = 0; i < guids.Length; i++)
 			{
@@ -60,36 +77,46 @@ namespace Core.Editor.Tools
 			return gameObjects;
 		}
 
-		private void MigrateNameAndDescription(GameObject[] gameObjects)
+		private void OnWizardCreate()
 		{
+			GameObject[] gameObjects = GetAllGameObjects();
 			foreach (var go in gameObjects)
 			{
-				log.Add("Processing " + go.name);
-				var entityIdentity = go.GetComponent<SimpleIdentity>();
-				var attributes = go.GetComponent<global::Attributes>();
-
-				if (entityIdentity == null || attributes == null) continue;
-				if (entityIdentity && attributes == null)
-				{
-					prefabsWithoutAttributes++;
-				}
-
-				entityIdentity.SetInitialName(attributes.ArticleName);
-				entityIdentity.SetDescription(string.Empty ,BuildDescription(attributes.InitialDescription));
-				EditorUtility.SetDirty(go);
-				PrefabUtility.RecordPrefabInstancePropertyModifications(go);
-				processedPrefabs++;
+				gameObjectsToProcess.Enqueue(go);
 			}
 
-			log.Add("Saving assets...");
-			AssetDatabase.SaveAssets();
+			isProcessing = true;
+		}
+
+		private void MigrateNameAndDescription(GameObject go)
+		{
+			log.Add("Processing " + go.name);
+			var entityIdentity = go.GetComponent<SimpleIdentity>();
+			var attributes = go.GetComponent<global::Attributes>();
+
+			if (entityIdentity == null || attributes == null)
+			{
+				log.Add($"{go.name} has no identity or attributes component!");
+				noComponents++;
+				return;
+			}
+
+			if (entityIdentity && attributes == null)
+			{
+				log.Add($"{go.name} has identity but no attributes component!");
+				prefabsWithoutAttributes++;
+			}
+
+			entityIdentity.SetInitialName(attributes.InitialName);
+			entityIdentity.SetDescription(BuildDescription(attributes.InitialDescription));
+			EditorUtility.SetDirty(go);
+			processedPrefabs++;
 		}
 
 		private string BuildDescription(string description)
 		{
 			if (string.IsNullOrEmpty(description))
 			{
-
 				var article = "a";
 				if (description!.StartsWith("a", true, CultureInfo.InvariantCulture)
 				    || description.StartsWith("e", true, CultureInfo.InvariantCulture)
@@ -99,15 +126,11 @@ namespace Core.Editor.Tools
 				{
 					article = "an";
 				}
-				return "This is "+ article + " {0}.";
+
+				return "This is " + article + " {0}.";
 			}
 
 			return description;
-		}
-
-		private void OnWizardCreate()
-		{
-			MigrateNameAndDescription(AllGameObjects());
 		}
 	}
 }
