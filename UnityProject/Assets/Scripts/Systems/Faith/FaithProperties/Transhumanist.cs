a663bf9b0bd37661597e9cd6f25f11c6d3525d20
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-09-15 19:54:28 +0300

    weew

--- UnityProject/Assets/Scripts/Systems/Faith/FaithProperties/Transhumanist.cs
+++ UnityProject/Assets/Scripts/Systems/Faith/FaithProperties/Transhumanist.cs
@@ -1,7 +1,17 @@
-﻿namespace Systems.Faith.FaithProperties
+﻿using System.Collections.Generic;
+using HealthV2;
+using Items.Weapons;
+using UnityEngine;
+
+namespace Systems.Faith.FaithProperties
 {
 	public class Transhumanist : IFaithProperty
 	{
+		[SerializeField] private Occupation chaplainOccupation;
+		[SerializeField] private string chaplainBecomeBorgText;
+		[SerializeField] private List<BodyPart> bodyPartsToMakeTranshumanist = new List<BodyPart>();
+		[SerializeField] private Grenade pettyLeave;
+
 		private string faithPropertyName;
 		private string faithPropertyDesc;
 
@@ -19,17 +29,32 @@
 
 		public void Setup()
 		{
-			throw new System.NotImplementedException();
+
 		}
 
 		public void OnJoinFaith(PlayerScript newMember)
 		{
-			throw new System.NotImplementedException();
+			HandleChaplain(newMember);
+		}
+
+		private void HandleChaplain(PlayerScript newMember)
+		{
+			if (newMember.Mind.occupation != chaplainOccupation) return;
+			foreach (var parts in bodyPartsToMakeTranshumanist)
+			{
+				var newPart = Spawn.ClientPrefab(parts.gameObject);
+				newMember.playerHealth.AddingBodyPart(newPart.GameObject.GetComponent<BodyPart>());
+			}
+			Chat.AddExamineMsg(newMember.GameObject, chaplainBecomeBorgText);
 		}
 
 		public void OnLeaveFaith(PlayerScript member)
 		{
-			throw new System.NotImplementedException();
+			if (DMMath.Prob(50))
+			{
+				var EMP = Spawn.ServerPrefab(pettyLeave.gameObject);
+				EMP.GameObject.GetComponent<Grenade>()?.Explode();
+			}
 		}
 
 		public bool HasTriggeredFaithAction(PlayerScript memberWhoTriggered)
