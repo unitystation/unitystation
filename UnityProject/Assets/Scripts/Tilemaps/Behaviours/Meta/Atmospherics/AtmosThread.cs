c5b3986c9360e85a38ac83cf9e1a562bab9d2c15
Bod9001 <Bod9001@gmail.com>
2023-09-18 23:54:44 +0100

    Portable scanner

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/AtmosThread.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/AtmosThread.cs
@@ -17,7 +17,55 @@ namespace Systems.Atmospherics
 
 			if (atmosManager.StopPipes == false)
 			{
-				atmosManager.pipeList.Iterate(atmosManager.processPipeDelegator);
+				lock (atmosManager.addingpipeList)
+				{
+					var count = atmosManager.addingpipeList.Count;
+					for (int i = 0; i < count; i++)
+					{
+						atmosManager.pipeList.Add(atmosManager.addingpipeList[i]);
+					}
+					atmosManager.addingpipeList.Clear();
+
+					count = atmosManager.removeingpipeList.Count;
+					for (int i = 0; i < count; i++)
+					{
+						atmosManager.pipeList.Remove(atmosManager.removeingpipeList[i]);
+					}
+					atmosManager.removeingpipeList.Clear();
+				}
+
+				var countpipeList = atmosManager.pipeList.Count;
+
+				for (int i = 0; i < countpipeList; i++)
+				{
+					atmosManager.ProcessPipe(atmosManager.pipeList[i]);
+				}
+
+
+				lock (atmosManager.addingUpdates)
+				{
+					var count = atmosManager.addingUpdates.Count;
+					for (int i = 0; i < count; i++)
+					{
+						atmosManager.atmosphericsUpdates.Add(atmosManager.addingUpdates[i]);
+					}
+					atmosManager.addingUpdates.Clear();
+
+					count = atmosManager.removeingUpdates.Count;
+					for (int i = 0; i < count; i++)
+					{
+						atmosManager.atmosphericsUpdates.Remove(atmosManager.removeingUpdates[i]);
+					}
+					atmosManager.removeingUpdates.Clear();
+				}
+
+				var loopFor = atmosManager.atmosphericsUpdates.Count;
+
+				for (int i = 0; i < loopFor; i++)
+				{
+					atmosManager.ProcessAction(atmosManager.atmosphericsUpdates[i]);
+				}
+
 			}
 
 			foreach (var reactionManger in atmosManager.reactionManagerList)
