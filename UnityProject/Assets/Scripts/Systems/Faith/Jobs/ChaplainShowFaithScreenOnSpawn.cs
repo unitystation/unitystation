9fa1876f11bc0f3242b40c9209ca8f1d883f1ef3
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-09-22 04:53:55 +0300

    ui manager

---
+++ UnityProject/Assets/Scripts/Systems/Faith/Jobs/ChaplainShowFaithScreenOnSpawn.cs
@@ -0,0 +1,14 @@
+﻿using Systems.Character;
+
+namespace Systems.Faith.Jobs
+{
+	public class ChaplainShowFaithScreenOnSpawn : OccupationCustomEffectBase, IModifyCharacterSettings
+	{
+		//(Max): I couldn't find a better way to handle this so this stays like this i guess *shrug*
+		public CharacterSheet ModifyingCharacterSheet(CharacterSheet CharacterSheet)
+		{
+			UIManager.Instance.ChaplainFirstTimeSelectScreen.gameObject.SetActive(true);
+			return CharacterSheet;
+		}
+	}
+}
