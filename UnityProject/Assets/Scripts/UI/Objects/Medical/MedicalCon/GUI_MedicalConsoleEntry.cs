1f372d13641fc6bab6f1dece96ef456198a745fb
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-10-06 12:11:55 +0300

    Update GUI_MedicalConsoleEntry.cs

--- UnityProject/Assets/Scripts/UI/Objects/Medical/MedicalCon/GUI_MedicalConsoleEntry.cs
+++ UnityProject/Assets/Scripts/UI/Objects/Medical/MedicalCon/GUI_MedicalConsoleEntry.cs
@@ -9,33 +9,10 @@ namespace UI.Objects.Medical.MedicalCon
 	public class GUI_MedicalConsoleEntry : DynamicEntry
 	{
 		[SerializeField] private NetText_label label;
-		[SerializeField] private Image image;
-		[SerializeField] private Color unknownColor;
-		[SerializeField] private Color healthyColor;
-		[SerializeField] private Color dangerColor;
-		[SerializeField] private Color deadColor;
 
 		public void SetValues(MedicalTerminal.HealthInfo info)
 		{
 			label.MasterSetValue($"{info.Info}");
-			if (info.Mode == SuitSensor.SensorMode.OFF)
-			{
-				image.color = unknownColor;
-				return;
-			}
-
-			if (info.HealthPercent >= 75)
-			{
-				image.color = healthyColor;
-			}
-			if (info.HealthPercent < 75)
-			{
-				image.color = dangerColor;
-			}
-			if (info.HealthPercent <= 25)
-			{
-				image.color = deadColor;
-			}
 		}
 	}
 }
