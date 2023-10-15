8614c8469e775b34e2639b4444b65c53f4393a02
Bod9001 <Bod9001@gmail.com>
2023-10-15 22:49:07 +0100

    Revert "feat: fluent utility to create Rich texts with TMP (#10087)" (#10138)
    This reverts commit 062039438e2b0d0d40ea1ebfb7de694e54b48152.

--- UnityProject/Assets/Scripts/Util/Independent/FluentRichText/Styles/NonBreakingSpacesStrategy.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Util.Independent.FluentRichText.Styles
-{
-	public class NonBreakingSpacesStrategy: IStyleStrategy
-	{
-		public string ApplyStyle(string text) => $"<nobr>{text}</nobr>";
-	}
-}
