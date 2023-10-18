e77b2025bbce510aad665e997da6ac35ad4ea79b
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-10-18 16:47:02 +0300

    Revert "Revert "feat: fluent utility to create Rich texts with TMP (#10087)" (#10138)"
    This reverts commit 8614c8469e775b34e2639b4444b65c53f4393a02.

---
+++ UnityProject/Assets/Scripts/Util/Independent/FluentRichText/Styles/LinkStrategy.cs
@@ -0,0 +1,14 @@
+﻿namespace Util.Independent.FluentRichText.Styles
+{
+	public class LinkStrategy : IStyleStrategy
+	{
+		private readonly string url;
+
+		public LinkStrategy(string url)
+		{
+			this.url = url;
+		}
+
+		public string ApplyStyle(string text) => $"<link=\"{url}\">{text}</link>";
+	}
+}
---
+++ UnityProject/Assets/Scripts/Util/Independent/FluentRichText/Styles/WidthStrategy.cs
@@ -0,0 +1,27 @@
+﻿using Logs;
+
+namespace Util.Independent.FluentRichText.Styles
+{
+	public class WidthStrategy: IStyleStrategy
+	{
+		private readonly string amount;
+
+		public WidthStrategy(string amount)
+		{
+			if (CommonValidations.IsValidPixelValue(amount) == false &&
+			    CommonValidations.IsValidFontUnitValue(amount) == false &&
+			    CommonValidations.IsValidPercentageValue(amount) == false)
+			{
+				Loggy.LogError($"RichText received invalid width amount: {amount}");
+				return;
+			}
+
+			this.amount = amount;
+		}
+
+		public string ApplyStyle(string text)
+		{
+			return string.IsNullOrEmpty(amount) ? text : $"<width={amount}>{text}</width>";
+		}
+	}
+}
