ae62715c5e8d21d32e8d2e31e8a431f4db27c836
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-10-25 18:48:42 +0300

    finally working UI

--- UnityProject/Assets/Scripts/Systems/Faith/UI/ChaplainPointShopScreen.cs
+++ UnityProject/Assets/Scripts/Systems/Faith/UI/ChaplainPointShopScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Items;
+using TMPro;
 using UI.Core.NetUI;
 using UnityEngine;
 
@@ -7,13 +8,17 @@ namespace Systems.Faith.UI
 	public class ChaplainPointShopScreen : NetTab
 	{
 		[SerializeField] private NetUIDynamicList shopListTransform;
+		[SerializeField] private TMP_Text costText;
+		[SerializeField] private TMP_Text titleText;
+		[SerializeField] private TMP_Text descText;
 
 		private HolyBook provider;
+		private ShopItemButton currentFocusedElement;
 
 		private void Start()
 		{
-			if (CustomNetworkManager.IsServer == false) return;
 			provider = Provider.GetComponent<HolyBook>();
+			if (CustomNetworkManager.IsServer == false) return;
 			RefreshShop();
 		}
 
@@ -27,6 +32,27 @@ namespace Systems.Faith.UI
 				var newItem = shopListTransform.AddItem() as ShopItemButton;
 				newItem.SetValues(miracle);
 			}
+
+			if (shopListTransform.Entries.Count == 0) return;
+			//sets the data of the first item in the list in the UI as if its selected.
+			if (shopListTransform.Entries[0] is ShopItemButton button)
+			{
+				button.OnClick();
+			}
+		}
+
+		public void SetData(string title, string cost, string desc, ShopItemButton focusedElement)
+		{
+			costText.text = cost;
+			titleText.text = title;
+			descText.text = desc;
+			currentFocusedElement = focusedElement;
+		}
+
+		public void OnBuy()
+		{
+			if (currentFocusedElement == null) return;
+			currentFocusedElement.ExecuteServer(PlayerManager.LocalPlayerScript.PlayerInfo);
 		}
 	}
 }
--- UnityProject/Assets/Scripts/Systems/Faith/UI/ShopItemButton.cs
+++ UnityProject/Assets/Scripts/Systems/Faith/UI/ShopItemButton.cs
@@ -8,6 +8,8 @@ namespace Systems.Faith.UI
 	public class ShopItemButton : DynamicEntry
 	{
 		[SerializeField] private NetText_label text;
+		[SerializeField] private NetText_label hiddenCost;
+		[SerializeField] private NetText_label HiddenDesc;
 		[SerializeField] private NetSpriteHandler spriteSO;
 		[SerializeField] private ChaplainPointShopScreen screen;
 
@@ -17,12 +19,27 @@ namespace Systems.Faith.UI
 		{
 			miracle = newMiracle;
 			text.MasterSetValue(miracle.FaithMiracleName);
+			hiddenCost.MasterSetValue(miracle.MiracleCost.ToString());
+			HiddenDesc.MasterSetValue(miracle.FaithMiracleDesc);
 			spriteSO.MasterSetValue(miracle.MiracleIcon.SetID);
+			ExecuteClient();
+		}
+
+		public override void ExecuteClient()
+		{
+			base.ExecuteClient();
+			gameObject.SetActive(true);
+		}
+
+		public override void ExecuteServer(PlayerInfo subject)
+		{
+			base.ExecuteServer(subject);
+			miracle.DoMiracle();
 		}
 
 		public void OnClick()
 		{
-
+			screen.SetData(text.Value, hiddenCost.Value, HiddenDesc.Value, this);
 		}
 	}
 }
