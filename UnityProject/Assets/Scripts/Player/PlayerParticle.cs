8268d8bda0abfbc952354a59c7fcace23ebbf1b6
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-12-22 23:43:59 +0200

    Jetpack fixes and improvements

---
+++ UnityProject/Assets/Scripts/Player/PlayerParticle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Logs;
+using Mirror;
+using UnityEngine;
+
+namespace Player
+{
+	public class PlayerParticle : NetworkBehaviour
+	{
+		[SerializeField] private Transform particleHolder;
+		private List<PlayerParticleObject> particles = new List<PlayerParticleObject>();
+
+		private void Start()
+		{
+			for (int i = 0; i < particleHolder.childCount - 1; i++)
+			{
+				var obj = particleHolder.GetChild(i).gameObject;
+				PlayerParticleObject particleObject = new PlayerParticleObject()
+				{
+					Reference = obj,
+				};
+				particles.Add(particleObject);
+			}
+		}
+
+		[ClientRpc]
+		public void ToggleParticle(string id, bool state)
+		{
+			foreach (var particle in particles)
+			{
+				if (particle.ID != id) continue;
+				particle.Reference.SetActive(state);
+				return;
+			}
+			Loggy.LogError($"[PlayerParticle/ToggleParticle] - no such object named {id}.");
+		}
+	}
+
+	public class PlayerParticleObject
+	{
+		public GameObject Reference;
+		public string ID => Reference.name;
+	}
+}
