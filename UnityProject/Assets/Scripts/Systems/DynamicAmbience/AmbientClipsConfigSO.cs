168feda6dcca7ce499c53de73371320e2477669f
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-02-07 12:30:27 +0200

    done

--- UnityProject/Assets/Scripts/Systems/DynamicAmbience/AmbientClipsConfigSO.cs
+++ UnityProject/Assets/Scripts/Systems/DynamicAmbience/AmbientClipsConfigSO.cs
@@ -14,18 +14,22 @@ namespace Systems.DynamicAmbience
 		public List<AddressableAudioSource> ambientClips = new List<AddressableAudioSource>();
 		public List<BasicTile> requiredTiles = new List<BasicTile>();
 		public bool needsUnderFloorsNotCovered = false;
+		public bool onlyWorksOnMainStation = false;
+		public bool onlyUsesTileChecks = false;
 		public int priority = 0;
 
 		public bool CanTrigger(List<ItemTrait> nearbyTraits, GameObject player)
 		{
+			if (onlyUsesTileChecks && TileChecks(player)) return true;
 			return triggerTraits.Any(nearbyTraits.Contains) && TileChecks(player);
 		}
 
 		private bool TileChecks(GameObject player)
 		{
 			var registerTile = player.RegisterTile();
-			if(needsUnderFloorsNotCovered && registerTile.IsUnderFloor() == false) return false;
-			if(requiredTiles.Count == 0) return true;
+			if (onlyWorksOnMainStation && registerTile.Matrix.IsMainStation == false) return false;
+			if (needsUnderFloorsNotCovered && registerTile.IsUnderFloor() == false) return false;
+			if (requiredTiles.Count == 0) return true;
 			var tile = player.RegisterTile().GetCurrentStandingTile();
 			return tile != null && requiredTiles.Contains(tile);
 		}
--- UnityProject/Assets/Scripts/Systems/DynamicAmbience/DynamicAmbientSounds.cs
+++ UnityProject/Assets/Scripts/Systems/DynamicAmbience/DynamicAmbientSounds.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Core;
 using UnityEngine;
@@ -9,26 +10,38 @@ namespace Systems.DynamicAmbience
 	{
 		public PlayerScript root;
 		public List<AmbientClipsConfigSO> ambinceConfigs = new List<AmbientClipsConfigSO>();
+		public float timeBetweenAmbience = 135f;
 
 		private void Start()
 		{
 			if (CustomNetworkManager.IsHeadless) return;
-			UpdateManager.Add(CheckForAmbienceToPlay, 15f);
+			UpdateManager.Add(CheckForAmbienceToPlay, timeBetweenAmbience);
+		}
+
+		private void OnDestroy()
+		{
+			if (CustomNetworkManager.IsHeadless) return;
+			UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, CheckForAmbienceToPlay);
 		}
 
 		private void CheckForAmbienceToPlay()
 		{
-			if (root.Mind.NonImportantMind) return;
+			if (root.Mind.NonImportantMind || root.isOwned == false) return;
 			var traitsNearby = ComponentsTracker<Attributes>.GetNearbyTraits(root.gameObject, 6f, false);
 			var configsToPlay = new List<AmbientClipsConfigSO>();
-			var highestPriority = ambinceConfigs.First();
+			AmbientClipsConfigSO highestPriority = null;
 			foreach (var config in ambinceConfigs)
 			{
 				if (config.CanTrigger(traitsNearby, root.gameObject) == false) continue;
 				configsToPlay.Add(config);
+				if (highestPriority is null)
+				{
+					highestPriority = config;
+					continue;
+				}
 				if (config.priority > highestPriority.priority) highestPriority = config;
 			}
-			var configChoosen = DMMath.Prob(80) ? highestPriority : configsToPlay.PickRandom();
+			var configChoosen = DMMath.Prob(80) && highestPriority is not null ? highestPriority : configsToPlay.PickRandom();
 			configChoosen.OrNull()?.PlayRandomClipLocally();
 		}
 	}
