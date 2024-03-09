61697984b2b68345bf0fd033e176b5fc7f196e44
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-03-09 05:14:26 +0200

    Revert "asset tests"
    This reverts commit c8149a3e296a48d057ff766e7104c4125f520e58.

--- UnityProject/Assets/Tests/Asset/MissingAssetReferences.cs
+++ UnityProject/Assets/Tests/Asset/MissingAssetReferences.cs
@@ -12,11 +12,13 @@ namespace Tests.Asset
 	/// <summary>
 	/// Checks for missing references on prefabs and scriptable objects.
 	/// </summary>
+	[Category(nameof(Asset))]
 	public class MissingAssetReferences
 	{
 		/// <summary>
 		/// Checks if there is any prefab with components that could not be loaded.
 		/// </summary>
+		[Test]
 		public void CheckMissingComponentOnPrefabs()
 		{
 			var report = new TestReport();
@@ -35,6 +37,7 @@ namespace Tests.Asset
 		/// <summary>
 		/// Checks if there is any prefab with missing references in its components.
 		/// </summary>
+		[Test]
 		public void CheckMissingReferenceFieldsOnPrefabs()
 		{
 			var report = new TestReport();
@@ -161,6 +164,7 @@ namespace Tests.Asset
 		/// <summary>
 		/// Check if there are scriptable objects that have serializable object fields with a missing reference.
 		/// </summary>
+		[Test]
 		public void TestScriptableObjects()
 		{
 			CheckMissingScriptableObjects("ScriptableObjects");
@@ -168,6 +172,7 @@ namespace Tests.Asset
 				ReferenceStatus.Missing);
 		}
 
+		[Test]
 		public void TestScriptableObjectsSprites()
 		{
 			CheckMissingOrNullReferenceFieldsSpriteSOs();
@@ -176,6 +181,7 @@ namespace Tests.Asset
 		/// <summary>
 		/// Check if there are singleton scriptable objects that have serializable object fields with no reference.
 		/// </summary>
+		[Test]
 		public void TestSingletonScriptableObjects()
 		{
 			CheckMissingScriptableObjects("Resources/ScriptableObjectsSingletons");
