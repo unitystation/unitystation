2acc286b60136cfa6efce274ad96b05ddd5ec4ac
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-04-08 22:58:01 +0200

    RandomItemPool Fixes (#10324)
    * RandomItemPool Fixes
    
    * Update NetworkManager.prefab

---
+++ UnityProject/Assets/Scripts/Systems/Inventory/IUniversalInventoryAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systems
+{
+	public interface IUniversalInventoryAPI
+	{
+		/// <summary>
+		/// Shoves all objects in the target list into a storage system, such as ItemStorage or ObjectContainer.
+		/// </summary>
+		/// <param name="target">The objects that you want to add.</param>
+		/// <param name="onGrab">An action to do after the inventory successfully finishes adding all items.</param>
+		public void GrabObjects(List<GameObject> target, Action onGrab = null);
+
+		/// <summary>
+		/// Drops all objects inside an inventory.
+		/// </summary>
+		/// <param name="onDrop"></param>
+		public void DropObjects(Action onDrop = null);
+	}
+}
