8d2c2413a4fd814a3cc7637a396bcf472ac43623
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-07-06 04:22:40 +0300

    headstuck

---
+++ UnityProject/Assets/Scripts/UI/Systems/AdminTools/AdminLogs/AdminLogsWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Admin.Logs;
+using UnityEngine;
+
+namespace UI.Systems.AdminTools.AdminLogs
+{
+	public class AdminLogsWindow : MonoBehaviour
+	{
+		private List<LogEntry> _logEntries = new List<LogEntry>();
+
+		private void OnEnable()
+		{
+			UpdateManager.Add(UpdateMe, 4f);
+		}
+
+		private void OnDisable()
+		{
+			UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, UpdateMe);
+		}
+
+
+		private void UpdateMe()
+		{
+
+		}
+	}
+}
