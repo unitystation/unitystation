8bf099a97bb9aa4c640946f5ecddaefe060caaf5
Autumn <55225562+AutumnNova@users.noreply.github.com>
2024-07-13 04:19:24 +1000

    jukebox

---
+++ UnityProject/Assets/Scripts/Items/Bar/VinylRecord.cs
@@ -0,0 +1,11 @@
+using AddressableReferences;
+using UnityEngine;
+
+namespace Items.Bar
+{
+	public class VinylRecord : MonoBehaviour
+	{
+		[SerializeField] public AddressableAudioSource music = null;
+	}
+}
+
