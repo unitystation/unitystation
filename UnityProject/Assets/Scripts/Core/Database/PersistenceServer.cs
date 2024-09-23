78126f0a4ad43360828c1b64ff2fd9670c1245b4
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-09-23 05:23:55 +0300

    cleans up some of online characters code, and fix for character sheet db spam (#10531)
    * aaaaaaaaaaa
    
    * remove un-needed log
    
    * Update ApiServer.cs

--- UnityProject/Assets/Scripts/Core/Database/PersistenceServer.cs
+++ UnityProject/Assets/Scripts/Core/Database/PersistenceServer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Initialisation;
+using Logs;
+using UnityEngine;
 
 namespace Core.Database
 {
@@ -11,6 +14,7 @@ namespace Core.Database
 		public static Uri GetUri(string endpoint, string queries = null)
 		{
 			UriBuilder.Path = $"/persistence/{endpoint}";
+
 
 			if (string.IsNullOrEmpty(queries) == false)
 			{
@@ -59,7 +63,7 @@ namespace Core.Database
 
 		public static async Task<ApiResult<JsonObject>> DeleteAccountsCharacterByID(int id, string token)
 		{
-			var response = await ApiServer.Delete<JsonObject>(GetUri($"characters/{id}/delete"), token);
+			var response = await ApiServer.Delete<JsonObject>(GetUri($"characters/{id}/delete",""), token);
 			return response;
 		}
 	}
--- UnityProject/Assets/Scripts/UI/Systems/Character/CharacterSettings.cs
+++ UnityProject/Assets/Scripts/UI/Systems/Character/CharacterSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using Systems.Character;
