3758c6f9d4ba643e2d5065a19944604ebe94fc99
Bod9001 <Bod9001@gmail.com>
2024-10-08 22:29:51 +0100

    tweaked requiring to tritium

--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/AtmosDefines.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/AtmosDefines.cs
@@ -46,8 +46,13 @@ namespace Systems.Atmospherics
 		public const float FREON_BURN_RATE_DELTA = 4f;
 		public const float FIRE_FREON_ENERGY_RELEASED = -300000f; //amount of heat absorbed per mole of burnt freon in the tile
 
-		public const float FREON_MAXIMUM_BURN_TEMPERATURE = 273.15f;
-		public const float FREON_LOWER_TEMPERATURE = 60f; //minimum temperature allowed for the burn to go, we would have negative pressure otherwise
+		public const float FREON_MAXIMUM_BURN_TEMPERATURE = 323.15f;
+		public const float FREON_LOWER_TEMPERATURE = 223.15f; //minimum temperature allowed for the burn to go, we would have negative pressure otherwise
+
+		public const float FREON_TRITIUM_MAXIMUM_BURN_TEMPERATURE = 323.15f;
+		public const float FREON_TRITIUM_LOWER_TEMPERATURE = 60f; //minimum temperature allowed for the burn to go, we would have negative pressure otherwise
+
+
 		public const float FREON_OXYGEN_FULLBURN = 10f;
 
 		public const float N2O_DECOMPOSITION_MIN_ENERGY = 1400f;
--- UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Data/Reactions/FreonFireReaction.cs
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Meta/Atmospherics/Data/Reactions/FreonFireReaction.cs
@@ -7,6 +7,7 @@ namespace Systems.Atmospherics
 	public class FreonFireReaction : Reaction
 	{
 		private static System.Random rnd = new System.Random();
+
 		public bool Satisfies(GasMix gasMix)
 		{
 			throw new System.NotImplementedException();
@@ -15,20 +16,42 @@ namespace Systems.Atmospherics
 		public void React(GasMix gasMix, MetaDataNode node)
 		{
 			var energyReleased = 0f;
-			var oldHeatCap = gasMix.WholeHeatCapacity;
 
 			var temperatureScale = 1f;
 
-			if (gasMix.Temperature < AtmosDefines.FREON_LOWER_TEMPERATURE)
+			if (gasMix.Temperature is < AtmosDefines.FREON_TRITIUM_LOWER_TEMPERATURE
+			    or > AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE)
 			{
 				temperatureScale = 0;
 			}
+
+			var TotalMoles = gasMix.Moles;
+			var TritiumMoles = gasMix.GetMoles(Gas.Tritium);
+
+			var HotIceCompatible = false;
+
+			if ((TritiumMoles / TotalMoles) > 0.005f) //0.5%
+			{
+				HotIceCompatible = true;
+				temperatureScale = (AtmosDefines.FREON_TRITIUM_MAXIMUM_BURN_TEMPERATURE - gasMix.Temperature) /
+				                   (AtmosDefines.FREON_TRITIUM_MAXIMUM_BURN_TEMPERATURE - AtmosDefines.FREON_TRITIUM_LOWER_TEMPERATURE);
+			}
 			else
 			{
-				temperatureScale = (AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE - gasMix.Temperature) / (AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE - AtmosDefines.FREON_LOWER_TEMPERATURE);
+				HotIceCompatible = false;
+				temperatureScale = (AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE - gasMix.Temperature)
+				                   / (AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE - AtmosDefines.FREON_LOWER_TEMPERATURE);
+
+				if (gasMix.Temperature is < AtmosDefines.FREON_LOWER_TEMPERATURE
+				    or > AtmosDefines.FREON_MAXIMUM_BURN_TEMPERATURE)
+				{
+					temperatureScale = 0;
+				}
 			}
 
-			if (temperatureScale >= 0)
+
+			var oldHeatCap = gasMix.WholeHeatCapacity;
+			if (temperatureScale > 0)
 			{
 				var oxygenBurnRate = AtmosDefines.OXYGEN_BURN_RATE_BASE - temperatureScale;
 
@@ -41,19 +64,21 @@ namespace Systems.Atmospherics
 				}
 				else
 				{
-					freonBurnRate = (temperatureScale * (gasMix.GetMoles(Gas.Oxygen) / AtmosDefines.FREON_OXYGEN_FULLBURN) / AtmosDefines.FREON_BURN_RATE_DELTA);
+					freonBurnRate = (temperatureScale *
+					                 (gasMix.GetMoles(Gas.Oxygen) / AtmosDefines.FREON_OXYGEN_FULLBURN) /
+					                 AtmosDefines.FREON_BURN_RATE_DELTA);
 				}
 
 				if (freonBurnRate > 0.0001f)
 				{
-					freonBurnRate = Mathf.Min(freonBurnRate, gasMix.GetMoles(Gas.Freon), gasMix.GetMoles(Gas.Oxygen));
+					freonBurnRate = Mathf.Min(freonBurnRate, gasMix.GetMoles(Gas.Freon),
+						gasMix.GetMoles(Gas.Oxygen));
 
 					gasMix.RemoveGas(Gas.Freon, freonBurnRate);
 					gasMix.RemoveGas(Gas.Oxygen, freonBurnRate * oxygenBurnRate);
-
 					gasMix.AddGasWithTemperature(Gas.CarbonDioxide, freonBurnRate, gasMix.Temperature);
 
-					if (gasMix.Temperature < 160 && gasMix.Temperature > 120 && rnd.Next(0, 2) == 0)
+					if (gasMix.Temperature < 160 && gasMix.Temperature > 120 && DMMath.Prob(2) && HotIceCompatible)
 					{
 						SpawnSafeThread.SpawnPrefab(node.WorldPosition, AtmosManager.Instance.HotIce);
 					}
