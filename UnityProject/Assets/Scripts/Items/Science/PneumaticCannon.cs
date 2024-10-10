52877c503542a25037aef45cf6d1c7eca96ff688
Atner <48405920+AtnerNT@users.noreply.github.com>
2024-10-11 03:06:06 +0800

    Firther Pneumatic Stuff

--- UnityProject/Assets/Scripts/Items/Science/PneumaticCannon.cs
+++ UnityProject/Assets/Scripts/Items/Science/PneumaticCannon.cs
@@ -5,7 +5,7 @@ using Objects.Atmospherics;
 using Systems.Atmospherics;
 using UnityEngine;
 
-public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryApply>, ICheckedInteractable<AimApply>, ICheckedInteractable<HandActivate>, IServerSpawn
+public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryApply>, ICheckedInteractable<MouseDrop>, ICheckedInteractable<AimApply>, ICheckedInteractable<HandActivate>, IServerSpawn
 {
 	private enum PressureSetting
 	{
@@ -29,8 +29,7 @@ public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryAppl
 	[SerializeField] private SpriteHandler canisterSpriteHandler = null;
 	[SerializeField] private SpriteHandler bindingSprite = null;
 
-	[SerializeField] private AddressableAudioSource firingSound = null;
-
+	[SerializeField] private AddressableAudioSource dryFiringSound = null;
 	private bool isOnCooldown => Time.time - lastShootTime < SHOOT_COOLDOWN_TIME;
 
 	private float lastShootTime = 0;
@@ -71,6 +70,7 @@ public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryAppl
 
 	public bool WillInteract(InventoryApply interaction, NetworkSide side)
 	{
+		Debug.Log("Will Interact");
 		if (DefaultWillInteract.Default(interaction, side) == false) return false;
 		if (interaction.TargetObject == gameObject && interaction.IsFromHandSlot) return true;
 
@@ -81,55 +81,75 @@ public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryAppl
 	{
 		if (interaction.TargetObject != gameObject || interaction.IsFromHandSlot == false) return;
 
-		if (interaction.UsedObject != null) FullHandInteraction(interaction);
-		else EmptyHandInteraction(interaction);
+		if (interaction.UsedObject != null) FullHandInteraction(interaction.FromSlot, interaction.Performer, interaction.UsedObject);
+		else EmptyHandInteraction(interaction.FromSlot);
 	}
 
-	public void FullHandInteraction(InventoryApply interaction)
+	public void FullHandInteraction(ItemSlot fromSlot, GameObject performer, GameObject usedObject)
 	{
-		if (interaction.UsedObject.TryGetComponent<GasContainer>(out var canister))
+		if (usedObject.TryGetComponent<GasContainer>(out var canister))
 		{
 			if (this.canister == null)
 			{
 				this.canister = canister;
-				Inventory.ServerTransfer(interaction.FromSlot, canisterSlot, ReplacementStrategy.Cancel);
-				Chat.AddExamineMsgFromServer(interaction.Performer, $"You load the {interaction.UsedObject.ExpensiveName()} into the {gameObject.ExpensiveName()}'s canister slot.");
+				Inventory.ServerTransfer(fromSlot, canisterSlot, ReplacementStrategy.DropOther);
+				Chat.AddExamineMsgFromServer(performer, $"You load the {usedObject.ExpensiveName()} into the {gameObject.ExpensiveName()}'s canister slot.");
 				UpdateCanisterSprites(canister);
 				return;
 			}
 			else
 			{
-				Chat.AddWarningMsgFromServer(interaction.Performer, $"The {gameObject.ExpensiveName()} already contains a valid gas canister! Please remove the existing tank before attempting to attach another.");
+				Chat.AddWarningMsgFromServer(performer, $"The {gameObject.ExpensiveName()} already contains a valid gas canister! Please remove the existing tank before attempting to attach another.");
 				return;
 			}
 		}
 		else if (itemSlot.IsEmpty == false)
 		{
-			Chat.AddWarningMsgFromServer(interaction.Performer, $"The {gameObject.ExpensiveName()} already contains an item in it's barrel! Please remove the existing item before attempting to load another.");
+			Chat.AddWarningMsgFromServer(performer, $"The {gameObject.ExpensiveName()} already contains an item in it's barrel! Please remove the existing item before attempting to load another.");
 			return;
 		}
 		else
 		{
-			Inventory.ServerTransfer(interaction.FromSlot, itemSlot, ReplacementStrategy.Cancel);
-			Chat.AddExamineMsgFromServer(interaction.Performer, $"You load the {interaction.UsedObject.ExpensiveName()} into the {gameObject.ExpensiveName()}'s item slot.");
+			Inventory.ServerTransfer(fromSlot, itemSlot, ReplacementStrategy.DropOther);
+			Chat.AddExamineMsgFromServer(performer, $"You load the {usedObject.ExpensiveName()} into the {gameObject.ExpensiveName()}'s item slot.");
 			return;
 		}
 	}
 
-	public void EmptyHandInteraction(InventoryApply interaction)
+	public void EmptyHandInteraction(ItemSlot fromSlot)
 	{
 		if (itemSlot.IsEmpty == false)
 		{
-			Inventory.ServerTransfer(itemSlot, interaction.FromSlot, ReplacementStrategy.Cancel);
+			Inventory.ServerTransfer(itemSlot, fromSlot, ReplacementStrategy.Cancel);
 			return;
 		}
 		if (canisterSlot.IsEmpty == false)
 		{
-			Inventory.ServerTransfer(canisterSlot, interaction.FromSlot, ReplacementStrategy.Cancel);
+			Inventory.ServerTransfer(canisterSlot, fromSlot, ReplacementStrategy.Cancel);
 			canister = null;
 			UpdateCanisterSprites(canister);
 			return;
 		}
+	}
+
+	#endregion
+
+	#region MouseDrop Interactions
+
+	public bool WillInteract(MouseDrop interaction, NetworkSide side)
+	{
+		if (DefaultWillInteract.Default(interaction, side) == false) return false;
+		if (interaction.TargetObject == gameObject && interaction.IsFromInventory) return true;
+
+		return false;
+	}
+
+	public void ServerPerformInteraction(MouseDrop interaction)
+	{
+		if (interaction.TargetObject != gameObject || interaction.IsFromInventory == false) return;
+
+		if (interaction.UsedObject != null) FullHandInteraction(interaction.FromSlot, interaction.Performer, interaction.UsedObject);
+		else EmptyHandInteraction(interaction.FromSlot);
 	}
 
 	#endregion
@@ -174,18 +194,34 @@ public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryAppl
 
 	public void ServerPerformInteraction(AimApply interaction)
 	{
-		lastShootTime = Time.time;
+		bool fireFailed = false;
 		if(canister == null)
 		{
-			Chat.AddWarningMsgFromServer(interaction.Performer, "You attempt to fire the cannon but no canister is loaded!");
-			return;
+			Chat.AddWarningMsgFromServer(interaction.Performer, $"You attempt to fire the cannon {gameObject.ExpensiveName()} no canister is loaded!");
+			fireFailed = true;
 		}
-		if (canister.ServerInternalPressure < CANNON_MIN_FUNCTIONAL_PRESSURE)
+		else if (canister.ServerInternalPressure < CANNON_MIN_FUNCTIONAL_PRESSURE)
 		{
-			Chat.AddWarningMsgFromServer(interaction.Performer, "You attempt to fire the cannon but the gas pressure was too low!");
+			Chat.AddWarningMsgFromServer(interaction.Performer, $"You attempt to fire the cannon {gameObject.ExpensiveName()} the gas pressure was too low!");
+			fireFailed = true;
+		}
+		else if (itemSlot.IsEmpty)
+		{
+			Chat.AddWarningMsgFromServer(interaction.Performer, $"You attempt to fire the {gameObject.ExpensiveName()} but no item was loaded!");
+			fireFailed = true;
+		}
+
+		if(fireFailed)
+		{
+			SoundManager.PlayNetworkedAtPos(dryFiringSound, transform.position, sourceObj: interaction.Performer);
 			return;
 		}
 
+		Chat.AddActionMsgToChat(interaction.Performer,
+					$"You fire your {gameObject.ExpensiveName()}",
+					$"{interaction.Performer.ExpensiveName()} fires their {gameObject.ExpensiveName()}");
+
+		lastShootTime = Time.time;
 		float percentDischarge = Mathf.Clamp(((int)pressureSetting * CANNON_MAX_PRESSURE) / canister.GasMixLocal.Pressure, 0, 1);
 		float pressureToDischarge = Math.Min((int)pressureSetting * CANNON_MAX_PRESSURE, canister.GasMixLocal.Pressure);
 
@@ -196,10 +232,8 @@ public class PneumaticCannon : MonoBehaviour, ICheckedInteractable<InventoryAppl
 		GasMix mixToEffect = metaNode.GasMixLocal;
 		GasMix.TransferGas(mixToEffect, canister.GasMixLocal, molesToTransfer);
 
-		SoundManager.PlayNetworkedAtPos(firingSound, interaction.originatorPosition);
+		SoundManager.PlayNetworkedAtPos(CommonSounds.Instance.Valve, transform.position, sourceObj: interaction.Performer);
 		if (pressureToDischarge >= (int)PressureSetting.High * CANNON_MAX_PRESSURE) interaction.PerformerPlayerScript.RegisterPlayer.ServerStun(dropItem: false);
-
-		if (itemSlot.IsEmpty) return;
 
 		//We assume a barrel area of 0.1 square meters applied for 1/10th of a second.
 		var physics = itemSlot.ItemObject.GetComponent<UniversalObjectPhysics>();
