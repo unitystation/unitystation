dbc3bc68f1a38c1158ae9f9c3c9397c72e74e675
Bod9001 <Bod9001@gmail.com>
2024-10-22 23:21:15 +0100

    hopefully better hhandling of client state

--- UnityProject/Assets/Scripts/Core/Utils/IClientSynchronisedEffect.cs
+++ UnityProject/Assets/Scripts/Core/Utils/IClientSynchronisedEffect.cs
@@ -6,9 +6,7 @@ public interface IClientSynchronisedEffect : IClientPlayerLeaveBody, IClientPlay
 {
 	public uint OnPlayerID { get; }
 
-	public bool IsOnLocalPlayer => PlayerManager.LocalMindScript != null
-	                               && PlayerManager.LocalMindScript.IsRelatedToObject(OnPlayerID.NetIdToGameObject())
-	                               && PlayerManager.LocalMindScript.IsGhosting == false;
+	public bool IsOnLocalPlayer => ClientSynchronisedEffectsManager.CurrentlyOn == OnPlayerID;
 
 	void IClientPlayerLeaveBody.ClientOnPlayerLeaveBody()
 	{
