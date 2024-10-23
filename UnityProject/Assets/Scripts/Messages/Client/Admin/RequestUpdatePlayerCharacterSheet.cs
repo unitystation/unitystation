6b288164760d084cfb47acfb42b1c7df535efc7c
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2024-10-23 12:45:20 +0300

    Admin Character Sheet Editor

---
+++ UnityProject/Assets/Scripts/Messages/Client/Admin/RequestUpdatePlayerCharacterSheet.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Logs;
+using Messages.Server;
+using Mirror;
+using Newtonsoft.Json;
+using Systems.Character;
+using UI.Admin.DIMGUI.Characters;
+using UnityEngine;
+
+namespace Messages.Client.Admin
+{
+	public class RequestUpdatePlayerCharacterSheet : ClientMessage<RequestUpdatePlayerCharacterSheet.NetMessage>
+	{
+		public struct NetMessage : NetworkMessage
+		{
+			public string CharacterSheetJson;
+			public string AccountID;
+		}
+
+		public override void Process(NetMessage msg)
+		{
+			PlayerInfo account = null;
+			foreach (var player in PlayerList.Instance.AllPlayers)
+			{
+				if (player.AccountId == msg.AccountID)
+				{
+					account = player;
+					break;
+				}
+			}
+
+			if (account == null)
+			{
+				Loggy.LogError("[RequestUpdatePlayerCharacterSheet] Could not find account to update, or lacking admin permissions.");
+				UpdateTheRequestToCharacterSheetUpdateToRequests.SendSheetUpdate(SentByPlayer, false);
+				return;
+			}
+
+			try
+			{
+				CharacterSheet sheet = JsonConvert.DeserializeObject<CharacterSheet>(msg.CharacterSheetJson);
+				account.Mind.CurrentCharacterSettings = sheet;
+				UpdateTheRequestToCharacterSheetUpdateToRequests.SendSheetUpdate(SentByPlayer);
+			}
+			catch (Exception e)
+			{
+				if (account.Mind == null)
+				{
+					Loggy.LogError("[RequestUpdatePlayerCharacterSheet] Could not find a mind to link sheet to.");
+				}
+				else
+				{
+					Loggy.LogError(e.ToString());
+				}
+				UpdateTheRequestToCharacterSheetUpdateToRequests.SendSheetUpdate(SentByPlayer, false);
+			}
+		}
+
+		public static NetMessage SendSheetUpdate(string userIDToUpdate, string characterSheetJson)
+		{
+			var msg = new NetMessage
+			{
+				CharacterSheetJson = characterSheetJson,
+				AccountID = userIDToUpdate,
+			};
+
+			Send(msg);
+			return msg;
+		}
+	}
+
+	public class UpdateTheRequestToCharacterSheetUpdateToRequests : ServerMessage<UpdateTheRequestToCharacterSheetUpdateToRequests.NetMessage>
+	{
+		public struct NetMessage : NetworkMessage
+		{
+			public bool IsSuccess;
+		}
+
+		public override void Process(NetMessage msg)
+		{
+			//(Max): This sucks massive balls for performance. but we can't use netid on UI.
+			// at least the client is the one that will handle this, not the server.
+			var obj = GameObject.FindObjectsByType<CharacterSheetEditor>(FindObjectsSortMode.InstanceID);
+			if (obj == null)
+			{
+				Loggy.LogError("[UpdateTheRequestToCharacterSheetUpdateToRequests] Could not find editor to update.");
+				return;
+			}
+			if (obj.Length != 0)
+			{
+				if (msg.IsSuccess)
+				{
+					obj[0].NetMessage_SuccessEvent();
+					Debug.Log("Success");
+				}
+				else
+				{
+					obj[0].NetMessage_FailEvent();
+				}
+			}
+		}
+
+		public static NetMessage SendSheetUpdate(PlayerInfo requester, bool isSuccess = true)
+		{
+			var msg = new NetMessage
+			{
+				IsSuccess = isSuccess
+			};
+
+			Debug.Log(requester.AccountId + isSuccess);
+
+			SendTo(requester, msg);
+			return msg;
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Util/IMGUIHelper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using ImGuiNET;
+
+namespace Util
+{
+	public static class IMGUIHelper
+	{
+		public static void DrawField(FieldInfo field, object target)
+		{
+			var fieldType = field.FieldType;
+			var value = field.GetValue(target);
+
+			if (fieldType == typeof(int))
+			{
+				var intValue = (int)value;
+				if (ImGui.InputInt(field.Name, ref intValue)) field.SetValue(target, intValue);
+			}
+			else if (fieldType.IsEnum)
+			{
+				// Get the names and values of the enum
+				var enumValues = Enum.GetValues(fieldType);
+				var enumNames = Enum.GetNames(fieldType);
+				var currentEnumIndex = Array.IndexOf(enumValues, value);
+
+				if (ImGui.Combo(field.Name, ref currentEnumIndex, enumNames, enumNames.Length))
+					// Set the field to the newly selected enum value
+					field.SetValue(target, enumValues.GetValue(currentEnumIndex));
+			}
+			else if (typeof(IList).IsAssignableFrom(fieldType) || fieldType.IsArray)
+			{
+				var list = (IList)value;
+
+				if (list == null)
+				{
+					ImGui.Text($"{field.Name}: null");
+					return;
+				}
+
+				// Create a collapsible TreeNode for the list
+				if (ImGui.TreeNode($"{field.Name} (Count: {list.Count})"))
+				{
+					ImGui.Separator();
+
+					// Iterate over the list and draw its elements when expanded
+					for (var i = 0; i < list.Count; i++)
+					{
+						ImGui.Text($"Element {i}:");
+						var element = list[i];
+
+						if (element != null)
+							DrawObjectField(element, i.ToString()); // Draw list element based on type
+						else
+							ImGui.Text("null");
+
+						// Optionally, allow removing elements
+						if (ImGui.Button($"Remove Element {i}"))
+						{
+							list.RemoveAt(i);
+							break; // Safeguard to avoid modifying the list while iterating
+						}
+					}
+
+					// Optionally, add a button to allow adding new elements
+					if (ImGui.Button($"Add Element to {field.Name}"))
+					{
+						var elementType = fieldType.IsArray
+							? fieldType.GetElementType()
+							: fieldType.GetGenericArguments()[0];
+						var newElement = CreateDefaultInstance(elementType);
+						list.Add(newElement);
+					}
+
+					// Close the collapsible TreeNode
+					ImGui.TreePop();
+				}
+			}
+			else if (fieldType == typeof(float))
+			{
+				var floatValue = (float)value;
+				if (ImGui.InputFloat(field.Name, ref floatValue)) field.SetValue(target, floatValue);
+			}
+			else if (fieldType == typeof(bool))
+			{
+				var boolValue = (bool)value;
+				if (ImGui.Checkbox(field.Name, ref boolValue)) field.SetValue(target, boolValue);
+			}
+			else if (fieldType == typeof(string))
+			{
+				var strValue = (string)value ?? string.Empty;
+				if (ImGui.InputText(field.Name, ref strValue, 100)) field.SetValue(target, strValue);
+			}
+			else
+			{
+				ImGui.Text($"{field.Name}: {value?.ToString() ?? "null"}");
+			}
+		}
+
+		public static void DrawObjectField(object obj, string label)
+		{
+			if (obj == null)
+			{
+				ImGui.Text($"{label}: null");
+				return;
+			}
+
+			var type = obj.GetType();
+			var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (var field in fields) DrawField(field, obj); // Recursively draw fields of the object
+		}
+
+		public static object CreateDefaultInstance(Type type)
+		{
+			if (type == typeof(int)) return 0;
+			if (type == typeof(float)) return 0.0f;
+			if (type == typeof(bool)) return false;
+			if (type == typeof(string)) return string.Empty;
+
+			// For other types, use Activator to create a new instance (for classes or structs)
+			return Activator.CreateInstance(type);
+		}
+
+		public static void DrawObjectFields(object target)
+		{
+			if (target == null)
+			{
+				ImGui.Text("Error: Attempted to render object that is null.");
+				return;
+			}
+
+			var type = target.GetType();
+			var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (var field in fields)
+			{
+				DrawField(field, target);
+			}
+		}
+	}
+}
