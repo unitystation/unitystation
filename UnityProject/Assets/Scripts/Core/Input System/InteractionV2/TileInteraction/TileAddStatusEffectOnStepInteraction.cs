567d4809c1e810fa5718140e9a2e6aa548fb7e87
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-03-02 19:15:32 +0200

    OnStepSlow

---
+++ UnityProject/Assets/Scripts/Core/Input System/InteractionV2/TileInteraction/TileAddStatusEffectOnStepInteraction.cs
@@ -0,0 +1,23 @@
+﻿using Systems.Interaction;
+using Systems.StatusesAndEffects;
+using UnityEngine;
+
+namespace Core.Input_System.InteractionV2.TileInteraction
+{
+	[CreateAssetMenu(fileName = "TileStatusEffectOnStep", menuName = "Interaction/TileInteraction/TileStatusEffectOnStep")]
+	public class TileAddStatusEffectOnStepInteraction : TileStepInteraction
+	{
+		public StatusEffect StatusEffectOnStep;
+
+		public override bool WillAffectPlayer(PlayerScript playerScript)
+		{
+			return true;
+		}
+
+		public override void OnPlayerStep(PlayerScript playerScript)
+		{
+			base.OnPlayerStep(playerScript);
+			playerScript.StatusEffectManager.AddStatus(StatusEffectOnStep);
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Systems/StatusesAndEffects/Implementations/SpeedDebuffSingle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using HealthV2;
+using Logs;
+using Systems.StatusesAndEffects.Interfaces;
+using UnityEngine;
+
+namespace Systems.StatusesAndEffects.Implementations
+{
+	[CreateAssetMenu(fileName = "Speed Debuff", menuName = "ScriptableObjects/StatusEffects/SpeedDebuff")]
+	public class SpeedDebuffSingle : StatusEffect, IExpirableStatus
+	{
+		public event Action<IExpirableStatus> Expired;
+		public float Duration => duration;
+		public float duration = 2f;
+		public DateTime DeathTime { get; set; }
+
+		public float Debuff = 2.5f;
+
+
+		public override void OnAdded()
+		{
+			DeathTime = DateTime.Now.AddSeconds(Duration);
+			var playerBase = target.GetComponent<PlayerScript>();
+			UpdateManager.Add(CheckExpiration, 1.5f);
+			if (playerBase == null)
+			{
+				Loggy.Warning($"[SpeedBuff] - Oi govna, can't make an inanimate object ({target}) more inanimate.");
+				return;
+			}
+			foreach (var limb in playerBase.playerHealth.GetBodyFunctionsOfType<Limb>())
+			{
+				limb.SetNewEfficiency(-Debuff, this);
+			}
+		}
+
+		public override void OnRemoved()
+		{
+			base.OnRemoved();
+			UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, CheckExpiration);
+			var playerBase = target.GetComponent<PlayerScript>();
+			if (playerBase == null) return;
+			foreach (var limb in playerBase.playerHealth.GetBodyFunctionsOfType<Limb>())
+			{
+				limb.RemoveEfficiency(this);
+			}
+		}
+
+		public void CheckExpiration()
+		{
+			if (DateTime.Now > DeathTime)
+			{
+				Expired?.Invoke(this);
+			}
+		}
+	}
+}
