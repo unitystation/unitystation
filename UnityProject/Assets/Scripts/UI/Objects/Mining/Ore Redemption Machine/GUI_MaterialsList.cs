7618f29e5c4f05ace07174c9434354f9bc1ba8d8
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-03-21 18:39:37 +0200

    Fix ore machine UI not updating correctly (#10762)
    * Fix ore machine UI not updating correctly
    
    * explaniation

--- UnityProject/Assets/Scripts/UI/Objects/Mining/Ore Redemption Machine/GUI_MaterialsList.cs
+++ UnityProject/Assets/Scripts/UI/Objects/Mining/Ore Redemption Machine/GUI_MaterialsList.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UI.Core.NetUI;
 using Objects.Machines;
@@ -13,15 +12,26 @@ namespace UI.Objects.Cargo
 
 		public void UpdateMaterialList()
 		{
+			_ = SetList();
+		}
+
+		private async UniTask SetList()
+		{
 			var materialRecords = materialStorageLink.usedStorage.MaterialList;
 			materialList.Clear();
+			await UniTask.WaitForEndOfFrame(); // wait for the list to clear correctly.
 			materialList.AddItems(materialRecords.Count);
 			var i = 0;
+			await UniTask.WaitForEndOfFrame(); // wait for the list populate.
 			foreach (var material in materialRecords.Keys)
 			{
 				var item = materialList.Entries[i] as GUI_MaterialEntry;
-				item.SetValues(material, materialRecords[material], this);
+				item?.SetValues(material, materialRecords[material], this);
 				i++;
+				//(Max): This shit fucking sucks major balls. NetUI is ass.
+				//At least we have a fun looking update effect from this workaround to make sure that NetUI doesn't shit itself
+				//when updating entries.
+				await UniTask.WaitForEndOfFrame();
 			}
 		}
 	}
