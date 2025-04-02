145407ca944d158bf3076160ed2fd7341b081e5e
Bod9001 <Bod9001@gmail.com>
2025-04-03 00:59:23 +0100

    Silly walk (#10787)
    * fixes chemistry link
    
    * fixes action text being rotated randomly
    
    * Silly walk
    
    * fix

--- UnityProject/Assets/Scripts/Items/Implants/Organs/Brain.cs
+++ UnityProject/Assets/Scripts/Items/Implants/Organs/Brain.cs
@@ -54,6 +54,9 @@ namespace Items.Implants.Organs
 		public UnityEvent OnDeath = new UnityEvent();
 		public UnityEvent OnRevival = new UnityEvent();
 
+		public bool hasSillyWalk;
+		public bool HasSillyWalk => hasSillyWalk;
+
 		[RightClickMethod]
 		public void Possess()
 		{
@@ -65,6 +68,21 @@ namespace Items.Implants.Organs
 
 					PlayerManager.LocalMindScript.CmdRequestPossess(this.gameObject.NetId());
 				}
+			}
+		}
+
+		public void SetSillyWalk(bool State)
+		{
+			hasSillyWalk = State;
+			RecordPositionSillyWalk(hasSillyWalk);
+		}
+
+
+		public void RecordPositionSillyWalk(bool State)
+		{
+			if (LivingHealthMaster != null)
+			{
+				(LivingHealthMaster.ObjectBehaviour as MovementSynchronisation).ServerSillyWalk.RecordPosition(this, State);
 			}
 		}
 
@@ -112,6 +130,8 @@ namespace Items.Implants.Organs
 				livingHealth.IsMute.RecordPosition(this, CannotSpeak);
 			}
 
+			RecordPositionSillyWalk(hasSillyWalk);
+
 			UpdateChatModifier(true);
 			livingHealth.OnDeath += DeathEvent;
 			livingHealth.OnRevive.AddListener(ReviveEvent);
@@ -126,6 +146,8 @@ namespace Items.Implants.Organs
 			{
 				livingHealth.SetBrain(null);
 			}
+
+			(LivingHealthMaster.ObjectBehaviour as MovementSynchronisation).ServerSillyWalk.RemovePosition(this);
 
 			livingHealth.IsMute.RemovePosition(this);
 			Itself.SetPossessingObject(null);
---
+++ UnityProject/Assets/Scripts/UI/Core/Animations/SillyWalk.cs
@@ -0,0 +1,89 @@
+using System;
+using Core.Physics;
+using HealthV2;
+using Logs;
+using Player.Movement;
+using UnityEngine;
+
+public class SillyWalk : MonoBehaviour
+{
+
+	public float UpAndDownMagnitude = 1;
+	public float RotateMagnitude = 70.88f;
+
+	public LivingHealthMasterBase health;
+	public MovementSynchronisation UOP;
+
+	public void OnEnable()
+	{
+		if (CustomNetworkManager.IsHeadless) return;
+		UpdateManager.Add(CallbackType.UPDATE, UpdateMe );
+	}
+
+	public void OnDisable()
+	{
+		if (CustomNetworkManager.IsHeadless) return;
+		UpdateManager.Remove(CallbackType.UPDATE, UpdateMe );
+	}
+
+	// Update is called once per frame
+    void UpdateMe()
+    {
+	    bool Animate = true;
+
+	    if (health.IsDead || health.IsCrit || health.IsSoftCrit)
+	    {
+		    Animate = false;
+	    }
+
+	    if (UOP.HasSillyWalk == false || UOP.IsCurrentlyFloating || UOP.IsFlyingSliding)
+	    {
+		    Animate = false;
+	    }
+
+	    if (UOP.AllowInput == false || UOP.CurrentMovementType == MovementType.Crawling)
+	    {
+		    Animate = false;
+	    }
+
+	    if (Animate == false)
+	    {
+		    transform.localPosition = Vector3.zero;
+		    transform.localRotation = Quaternion.identity;
+		    return;
+	    }
+
+	    float decimalPartx = UOP.transform.localPosition.x  - (float)Mathf.Floor(UOP.transform.localPosition.x );
+	    float decimalParty = UOP.transform.localPosition.y - (float)Mathf.Floor(UOP.transform.localPosition.y);
+
+	    float NonABD = 0;
+	    float NonABDlocalPosition = 0;
+	    if (decimalPartx > decimalParty)
+	    {
+		    NonABD = (UOP.transform.localPosition.x +0.25f  - (float)Mathf.Floor(UOP.transform.localPosition.x +0.25f )) - 0.5f;
+		    NonABDlocalPosition = (UOP.transform.localPosition.x  +0.5f - (float) Mathf.Floor(UOP.transform.localPosition.x +0.5f )) - 0.5f;
+	    }
+	    else
+	    {
+		    NonABD = (UOP.transform.localPosition.y +0.25f  - (float)Mathf.Floor(UOP.transform.localPosition.y +0.25f )) - 0.5f;
+		    NonABDlocalPosition = (UOP.transform.localPosition.y  +0.5f - (float) Mathf.Floor(UOP.transform.localPosition.y +0.5f)) - 0.5f;
+	    }
+
+	    //var NonABDlocalPosition =Mathf.Abs( Mathf.Max(decimalPartx, decimalParty) - 0.5f);
+
+	    //Loggy.Error("NonABD > " + NonABD);
+
+	    var biggest = Mathf.Abs(NonABD);
+	    var biggestlocalPosition = Mathf.Abs(NonABDlocalPosition);
+
+
+	    //Loggy.Error("biggest > " + biggest);
+
+	    var rotationy = (biggest -0.25f) * 2;
+	    //Loggy.Error("rotationy > " + rotationy);
+
+
+	    transform.localPosition =   new Vector3(0,biggestlocalPosition,0) * UpAndDownMagnitude;
+	    transform.localRotation  =   Quaternion.Euler(new Vector3(0,0,rotationy)* RotateMagnitude) ;
+    }
+}
