680cfea2fb1adbf42c2132b6f7980cee865bc745
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-04-09 15:36:58 +0200

    General cleanup for Mob AI and pathfinding, and new generic "follow mob" state for MobV2s (#10797)
    * Traversal Cancellation and MobTraversal cleanup
    
    * `Think()`
    
    * Fix Arials picking up shit they shouldn't touch
    
    * Update Attributes.cs
    
    * New generic AI state for MobV2s and clearer state management name
    
    * Update UpdateManager.cs
    
    * Update UpdateManager.cs
    
    * Update MobTraversal.cs
    
    ---------
    
    Co-authored-by: Bod9001 <Bod9001@gmail.com>

---
+++ UnityProject/Assets/Scripts/Mobs/BrainAI/States/GenericStates/GenericAiStateFollowMob.cs
@@ -0,0 +1,59 @@
+using AddressableReferences;
+using HealthV2;
+using Mobs.Traversal;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Mobs.BrainAI.States.GenericStates
+{
+	/// <summary>
+	/// A generic AI state that allows a mob to follow another mob. Will pathfind automatically once the target is far enough.
+	/// </summary>
+	public class GenericAiStateFollowMob : BrainMobState
+	{
+		[SerializeField] private AddressableAudioSource stateEnterSound;
+		[SerializeField] private AddressableAudioSource stateExitSound;
+		[SerializeField] private float minimumDistanceBeforePathfinding = 4f;
+
+		private MobTraversal pathfinder => master.Traversal;
+		[HideInInspector] public LivingHealthMasterBase MobToFollow = null;
+
+		public override void OnEnterState()
+		{
+			if (stateEnterSound != null) SoundManager.PlayNetworkedAtPos(stateEnterSound, LivingHealthMaster.gameObject.AssumedWorldPosServer());
+		}
+
+		public override void OnExitState()
+		{
+			if (stateExitSound != null) SoundManager.PlayNetworkedAtPos(stateExitSound, LivingHealthMaster.gameObject.AssumedWorldPosServer());
+		}
+
+		public override void OnUpdateTick()
+		{
+			if (MobToFollow == null)
+			{
+				master.RemoveAddState(this, null);
+				return;
+			}
+			if (LivingHealthMaster.IsSoftCrit || LivingHealthMaster.IsCrit)
+			{
+				return;
+			}
+			if (Vector2.Distance(MobToFollow.gameObject.TileLocalPosition(),
+				    LivingHealthMaster.gameObject.TileLocalPosition()) > minimumDistanceBeforePathfinding)
+			{
+				MobTraversal.TraversalDetails newDetails = new MobTraversal.TraversalDetails
+				{
+					TargetPosition = MobToFollow.gameObject.TileLocalPosition().To3Int(),
+					CancelOnSlip = true
+				};
+				UpdateManager.ThinkShot(() => _ = pathfinder.CancelQueueAndGenerateNewPathToFollow(newDetails), PeriodicUpdateInterval - 0.5f);
+			}
+		}
+
+		public override bool HasGoal()
+		{
+			return MobToFollow is not null;
+		}
+	}
+}
