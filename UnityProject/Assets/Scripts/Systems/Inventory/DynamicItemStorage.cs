880c601a655cb85b20a6fe70d8fdb0d3b1218d3e
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-04-13 09:56:16 +0200

    unblock error on lobby

--- UnityProject/Assets/Scripts/Systems/Inventory/DynamicItemStorage.cs
+++ UnityProject/Assets/Scripts/Systems/Inventory/DynamicItemStorage.cs
@@ -788,19 +788,27 @@ public class DynamicItemStorage : NetworkBehaviour, IOnPlayerRejoin, IOnControlP
 					}
 					WeakReference<DynamicItemStorage> wptr = new WeakReference<DynamicItemStorage>(this);
 
-					LoadManager.RegisterActionDelayed(() =>
+					try
 					{
-						DynamicItemStorage DIS;
-
-						int LocalTries = tries;
-						LocalTries++;
-
-						if (wptr.TryGetTarget(out DIS))
+						LoadManager.RegisterActionDelayed(() =>
 						{
-							DIS.ProcessChangeClient(newSt, LocalTries);
-						}
-					}, 60);
-					return;
+							DynamicItemStorage DIS;
+
+							int LocalTries = tries;
+							LocalTries++;
+
+							if (wptr.TryGetTarget(out DIS))
+							{
+								DIS.ProcessChangeClient(newSt, LocalTries);
+							}
+						}, 60);
+						return;
+					}
+					catch (Exception e)
+					{
+						Loggy.Error(e.ToString());
+						return;
+					}
 				}
 
 				bool Contain = false;
