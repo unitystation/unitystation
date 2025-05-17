c8844f069f5c7359f7fb7982f1faaee6174d0c3c
Autumn <55225562+AutumnNova@users.noreply.github.com>
2025-05-17 20:57:29 +1000

    Flamable fix and some spelling

--- UnityProject/Assets/Scripts/Health/Objects/Flammable.cs
+++ UnityProject/Assets/Scripts/Health/Objects/Flammable.cs
@@ -115,7 +115,7 @@ namespace Health.Objects
 				SyncOnFire(fireStacks, 0);
 				return;
 			}
-			node?.GasMixLocal.AddGas(Gas.Smoke, BURNING_DAMAGE_PER_STACK * 75, Kelvin.FromC(100f));
+			node?.GasMixLocal.AddGasWithTemperature(Gas.Smoke, BURNING_DAMAGE_PER_STACK * 75, Kelvin.FromC(100f));
 
 			if (integrity.Resistances.Flammable || skipFlammableCheck)
 			{
