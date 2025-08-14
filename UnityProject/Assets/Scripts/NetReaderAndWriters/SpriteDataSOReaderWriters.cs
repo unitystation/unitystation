b4bfaef5c06f70839e8d49df7dfca5268feaa32f
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-08-14 03:19:21 +0300

    Update SpriteDataSOReaderWriters.cs

--- UnityProject/Assets/Scripts/NetReaderAndWriters/SpriteDataSOReaderWriters.cs
+++ UnityProject/Assets/Scripts/NetReaderAndWriters/SpriteDataSOReaderWriters.cs
@@ -1,16 +1,39 @@
+using System;
+using Logs;
 using Mirror;
-using UnityEngine;
 
 public static class SpriteDataSOReaderWriters
 {
 	public static SpriteDataSO Deserialize(this NetworkReader reader)
 	{
-		var Index = reader.ReadUInt();
-		return SpriteCatalogue.ResistantCatalogue[(int)Index];
+		try
+		{
+			var index = reader.ReadUInt();
+			return SpriteCatalogue.ResistantCatalogue[(int)index];
+		}
+		catch (Exception e)
+		{
+			Loggy.Error($"An error occured while deserializing sprite data.\n {e}");
+			return SpriteCatalogue.ResistantCatalogue[0];
+		}
 	}
 
 	public static void Serialize(this NetworkWriter writer, SpriteDataSO spriteDataSo)
 	{
-		writer.WriteUInt((uint)spriteDataSo.SetID);
+		if (spriteDataSo == null)
+		{
+			Loggy.Warning("Null sprite detected. Giving default value of 0.");
+			writer.WriteUInt(0);
+			return;
+		}
+		try
+		{
+			writer.WriteUInt((uint)spriteDataSo.SetID);
+		}
+		catch (Exception e)
+		{
+			Loggy.Error($"An error occured while serializing sprite data.\n {e}");
+			writer.WriteUInt(0);
+		}
 	}
 }
