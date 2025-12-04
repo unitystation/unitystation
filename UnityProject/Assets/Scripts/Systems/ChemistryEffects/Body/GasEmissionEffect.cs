0b76f24f62707ce2db5adff49e3643e2aabd97bd
Atner <48405920+AtnerNT@users.noreply.github.com>
2025-12-05 05:51:57 +0800

    Playtest fixes (#11042)
    * RedPlagueNerfAndFix
    
    * MaintroomFixes
    
    * Update IDCard.asset
    
    * PlagueDoctor

---
+++ UnityProject/Assets/Scripts/Systems/ChemistryEffects/Body/GasEmissionEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Chemistry;
+using HealthV2;
+using HealthV2.Living.PolymorphicSystems.Bodypart;
+using ScriptableObjects.Atmospherics;
+using UnityEngine;
+
+namespace Chemistry.Effects
+{
+	[CreateAssetMenu(fileName = "GasEmission",
+		menuName = "ScriptableObjects/Chemistry/Effects/GasEmissionEffect")]
+	public class GasEmissionEffect : Chemistry.Effect
+	{
+		[SerializeField] private GasSO gasToEmit = null;
+		[SerializeField] private float amountOfGas = 1;
+
+		[SerializeField] private float emissionChance = 15;
+
+		public override void Apply(MonoBehaviour sender, float amount)
+		{
+			if (DMMath.Prob(emissionChance) == false) return;
+
+			if (sender is MetabolismComponent metabolismComponent == false) return;
+			if (metabolismComponent.RelatedPart.HealthMaster == false) return;
+
+			LivingHealthMasterBase healthMaster = metabolismComponent.RelatedPart.HealthMaster;
+			Vector3 actorPos = healthMaster.gameObject.AssumedWorldPosServer();
+
+			//Add gas to area (Typically Miasma)
+			if (gasToEmit != null && amountOfGas > 0)
+			{
+				MetaDataNode node = MatrixManager.GetMetaDataAt(actorPos.CutToInt());
+				node.GasMixLocal.AddGasWithTemperature(gasToEmit, amountOfGas, node.GasMixLocal.Temperature);
+			}
+		}
+	}
+}
