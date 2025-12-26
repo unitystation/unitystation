d5d53c7a2cda3f4081d279f0dc3d55f8cf8dc747
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-12-26 13:14:30 +0200

    Add the ability for V2 actions to use an object's SpriteHandler to set its button's icon (#11069)
    * Cleanup cooldown code for actionv2
    
    * Grab Icon from sprite handler for V2 Actions

--- UnityProject/Assets/Scripts/Actions/V2/ActionManager.cs
+++ UnityProject/Assets/Scripts/Actions/V2/ActionManager.cs
@@ -20,32 +20,6 @@ namespace Actions.V2
 
 		private const float MINIMUM_COOLDOWN_TIME = 0.085f;
 
-		[Serializable]
-		public class CooldownInfo : NetworkMessage
-		{
-			public string ActionId { get; private set; }
-			public DateTime CooldownEnd { get; private set; }
-
-			public CooldownInfo() { }
-			public CooldownInfo(string actionId, DateTime cooldownEnd)
-			{
-				ActionId = actionId;
-				CooldownEnd = cooldownEnd;
-			}
-
-			public void Serialize(NetworkWriter writer)
-			{
-				writer.WriteString(ActionId);
-				writer.WriteLong(CooldownEnd.Ticks);
-			}
-
-			public void Deserialize(NetworkReader reader)
-			{
-				ActionId = reader.ReadString();
-				CooldownEnd = new DateTime(reader.ReadLong());
-			}
-		}
-
 		public enum OnType
 		{
 			Body,
