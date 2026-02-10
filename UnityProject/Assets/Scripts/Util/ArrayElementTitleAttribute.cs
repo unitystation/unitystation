219bc4dfae10639f4acc65a8b80bafeecd1ec29c
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2026-02-10 09:07:06 +0200

    more build fixes

---
+++ UnityProject/Assets/Scripts/Util/ArrayElementTitleAttribute.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+	public class ArrayElementTitleAttribute : PropertyAttribute
+	{
+		public string Varname;
+		public string Nullname;
+		public ArrayElementTitleAttribute(string ElementTitleVar, string NullvalueString = "null")
+		{
+			Varname = ElementTitleVar;
+			Nullname = NullvalueString;
+		}
+	}
+}
--- UnityProject/Assets/Scripts/Util/ArrayElementTitleDrawer.cs
+++ UnityProject/Assets/Scripts/Util/ArrayElementTitleDrawer.cs
@@ -80,16 +80,5 @@ namespace Util
 			return "";
 		}
 	}
-
-	public class ArrayElementTitleAttribute : PropertyAttribute
-	{
-		public string Varname;
-		public string Nullname;
-		public ArrayElementTitleAttribute(string ElementTitleVar, string NullvalueString = "null")
-		{
-			Varname = ElementTitleVar;
-			Nullname = NullvalueString;
-		}
-	}
 }
 #endif
