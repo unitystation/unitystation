156b60e4efcc09c9be111b8118bd62fe2de9d4f7
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2026-02-24 18:45:39 +0200

    Add extension for chat

---
+++ UnityProject/Assets/Scripts/US13/Core/Chat/ChatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace US13.Core.Chat
+{
+	public static class ChatExtensions
+	{
+		/// <summary>
+		/// Helper function that calls Chat.AddActionMsgToChat with the given message and this gameobject as the originator.
+		/// </summary>
+		/// <param name="originator"></param>
+		/// <param name="everyoneMessage"></param>
+		public static void AddActionMsgToChat(this GameObject originator, string everyoneMessage)
+		{
+			Chat.AddActionMsgToChat(originator, everyoneMessage);
+		}
+	}
+}
