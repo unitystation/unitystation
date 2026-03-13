5641b47cb98a127018c15d67c736bbd37d116be3
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2026-03-13 02:26:35 +0200

    `UnityEvent` scanner tool (#11129)

---
+++ UnityProject/Assets/Scripts/US13/Core/Editor/Tools/Debugging/UnityEvents/EventEntry.cs
@@ -0,0 +1,17 @@
+﻿namespace US13.Core.Editor.Tools.Debugging.UnityEvents
+{
+	public class EventEntry
+	{
+		public string PrefabPath;
+		public string GameObjectPath;
+		public string ComponentName;
+		public string FieldName;
+		public int    ListenerCount;
+		public int    BrokenListenerCount;
+		public bool   HasListeners => ListenerCount > 0;
+		public bool   IsBroken     => BrokenListenerCount > 0;
+
+		public string DisplayPath;
+		public string SearchKey;
+	}
+}
