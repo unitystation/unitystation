958774960ac56d11054e9a8febbdd6d7d370c15c
Gilles <43683714+corp-0@users.noreply.github.com>
2026-03-15 20:44:04 -0300

    feat: make sure that no matter what interactions an item can have, if it is edible we can feed it by aiming to the mouth.

---
+++ UnityProject/Assets/Scripts/US13/Items/Food/Feedable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using US13.Core.Input_System.InteractionV2;
+using US13.Core.Input_System.InteractionV2.Interactions;
+using US13.Core.Input_System.InteractionV2.Interfaces;
+using US13.HealthV2;
+using Util;
+
+namespace US13.Items.Food
+{
+	/// <summary>
+	/// This makes it possible to always eat something that is edible but might have other interactions implementors that consume
+	/// the eating interaction. Players have to aim to the mouth for this to work.
+	/// </summary>
+	public class Feedable : MonoBehaviour, IFirstInteractable<HandApply>
+	{
+		public bool WillInteract(HandApply interaction, NetworkSide side)
+		{
+			if (interaction.TargetBodyPart != BodyPartType.Mouth) return false;
+			if (DefaultWillInteract.Default(interaction, side) == false) return false;
+			if (interaction.HandObject == null) return false;
+			if (!interaction.HandObject.TryGetComponentCustom<Edible>(out _)) return false;
+			return true;
+		}
+
+		public void ServerPerformInteraction(HandApply interaction)
+		{
+			var edible = interaction.HandObject.GetComponentCustom<Edible>();
+			edible.TryConsume(interaction.Performer, interaction.TargetObject);
+		}
+	}
+}
