2aa933b5e94db667e52bc83e854a113761cb1d95
Doobly <rob@smashingpixels.com.au>
2026-05-31 18:51:43 +1000

    Revert "Improve the null checks for optional directional / integrity references"
    This reverts commit e5dbb623a96e4c66d98cd8562f3b73720a348be5.

--- UnityProject/Assets/Scripts/US13/Core/Lighting/LightSource.cs
+++ UnityProject/Assets/Scripts/US13/Core/Lighting/LightSource.cs
@@ -140,28 +140,14 @@ namespace US13.Core.Lighting
 
 		private void OnEnable()
 		{
-			if (directional)
-			{
-				directional.OnRotationChange.AddListener(OnDirectionChange);
-			}
-
-			if (integrity)
-			{
-				integrity.OnApplyDamage += OnDamageReceived;
-			}
+			directional.OnRotationChange.AddListener(OnDirectionChange);
+			integrity.OnApplyDamage += OnDamageReceived;
 		}
 
 		private void OnDisable()
 		{
-			if (directional)
-			{
-				directional.OnRotationChange.RemoveListener(OnDirectionChange);
-			}
-
-			if (integrity)
-			{
-				integrity.OnApplyDamage -= OnDamageReceived;
-			}
+			directional.OnRotationChange.RemoveListener(OnDirectionChange);
+			if (integrity) integrity.OnApplyDamage -= OnDamageReceived;
 
 			UpdateManager.Remove(CallbackType.PERIODIC_UPDATE, TrySpark);
 		}
