b8fbc8a3cd7490cf3cbb09e202becca9e8b80ffd
Atner <48405920+AtnerNT@users.noreply.github.com>
2026-06-29 03:41:16 +0800

    feat: Vampire progress bar for HUD (#11284)
    * Vampire Progress Bar
    
    * Fixes
    
    * Networking for bar
    
    * UndoDiff

---
+++ UnityProject/Assets/Scripts/US13/Messages/Server/VampireHudUpdateMessage.cs
@@ -0,0 +1,37 @@
+﻿
+using Mirror;
+using US13.Player;
+using US13.Systems.Antagonists;
+
+namespace US13.Messages.Server
+{
+	public class VampireHudUpdateMessage : ServerMessage<VampireHudUpdateMessage.NetMessage>
+	{
+		public struct NetMessage : NetworkMessage
+		{
+			public int stage;
+			public float minNeededForStage;
+			public float currentCorruption;
+			public float neededCorruption;
+		}
+
+		public override void Process(NetMessage msg)
+		{
+			PlayerScript localPlayer = PlayerManager.LocalPlayerScript;
+			if (localPlayer.TryGetComponent<VampireStageProgression>(out var vampirePlayer) == false) return;
+			vampirePlayer.UpdateHudProgress(msg.stage, msg.currentCorruption, msg.minNeededForStage, msg.neededCorruption);
+		}
+
+		public static void SendTo(NetworkConnectionToClient conn, int _stage, float currentAmount, float minAmount, float maxAmount)
+		{
+			var msg = new NetMessage
+			{
+				stage = _stage,
+				currentCorruption = currentAmount,
+				minNeededForStage = minAmount,
+				neededCorruption =  maxAmount
+			};
+			SendTo(conn, msg);
+		}
+	}
+}
